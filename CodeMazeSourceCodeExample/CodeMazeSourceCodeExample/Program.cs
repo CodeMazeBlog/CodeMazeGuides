5ba73866d1cf473bb90bf133397682988007817c
Code Maze <codemazeblog@gmail.com>
2021-09-16 13:19:00 +0200

    An example solution

---
+++ CodeMazeSourceCodeExample/CodeMazeSourceCodeExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeMazeSourceCodeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CodeMaze example project");
+        }
+    }
+}
