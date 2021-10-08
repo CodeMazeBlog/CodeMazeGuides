583823360628ff3f8a03b4fde8ed8cda700d20d1
Muhammed Saleem P <muhammedsalimp@gmail.com>
2021-10-08 14:57:20 +0530

    CM-68 : Google Sheets and .NET Core with C# (#4)
    * CM-68 : Google Sheets and .NET Core with C#
    
    * addressed comments and suggestions
    
    * adding secrets for fixing tests
    
    * Fixed test naming
    
    * correcting test name
    
    * adding secrets back for fixing pipelines
    
    Co-authored-by: Muhammed Saleem <Muhammed.P@gds.ey.com>

---
+++ dotnet-client-libraries/Google Sheets API/GoogleSheetsAPI/GoogleSheetsHelper.cs
@@ -0,0 +1,40 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using System.IO;
+
+namespace GoogleSheetsAPI
+{
+    public class GoogleSheetsHelper
+    {
+        public SheetsService Service { get; set; }
+        const string APPLICATION_NAME = "GroceryStore";
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+
+        public GoogleSheetsHelper()
+        {
+            InitializeService();
+        }
+
+        private void InitializeService()
+        {
+            var credential = GetCredentialsFromFile();
+            Service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = APPLICATION_NAME
+            });
+        }
+
+        private GoogleCredential GetCredentialsFromFile()
+        {
+            GoogleCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+            }
+
+            return credential;
+        }
+    }
+}
