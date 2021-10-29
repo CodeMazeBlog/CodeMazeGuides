a9eb0d7201d6bf57385abdd6aec6e858b911ccd6
Code Maze <codemazeblog@gmail.com>
2021-10-29 19:34:50 +0200

    Example for testing

---
+++ testing-live-tests/ExampleTestProject/Tests/ExampleLiveTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+	[TestClass]
+	public class ExampleLiveTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(true, true);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			Assert.AreEqual(true, true);
+		}
+	}
+}
---
+++ testing-live-tests/ExampleTestProject/Tests/ExampleTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+	[TestClass]
+	public class ExampleTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(true, true);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			Assert.AreEqual(true, true);
+		}
+	}
+}
