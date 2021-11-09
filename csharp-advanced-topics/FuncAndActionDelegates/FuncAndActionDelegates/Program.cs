eef144f09576d19a37d310cd19f16156851462c0
DvD <dvd1202@gmail.com>
2021-11-09 13:49:46 +0100

    - Fluff code removed

--- csharp-advanced-topics/FuncAndActionDelegates/FuncAndActionDelegates/Program.cs
+++ csharp-advanced-topics/FuncAndActionDelegates/FuncAndActionDelegates/Program.cs
@@ -4,12 +4,8 @@ namespace FuncAndActionDelegates
 {
     public class Program
     {
-        public static void DelegateIntro()
-        {
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
