bdee583886dc3c52310291b98f4cebeb5491aa17
DvD <dvd1202@gmail.com>
2021-11-09 13:45:47 +0100

    - FuncAndActionDelegates first setup

---
+++ csharp-advanced-topics/FuncAndActionDelegates/FuncAndActionDelegates/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FuncAndActionDelegates
+{
+    public class Program
+    {
+        public static void DelegateIntro()
+        {
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
