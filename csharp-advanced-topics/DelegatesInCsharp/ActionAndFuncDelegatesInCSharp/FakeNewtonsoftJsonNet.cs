9d2f1f972c2a48578fa29051d77ddf7368e00176
Bartosz Jarmuz <Bartosz.Jarmuz@EntainGroup.com>
2021-11-14 22:11:29 +0100

    Added demo code for action and func eval article

---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionAndFuncDelegatesInCSharp/FakeNewtonsoftJsonNet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegatesInCSharp
+{
+    /// <summary>
+    /// This is not the JSON serializer you are looking for.
+    /// Used for demonstrating a JSON.NET-like interface without adding a nuget.
+    /// </summary>
+    public static class JsonConvert
+    {
+        public static string SerializeObject(object input) => input.ToString();
+    }
+}
---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ActionAndFuncDelegatesInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someClient = new SomeClient(MyJsonSerializer.SerializeAsJson, new CdWriter().Write);
+            someClient.DoBusinessLogic("Hello World!");
+            Console.ReadKey();
+        }
+    }
+
+    public class SomeClient
+    {
+        private readonly Func<object, string> serialize;
+        private readonly Action<string> persist;
+
+        public SomeClient(Func<object, string> serializerFunction, Action<string> persist)
+        {
+            this.serialize = serializerFunction;
+            this.persist = persist;
+        }
+
+        public void DoBusinessLogic(object data)
+        {
+            var serialized = this.serialize(data);
+            this.persist(serialized);
+        }
+    }
+
+    public class CdWriter
+    {
+        public void Write(string data) {Console.WriteLine($"Pretending to write [{data}] to CD...");}
+    }
+
+    public static class MyJsonSerializer
+    {
+        public static string SerializeAsJson(object input) => JsonConvert.SerializeObject(input);
+    }
+
+
+
+}
