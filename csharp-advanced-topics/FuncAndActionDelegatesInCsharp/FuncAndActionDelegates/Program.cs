f486d3cee4e6a70620be2791c6c69ea65aff9c8d
Domagoj Beti <domagoj.Beti@avy.onmicrosoft.com>
2021-11-16 17:25:00 +0100

    Add additional examples.

--- csharp-advanced-topics/FuncAndActionDelegatesInCsharp/FuncAndActionDelegates/Program.cs
+++ csharp-advanced-topics/FuncAndActionDelegatesInCsharp/FuncAndActionDelegates/Program.cs
@@ -10,9 +10,11 @@ namespace FuncAndActionDelegates
         {
             // LINQ example
             var numbers = new int[] { 0, 1, -2, 3, -4, 5, 6 };
+
             var absoluteNumbers = numbers
                 .Select(x => x < 0 ? -x : x)
                 .ToArray();
+
             var signNumbers = numbers
                 .Select(x => x < 0 ? -1 : x > 0 ? 1 : 0)
                 .ToArray();
@@ -20,15 +22,37 @@ namespace FuncAndActionDelegates
             var absoluteNumbersWithoutFunc = numbers
                 .Select(new AbsoluteSelector())
                 .ToArray();
+
             var signNumbersWithoutFunc = numbers
                 .Select(new SignSelector())
                 .ToArray();
+
+            // Advantages over traditional delegates
+            MathFunctions mathFunctions = new MathFunctions();
+
+            UnaryDelegate absoluteDelegate = new UnaryDelegate(mathFunctions.AbsoluteFunction);
+
+            UnaryDelegate signDelegate = new UnaryDelegate(mathFunctions.SignFunction);
+
+            // Func delegates
+
+            Func<int, int> absoluteFuncDelegate = delegate (int x)
+            {
+                return x < 0 ? -x : x;
+            };
+
+            Func<int, int> signFuncDelegate = delegate (int x)
+            {
+                return x < 0 ? -1 : x > 0 ? 1 : 0;
+            };
+
 
             // Multicast example
             Action<string> logger = (message) => { 
                 var log = $"Message from first logger: {message}"; 
                 // Send log somewhere
             };
+
             logger += (message) => { 
                 var log = $"Message from second logger: {message}";
                 // Send log somewhere
@@ -42,6 +66,7 @@ namespace FuncAndActionDelegates
             Action<object> contravarianceExample = (x) => {
                 var exampleVariable = x;
             };
+
             Action<string> x = contravarianceExample;
         }
     }
@@ -92,6 +117,27 @@ namespace FuncAndActionDelegates
         public int Selector(int item)
         {
             return item < 0 ? -1 : item > 0 ? 1 : 0;
+        }
+    }
+
+    /// <summary>
+    /// Explicitly defined delegate
+    /// </summary>
+    public delegate int UnaryDelegate(int x);
+
+    /// <summary>
+    /// Implementations for the delegates
+    /// </summary>
+    public class MathFunctions
+    {
+        public int AbsoluteFunction(int x)
+        {
+            return x < 0 ? -x : x;
+        }
+
+        public int SignFunction(int x)
+        {
+            return x < 0 ? -1 : x > 0 ? 1 : 0;
         }
     }
 }
