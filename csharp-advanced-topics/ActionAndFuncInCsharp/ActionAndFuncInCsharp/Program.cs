388ff34bfdb4d89c24341139b600af294d0ddfcb
unknown <eng.sarilakks@gmail.com>
2021-11-16 15:57:13 +0200

    Article Code

---
+++ csharp-advanced-topics/ActionAndFuncInCsharp/ActionAndFuncInCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿//Func Delegate
+Func<string, int> methodPointer= Count;
+int num = methodPointer.Invoke("C Sharp");
+
+Console.WriteLine(methodPointer("C sharp"));
+
+int Count(string s)
+{
+    return s.Length;
+}
+
+//Action Delegate
+Action<string> voidMethodPointer= PrintCount;
+voidMethodPointer("Show me the numbers");
+
+void PrintCount(string s)
+{
+    Console.WriteLine(s.Length);
+}
+
+
+
+
+
