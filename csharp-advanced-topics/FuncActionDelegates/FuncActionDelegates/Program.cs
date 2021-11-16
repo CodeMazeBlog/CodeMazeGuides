61c12b7204dd3b0036e753ee071b3b93663ff0ed
Ray Carneiro <ray.silva@msn.com>
2021-11-16 16:17:07 -0300

    add new FuncActionDelegates project

---
+++ csharp-advanced-topics/FuncActionDelegates/FuncActionDelegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FuncActionDelegates
+{
+    class Program
+    {
+        public static int SumTwoNumbers(int first, int second)
+        {
+            return first + second;
+        }
+        static void Main(string[] args)
+        {
+            Func<int, int, int> funcVariables = SumTwoNumbers;
+
+            int returnFromSum = funcVariables(100, 50);
+
+            Console.WriteLine("Sum of variables is:" + returnFromSum);
+
+            CallActionDelegate();
+        }
+
+        private static void CallActionDelegate()
+        {
+            Action<int> addCredit = delegate (int value)
+            {
+                Console.WriteLine("Added credit value: " + value);
+            };
+
+            addCredit(10);
+        }
+    }
+}
