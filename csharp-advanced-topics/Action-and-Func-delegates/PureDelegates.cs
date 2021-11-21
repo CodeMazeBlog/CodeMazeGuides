a42ef29acc3715c199575fd1c5aeb346e8e3c35e
izzumani <isaac.njiru@gmail.com#>
2021-11-21 22:15:55 +0300

    Test and UnitTests

---
+++ csharp-advanced-topics/Action-and-Func-delegates/PureDelegates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegatesInCsharp
+{
+    public class PureDelegates
+    {
+        //delegate <return type> <delegate-name> <parameter list>
+
+        private delegate string pureDelegate(string s);
+        
+        private static string PrintToScreen(string s)
+        {
+            
+            return s;
+        }
+        public static void ExecutePureDelegate (string names)
+        {
+            pureDelegate _pureDelegate = new pureDelegate(PrintToScreen);
+            Console.WriteLine(_pureDelegate(names));
+        }
+    }
+}
