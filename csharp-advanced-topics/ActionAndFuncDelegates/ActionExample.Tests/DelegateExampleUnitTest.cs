7b4f66697e5b2925ced0dd024794d3a765e05b20
Ahmet Arslan <arslanahmet1981@gmail.com>
2021-11-29 15:35:03 +0300

    Delegate Unit Test added.

---
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionExample.Tests/DelegateExampleUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Delegates.Tests
+{
+    [TestClass]
+    public class DelegateExampleUnitTest
+    {
+        delegate void MyDelegateMathFuction(int x, int y);
+        static void MyAddFuction(int x, int y)
+        {
+            Console.Write($"{x + y}");
+        }
+
+        [TestMethod]
+        public void givenDelegate_whenSendingParameters_thenConsoleOutputEqual()
+        {
+            var currentConsoleOut = Console.Out;
+
+            using (var consoleOutput = new ConsoleOutput())
+            {
+                MyDelegateMathFuction delFunction = null;
+
+                delFunction = new MyDelegateMathFuction(MyAddFuction);
+                delFunction.Invoke(2, 3);
+
+                Assert.AreEqual("5", consoleOutput.GetOuput());
+            }
+
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+        }
+
+    }
+}
