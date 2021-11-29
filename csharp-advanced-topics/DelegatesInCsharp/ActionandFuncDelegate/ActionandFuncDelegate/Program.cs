fcaee4e613624525c5317f5bb2c01a2c6e1d315c
Ali Hanif <alihanif018@gmail.com>
2021-11-29 19:54:59 +0500

    Action and fucn delegates by Ali
    Action and Func delegates with examples .

---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionandFuncDelegate/ActionandFuncDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ActionandFuncDelegate
+{
+    class Program
+    {   
+        public static double sum(int a , double b)
+        {
+            return a + b;
+        }
+        public static void PrintSum(int a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Main(string[] args)
+        { 
+            //Func Delegate
+            Func<int,double,double> funcDelegate = (a,b)=> a+b;
+            //Action Delegate
+            Action<int, double> Actiondelegate = (a,b) => Console.WriteLine(a+b);
+            var result = funcDelegate.Invoke(2, 3.5); // Call a Func Delegate
+            Console.WriteLine(result);
+            Actiondelegate.Invoke(2, 3.5); // Call an Action Delegate
+            
+        }
+    }
+}
---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionandFuncDelegate/LINQ Function With Action and Func Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Function_With_Action_and_Func_Delegates
+{
+    class Program
+    {
+        // Class of Employee
+        public class Employee
+        {
+            public string Name { get; set; }
+            public double Salary { get; set; }
+            public float Experience { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            // List of Employees
+            List<Employee> employees  = new List<Employee>();
+            // Let's Add some Data in List
+            employees.Add(new Employee { Name = "Mark", Salary = 1000, Experience = 3 });
+            employees.Add(new Employee { Name = "John", Salary = 2000, Experience = 4 });
+            employees.Add(new Employee { Name = "Daniel", Salary = 3000, Experience = 5 });
+
+            // Let's use some LINQ function that Use Func Delegate
+            // There are several LINQ function that uses Fuction delegate , we can either pass Fucntion reference or use Lambda expressions to get data .
+            
+            var averageSalary = employees.Average(x => x.Salary);
+            Console.WriteLine($"Employee Average Salary is {averageSalary}");
+
+            // Let's Try some fucntion with Action Delegate
+
+            employees.ForEach(x => { if (x.Experience > 3) Console.WriteLine($"Employees Whose Experience is greater than 1 : {x.Name}"); });
+
+        }
+    }
+}
+
