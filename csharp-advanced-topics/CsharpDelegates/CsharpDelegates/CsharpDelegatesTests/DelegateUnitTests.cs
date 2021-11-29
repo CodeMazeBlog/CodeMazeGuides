caa6bf73fe7d91930549f94e5f95d694b0bb3a8a
Martin Chege <martinkanyoroki@gmail.com>
2021-11-29 21:08:41 +0300

    Implemented Action and Func delegates in C#

---
+++ csharp-advanced-topics/CsharpDelegates/CsharpDelegates/CsharpDelegatesTests/DelegateUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CsharpDelegates;
+
+namespace CsharpDelegatesTests
+{
+    [TestClass]
+    public class DelegateUnitTests
+    {
+        [TestMethod]
+        public void TestCalcProduct()
+        {
+            int first = 20;
+            int second = 20;
+            int expected = 400;
+            int actual = Program.CalcProduct(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+
+       
+    }
+}
