077ed68e4aed13036d8d5c166450ad381ee17b6c
Bartosz Jarmuz <Bartosz.Jarmuz@EntainGroup.com>
2021-11-30 22:56:48 +0100

    Added multithreading sample app

---
+++ csharp-advanced-topics/MultithreadingInCsharp/RunCodeInNewThreadApplication/PdfValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace RunCodeInNewThreadApplication
+{
+    public class PdfValidator
+    {
+        public ValidationResult ValidateFile()
+        {
+            //let's pretend this is a long, CPU-intensive work.
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            return new ValidationResult();
+        }
+    }
+
+    public class ValidationResult
+    {
+        public string ResultCode { get; } = Guid.NewGuid().ToString();
+    }
+}
