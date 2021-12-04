5ee9c487d17e7cd09f86446d32956ce7821af160
Wahid Bitar <wahid.wb@gmail.com>
2021-12-04 16:17:04 +0300

    added simple ActionsAndFuncs project

---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionsAndFuncs/DummyWorker.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ActionsAndFuncs
+{
+    internal class DummyWorker
+    {
+        private readonly int fakeValue;
+
+        public DummyWorker(int fakeValue)
+        {
+            this.fakeValue = fakeValue;
+        }
+
+        public double DoTheFuncJob(Func<int, double> operation)
+        {
+            //You'll do a heavy work here ^_^
+            return operation(fakeValue);
+        }
+
+        public void DoTheActionJob(Action<double> operation)
+        {
+            operation(fakeValue);
+        }
+    }
+}
---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionsAndFuncs/Program.cs
@@ -0,0 +1,32 @@
+﻿using ActionsAndFuncs;
+
+
+var dummy = new DummyWorker(1);
+
+var powResult = dummy.DoTheFuncJob(SimplePow);
+Console.WriteLine($"The result of SimplePow: {powResult}");
+
+var duplicateResult = dummy.DoTheFuncJob(DuplicateIt);
+Console.WriteLine($"The result of duplicateResult: {duplicateResult}");
+
+var inlineResult = dummy.DoTheFuncJob(x => x * 5);
+Console.WriteLine($"The result of inlineResult: {inlineResult}");
+
+var inlineResult2 = dummy.DoTheFuncJob(x => { return x / 5; });
+Console.WriteLine($"The result of inlineResult2: {inlineResult2}");
+
+//inline Action taks one parameter
+dummy.DoTheActionJob(x => Console.WriteLine($"The value of the fakeValue: {x}"));
+
+
+
+// Dummy methods to be passed as Func
+static double SimplePow(int number)
+{
+    return Math.Pow(number, 2);
+}
+
+static double DuplicateIt(int number)
+{
+    return number * 2;
+}
