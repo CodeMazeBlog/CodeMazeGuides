264007fbd2d07a7e8a2f4a8b1e20e218b6ab3452
Domagoj Beti <domagoj.Beti@avy.onmicrosoft.com>
2021-12-05 14:50:25 +0100

    Code refactoring.

--- async-csharp/CancellationTokensIAsyncEnumerable/CancellationIAsyncEnumerable/Program.cs
+++ async-csharp/CancellationTokensIAsyncEnumerable/CancellationIAsyncEnumerable/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,10 +9,65 @@ namespace CancellationIAsyncEnumerable
     {
         static async Task Main(string[] args)
         {
+            await IndefinitelyRunningMethod();
+
+            await IndefinitelyRunningMethodCancelled();
+
             await CompilerGeneratingExample();
-            await CancelledIndefiniteEnumerationExample();
-            await NotCancelledIndefiniteEnumerationWithCancellationExample();
-            await CancelledIndefiniteEnumerationWithCancellationExample();
+
+            await IndefinitelyRunningWrappedMethodCancelled();
+        }
+
+        public static async Task IndefinitelyRunningMethod()
+        {
+            var indefinitelyRunningRange = GetIndefinitelyRunningRangeAsync();
+
+            await foreach (int index in indefinitelyRunningRange)
+            {
+                // Do something with the index 
+            }
+        }
+
+        public static async Task IndefinitelyRunningMethodCancelled()
+        {
+            var cancellationTokenSource = new CancellationTokenSource(); 
+            cancellationTokenSource.CancelAfter(7000);
+
+            var indefinitelyRunningRange = GetIndefinitelyRunningRangeAsync(cancellationTokenSource.Token);
+
+            await foreach (int index in indefinitelyRunningRange)
+            {
+                // Do something with the index 
+            }
+        }
+
+        public static async Task IndefinitelyRunningWrappedMethodCancelled()
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(7000);
+
+            var indefinitelyRunningRange = GetIndefinitelyRunningRangeWrapperAsync();
+
+            await foreach (int index in indefinitelyRunningRange.WithCancellation(cancellationTokenSource.Token))
+            {
+                // Do something with the index 
+            }
+        }
+
+        private static IAsyncEnumerable<int> GetIndefinitelyRunningRangeWrapperAsync()
+        {
+            return GetIndefinitelyRunningRangeAsync();
+        }
+
+        private static async IAsyncEnumerable<int> GetIndefinitelyRunningRangeAsync(
+            [EnumeratorCancellation] System.Threading.CancellationToken cancellationToken = default)
+        {
+            int index = 0;
+            while (true)
+            {
+                await Task.Delay(5000, cancellationToken);
+                yield return index++;
+            }
         }
 
         private static async Task CompilerGeneratingExample()
@@ -35,107 +89,12 @@ namespace CancellationIAsyncEnumerable
             }
         }
 
-        public static async Task CancelledIndefiniteEnumerationExample()
-        {
-            static async IAsyncEnumerable<int> GetIndefinitelyRunningRangeAsync(
-                System.Threading.CancellationToken cancellationToken = default)
-            {
-                int index = 0;
-                while (true)
-                {
-                    await Task.Delay(5000, cancellationToken);
-                    yield return index++;
-                }
-            }
-
-            var cancellationTokenSource = new CancellationTokenSource();
-            cancellationTokenSource.CancelAfter(7000);
-
-            var indefinitelyRunningRange = GetIndefinitelyRunningRangeAsync(cancellationTokenSource.Token);
-
-            await foreach (int index in indefinitelyRunningRange)
-            {
-                // Do something with the index 
-            }
-        }
-
-        public static async Task NotCancelledIndefiniteEnumerationWithCancellationExample()
-        {
-            static async IAsyncEnumerable<int> GetIndefinitelyRunningRangeAsync(
-                System.Threading.CancellationToken cancellationToken = default)
-            {
-                int index = 0;
-                while (true)
-                {
-                    await Task.Delay(5000, cancellationToken);
-                    yield return index++;
-                }
-            }
-
-            static IAsyncEnumerable<int> GetIndefinitelyRunningRangeWrapperAsync()
-            {
-                return GetIndefinitelyRunningRangeAsync();
-            }
-
-            var cancellationTokenSource = new CancellationTokenSource();
-            cancellationTokenSource.CancelAfter(7000);
-
-            var indefinitelyRunningRange = GetIndefinitelyRunningRangeWrapperAsync();
-            await foreach (int index in indefinitelyRunningRange.WithCancellation(cancellationTokenSource.Token))
-            {
-                // Do something with the index
-            }
-        }
-
-        public static async Task CancelledIndefiniteEnumerationWithCancellationExample()
-        {
-            var cancellationTokenSource = new CancellationTokenSource();
-            cancellationTokenSource.CancelAfter(7000);
-
-            var indefinitelyRunningRange = GetIndefinitelyRunningRangeWrapperAsync();
-            await foreach (int index in indefinitelyRunningRange.WithCancellation(cancellationTokenSource.Token))
-            {
-                // Do something with the index
-            }
-        }
-        private static IAsyncEnumerable<int> GetIndefinitelyRunningRangeWrapperAsync()
-        {
-            return GetIndefinitelyRunningRangeAsync();
-        }
-
-        private static async IAsyncEnumerable<int> GetIndefinitelyRunningRangeAsync(
-            [EnumeratorCancellation] System.Threading.CancellationToken cancellationToken = default)
-        {
-            int index = 0;
-            while (true)
-            {
-                await Task.Delay(5000, cancellationToken);
-                yield return index++;
-            }
-        }
-
-        async Task IndefinitelyRunningTask(CancellationToken cancellationToken)
+        private async Task IndefinitelyRunningTask(CancellationToken cancellationToken)
         {
             while (true)
             {
                 await Task.Delay(5000);
                 cancellationToken.ThrowIfCancelled();
-            }
-        }
-    }
-
-    public class CancellationToken
-    {
-        public bool IsCancelled { get; set; }
-        public void Cancel()
-        {
-            IsCancelled = true;
-        }
-        public void ThrowIfCancelled()
-        {
-            if (IsCancelled)
-            {
-                throw new OperationCanceledException();
             }
         }
     }
