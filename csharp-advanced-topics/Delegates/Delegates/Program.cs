e1d624d847a08a857464aecb0c00d8205c647bd8
Martin Chege <martinkanyoroki@gmail.com>
2021-12-06 13:14:53 +0300

    Made th code cleaner to demonstrate the use of delegates in C# by Martin

--- csharp-advanced-topics/Delegates/Delegates/Program.cs
+++ csharp-advanced-topics/Delegates/Delegates/Program.cs
@@ -2,28 +2,39 @@
 
 namespace Delegates
 {
-   public class Program
+    public class Program
     {
+
         //define a function that takes two numbers and returns their product 
         public static int CalcProduct(int firstNum, int secondNum)
         {
             return firstNum * secondNum;
         }
+
         //define a void function that prints the product of two numbers
         public static void PrintProduct(int result)
         {
             Console.WriteLine("The product is {0}", result);
         }
+
         static void Main(string[] args)
         {
+            //define and declare variables to hold two numbers 
+            int firstNum = 20, secondNum = 20;
+
             //define a func delegate that takes two integer inputs and returns an integer output
             Func<int, int, int> product = CalcProduct;
-            int result = product(10, 10);
+
+            //assign the calculated value to a variable
+            int result = product(firstNum, secondNum);
+           
+
             //create an action delegate
             Action<int> printDelegate = PrintProduct;
-            //invoke the delegate 
+
+            //invoke the action delegate 
             printDelegate(result);
-            Console.ReadLine();
+            
         }
     }
 }
