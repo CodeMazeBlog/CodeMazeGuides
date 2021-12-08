c4201b47708dbd6335af692c8d7d0da80a4c7bce
Himanshu Kumar <hiku@microsoft.com>
2021-12-09 00:55:43 +0530

    code for evaluation article

---
+++ himanshu-evaluation-article-code/ConceptualFuncDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConceptualFuncDelegate
+{
+	public class FuncyMath
+	{
+		public int DoubleUp(int num)
+		{
+			return (num * 2);
+		}
+	}
+
+	public class FuncDelegateConcept
+	{
+		public static void Main()
+		{
+			FuncyMath foo = new FuncyMath();
+			Func<int, int> funcFunctionPointer = foo.DoubleUp;
+			int result = funcFunctionPointer(5);
+			Console.WriteLine(result);
+			Console.ReadLine();
+		}
+	}
+
+}
