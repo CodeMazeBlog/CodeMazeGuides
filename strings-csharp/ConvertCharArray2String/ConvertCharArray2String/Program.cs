f55797afac3de4de63145bb476fdd0ff33bf6b3f
Ahmet Arslan <arslanahmet1981@gmail.com>
2021-12-09 00:59:14 +0300

    First Commit

---
+++ strings-csharp/ConvertCharArray2String/ConvertCharArray2String/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text;
+
+namespace ConvertCharArray2String
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            char[] charArray = { 'c', 'o', 'd', 'e', ' ', 'm', 'a', 'z', 'e' };
+
+            var convertedToString = new string(charArray);
+
+            Console.WriteLine(convertedToString);
+
+            var joinedToString = string.Join("", charArray);
+            
+            Console.WriteLine(joinedToString);
+
+            var concattedToString = string.Concat(charArray);
+
+            Console.WriteLine(concattedToString); 
+
+            Console.ReadLine();
+        }
+    }
+}
