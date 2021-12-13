4637d97bc33ca5b6660dfa992637ab26319f8b71
Ayub Kokabi <44271870+kokabi1365@users.noreply.github.com>
2021-12-13 19:47:32 +0330

    Refactor code and add some unit tests

--- csharp-advanced-topics/FuncActionDelegate/FuncActionDelegate/Program.cs
+++ csharp-advanced-topics/FuncActionDelegate/FuncActionDelegate/Program.cs
@@ -1,44 +1,20 @@
 ﻿public class Program
 {
-    public static void Main(string[] args)
+    public class Candidate
     {
-        var developers = new List<Candidate>
-        {
-            new Candidate("David", 20, 5),
-            new Candidate("Alex", 42, 10),
-            new Candidate("David", 45, 1)
-        };
-
-        EvaluateCandidates(developers, DisplayScore, CheckAge, CheckExperience);
-
-        Console.ReadKey();
-
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Experience { get; set; }
     }
 
-    private static void EvaluateCandidates(List<Candidate> candidates, Action<Candidate, int> displayScore, params Func<Candidate, bool>[] checkList)
-    {
-        foreach (var candidate in candidates)
-        {
-            int score = 0;
-
-            foreach (var check in checkList)
-            {
-                if (check(candidate))
-                    score += 5;
-
-            }
-
-            displayScore(candidate, score);
-        }
-    }
     private static bool CheckExperience(Candidate candidate)
     {
-        return candidate.Experience >= 3;
+        return candidate.Experience >= 3; // 5 scores
     }
 
     private static bool CheckAge(Candidate candidate)
     {
-        return candidate.Age < 40;
+        return candidate.Age < 40; // 5 scores
     }
 
     private static void DisplayScore(Candidate candidate, int score)
@@ -46,18 +22,37 @@
         Console.WriteLine($"{candidate.Name}'s score is {score}");
     }
 
-}
-
-public class Candidate
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public int Experience { get; set; }
-
-    public Candidate(string name, int age, int experience)
+    private static void EvaluateCandidates(List<Candidate> candidates, List<Func<Candidate, bool>> checks, Action<Candidate, int> displayScore)
     {
-        Name = name;
-        Age = age;
-        Experience = experience;
+        foreach (var candidate in candidates)
+        {
+            int score = 0;
+
+            foreach (var check in checks)
+            {
+                if (check(candidate))
+                    score += 5;
+            }
+
+            displayScore(candidate, score);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        var candidates = new List<Candidate>
+        {
+            new() { Name = "David",Age = 20,Experience = 5},
+            new() { Name = "Alex",Age = 42,Experience = 10},
+            new() { Name = "Bill",Age = 45,Experience = 1}
+        };
+
+        Action<Candidate, int> displayScoreAction = DisplayScore;
+
+        var checkFuncDelegates = new List<Func<Candidate, bool>> { CheckAge, CheckExperience };
+
+        EvaluateCandidates(candidates, checkFuncDelegates, displayScoreAction);
+
+        Console.ReadKey();
     }
 }
--- csharp-advanced-topics/FuncActionDelegate/Tests/FuncActionDelegateTests.cs
+++ csharp-advanced-topics/FuncActionDelegate/Tests/FuncActionDelegateTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -5,6 +8,82 @@ namespace Tests
     [TestClass]
     public class FuncActionDelegateTests
     {
-        //TODO Add test case
+        public class Candidate
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int Experience { get; set; }
+        }
+
+        private static bool CheckExperience(Candidate candidate)
+        {
+            return candidate.Experience >= 3;
+        }
+
+        private static bool CheckAge(Candidate candidate)
+        {
+            return candidate.Age < 40;
+        }
+
+        private static void DisplayScore(Candidate candidate, int score)
+        {
+            Console.WriteLine($"{candidate.Name}'s score is {score}");
+        }
+
+        [TestMethod]
+        public void whenActionDelegate_DelegateInvocationListNotEmpty()
+        {
+            Action<Candidate,int> executeDisplayScoreAction = DisplayScore;
+            var invocationList = executeDisplayScoreAction.GetInvocationList();
+            Assert.AreEqual(invocationList.Length,1);
+        }
+
+        [TestMethod]
+        public void whenFuncDelegate_DelegateInvocationListNotEmpty()
+        {
+            Func<Candidate, bool> checkAgeFunc = CheckAge;
+            Func<Candidate, bool> checkExperienceFunc = CheckExperience;
+            
+            var invocationList1 = checkAgeFunc.GetInvocationList();
+            var invocationList2 = checkExperienceFunc.GetInvocationList();
+
+            Assert.AreEqual(invocationList1.Length, 1);
+            Assert.AreEqual(invocationList2.Length, 1);
+        }
+
+        [TestMethod]
+        public void whenActionDelegate_DelegateExecutesTheReferenceMethod()
+        {
+            var candidate = new Candidate { Name = "David", Age = 20, Experience = 5 };
+            Action<Candidate, int> executeDisplayScoreAction= DisplayScore;
+
+            try
+            {
+                executeDisplayScoreAction(candidate, 10);
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void givenCandidate_whenFuncDelegates_DelegateDisplayScore()
+        {
+            var candidate = new Candidate { Name = "David", Age = 20, Experience = 5 };
+            var checkFuncDelegates = new List<Func<Candidate, bool>> { CheckAge, CheckExperience };
+            int score = 0;
+
+            foreach (var check in checkFuncDelegates)
+            {
+                if (check(candidate))
+                {
+                    score += 5;
+                }
+            }
+
+            Assert.AreEqual(score,10);
+        }
     }
 }
