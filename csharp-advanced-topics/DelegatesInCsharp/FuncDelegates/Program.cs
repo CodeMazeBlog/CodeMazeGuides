ca456357767237af9da135656e22d93b39a6e1e2
git-sarita <95922934+git-sarita@users.noreply.github.com>
2021-12-13 16:03:33 +0530

    added func delegate

---
+++ csharp-advanced-topics/DelegatesInCsharp/FuncDelegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace FuncDelegates
+{
+    class Program
+    {
+        static string GetTitleCaseMessage(string msg)
+        {
+            string msgTitleCase = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(msg.ToLower());
+            return $"Message in Title Case: {msgTitleCase}";
+        }
+
+        static void Main(string[] args)
+        {
+            //*** Use of Func delegate ***//
+            Console.WriteLine("\n*************Func Delegate*************");
+            Func<string, string> message = GetTitleCaseMessage;
+            string result = message("write some text here");
+            Console.WriteLine(result);
+
+            //*** Use of Func delegate with anonymous method ***//
+            Console.WriteLine("\n*************Func Delegate with Anonymous Method*************");
+            Func<string, string> strTitleCase = delegate (string str)
+                                                {
+                                                    return GetTitleCaseMessage(str);
+                                                };
+            string strResult = strTitleCase("write some text here");
+            Console.WriteLine(strResult);
+
+            //*** Use of Func delegate with Lambda expression ***//
+            Console.WriteLine("\n*************Func Delegate with Lambda Expression*************");
+            Func<string, string> txtTitleCase = t => GetTitleCaseMessage(t);
+            string res = txtTitleCase("write some text here");
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+}
