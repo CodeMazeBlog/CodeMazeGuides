4f51e7acddfc336b507470bfcc69b95ed794530f
Harshit Gupta <napsterhopes@gmail.com>
2021-12-14 23:18:12 +0530

    CM - FuncAndActionInCSharp

---
+++ csharp-advanced-topics/FuncAndActionInCSharp/FuncAndAction/EntryPoint.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FuncAndAction
+{
+    public class EntryPoint
+    {
+        static void Main()
+        {
+            string[] names = { "Alice", "John", "Bobby", "Kyle", "Scott", "Tod", "Sharon", "Armin", "George" };
+            
+            Func<string, bool> lessThanFive = x => x.Length < 5;
+
+            List<string> namesLessThanFiveChars = ExtractStrings(names, lessThanFive);
+ 
+            Console.WriteLine("All names: " + string.Join(", ", names));
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("Names less than five chars: " + string.Join(", ", namesLessThanFiveChars));
+            Console.WriteLine(new string('-', 40));
+            
+            // Action Delegate Example
+            Action<string> printer = Print;
+            printer += Print;
+
+            printer("Message");
+        }
+
+        public static List<string> ExtractStrings(string[] array, Func<string, bool> filter)
+        {
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (filter(array[i]))
+                {
+                    result.Add(array[i]);
+                }
+            }
+
+            return result;
+        }
+
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ csharp-advanced-topics/FuncAndActionInCSharp/Tests/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FuncAndAction;
+using System.Collections.Generic;
+
+namespace Tests
+{
+	[TestClass]
+	public class Tests
+	{
+		string[] names = { "Alice", "John", "Bobby", "Kyle", "Scott", "Tod", "Sharon", "Armin", "George" };
+		Func<string, bool> lessThanFive = x => x.Length < 5;
+
+		private static List<string> tempLessThanFive(string[] items) 
+		{
+			List<string> tempNamesLessThanFiveChars = new List<string>();
+
+			foreach (var item in items)
+            {
+				if(item.Length < 5)
+				{ 
+				tempNamesLessThanFiveChars.Add(item);
+				}
+			}
+			return tempNamesLessThanFiveChars;
+		}
+
+		[TestMethod]
+		public void whenStringsAreSent_FuncExtractsStringWithLengthLessThanFiveChars()
+		{
+			List<string> namesLessThanFiveChars = EntryPoint.ExtractStrings(names,lessThanFive);
+			Assert.AreEqual(tempLessThanFive(names).Count, namesLessThanFiveChars.Count);
+		}
+
+        [TestMethod]
+        public void whenStringIsSent_ActionPrints()
+        {
+            EntryPoint.Print("Print Something");
+			Assert.IsTrue(true);
+        }
+
+    }
+}
