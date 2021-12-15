049e559b5fefa08ebb1cfea86efb835bd0ac635a
= <=>
2021-12-16 00:11:07 +0330

    added Action and Func Delegates in CSharp

---
+++ csharp-advanced-topics/ActionAndFuncInCSharp/ActionAndFuncInCSharp/Calculator.cs
@@ -0,0 +1,49 @@
+﻿class CalculatorController
+{
+    public static void Main(string[] args)
+    {
+        var calculator = new Calculator
+        {
+            Print = n => Console.WriteLine($"*** {n} ***")
+        };
+        calculator.SetOperation('+');
+        calculator.Calculate(4, 2);
+        calculator.Print += n => Console.WriteLine("*********");
+        calculator.Calculate(-3, 4);
+    }
+
+    class Calculator
+    {
+        public Action<int> Print { get; set; }
+
+        public Func<int, int, int> Operate { get; set; }
+
+        public void SetOperation(char operation)
+        {
+            switch (operation)
+            {
+                case '+':
+                    Operate = (a, b) => a + b;
+                    break;
+                case '-':
+                    Operate = (a, b) => a - b;
+                    break;
+                case '*':
+                    Operate = (a, b) => a * b;
+                    break;
+                case '/':
+                    Operate = (a, b) => a / b;
+                    break;
+                default:
+                    Operate = (a, b) => -1;
+                    break;
+            }
+        }
+
+        public void Calculate(int a, int b)
+        {
+            int result = Operate(a, b);
+            Print(result);
+        }
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncInCSharp/ActionAndFuncInCSharp/Examples.cs
@@ -0,0 +1,45 @@
+﻿class CodeMaze
+{
+    void exampleOne()
+    {
+        Func<int, int, int> Calculate = Add; // our func that will calculate the result
+        Action<int> Print = PrintWithAsterisk; // our action that will print the result
+        int result = Calculate(3, 4); // calculating the result
+        Print(result); // printing it
+    }
+
+    int Add(int a, int b) // takes two integers and returns the sum
+    {
+        return a + b;
+    }
+
+    void PrintWithAsterisk(int n) // takes an integer and prints it in a custom format
+    {
+        Console.WriteLine($"*** {n} ***");
+    }
+
+    void exampleTwo()
+    {
+        Func<int, int, int> calculate = (a, b) => a - b;
+        Action<int> print = n => Console.WriteLine($"The result is: {n}");
+        int result = calculate(8, 2);
+        print(result);
+    }
+
+    void exampleThree()
+    {
+        Func<int, int, int> calculate = delegate(int a, int b) { return a * b; };
+        Action<int> print = delegate(int n) { Console.WriteLine($"The result is: {n}"); };
+        int result = calculate(2, 2);
+        print(result);
+    }
+
+    void exampleFour()
+    {
+        Func<int, int, int> calculate = delegate(int a, int b) { return a * b; };
+        Action<int> print = delegate(int n) { Console.WriteLine($"The result is: {n}"); };
+        int result = calculate(2, 2);
+        print += n => Console.WriteLine("*****************");
+        print(result);
+    }
+}
