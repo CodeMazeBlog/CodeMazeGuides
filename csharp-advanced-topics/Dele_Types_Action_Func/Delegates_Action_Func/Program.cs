78ed0b3d8f08ac554c32f353ab10c2c03da1e00b
achinya <95957752+achinya@users.noreply.github.com>
2021-12-16 14:48:47 +0530

    Add files via upload
    main project files - class file for the delegate example and the project file

---
+++ csharp-advanced-topics/Dele_Types_Action_Func/Delegates_Action_Func/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Delegates_Action_Func
+{
+    class Program
+    {
+        public delegate void MyMessage(string message);
+        static void Main(string[] args)
+        {
+            // working for a normal delegate.
+            MyMessage Val = Greeting.DisplayMsg;
+            Val("Good Morning");
+
+            //Working of an action delegate.
+            //declare and use an action delegate here. // delegate has two parameters. 
+            Action<int, int> val = myMath;
+            val(20, 5);
+
+            //Working of a Func Delegate.
+            //declare and use a func delegate with 2 input and one out parameter.
+            Func<int, int, int> SumDelegate = Sum;
+            Console.WriteLine("Func Delegate implemented - Output:" + SumDelegate(10, 20));
+            Console.ReadKey();
+
+        }
+        //Method to show Action delegate implementation
+        public static void myMath(int a, int b)
+        {
+            Console.WriteLine("Action Delegate implemented - Output:" + (a - b));
+        }
+
+        //Method to show Func Delegate implementation
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+    //class to show delegate implementation.
+    class Greeting
+    {
+        public static void DisplayMsg(string message)
+        {
+            Console.WriteLine("This is a greeting through a delegate :" + message);
+        }
+    }
+}
