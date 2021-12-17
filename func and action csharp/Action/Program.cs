192f4f8c5e2d3716abb1ca1a91f1ccbdeede46f9
mohammedisilverkey <mohammedi@silverkeytech.com>
2021-12-18 01:50:14 +0200

    Action Project

---
+++ func and action csharp/Action/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Action
+{
+    class Program
+    {
+        static void ConsolePrint(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        static void Main(string[] args)
+        {
+            Action<int> printActionDel = ConsolePrint;
+            printActionDel(10);
+
+            // You can initialize an Action delegate using the new keyword 
+
+            Action<int> printActionDelWithNewKeyword = ConsolePrint;
+
+            //Or
+
+            Action<int> printActionDelWithNewKeyword2 = new Action<int>(ConsolePrint);
+
+            // Anonymous method with Action delegate
+
+            Action<int> printActionDelWithAnonymousMethod = delegate(int i)
+            {
+                Console.WriteLine(i);
+            };
+
+            printActionDelWithAnonymousMethod(10);
+
+            // lambda method with Action delegate
+
+            Action<int> printActionDelWithLambda = i => Console.WriteLine(i);
+       
+            printActionDelWithLamda(10);
+        }
+    }
+}
