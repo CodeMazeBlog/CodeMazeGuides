8b75b16a6a3cfab6b659743c8c0797d3932ceade
Bartosz Jarmuż <bartosz.jarmuz@gmail.com>
2021-12-18 19:41:11 +0100

    Added second example

--- csharp-basic-topics/PatternMatchingInCSharp/PatternMatchingInCSharp/PropertyPatternsDemo/PropertyPatternsSample.cs
+++ csharp-basic-topics/PatternMatchingInCSharp/PatternMatchingInCSharp/PropertyPatternsDemo/PropertyPatternsSample.cs
@@ -6,8 +6,8 @@ namespace PropertyPatternsDemo
     public class PropertyPatternsSample
     {
         //let's declare a data model that is small, but contains enough property nesting to show our point
-        record Order(FinancialData FinancialData, Customer Customer);
-        record FinancialData(Price Price, DateTime? PaymentDate = null);
+        record Order(Payment Payment, Customer Customer);
+        record Payment(Price Price, DateTime? PaymentDate = null);
         record Price(string Currency, decimal Amount);
         record Customer(string Name, string Group);
 
@@ -17,10 +17,10 @@ namespace PropertyPatternsDemo
         {
             public static string Get(Order order) => order switch
             {
-                { FinancialData.Price.Currency: "BTC" } => "CryptoOrderProcessor",
-                { Customer.Group: "Banking", FinancialData.Price.Currency: "JPY" } => "JapaneseBankingProcessor",
-                { FinancialData.Price.Amount: >= 500000 } or { Customer.Group: "VIP" } => "ImportantOrderProcessor",
-              //{ FinancialData: { Price: { Amount: > 5000000} } } or { Customer: { Group: "VIP" } } => "ImportantOrderProcessor", <- this is the equivalent expression without using the C# 10 feature
+                { Payment.Price.Currency: "BTC" } => "CryptoOrderProcessor",
+                { Customer.Group: "Banking", Payment.Price.Currency: "JPY" } => "JapaneseBankingProcessor",
+                { Payment.Price.Amount: >= 500000 } or { Customer.Group: "VIP" } => "ImportantOrderProcessor",
+                //{ Payment: { Price: { Amount: > 5000000} } } or { Customer: { Group: "VIP" } } => "ImportantOrderProcessor",// <- this is the equivalent expression without using the C# 10 feature
                 _ => "DefaultOrderProcessor"
             };
         }
@@ -29,27 +29,56 @@ namespace PropertyPatternsDemo
         public void OrderProcessorFactory_ProperProcessorReturnedDependingOnOrder()
         {
             //first let's test a regular order
-            var standardOrder = new Order(new FinancialData(new Price("USD", 1000)), new Customer("HSBC", "Banking"));
+            var standardOrder = new Order(new Payment(new Price("USD", 1000)), new Customer("HSBC", "Banking"));
             Assert.AreEqual("DefaultOrderProcessor", OrderProcessorFactory.Get(standardOrder));
 
             //now let's make it match a bit more specific criteria
-            var highPriorityOrder = standardOrder with { FinancialData = new FinancialData(new Price("USD", 9900000)) };
+            var highPriorityOrder = standardOrder with { Payment = new Payment(new Price("USD", 9900000)) };
             Assert.AreEqual("ImportantOrderProcessor", OrderProcessorFactory.Get(highPriorityOrder));
-            Assert.AreEqual("ImportantOrderProcessor", OrderProcessorFactory.Get(highPriorityOrder with {FinancialData = new FinancialData(new Price("USD", 5)), Customer = new Customer("John Doe", "VIP")}));
+            Assert.AreEqual("ImportantOrderProcessor", OrderProcessorFactory.Get(highPriorityOrder with { Payment = new Payment(new Price("USD", 5)), Customer = new Customer("John Doe", "VIP")}));
 
             //now let's make a banking order in JPY
-            var japaneseOrder = new Order(new FinancialData(new Price("JPY", 33333333)), new Customer("Bank Of Japan", "Banking"));
+            var japaneseOrder = new Order(new Payment(new Price("JPY", 33333333)), new Customer("Bank Of Japan", "Banking"));
             Assert.AreEqual("JapaneseBankingProcessor", OrderProcessorFactory.Get(japaneseOrder));
 
             //and finally match the criteria with highest 'priority'
-            Assert.AreEqual("CryptoOrderProcessor", OrderProcessorFactory.Get(japaneseOrder with { FinancialData = new FinancialData( new Price("BTC", 100)) }));
+            Assert.AreEqual("CryptoOrderProcessor", OrderProcessorFactory.Get(japaneseOrder with { Payment = new Payment( new Price("BTC", 100)) }));
         }
 
+        //let's pretend this class enforces some business rules to ensure Orders are valid.
+        static class OrderValidator
+        {
+            public static void Validate(Order order)
+            {
+                //current syntax version (different rules would be validated further below)
+                if (order is { Payment.Price.Currency: "BTC" } and { Customer.Group: "Retail" })
+                {
+                    throw new InvalidOperationException("Crypto transactions not allowed for retail customers");
+                }
 
-        
+                //as above, but without extended property patters (C# 8.0)
+                if (order is { Payment: { Price: { Currency: "BTC" } } } and { Customer: { Group: "Retail" } })
+                {
+                    throw new InvalidOperationException("Crypto transactions not allowed for retail customers");
+                }
 
+                //the same without pattern matching
+                if (order.Payment.Price.Currency == "BTC" && order.Customer.Group == "Retail")
+                {
+                    throw new InvalidOperationException("Crypto transactions not allowed for retail customers");
+                }
+            }
+        }
 
+        [Test]
+        public void OrderValidator_ThrowsAnErrorWhenNeeded()
+        {
+            //first let's test an unallowed order type
+            var order = new Order(new Payment(new Price("BTC", 1000)), new Customer("Jim Beam", "Retail"));
+            Assert.Throws<InvalidOperationException>(() => OrderValidator.Validate(order));
 
-
+            //now let's modify the order so that it is allowed
+            Assert.DoesNotThrow(() => OrderValidator.Validate(order with { Customer = new Customer("Jack Black", "Enterprise")}));
+        }
     }
 }
