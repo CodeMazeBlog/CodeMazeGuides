a44d9ca133d9d49de75c719b20682c985b6db357
RPM1984 <rpmir1@gmail.com>
2021-12-20 14:35:41 +1100

    Added tests

--- csharp-basic-topics/RecordsInCSharp/RecordsInCSharpExample/Program.cs
+++ csharp-basic-topics/RecordsInCSharp/RecordsInCSharpExample/Program.cs
@@ -1,22 +1,14 @@
 namespace RecordsInCSharpExample;
 
-public abstract record Person(string FirstName, string LastName);
-public record Employee(string FirstName, string LastName, string Job) : Person(FirstName, LastName);
-
 public class Program
 {
     public static void Main(string[] args)
     {
-        var person1 = new Employee("Joe", "Bloggs" , "Firefigher");
-        var person2 = new Employee("Joe", "Bloggs", "Teacher");
-        var person3 = new Employee("Jane", "Bloggs", "Programmer");
-                
+        var person1 = new Person("Joe", "Bloggs");
+        var person2 = new Person("Joe", "Bloggs");
+        var person3 = new Person("Jane", "Bloggs");
         Console.WriteLine($"Person1 == Person2? {person1 == person2}");
         Console.WriteLine($"Person1 == Person3? {person1 == person3}");
-
-        var person4 = person3 with { LastName = "Doe" };
-        Console.WriteLine(person4);
-
         Console.ReadKey();
     }
 }
