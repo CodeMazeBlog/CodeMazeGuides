420072da2044294f645d9af0b8b5b8221e0b12da
Haris Tse <haris.tsetsekas@gmail.com>
2021-12-21 11:53:44 +0200

    CM-15 Polymorphism in C#

---
+++ csharp-intermediate-topics/PolymorphismInCsharp/PolymorphismInCsharp/CourierBranch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Polymorphism_v2;
+
+namespace Polymorphism
+{
+    public class CourierBranch
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Package> packages { get; set; }
+
+        public CourierBranch(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            packages = new List<Package>();
+        }
+
+        public void AddPackage(Package newPackage)
+        {
+            packages.Add(newPackage);
+        }
+
+        public decimal GetTotalCost()
+        {
+            decimal totalCost = 0;
+            foreach (var package in packages)
+            {
+                totalCost += package.GetDeliveryCost();
+            }
+            return totalCost;
+        }
+
+        public void PrintList()
+        {
+            foreach (var package in packages)
+            {
+                Console.WriteLine("Cost: " + package.GetDeliveryCost());
+                Console.WriteLine("Delivery date: " + package.GetDeliveryDate());
+            }
+        }
+    }
+}
