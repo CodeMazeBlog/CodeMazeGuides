fd1d3a5b465bb5eee53c511686a45feb2e8668e0
dgarcia202 <wft2000@gmail.com>
2021-12-22 10:18:08 +0100

    CM-293: Expression-bodied Members in C# (#120)

---
+++ csharp-basic-topics/ExpressionBodiedMembersInCsharp/ExpressionBodiedMembersInCsharp/Square.cs
@@ -0,0 +1,14 @@
+﻿namespace ExpressionBodiedMembersInCsharp
+{
+    public class Square
+    {
+        private double _side;
+
+        public Square(double side)
+        {
+            _side = side;
+        }
+
+        public double CalculateArea() => Math.Pow(_side, 2);
+    }
+}
