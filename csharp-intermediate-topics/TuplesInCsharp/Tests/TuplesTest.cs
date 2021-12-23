4a244c4ab65982d39eac37ea16110466ce21543b
ogubuike Alex <ogubuikealex@gmail.com>
2021-12-23 11:55:47 +0100

    Created Unit test

---
+++ csharp-intermediate-topics/TuplesInCsharp/Tests/TuplesTest.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class TuplesTest
+    {
+        [Theory]
+        [InlineData(20)]
+        [InlineData(46)]
+        [InlineData(92)]
+        public void MethodA_WithAnEvenNum_ReturnsATupleWithATrueValue(int num)
+        {
+            var tupleOne = Program.MethodA(num);
+
+            Assert.IsType<Tuple<bool, string, int[]>>(tupleOne);
+
+            Assert.True(tupleOne.Item1);
+            Assert.Contains("is even", tupleOne.Item2);
+        }
+
+        [Theory]
+        [InlineData(21)]
+        [InlineData(47)]
+        [InlineData(93)] 
+        public void MethodA_WithAnOddNum_ReturnsATupleWithAFalseValue(int num)
+        {
+            var tupleOne = Program.MethodA(num);
+
+            Assert.IsType<Tuple<bool, string, int[]>>(tupleOne);
+
+            Assert.False(tupleOne.Item1);
+            Assert.Contains("is odd", tupleOne.Item2);
+        }
+
+        [Fact]
+        public void MethodB_WhenExecuted_ReturnsAString()
+        {
+            var testTuple = Tuple.Create(10, false);
+            var result = Program.MethodB(testTuple);
+
+            Assert.IsType<string>(result);
+        }
+    }
+}
