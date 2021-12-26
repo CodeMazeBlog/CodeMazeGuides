04f8a3aeec256e2a2e03d348f59d2dc3cbc64b5e
Ashkan Farmanbar <1992ashfar@gmail.com>
2021-12-26 21:04:00 +0330

    init commit by ashkan farmanbar

---
+++ asp_dotnet_core/WebAPI_With_SQLite/WebAPI_With_SQLite/DataModel/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_With_SQLite.DataModel
+{
+    public class Student
+    {
+        [Key] public int id { get; set; }
+        public string name { get; set; }
+        public string last_name { get; set; }
+        public string faculty { get; set; }
+        public string avg_GPA { get; set; }
+    }
+}
---
+++ asp_dotnet_core/WebAPI_With_SQLite/WebAPI_With_SQLite/DatabaseProvider/SQLite_db.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_With_SQLite.DataModel;
+
+namespace WebAPI_With_SQLite.DatabaseProvider
+{
+    public class SQLite_db : DbContext
+    {
+        public SQLite_db(DbContextOptions<SQLite_db> options) : base(options)
+        {
+            // Applies any pending migrations for the context to the database.
+            // Will create the database if it does not already exist.
+            Database.Migrate();
+        }
+        // Override this method to further configure the model that was discovered
+        // by convention from the entity types exposed in DbSet<TEntity> properties
+        // on your derived context. The resulting model may be cached and re-used for
+        // subsequent instances of your derived context.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("students").Property(e => e.id).ValueGeneratedOnAdd();
+        }
+        public DbSet<Student> students { get; set; }
+    }
+}
