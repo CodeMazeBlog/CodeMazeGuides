7f8f51d2b908cfa11cb3df0b4bbc78efd2cc978c
chuksel95@gmail.com <chuksel95@gmail.com>
2021-12-27 07:14:31 +0100

    ActionandFunc

---
+++ csharp-advanced-topics/Action&FuncDelegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+
+        //Declaring method
+        public static void CheckIfAdult(int age)
+        {
+            if (age >= 18)
+            {
+                Console.WriteLine("User is an Adult");
+            }
+            else
+            {
+                Console.WriteLine("User is still a minor");
+            }
+        }
+        public static int AreaSquare(int lenght, int width)
+        {
+            return lenght * width;
+        }
+        static public void Main()
+        {
+            //Declare action delegate
+            //instantiate & assign delegate to method
+            Action<int> action = CheckIfAdult;
+            action(3);
+            //Declare func delegate
+            //instantiate & assign delegate to method
+            Func<int, int, int> func = AreaSquare;
+
+            Console.WriteLine(func(23, 32));
+        }
+    }
+}
