3a06802a1746ec2adee3b871cee70655c63a2b8f
Himanshu Kumar <hiku@microsoft.com>
2021-12-29 23:53:39 +0530

    incorporated review comments

---
+++ csharp-advanced-topics/HimanshuEvaluationProject/DelegatesTests/ActionDelegateRealWorldTest.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RealWorldActionDelegate;
+using System;
+using System.IO;
+
+namespace DelegateTests
+{
+    [TestClass]
+    public class ActionDelegateRealWorldTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+
+                TTMixDoubleParticipants ttMixDoubleParticipants = new TTMixDoubleParticipants();
+                Action<string> toUpperFunctionPointer = Helpers.PrintUpperCase;
+                ttMixDoubleParticipants.players.ForEach(toUpperFunctionPointer);
+
+                Assert.IsTrue(stringWriter.ToString().Contains("BRUCE"));
+                Assert.IsTrue(stringWriter.ToString().Contains("TINA"));
+                Assert.IsTrue(stringWriter.ToString().Contains("TIM"));
+                Assert.IsTrue(stringWriter.ToString().Contains("MINA"));
+            }
+        }
+    }
+}
---
+++ csharp-advanced-topics/HimanshuEvaluationProject/DelegatesTests/FuncDelegateRealWorldTest.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RealWorldFuncDelegate;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DelegateTests
+{
+    [TestClass]
+    public class FuncDelegateRealWorldTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+
+                TTMixDoubleParticipants ttMixDoubleParticipants = new TTMixDoubleParticipants();
+                Func<List<string>, List<string>> toUpperFunctionPointer = Helpers.ConvertToUpperCaseList;
+                Action<string> printerFunctionPointer = Helpers.Print;
+                List<string> upperCaseList = toUpperFunctionPointer(ttMixDoubleParticipants.players);
+                List<string> expected = new List<string>() { "BRUCE", "TINA", "TIM", "MINA" };
+                CollectionAssert.AreEqual(expected, upperCaseList);
+                upperCaseList.ForEach(printerFunctionPointer);
+                Assert.IsTrue(stringWriter.ToString().Contains("BRUCE"));
+                Assert.IsTrue(stringWriter.ToString().Contains("TINA"));
+                Assert.IsTrue(stringWriter.ToString().Contains("TIM"));
+                Assert.IsTrue(stringWriter.ToString().Contains("MINA"));
+            }
+        }
+    }
+}
