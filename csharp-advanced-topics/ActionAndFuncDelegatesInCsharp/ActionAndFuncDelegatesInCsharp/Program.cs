78dc468ac90ed5def1ecd09b4d652199b909abcc
bala <bala.ponnuvel@accelerationpoint.com>
2021-12-29 10:06:12 +0530

    Initial commit

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> logToConsole = (msg) => Console.WriteLine(msg);
+            Action<string> logToFile = (msg) => File.AppendAllText("log.txt", msg); 
+
+            Func<int, bool> oddNumber = (num) => num % 2 == 1;
+            Func<int, bool> evenNumber = (num) => num % 2 == 0;
+
+            //logs odd numbers to a file.
+            GenerateData(oddNumber, logToFile);
+
+            //logs even numbers to console.
+            GenerateData(evenNumber, logToConsole);
+        }
+
+        static void GenerateData(Func<int, bool> predicate, Action<string> logger)
+        {
+            var numbers = Enumerable.Range(1, 100)
+                            .Where(predicate)
+                            .ToList();
+            
+            logger(String.Join(',', numbers));
+        }
+    }
+    
+}
