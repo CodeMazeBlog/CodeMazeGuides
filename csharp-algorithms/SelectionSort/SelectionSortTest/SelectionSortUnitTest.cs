eeb581daa286df4d8f0bceb718503be802430477
martinchege324 <martinchege324@gmail.com>
2021-12-30 17:44:27 +0300

    Removed comments from the project and moved the project into a different folder.

--- csharp-algorithms/SelectionSort/SelectionSortTest/SelectionSortUnitTest.cs
+++ csharp-algorithms/SelectionSort/SelectionSortTest/SelectionSortUnitTest.cs
@@ -9,17 +9,14 @@ namespace SelectionSortTest
         [TestMethod]
         public void GivenUnsortedArray_ThenreturnSortedArray()
         {
-            //define an unsorted array
+           
             int[] numArray = new int[10] { 57, 1, 98, 65, 88, 24, 45, 13, 79, 34 };
             int arrayLength = numArray.Length;
 
-            //the expected sorted array
             int[] expectedArray = new int[10] { 1, 13, 24, 34, 45, 57, 65, 79, 88, 98 };
 
-            //call the selection sort algorithm on the unsorted array
             int[] actualArray = Program.SortArray(numArray, arrayLength);
 
-            //we expect both arrays to be equal when their elements are compared
             bool expected = true;
             bool actual = Enumerable.SequenceEqual(expectedArray, actualArray);
 
@@ -40,17 +37,13 @@ namespace SelectionSortTest
         [TestMethod]
         public void GivenSortedArray_ReturnSortedArray()
         {
-            //define an unsorted array
             int[] numArray = new int[10] { 1, 13, 24, 34, 45, 57, 65, 79, 88, 98 };
             int arrayLength = numArray.Length;
 
-            //the expected sorted array
             int[] expectedArray = new int[10] { 1, 13, 24, 34, 45, 57, 65, 79, 88, 98 };
 
-            //call the selection sort algorithm on the unsorted array
             int[] actualArray = Program.SortArray(numArray, arrayLength);
 
-            //we expect both arrays to be equal when their elements are compared
             bool expected = true;
             bool actual = Enumerable.SequenceEqual(expectedArray, actualArray);
 
