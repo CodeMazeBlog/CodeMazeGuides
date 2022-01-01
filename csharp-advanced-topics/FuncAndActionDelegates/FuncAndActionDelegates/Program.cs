65a466fb37850e3bf7ad0e932052af5384da216a
Satya <satyap48@gmail.com>
2022-01-01 20:25:50 +0530

    Func and Action Delegates

---
+++ csharp-advanced-topics/FuncAndActionDelegates/FuncAndActionDelegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FuncAndActionDelegates
+{
+    public class Program
+    {
+        public delegate int AdditionDelegate(int num1, int num2);
+        public static void Main(string[] args)
+        {
+            AdditionDelegate additionDelegate = AddNumbers;
+            var sum = additionDelegate(10, 20);
+            Console.WriteLine(sum);
+
+            Func<int, int, int> additionFuncDel = AddNumbers;
+            var sum1 = additionFuncDel(15, 25);
+            Console.WriteLine(sum1);
+
+            Func<int, string> checkEvenFuncDel = (num) => num % 2 == 0 ? "Even" : "Odd";
+            var result = checkEvenFuncDel(30);
+            Console.WriteLine(result);
+
+            Action printActionDel = PrintMessage;
+            printActionDel();
+
+            Action<string, string> printNameActionDel = PrintFullName;
+            printNameActionDel("Jane", "Doe");
+
+            Action<int> tripleNumActionDel = (num) => Console.WriteLine(num * 3);
+            tripleNumActionDel(6);
+        }
+
+        public static int AddNumbers(int num1, int nums2)
+        {
+            return num1 + nums2;
+        }
+
+        public static void PrintMessage()
+        {
+            Console.WriteLine("THIS IS SPARTA!!!");
+        }
+
+        public static void PrintFullName(string firstName, string lastName)
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+}
---
+++ csharp-advanced-topics/FuncAndActionDelegates/Tests/Tests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FuncAndActionDelegates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncAndActionDelegates.Tests
+{
+    [TestClass()]
+    public class Tests
+    {
+        [TestMethod()]
+        public void GivenFuncDelegate_WhenMethodReferenced_DelegateInvokationListNotEmpty()
+        {
+            Func<int, int, int> funcDel = Program.AddNumbers;
+            var invokationList = funcDel.GetInvocationList();
+            Assert.IsNotNull(invokationList);
+            Assert.AreEqual(1, invokationList.Length);
+        }
+
+        [TestMethod()]
+        public void GivenFuncDelegate_WhenMethodReferenced_DelegateExecutesTheReferencedMethod()
+        {
+            Func<int, int, int> funcDel = Program.AddNumbers;
+            var result = funcDel(4, 8);
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod()]
+        public void GivenActionDelegate_WhenParameterlessMethodReferenced_DelegateInvokationListNotEmpty()
+        {
+            Action actionDel = Program.PrintMessage;
+            var invokationList = actionDel.GetInvocationList();
+            Assert.IsNotNull(invokationList);
+            Assert.AreEqual(1, invokationList.Length);
+        }
+
+        [TestMethod()]
+        public void GivenActionDelegate_WhenParameterizedMethodReferenced_DelegateInvokationListNotEmpty()
+        {
+            Action<string, string> actionDel = Program.PrintFullName;
+            var invokationList = actionDel.GetInvocationList();
+            Assert.IsNotNull(invokationList);
+            Assert.AreEqual(1, invokationList.Length);
+        }
+    }
+}
