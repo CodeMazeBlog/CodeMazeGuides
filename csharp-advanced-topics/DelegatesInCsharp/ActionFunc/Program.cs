0dbb8a518ea159c90bb6980baa99131e934faffa
Luca Beretta <luca.beretta@cerbertech.com>
2022-01-09 16:53:10 +0100

    ActionFuncDelegates

---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionFunc/Program.cs
@@ -0,0 +1,34 @@
+﻿using ActionFunc;
+
+Console.Clear();
+
+// Basic
+Console.WriteLine($"# Basics samples {Environment.NewLine}");
+var shape = new Shape();
+
+Action<int> action = shape.Print;
+action(9);
+
+Func<int, int, int> func = shape.Compute;
+Console.WriteLine($"Computed : {func(23, 45)}");
+
+// Anonymous functions
+Console.WriteLine($"{Environment.NewLine}# Anonymous functions samples {Environment.NewLine}");
+Func<int, int, string> myAnonimousFunc = (a, b) => { return $"{a * b}"; };
+Console.WriteLine(myAnonimousFunc(3, 9));
+
+Action<string> myAnonymousAction = x => { Console.WriteLine($"Hello anonymous {x}"); };
+myAnonymousAction("Peter");
+
+// IEnumerable
+Console.WriteLine($"{Environment.NewLine}# IEnumerable samples {Environment.NewLine}");
+string[] names = { "Peter", "John", "Robert", "Lucille" };
+foreach (var item in names.Select(x => x.ToUpper()))
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in names.Select((name, index) => $"{name} ({index})"))
+{
+    Console.WriteLine(item);
+}
---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionFunc/Shape.cs
@@ -0,0 +1,18 @@
+﻿namespace ActionFunc
+{
+    internal class Shape
+    {
+        internal void Print(int Times)
+        {
+            for (int i = 0; i < Times; i++)
+            {
+                Console.WriteLine(this.ToString());
+            }
+        }
+
+        internal int Compute(int FirstValue, int SecondValue)
+        {
+            return FirstValue * SecondValue;
+        }
+    }
+}
