ce96f4300d6727d1846d757f7ca906de6e27d639
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-12 15:54:55 +0100

    Added Code Samples as Methods. Methods in this file holds the methods to the various sections of the article

---
+++ csharp-intermediate-topics/DifferencesBetweenValueTupleAndTupleInCSharp/DifferencesBetweenValueTupleAndTupleInCSharp/Program.cs
@@ -0,0 +1,199 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Run();
+
+public partial class Program
+{
+    public static void Run()
+    {
+        //This shows a chronological order of the code in the article, each method represents and holds all the code examples in a section of the article
+        InitializationDifferences();
+        StorageCapacity();
+        AccessingMemberValues();
+        ReturnAValueTuple();
+        ReturnATuple();
+        TakeInAValueTuple((4,true));
+        TakeInATuple(Tuple.Create(6, false));
+        Deconstruction();
+        CheckingEqualityOfValueTuples();
+        InterConversionOfTuples();
+    }
+
+    public static void InitializationDifferences()
+    {
+        //Initialization differences
+        (int, int) initializedValueTupleOne = (1, 2);
+
+        //equivalent Tuple code
+        var InitializedTupleOne = new Tuple<int, int>(1, 2);
+        var InitializedTupleTwo = Tuple.Create(1, 2);
+
+        //simplified version of code
+        var initializedValueTupleTwo = (1, 2);
+
+        //using named parameters
+        (int LastYear, int CurrentYear) namedValueTupleOne = (2021, 2022);
+        var currentYear = namedValueTupleOne.CurrentYear;
+
+        //right hand assignment
+        var rightHandNamedValueTuple = (Language: "C#", Tool: "Laptop", Occupation: "Developer");
+
+        //Both side assignment of names
+        (string lang, string machine, string career) rightHandNamedValueTupleTwo = (Language: "C#", Tool: "Laptop", Occupation: "Developer");
+
+        //Creating a ValueTuple with .Create method
+        var initializedValueTupleFour = ValueTuple.Create("code-maze", "johndoe", "dotnet");
+
+        //equivalent tuple code
+        var initializedTupleThree = Tuple.Create("code-maze", "johndoe", "dotnet");
+        var valueTupleWithNoElementsOne = ValueTuple.Create();
+
+        //Using class based constructors
+        var valueTupleWithNoElements = new ValueTuple(); //creating valueTuple with no elements
+        var initializedValueTupleSix = new ValueTuple<decimal, decimal, decimal>(10000M, 20000M, 30000m);
+
+    }
+
+    public static void MutabilityOfValueTuples()
+    {
+        //Mutability
+        (string satellite, string star, string planet, string galaxy) heavenlybodies = ("satellite", "star", "planet", "galaxy");
+
+        heavenlybodies.planet = "Earth";
+        heavenlybodies.galaxy = "Milky way";
+        heavenlybodies.star = "Antares";
+        heavenlybodies.satellite = "moon";
+    }
+
+    public static void StorageCapacity()
+    {
+        //Storing more than eight items in tuple
+        var nestedTuple = Tuple.Create("my", "name", "is", "john", "doe", "and", "I", Tuple.Create("am", 1000, "years", "old", true));
+
+        //Storing more than eight items in ValueTuple
+        var valueTuple = ("my", "name", "is", "john", "doe", "and", "I", "am", 1000, "years", "old", true);
+
+        //Using variables as ValueTuple member values
+        var age = 999;
+        var name = "Metuse";
+        var gender = "male";
+        var occupation = "pilot";
+        var hobby = "Anime watching";
+        var mail = "metuse11@mail.com";
+        var bestColor = "blue";
+        var weight = "1000kg";
+
+        var profile = (age, name, gender, occupation, hobby, mail, bestColor, weight);
+    }
+
+    public static void AccessingMemberValues()
+    {
+        //Accessing ValueTuple members
+        //==> uunamed params
+        (string, string, int, bool) ApiBook = ("codemaze", "Ultimate_ASP.NET_Core_Web_API_", 2021, false);
+        var itemOne = ApiBook.Item1; //returns "codemaze"
+        var itemThree = ApiBook.Item3; //returns 2021
+
+        //Acccessiing a Tuple
+        var vowels = Tuple.Create("a", "e", "i", "o", "u");
+        var fourthVowel = vowels.Item4; //returns "o"
+
+        //==> Named Params
+        (string author, string booktitle, int publishYear, bool isFiction) ApiBookTwo = ("codemaze", "Ultimate_ASP.NET_Core_Web_API_", 2021, false);
+
+        var author = ApiBookTwo.author; //returns "codemaze
+        var publishYear = ApiBookTwo.publishYear; //returns 2021
+
+        //Accesing the last parameter of a tuple
+        var evenNumbers = Tuple.Create(2, 4, 6, 8, 10, 12, 14,
+        Tuple.Create(16, 18, 20, 22, 24, 26));
+
+        var evenNumbersRestItem = evenNumbers.Rest.Item1; //returns (16, 18, 20, 22, 24, 26)
+        var evenNumberRestItemElementOne = evenNumbers.Rest.Item1.Item1; //returns 16
+
+        //Accesing the last parameter of a valueTuple
+        var evenNumbers2 = (one: 2, two: 4, three: 6, four: 8, five: 10, six: 12, seven: 14, eight: 16, nine: 18, ten: 20);
+        var itemTwelve = evenNumbers2.nine; // returns 18
+    }
+    public static void CheckingEqualityOfValueTuples()
+    {
+        //Checking Equality
+        var valueTupleOne = (1, 3, 6);
+        var valueTupleTwo = (2, 3, 7);
+
+        var isEqual = valueTupleOne == valueTupleTwo;
+        var isEqualTwo = (1, 3, 6) == (2, 3, 7);
+    }
+
+    //As method type
+    public static (bool isDay, string Greeting) ReturnAValueTuple()
+    {
+        if (DateTime.Now.Hour < 12)
+            return (isDay: true, Greeting: "Good Morning");
+
+        return (isDay: false, Greeting: "Good Evening");
+    }
+
+    public static Tuple<bool, string> ReturnATuple()
+    {
+        if (DateTime.Now.Hour < 12)
+            return Tuple.Create(true, "Good morning");
+
+        return Tuple.Create(false, "Good Evening");
+    }
+
+    //Tuple as method parameter
+    public static string TakeInAValueTuple((int legCount, bool isBlue) organism)
+    {
+        var hasAtLeastFourLegs = organism.legCount >= 4;
+        var isBlue = organism.isBlue;
+
+        return $"hasAtLeastFourLegs : {hasAtLeastFourLegs} and isBlue : {isBlue}";
+    }
+
+    public static string TakeInATuple(Tuple<int, bool> organism)
+    {
+        var hasAtLeastFourLegs = organism.Item1 >= 4;
+        var isBlue = organism.Item2;
+
+        return $"hasAtLeastFourLegs : {hasAtLeastFourLegs} and isBlue : {isBlue}";
+    }
+
+    public static void InterConversionOfTuples()
+    {
+        //IConvertible
+        var tuple = new Tuple<int, string>(24, "john doe");
+        var tupleConvertedToValueTuple = tuple.ToValueTuple();
+
+        //Converting a valueTuple to a tuple: 
+        var valueTuple = (24, "john-doe");
+        var valueTupleConvertedToTuple = valueTuple.ToTuple();
+    }
+
+    public static void Deconstruction()
+    {
+        //Accessing the Decostrucring example
+
+        var result = CheckStringLength("October");
+        var isLengthEven = result.isLengthEven;
+        var message = result.message;
+        var stringLength = result.stringLength;
+
+        //Using deconstruction
+        var (isLengthEvenn, messagee, stringLengthh) = CheckStringLength("October");
+
+        //using discards
+
+        var (_, message2, _) = CheckStringLength("January");
+    }
+
+    public static (bool isLengthEven, string message, int stringLength) CheckStringLength(string word)
+    {
+        var stringLength = word.Length;
+        var isLengthEven = stringLength % 2 == 0;
+        var message = isLengthEven ? "This is even" : "This is odd";
+
+        return (isLengthEven, message, stringLength);
+    }
+
+}
