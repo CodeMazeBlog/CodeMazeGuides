2f9a221f59018898b73634d383aceefde5862a6a
Parvaiz From Ari Bagh Srinagar Kashmir <14068610+parvaiz13@users.noreply.github.com>
2022-01-14 12:28:46 +0530

    Action and Func Delegate Class Files
    Action and Func Delegate Class Files

---
+++ Action_And_Function_Delegates/Action_Delegate_Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Action_Delegates.Pages.Shared
+{
+    
+    public class Action_Delegate_Class
+    {
+
+        // Method
+        public static void GetCompleteName(string FName, string LName)
+        {
+            string CompleteName = FName + " " + LName;
+
+            Console.WriteLine($"{CompleteName }");
+        }
+
+
+        static public void Main()
+        {
+            Action<string, string> Concate = GetCompleteName;
+
+            Concate("Ricky", "Martin ");
+        }
+
+    }
+}
+
+
---
+++ Action_And_Function_Delegates/Func_Delegate_Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Func_Delegates.Pages.Shared
+{
+    public class Func_Delegate_Class
+    {
+
+        // Method
+        public static decimal Percentage_Calculation(int marks, int total)
+        {
+            return (((decimal)marks / total) * 100);
+        }
+
+        static public void Main()
+        {
+            Func<int, int, decimal> Get_Percentage = Percentage_Calculation;
+
+            decimal MarksPercent = Get_Percentage(320, 500);
+            
+            Console.WriteLine($"{MarksPercent }");
+        }
+    }
+}
+
+
