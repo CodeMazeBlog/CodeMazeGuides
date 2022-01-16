df60f70035996195bcc2f6eb36ad68a3de905db0
Parvaiz From Ari Bagh Srinagar Kashmir <14068610+parvaiz13@users.noreply.github.com>
2022-01-16 22:00:18 +0530

    Add files via upload

---
+++ csharp-delegates/Action-and-Func-Delegates/Action_and_Func_Delegates/Action_Delegate_Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Action_Delegates
+{
+    
+    public class Action_Delegate_Class
+    {
+         
+        // Method
+        public static void GetCompleteName(string FName, string LName)
+        {
+            string CompleteName = FName + " " + LName;
+
+            Console.WriteLine($"{CompleteName }");
+        }
+
+
+
+    }
+}
+
+
