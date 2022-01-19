cf8563024cdd4ae4b87f7f89b53e5ea432db47d2
Ray Carneiro <ray.silva@msn.com>
2022-01-19 17:08:24 -0300

    changes in code

--- csharp-advanced-topics/IDisposableInCsharp/IDisposableInCsharp/StreamWriterClass.cs
+++ csharp-advanced-topics/IDisposableInCsharp/IDisposableInCsharp/StreamWriterClass.cs
@@ -18,23 +18,12 @@
         }
         public void WriteStreamAndDontDisposeObject()
         {
+            var fileName = Guid.NewGuid() + ".txt";
+            var firstWriter = new StreamWriter(fileName);
+            firstWriter.WriteLine("New line written in the txt file.");
 
-            try
-            {
-                var fileName = Guid.NewGuid() + ".txt";
-                var firstWriter = new StreamWriter(fileName);
-                firstWriter.WriteLine("New line written in the txt file.");
-
-                var secondWriter = new StreamWriter(fileName);
-                secondWriter.WriteLine("Since this file is already open, it won't work. The file is already opened by the other StreamWrite object in memory.");
-            }
-            catch (IOException exception)
-            {
-                Console.WriteLine("Method exception due to " + exception?.Message);
-                Dispose();
-            }
-
-
+            var secondWriter = new StreamWriter(fileName);
+            secondWriter.WriteLine("Since this file is already open, it won't work. The file is already opened by the other StreamWrite object in memory.");
         }
         public void Dispose()
         {
