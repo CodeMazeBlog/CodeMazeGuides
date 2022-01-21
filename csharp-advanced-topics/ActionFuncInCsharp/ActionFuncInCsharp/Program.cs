a3183e51c5798a0b5189b25dd007a95a3475d159
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2022-01-21 18:47:30 +0100

    added changes

--- csharp-advanced-topics/ActionFuncInCsharp/ActionFuncInCsharp/Program.cs
+++ csharp-advanced-topics/ActionFuncInCsharp/ActionFuncInCsharp/Program.cs
@@ -2,40 +2,33 @@
 
 namespace ActionFuncInCsharp
 {
-    public delegate void LogError(string message);
+    public delegate void logError(string message);
+
     class Program
     {
         static void Main(string[] args)
         {
-            //instantiating delegate
-            var log = new LogError(ErrorMessage);
-
-            //invoking delegate
+            var log = new logError(ErrorMessage);
             log("Null value not accepted");
+             
+            Action<string> logError = ErrorMessage;
+            logError("Null value is not accepted");
 
-            //instantiating Action with method
-            Action<string> LogError = ErrorMessage;
-
-            //instantiating Action with lambda expression
-            //Action<string> LogError = (message) => Console.WriteLine(message);
-
-            //invoking Action
-            LogError("Null value is not accepted");
-
-            //instantiating Func with method
             Func<string, string> greetings = GreetUser;
-
-            //instantiating Func with lambda expression
-            //Func<string, string> greetings = (user) => $"Welcome back {user}. We are glad to see you";
-
-            //invoking Func
             Console.WriteLine(greetings("John"));
+
+            Action<string> logError2 = (message) => Console.WriteLine(message);
+            logError2("Null value is not accepted");
+
+            Func<string, string> greetings2 = (user) => $"Welcome back {user}. We are glad to see you";
+            Console.WriteLine(greetings2("John"));
         }
 
         public static void ErrorMessage(string message)
         {
             Console.WriteLine($"Application down due to the following error: {message}");
         }
+
         public static string GreetUser(string user)
         {
             return $"Welcome back {user}. We are glad to see you";
--- csharp-advanced-topics/ActionFuncInCsharp/Tests/Test.cs
+++ csharp-advanced-topics/ActionFuncInCsharp/Tests/Test.cs
@@ -8,9 +8,9 @@ namespace Tests
     public class Test
     {
         [TestMethod]
-        public void Test_Delegate()
+        public void whenStringIsSent_DelegateExecutesTheReferencedMethod()
         {
-            var log = new LogError(ErrorMessage);
+            var log = new logError(ErrorMessage);
             log("An error occured while testing");
 
             var invocationList = log.GetInvocationList();
@@ -18,20 +18,22 @@ namespace Tests
         }
 
         [TestMethod]
-        public void Test_Action()
+        public void whenStringIsSent_ActionDelegateExecutesTheReferencedMethod()
         {
-            Action<string> LogError = ErrorMessage;
-            LogError("An error occured while testing.");
-            var invocationList = LogError.GetInvocationList();
+            Action<string> logError = ErrorMessage;
+            logError("An error occured while testing.");
+
+            var invocationList = logError.GetInvocationList();
             Assert.AreEqual(invocationList.Length, 1);
         }
 
-
         [TestMethod]
-        public void Test_Func()
+        public void whenStringIsPassed_FuncDelegateReturnsBackGreeting()
         {
             Func<string, string> greetings = GreetUser;
+
             string outcome = "Welcome back John. We are glad to see you";
+
             Assert.AreEqual(outcome , greetings("John"));
         }
 
@@ -39,6 +41,7 @@ namespace Tests
         {
             Console.WriteLine($"Application down due to the following error: {message}");
         }
+
         public static string GreetUser(string user)
         {
             return $"Welcome back {user}. We are glad to see you";
