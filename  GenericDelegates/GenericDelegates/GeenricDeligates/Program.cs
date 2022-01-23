78e2139cde8120c01f66acfbf255105f48757736
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2022-01-24 04:56:27 +0500

    GenericDeligates

---
+++  GenericDelegates/GenericDelegates/GeenricDeligates/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GenericDeligates
+{
+    // Define Delegate for the void return type
+    public delegate void DelegateVoid(int x, int y);
+  //public static   void SumReturVoid(int x , int y)
+ 
+
+    // Define Delegate for the Int return type
+    public delegate int DelegateInt(int x, int y);
+  //public static   int SumReturInt(int x , int y)
+
+   
+    class Program
+    {
+        // Calculate Sum of Two integers and prints the sum
+        public static void SumReturVoid(int x, int y)
+        {
+            int sum = x + y;
+            Console.WriteLine("void return type : " + sum);
+        }
+        // Calculate Sum of Two integers and returns the sum
+        public static int SumReturInt(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            //First Form before mutation 
+            //DelegateVoid objVoid = new DelegateVoid(SumReturVoid);
+            //DelegateInt objInt = new DelegateInt(SumReturInt);
+
+            //objVoid.Invoke(10, 20);
+            //int result = objInt.Invoke(10, 20);
+            //Console.WriteLine("Int return type : " + result);
+
+            //Second Form After mutation 
+            //DelegateVoid objVoid = SumReturVoid;
+            //DelegateInt  objInt  = SumReturInt;
+
+            //objVoid.Invoke(10, 20);
+
+            //int result = objInt.Invoke(10,20);
+            //Console.WriteLine("Int  return type : " + result);
+
+
+            //Third Form After mutation 
+            Action<int,int> objVoid = SumReturVoid;
+            Func<int,int,int> objInt  = SumReturInt;
+
+            objVoid.Invoke(10, 20);
+
+            int result = objInt.Invoke(10,20);
+            Console.WriteLine("Int  return type : " + result);
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
