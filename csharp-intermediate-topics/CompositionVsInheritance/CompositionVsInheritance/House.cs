410cc4eea0b6491efaf7bc2973f9a82f362e9f9d
Satya <satyap48@gmail.com>
2022-01-25 15:37:43 +0530

    CM-388: Composition vs Inheritance in C#

---
+++ csharp-intermediate-topics/CompositionVsInheritance/CompositionVsInheritance/House.cs
@@ -0,0 +1,82 @@
+﻿namespace CompositionVsInheritance
+{
+    public class House
+    {
+        public string Color { get; set; }
+
+        public Ceiling ceiling = new Ceiling();
+        public Floor floor = new Floor();
+
+        public string GetCeiling()
+        {
+            return ceiling.BuildCeiling();
+        }
+
+        public string GetFloor()
+        {
+            return floor.BuildFloor();
+        }
+
+        //Moved to Address Component Class
+        //public virtual string GetAddress()
+        //{
+        //    return "Address";
+        //}
+    }
+
+    public class BrickHouse : House
+    {
+        public Address address = new Address();
+        public string GetAddress()
+        {
+            return address.GetAddress();
+        }
+    }
+
+    public class GlassHouse : House
+    {
+        public Address address = new Address();
+        public string GetAddress()
+        {
+            return address.GetAddress();
+        }
+
+        public string WarningSign()
+        {
+            return "No rocks please!";
+        }
+    }
+
+    public class Caravan : House
+    {
+        //No override needed after composition
+        //public override string GetAddress()
+        //{
+        //    throw new Exception("No fixed address");
+        //}
+    }
+
+    public class Address
+    {
+        public string GetAddress()
+        {
+            return "Address";
+        }
+    }
+
+    public class Ceiling
+    {
+        public string BuildCeiling()
+        {
+            return "Building a ceiling...";
+        }
+    }
+
+    public class Floor
+    {
+        public string BuildFloor()
+        {
+            return "Building a floor...";
+        }
+    }
+}
---
+++ csharp-intermediate-topics/CompositionVsInheritance/Tests/Tests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompositionVsInheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CompositionVsInheritance.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void WhenBaseClassInstantiated_AllMembersAccesible()
+        {
+            var house = new House();
+
+            Assert.AreEqual("Building a floor...", house.GetFloor());
+
+            Assert.AreEqual("Building a ceiling...", house.GetCeiling());
+        }
+
+        [TestMethod]
+        public void WhenDerivedClassInstantiated_AllBaseMethodsAccesible()
+        {
+            var glassHouse = new GlassHouse();
+
+            Assert.AreEqual("Building a floor...", glassHouse.GetFloor());
+
+            Assert.AreEqual("Building a ceiling...", glassHouse.GetCeiling());
+        }
+
+        [TestMethod]
+        public void WhenClassInstantiated_AllComponentMethodsAccesible()
+        {
+            var brickHouse = new BrickHouse();
+
+            Assert.AreEqual("Address", brickHouse.GetAddress());
+        }
+    }
+}
