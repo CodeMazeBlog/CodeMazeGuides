25c5e5417f2d836c11fd3c06cb835ac7de1b6c7f
Parvaiz From Ari Bagh Srinagar Kashmir <14068610+parvaiz13@users.noreply.github.com>
2022-01-31 09:36:01 +0530

    commet
    Comment

---
+++ csharp-advanced-topics/Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static public void Main()
+    {
+        //Func Delegate
+        //  Func<int, int, decimal> Get_Percentage = Func_Delegates.Func_Delegate_Class.Percentage_Calculation;
+        Func<int, int, decimal> Get_Percentage = Percentage_Calculation;
+        decimal MarksPercent = Get_Percentage(320, 500);
+        Console.WriteLine($"{MarksPercent }");
+
+        //Action Delegate
+        // Action<string, string> Concate = Action_Delegates.Action_Delegate_Class.GetCompleteName;
+        Action<string, string> Concate = GetCompleteName;
+        Concate("Ricky", "Martin ");
+    }
+
+
+    public static void GetCompleteName(string FName, string LName)
+    {
+        string CompleteName = FName + " " + LName;
+
+        Console.WriteLine($"{CompleteName }");
+    }
+
+    public static decimal Percentage_Calculation(int marks, int total)
+    {
+        return (((decimal)marks / total) * 100);
+    }
+}
