6479fd3c2b9f4de4416ed67da79c639d1f6f5057
Nnaemeka Eziamaka <Nnaemeka.Eziamaka@stanbicibtc.com>
2022-01-31 19:33:59 +0100

    complete code changes

---
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ActionAndFuncDelegates
+{
+    class Program
+    {
+        public delegate void OurFirstDelegate(int x);
+
+        static void Main(string[] args)
+        {
+            var firstVariable = 1;
+
+            var secondVariable = DoubleTheVariable(firstVariable);
+
+            PrintVariable(secondVariable);
+
+            var firstDelegate = new OurFirstDelegate(PrintVariable);
+
+            firstDelegate(200);
+
+            PassADelegeteToAFunction(firstDelegate);
+
+            Func<string, string> HelloFuncDelegate = HelloFunction;
+
+            Action<string> GoodbyeActionDelegate = GoodbyeFunction;
+
+        }
+
+        public static int DoubleTheVariable(int parameter)
+        {
+            return parameter * 2;
+        }
+
+        public static void PrintVariable(int parameter)
+        {
+            Console.WriteLine(parameter);
+        }
+
+        public static void PassADelegeteToAFunction(OurFirstDelegate myDelegate)
+        {
+            myDelegate(100);
+        }
+
+        public static string HelloFunction(string name)
+        {
+            return "Hello " + name;
+        }
+
+        public static void GoodbyeFunction(string name)
+        {
+            Console.WriteLine("Goodbye " + name);
+        }
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegates/Tests/Tests.cs
@@ -0,0 +1,68 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        public delegate void OurFirstDelegate(int x);
+
+
+        [TestMethod]
+        public void WhenAParameterIsPassed_TheNumberIsDoubled()
+        {
+            var variable = 2;
+            var result = DoubleTheVariable(variable);
+            Assert.AreEqual(4, result);
+        }
+        [TestMethod]
+        public void WhenAParameterIsPassed_TheDelegateExecutesTheReferencedMethod()
+        {
+            var firstDelegate = new OurFirstDelegate(PrintVariable);
+            firstDelegate(4);
+
+            var invocationList = firstDelegate.GetInvocationList();
+            Assert.AreEqual(invocationList.Length, 1);
+        }
+        [TestMethod]
+        public void WhenADelegateIsPassedToAFunction_TheMethodExecutesTheDelegate()
+        {
+            var firstDelegate = new OurFirstDelegate(PrintVariable);
+            PassADelegeteToAFunction(firstDelegate);
+
+            var invocationList = firstDelegate.GetInvocationList();
+            Assert.AreEqual(invocationList.Length, 1);
+        }
+        [TestMethod]
+        public void WhenAStringIsPassed_TheFuncDelegateExecutesTheReferencedMethod()
+        {
+            Func<string, string> HelloFuncDelegate = HelloFunction;
+            var result = HelloFuncDelegate("Naruto");
+
+
+            Assert.AreEqual(result, "Hello Naruto");
+        }
+        [TestMethod]
+        public void WhenAStringIsPassed_TheActionDelegateExecutesTheReferencedMethod()
+        {
+            Action<string> GoodbyeActionDelegate = GoodbyeFunction;
+            GoodbyeActionDelegate("Naruto");
+
+            var invocationList = GoodbyeActionDelegate.GetInvocationList();
+
+
+            Assert.AreEqual(invocationList.Length, 1);
+        }
+
+        public static int DoubleTheVariable(int parameter) { return parameter * 2; }
+
+        public static void PrintVariable(int parameter) { Console.WriteLine(parameter); }
+
+        public static void PassADelegeteToAFunction(OurFirstDelegate myDelegate) { myDelegate(100); }
+
+        public static string HelloFunction(string name) { return "Hello " + name; }
+
+        public static void GoodbyeFunction(string name) { Console.WriteLine("Goodbye " + name); }
+    }
+}
