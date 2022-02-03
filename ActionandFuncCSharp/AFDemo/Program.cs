788a0655a4c781d83be89143e5f0bd4eba3371a6
OniCode <bsl_91@yahoo.com>
2022-02-03 23:17:04 +0900

    Actions and Func Delegates in CSharp - Brianslongl91
    Action delegate implementation
    Countdown Timer which executes given action upon completion.

---
+++ ActionandFuncCSharp/AFDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Timers;
+
+namespace AFDemo
+{
+    public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Action DoThis = PrintMessage;
+            CustomCountdown countdown = new CustomCountdown(10, DoThis);
+            countdown.Start();
+           
+            Console.ReadLine();
+        }
+
+        public static void PrintMessage()
+        {
+            Console.WriteLine("Look dad I made it");
+        }
+    }
+
+    public class CustomCountdown
+    {
+        public int TimeLeft { get; set; }
+        Timer Timekeep { get; set; }
+        public Action Callbackaction { get; set; }
+        public CustomCountdown(int initial_time, Action action)
+        {
+            TimeLeft = initial_time;
+            Callbackaction = action;
+            Timekeep = new Timer(1000);
+            Timekeep.Elapsed += Timekeep_Elapsed;
+        }
+
+        public void Start()
+        { 
+            Timekeep.Start();
+        }
+
+        private void Timekeep_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            TimeLeft -= 1;       
+
+            if (TimeLeft<= 0)
+            {
+                Timekeep.Stop();
+                Callbackaction();
+            }
+        }
+
+        
+    }
+}
---
+++ ActionandFuncCSharp/AFTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using AFDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AFTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public CustomCountdown countdown;
+        
+
+
+        [TestMethod]
+        public void CountdownActionTest()
+        {
+            Action hello  = LogHello;
+            countdown = new CustomCountdown(10, hello);
+            var invocations = countdown.Callbackaction.GetInvocationList();
+            Assert.AreEqual(invocations.Length, 1);
+        }
+
+        public void LogHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+
+
+    }
+}
