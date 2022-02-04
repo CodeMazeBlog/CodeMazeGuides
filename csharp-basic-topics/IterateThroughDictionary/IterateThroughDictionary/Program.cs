098775554833649c4a9eef0c94ddd0bccd433077
achinya <95957752+achinya@users.noreply.github.com>
2022-02-04 19:45:10 +0530

    CM-222 Different Ways to Iterate through a Dictionary
    CM-222 Different Ways to Iterate through a Dictionary in C# 
    project and class file

---
+++ csharp-basic-topics/IterateThroughDictionary/IterateThroughDictionary/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace IterateThroughDictionary
+{
+    public  class Program
+    {
+        public static void Main(string[]args)
+        {
+            Dictionary<int, string> MonthsInYear = new Dictionary<int, string>
+            {
+                {1,"January" },
+                {2,"February" },
+                {3,"March" },
+                {4,"April" }
+            };
+
+            SubDictionaryUsingForEach(MonthsInYear);
+            SubDictionaryKeyValuePair(MonthsInYear);
+            SubDictionaryForLoop(MonthsInYear);
+            SubDictionaryParallelEnumerable(MonthsInYear);
+            SubDictionaryStringJoin(MonthsInYear);
+
+        }
+        public static void SubDictionaryUsingForEach(Dictionary<int,string> MonthsInYear )
+        {
+            foreach (var month in MonthsInYear )
+            {
+                Console.WriteLine(month.Key + ":" + month.Value);
+
+            }
+        }
+
+        public static void SubDictionaryKeyValuePair(Dictionary<int,string> MonthsInYear)
+        {
+            foreach(KeyValuePair<int,string> entry in MonthsInYear )
+            {
+                Console.WriteLine(entry.Key + ":" + entry.Value);
+            }
+
+            foreach( var (key,value) in MonthsInYear )
+            {
+                Console.WriteLine(key + ":" + value);   
+            }
+        }
+
+        public static void SubDictionaryForLoop(Dictionary<int, string> MonthsInYear)
+        {
+            for (int index = 0; index < MonthsInYear.Count; index ++)
+            {
+                KeyValuePair<int, string> month = MonthsInYear.ElementAt(index);
+
+                Console.WriteLine(month.Key + ":" + month.Value);
+            }
+        }
+
+        public static void SubDictionaryParallelEnumerable(Dictionary<int, string> MonthsInYear)
+        {
+            MonthsInYear.AsParallel()
+                        .ForAll(month => Console.WriteLine(month.Key + ":" + month.Value));
+
+        }
+
+        public static void SubDictionaryStringJoin(Dictionary<int, string> MonthsInYear)
+        {
+            Console.WriteLine(String.Join(Environment.NewLine, MonthsInYear));
+        }
+    }
+}
