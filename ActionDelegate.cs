985f9b0a27c69a2764a8291fe2482876297c3732
JyotiRathod2022 <98772287+JyotiRathod2022@users.noreply.github.com>
2022-02-04 21:56:11 +0100

    Action and Func in Delegates C#

---
+++ ActionDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class ActionDelegate
+    {
+        static void Main(string[] args)
+        {
+            Action<string> writeDelegate = Write;
+            writeDelegate("Hello World from Delegate..");
+            Console.ReadLine();
+        }
+        public static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> sumDelegate = Sum;
+            Console.WriteLine(sumDelegate(10, 20));
+            Console.ReadLine();
+        }
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
