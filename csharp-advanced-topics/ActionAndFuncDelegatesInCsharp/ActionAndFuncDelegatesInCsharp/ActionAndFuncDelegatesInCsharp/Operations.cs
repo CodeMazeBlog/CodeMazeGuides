06cd398776b1464211d096053eafc935231eee08
Ahmed Joum <ahmed.moniim@gmail.com>
2022-02-05 15:43:32 +0200

    Action And Func Delegate demo for the Article

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegatesInCsharp
+{
+    public class Operations
+    {
+        public string Name { get; } = "Joe";
+
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, {Name}");
+        }
+
+        public void SayHiToFullName(string firtsName, string lastName)
+        {
+            Console.WriteLine($"Hi, {firtsName} {lastName}");
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Test/FuncDemo_Test.cs
@@ -0,0 +1,53 @@
+﻿using ActionAndFuncDelegatesInCsharp;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class FuncDemo_Test
+    {
+        public Operations _operations;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _operations = new();
+        }
+
+        [TestMethod]
+        public void GetName_Test()
+        {
+            Func<string> getNameDelegate = _operations.GetName;
+            var name = getNameDelegate();
+
+            Assert.AreEqual(name, _operations.Name);
+        }
+
+        [TestMethod]
+        public void Sum_Test()
+        {
+            Func<int, int, int> intSumDelegate = _operations.Sum;
+            var sum = intSumDelegate(3, 3);
+            Assert.AreEqual(6, sum);
+        }
+
+        [TestMethod]
+        public void Multiply_Test()
+        {
+            Func<int, int, int> multiplyDelegate = delegate (int x, int y) { return x * y; };
+            var multiply = multiplyDelegate(3, 2);
+            Assert.AreEqual(6, multiply);
+        }
+
+        [TestMethod]
+        public void Subtract_Test()
+        {
+            Func<int, int, int> subtractDelegate = (int x, int y) => x - y;
+            var subtract = subtractDelegate(3, 2);
+            Assert.AreEqual(1, subtract);
+        }
+    }
+}
