26c8d97aeb72e3fb264d0751dc7e6a753f23539f
Emmanuel Allison <emmallison13@gmail.com>
2022-02-05 20:11:58 +0100

    Added another method to the delegate.

--- csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
+++ csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -2,10 +2,17 @@
 
 void WriteToConsole(string message)
 {
-    message.Trim();
     Console.WriteLine("Delegate is writing to the console...");
     Console.WriteLine(message);
 }
 
+void WriteToDb(string message)
+{
+    Console.WriteLine("Delegate is writing to the database...");
+
+    // Database logic...
+}
+
 DelegateClass.Writers = WriteToConsole;
+DelegateClass.Writers += WriteToDb;
 DelegateClass.Execute("Hello, World!");
