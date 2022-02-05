4ec4e84a0d65e54b1b95554af814166bad92f45d
Emmanuel Allison <emmallison13@gmail.com>
2022-02-05 17:08:30 +0100

    Initial commit and delegate example

---
+++ csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/DelegateClass.cs
@@ -0,0 +1,13 @@
+﻿namespace ActionAndFuncDelegates;
+
+public delegate void Writer(string message);
+
+public static class DelegateClass
+{
+    public static Writer Writers;
+
+    public static void Execute(string message)
+    {
+        Writers(message);
+    }
+}
---
+++ csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,11 @@
+﻿using ActionAndFuncDelegates;
+
+void WriteToConsole(string message)
+{
+    message.Trim();
+    Console.WriteLine("Delegate is writing to the console...");
+    Console.WriteLine(message);
+}
+
+DelegateClass.Writers = WriteToConsole;
+DelegateClass.Execute("Hello, World!");
