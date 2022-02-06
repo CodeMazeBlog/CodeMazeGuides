0d89f90245a1f0a2195fe1d57d919424fa9d02a3
Bartosz Jarmuż <33833009+bartosz-jarmuz@users.noreply.github.com>
2022-02-06 09:23:46 +0100

    CM-6: Visitor Pattern in C#

---
+++ csharp-design-patterns/VisitorPattern/VisitorPatternTests/Structural Code Tests/Elements/ConcreteElementOne.cs
@@ -0,0 +1,8 @@
+﻿namespace VisitorPatternTests
+{
+    public class ConcreteElementOne : IVisitableElement
+    {
+        public string ElementOneData { get; } = "The Ultimate Answer To Everything: ";
+        public void Accept(IVisitor visitor) => visitor.Visit(this);
+    }
+}
