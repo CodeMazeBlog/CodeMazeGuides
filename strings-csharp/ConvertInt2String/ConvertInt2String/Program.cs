14306dcb66de203a55d6c8082e30b609f9ca0512
Zoran_Despotovski <zoran.despotovski@deptagency.com>
2022-02-07 02:18:28 +0100

    Added project files and code examples

---
+++ strings-csharp/ConvertInt2String/ConvertInt2String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+int luckyNumber = 3;
+Console.WriteLine(luckyNumber);
+Console.WriteLine(luckyNumber.ToString());
+
+Console.WriteLine(Convert.ToString(luckyNumber));
+
+Console.WriteLine(string.Format("{0}", luckyNumber));
+
+Console.WriteLine($"{luckyNumber}");
+
+Console.WriteLine(string.Concat(string.Empty, luckyNumber));
+Console.WriteLine(string.Join(string.Empty, luckyNumber));
+Console.WriteLine(string.Empty + luckyNumber);
+Console.WriteLine(luckyNumber + luckyNumber + string.Empty + luckyNumber + luckyNumber);
+
+Console.WriteLine(new StringBuilder().Append(luckyNumber).ToString());
---
+++ strings-csharp/ConvertInt2String/ConvertInt2StringTests/ConvertInt2StringTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConvertInt2StringTests
+{
+    [TestClass]
+    public class ConvertInt2StringTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
