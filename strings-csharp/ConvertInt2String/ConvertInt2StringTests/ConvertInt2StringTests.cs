a4a087d43cff8bd6173bbcea9e881f006fcde185
Zoran_Despotovski <zoran.despotovski@deptagency.com>
2022-02-07 09:10:49 +0100

    Added tests

--- strings-csharp/ConvertInt2String/ConvertInt2StringTests/ConvertInt2StringTests.cs
+++ strings-csharp/ConvertInt2String/ConvertInt2StringTests/ConvertInt2StringTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
 
 namespace ConvertInt2StringTests
 {
@@ -6,8 +7,75 @@ namespace ConvertInt2StringTests
     public class ConvertInt2StringTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenInt_WhenConvertedToStringWithMethodToString_ThenReturnString()
         {
+            int num = 3;
+            string str = num.ToString();
+
+            Assert.AreEqual("3", str);
+        }
+
+        [TestMethod]
+        public void GivenInt_WhenConvertedToStringWithMethodConvertToString_ThenReturnString()
+        {
+            int num = 3;
+            string str = System.Convert.ToString(num);
+
+            Assert.AreEqual("3", str);
+        }
+
+        [TestMethod]
+        public void GivenInt_WhenConvertedToStringWithMethodStringFormat_ThenReturnString()
+        {
+            int num = 3;
+            string str = string.Format("{0}", num);
+
+            Assert.AreEqual("3", str);
+        }
+
+        [TestMethod]
+        public void GivenInt_WhenConvertedToStringWithStringInterpolation_ThenReturnString()
+        {
+            int num = 3;
+            string str = $"{num}";
+
+            Assert.AreEqual("3", str);
+        }
+
+        [TestMethod]
+        public void GivenInt_WhenConvertedToStringWithMethodStringConcat_ThenReturnString()
+        {
+            int num = 3;
+            string str = string.Concat(string.Empty, num);
+
+            Assert.AreEqual("3", str);
+        }
+
+        [TestMethod]
+        public void GivenInt_WhenConvertedToStringWithMethodStringJoin_ThenReturnString()
+        {
+            int num = 3;
+            string str = string.Join(string.Empty, num);
+
+            Assert.AreEqual("3", str);
+        }
+
+        [TestMethod]
+        public void GivenInt_WhenConvertedToStringWithPlusSignConcatenation_ThenReturnString()
+        {
+            int num = 3;
+            string str = string.Empty + num;
+
+            Assert.AreEqual("3", str);
+        }
+
+        [TestMethod]
+        public void GivenInt_WhenConvertedToStringWithStringBuilderAppend_ThenReturnString()
+        {
+            int num = 3;
+            string str = new StringBuilder().Append(num).ToString();
+
+            Assert.AreEqual("3", str);
         }
     }
 }
