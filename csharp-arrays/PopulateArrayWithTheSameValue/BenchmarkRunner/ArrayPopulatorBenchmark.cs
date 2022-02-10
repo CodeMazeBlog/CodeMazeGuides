9d268f00dd571b433c0eba425ea53b3e134072a0
filipibc <filipibc@gmail.com>
2022-02-10 20:57:38 -0300

    How to populate or instantiate an array with the same value implementation

---
+++ csharp-arrays/PopulateArrayWithTheSameValue/BenchmarkRunner/ArrayPopulatorBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using PopulateArrayWithTheSameValue;
+
+namespace BenchmarkRunner
+{
+    public class ArrayPopulatorBenchmark
+    {
+        public static readonly ArrayPopulator _arrayPopulator = new();
+        private readonly Article[] benchMarkArray = new Article[1000 * 100];
+        private readonly Article benchmarkArticle = new()
+            { 
+                Title = "How to Copy Array Elements to New Array in C#", 
+                LastUpdate = new DateTime(2022, 01, 31) 
+            };
+
+        [Benchmark]
+        public void FillArray()
+        {
+            _arrayPopulator.FillArray(benchMarkArray, benchmarkArticle);
+        }
+
+        [Benchmark]
+        public void EnumerableRepeat()
+        {
+            _arrayPopulator.EnumerableRepeat(benchmarkArticle, benchMarkArray.Length);
+        }
+
+        [Benchmark]
+        public void ForStatement()
+        {
+            _arrayPopulator.ForStatement(benchMarkArray, benchmarkArticle);
+        }
+
+        [Benchmark]
+        public void ForStatementShallowCopy()
+        {
+            _arrayPopulator.ForStatementShallowCopy(benchMarkArray, benchmarkArticle);
+        }
+    }
+}
