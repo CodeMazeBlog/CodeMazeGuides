7ad33b7220363120427c9ffaef335b13c871cfb4
achinya <95957752+achinya@users.noreply.github.com>
2022-02-11 23:06:04 +0530

    CM-222 Different Ways to Iterate through a Dictionary
    CM-222 Different Ways to Iterate through a Dictionary in C# - class and project file

---
+++ collections-csharp/IterateThroughDictionary/IterateThroughDictionary/PerformAnalysisTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace IterateThroughDictionary
+{
+    public class PerformAnalysisTest
+    {
+        private Dictionary<int, string> TestValues = new Dictionary<int, string>();
+
+        private void FillData()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                TestValues.Add(i, "value-" + i);
+            }
+        }
+
+        public void TestDictionaryLoopResult()
+        {
+            FillData();
+            WhenDictionaryUsingForEach(TestValues);
+            WhenDictionaryUsingForLoop(TestValues);
+            WhenDictionaryParallelEnumerable(TestValues);
+            WhenDictionaryJoinString(TestValues);
+        }
+
+        private void WhenDictionaryUsingForEach(Dictionary<int, string> TestValues)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var result = 0;
+            foreach (int i in TestValues.Keys)
+            {
+                result += i;
+            }
+            watch.Stop();
+
+            Console.WriteLine($"Dictionary using ForEach loop - { watch.Elapsed} ms");
+        }
+
+        private void WhenDictionaryUsingForLoop(Dictionary<int, string> TestValues)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            for (int i = 0; i < TestValues.Count; i++)
+            {
+                var result = TestValues[i].Length + 1;
+            }
+            watch.Stop();
+
+            Console.WriteLine($"Dictionary using For loop - {watch.Elapsed} ms");
+        }
+
+        private void WhenDictionaryParallelEnumerable(Dictionary<int, string> TestValues)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            TestValues.AsParallel()
+                      .ForAll(val => val.Key.ToString());
+            watch.Stop();
+            
+            Console.WriteLine($"Dictionary using Parallel Enumerable - {watch.Elapsed} ms");
+        }
+
+        private void WhenDictionaryJoinString(Dictionary<int, string> TestValues)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            
+            var resultstring = (String.Join(Environment.NewLine, TestValues));
+            watch.Stop();
+
+            Console.WriteLine($"Dictionary using String Join - {watch.Elapsed} ms");
+        }
+    }
+}
---
+++ collections-csharp/IterateThroughDictionary/IterateThroughDictionary/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace IterateThroughDictionary
+{
+    public  class Program
+    {
+        public static void Main(string[]args)
+        {
+            var pTest = new PerformAnalysisTest();
+
+            var MonthsInYear = new Dictionary<int, string>
+            {
+                {1,"January" },
+                {2,"February" },
+                {3,"March" },
+                {4,"April" }
+            };
+
+            SubDictionaryUsingForEach(MonthsInYear);
+            SubDictionaryKeyValuePair(MonthsInYear);
+            SubDictionaryForLoop(MonthsInYear);
+            SubDictionaryParallelEnumerable(MonthsInYear);
+            SubDictionaryStringJoin(MonthsInYear);
+            pTest.TestDictionaryLoopResult();
+        }
+        
+        public static void SubDictionaryUsingForEach(Dictionary<int,string> MonthsInYear)
+        {
+            foreach (var month in MonthsInYear)
+            {
+                Console.WriteLine($"{month.Key } : {month.Value}");
+            }
+        }
+
+        public static void SubDictionaryKeyValuePair(Dictionary<int,string> MonthsInYear)
+        {
+            foreach (KeyValuePair<int,string> entry in MonthsInYear)
+            {
+                Console.WriteLine($"{entry.Key} : {entry.Value}");
+            }
+
+            foreach (var (key,value) in MonthsInYear)
+            {
+                Console.WriteLine($"{key} : {value}");   
+            }
+        }
+
+        public static void SubDictionaryForLoop(Dictionary<int, string> MonthsInYear)
+        {
+            for (int index = 0; index < MonthsInYear.Count; index ++)
+            {
+                KeyValuePair<int, string> month = MonthsInYear.ElementAt(index);
+
+                Console.WriteLine($"{month.Key} : {month.Value}");
+            }
+        }
+
+        public static void SubDictionaryParallelEnumerable(Dictionary<int, string> MonthsInYear)
+        {
+            MonthsInYear.AsParallel()
+                        .OrderBy(month => month.Key)
+                        .ForAll(month => Console.WriteLine($"{month.Key} : {month.Value}"));
+        }
+
+        public static void SubDictionaryStringJoin(Dictionary<int, string> MonthsInYear)
+        {
+            Console.WriteLine(String.Join(Environment.NewLine, MonthsInYear));
+        }
+    }
+}
