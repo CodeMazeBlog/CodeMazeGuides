4bd79205cc0051fa55325e631f2d340d6562e9ee
Sara Ahmed <saraahmed2693@gmail.com>
2022-02-12 12:43:28 +0200

    Add files via upload

---
+++ csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/FuncDelegates/FuncDelegates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncDelegates
+{
+    public class FuncDelegates
+    {
+
+        public delegate string RemoteControl(string button);
+        public static string TurnOff(string b)
+        {
+            return "TV is Turned Off";
+        }
+        public static string TurnOn(string b)
+        {
+            return "TV is Turned On";
+        }
+
+        static void Main(string[] args)
+        {
+            Func<string, string> rcDelegate2 = TurnOff; 
+            Func<string, string> rcDelegate1 = TurnOn; 
+            
+            string result1 = rcDelegate1("Power On Button"); 
+            string result2 = rcDelegate2("Shut down Button"); 
+            
+            Console.WriteLine(result1); 
+            Console.WriteLine(result2);
+        }
+    }
+}
