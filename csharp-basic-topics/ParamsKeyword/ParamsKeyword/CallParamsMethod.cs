88d8dee4286628298f05cd00376a3a03fc3f26bc
Bartosz Jarmuż <bartosz.jarmuz@gmail.com>
2022-02-13 18:16:01 +0100

    added sample code

---
+++ csharp-basic-topics/ParamsKeyword/ParamsKeyword/CallParamsMethod.cs
@@ -0,0 +1,82 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ParamsKeyword
+{
+    [TestFixture]
+    public class CallParamsMethod
+    {
+        [Test]
+        public void CommaDelimitedListOfArgs_ReturnsValidResult()
+        {
+            //capture the console output
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var shoppingList = new ShoppingList();
+            shoppingList.Add("Bananas", "Grapes", "Ham", "Cheese");
+            shoppingList.Add("Headphones");
+
+            var expectedResult = new string[] {
+                "Added: Bananas",
+                "Added: Grapes",
+                "Added: Ham",
+                "Added: Cheese",
+                "Added: Headphones",
+            };
+
+            Assert.AreEqual(expectedResult, writer.ToString().Split("\r\n", StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        [Test]
+        public void CollectionArgument_ReturnsValidResult()
+        {
+            //capture the console output
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var shoppingList = new ShoppingList();
+            var groceries = new [] { "Bananas", "Grapes", "Ham", "Cheese" };
+            shoppingList.Add(groceries);
+
+            var expectedResult = new string[] {
+                "Added: Bananas",
+                "Added: Grapes",
+                "Added: Ham",
+                "Added: Cheese"
+            };
+
+            Assert.AreEqual(expectedResult, writer.ToString().Split("\r\n", StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        [Test]
+        public void NoArguments_ReturnsValidResult()
+        {
+            //capture the console output
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var shoppingList = new ShoppingList();
+            shoppingList.Add();
+
+            Assert.AreEqual(string.Empty, writer.ToString());
+        }
+
+        [Test]
+        public void NullArgument_ThrowsAnException()
+        {
+            //capture the console output
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var shoppingList = new ShoppingList();
+            string[]? groceries = default;
+
+#pragma warning disable CS8604 // Possible null reference argument.
+            Assert.Throws<NullReferenceException>(()=>shoppingList.Add(groceries));
+#pragma warning restore CS8604 // Possible null reference argument.
+        }
+    }
+}
