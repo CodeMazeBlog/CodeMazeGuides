70841b3a1c652a7e98f54762568208f0ad99586d
Zoran_Despotovski <zoran.despotovski@deptagency.com>
2022-02-17 22:58:15 +0100

    Moved benchmark code in separate class.

--- strings-csharp/ConvertInt2String/ConvertInt2String/Program.cs
+++ strings-csharp/ConvertInt2String/ConvertInt2String/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+﻿using ConvertInt2String;
 using System.Text;
 
 var luckyNumber = 3;
@@ -14,134 +14,10 @@ Console.WriteLine(string.Empty + luckyNumber);
 Console.WriteLine(luckyNumber + luckyNumber + string.Empty + luckyNumber + luckyNumber);
 Console.WriteLine(new StringBuilder().Append(luckyNumber).ToString());
 
-var repsV1 = 20000;
-var repsV2 = 1000000000;
-const int formatOffsetLeft = 23;
-const int formatOffsetRight = 10;
-StringBuilder benchmarkResultsBuilder = new StringBuilder($"\r\nBenchmarks with {repsV1:N0} repetitions: \r\n")
-    .AppendLine($"|{"Method",formatOffsetLeft} | {"Speed (ms)",formatOffsetRight}|")
-    .AppendLine($"|{"-----------------------",formatOffsetLeft}-|-{"----------",formatOffsetRight}|")
-    .AppendLine(BenchmarkToString(luckyNumber, repsV1))
-    .AppendLine(BenchmarkConvertToString(luckyNumber, repsV1))
-    .AppendLine(BenchmarkStringFormat(luckyNumber, repsV1))
-    .AppendLine(BenchmarkStringInterpolation(luckyNumber, repsV1))
-    .AppendLine(BenchmarkStringConcat(luckyNumber, repsV1))
-    .AppendLine(BenchmarkStringJoin(luckyNumber, repsV1))
-    .AppendLine(BenchmarkPlusSign(luckyNumber, repsV1))
-    .AppendLine(BenchmarkStringBuilderAppend(luckyNumber, repsV1))
-    .AppendLine($"\r\n \r\nBenchmarks with {repsV2:N0} repetitions:")
-    .AppendLine($"|{"Method",formatOffsetLeft} | {"Speed (ms)",formatOffsetRight}|")
-    .AppendLine($"|{"-----------------------",formatOffsetLeft}-|-{"----------",formatOffsetRight}|")
-    .AppendLine(BenchmarkToString(luckyNumber, repsV2))
-    .AppendLine(BenchmarkConvertToString(luckyNumber, repsV2))
-    .AppendLine(BenchmarkStringFormat(luckyNumber, repsV2))
-    .AppendLine(BenchmarkStringInterpolation(luckyNumber, repsV2))
-    .AppendLine(BenchmarkStringConcat(luckyNumber, repsV2))
-    .AppendLine(BenchmarkStringJoin(luckyNumber, repsV2))
-    .AppendLine(BenchmarkPlusSign(luckyNumber, repsV2))
-    .AppendLine(BenchmarkStringBuilderAppend(luckyNumber, repsV2));
-Console.WriteLine(benchmarkResultsBuilder.ToString());
+Console.WriteLine(Benchmark.Run(luckyNumber, 20000));
+Console.WriteLine(Benchmark.Run(luckyNumber, 1000000000));
 
 Console.ReadLine();
-
-#region Benchmark Methods
-static string BenchmarkToString(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = num.ToString();
-    }
-    watch.Stop();
-
-    return $"|{"'ToString'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-static string BenchmarkConvertToString(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = num.ToString();
-    }
-    watch.Stop();
-
-    return $"|{"'ConvertToString'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-static string BenchmarkStringFormat(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = string.Format("{0}", num);
-    }
-    watch.Stop();
-
-    return $"|{"'String.Format'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-static string BenchmarkStringInterpolation(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = $"{num}";
-    }
-    watch.Stop();
-
-    return $"|{"'String Interpolation'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-static string BenchmarkStringConcat(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = string.Concat(string.Empty, num);
-    }
-    watch.Stop();
-
-    return $"|{"'String.Concat'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-static string BenchmarkStringJoin(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = string.Join(string.Empty, num);
-    }
-    watch.Stop();
-
-    return $"|{"'String.Join'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-static string BenchmarkPlusSign(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = string.Empty + num;
-    }
-    watch.Stop();
-
-    return $"|{"'Plus sign'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-static string BenchmarkStringBuilderAppend(int num, int reps)
-{
-    var watch = new Stopwatch();
-    watch.Start();
-    for (int i = 0; i < reps; i++)
-    {
-        var tmpResult = new StringBuilder().Append(num).ToString();
-    }
-    watch.Stop();
-
-    return $"|{"'Stringbuilder Append'",formatOffsetLeft} | {watch.ElapsedMilliseconds,formatOffsetRight:N0}|";
-}
-#endregion
 
 
 
