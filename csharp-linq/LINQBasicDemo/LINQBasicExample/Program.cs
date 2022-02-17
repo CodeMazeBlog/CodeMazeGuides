472e61d549920dfb14f774f0221151c3edff7647
ShaJay111@hotmail.com <ShaJay111@hotmail.com>
2022-02-17 16:54:19 +0530

    initial

---
+++ csharp-linq/LINQBasicDemo/LINQBasicExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQBasicExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
