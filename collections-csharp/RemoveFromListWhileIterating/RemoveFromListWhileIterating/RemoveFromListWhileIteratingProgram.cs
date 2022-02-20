9a8ba9bf87d4eb39609e4ebe9386985dfda56483
dirk.coetsee <dirk.coetsee@gmail.com>
2022-02-20 19:26:55 +0200

    Near final commit

---
+++ collections-csharp/RemoveFromListWhileIterating/RemoveFromListWhileIterating/RemoveFromListWhileIteratingProgram.cs
@@ -0,0 +1,128 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace RemoveFromListWhileIterating
+{
+    [MemoryDiagnoser]
+    public class RemoveFromListWhileIteratingProgram
+    {
+        private static int TestIterations = 100;
+        private static List<int> NumberList = Enumerable.Range(1, 200000).ToList();
+        
+        [Benchmark]
+        public void RunReverseIterate()
+        {
+            for (int i = 0; i < TestIterations; i++)
+            {
+                ReverseIterate(NumberList);
+            }
+        }
+
+        [Benchmark]
+        public void RunSimpleIterateRemoveWithToList()
+        {
+            for (int i = 0; i < TestIterations; i++)
+            {
+                SimpleIterateRemoveWithToList(NumberList);
+            }
+        }
+
+        [Benchmark]
+        public void RunOneLineRemoveAll()
+        {
+            for (int i = 0; i < TestIterations; i++)
+            {
+                OneLineRemoveAll(NumberList);
+            }
+        }
+        
+
+        [Benchmark]
+        public void RunRemoveWithLinq()
+        {
+            for (int i = 0; i < TestIterations; i++)
+            {
+                RemoveWithLinq(NumberList);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<RemoveFromListWhileIteratingProgram>();
+        }
+
+        public static List<int> NaiveIterateRemove(List<int> sourceList)
+        {
+            //This will fail with System.InvalidOperationException:
+            foreach (var el in sourceList)
+            {
+                if (el % 2 == 0) //try remove evens
+                {
+                    sourceList.Remove(el);
+                }
+            }
+
+            return sourceList;
+        }
+
+        public static List<int> ReverseIterate(List<int> sourceList)
+        {
+            //by iterating backwards, we avoid InvalidOperationException
+            for (int i = sourceList.Count - 1; i >= 0; i--)
+            {
+                if (sourceList[i] % 2 == 0)
+                {
+                    sourceList.RemoveAt(i);
+                }
+            }
+            //NB: it is also possible to use the .Reverse() operator but this will create an array copy so you may as well use .ToList()
+            return sourceList;
+        }
+
+        public static List<int> SimpleIterateRemoveWithToList(List<int> sourceList)
+        {
+            foreach (var el in sourceList.ToList())
+            {
+                if (el % 2 == 0) //remove evens
+                {
+                    sourceList.Remove(el);
+                }
+            }
+
+            return sourceList;
+        }
+
+        public static List<int> OneLineRemoveAll(List<int> sourceList)
+        {
+            sourceList.RemoveAll(el => el % 2 == 0);
+            return sourceList;
+        }
+
+        public static void PerformOperation(int element)
+        {
+            // This method would contain the logic needed to be performed
+            // per iteration on each element
+            Console.WriteLine($"Checking {element}");
+        }
+
+        public static List<int> OneLineRemoveAllWithSideEffect(List<int> sourceList)
+        {
+            sourceList.RemoveAll(item =>
+            {
+                PerformOperation(item);
+                // In the end return true if the item is to be removed
+                return item % 2 == 0;
+            }); ;
+            return sourceList;
+        }
+
+        public static List<int> RemoveWithLinq(List<int> sourceList)
+        {
+            var newList = (from el in sourceList
+                where el % 2 != 0
+                select el).ToList();
+
+            return newList;
+        }
+    }
+}
---
+++ collections-csharp/RemoveFromListWhileIterating/RemoveFromListWhileIteratingTests/RemoveFromListWhileIteratingTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveFromListWhileIterating;
+
+namespace RemoveFromListWhileIteratingTests
+{
+    [TestClass]
+    public class RemoveFromListWhileIteratingTests
+    {
+        private static List<int> NumberList = Enumerable.Range(1, 20).ToList();
+        private static List<int> NumberListCopy = Enumerable.Range(1, 20).ToList();
+        private static List<int> OddNumbers = NumberList.Where(i => i % 2 != 0).ToList();
+
+        [TestMethod]
+        public void WhenIteratingForward_ThenExceptionIsThrown()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => RemoveFromListWhileIteratingProgram.NaiveIterateRemove(NumberList));
+        }
+
+        [TestMethod]
+        public void WhenIteratingBackwards_RemoveSucceedsAndOriginalIsModified()
+        {
+            var filteredList = RemoveFromListWhileIteratingProgram.ReverseIterate(NumberList);
+            CollectionAssert.AreEqual(OddNumbers, filteredList);
+            CollectionAssert.AreEqual(filteredList, NumberList);
+        }
+
+        [TestMethod]
+        public void WhenUsingToList_NoExceptionIsThrownAndOriginalIsModified()
+        {
+            var filteredList = RemoveFromListWhileIteratingProgram.SimpleIterateRemoveWithToList(NumberList);
+            CollectionAssert.AreEqual(OddNumbers, filteredList);
+            CollectionAssert.AreEqual(filteredList, NumberList);
+        }
+
+        [TestMethod]
+        public void WhenUsingRemoveAll_ListIsFilteredAndOriginalIsModified()
+        {
+            var filteredList = RemoveFromListWhileIteratingProgram.OneLineRemoveAll(NumberList);
+            CollectionAssert.AreEqual(OddNumbers, filteredList);
+            CollectionAssert.AreEqual(filteredList, NumberList); 
+        }
+
+        [TestMethod]
+        public void WhenUsingRemoveAllWithSideEffect_ListIsFilteredAndOriginalIsModified()
+        {
+            var filteredList = RemoveFromListWhileIteratingProgram.OneLineRemoveAllWithSideEffect(NumberList);
+            CollectionAssert.AreEqual(OddNumbers, filteredList);
+            CollectionAssert.AreEqual(filteredList, NumberList);
+        }
+
+        [TestMethod]
+        public void WhenUsingLinqSelect_OriginalListUnmodified()
+        {
+            var filteredList = RemoveFromListWhileIteratingProgram.RemoveWithLinq(NumberList);
+            CollectionAssert.AreEqual(OddNumbers, filteredList);
+            CollectionAssert.AreEqual(NumberListCopy, NumberList);
+            CollectionAssert.AreNotEqual(filteredList, NumberList);
+        }
+
+
+    }
+}
