b71b0bc1e58704b36330fdd4d7cc7123e5aca8e6
Mathew Grabau <mgrabau@mathewgrabau.ca>
2022-02-22 00:37:04 -0600

    Updated to a more basic example

---
+++ csharp-intermediate-topics/BasicDelegates/BasicDelegates/BasicDelegates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BasicDelegates
+{
+    public class BasicDelegates
+    {
+        static void PrintGreeting()
+        {
+            Console.WriteLine("Hello from PrintGreeting!");
+        }
+
+        static void DelegateReceiver(Action delegateToUse)
+        {
+            delegateToUse();
+        }
+
+        public static void Main(string[] args)
+        {
+            // Each of these will invoke the PrintGreeting method.
+            Action printDelegate;
+            printDelegate = PrintGreeting;
+            PrintGreeting();
+
+            // Using the delegate as a variable.
+            printDelegate();
+
+            // Passing it to another parameter.
+            DelegateReceiver(printDelegate);
+        }
+    }
+}
