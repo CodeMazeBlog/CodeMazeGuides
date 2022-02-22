5c32e98aee5bb6e9acf003c9cd76dfdf49c7a56a
achinya <95957752+achinya@users.noreply.github.com>
2022-02-22 16:28:25 +0530

    CM-222 Different Ways to Iterate through a Dictionary
    CM-222 Different Ways to Iterate through a Dictionary in C# 
    review changes done

--- collections-csharp/IterateThroughDictionary/IterateThroughDictionary/PerformAnalysisTest.cs
+++ collections-csharp/IterateThroughDictionary/IterateThroughDictionary/PerformAnalysisTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -7,32 +7,32 @@ namespace IterateThroughDictionary
 {
     public class PerformAnalysisTest
     {
-        private Dictionary<int, string> TestValues = new Dictionary<int, string>();
+        private Dictionary<int, string> _testValues = new Dictionary<int, string>();
 
         private void FillData()
         {
             for (int i = 0; i < 100000; i++)
             {
-                TestValues.Add(i, "value-" + i);
+                _testValues.Add(i, "value-" + i);
             }
         }
 
         public void TestDictionaryLoopResult()
         {
             FillData();
-            WhenDictionaryUsingForEach(TestValues);
-            WhenDictionaryUsingForLoop(TestValues);
-            WhenDictionaryParallelEnumerable(TestValues);
-            WhenDictionaryJoinString(TestValues);
+            WhenDictionaryUsingForEach(_testValues);
+            WhenDictionaryUsingForLoop(_testValues);
+            WhenDictionaryParallelEnumerable(_testValues);
+            WhenDictionaryJoinString(_testValues);
         }
 
-        private void WhenDictionaryUsingForEach(Dictionary<int, string> TestValues)
+        private void WhenDictionaryUsingForEach(Dictionary<int, string> _testValues)
         {
             var watch = new Stopwatch();
             watch.Start();
 
             var result = 0;
-            foreach (int i in TestValues.Keys)
+            foreach (int i in _testValues.Keys)
             {
                 result += i;
             }
@@ -41,38 +41,38 @@ namespace IterateThroughDictionary
             Console.WriteLine($"Dictionary using ForEach loop - { watch.Elapsed} ms");
         }
 
-        private void WhenDictionaryUsingForLoop(Dictionary<int, string> TestValues)
+        private void WhenDictionaryUsingForLoop(Dictionary<int, string> _testValues)
         {
             var watch = new Stopwatch();
             watch.Start();
 
-            for (int i = 0; i < TestValues.Count; i++)
+            for (int i = 0; i < _testValues.Count; i++)
             {
-                var result = TestValues[i].Length + 1;
+                var result = _testValues[i].Length + 1;
             }
             watch.Stop();
 
             Console.WriteLine($"Dictionary using For loop - {watch.Elapsed} ms");
         }
 
-        private void WhenDictionaryParallelEnumerable(Dictionary<int, string> TestValues)
+        private void WhenDictionaryParallelEnumerable(Dictionary<int, string> _testValues)
         {
             var watch = new Stopwatch();
             watch.Start();
 
-            TestValues.AsParallel()
+            _testValues.AsParallel()
                       .ForAll(val => val.Key.ToString());
             watch.Stop();
             
             Console.WriteLine($"Dictionary using Parallel Enumerable - {watch.Elapsed} ms");
         }
 
-        private void WhenDictionaryJoinString(Dictionary<int, string> TestValues)
+        private void WhenDictionaryJoinString(Dictionary<int, string> _testValues)
         {
             var watch = new Stopwatch();
             watch.Start();
             
-            var resultstring = (String.Join(Environment.NewLine, TestValues));
+            var resultstring = (String.Join(Environment.NewLine, _testValues));
             watch.Stop();
 
             Console.WriteLine($"Dictionary using String Join - {watch.Elapsed} ms");
