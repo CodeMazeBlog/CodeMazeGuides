6ba41584a5a408f375e4630b90e70ebb10ae0b62
George Zarogkikas <gzg@sitecore.net>
2022-02-27 18:13:53 +0200

    evaluation article Code

---
+++ func-action-delegates/src/ActionDelegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ActionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string, string> actionDelegate = Print;
+            actionDelegate("Code", "Maze");
+            Console.ReadLine();
+        }
+        static void Print(string firstName, string lastName)
+        {
+            string fullName = firstName + lastName;
+            Console.WriteLine($"{fullName}");
+        }
+    }
+}
---
+++ func-action-delegates/src/FuncDelegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace FuncDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, string> funcDelegate = Append;
+            string fullName = funcDelegate("Code", "Maze");
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+        }
+        static string Append(string firstName, string lastName)
+        {
+            return firstName + lastName;
+        }
+    }
+}
