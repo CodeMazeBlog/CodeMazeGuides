635ae3f89d6a62839145d4c92843f3d944cf0ea4
Martin Chege <martinchege324@gmail.com>
2022-02-28 19:47:15 +0300

    removed redundant methods used for  the benchmark

--- csharp-algorithms/BubbleSort/BubbleSort/Bubble.cs
+++ csharp-algorithms/BubbleSort/BubbleSort/Bubble.cs
@@ -12,27 +12,10 @@ namespace BubbleSort
         int[] sortedSmallArray = GenerateSortedNumber(200);
         int[] sortedMediumArray = GenerateSortedNumber(2000);
         int[] sortedLargeArray = GenerateSortedNumber(200000);
-        public IEnumerable<int[]> ArraySizes => new[] { smallArray, mediumArray, largeArray, sortedSmallArray, sortedMediumArray, sortedLargeArray };
+        public IEnumerable<int[]> ArraySizes => new[] { sortedSmallArray, sortedMediumArray, sortedLargeArray };
 
         [Benchmark]
         public int[] SortArray() 
-        {
-            var n = NumArray.Length;
-
-            for (int i = 0; i < n - 1; i++)
-                for (int j = 0; j < n - i - 1; j++)
-                    if (NumArray[j] > NumArray[j + 1])
-                    {
-                        var tempVar = NumArray[j];
-                        NumArray[j] = NumArray[j + 1];
-                        NumArray[j + 1] = tempVar;
-                    }
-
-            return NumArray;
-        }
-
-        [Benchmark]
-        public int[] SortOrderedArray()
         {
             var n = NumArray.Length;
 
@@ -69,30 +52,6 @@ namespace BubbleSort
                     break;
             }
    
-            return NumArray;
-        }
-
-        [Benchmark]
-        public int[] SortOptimizedOrderedArray()
-        {
-            var n = NumArray.Length;
-            bool swapRequired;
-
-            for (int i = 0; i < n - 1; i++)
-            {
-                swapRequired = false;
-                for (int j = 0; j < n - i - 1; j++)
-                    if (NumArray[j] > NumArray[j + 1])
-                    {
-                        var tempVar = NumArray[j];
-                        NumArray[j] = NumArray[j + 1];
-                        NumArray[j + 1] = tempVar;
-                        swapRequired = true;
-                    }
-                if (swapRequired == false)
-                    break;
-            }
-
             return NumArray;
         }
 
