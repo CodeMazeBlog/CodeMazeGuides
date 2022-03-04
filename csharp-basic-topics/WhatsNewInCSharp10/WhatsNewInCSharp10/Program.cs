dd2019f85e9bf75037431e346ef364c5139dc9a1
Domagoj Beti <domagoj.beti@gmail.com>
2022-03-04 14:49:09 +0100

    Apply suggestions.

--- csharp-basic-topics/WhatsNewInCSharp10/WhatsNewInCSharp10/Program.cs
+++ csharp-basic-topics/WhatsNewInCSharp10/WhatsNewInCSharp10/Program.cs
@@ -1,13 +1,24 @@
-const string attribute = "not awesome";
-const string message = $"Code maze is {attribute}.";
+// Custom interpolated string handlers example
+var attribute = "not awesome"; 
 
-ProcessMessage($"Code maze is {attribute}.");
-
-var codeMazeArticleRepresentation = new CodeMazeArticle("Author", "Title", "Comment").ToString();
+var processedMessage = ProcessMessage($"Code maze is {attribute}.");
 
 string ProcessMessage(CustomInterpolatedStringHandler builder) => builder.GetFormattedText();
 
+// Lambda expression improvements
+var lambda = [DebuggerStepThrough]() => "Hello world";
+
+// Constant interpolated strings example
+const string constantAttribute = "not awesome";
+const string constantMessage = $"Code maze is {constantAttribute}.";
+
+// Sealing records ToString method example
+var codeMazeArticleRepresentation = new CodeMazeArticle("Author", "Title", "Comment").ToString();
+
+// Assignment and declaration in the same deconstruction example
 var articles = (new Article("Author", "Title"), new CodeMazeArticle("Author", "Title", "Comment"));
-Article article = new Article("Another author", "Another title");
+var article = new Article("Another author", "Another title");
 
 (article, CodeMazeArticle codeMazeArticle) = articles;
+
+var areAuthorsEqual = article.Author == codeMazeArticle.Author;
--- csharp-basic-topics/WhatsNewInCSharp10/WhatsNewInCSharp10/Usings.cs
+++ csharp-basic-topics/WhatsNewInCSharp10/WhatsNewInCSharp10/Usings.cs
@@ -1,3 +1,4 @@
-﻿global using System.Runtime.CompilerServices;
+﻿global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
 global using System.Text;
 global using WhatsNewInCSharp10;
