c1305ba8a5ab4137eba4fc8070bc611653422ef5
DvD <dvd1202@gmail.com>
2022-03-05 19:52:03 +0100

    - Moving to relevant folder - Test names following guidelines

--- strings-csharp/UsingVariablesInsideStrings/UsingVariablesInsideStrings/MyClass.cs
+++ strings-csharp/UsingVariablesInsideStrings/UsingVariablesInsideStrings/MyClass.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UsingVariablesInsideStrings
+﻿namespace UsingVariablesInsideStrings
 {
     public class MyClass
     {
         public int MyNumber { get; set; }
+
         public MyClass(int num)
         {
             MyNumber = num;
         }
+
         public override string ToString()
         {
             return MyNumber.ToString();
