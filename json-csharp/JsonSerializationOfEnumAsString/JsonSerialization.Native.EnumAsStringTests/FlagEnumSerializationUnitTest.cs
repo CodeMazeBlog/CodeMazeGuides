5fed6d807451eb4fb28efb47041caa45e7b7169c
Ahsan <ullahahsan1983@gmail.com>
2022-03-06 11:26:17 +0600

    Included test for flag enum

---
+++ json-csharp/JsonSerializationOfEnumAsString/JsonSerialization.Native.EnumAsStringTests/FlagEnumSerializationUnitTest.cs
@@ -0,0 +1,26 @@
+using JsonSerialization.EnumAsString.Models;
+
+namespace JsonSerialization.Native.EnumAsStringTests;
+
+public class FlagEnumSerializationUnitTest : UnitTestBase
+{
+    [Fact]
+    public void GivenFlagEnum_WhenSerializeByDefault_ThenSerializeAsCombinedNumber()
+    {
+        var style = TextStyle.Bold | TextStyle.Italic | TextStyle.Underline;
+
+        var json = Serialize(new { Format = style });
+
+        Assert.Equal("{\"Format\":7}", json);
+    }
+
+    [Fact]
+    public void GivenFlagEnum_WhenSerializeWithEnumConverter_ThenSerializeAsCsvString()
+    {
+        var style = TextStyle.Bold | TextStyle.Italic | TextStyle.Underline;
+
+        var json = SerializeWithStringEnum(new { Format = style });
+
+        Assert.Equal("{\"Format\":\"Bold, Italic, Underline\"}", json);
+    }
+}
