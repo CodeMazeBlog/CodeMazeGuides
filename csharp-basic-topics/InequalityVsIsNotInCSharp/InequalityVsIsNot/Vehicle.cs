d9a90dc1f075f6455788a4d686ed30ade7eaadd0
Flavia Tavani <flavia.tavani@gmail.com>
2022-03-08 19:25:29 +0100

    added operator overload

--- csharp-basic-topics/InequalityVsIsNotInCSharp/InequalityVsIsNot/Vehicle.cs
+++ csharp-basic-topics/InequalityVsIsNotInCSharp/InequalityVsIsNot/Vehicle.cs
@@ -4,5 +4,25 @@
     {
         public Brand VehicleBrand { get; set; }
         public int SerialNumber { get; set; }
+
+        public static bool operator ==(Vehicle vehicle, Vehicle otherVehicle)
+        {
+            if (otherVehicle is not null)
+            {
+                return otherVehicle.VehicleBrand == vehicle.VehicleBrand;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Vehicle person1, Vehicle person2)
+        {
+            if (person2 is not null)
+            {
+                return person2.VehicleBrand != person1.VehicleBrand;
+            }
+
+            return false;
+        }
     }
 }
