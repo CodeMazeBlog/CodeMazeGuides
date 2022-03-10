3f2cef8fd4155c1a3f67bc2e36721e79da59db0c
alexandra <alexandrabalean92@gmail.com>
2022-03-10 19:40:06 +0200

    Use var on object declarations instead of direct type

--- dotnet-datetime/GetDatePartFromDateTime/GetDatePartFromDateTime/Program.cs
+++ dotnet-datetime/GetDatePartFromDateTime/GetDatePartFromDateTime/Program.cs
@@ -1,5 +1,5 @@
-﻿DateTime date1 = new DateTime(2022, 02, 14, 10, 40, 00); 
-DateTime date2 = new DateTime(2018, 10, 18, 11, 23, 34); 
+﻿var date1 = new DateTime(2022, 02, 14, 10, 40, 00); 
+var date2 = new DateTime(2018, 10, 18, 11, 23, 34); 
 
 Console.WriteLine("Full Date:"); 
 Console.WriteLine(date1.ToString()); 
@@ -13,22 +13,22 @@ Console.WriteLine("Hide the time part:");
 Console.WriteLine(date1.Date.ToString("MM/dd/yyyy")); 
 Console.WriteLine(date2.Date.ToString("dd/MM/yyyy"));
 
-DateTime date3 = new DateTime(2022, 02, 14, 10, 40, 00); 
+var date3 = new DateTime(2022, 02, 14, 10, 40, 00); 
 
 Console.WriteLine("Short Date Value:"); 
 Console.WriteLine(date3.Date.ToShortDateString());
 
-DateTime date = new DateTime(2021, 7, 8, 11, 10, 9); 
-DateOnly dateOnly = new DateOnly(date.Year, date.Month, date.Day); 
+var date = new DateTime(2021, 7, 8, 11, 10, 9); 
+var dateOnly = new DateOnly(date.Year, date.Month, date.Day); 
 
 Console.WriteLine(dateOnly);
 
-DateTime may12 = new DateTime(2022, 5, 12, 10, 15, 11); 
+var may12 = new DateTime(2022, 5, 12, 10, 15, 11); 
 
 Console.WriteLine(String.Format($"{may12:d}")); 
 Console.WriteLine(String.Format($"{may12:D}"));
 
-DateTime april10 = new DateTime(2022, 4, 10, 10, 15, 11); 
+var april10 = new DateTime(2022, 4, 10, 10, 15, 11); 
 
 Console.WriteLine(String.Format($"{april10:dd-MM-yy}")); 
 Console.WriteLine(String.Format($"{april10:ddd, dd MM yy}")); 
