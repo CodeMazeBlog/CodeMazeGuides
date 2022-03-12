0ae41e3e5c7ddcafa48c011d2159207c562ec77c
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2022-03-12 23:46:51 +0530

    CM-83: Serialization and Deserialization in C# code sample

---
+++ aspnetcore-webapi/SerializationDemo/SerializationDemo.Client/Clients/IClient.cs
@@ -0,0 +1,11 @@
+﻿using SerializationDemo.Common.Models;
+
+namespace SerializationDemo.Client.Clients
+{
+    internal interface IClient
+    {       
+        Task<List<Employee>> GetAllEmployees();
+        Task<Employee> CreateEmployee(Employee employee);
+
+    }
+}
