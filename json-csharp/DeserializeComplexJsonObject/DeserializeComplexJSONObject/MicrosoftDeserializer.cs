4bc5ee3ffc8e8766d4b8972254159a725c0cd8db
Filipi Braga <filipibc@gmail.com>
2022-03-14 06:12:45 -0300

    CM-473: How to Deserialize a Complex JSON Object in C# .NET? (#370)

---
+++ json-csharp/DeserializeComplexJsonObject/DeserializeComplexJSONObject/MicrosoftDeserializer.cs
@@ -0,0 +1,22 @@
+﻿using DeserializeComplexJSONObject.POCO;
+using System.Text.Json;
+
+namespace DeserializeComplexJSONObject
+{
+    public class MicrosoftDeserializer
+    {
+        public Company? DeserializeUsingGenericSystemTextJson(string json)
+        {
+            var company = JsonSerializer.Deserialize<Company>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            return company;
+        }
+
+        public Company? DeserializeUsingSystemTextJson(string json)
+        {
+            var company = (Company?)JsonSerializer.Deserialize(json, typeof(Company), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            return company;
+        }
+    }
+}
