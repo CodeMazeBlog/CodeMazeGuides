72c1999860965d2d43b0555c674eda5a85e48cec
Domagoj Beti <domagoj.beti@gmail.com>
2022-03-17 00:16:08 +0100

    Initial ModelStateValidation.

---
+++ aspnetcore-webapi/ModelStateValidation/ModelStateValidation/ActionFilters/ValidationFilterAtribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ModelStateValidation.ActionFilters;
+
+public class ValidationFilterAttribute : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+        }
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context) {}
+}
