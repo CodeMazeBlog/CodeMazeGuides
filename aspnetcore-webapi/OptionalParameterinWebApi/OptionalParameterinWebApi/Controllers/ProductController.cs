62b3b04a2ca1195d43b6a1003afdc0fbc0aedffd
King-alex-d-great <58665121+king-Alex-d-great@users.noreply.github.com>
2022-03-25 09:46:20 +0100

    CM-304 : Optional Parameters in ASP.NET Core Web API Routing

---
+++ aspnetcore-webapi/OptionalParameterinWebApi/OptionalParameterinWebApi/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OptionalParameterinWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private static readonly string[] Products = new[]
+        {
+            "Sweater", "Umbrella", "Jacket", "Polo", "Boots", "Microwave", "Schoolbag", "Sunshade", "SKinny Jeans", "Sunscreen"
+        };
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var products = new List<Product>();
+
+            for (int i = 0; i < Products.Length; i++)
+            {
+                products.Add(new Product
+                {
+                    Id = i + 1,
+                    Price = ($" ${Random.Shared.Next(100, 6000)}"),
+                    Name = Products[i]
+                });
+            }
+
+            return products;
+        }
+
+        [HttpGet("GetById/{id:int?}")]
+        public Product GetById(int id = 1)
+        {
+            var products = Get();
+
+            return products.Where(p => p.Id == id).FirstOrDefault()!;
+        }
+    }
+}
+
