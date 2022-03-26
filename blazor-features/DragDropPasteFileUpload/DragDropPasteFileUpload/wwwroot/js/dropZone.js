681bfb76d0e4314fbf13a3cff5dc826646f6ae0c
PhilBroderick <philbroderick12@gmail.com>
2022-03-26 20:09:36 +0000

    solution commit

---
+++ blazor-features/DragDropPasteFileUpload/DragDropPasteFileUpload/wwwroot/js/dropZone.js
@@ -0,0 +1,48 @@
+﻿export function initializeFileDropZone(dropZoneElement, inputFile) {
+    // Add a class when the user drags a file over the drop zone
+    function onDragHover(e) {
+        e.preventDefault();
+        dropZoneElement.classList.add("hover");
+    }
+
+    function onDragLeave(e) {
+        e.preventDefault();
+        dropZoneElement.classList.remove("hover");
+    }
+
+    // Handle the paste and drop events
+    function onDrop(e) {
+        e.preventDefault();
+        dropZoneElement.classList.remove("hover");
+
+        // Set the files property of the input element and raise the change event
+        inputFile.files = e.dataTransfer.files;
+        const event = new Event('change', { bubbles: true });
+        inputFile.dispatchEvent(event);
+    }
+
+    function onPaste(e) {
+        // Set the files property of the input element and raise the change event
+        inputFile.files = e.clipboardData.files;
+        const event = new Event('change', { bubbles: true });
+        inputFile.dispatchEvent(event);
+    }
+
+    // Register all events
+    dropZoneElement.addEventListener("dragenter", onDragHover);
+    dropZoneElement.addEventListener("dragover", onDragHover);
+    dropZoneElement.addEventListener("dragleave", onDragLeave);
+    dropZoneElement.addEventListener("drop", onDrop);
+    dropZoneElement.addEventListener('paste', onPaste);
+
+    // The returned object allows to unregister the events when the Blazor component is destroyed
+    return {
+        dispose: () => {
+            dropZoneElement.removeEventListener('dragenter', onDragHover);
+            dropZoneElement.removeEventListener('dragover', onDragHover);
+            dropZoneElement.removeEventListener('dragleave', onDragLeave);
+            dropZoneElement.removeEventListener("drop", onDrop);
+            dropZoneElement.removeEventListener('paste', handler);
+        }
+    }
+}
---
+++ blazor-features/DragDropPasteFileUpload/Tests/Tests/DragDropPasteFileUploadTest.cs
@@ -0,0 +1,58 @@
+﻿using Bunit;
+using DragDropPasteFileUpload.Pages;
+using Microsoft.AspNetCore.Components;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests;
+public class DragDropPasteFileUploadTest : TestContext
+{
+    [Fact]
+    public void GivenAValidContext_WhenComponentIsRenderedForFirstTime_ThenImportsDropZoneJavaScriptFile()
+    {
+        // Arrange
+        const string jsFileName = "./js/dropZone.js";
+        JSInterop.SetupModule(jsFileName);
+        JSInterop.Mode = JSRuntimeMode.Loose;
+
+        // Act
+        RenderComponent<FileUpload>();
+
+        // Assert
+        var verifyInvocation = JSInterop.VerifyInvoke("import");
+        Assert.Equal(verifyInvocation.Arguments[0], jsFileName);
+    }
+
+    [Fact]
+    public void GivenAValidContext_WhenComponentIsRenderedForFirstTime_ThenInvokesInitializeFileDropZoneFunction()
+    {
+        // Arrange
+        const string jsFunctionName = "initializeFileDropZone";
+        JSInterop.SetupVoid(jsFunctionName, It.IsAny<ElementReference[]>());
+        JSInterop.Mode = JSRuntimeMode.Loose;
+
+        // Act
+        RenderComponent<FileUpload>();
+
+        // Assert
+        var verifyInvocation = JSInterop.VerifyInvoke(jsFunctionName);
+        Assert.IsType<ElementReference>(verifyInvocation.Arguments[0]);
+        Assert.IsType<ElementReference>(verifyInvocation.Arguments[1]);
+    }
+
+    [Fact]
+    public async Task GivenAValidContext_WhenComponentIsDisposed_ThenInvokesDisposeFunction()
+    {
+        // Arrange
+        const string jsDisposeFunction = "dispose";
+        JSInterop.Mode = JSRuntimeMode.Loose;
+
+        // Act
+        var cut = RenderComponent<FileUpload>();
+        await cut.Instance.DisposeAsync();
+
+        // Assert
+        JSInterop.VerifyInvoke(jsDisposeFunction);
+    }
+}
