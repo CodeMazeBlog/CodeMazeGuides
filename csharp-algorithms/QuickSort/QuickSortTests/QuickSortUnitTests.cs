c6cd61ab4076493268cf4bcc69ca75314671a498
Martin Chege <martinchege324@gmail.com>
2022-03-29 17:29:52 +0300

    Naming changes made to the tests and added a sample test to test stackoverflow exception

--- csharp-algorithms/QuickSort/QuickSortTests/QuickSortUnitTests.cs
+++ csharp-algorithms/QuickSort/QuickSortTests/QuickSortUnitTests.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using QuickSort;
 using System;
-using System.Diagnostics;
 
 namespace QuickSortTests
 {
@@ -9,7 +8,7 @@ namespace QuickSortTests
     public class QuickSortUnitTests
     {
         [TestMethod]
-        public void GivenUnsortedArray_ThenReturnSortedArray()
+        public void GivenUnsortedArray_WhenArrayIsNotEmpty_ThenReturnSortedArray()
         {
             var array = new int[] { 73, 57, 49, 99, 133, 20, 1 };
             var expected = new int[] { 1, 20, 49, 57, 73, 99, 133 };
@@ -22,7 +21,7 @@ namespace QuickSortTests
         }
 
         [TestMethod]
-        public void GivenUnsortedArray_CheckInstanceType()
+        public void GivenUnsortedArray_WhenArrayIsNotEmpty_ThenCheckInstanceType()
         {
             var sortFunction = new QuickSortMethods();
             var array = QuickSortMethods.CreateRandomArray(200);
@@ -33,7 +32,7 @@ namespace QuickSortTests
         }
 
         [TestMethod]
-        public void GivenUnsortedArray_CheckSortedNotNull()
+        public void GivenUnsortedArray_WhenArrayIsSorted_ThenResultingArrayIsNotNull()
         {
             var sortFunction = new QuickSortMethods();
             var array = QuickSortMethods.CreateRandomArray(200);
@@ -41,6 +40,18 @@ namespace QuickSortTests
             var sortedArray = sortFunction.SortArray(array, 0, array.Length - 1, string.Empty);
 
             Assert.IsNotNull(sortedArray);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(StackOverflowException))]
+        public void GivenLargeSortedArray_WhenArrayIsSorted_ThenThrowsStackOverflowException()
+        {
+            var sortFunction = new QuickSortMethods();
+            var array = QuickSortMethods.CreateSortedArray(20000000);
+
+            var sortedArray = sortFunction.SortArray(array, 0, array.Length - 1, string.Empty);
+           
+            Assert.ThrowsException<StackOverflowException>(() => sortFunction.SortArray(array, 0, array.Length - 1, string.Empty));
         }
     }
 }
