601d012cdb82ff605a02b7be505cbbcbac4e1335
tarekmansour <tarekmansour.net@gmail.com>
2022-04-01 23:56:04 +0200

    update tests

---
+++ dotnet-ef/DotnetEfNotFoundError/CodeFirstMigration.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace CodeFirstMigration.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestSQLiteOk()
+        {
+            //arrange
+            using var context = new CustomerContext();
+
+            //act & assert
+
+            context.Database.EnsureCreated();
+        }
+    }
+}
