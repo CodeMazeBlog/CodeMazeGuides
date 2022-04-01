a5e62145a406ca76606b366b7ca1276d71b58bf9
tarekmansour <tarekmansour.net@gmail.com>
2022-04-01 23:37:57 +0200

    add neded code and run inital migration

---
+++ dotnet-ef/DotnetEfNotFoundError/CodeFirstMigration/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeFirstMigration
+{
+    //model
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public string? Address { get; set; }
+        public string? CardNumber { get; set; }
+    }
+}
---
+++ dotnet-ef/DotnetEfNotFoundError/CodeFirstMigration/CustomerContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstMigration
+{
+    //CustomerContext class to automatically initialize DbContext instance
+    public class CustomerContext: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        //configure database
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=DotnetEfNotFoundErrorDb;Integrated Security=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasKey(x => x.Id);
+        }
+    }
+}
