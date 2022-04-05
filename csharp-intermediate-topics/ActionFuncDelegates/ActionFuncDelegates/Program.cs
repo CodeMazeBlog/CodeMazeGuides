bee26be539c1176e1448c11671acb2310dbe295c
Shayar Shrestha <shayarshrestha23@gmail.com>
2022-04-06 00:41:56 +0545

    ActionFuncChanges

---
+++ csharp-intermediate-topics/ActionFuncDelegates/ActionFuncDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ActionFuncDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action learningAction = new Action(Learning);
+            learningAction();
+
+            Func<string, string, string> concateFunc = ConcateString;
+            string concatedText = concateFunc("Hi, I'm learning c#", "from code-maze.com");
+            Console.WriteLine(concatedText);
+        }
+        public static void Learning()
+        {
+            Console.WriteLine("Hi, I am learning Action Delegates");
+        }
+        public static string ConcateString(string input1, string input2)
+        {
+            return input1 + " " + input2;
+        }
+    }
+}
