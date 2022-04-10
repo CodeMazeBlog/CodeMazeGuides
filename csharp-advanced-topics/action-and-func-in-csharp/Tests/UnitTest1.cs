8c0de1c318f24048c2ede67ddc05cc794312e89d
Jose Frank Geraldino <josefrank@gmail.com>
2022-04-10 16:40:32 -0400

    xunit?

--- csharp-advanced-topics/action-and-func-in-csharp/Tests/UnitTest1.cs
+++ csharp-advanced-topics/action-and-func-in-csharp/Tests/UnitTest1.cs
@@ -1,51 +1,51 @@
+
 using ActionAndFuncInCsharp;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Tests
 {
-    [TestClass]
     public class PersonGreeterTest
     {
-        [TestMethod]
+        [Fact]
         public void WhenGreetingPersonWithBasicDelegate_ThenPersonAddedToList()
         {
             var person = new Person("John", "Gates");
             var personGreeter = new PersonGreeter();
             personGreeter.SayHelloWithBasicDelegate(person);
-            Assert.AreEqual(personGreeter.greetedPeople[0], person);
+            Assert.Equal(personGreeter.greetedPeople[0], person);
 
         }
 
-        [TestMethod]
+        [Fact]
         public void WhenGreetingPersonWithAction_ThenPersonAddedToList()
         {
             var person = new Person("John", "Jobs");
             var personGreeter = new PersonGreeter();
             personGreeter.SayHelloWithAction(person);
-            Assert.AreEqual(personGreeter.greetedPeople[0], person);
+            Assert.Equal(personGreeter.greetedPeople[0], person);
 
         }
 
-        [TestMethod]
+        [Fact]
         public void WhenGreetingPersonWithShorterAction_ThenPersonAddedToList()
         {
             var person = new Person("Bill", "Gates");
             var personGreeter = new PersonGreeter();
             personGreeter.SayHelloWithShorterAction(person);
-            Assert.AreEqual(personGreeter.greetedPeople[0], person);
+            Assert.Equal(personGreeter.greetedPeople[0], person);
 
         }
 
-        [TestMethod]
+        [Fact]
         public void WhenGreetingPersonWithFunc_ThenPersonAddedToListAndReturned()
         {
             var person = new Person("John", "Jobs");
             var personGreeter = new PersonGreeter();
             var result = personGreeter.SayHelloWithFunc(person);
-            Assert.AreEqual(personGreeter.greetedPeople[0].FirstName, person.FirstName);
-            Assert.AreEqual(personGreeter.greetedPeople[0].LastName, person.LastName);
-            Assert.AreEqual(person.FirstName, result.FirstName);
-            Assert.AreEqual(person.LastName, result.LastName);
+            Assert.Equal(personGreeter.greetedPeople[0].FirstName, person.FirstName);
+            Assert.Equal(personGreeter.greetedPeople[0].LastName, person.LastName);
+            Assert.Equal(person.FirstName, result.FirstName);
+            Assert.Equal(person.LastName, result.LastName);
         }
     }
 }
