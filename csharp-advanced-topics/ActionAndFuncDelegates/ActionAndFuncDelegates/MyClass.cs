870ecd0bb2dd013c0f738dd92215aaf984ce05cc
Zulqornain Gambari <zulqornaingambari@gmail.com>
2022-04-14 23:20:01 +0200

    implemented corrections

---
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegates
+{
+    public class MyClass
+    {
+        public string MethodOne(string message)
+        {
+            return $"The message is: {message}";
+        }
+
+        public string MethodWithDelegateParam(MyFirstDelegate delParam)
+        {
+            var result = delParam("Hi There");
+            return result;
+        }
+
+        public string MethodTwo(string firstName, string lastName) 
+        {
+            var fullName = $"{firstName} {lastName}";
+            return fullName;
+        }
+
+        public static string name;
+        public void MethodThree(string firstName, string lastName)
+        {
+            name = $"{firstName} {lastName}";
+        }
+    }
+}
--- csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegatesTest/Test.cs
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegatesTest/Test.cs
@@ -7,86 +7,94 @@ namespace ActionAndFuncDelegatesTest
     public class Test
     {
         MyClass sut;
-        static string hh = "musa";
-
         public Test()
         {
             sut = new MyClass();
         }
 
         [Fact]
-        public void WhenDelegateIsAssigned_DelegateExecutesReferencedMethod()
+        public void WhenDelegateIsAssigned_ThenDelegateExecutesReferencedMethod()
         {
             MyFirstDelegate del = sut.MethodOne;
             var result = sut.MethodOne("Hey");
+
             Assert.Equal(del("Hey"), result);
         }
 
         [Fact]
-        public void WhenDelegateIsPassedAsParameter_DelegateExecutesReferencedMethod()
+        public void WhenDelegateIsPassedAsParameter_ThenDelegateExecutesReferencedMethod()
         {
             MyFirstDelegate del = sut.MethodOne;
             var result = sut.MethodWithDelegateParam(del);
+
             Assert.Equal(del("Hi There"), result);
         }
 
         [Fact]
-        public void WhenFuncDelegateIsAssigned_DelegateExecutesReferencedMethod()
+        public void WhenFuncDelegateIsAssigned_ThenDelegateExecutesReferencedMethod()
         {
-            Func<string, string, string> FuncDelegate = sut.MethodeTwo;
-            var result = sut.MethodeTwo("james", "jones");
+            Func<string, string, string> FuncDelegate = sut.MethodTwo;
+            var result = sut.MethodTwo("james", "jones");
+
             Assert.Equal(FuncDelegate("james", "jones"), result);
         }
 
         [Fact]
-        public void WhenFuncDelegateIsAssignedToAnonymousMethod_DelegateExecutesTheAnonymousdMethod()
+        public void WhenFuncDelegateIsAssignedToAnonymousMethod_ThenDelegateExecutesTheAnonymousdMethod()
         {
             Func<string, string, string> FuncDelegate = delegate (string firstName, string lastName)
             {
-                return firstName + " " + lastName;
+                return $"{firstName} {lastName}";
             };
             var result = FuncDelegate("One", "Two");
+
             Assert.Equal("One Two", result);
         }
 
         [Fact]
-        public void WhenFuncDelegateIsAssignedToLambdaExpression_DelegateExecutesTheLambdaExpression()
+        public void WhenFuncDelegateIsAssignedToLambdaExpression_ThenDelegateExecutesTheLambdaExpression()
         {
             Func<string, string, string> FuncDelegate = (string firstName, string lastName) =>
             {
-                return firstName + " " + lastName;
+                return $"{firstName} {lastName}";
             };
             var result = FuncDelegate("One", "Two");
+
             Assert.Equal("One Two", result);
         }
 
 
         [Fact]
-        public void WhenActionDelegateIsAssigned_DelegateExecutesReferencedMethod()
+        public void WhenActionDelegateIsAssigned_ThenDelegateExecutesReferencedMethod()
         {
             Action<string, string> actionDelegate = sut.MethodThree;
             actionDelegate("john", "doe");
+
             Assert.Equal("john doe", MyClass.name);
         }
 
         [Fact]
-        public void WhenActionDelegateIsAssignedToAnonymousMethod_DelegateExecutesTheAnonymousdMethod()
+        public void WhenActionDelegateIsAssignedToAnonymousMethod_ThenDelegateExecutesTheAnonymousdMethod()
         {
-            Action<string, string> actionDelegate = delegate (string name, string lastName)
+            Action<string, string> actionDelegate = delegate (string firstName, string lastName)
             {
-                MyClass.name = name + " " + lastName;
+                MyClass.name = $"{firstName} {lastName}";
             };
-            actionDelegate("James Drinkwater", MyClass.name);
+            actionDelegate("James", "Drinkwater");
+
+            Assert.Equal("James Drinkwater", MyClass.name);
         }
 
         [Fact]
-        public void WhenActionDelegateIsAssignedToLambdaExpression_DelegateExecutesTheLambdaExpression()
+        public void WhenActionDelegateIsAssignedToLambdaExpression_ThenDelegateExecutesTheLambdaExpression()
         {
-            Action<string, string> actionDelegate = (string name, string lastName) =>
+            Action<string, string> actionDelegate = (string firstName, string lastName) =>
             {
-                MyClass.name = name + " " + lastName;
+                MyClass.name = $"{firstName} {lastName}";
             };
-            actionDelegate("James Drinkwater", MyClass.name);
+            actionDelegate("Jamees", "Drinkwater");
+
+            Assert.Equal("Jamees Drinkwater", MyClass.name);
         }
     }
 }
