ac43e0367d8b435c2563f3c2d6cba68a8a0d213b
RPM1984 <rpmir1@gmail.com>
2022-04-14 16:56:16 +1000

    CM-270: Introduction to Dotnet Format

---
+++ dotnet-cli/DotnetFormat/DotnetFormatExample/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace DotnetFormatExample
+{
+    public class Class1
+    {
+        public int Foo()
+        {
+            int x = 1;
+
+            if (1 == 1)
+            {
+                x = 2;
+            }
+            else
+            {
+                x = 3;
+            }
+
+            return x;
+        }
+
+    }
+}
