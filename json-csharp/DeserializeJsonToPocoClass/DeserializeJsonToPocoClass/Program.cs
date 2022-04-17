2b22c6589ff58ed0ae7724f5339b4edb79da7560
Alvaro Montoya <alvaro.montoya@priorauthnow.com>
2022-04-17 13:40:14 -0400

    Added Deserialization example program and tests

---
+++ json-csharp/DeserializeJsonToPocoClass/DeserializeJsonToPocoClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using DeserializeJsonToPocoClass.POCO;
+using System.Text.Json;
+using System.IO;
+using NewtonsoftJson = Newtonsoft.Json;
+
+namespace DeserializeJsonToPocoClass
+{
+    public class Program
+    {
+        public static Car DeserializedJsonCar { get; set; } = default!;
+        public static Car NewtonsoftDeserializedJsonCar { get; set; } = default!;
+
+        public static void Main()
+        {
+            // Read JSON file. This could also be a DB or API response.
+            var jsonStr = File.ReadAllText("JSON/HondaCivic.json");
+
+            // Deserialize to get an object of type Car
+            DeserializedJsonCar = JsonSerializer.Deserialize<Car>(jsonStr)!;
+
+            // Deserialize to get an object of type Car
+            NewtonsoftDeserializedJsonCar = NewtonsoftJson.JsonConvert.DeserializeObject<Car>(jsonStr)!;
+        }
+    }
+}
