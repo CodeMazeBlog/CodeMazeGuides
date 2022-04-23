1553581b62cc7815981e08cd949e47ea061e9eca
Alvaro Montoya <alvaro.montoya@priorauthnow.com>
2022-04-23 17:41:22 -0400

    WIP: wrote deep copy algorithm

---
+++ files-csharp/CopyAllContentInDirectory/CopyAllContentInDirectory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Abstractions;
+
+namespace CopyAllContentInDirectory
+{
+    public class Program
+    {
+        public static string sourceDir { get; set; } = "";
+        public static string destDir { get; set; } = "";
+
+        public static void Main(IFileSystem fs)
+        {
+            foreach (string dir in fs.Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories))
+            {
+                string dirToCreate = dir.Replace(sourceDir, destDir);
+                fs.Directory.CreateDirectory(dirToCreate);
+            }
+
+            foreach (string newPath in fs.Directory.GetFiles(sourceDir, "*.*", SearchOption.AllDirectories))
+            {
+                fs.File.Copy(newPath, newPath.Replace(sourceDir, destDir), true);
+            }
+        }
+    }
+}
