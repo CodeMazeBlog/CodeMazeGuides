4de0aaa4fa5dea898e0d7045e1bcde0910714177
Satya <satyap48@gmail.com>
2022-04-24 20:12:02 +0530

    Using Charts With Angular and ASP.NET Core Web API

---
+++ aspnetcore-webapi/UsingGoogleCharts/API/API/Data/Seed.cs
@@ -0,0 +1,28 @@
+﻿using API.Model;
+using System.Text.Json;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+
+        public Seed(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedData()
+        {
+            var languageData = File.ReadAllText("Data/LanguageSeedData.json");
+            var languages = JsonSerializer.Deserialize<List<Language>>(languageData);
+
+            if (!_context.Languages.Any())
+            {
+                _context.Languages.AddRange(languages);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
