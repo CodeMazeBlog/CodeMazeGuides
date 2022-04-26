ca3c714807ef745b6e3573472c5bb82f0afe9b82
Chayan Adhikari <adhikari.chayan@gmail.com>
2022-04-26 12:54:56 +0530

    CM-497: Long-Running Tasks in Monolith ASP.NET Core Application

---
+++ aspnetcore-webapi/LongRunningTaskDemo/Common/Models/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Models
+{
+    public enum OrderStatus
+    {
+        Inprogress,
+        Failure,
+        Successful
+    }
+}
---
+++ aspnetcore-webapi/LongRunningTaskDemo/Monolith.ShoppingCartApi/Services/ReceiptGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Common.Models;
+using Monolith.ShoppingCartApi.Services.Interfaces;
+
+namespace Monolith.ShoppingCartApi.Services
+{
+    public class ReceiptGenerator : IReceiptGenerator
+    {
+        private readonly ILogger _logger;
+        public ReceiptGenerator(ILogger<ReceiptGenerator> logger)
+        {
+            _logger = logger;
+        }
+        public async Task GenerateAsync(Guid customerId, CheckoutResponse response, int amount)
+        {
+            await Task.Delay(50);
+            _logger.LogInformation("Receipt Generated and Order Status persisted in DB.");
+
+            await Task.Delay(50);
+            _logger.LogInformation("Email is sent with Order Status and receipt.");
+        }
+
+        public async Task ProcessFailuresAsync(Guid customerId, CheckoutResponse response)
+        {
+            await Task.Delay(50);
+            _logger.LogInformation("Failure Order Status and reason persisted in DB.");
+
+            await Task.Delay(50);
+            _logger.LogInformation("Email is sent with Order Status and failure reason.");
+        }
+    }
+}
