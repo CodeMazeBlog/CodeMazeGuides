00804a2bcb6a4a11b2760db0a0d894dcbd9281ed
Satya Prakash <51241619+lasaetarubia@users.noreply.github.com>
2022-04-28 15:56:07 +0530

    CM-504 : Introduction to Google Charts With Angular and ASP.NET Core Web API (#444)

---
+++ aspnetcore-webapi/UsingGoogleCharts/UsingGoogleCharts/Data/LanguageRepository.cs
@@ -0,0 +1,24 @@
+﻿using API.Model;
+
+namespace API.Data
+{
+    public class LanguageRepository
+    {
+        private readonly DataContext _context;
+
+        public LanguageRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Language> GetLanguages()
+        {
+            return _context.Languages.ToList();
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
