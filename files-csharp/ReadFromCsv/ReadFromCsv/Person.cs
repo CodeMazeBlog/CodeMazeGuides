18525867b5240f543f775686745b1661373444d6
Hasan Kurt <hasan1989@gmail.com>
2022-05-01 17:58:22 +0200

    Reading from CSV file using CsvHelper Include unittest for reading CSV file

---
+++ files-csharp/ReadFromCsv/ReadFromCsv/Person.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromCsv
+{
+    public class Person
+    {
+        [Index(0)]
+        public int Id { get; set; }
+        [Index(1)]
+        public string Name { get; set; }
+        [Index(2)]
+        public bool IsLiving { get; set; }
+        [Index(3)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ files-csharp/ReadFromCsv/ReadFromCsv/ReadMethods.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromCsv
+{
+    public static class ReadMethods
+    {
+        public static void ReadPersons()
+        {
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ",",
+                Comment = '#',
+                HasHeaderRecord = false
+            };
+            using (var reader = new StreamReader("filePersons.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var persons = csv.GetRecords<Person>();
+            }
+
+        }
+    }
+}
