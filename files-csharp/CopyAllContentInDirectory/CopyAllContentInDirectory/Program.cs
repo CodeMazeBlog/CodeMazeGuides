4d83320d21ca943a4e6267999803837c9adbc6c3
Alvaro Montoya <alvaro.montoya@priorauthnow.com>
2022-05-02 14:35:29 -0400

    PR comment changes; added unit tests

--- files-csharp/CopyAllContentInDirectory/CopyAllContentInDirectory/Program.cs
+++ files-csharp/CopyAllContentInDirectory/CopyAllContentInDirectory/Program.cs
@@ -4,21 +4,21 @@ namespace CopyAllContentInDirectory
 {
     public static class Program
     {
-        public static string sourceDir { get; set; } = "";
-        public static string destinationDir { get; set; } = "";
-        public static IFileSystem fileSystem { get; set; } = new FileSystem(); // for testing purposes
+        public static string SourceDir { get; set; } = "";
+        public static string DestinationDir { get; set; } = "";
+        public static IFileSystem FileSystem { get; set; } = new FileSystem(); // for testing purposes
 
         public static void Main()
         {
-            foreach (string dir in fileSystem.Directory.GetDirectories(sourceDir, "*", SearchOption.AllDirectories))
+            foreach (string dir in FileSystem.Directory.GetDirectories(SourceDir, "*", SearchOption.AllDirectories))
             {
-                string dirToCreate = dir.Replace(sourceDir, destinationDir);
-                fileSystem.Directory.CreateDirectory(dirToCreate);
+                string dirToCreate = dir.Replace(SourceDir, DestinationDir);
+                FileSystem.Directory.CreateDirectory(dirToCreate);
             }
 
-            foreach (string newPath in fileSystem.Directory.GetFiles(sourceDir, "*.*", SearchOption.AllDirectories))
+            foreach (string newPath in FileSystem.Directory.GetFiles(SourceDir, "*.*", SearchOption.AllDirectories))
             {
-                fileSystem.File.Copy(newPath, newPath.Replace(sourceDir, destinationDir), true);
+                FileSystem.File.Copy(newPath, newPath.Replace(SourceDir, DestinationDir), true);
             }
         }
     }
