306b102cc330c50a0a50eaca32128b198e4c46c7
Bartosz Jarmuż <bartosz.jarmuz@gmail.com>
2022-05-08 13:48:29 +0200

    CM-519 - added initial code

---
+++ visual-studio/CentralPackageManagementDemo/CentralPackageManagement.Tests/TestCentrallyControlledPackagesVersion.cs
@@ -0,0 +1,21 @@
+using CentrallyManagedProject;
+using NUnit.Framework;
+
+namespace CentralPackageManagement.Tests
+{
+    [TestFixture]
+    public class TestCentrallyControlledPackagesVersion
+    {
+        [Test]
+        public void WhenCheckingCentrallyManagedPackage_ThenCentrallySetVersionIsReturned()
+        {
+            Assert.AreEqual("13.0.1.25517", CentrallyManagedDependencyPresenter.GetJsonVersion());
+        }
+
+        [Test]
+        public void WhenCheckingManagedPackage_ThenOverridenVersionIsReturned()
+        {
+            Assert.AreEqual("2.11.0.0", CentrallyManagedDependencyPresenter.GetSerilogVersion());
+        }
+    }
+}
