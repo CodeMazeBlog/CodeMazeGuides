f8166124f55e4845721c7daccb2310aae4f69209
filipibc <filipibc@gmail.com>
2022-05-11 10:07:30 -0300

    - Updating the LaunchSettings - Updating the Program.cs class.

--- aspnetcore-webapi/ApiGatewayWithOcelot/OcelotApiGateway/Program.cs
+++ aspnetcore-webapi/ApiGatewayWithOcelot/OcelotApiGateway/Program.cs
@@ -2,9 +2,6 @@ using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-IConfiguration configuration = new ConfigurationBuilder()
-                            .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-                            .Build();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,7 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot(configuration)
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration)
     .AddCacheManager(x =>
     {
         x.WithDictionaryHandle();
