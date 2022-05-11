52b1958194f2a95258b3394223b32b29e2b80a1a
Bazen Teklehaymanot <bazenteklehaymanot29@gmail.com>
2022-05-11 07:24:22 +0300

    Function codes separated

--- csharp-intermediate-topics/ActionAndFunc/ActionAndFuncExample/Program.cs
+++ csharp-intermediate-topics/ActionAndFunc/ActionAndFuncExample/Program.cs
@@ -15,9 +15,11 @@ public class Program
     {
         Console.WriteLine("\n\nDelegate example output:");
         var message = "delegate example message";
+
         Log logger = Logger.LogToConsole;
         Log logger1 = delegate (string message) { Console.WriteLine($"ANONYMOUS:\t{message}"); };
         Log logger2 = message => Console.WriteLine($"LAMBDA :\t{message}");
+
         logger(message);
         logger1(message);
         logger2(message);
@@ -27,9 +29,11 @@ public class Program
     {
         Console.WriteLine("\n\nAction example output:");
         var message = "action example message";
+
         Action<string> logger = Logger.LogToConsole;
         Action<string> logger1 = delegate (string message) { Console.WriteLine($"ANONYMOUS:\t{message}"); };
         Action<string> logger2 = message => Console.WriteLine($"LAMBDA :\t{message}");
+
         logger(message);
         logger1(message);
         logger2(message);
@@ -39,13 +43,15 @@ public class Program
     {
         Console.WriteLine("\n\nFunc example output:");
         int num1 = 10, num2 = 2;
-        Action<string> logger = message => Console.WriteLine($"{DateTime.Now} :\t{message}");
+        Action<string> logger = Logger.LogToConsole;
+        
         Func<int, int, int> calculator = Calculator.Add;
         var result = calculator(num1, num2);
-        logger($"{num1} + {num2} = {result}");
+        logger($"ADD: {num1} + {num2} = {result}");
+        
         calculator = Calculator.Multiply;
         var result2 = calculator(num1, num2);
-        logger($"{num1} + {num2} = {result2}");
+        logger($"MULTIPLY: {num1} + {num2} = {result2}");
     }
 
 }
