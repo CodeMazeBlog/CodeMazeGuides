21913fab04662a3f98af9bc17e96eccc38592152
Code Maze <contact@code-maze.com>
2022-05-12 07:47:08 +0200

    updated models

--- json-csharp/JsonToXmlBackAndForth/JsonToXmlBackAndForth/Native/SquidGame.cs
+++ json-csharp/JsonToXmlBackAndForth/JsonToXmlBackAndForth/Native/SquidGame.cs
@@ -2,13 +2,11 @@
 
 namespace JsonToXmlBackAndForth.Native;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 public class SquidGame
 {
-    public string Genre { get; set; }
-    public Rating Rating { get; set; }
+    public string? Genre { get; set; }
+    public Rating? Rating { get; set; }
     [XmlElement]
-    public string[] Stars { get; set; }
-    public object Budget { get; set; }
+    public string[]? Stars { get; set; }
+    public object? Budget { get; set; }
 }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
