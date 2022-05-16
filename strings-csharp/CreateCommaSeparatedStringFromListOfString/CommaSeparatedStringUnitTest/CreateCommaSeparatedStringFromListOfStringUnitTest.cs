274e8a22abbddb900d155defb8b1f4c290a86862
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2022-05-16 10:50:29 +0100

    changed local var to readonly

--- strings-csharp/CreateCommaSeparatedStringFromListOfString/CommaSeparatedStringUnitTest/CreateCommaSeparatedStringFromListOfStringUnitTest.cs
+++ strings-csharp/CreateCommaSeparatedStringFromListOfString/CommaSeparatedStringUnitTest/CreateCommaSeparatedStringFromListOfStringUnitTest.cs
@@ -6,7 +6,7 @@ namespace CommaSeparatedStringUnitTest
 {
     public class CreateCommaSeparatedStringFromListOfStringUnitTest
     {
-        List<string> fruitList = new List<string>()
+        private readonly List<string> _fruitList = new List<string>()
         {
             "apple",
             "orange",
@@ -18,19 +18,19 @@ namespace CommaSeparatedStringUnitTest
         [Fact]
         public void GivenStringJoin_WhenListOfStringIsPassed_ThenReturnCommaSeparatedString()
         {
-            Assert.Equal("apple,orange,pineapple,grape,coconut", string.Join(",", fruitList));
+            Assert.Equal("apple,orange,pineapple,grape,coconut", string.Join(",", _fruitList));
         }
 
         [Fact]
         public void GivenStringJoin_WhenListOfStringIsPassedAndFiltered_ThenReturnCommaSeparatedStringOfFilteredOutcome()
         {
-            Assert.Equal("apple,pineapple", string.Join(",", fruitList.Where(fruit => fruit.Contains("apple"))));
+            Assert.Equal("apple,pineapple", string.Join(",", _fruitList.Where(fruit => fruit.Contains("apple"))));
         }
 
         [Fact]
         public void GivenStringJoin_WhenArrayOfStringIsPassedWithIndexSpecified_ThenReturnCommaSeparatedStringOfIndicatedIndexes()
         {
-            Assert.Equal("pineapple,grape,coconut", string.Join(",", fruitList.ToArray(), 2, 3));
+            Assert.Equal("pineapple,grape,coconut", string.Join(",", _fruitList.ToArray(), 2, 3));
         }
     }
 }
