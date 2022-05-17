4b5c293ed29fe93774cf64235ec1b3ef2a8dc601
Ahsan <ullahahsan1983@gmail.com>
2022-05-17 19:11:59 +0600

    Initial work

---
+++ csharp-intermediate-topics/ReflectionInCSharp/ReflectionInCSharp/MotionSensor.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace ReflectionInCSharp;
+
+[Description("Detects movements in the vicinity")]
+public class MotionSensor : IMotionSensor
+{
+    private int _detections;
+
+    public string? FocalPoint;
+
+    public MotionSensor() : this("*") { }
+
+    public MotionSensor(string focalPoint) => FocalPoint = focalPoint;
+
+    public event EventHandler<string>? MotionDetected;
+
+    [Description("Turn On/Off")]
+    public bool Enabled { get; set; }
+
+    public string Status => _detections > 0 ? "Red" : "Green";
+
+    public void Observe() => RaiseMotionDetected("*");
+
+    public void Observe(string direction) => RaiseMotionDetected(direction);
+
+    public bool IsCritical(int threshold) => _detections >= threshold;
+
+    private void RaiseMotionDetected(string direction)
+    {
+        _detections++;
+        FocalPoint = direction;
+        MotionDetected?.Invoke(this, direction);
+    }
+}
