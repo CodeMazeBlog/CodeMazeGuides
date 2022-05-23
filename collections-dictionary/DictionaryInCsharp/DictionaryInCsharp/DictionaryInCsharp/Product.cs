7b6425e30ec4728d92cbcb3e295b9396167b9a77
Ceren <cerenaltay.lsb@gmail.com>
2022-05-23 17:36:07 +0300

    CM-85: Dictionary in C#

---
+++ collections-dictionary/DictionaryInCsharp/DictionaryInCsharp/DictionaryInCsharp/Product.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DictionaryInCsharp
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
