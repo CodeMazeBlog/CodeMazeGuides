8d0f1131ba9dbb6c4d1e5f20a2f9d8a1d4c90b2d
Zulqornain Gambari <zulqornaingambari@gmail.com>
2022-05-30 22:53:06 +0200

    updated class

---
+++ csharp-intermediate-topics/MethodOverloadingInCsharp/MethodOverloadingInCsharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodOverloadingInCsharp
+{
+    public class Program
+    {
+        public int AddNumbers(int num1, int num2) 
+        { 
+            return num1 + num2; 
+        } 
+        public int AddNumbers(int num1, int num2, int num3) 
+        { 
+            return num1 + num2 + num3; 
+        }
+        public string Append(int numb) 
+        {
+            return $"Value is: {numb}"; 
+        }
+        public string Append(string numb) 
+        { 
+            return $"Value is: {numb}"; 
+        }
+        public string Order(int numb, string item) 
+        { 
+            return item + numb; 
+        }
+        public string Order(string item, int numb) 
+        { 
+            return item + numb; 
+        }
+        public static void Main(string[] args)
+        {
+            var program = new Program(); 
+            var sum1 = program.AddNumbers(1, 2); 
+            var sum2 = program.AddNumbers(1, 2, 3);
+
+            var obj1 = program.Append(1); 
+            var obj2 = program.Append("one");
+
+            var val1 = program.Order(1, "item"); 
+            var val2 = program.Order("item", 2);
+        }
+    }
+}
---
+++ csharp-intermediate-topics/MethodOverloadingInCsharp/Tests/MethodOverloadingTest.cs
@@ -0,0 +1,56 @@
+﻿using MethodOverloadingInCsharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class MethodOverloadingTest
+    {
+        [Fact]
+        public void WhenOverloadedMethodsWithDifferentParamterCounsAreCalled_ThenTheyShouldRetrunTheCorrectValues() 
+        {
+            // Arrange.
+            var program = new Program();
+
+            // Act.
+            var val1 = program.AddNumbers(3,3);
+            var val2 = program.AddNumbers(1,2,3);
+
+            // Assert.
+            Assert.Equal(val1, val2);
+        }
+
+        [Fact]
+        public void WhenOverloadedMethodsWithDifferentParamterTypeAreCalled_ThenTheyShouldRetrunTheCorrectValues()
+        {
+            // Arrange.
+            var program = new Program();
+
+            // Act.
+            var val1 = program.Append(0);
+            var val2 = program.Append("zero");
+
+            // Assert.
+            Assert.Equal("Value is: 0", val1);
+            Assert.Equal("Value is: zero", val2);
+        }
+
+        [Fact]
+        public void WhenOverloadedMethodsWithDifferentParamterOrderAreCalled_ThenTheyShouldRetrunTheCorrectValues()  
+        {
+            // Arrange.
+            var program = new Program();
+
+            // Act.
+            var val1 = program.Order(0,"item");
+            var val2 = program.Order("item",0);
+
+            // Assert.
+            Assert.Equal(val1, val2);
+        }
+    }
+}
