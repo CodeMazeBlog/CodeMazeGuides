82951b97a5a2451a4ede2bf23306d783578ceda1
sethdtwigg <94552489+sethdtwigg@users.noreply.github.com>
2022-06-02 14:12:03 -0400

    CM-144 Pattern Matching in C#

---
+++ csharp-basic-topics/Patterns/Patterns/Animal.cs
@@ -0,0 +1,31 @@
+﻿namespace Patterns
+{
+    public abstract class Animal
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Age { get; set; }
+        public Animal? AnimalParent { get; set; }
+        public Animal? AnimalChild { get; set; }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat()
+        {
+            Name = "Cat";
+            Description = "furry animal with long tail and claws";
+            Age = new Random().Next(100);
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            Name = "Dog";
+            Description = "furry animal with tail and paws";
+            Age = new Random().Next(100);
+        }
+    }
+}
