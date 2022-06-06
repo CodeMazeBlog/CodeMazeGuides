c2bcdd56c0310e72240ea86825149c86c7128c6f
Alvaro Montoya <alvaro.montoya@knights.ucf.edu>
2022-06-06 09:36:56 -0400

    added parse code with span and string and added benchmark

---
+++ csharp-advanced-topics/SpanInCsharp/SpanExample/SpanExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace SpanExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BenchmarkRunner.Run<StringSpanBenchmark>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class StringSpanBenchmark
+    {
+        string hamletText = File.ReadAllText("./hamletActOne.txt");
+
+        // string func
+        [Benchmark]
+        public void ParseWithString()
+        {
+            string[] hamletlines = hamletText.Split('\n');
+        }
+
+        // span func
+        [Benchmark]
+        public void ParseWithSpan()
+        {
+            var hamletSpan = hamletText.AsSpan();
+
+            int indexPrev = 0;
+            int indexCurrent = 0;
+            foreach (char c in hamletSpan)
+            {
+                if (c == '\n')
+                {
+                    var slice = hamletSpan.Slice(indexPrev + 1, indexCurrent - indexPrev);
+                    indexPrev = indexCurrent;
+                }
+                indexCurrent++;
+            }
+        }
+
+        private string[] getAllWords(string text)
+        {
+            MatchCollection matches = Regex.Matches(text, @"\b[\w']+\b");
+
+            var words = from m in matches.Cast<Match>()
+                        where !string.IsNullOrEmpty(m.Value)
+                        select m.Value;
+
+            return words.ToArray();
+        }
+    }
+}
