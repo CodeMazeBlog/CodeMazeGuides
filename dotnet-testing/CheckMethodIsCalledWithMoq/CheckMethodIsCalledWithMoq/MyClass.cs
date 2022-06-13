8240520d91dfb800426d4ba6f398e98b746599e1
Marco Luzzara <marco.luzzara@hotmail.it>
2022-06-13 22:56:22 +0200

    CM-514: Using Moq to Determine If a Method is Called

---
+++ dotnet-testing/CheckMethodIsCalledWithMoq/CheckMethodIsCalledWithMoq/MyClass.cs
@@ -0,0 +1,44 @@
+﻿namespace CheckMethodIsCalledWithMoq;
+
+public class MyClass
+{
+    private Dependency _dep;
+    private IDependency _idep;
+
+    public MyClass(Dependency dep, IDependency idep)
+    {
+        _dep = dep;
+        _idep = idep;
+    }
+
+    public void CallingPublicMethod() 
+    {
+        PublicMethod();
+    }
+
+    public virtual void PublicMethod() {}
+
+    public void CallingProtectedMethod() 
+    {
+        ProtectedMethod();
+    }
+
+    protected virtual void ProtectedMethod() {}
+
+    public void CallingInternalMethod() 
+    {
+        InternalMethod();
+    }
+
+    virtual internal void InternalMethod() {}
+
+    public void CallingDependencyPublicMethod() 
+    {
+        _dep.DepPublicMethod();
+    }
+
+    public void CallingAbstractDependencyPublicMethod() 
+    {
+        _idep.DepAbstractPublicMethod();
+    }
+}
