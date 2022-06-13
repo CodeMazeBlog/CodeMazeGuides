34cbd6e22815d08424f4125a752b1a00a53754d6
filipibc <filipibc@gmail.com>
2022-06-13 13:10:16 -0300

    Retry basic logic implementation

---
+++ csharp-intermediate-topics/RetryLogic/RetryLogic/Executor.cs
@@ -0,0 +1,45 @@
+﻿using RetryLogic.Exceptions;
+
+namespace RetryLogic
+{
+    public static class Executor
+    {
+        public static void Execute(Action action, int numberOfRetries)
+        {
+            int tries = 0;
+
+            while (tries <= numberOfRetries)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch
+                {
+                    tries++;
+                }
+            }
+
+            throw new RetryException($"Error after {numberOfRetries + 1 } tries");
+        }
+
+        public static TResult? Execute<TResult>(Func<TResult> func, int numberOfRetries)
+        {
+            int i = 0;
+            while (i <= numberOfRetries)
+            {
+                try
+                {
+                    return func();
+                }
+                catch
+                {
+                    i++;
+                }
+            }
+
+            throw new RetryException($"Error after {numberOfRetries + 1 } tries");
+        }
+    }
+}
