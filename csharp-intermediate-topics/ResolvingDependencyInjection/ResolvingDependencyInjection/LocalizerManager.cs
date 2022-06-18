88b21ef122769aec73fb84aff703872da43fe01f
Victor Okafor <victor.fortman@gfinity.net>
2022-06-18 10:40:20 -0700

    Accompanying PR for CM-158: Resolving Dependency Injections

---
+++ csharp-intermediate-topics/ResolvingDependencyInjection/ResolvingDependencyInjection/LocalizerManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace ResolvingDependencyInjection
+{
+    public class LocalizerManager
+    {
+        public static IStringLocalizer? localizer;
+
+        public static void SetLocalizationOptions(MvcOptions options)
+        {
+
+            options.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(
+                () => localizer.GetString("MissingRequestBodyRequiredValue"));
+        }
+    }
+}
