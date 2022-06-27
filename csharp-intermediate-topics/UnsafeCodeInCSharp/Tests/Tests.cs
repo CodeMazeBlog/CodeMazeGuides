5793b1f07719118b0a1f2e1502ab99d14dba7647
Satya <satyap48@gmail.com>
2022-06-27 22:33:36 +0530

    CM-432: Unsafe Code in C# (unsafe keyword)

---
+++ csharp-intermediate-topics/UnsafeCodeInCSharp/Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnsafeCodeInCSharp.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void WhenPointerVariablePassedThenValueTriples()
+        {
+            unsafe
+            {
+                int input = 30;
+                int* ptr = &input;
+
+                Program.GetTriple(ptr);
+
+                Assert.AreEqual(90, input);
+            }
+        }
+    }
+}
---
+++ csharp-intermediate-topics/UnsafeCodeInCSharp/UnsafeCodeInCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace UnsafeCodeInCSharp
+{
+    public class Program
+    {
+        public static unsafe void Main(string[] args)
+        {
+            unsafe
+            {
+                int a = 100;
+                int* ptr = &a;
+
+                Console.WriteLine("The value of variable: {0}", a);
+                Console.WriteLine("The value of variable using pointer: {0}", ptr->ToString());
+                Console.WriteLine("The address of variable : {0}", (int)ptr);
+            }
+
+            int num = 10;
+            int* arg = &num;
+
+            Console.WriteLine("GetTriple Input: {0}", num);
+
+            GetTriple(arg);
+
+            Console.WriteLine("GetTriple Output: {0}", num);
+
+            string text = "Happy";
+
+            fixed (char* ptr = text)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    Console.WriteLine("text[{0}] : {1}", i, *(ptr + i));
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        public static unsafe void GetTriple(int* num)
+        {
+            *num = *num * 3;
+        }
+    }
+}
