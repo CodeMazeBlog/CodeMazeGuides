f7571c23385eae9e2f8009dc207e159a8be2f99c
Soundar Anbalagan <soundaranbu@gmail.com>
2022-07-02 09:29:23 +0530

    CM-539 Added code sample for cookie authentication with ASP.NET Core and Angular

---
+++ authorization-dotnet/CookieAuthenticationWithAngular/CookieAuthenticationWithAngular/ClientApp/src/app/auth/auth-service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpEvent } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    constructor(private http: HttpClient) {
+    }
+
+    public signIn(email: string, password: string) {
+        return this.http.post<Response>('api/signin', {
+            email: email,
+            password: password
+        });
+    }
+
+    public signOut() {
+        return this.http.get('api/signout');
+    }
+
+    public user() {
+        return this.http.get<UserClaim[]>('api/user');
+    }
+
+    public isSignedIn(): Observable<boolean> {
+        return this.user().pipe(
+            map((userClaims) => {
+                let hasClaims = userClaims.length > 0;
+                return !hasClaims ? false : true;
+            }),
+            catchError((error) => {
+                return of(false);
+            }));
+    }
+}
+
+export interface UserClaim {
+    type: string;
+    value: string;
+}
+
+export interface Response {
+    isSuccess: boolean;
+    message: string;
+}
---
+++ authorization-dotnet/CookieAuthenticationWithAngular/CookieAuthenticationWithAngular/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using CookieAuthenticationWithAngular.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CookieAuthenticationWithAngular.Controllers;
+
+[ApiController]
+public class AuthController : Controller
+{
+    private List<User> users = new()
+    {
+        new("user1@test.com", "User 1", "user1"),
+        new("user2@test.com", "User 2", "user2"),
+    };
+
+    [HttpPost("api/signin")]
+    public async Task<Response> SignInAsync(SignInRequest signInRequest)
+    {
+        var user = users.FirstOrDefault(x => x.Email == signInRequest.Email &&
+                                            x.Password == signInRequest.Password);
+        if (user is null)
+        {
+            return new Response(false, "Invalid credentials.");
+        }
+
+        var claims = new List<Claim>
+    {
+        new Claim(type: ClaimTypes.Email, value: signInRequest.Email),
+        new Claim(type: ClaimTypes.Name,value: user.Name)
+    };
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(identity),
+            new AuthenticationProperties
+            {
+                IsPersistent = true,
+                AllowRefresh = true,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+            });
+        return new Response(true, "Signed in successfully");
+    }
+
+    [Authorize]
+    [HttpGet("api/user")]
+    public IActionResult GetUser()
+    {
+        var userClaims = User.Claims.Select(x => new UserClaim(x.Type, x.Value)).ToList();
+        return Ok(userClaims);
+    }
+
+    [HttpGet("api/signout")]
+    [Authorize]
+    public async Task SignOutAsync()
+    {
+        await HttpContext.SignOutAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
