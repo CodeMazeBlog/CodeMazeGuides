557c191f3100ccc70a39197963971178d182b51c
Satya Prakash <51241619+lasaetarubia@users.noreply.github.com>
2022-07-04 12:12:52 +0530

    CM-432: Unsafe Code in C# (unsafe keyword) (#526)
    * CM-432: Unsafe Code in C# (unsafe keyword)
    
    * CM-432: Unsafe Code in C# (unsafe keyword). Modifications according to review comments

---
+++ csharp-intermediate-topics/UnsafeCodeInCSharp/Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnsafeCodeInCSharp.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void WhenPointerVariablePassed_ThenValueTriples()
+        {
+            unsafe
+            {
+                var input = 30;
+                int* inputPtr = &input;
+
+                Program.GetTriple(inputPtr);
+
+                Assert.AreEqual(90, input);
+            }
+        }
+    }
+}
