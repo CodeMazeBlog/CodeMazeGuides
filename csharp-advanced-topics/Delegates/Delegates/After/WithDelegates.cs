6340fa9157b209ef52abce2ae663085b0cd2a2e0
Osama Al-Rashed <Osama Al-Rashed@DESKTOP-8954161>
2022-07-04 11:10:29 +0300

    Add Deleagtes Project For Eval Article

---
+++ csharp-advanced-topics/Delegates/Delegates/After/WithDelegates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.After
+{
+    public class WithDelegates
+    {
+        public static string PrintProcessedText(string text, Action<string> print, Func<string, string> operation)
+        {
+            if (text == null)
+                throw new ArgumentNullException("text");
+            if (print == null)
+                throw new ArgumentNullException("print");
+            if (operation == null)
+                throw new ArgumentNullException("operation");
+
+            text = operation(text);
+            print(text);
+            return text;
+        }
+    }
+}
---
+++ csharp-advanced-topics/Delegates/Delegates/Before/WithoutDelegates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.Before
+{
+    public class WithoutDelegates
+    {
+        public static string PrintInUpperCase(string text)
+        {
+            if (text == null)
+                throw new ArgumentNullException("text");
+            text = text.ToUpper();
+            Console.WriteLine("Print in Upper Case: {0}", text);
+            return text;
+        }
+
+        public static string PrintInLowerCase(string text)
+        {
+            if (text == null)
+                throw new ArgumentNullException("text");
+            text = text.ToLower();
+            Console.WriteLine("Print in Lower Case: {0}", text);
+            return text;
+        }
+
+    }
+}
