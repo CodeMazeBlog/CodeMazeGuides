1f745ce536e9d9543d2e6ce4145d6c48e6a7c77c
Raghuvardhan Karanam <karanamraghuvardhan12@gmail.com>
2022-07-07 15:17:58 +0530

    CM-30: Constructors in C# (#492)

---
+++ csharp-intermediate-topics/ConstructorsInCSharp/CSConstructors/Person.cs
@@ -0,0 +1,42 @@
+﻿public class Person
+{
+    private string _name;
+    private int _age;
+
+    private static int studentCount;
+
+    static Person()
+    {
+        studentCount = -1;
+    }
+
+    public Person()
+    {
+        _name = "testName";
+        _age = 25;
+        Console.WriteLine("Default Constructor Invoked");
+    }
+
+    public Person(string name)
+    {
+        _name = name;
+    }
+
+    public Person(string name, int age): this(name)
+    {
+        _age = age;
+        Console.WriteLine("Constructor are chained and Invoked");
+    }
+
+    public Person(Person student)
+    {
+        _name = student._name;
+        _age = student._age;
+        Console.WriteLine("Copy Constructor Invoked");
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(_name + " " + _age);
+    }
+}
