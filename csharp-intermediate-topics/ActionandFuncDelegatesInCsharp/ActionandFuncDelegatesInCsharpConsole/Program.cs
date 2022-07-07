1eaea1a82c5b6e8eb51a5e06bc20b6abe771ad89
GABRIEL EGHOSA EMMANUEL <43476099+Virifortissimi@users.noreply.github.com>
2022-07-07 22:22:22 +0100

    Task/Created project for Action and Func examples

---
+++ csharp-intermediate-topics/ActionandFuncDelegatesInCsharp/ActionandFuncDelegatesInCsharpConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Assigning methods to Action Delegates
+static void SayHello(string name)
+{
+    Console.WriteLine($"Hello {name}");
+}
+
+Action<string> Greet = SayHello;
+
+Greet("John Doe");
+
+//Assigning anonymous methods to Action Delegates
+Action<int, int> AddNumbers = delegate (int num1, int num2) { Console.WriteLine($"Total of {num1} + {num2} is {num1 + num2}"); };
+
+AddNumbers(20, 32);
+
+
+//Using Action Delegates with Lambda expressions
+Action<string> Greetings = (name) => Console.WriteLine($"Good day, {name}.");
+Greetings("John Doe");
+
+//Assigning methods to Func Delegates
+static int AddNumber(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+Func<int, int, int> Sum = AddNumber;
+
+Console.WriteLine(Sum(5, 8));
+Console.WriteLine(Sum(32, 81));
+
+//Assigning anonymous methods to Func Delegates
+Func<int, bool> OlderThanEighteen = delegate (int age) { return age > 18; };
+
+Console.WriteLine(OlderThanEighteen(19));
+Console.WriteLine(OlderThanEighteen(8));
+
+
+//Using Action Delegates with Lambda expressions
+Func<string, string, string> GetFullName = (firstName, lastName) => $"Your full name is - {firstName} {lastName}";
+Console.WriteLine(GetFullName("John", "Doe"));
+
+double GetSquareRoot(int number)
+{
+    return Math.Sqrt(number);
+}
+
+Func<int, double> GetSquareRootFunc = GetSquareRoot;
+
+double result = GetSquareRootFunc(25);
+
+Console.WriteLine(result);
---
+++ csharp-intermediate-topics/ActionandFuncDelegatesInCsharp/Test/ActionandFuncDelegatesinCsharpUnitTest.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Test
+{
+    [TestClass]
+    public class ActionandFuncDelegatesinCsharpUnitTest
+    {
+        [TestMethod]
+        public void GivenFuncDelegate_WhenCalculatingAge_ThenReturnCorrectValue()
+        {
+            Func<DateTime, int> CalculateAge = (dateOfBirth) =>
+            {
+                int age = 0;
+                age = DateTime.Now.Subtract(dateOfBirth).Days;
+                age = age / 365;
+                return age;
+            };
+
+            Assert.AreEqual(30, CalculateAge(Convert.ToDateTime("5/14/1992")));
+        }
+
+        [TestMethod]
+        public void GivenFuncDelegate_WhenCalculatingSqaureRoot_ThenReturnCorrectValue()
+        {
+            double GetSquareRoot(int number)
+            {
+                return Math.Sqrt(number);
+            }
+
+            Func<int, double> GetSquareRootFunc = GetSquareRoot;
+
+            Assert.AreEqual(6, GetSquareRootFunc(36));
+        }
+
+        [TestMethod]
+        public void GivenActionDelegate_WhenGivenFirstNameAndLastName_ThenGreetsWithFullName()
+        {
+            var currentConsoleOut = Console.Out;
+
+            string text = "Good day, John Doe.\r\n";
+
+            using (var consoleOutput = new ConsoleOutput())
+            {
+                Action<string> Greetings = (name) => Console.WriteLine($"Good day, {name}.");
+
+                Greetings("John Doe");
+
+                Assert.AreEqual(text, consoleOutput.GetOuput());
+            }
+
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+        }
+    }
+
+    public class ConsoleOutput : IDisposable
+    {
+        private readonly StringWriter stringWriter;
+        private readonly TextWriter originalOutput;
+
+        public ConsoleOutput()
+        {
+            stringWriter = new StringWriter();
+            originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+        }
+
+        public string GetOuput()
+        {
+            return stringWriter.ToString();
+        }
+
+        protected virtual void Dispose(Boolean disposing)
+        {
+            if (disposing)
+            {
+                Console.SetOut(originalOutput);
+                stringWriter.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
