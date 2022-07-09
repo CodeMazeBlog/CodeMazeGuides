b432f57dc603d6e882287476cb4b6097417cd849
Diego García <wft2000@gmail.com>
2022-07-09 20:13:06 +0200

    CM-445: How to Turn a C# object into a JSON String in .NET

---
+++ json-csharp/HowToTurnCSharpObjectIntoJson/HowToTurnCSharpObjectIntoJson/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using HowToTurnCSharpObjectIntoJson.Converters;
+using System.Text.Json.Serialization;
+
+namespace HowToTurnCSharpObjectIntoJson.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }
+
+        [JsonConverter(typeof(GeneralDateTimeConverter))]
+        public DateTime BirthDate { get; set; }
+    }
+}
