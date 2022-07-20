9288455743c4305c91154f8b57d7360a03d6c523
sethdtwigg <94552489+sethdtwigg@users.noreply.github.com>
2022-07-20 12:25:33 -0400

    CM-67 How to Implement Lucene.NET

--- dotnet-searching/Lucene.NetBasicExample/Lucene.NetBasicExample/SearchEngine.cs
+++ dotnet-searching/Lucene.NetBasicExample/Lucene.NetBasicExample/SearchEngine.cs
@@ -9,26 +9,16 @@ using Lucene.Net.Util;
 
 namespace Lucene.NetBasicExample
 {
-    public class Program
+    public class SearchEngine
     {
         public static IndexWriter Writer;
-        public static RAMDirectory Directory;
+        public static List<Person> Data;
+        private static RAMDirectory Directory;
+        private static string PersonGuidToBeUpdated;
 
-        public static void Main(string[] args)
+        public static void GetData()
         {
-            Index(GetData());
-            UpdateIndex();
-            var run = true;
-            while (run)
-            {
-                Console.WriteLine($"{Environment.NewLine} Enter Search Criteria or Just Press Enter to End Program:");
-                run = RunSearch();
-            }
-        }
-
-        public static List<Person> GetData()
-        {
-            var persons = new List<Person>()
+            Data = new List<Person>()
             {
                 new Person(Guid.NewGuid().ToString(),"Fred","George","Herb","A tall thin man."),
                 new Person(Guid.NewGuid().ToString(),"Frank","Ed","Stevens","A short fat man."),
@@ -41,14 +31,10 @@ namespace Lucene.NetBasicExample
                 new Person(Guid.NewGuid().ToString(),"Clint","Johnny","Williams","A very tiny boy."),
                 new Person(Guid.NewGuid().ToString(),"Susan","Michele","Brown","A very tiny girl.")
             };
-
-            return persons;
         }
 
-        public static void Index(List<Person> people)
+        public static void Index()
         {
-            Console.WriteLine("Beginning Index");
-
             const LuceneVersion lv = LuceneVersion.LUCENE_48;
             Analyzer a = new StandardAnalyzer(lv);
             Directory = new RAMDirectory();
@@ -58,8 +44,8 @@ namespace Lucene.NetBasicExample
             var guidField = new StringField("GUID", "", Field.Store.YES);
             var fNameField = new TextField("FirstName", "", Field.Store.YES);
             var mNameField = new TextField("MiddleName", "", Field.Store.YES);
-            var lNameField = new TextField("LastName","",Field.Store.YES);
-            var descriptionField = new TextField("Description","",Field.Store.YES);
+            var lNameField = new TextField("LastName", "", Field.Store.YES);
+            var descriptionField = new TextField("Description", "", Field.Store.YES);
 
             var d = new Document()
             {
@@ -70,7 +56,7 @@ namespace Lucene.NetBasicExample
                 descriptionField
             };
 
-            foreach (Person person in people)
+            foreach (Person person in Data)
             {
                 guidField.SetStringValue(person.GUID);
                 fNameField.SetStringValue(person.FirstName);
@@ -80,80 +66,54 @@ namespace Lucene.NetBasicExample
 
                 Writer.AddDocument(d);
             }
-            
+
             Writer.Commit();
-            Console.WriteLine("Completed Index");
         }
 
-        private static void UpdateIndex()
+        public static void AddToIndex()
         {
-            Console.WriteLine("Beginning Index Update");
-
-            var personGuidToBeUpdated = Guid.NewGuid().ToString();
-
-            var guidField = new StringField("GUID", personGuidToBeUpdated, Field.Store.YES);
-            var fNameField = new TextField("FirstName", "AddedFirstName", Field.Store.YES);
-            var mNameField = new TextField("MiddleName", "AddedMiddleName", Field.Store.YES);
-            var lNameField = new TextField("LastName", "AddedLastName", Field.Store.YES);
-            var descriptionField = new TextField("Description", "Added Description", Field.Store.YES);
+            PersonGuidToBeUpdated = Guid.NewGuid().ToString();
 
             var d = new Document()
             {
-                guidField,
-                fNameField,
-                mNameField,
-                lNameField,
-                descriptionField
+                new StringField("GUID", PersonGuidToBeUpdated, Field.Store.YES),
+                new TextField("FirstName", "AddedFirstName", Field.Store.YES),
+                new TextField("MiddleName", "AddedMiddleName", Field.Store.YES),
+                new TextField("LastName", "AddedLastName", Field.Store.YES),
+                new TextField("Description", "Added Description", Field.Store.YES)
             };
-            AddToIndex(d);
 
-            fNameField.SetStringValue("UpdateFirstName");
-            mNameField.SetStringValue("UpdatedMiddleName");
-            lNameField.SetStringValue("UpdatedLastName");
-            descriptionField.SetStringValue("Updated Description");
-            ChangeInIndex(d, personGuidToBeUpdated);
-
-            DeleteFromIndex(personGuidToBeUpdated);
-
-            Console.WriteLine("Completed Index Update");
-        }
-
-        public static void AddToIndex(Document d)
-        {
             Writer.AddDocument(d);
             Writer.Commit();
         }
 
-        public static void ChangeInIndex(Document d, string guid)
+        public static void ChangeInIndex()
         {
-            Writer.UpdateDocument(new Term("GUID", guid), d);
+            var d = new Document()
+            {
+                new StringField("GUID", PersonGuidToBeUpdated, Field.Store.YES),
+                new TextField("FirstName", "UpdateFirstName", Field.Store.YES),
+                new TextField("MiddleName", "UpdatedMiddleName", Field.Store.YES),
+                new TextField("LastName", "UpdatedLastName", Field.Store.YES),
+                new TextField("Description", "Updated Description", Field.Store.YES)
+            };
+
+            Writer.UpdateDocument(new Term("GUID", PersonGuidToBeUpdated), d);
             Writer.Commit();
         }
 
-        public static void DeleteFromIndex(string guid)
+        public static void DeleteFromIndex()
         {
-            Writer.DeleteDocuments(new Term("GUID", guid));
+            Writer.DeleteDocuments(new Term("GUID", PersonGuidToBeUpdated));
             Writer.Commit();
         }
 
-        public static bool RunSearch()
+        public static void Dispose()
         {
-            var input = Console.ReadLine();
-            if (String.IsNullOrWhiteSpace(input)) 
-            {
-                Writer.Dispose();
-                Directory.Dispose();
-                return false; 
-            }
-
-            foreach (var s in Search(input))
-            {
-                Console.WriteLine(s);
-            }
-
-            return true;
+            Writer.Dispose();
+            Directory.Dispose();
         }
-        
+
         public static List<string> Search(string input)
         {
             const LuceneVersion lv = LuceneVersion.LUCENE_48;
@@ -164,7 +124,7 @@ namespace Lucene.NetBasicExample
             string[] fnames = { "GUID", "FirstName", "MiddleName", "LastName", "Age", "Description" };
             var multiFieldQP = new MultiFieldQueryParser(lv, fnames, a);
             Query query = multiFieldQP.Parse(input.Trim());
-            
+
             ScoreDoc[] docs = searcher.Search(query, null, 1000).ScoreDocs;
 
             var results = new List<string>();
@@ -179,9 +139,10 @@ namespace Lucene.NetBasicExample
 
                 results.Add($"{guid} {firstname} {middlename} {lastname} {description}");
             }
-            
+
             dirReader.Dispose();
             return results;
         }
+
     }
 }
