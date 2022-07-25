ae933f3c705628223e69e7ffc3167555547f4c3a
Marco Luzzara <marco.luzzara@hotmail.it>
2022-07-25 12:16:54 +0200

    CM-565: How to Create Classes Dynamically in C#

---
+++ csharp-advanced-topics/DynamicallyCreateClass/DynamicallyCreateClass/WeatherData.cs
@@ -0,0 +1,10 @@
+public class WeatherData {
+    public WeatherData(double temperature, int humidity) 
+    {
+        this.Temperature = temperature;
+        this.Humidity = humidity;
+    }
+
+    public double Temperature { get; init; }
+    public int Humidity { get; init; }
+}
