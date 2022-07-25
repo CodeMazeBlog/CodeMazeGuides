aaf3aad74c33676b3c4c88edc4adc73347daba45
Asiful Nobel <asif.nobel@gmail.com>
2022-07-26 00:30:08 +0600

    Added coding samples

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -0,0 +1,33 @@
+﻿void PrintHelloWorld()
+{
+    Console.WriteLine("Hello World!");
+}
+
+var action1 = new Action(PrintHelloWorld);
+var action2 = PrintHelloWorld;
+var action3 = () => { Console.WriteLine("Hello World!"); };
+
+action1();
+action2();
+action3();
+
+void RunAction(Action action)
+{
+    Console.Write("Greetings! ");
+    action();
+}
+
+RunAction(action3);
+
+Action<string> printMessage
+    = (message) => { Console.WriteLine(message); };
+
+printMessage("Hello Code Maze!");
+
+Func<int> get42 = () => 42;
+
+Console.WriteLine(get42());
+
+Func<int, int> add42 = (x) => x + 42;
+
+Console.WriteLine(add42(5));
