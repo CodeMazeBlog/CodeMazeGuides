a1b99658366850e7373c3aa43ff41236b45a7998
Asiful Nobel <asif.nobel@gmail.com>
2022-07-26 01:14:35 +0600

    Added unit tests

--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -1,33 +1,64 @@
-﻿void PrintHelloWorld()
+﻿DemoClass.RunActionWithInstantiation();
+DemoClass.RunActionWithReference();
+DemoClass.RunActionWithLambdaMethod();
+DemoClass.RunActionInHigherOrderFunction();
+DemoClass.RunActionWithAParameter();
+DemoClass.RunFuncWithoutParameter();
+DemoClass.RunFuncWithAParameter();
+
+public class DemoClass
 {
-    Console.WriteLine("Hello World!");
+    private static void PrintHelloWorld()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    private static void RunAction(Action action)
+    {
+        Console.Write("Greetings! ");
+        action();
+    }
+
+    public static void RunActionWithInstantiation()
+    {
+        var action = new Action(PrintHelloWorld);
+        action();
+    }
+
+    public static void RunActionWithReference()
+    {
+        var action = PrintHelloWorld;
+        action();
+    }
+
+    public static void RunActionWithLambdaMethod()
+    {
+        var action = () => { Console.WriteLine("Hello World!"); };
+        action();
+    }
+
+    public static void RunActionInHigherOrderFunction()
+    {
+        RunAction(PrintHelloWorld);
+    }
+
+    public static void RunActionWithAParameter()
+    {
+        Action<string> printMessage
+            = (message) => { Console.WriteLine(message); };
+
+        printMessage("Hello Code Maze!");
+    }
+
+    public static void RunFuncWithoutParameter()
+    {
+        Func<int> get42 = () => 42;
+        Console.WriteLine(get42());
+    }
+
+    public static void RunFuncWithAParameter()
+    {
+        Func<int, int> add42 = (x) => x + 42;
+        Console.WriteLine(add42(5));
+    }
 }
-
-var action1 = new Action(PrintHelloWorld);
-var action2 = PrintHelloWorld;
-var action3 = () => { Console.WriteLine("Hello World!"); };
-
-action1();
-action2();
-action3();
-
-void RunAction(Action action)
-{
-    Console.Write("Greetings! ");
-    action();
-}
-
-RunAction(action3);
-
-Action<string> printMessage
-    = (message) => { Console.WriteLine(message); };
-
-printMessage("Hello Code Maze!");
-
-Func<int> get42 = () => 42;
-
-Console.WriteLine(get42());
-
-Func<int, int> add42 = (x) => x + 42;
-
-Console.WriteLine(add42(5));
--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharpTests/ActionAndFuncDelegateTests.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharpTests/ActionAndFuncDelegateTests.cs
@@ -1,13 +1,76 @@
+using System;
+using System.IO;
 using Xunit;
 
 namespace ActionAndFuncDelegatesInCsharpTests
 {
-    public class ActionAndFuncDelegateTests
+    public class ActionAndFuncDelegateTests : IDisposable
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly TextWriter _originalConsoleOut;
+        private readonly StringWriter _fakeConsoleOut;
 
+        private readonly string _expectedHelloWorldOutput = $"Hello World!{Environment.NewLine}";
+
+        public ActionAndFuncDelegateTests()
+        {
+            _originalConsoleOut = Console.Out;
+            _fakeConsoleOut = new StringWriter();
+            Console.SetOut(_fakeConsoleOut);
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(_originalConsoleOut);
+            _fakeConsoleOut.Dispose();
+        }
+
+        [Fact]
+        public void WhenActionInstanceIsExecuted_ThenConsoleOutputsHelloWorld()
+        {
+            DemoClass.RunActionWithInstantiation();
+            Assert.Equal(_expectedHelloWorldOutput, _fakeConsoleOut.ToString());
+        }
+
+        [Fact]
+        public void WhenReferenceActionIsExecuted_ThenConsoleOutputsHelloWorld()
+        {
+            DemoClass.RunActionWithReference();
+            Assert.Equal(_expectedHelloWorldOutput, _fakeConsoleOut.ToString());
+        }
+
+        [Fact]
+        public void WhenLambdaActionIsExecuted_ThenConsoleOutputsHelloWorld()
+        {
+            DemoClass.RunActionWithLambdaMethod();
+            Assert.Equal(_expectedHelloWorldOutput, _fakeConsoleOut.ToString());
+        }
+
+        [Fact]
+        public void WhenHigherOrderActionIsExecuted_ThenConsoleOutputsGreetingsHelloWorld()
+        {
+            DemoClass.RunActionInHigherOrderFunction();
+            Assert.Equal($"Greetings! {_expectedHelloWorldOutput}", _fakeConsoleOut.ToString());
+        }
+
+        [Fact]
+        public void WhenActionWithAParameterIsExecuted_ThenConsoleOutputsHelloCodeMaze()
+        {
+            DemoClass.RunActionWithAParameter();
+            Assert.Equal($"Hello Code Maze!{Environment.NewLine}", _fakeConsoleOut.ToString());
+        }
+
+        [Fact]
+        public void WhenFuncWithoutParameterIsExecuted_ThenConsoleOutputs42()
+        {
+            DemoClass.RunFuncWithoutParameter();
+            Assert.Equal($"42{Environment.NewLine}", _fakeConsoleOut.ToString());
+        }
+
+        [Fact]
+        public void WhenFuncWithAParameterIsExecuted_ThenConsoleOutputs47()
+        {
+            DemoClass.RunFuncWithAParameter();
+            Assert.Equal($"47{Environment.NewLine}", _fakeConsoleOut.ToString());
         }
     }
 }
