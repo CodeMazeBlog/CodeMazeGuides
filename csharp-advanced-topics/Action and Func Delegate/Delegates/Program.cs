592e7cf03ba524b741f7d1d9d3cf670cf9272504
Ahmad Sabbagh <engahmad.sabag1998@gmail.com>
2022-08-02 02:05:28 +0300

    Add Delegate Project for Evaluation Article.

---
+++ csharp-advanced-topics/Action and Func Delegate/Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿using Delegates.After;
+using Delegates.Before;
+
+var text = "Code Maze";
+
+Console.WriteLine("Brfore:");
+WithoutDelegates.PrintInUpperCase(text);
+WithoutDelegates.PrintInLowerCase(text);
+
+Console.WriteLine();
+Console.WriteLine("After:");
+
+Func<string, string> operation = x => x.ToUpper();
+Action<string> print = x => Console.WriteLine("Print in Upper Case: {0}", x);
+WithDelegates.PrintProcessedText(text, print, operation);
+
+operation = x => x.ToLower();
+print = x => Console.WriteLine("Print in Lower Case: {0}", x);
+WithDelegates.PrintProcessedText(text, print, operation);
---
+++ csharp-advanced-topics/Action and Func Delegate/Test/DelegatesUnitTest.cs
@@ -0,0 +1,74 @@
+using Delegates.After;
+using Delegates.Before;
+using Xunit;
+
+namespace Test
+{
+    public class DelegatesUnitTest
+    {
+        [Fact]
+        public void GivenPrintInUpperCase_WhenStringIsNotNull_ThenReturnStringInUpperCase()
+        {
+            var result = WithoutDelegates.PrintInUpperCase("Delegates in C#.");
+
+            Assert.Equal("DELEGATES IN C#.", result);
+        }
+
+        [Fact]
+        public void GivenPrintInLowerCase_WhenStringIsNotNull_ThenReturnStringInLowerCase()
+        {
+            var result = WithoutDelegates.PrintInLowerCase("Delegates in C#.");
+
+            Assert.Equal("delegates in c#.", result);
+        }
+
+        [Fact]
+        public void GivenPrintInUpperCase_WhenStringIsNull_ThenThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => WithoutDelegates.PrintInUpperCase(null));
+        }
+
+        [Fact]
+        public void GivenPrintInLowerCase_WhenStringIsNull_ThenThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => WithoutDelegates.PrintInLowerCase(null));
+        }
+
+        [Fact]
+        public void GivenPrintProcessedText_WhenStringIsNull_ThenThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(()
+                => WithDelegates.PrintProcessedText(null, x => Console.WriteLine(""), x => x.ToUpper()));
+        }
+
+        [Fact]
+        public void GivenPrintProcessedText_WhenPrintIsNull_ThenThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(()
+                => WithDelegates.PrintProcessedText("", null, x => x.ToUpper()));
+        }
+
+        [Fact]
+        public void GivenPrintProcessedText_WhenOperationIsNull_ThenThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(()
+                => WithDelegates.PrintProcessedText("", x => Console.WriteLine(""), null));
+        }
+
+        [Fact]
+        public void GivenPrintProcessedText_WhenAllIsNotNull_ThenReturnStringInUpperCase()
+        {
+            var result = WithDelegates.PrintProcessedText("Delegates in C#.", x => Console.WriteLine(""), x => x.ToUpper());
+
+            Assert.Equal("DELEGATES IN C#.", result);
+        }
+
+        [Fact]
+        public void GivenPrintProcessedText_WhenAllIsNotNull_ThenReturnStringInLowerCase()
+        {
+            var result = WithDelegates.PrintProcessedText("Delegates in C#.", x => Console.WriteLine(""), x => x.ToLower());
+
+            Assert.Equal("delegates in c#.", result);
+        }
+    }
+}
