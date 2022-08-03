9dc714fc6544fe10f6ad9200f401d2ac4e8498a4
Usman <usman.solemn@gmail.com>
2022-08-03 10:43:56 +0500

    Tests updated with naming convenstion

--- csharp-advanced-topics/ActionFuncInCsharp/TestsProject/ActionTests.cs
+++ csharp-advanced-topics/ActionFuncInCsharp/TestsProject/ActionTests.cs
@@ -6,20 +6,17 @@ namespace TestsProject
     public class ActionTests
     {
         [TestMethod]
-        public void ShouldHaveOneActionDelegate()
+        public void GivenFunction_WhenOneActionDelegatePassed_ShouldHaveOneDeletageLinked()
         {            
             // arrage
             Action<string> actionDelegate = ActionDelegate;
             var invoicingService = new InvoicingService();
-
             // act
             invoicingService.GenerateInvoiceWithAction(actionDelegate);
             var invocationList = actionDelegate.GetInvocationList();
-
             // assert
             Assert.AreEqual(invocationList.Length, 1);
         }
-
         private void ActionDelegate(string arg)
         {
             // any internl processing
--- csharp-advanced-topics/ActionFuncInCsharp/TestsProject/FuncTests.cs
+++ csharp-advanced-topics/ActionFuncInCsharp/TestsProject/FuncTests.cs
@@ -6,21 +6,17 @@ namespace TestsProject
     public class FuncTests
     {
         [TestMethod]
-        public void ShouldHaveOneFuncDelegate()
+        public void GivenFunction_WhenOneFuncDelegatePassed_ShouldHaveOneDeletageLinked()
         {            
             // arrange
             Func<decimal, decimal> funcDelegate = FuncDelegate;
             var invoicingService = new InvoicingService();
-
             // act
             invoicingService.GenerateInvoiceWithFunc(10, funcDelegate);
             var invocationList = funcDelegate.GetInvocationList();
-
             // assert
             Assert.AreEqual(invocationList.Length, 1);
-            
         }
-
         private decimal FuncDelegate(decimal arg)
         {
             return arg + (arg * 0.2M);
