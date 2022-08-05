035c9e031c7ea51f8b20728ba3f45c131a430b4b
Osama Al-Rashed <Osama Al-Rashed@DESKTOP-8954161>
2022-08-05 12:06:08 +0300

    CM-545: Flags Attribute For Enum In CSharp

---
+++ csharp-basic-topics/FlagsAttributeForEnum/FlagsAttributeForEnum/UserTypeExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagsAttributeForEnum
+{
+    public static class UserTypeExtensionMethods
+    {
+        public static UserType Add(this UserType userType, params UserType[] typesToAdd)
+        {
+            foreach (var flag in typesToAdd)
+            {
+                userType |= flag;
+            }
+            return userType;
+        }
+
+        public static UserType Remove(this UserType userType, params UserType[] typesToRemove)
+        {
+            foreach (var item in typesToRemove)
+            {
+                userType &= ~item;
+            }
+            return userType;
+        }
+
+        public static bool CustomHasFlag(this UserType userType, UserType typeToCompare)
+            => (userType & typeToCompare) == typeToCompare;
+
+        public static void Print(this UserType userType)
+            => Console.WriteLine("This message is for users of types: {0}.", userType);
+
+    }
+}
