451e5e8ea1eb44084e327928c9964bfb6ad6e982
AsistAVALON8 <luis.coco@grupoavalon.es>
2022-08-10 12:39:45 +0200

    Test methods names changes
    Test methods names changes

--- csharp-advanced-topics/FuncActionDelegates/Tests/Tests.cs
+++ csharp-advanced-topics/FuncActionDelegates/Tests/Tests.cs
@@ -8,7 +8,7 @@ namespace Tests
 	public class Tests
 	{
 		[TestMethod]
-		public void whenStringIsSent_DelegateExecutesTheReferencedMethod()
+		public void whenStringIsSent_ThenDelegateExecutesTheReferencedMethod()
 		{
 			var delegate1 = new PrintMessage(SampleFunctions.WriteText);
 			var result = delegate1("You're gonna need a bigger boat.");
@@ -16,7 +16,7 @@ namespace Tests
 		}
 
 		[TestMethod]
-		public void whenDelegateIsInvoked_DelegateExecutesReferencedMethod()
+		public void whenDelegateIsInvoked_ThenDelegateExecutesReferencedMethod()
 		{
 			var delegate2 = SampleFunctions.WriteText;
 			var result = delegate2.Invoke("You're gonna need a bigger boat.");
@@ -24,7 +24,7 @@ namespace Tests
 		}
 
 		[TestMethod]
-		public void whenDelegateIsInvoked_DelegateExecutesAnonymousReferencedMethod()
+		public void whenDelegateIsInvoked_ThenDelegateExecutesAnonymousReferencedMethod()
 		{
 			var delegate3 = delegate (string text) { return $"Text:{text}"; };
 			var result = delegate3.Invoke("You're gonna need a bigger boat.");
@@ -32,7 +32,7 @@ namespace Tests
 		}
 
 		[TestMethod]
-		public void whenDelegateIsInvoked_DelegateExecutesExpressionLambda()
+		public void whenDelegateIsInvoked_ThenDelegateExecutesExpressionLambda()
 		{
 			PrintMessage delegate4 = text => { return $"Text:{text}"; };
 			var result = delegate4("You're gonna need a bigger boat.");
@@ -40,7 +40,7 @@ namespace Tests
 		}
 
 		[TestMethod]
-		public void whenFuncDelegate_DelegateExecutesReferencedMethod()
+		public void whenFuncDelegate_ThenDelegateExecutesReferencedMethod()
 		{
 			Func<int, int, int> AddTwoNumbers = SampleFunctions.AddNumbers;
 			int result = AddTwoNumbers(10, 20);
@@ -48,7 +48,7 @@ namespace Tests
 		}
 
 		[TestMethod]
-		public void whenFuncDelegate_DelegateExecutesAnonymousFunction()
+		public void whenFuncDelegate_ThenDelegateExecutesAnonymousFunction()
 		{
 			Func<int, int, int> AddTwoNumbers = delegate (int param1, int param2) { return param1 + param2; };
 			int result = AddTwoNumbers(10, 20);
@@ -56,7 +56,7 @@ namespace Tests
 		}
 
 		[TestMethod]
-		public void whenFuncDelegate_DelegateExecutesExpressionLambda()
+		public void whenFuncDelegate_ThenDelegateExecutesExpressionLambda()
 		{
 			Func<int, int, int> AddTwoNumbers = (param1, param2) => param1 + param2;
 			int result = AddTwoNumbers(10, 20);
@@ -64,7 +64,7 @@ namespace Tests
 		}
 
 		[TestMethod]
-		public void whenActionDelegate_DelegateExecutesReferencedMethod()
+		public void whenActionDelegate_ThenDelegateExecutesReferencedMethod()
 		{
 			Action<int, int> Addition = SampleFunctions.AddTwoNumbers;
 			Addition(10, 20);
@@ -72,7 +72,7 @@ namespace Tests
 		}
 
 		[TestMethod]		
-		public void whenActionDelegate_DelegateExecutesAnonymousMethod()
+		public void whenActionDelegate_ThenDelegateExecutesAnonymousMethod()
 		{
 			Action<int, int> Addition1 = delegate (int param1, int param2) { SampleFunctions.result = param1 + param2; };
 			Addition1(10, 20);
@@ -81,7 +81,7 @@ namespace Tests
 
 		[TestMethod]
 		
-		public void whenActionDelegate_DelegateExecutesExpressionLambda()
+		public void whenActionDelegate_ThenDelegateExecutesExpressionLambda()
 		{
 			Action<int, int> Addition2 = (param1, param2) => SampleFunctions.result = param1 + param2;
 			Addition2(10, 20);
