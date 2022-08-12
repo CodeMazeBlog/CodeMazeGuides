1236057518bde9d800021791327ab83788fa9fde
ABHINAV RAJ <abhinavshawarn@gmail.com>
2022-08-12 20:25:35 +0530

    test

---
+++ csharp-basic-topics/ActionandfuncDelegates/Delegates.cs
@@ -0,0 +1,31 @@
+using System;
+class mydelegate
+{
+
+
+    // DEFINING A METHOD AND PARAMETERS
+    public static void actiondelegate(int x, int y)
+    {
+        Console.WriteLine(x + y);
+    }
+
+    static public void actiondelegate()
+    {
+
+        // Using the action delegate
+        Action<int, int> val = actiondelegate;
+        val(20, 5);
+    }
+}
+
+
+public class myfuncdelegate
+{
+    static Func<int, int, int> operation;
+    public static int Sum(int x, int y) { return x + y; }
+    public static void Main()
+    {
+        Func<int, int, int> add = Sum; int result = add(10, 10);
+        Console.WriteLine(result);
+    }
+}
