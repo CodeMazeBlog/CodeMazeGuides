10aad84fad8d723de0d7de443289248e30486e9c
Ahsan <ullahahsan1983@gmail.com>
2022-08-14 14:30:10 +0600

    Minor naming fix

--- aspnetcore-webapi/MultiTenantApplication/MultiTenantApplication.Infrastructure/TenantRegistry.cs
+++ aspnetcore-webapi/MultiTenantApplication/MultiTenantApplication.Infrastructure/TenantRegistry.cs
@@ -2,19 +2,19 @@
 
 public class TenantRegistry : ITenantRegistry
 {
-    private readonly TenantOptions _registry;
+    private readonly TenantOptions _tenantOptions;
 
     public TenantRegistry(IConfiguration configuration)
     {
-        _registry = configuration.GetSection("TenantOptions").Get<TenantOptions>();
+        _tenantOptions = configuration.GetSection("TenantOptions").Get<TenantOptions>();
 
-        foreach(var tenant in _registry.Tenants.Where(e => string.IsNullOrEmpty(e.ConnectionString)))
+        foreach(var tenant in _tenantOptions.Tenants.Where(e => string.IsNullOrEmpty(e.ConnectionString)))
         {
-            tenant.ConnectionString = _registry.DefaultConnection;
+            tenant.ConnectionString = _tenantOptions.DefaultConnection;
         }
     }
 
-    public Tenant[] GetTenants() => _registry.Tenants;
+    public Tenant[] GetTenants() => _tenantOptions.Tenants;
 
-    public User[] GetUsers() => _registry.Users;
+    public User[] GetUsers() => _tenantOptions.Users;
 }
