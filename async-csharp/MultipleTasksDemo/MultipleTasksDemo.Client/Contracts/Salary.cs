2eec5543da30f6eeefbacd0a02fd827f64a180e4
Chayan Adhikari <adhikari.chayan@gmail.com>
2022-08-15 11:58:23 +0530

    CM-373: How to Execute Multiple Tasks Asynchronously in C#

---
+++ async-csharp/MultipleTasksDemo/MultipleTasksDemo.Client/Contracts/Salary.cs
@@ -0,0 +1,8 @@
+﻿namespace MultipleTasksDemo.Client.Contracts;
+
+public class Salary
+{
+    public Guid Id { get; set; }
+
+    public decimal SalaryInEuro { get; set; }
+}
---
+++ async-csharp/MultipleTasksDemo/MultipleTasksDemo.Client/Extensions/TaskExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace MultipleTasksDemo.Client.Extensions;
+
+public class TaskExtensions
+{
+    public static async Task<(T1, T2, T3)> WhenAll<T1, T2, T3>(Task<T1> task1, Task<T2> task2, Task<T3> task3)
+    {
+        var allTasks = Task.WhenAll(task1, task2, task3);
+        try
+        {
+            await allTasks;
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine("Task Exception", exp);
+            throw allTasks.Exception;
+        }
+
+        return (task1.Result, task2.Result, task3.Result);
+    }
+}
