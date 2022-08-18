38e2eaa6ebc8676154090981d9dbcaaa023c5f2d
Code Maze <contact@code-maze.com>
2022-08-18 11:07:04 +0200

    CM-374 Different Ways to Add Values to a C# Array - minor fix

--- collections-arrays/AddValuesToArray/AddValuesToArray.Benchmark/AddValuesToArrayBenchmark.cs
+++ collections-arrays/AddValuesToArray/AddValuesToArray.Benchmark/AddValuesToArrayBenchmark.cs
@@ -28,7 +28,7 @@ public class AddValuesToArrayBenchmark
 
     [Benchmark]
     [ArgumentsSource(nameof(ArraySize))]
-    public void LinqList(int arraySize)
+    public void ListCollection(int arraySize)
     {
         AddValuesToArrayMethods.LinqList(arraySize);
     }
@@ -45,13 +45,6 @@ public class AddValuesToArrayBenchmark
     public void LinqAppend(int arraySize)
     {
         AddValuesToArrayMethods.LinqAppend(arraySize);
-    }
-
-    [Benchmark]
-    [ArgumentsSource(nameof(ArraySize))]
-    public void ArrayResize(int arraySize)
-    {
-        AddValuesToArrayMethods.ArrayResize(arraySize);
     }
 
     [Benchmark]
