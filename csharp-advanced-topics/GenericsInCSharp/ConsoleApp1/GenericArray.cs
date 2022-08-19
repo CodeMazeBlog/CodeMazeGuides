ff97a25ee2fc50a244b5e16ab1b150cd07b3b3b4
Martin Chege <martinchege324@gmail.com>
2022-08-19 22:14:25 +0300

    Generics in C# first draft

---
+++ csharp-advanced-topics/GenericsInCSharp/ConsoleApp1/GenericArray.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericsInCSharp
+{
+    public class GenericArray<T>
+    {
+        private T[] arrayObj;
+        public GenericArray(int size)
+        {
+            arrayObj = new T[size + 1];
+        }
+        public T retrieveValue(int index)
+        {
+            return arrayObj[index];
+        }
+        public void insertValue(int index, T value)
+        {
+            arrayObj[index] = value;
+        }
+    }
+}
