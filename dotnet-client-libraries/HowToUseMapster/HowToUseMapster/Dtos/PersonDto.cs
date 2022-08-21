5364fdbbb947e197549a5444fd9028526a0fa9cf
Marko Hrncic <marko.hrncic@kogitas.com>
2022-08-21 14:02:41 +0200

    CM-571 Implemented web api project to perform Mapster operations.

---
+++ dotnet-client-libraries/HowToUseMapster/HowToUseMapster/Dtos/PersonDto.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+
+namespace HowToUseMapster.Dtos
+{
+    public class PersonDto
+    {
+        public string? Title { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello...");
+        }
+
+        public void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye...");
+        }
+    }
+}
---
+++ dotnet-client-libraries/HowToUseMapster/HowToUseMapster/Dtos/PersonShortInfoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HowToUseMapster.Dtos
+{
+    public class PersonShortInfoDto
+    {
+        public string? FullName { get; set; }
+        public int? Age { get; set; }
+        public string? FullAddress { get; set; }
+    }
+}
