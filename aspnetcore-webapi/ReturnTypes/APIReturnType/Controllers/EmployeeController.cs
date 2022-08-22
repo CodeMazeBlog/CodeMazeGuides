48d42506cf37231fd5eee851c3f7d9e5475c780d
Muhammed Saleem <Muhammed.P@gds.ey.com>
2022-08-22 18:19:50 +0530

    Changes for CM-128: ASP.NET Core Web API Action Return Types

---
+++ aspnetcore-webapi/ReturnTypes/APIReturnType/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIReturnType.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public FakeRepository _repository = new();
+
+        [HttpGet]
+        public IEnumerable<Employee> Get() =>
+            _repository.GetEmployees();
+
+        [HttpGet("active")]
+        public IEnumerable<Employee> GetActive()
+        {
+            foreach (var employee in _repository.GetActiveEmployees())
+            {
+                yield return employee;
+            }
+        }
+
+        [HttpGet("activeasync")]
+        public async IAsyncEnumerable<Employee> GetActiveAsync()
+        {
+            await foreach (var employee in _repository.GetActiveEmployeesAsync())
+            {
+                yield return employee;
+            }
+        }
+    }
+}
---
+++ aspnetcore-webapi/ReturnTypes/APIReturnType/FakeRepository.cs
@@ -0,0 +1,62 @@
+﻿namespace APIReturnType.Controllers
+{
+    public class FakeRepository
+    {
+        public List<Employee> Employees { get; set; }
+
+        internal IEnumerable<Employee> GetEmployees()
+        {            
+            Employees = new()
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    Name = "John Doe",
+                    IsActive = true
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "Bob Smith",
+                    IsActive = false
+                },
+                new Employee()
+                {
+                    Id = 3,
+                    Name = "Alice Tom",
+                    IsActive = true
+                }
+            };
+
+            return Employees;
+        }
+
+        internal bool TryGetEmployee(int id, out Employee? employee)
+        {
+            employee = GetEmployees().FirstOrDefault(e => e.Id == id);
+            return employee != null;
+        }
+
+        internal IEnumerable<Employee> GetActiveEmployees()
+        {
+            foreach (var employee in GetEmployees())
+            {
+                Thread.Sleep(3000);
+                yield return employee;
+            }
+        }
+
+        internal async IAsyncEnumerable<Employee> GetActiveEmployeesAsync()
+        {
+            foreach (var employee in GetEmployees())
+            {
+                await Task.Delay(3000);
+                yield return employee;
+            }
+        }
+
+        internal async Task AddEmployeeAsync(Employee employee)
+        {
+        }
+    }
+}
