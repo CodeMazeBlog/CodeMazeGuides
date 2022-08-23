6eb205048adcd81478101a15d1590cde2d315546
Martin Chege <martinchege324@gmail.com>
2022-08-23 19:26:36 +0300

    CM-33: Using Generics in C# (#585)
    * Generics in C# first draft
    
    * Three key changes:
    1. Renaming public methods to PascalCase
    2.  String interpolation
    3. Renaming the project folder

---
+++ csharp-advanced-topics/GenericsInCSharp/GenericsInCSharp/GenericArray.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericsInCSharp
+{
+    public class GenericArray<T>
+    {
+        private T[] arrayObj;
+        public GenericArray(int size)
+        {
+            arrayObj = new T[size + 1];
+        }
+        public T RetrieveValue(int index)
+        {
+            return arrayObj[index];
+        }
+        public void InsertValue(int index, T value)
+        {
+            arrayObj[index] = value;
+        }
+    }
+}
