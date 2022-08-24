e2507072286cca637740365f201dead196402aaf
Muhammed Saleem P <muhammedsalimp@gmail.com>
2022-08-24 11:40:03 +0530

    CM-128: ASP.NET Core Web API Action Return Types

---
+++ aspnetcore-webapi/ReturnTypes/Tests/Tests/EmployeeActionResultControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class EmployeeActionResultControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public EmployeeActionResultControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task WhenGettingEmployeeById_ThenReturnSuccess()
+        {
+            // arrange
+            var client = _factory.CreateClient();
+
+            // act
+            var response = await client.GetAsync("api/employeeactionresult/1");
+
+            //assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task WhenEmployeeNotFound_ThenReturnNotFound()
+        {
+            // arrange
+            var client = _factory.CreateClient();
+
+            // act
+            var response = await client.GetAsync("api/employeeactionresult/1001");
+
+            //assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+    }
+}
