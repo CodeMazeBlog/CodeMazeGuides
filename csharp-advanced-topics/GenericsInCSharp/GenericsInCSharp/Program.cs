a8726bacdb49faec0d7651e018ed3deea8d65fe9
Martin Chege <martinchege324@gmail.com>
2022-08-25 13:46:11 +0300

    CM-33: Using Generics in C#  (#591)
    * Spacing and naming changes
    
    * Formatting changes
    
    * More formatting changes

--- csharp-advanced-topics/GenericsInCSharp/GenericsInCSharp/Program.cs
+++ csharp-advanced-topics/GenericsInCSharp/GenericsInCSharp/Program.cs
@@ -9,11 +9,13 @@
             var delegateObj = new GenericDelegates();
             var addition = new ArithmeticDelegates<int>(delegateObj.AdditionFunc);
             var multiplication = new ArithmeticDelegates<int>(delegateObj.MultiplicationFunc);
+
             var num1 = 5;
             var num2 = 10;
-            var printOutput = $"The sum of {num1} and {num2} is {addition(num1, num2)}";
 
+            var printOutput = $"The sum of {num1} and {num2} is {addition(num1, num2)}";
             delegateObj.PrintString(printOutput);
+
             printOutput = $"The multiplication of {num1} and {num2} is {multiplication(num1, num2)}";
             delegateObj.PrintString(printOutput);
         }
