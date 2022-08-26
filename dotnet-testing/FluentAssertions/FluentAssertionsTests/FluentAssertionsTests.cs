4af23321eae7eb861858ef140d4dd60db7ebd627
Alvaro Montoya <alvaro.montoya@getrhyme.com>
2022-08-26 13:37:31 -0400

    Added all Unit tests for this project

--- dotnet-testing/FluentAssertions/FluentAssertionsTests/FluentAssertionsTests.cs
+++ dotnet-testing/FluentAssertions/FluentAssertionsTests/FluentAssertionsTests.cs
@@ -11,56 +11,339 @@ namespace FluentAssertionsTests
     public class FluentAssertionsTests
     {
         [TestMethod]
-        public async Task TestMethod1()
+        public async Task GivenAString_ThenAssertStartEndAndLength()
         {
-            string thing = "";
-            Customer thing2 = new Customer();
-            List<Customer> myList = new List<Customer>();
-
-            if (thing.StartsWith("He") && thing.EndsWith("g") && thing.Length == 28)
-            {
-                myList.Should().AllSatisfy(c => c.Id.Should().BeGreaterThan(0));
-
-                myList.Should().NotBeNull().And.HaveCountGreaterThan(0);
-                
-                thing2.Invoking(c => c.GetId()).Should().Throw<NullReferenceException>();
-
-                Action action = () => thing2.GetId();
-                action.Should().Throw<NullReferenceException>().Where(e => e.Message != null).Where(e => e.Data.Count > 0);
-
-                FluentActions.Invoking(() => thing2.GetId()).Should().Throw<ArgumentNullException>();
-
-                Func<Task> act = () => thing2.GetIdAsync();
-                await act.Should().ThrowAsync<ArgumentNullException>();
-
-                using (new AssertionScope())
-                {
-                    myList.Should().NotBeNull();
-                    myList.Should().HaveCountGreaterThan(0);
-                    myList.Should().OnlyHaveUniqueItems();
-                    myList.Should().AllBeOfType<Customer>();
-                    Execute.Assertion.FailWith("The list is empty").Then.ForCondition(myList.Count > 0);
-                }
-
-
-                return; // this is a pass
-            }
-            else 
-            {
-                Assert.Fail();
-            }
+            string myTestString = "Hello, this is a test string";
+            myTestString.Should().StartWith("He").And.EndWith("g").And.HaveLength(28);
         }
 
         [TestMethod]
-        public async Task TestMethod2()
+        public async Task GivenAString_ThenAssertBe()
         {
-            int num = -5;
-            num.Should().BeGreaterThan(5, "because Ids under five are reserved");
+            string myTestString = "Hello, this is a test string";
+            myTestString.Should().Be("Hello, this is a test string");
+            myTestString.Should().NotBe("Hello, this is the wrong string");
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertBeEquivalentTo()
+        {
+            string myTestString = "Hello, this is a test string";
+            myTestString.Should().BeEquivalentTo(myTestString);
+            myTestString.Should().NotBeEquivalentTo("Helo, this is a test string");
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertBeEmpty()
+        {
+            string myTestString = "";
+            string myOtherTestString = "Hello, this is a test string";
+            myTestString.Should().BeEmpty();
+            myOtherTestString.Should().NotBeEmpty();
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertBeNull()
+        {
+            string? myTestString = null;
+            string myOtherTestString = "Hello, this is a test string";
+            myTestString.Should().BeNull();
+            myOtherTestString.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertBeNullOrEmpty()
+        {
+            string? myTestString = null;
+            string myOtherTestString = "Hello, this is a test string";
+            myTestString.Should().BeNullOrEmpty();
+            myOtherTestString.Should().NotBeNullOrEmpty();
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertBeLowerCased()
+        {
+            // be careful of special characters as they will fail this assertion, even whitespace characters
+            string myTestString = "hello";
+            string myOtherTestString = "HEllo";
+            myTestString.Should().BeLowerCased();
+            myOtherTestString.Should().NotBeLowerCased();
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertBeUpperCased()
+        {
+            // be careful of special characters as they will fail this assertion, even whitespace characters
+            string myTestString = "HELLO";
+            string myOtherTestString = "HELlo";
+            myTestString.Should().BeUpperCased();
+            myOtherTestString.Should().NotBeUpperCased();
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertMatch()
+        {
+            string myTestString = "Hello, this is a test string";
+            string myOtherTestString = "Hello, this is a test string";
+            myTestString.Should().Match("Hello, this is a * string");
+            myOtherTestString.Should().NotMatch("Hello, this is a ? string");
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertMatchEquivalentOf()
+        {
+            string myTestString = "Hello, this is a test string";
+            string myOtherTestString = "Hello, this is a test string";
+            myTestString.Should().MatchEquivalentOf("HElLo, this is a * string");
+            myOtherTestString.Should().NotMatchEquivalentOf("HeLLo, this is a ? string");
+        }
+
+        [TestMethod]
+        public async Task GivenAString_ThenAssertMatchRegex()
+        {
+            string myTestString = "05/30/2022";
+            string myOtherTestString = "32A-45/2022";
+            myTestString.Should().MatchRegex("\\d{1,2}\\/\\d{1,2}\\/\\d{4}");
+            myOtherTestString.Should().NotMatchRegex("\\d{1,2}\\/\\d{1,2}\\/\\d{4}");
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertHaveElementAt()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer()
+            {
+                Id = 1,
+            };
+            customers.Add(customer);
+
+            customers.Should().HaveElementAt(0, customer);
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertElementPreceding()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer);
+            customers.Add(customer2);
+            customers.Add(customer3);
+
+            customers.Should().HaveElementPreceding(customer2, customer);
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertElementSucceeding()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer);
+            customers.Add(customer2);
+            customers.Add(customer3);
+
+            customers.Should().HaveElementSucceeding(customer, customer2);
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertElementsInAscendingOrder()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer);
+            customers.Add(customer2);
+            customers.Add(customer3);
+
+            customers.Should().BeInAscendingOrder(customer => customer.Id);
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertElementsInDescendingOrder()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer3);
+            customers.Add(customer2);
+            customers.Add(customer);
+            
+            customers.Should().BeInDescendingOrder(customer => customer.Id);
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertContains()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer3);
+            customers.Add(customer2);
+            customers.Add(customer);
+
+            customers.Should().Contain(customer);
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertIntersectsWith()
+        {
+            List<Customer> customers = new List<Customer>();
+            List<Customer> customers2 = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer3);
+            customers.Add(customer2);
+            customers.Add(customer);
+
+            customers2.Add(customer3);
+            customers2.Add(customer);
+
+            customers.Should().IntersectWith(customers2);
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertOnlyHasUniqueItems()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer3);
+            customers.Add(customer2);
+            customers.Add(customer);
+
+            customers.Should().OnlyHaveUniqueItems();
+        }
+
+        [TestMethod]
+        public async Task GivenACollection_ThenAssertAllSatisfy()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer3);
+            customers.Add(customer2);
+            customers.Add(customer);
+
+            customers.Should().AllSatisfy(customer => customer.Id.Should().BeGreaterThan(0));
+        }
+
+        [TestMethod]
+        public async Task GivenACustomer_ThenAssertOfType()
+        {
+            Customer customer = new Customer(1);
+
+            customer.Should().BeOfType<Customer>();
+        }
+
+        [TestMethod]
+        public async Task GivenASpecialCustomer_ThenAssertBeAssignableToCustomer()
+        {
+            SpecialCustomer customer = new SpecialCustomer();
+
+            customer.Should().BeAssignableTo<Customer>();
+        }
+
+        [TestMethod]
+        public async Task GivenACustomer_ThenAssertGetNullIdThrowsException()
+        {
+            // Customer Object with null Id property
+            var customer = new Customer();
+            
+            // Asserts that calling GetId() will throw
+            customer.Invoking(c => c.GetId()).Should().Throw<NullReferenceException>();
+
+            // Asserts that calling GetId() will throw
+            Action action = () => customer.GetId(); 
+            action.Should().Throw<NullReferenceException>(); 
+            
+            // Asserts that calling GetId() will throw
+            FluentActions.Invoking(() => customer.GetId()).Should().Throw<NullReferenceException>();
+
+            // same assertion only async
+            Func<Task> act = () => customer.GetIdAsync(); 
+            await act.Should().ThrowAsync<NullReferenceException>();
+        }
+
+        public async Task GivenAnAssertionScope_ThenAllAssertionsAreEvaluated()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer3);
+            customers.Add(customer2);
+            customers.Add(customer);
+
+            using (new AssertionScope()) 
+            {
+                customers.Should().NotBeNull();
+                customers.Should().HaveCountGreaterThan(0);
+                customers.Should().OnlyHaveUniqueItems();
+                customers.Should().AllBeOfType<Customer>(); 
+            }
+        }
+
+        public async Task GivenAssertionsChained_ThenAllAssertionsAreEvaluated()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(1);
+            Customer customer2 = new Customer(2);
+            Customer customer3 = new Customer(3);
+
+            customers.Add(customer3);
+            customers.Add(customer2);
+            customers.Add(customer);
+
+            customers.Should().NotBeNull().And.HaveCountGreaterThan(0); 
+            Execute.Assertion.FailWith("customers list is empty").Then.ForCondition(customers.Count > 0);
+        }
+
+        public async Task GivenACollection_ThenWhichEvaluatesElementsInCollection()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = new Customer(99);
+
+            customers.Add(customer);
+
+            customers.Should().ContainSingle().Which.Should().BeEquivalentTo(new Customer { Id = 99 });
+        }
+
+        public async Task GivenAnAction_ThenWhichEvaluatesResultingObject()
+        {
+            Customer customer = new Customer();
+            Action action = () => customer.GetId();
+
+            action.Should().Throw<NullReferenceException>().Where(ex => ex.Message != null).Where(ex => ex.Data.Count > 0);
         }
     }
 
     public class Customer 
     {
+        public Customer(int id)
+        {
+            Id = id;
+        }
+
+        public Customer() { }
+
         public int? Id { get; set; }
 
         public int GetId()
@@ -80,5 +363,9 @@ namespace FluentAssertionsTests
                 return GetId();
             });
         }
+    }
+    public class SpecialCustomer : Customer
+    {
+        public SpecialCustomer() { }
     }
 }
