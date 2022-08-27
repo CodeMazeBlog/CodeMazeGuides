fce01d3f93692e075fd5039d593d023ecb65962b
Marko Hrncic <marko.hrncic@kogitas.com>
2022-08-27 13:29:20 +0200

    CM-571 Added AddressStreet property to the PersonDto to perform flattening.

--- dotnet-client-libraries/HowToUseMapster/HowToUseMapster/Dtos/PersonDto.cs
+++ dotnet-client-libraries/HowToUseMapster/HowToUseMapster/Dtos/PersonDto.cs
@@ -8,6 +8,7 @@ namespace HowToUseMapster.Dtos
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public DateTime? DateOfBirth { get; set; }
+        public string? AddressStreet { get; set; }
 
         public void SayHello()
         {
