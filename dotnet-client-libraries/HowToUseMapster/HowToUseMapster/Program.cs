c962c92a1efedea2738d46afe99cbe33e70309d4
Marko Hrnčić <61161521+mahrncic@users.noreply.github.com>
2022-08-27 14:41:20 +0200

    CM-571: Getting Started with Mapster in ASP.NET Core (#587)

---
+++ dotnet-client-libraries/HowToUseMapster/HowToUseMapster/Program.cs
@@ -0,0 +1,25 @@
+using HowToUseMapster.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterMapsterConfiguration();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
