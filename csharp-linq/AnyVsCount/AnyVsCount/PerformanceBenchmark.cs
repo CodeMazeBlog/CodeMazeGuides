c168a6f43f371c957e873b137b5713d0c7bf25f8
Marko Hrncic <marko.hrncic@kogitas.com>
2022-09-01 19:50:30 +0200

    CM-562 Implemented naming changes for more readable code.

--- csharp-linq/AnyVsCount/AnyVsCount/PerformanceBenchmark.cs
+++ csharp-linq/AnyVsCount/AnyVsCount/PerformanceBenchmark.cs
@@ -3,38 +3,38 @@ using BenchmarkDotNet.Order;
 
 namespace AnyVsCount
 {
-    [MemoryDiagnoser, Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class PerformanceBenchmark { 
     
         private static readonly IEnumerable<int> _numbersEnumerable = GetRandomNumbersBySize(1000);
         private static readonly ICollection<int> _numbersList = GetRandomNumbersBySize(1000).ToList();
 
         [Benchmark]
-        public bool CheckIfAnyWithAny()
+        public bool CheckWithAny()
         {
             return _numbersEnumerable.Any();
         }
 
         [Benchmark]
-        public bool CheckIfAnyWithCount()
+        public bool CheckWithCount()
         {
             return _numbersEnumerable.Count() > 0;
         }
 
         [Benchmark]
-        public bool CheckIfAnyWithAnyAndCondition()
+        public bool CheckWithAnyAndCondition()
         {
             return _numbersEnumerable.Any(num => num > 500);
         }
 
         [Benchmark]
-        public bool CheckIfAnyWithCountAndCondition()
+        public bool CheckWithCountAndCondition()
         {
             return _numbersEnumerable.Count(num => num > 500) > 0;
         }
 
         [Benchmark]
-        public bool CheckIfAnyWithCountProperty()
+        public bool CheckWithCountProperty()
         {
             return _numbersList.Count > 0;
         }
--- csharp-linq/AnyVsCount/Tests/Tests.cs
+++ csharp-linq/AnyVsCount/Tests/Tests.cs
@@ -13,41 +13,41 @@ namespace Tests
         }
 
         [Fact]
-        public void WhenCheckingIfAnyWithAny_ThenReturnTrue()
+        public void WhenCheckingWithAny_ThenReturnTrue()
         {
-            var anyValue = _benchmarkRunner.CheckIfAnyWithAny();
+            var anyValue = _benchmarkRunner.CheckWithAny();
 
             Assert.True(anyValue);
         }
 
         [Fact]
-        public void WhenCheckingIfAnyWithCount_ThenReturnTrue()
+        public void WhenCheckingWithCount_ThenReturnTrue()
         {
-            var anyValue = _benchmarkRunner.CheckIfAnyWithCount();
+            var anyValue = _benchmarkRunner.CheckWithCount();
 
             Assert.True(anyValue);
         }
 
         [Fact]
-        public void WhenCheckingIfAnyWithAnyAndCondition_ThenReturnTrue()
+        public void WhenCheckingWithAnyAndCondition_ThenReturnTrue()
         {
-            var anyValueWithCodition = _benchmarkRunner.CheckIfAnyWithAnyAndCondition();
+            var anyValueWithCodition = _benchmarkRunner.CheckWithAnyAndCondition();
 
             Assert.True(anyValueWithCodition);
         }
 
         [Fact]
-        public void WhenCheckingIfAnyWithCountAndCondition_ThenReturnTrue()
+        public void WhenCheckingWithCountAndCondition_ThenReturnTrue()
         {
-            var anyValueWithCodition = _benchmarkRunner.CheckIfAnyWithCountAndCondition();
+            var anyValueWithCodition = _benchmarkRunner.CheckWithCountAndCondition();
 
             Assert.True(anyValueWithCodition);
         }
 
         [Fact]
-        public void WhenCheckingIfAnyWithCountProperty_ThenReturnTrue()
+        public void WhenCheckingWithCountProperty_ThenReturnTrue()
         {
-            var anyValueWithCountProperty = _benchmarkRunner.CheckIfAnyWithCountProperty();
+            var anyValueWithCountProperty = _benchmarkRunner.CheckWithCountProperty();
 
             Assert.True(anyValueWithCountProperty);
         }
