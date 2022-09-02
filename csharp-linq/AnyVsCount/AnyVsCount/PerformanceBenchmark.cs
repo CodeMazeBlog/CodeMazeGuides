82c7445f1f978266f3b2b23e45829b8c9df1ddfb
Marko Hrncic <marko.hrncic@kogitas.com>
2022-09-02 16:59:10 +0200

    CM-562 Removed random generator helper function.

--- csharp-linq/AnyVsCount/AnyVsCount/PerformanceBenchmark.cs
+++ csharp-linq/AnyVsCount/AnyVsCount/PerformanceBenchmark.cs
@@ -6,8 +6,8 @@ namespace AnyVsCount
     [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class PerformanceBenchmark { 
     
-        private static readonly IEnumerable<int> _numbersEnumerable = GetRandomNumbersBySize(1000);
-        private static readonly ICollection<int> _numbersList = GetRandomNumbersBySize(1000).ToList();
+        private static readonly IEnumerable<int> _numbersEnumerable = Enumerable.Range(1, 1000);
+        private static readonly ICollection<int> _numbersList = Enumerable.Range(1, 1000).ToList();
 
         [Benchmark]
         public bool CheckWithAny()
@@ -33,25 +33,10 @@ namespace AnyVsCount
             return _numbersEnumerable.Count(num => num > 500) > 0;
         }
 
-        [Benchmark]
+        //[Benchmark]
         public bool CheckWithCountProperty()
         {
             return _numbersList.Count > 0;
-        }
-
-
-        // Helper methods
-        public static IEnumerable<int> GetRandomNumbersBySize(int size)
-        {
-            var numbers = Enumerable.Range(1, size);
-            var rand = new Random();
-            var shuffledNumbers =
-                numbers.Select(x => new { Number = rand.Next(), Item = x })
-                       .OrderBy(x => x.Number)
-                       .Select(x => x.Item)
-                       .Take(size);
-
-            return shuffledNumbers.ToList();
         }
     }
 }
