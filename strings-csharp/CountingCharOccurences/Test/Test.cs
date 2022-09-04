711ad446a4f9ec7cce74f851658163c8561cc419
Satya Prakash <51241619+lasaetarubia@users.noreply.github.com>
2022-09-04 20:04:31 +0530

    CM-528 : Counting Occurrences of a String (Char) Within a String (#586)
    * CM-528 : Counting Occurrences of a String (Char) Within a String
    
    * CM-528 : Code Edits after review
    
    * CM-528 : Reverting back to BenchmarkDotNet
    
    * CM-528 : Added Ordering of methods based on performance

---
+++ strings-csharp/CountingCharOccurences/Test/Test.cs
@@ -0,0 +1,152 @@
+using CountingCharOccurences;
+
+namespace Test
+{
+    public class Test
+    {
+        private static readonly CountChars _countChars = new CountChars();
+
+        [Fact]
+        public void WhenSearchCharUsingCountThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingLinqCount(main, toFind);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingForeachThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingForeach(main, toFind);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingSpanThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingForeachSpan(main, toFind);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingIndexThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingIndex(main, toFind);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingForThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingFor(main, toFind);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingForReverseThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingForReverseIteration(main, toFind);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingForWithSpanThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingForWithSpan(main, toFind);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingSplitThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingSplit(main, toFind.ToString());
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchStringUsingSplitThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            string toFind = "Mary";
+
+            int actual = _countChars.CountCharsUsingSplit(main, toFind);
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingReplaceThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingReplace(main, toFind.ToString());
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchStringUsingReplaceThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            string toFind = "Mary";
+
+            int actual = _countChars.CountCharsUsingReplace(main, toFind);
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void WhenSearchCharUsingRegexThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+
+            int actual = _countChars.CountCharsUsingRegex(main, toFind.ToString());
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void WhenSearchStringUsingRegexThenReturnNumberOfOccurences()
+        {
+            string main = "Mary Had A Little Lamb";
+            string toFind = "Mary";
+
+            int actual = _countChars.CountCharsUsingRegex(main, toFind);
+
+            Assert.Equal(1, actual);
+        }
+    }
+}
