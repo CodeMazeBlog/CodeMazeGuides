2b8893226cd59bbb6e73759783d4518f2edb5495
Soundar Anbalagan <soundaranbu@gmail.com>
2022-09-04 23:20:27 +0530

    CM-570 Automapper vs Mapster in .NET

---
+++ dotnet-client-libraries/AutomapperVsMapster/AutomapperVsMapster/AttributeMapping/Mapster/MapsterAttributeMapping.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using Mapster;
+
+namespace AutomapperVsMapster.AttributeMapping.Mapster;
+public class MapsterAttributeMapping
+{
+    public static UserDto Map(User source)
+    {
+        var destination = source.Adapt<UserDto>();
+
+        return destination;
+    }
+
+    public static User GetSource()
+    {
+        var faker = new Faker<User>()
+            .Rules((f, o) =>
+            {
+                o.Id = f.Random.Number();
+                o.FirstName = f.Name.FirstName();
+                o.LastName = f.Name.LastName();
+                o.Email = f.Person.Email;
+                o.CreatedAt = DateTime.Now;
+            });
+        return faker.Generate();
+    }
+}
---
+++ dotnet-client-libraries/AutomapperVsMapster/AutomapperVsMapster/SimpleTypeMapping/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomapperVsMapster.SimpleTypeMapping;
+public class UserDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public string Email { get; set; } = null!;
+    public string CreatedAt { get; set; } = null!;
+}
