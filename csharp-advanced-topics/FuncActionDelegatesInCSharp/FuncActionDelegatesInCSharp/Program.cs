0d9920b1be1eb80927c8936e1a31a7f921b7d447
rmpersad <ricardo.persad@gmail.com>
2022-09-13 01:22:44 -0400

    Adding inital files for supporting article.

---
+++ csharp-advanced-topics/FuncActionDelegatesInCSharp/FuncActionDelegatesInCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Action<string> actionmessage = DisplayCodeMazeMessage;
+        actionmessage.Invoke("CodeMaze is best source of C# action delegate info online.");
+
+        Func<string> funcmessage = GetCodeMazeMessage;
+        Console.WriteLine(funcmessage.Invoke());
+    }
+
+    static void DisplayCodeMazeMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    static string GetCodeMazeMessage()
+    {
+        return ("CodeMaze is best source of C# func delegate info online.");
+    }
+}
---
+++ csharp-advanced-topics/FuncActionDelegatesInCSharp/Tests/Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class FuncActionDelegatesInCSharpUnitTest
+    {
+
+        static void DisplayCodeMazeMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static string GetCodeMazeMessage()
+        {
+            return ("CodeMaze is best source of C# func delegate info online.");
+        }
+
+        [TestMethod]
+        public void whenMessageSent_FuncDelegateExecutesTheReferencedMethod()
+        {
+            Func<string> funcmessage = GetCodeMazeMessage;
+
+            var result = funcmessage.Invoke();
+            Assert.AreEqual("CodeMaze is best source of C# func delegate info online.", result);
+        }
+    }
+}
