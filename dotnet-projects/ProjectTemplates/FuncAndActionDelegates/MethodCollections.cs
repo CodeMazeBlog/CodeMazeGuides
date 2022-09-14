87850d4f7cd8086401a556388b04293fdebb27c9
diana.iminza <diana.iminza@ronforddigital.com>
2022-09-14 14:21:01 +0300

    FuncAndActionDelegates

---
+++ dotnet-projects/ProjectTemplates/FuncAndActionDelegates/MethodCollections.cs
@@ -0,0 +1,24 @@
+﻿namespace FuncAndActionDelegates
+{
+    public class MethodCollections
+    {
+        //Methods that takes parameters but returns nothing:
+public static void PrintNumbers(int number){
+Console.WriteLine($"The number is {number}");
+}
+public static void MultiplyNumbers(int number){
+    number *=2;
+Console.WriteLine($"The multiply number is {number}");
+}
+       
+        //Methods that takes parameters and returns a value:
+
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        
+    }
+    
+}
---
+++ dotnet-projects/ProjectTemplates/FuncAndActionDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+class Program 
+{
+    static void Main(string[] args){
+   var number=3;
+PrintNumbers(number);
+MultiplyNumbers(number);
+    }
+public static void PrintNumbers(int number){
+Console.WriteLine($"The number is {number}");
+}
+public static void MultiplyNumbers(int number){
+    number *=2;
+Console.WriteLine($"The multiply number is {number}");
+}
+
+}
