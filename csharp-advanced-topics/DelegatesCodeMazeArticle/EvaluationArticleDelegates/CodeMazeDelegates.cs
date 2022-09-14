c278d3b636f6bb9680b3e7d48824c3cb0be8245f
Syed Zohair Ahmed <zohair1991@gmail.com>
2022-09-14 05:03:19 +0500

    Delegates in C#

---
+++ csharp-advanced-topics/DelegatesCodeMazeArticle/EvaluationArticleDelegates/CodeMazeDelegates.cs
@@ -0,0 +1,36 @@
+﻿public class CodeMazeDelegates
+{
+    public double ReturnsTheTotalSum(int x, float y, double z)
+    {
+        if (x <= 0 || y <= 0 || z <= 0)
+        {
+            return 0;
+        }
+
+        return x + y + z;
+    }
+
+    public void PrintsTheTotalSum(int x, float y, double z)
+    {
+        if (x <= 0 || y <= 0 || z <= 0)
+        {
+            throw new ArgumentException("All the input values must be greater than 0");
+        }
+
+        Console.WriteLine("The Total Sum is : {0}", x + y + z);
+    }
+
+    static void Main(string[] args)
+    {
+        CodeMazeDelegates codeMazeObject = new CodeMazeDelegates();
+
+        Func<int, float, double, double> funcDelegate = codeMazeObject.ReturnsTheTotalSum;
+        double result = funcDelegate.Invoke(15, 10.5f, 200.50);
+        Console.WriteLine("The Total Sum is : {0}", result);
+
+        Action<int, float, double> actionDelegate = codeMazeObject.PrintsTheTotalSum;
+        actionDelegate.Invoke(150, 10.5f, 200.50);
+
+        Console.ReadLine();
+    }
+}
---
+++ csharp-advanced-topics/DelegatesCodeMazeArticle/Tests/DelegatesUnitTest.cs
@@ -0,0 +1,45 @@
+namespace Tests
+{
+    public class DelegatesUnitTest
+    {
+        private CodeMazeDelegates _codeMazeDelegates { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            _codeMazeDelegates = new CodeMazeDelegates();
+        }
+
+        [TestCase(10, 25.50f, 10.50)]
+        [TestCase(14, 65.50f, 40.50)]
+        [TestCase(85, 15.50f, 5.2)]
+        public void WhenFetchingTotalSum_ThenCheckForZeroInputValue(int x, float y, double z)
+        {
+            //Act
+            Func<int, float, double, double> funcDelegate = _codeMazeDelegates.ReturnsTheTotalSum;
+            var totalSum = funcDelegate.Invoke(x, y, z);
+
+            //Assert
+            Assert.AreEqual((x + y + z), totalSum);
+        }
+
+        [TestCase(10, 10.50f, 2.50)]
+        [TestCase(20, 50.50f, 5.50)]
+        public void WhenPrintingTotalSum_ThenCheckForZeroInputValue(int x, float y, double z)
+        {
+            try
+            {
+                //Act
+                Action<int, float, double> funcDelegate = _codeMazeDelegates.PrintsTheTotalSum;
+                funcDelegate.Invoke(x, y, z);
+
+                //Assert
+                Assert.Pass();
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
