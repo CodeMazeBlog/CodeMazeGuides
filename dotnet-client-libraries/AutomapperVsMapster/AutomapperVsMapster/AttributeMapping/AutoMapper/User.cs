3404566099fc7891b8d3dcb25ab9e86d15143fd1
Soundar Anbalagan <SOUNDARANBU@GMAIL.COM>
2022-09-15 14:57:05 +0530

    CM-570: Automapper vs Mapster in .NET (#601)

---
+++ dotnet-client-libraries/AutomapperVsMapster/AutomapperVsMapster/AttributeMapping/AutoMapper/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomapperVsMapster.AttributeMapping.AutoMapper;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
---
+++ dotnet-client-libraries/AutomapperVsMapster/AutomapperVsMapster/Flattened/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomapperVsMapster.Flattened;
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public Address Address { get; set; } = null!;
+    public string GetFullName() => $"{FirstName} {LastName}";
+}
