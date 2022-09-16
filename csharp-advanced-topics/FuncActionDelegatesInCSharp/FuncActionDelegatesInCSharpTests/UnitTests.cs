7a35948fbdc070dac3c8a394039a7ac910908bc1
rmpersad <ricardo.persad@gmail.com>
2022-09-16 16:19:22 -0400

    Updating framework to 6

---
+++ csharp-advanced-topics/FuncActionDelegatesInCSharp/FuncActionDelegatesInCSharpTests/UnitTests.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FuncActionDelegatesInCSharpUnitTest
+    {
+
+        static void DisplayCodeMazeMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static string GetCodeMazeMessage()
+        {
+            return ("CodeMaze is best source of C# func delegate info online.");
+        }
+
+        [TestMethod]
+        public void whenMessageSent_FuncDelegateExecutesTheReferencedMethod()
+        {
+            Func<string> funcmessage = GetCodeMazeMessage;
+
+            var result = funcmessage.Invoke();
+            Assert.AreEqual("CodeMaze is best source of C# func delegate info online.", result);
+        }
+    }
+}
