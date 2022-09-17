d91f000ad96192f34640ac1132394b9d2541f0a0
Marko Hrncic <marko.hrncic@kogitas.com>
2022-09-17 13:24:22 +0200

    CM-81 Implemented unit tests.

---
+++ csharp-intermediate-topics/DtoVsPoco/Tests/Tests.cs
@@ -0,0 +1,31 @@
+using DtoVsPoco.Dtos;
+using DtoVsPoco.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private readonly IPersonService _personService = new PersonService();
+
+        [Fact]
+        public async Task WhenGettingPersonInfoData_ThenReturnNotEmptyCollectionOfPersonInfoDtos()
+        {
+            var personInfoData = await _personService.GetPersonInfoData();
+
+            Assert.True(personInfoData is ICollection<PersonInfo>);
+            Assert.NotEmpty(personInfoData);
+        }
+
+        [Fact]
+        public async Task WhenGettingPersonDetailsData_ThenReturnNotEmptyCollectionOfPersonDetailsDtos()
+        {
+            var personDetailsData = await _personService.GetPersonDetailsData();
+
+            Assert.True(personDetailsData is ICollection<PersonDetails>);
+            Assert.NotEmpty(personDetailsData);
+        }
+    }
+}
