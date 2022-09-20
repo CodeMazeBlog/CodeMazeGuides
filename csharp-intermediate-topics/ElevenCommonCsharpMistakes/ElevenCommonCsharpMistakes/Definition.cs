bd0d8e176b813640c33cb3ba4f8f500b4f960799
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-09-20 19:46:41 +0100

    Added Code files

---
+++ csharp-intermediate-topics/ElevenCommonCsharpMistakes/ElevenCommonCsharpMistakes/Definition.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenCommonCsharpMistakes
+{
+    public class Definitions
+    {
+    }
+    public enum Taste
+    {
+        Sugary,
+        Bitter,
+        Sour
+    }
+
+    public class Drink
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Color { get; set; }
+        public string Taste { get; set; }
+
+        public void Break()
+        {
+            Console.WriteLine("Broken into pieces");
+        }
+
+        public void Evaporate()
+        {
+            Console.WriteLine("Eveporating into thin air");
+        }
+    }
+
+    public class Bottle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public void Break()
+        {
+            Console.WriteLine("Broken into pieces");
+        }
+    }
+
+    public class Wine
+    {
+        public string Color { get; set; }
+        public string Taste { get; set; }
+        public void Evaporate()
+        {
+            Console.WriteLine("Eveporating into thin air");
+        }
+    }
+
+    public class WineBottle
+    {
+        public Wine Wine { get; set; }
+        public Bottle Bottle { get; set; }
+    }
+}
