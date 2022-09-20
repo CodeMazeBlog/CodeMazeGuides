99b6426e20e3ab10d2b9d98fc9f1330702a71a29
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-09-20 18:46:55 +0100

    Added Main Code

---
+++ csharp-intermediate-topics/Common Mistakes in CSharp/CommonMistakesInCsharp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommonMistakesInCsharp;
+
+Console.WriteLine("Hello, World!");
+Application.MisplaceTypes();
+Application.OverLookingExtensionTypes();
+MultipurposeMethod.JackOfAllTrade();
+MultipurposeMethod.HandleUser();
+Application.StringConcantenation();
+Application.ManualIterations();
