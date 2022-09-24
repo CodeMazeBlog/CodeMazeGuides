386eeb7442bd67a135c84ed9f84ac5346572c02a
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2022-09-24 11:37:40 +0530

    CM-553: Code for the introductory microservices article
    CM-553: Code for the introductory microservices article

---
+++ dotnet-microservices/Introduction to Microservices Architecture in ASP.NET Core/CodeMazeShop/CodeMazeShop.WebClient/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using CodeMazeShop.WebClient.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeMazeShop.WebClient.Controllers;
+
+public class OrderController : Controller
+{
+    
+    private readonly CookieSettings _cookieSettings;
+
+    public OrderController(CookieSettings cookieSettings)
+    {
+        
+        _cookieSettings = cookieSettings;
+    }
+
+    public IActionResult Index()
+    {      
+        return View(new OrderViewModel());
+    }
+}
---
+++ dotnet-microservices/Introduction to Microservices Architecture in ASP.NET Core/CodeMazeShop/CodeMazeShop.WebClient/Controllers/ProductCatalogController.cs
@@ -0,0 +1,46 @@
+﻿using CodeMazeShop.WebClient.Models;
+using CodeMazeShop.WebClient.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeMazeShop.WebClient.Controllers;
+
+public class ProductCatalogController : Controller
+{
+    private readonly IProductCatalogService _productCatalogService;    
+    private readonly CookieSettings _cookieSettings;
+
+    public ProductCatalogController(IProductCatalogService productCatalogService, CookieSettings cookieSettings)
+    {
+        _productCatalogService = productCatalogService;        
+        _cookieSettings = cookieSettings;
+    }
+    public async Task<IActionResult> Index(Guid categoryId)
+    {
+        var getCategories = _productCatalogService.GetCategories();
+
+        var getProducts = categoryId == Guid.Empty ? _productCatalogService.GetAll() :
+            _productCatalogService.GetByCategoryId(categoryId);        
+
+        await Task.WhenAll(new Task[] { getCategories, getProducts });
+
+        return View(new ProductListModel
+        {
+            Products = getProducts.Result,
+            Categories = getCategories.Result,
+            SelectedCategory = categoryId,
+            NumberOfItems = 0
+        });
+    }
+
+    [HttpPost]
+    public IActionResult SelectCategory([FromForm] Guid selectedCategory)
+    {
+        return RedirectToAction("Index", new { categoryId = selectedCategory });
+    }
+
+    public async Task<IActionResult> Detail(Guid productId)
+    {
+        var product = await _productCatalogService.GetProduct(productId);
+        return View(product);
+    }
+}
