5824d15d51f4b28ce55d7917699518dedf8ae8fb
Serigne Saliou Dioum <sdioum@expertime.com>
2022-10-07 10:55:48 +0200

    Fix: Move my project from the root folder to the folder csharp-advanced-topics

--- FanIn-Fan-out/FanIn-Fan-out.Functions/Configurations/Options/DatabaseOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FanIn_Fan_out.Functions.Configurations.Options;
-
-public class ConfigurationSections
-{
-    public static readonly string Database = "DefaultConnection";
-
-    public static readonly string FeatureFlagsOptions = "FeatureFlags";
-}
