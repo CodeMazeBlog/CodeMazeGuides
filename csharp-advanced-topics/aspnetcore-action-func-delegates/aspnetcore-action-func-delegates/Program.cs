9f3b847726b36633703707006efba6c3763cee56
Serigne Saliou Dioum <sdioum@expertime.com>
2022-10-07 11:18:04 +0200

    Feat: Func and Action Delegate in C#

---
+++ csharp-advanced-topics/aspnetcore-action-func-delegates/aspnetcore-action-func-delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aspnetcore_action_func_delegates.ActionDelegate;
+using aspnetcore_action_func_delegates.DelegateClass;
+using aspnetcore_action_func_delegates.FuncDelegate;
+
+Console.WriteLine("Project to explain the delegate type");
+
+// Test the delegate class
+Console.WriteLine("\n\n-------------------------------------");
+MyDelegateClass.MainDelegateClass();
+
+// Test the delegate action
+Console.WriteLine("\n\n-------------------------------------");
+MyDelegateAction.MainAction();
+
+// Test the delegate func
+Console.WriteLine("\n\n-------------------------------------");
+MyDelegateFunc.MainFunc();
