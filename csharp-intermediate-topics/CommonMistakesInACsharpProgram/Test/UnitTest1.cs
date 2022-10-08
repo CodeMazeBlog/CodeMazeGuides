3f9a9d7c9ac833750f9165206dbd327834d39754
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-10-08 06:17:25 +0100

    Added Unit Test to Project

---
+++ csharp-intermediate-topics/CommonMistakesInACsharpProgram/Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+using CommonMistakesInACsharpProgram;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void GivenManualIteratons_WhenExecuted_ReturnsIEnumerable()
+        {
+            var result = Application.ManualIterations();
+            Assert.IsType<List<int>?>(result);
+        }
+        [Fact]
+        public void GivenMisplaceTypes_WhenExecuted_ReturnsBool()
+        {
+            var result = Application.MisplaceTypes();
+            Assert.IsType<bool>(result);
+        }
+        [Fact]
+        public void GivenOverLookingExtensionTypes_WhenExecutedWithTrueOrFalse_ReturnsString()
+        {
+            var result = Application.OverLookingExtensionTypes();
+            Assert.IsType<string>(result);
+        }
+        [Fact]
+        public void GivenStringConcantenation_WhenExecutedWithTrueOrFalse_ReturnsString()
+        {
+            var result = Application.StringConcantenation();
+            Assert.IsType<string>(result);
+        }
+    }
+}
