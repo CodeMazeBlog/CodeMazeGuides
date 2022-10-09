973d02d34d9cfbd43a3334780484a9543d56b97a
Phil <philbroderick12@gmail.com>
2022-10-09 07:25:17 +0100

    Remove template code

--- aspnetcore-features/Localization/Localization/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Localization.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- aspnetcore-features/Localization/Localization/Program.cs
+++ aspnetcore-features/Localization/Localization/Program.cs
@@ -32,7 +32,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -48,6 +47,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Localization}/{action=Index}/{id?}");
 
 app.Run();
