f03e34a14a58f7a6f83be49950292e220ae79dbe
Anurag <anurag3487@gmail.com>
2022-10-09 21:24:12 +0530

    CM 574: What is IndexOutOfRangeException in C#? (#576)

---
+++ csharp-exceptions/IndexOutOfRangeExceptionSolution/IndexOutOfRangeExceptionProject/Repository/StaticDataRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace IndexOutOfRangeExceptionProject.Repository
+{
+    public class StaticDataRepository
+    {
+        public static DataTable GetGroceries()
+        {
+            var dataTable = new DataTable
+            {
+                TableName = "Groceries"
+            };
+            dataTable.Columns.Add("Id", typeof(int));
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("Description", typeof(string));
+
+            dataTable.Rows.Add(1, "Macaroni", "Tasty Pasta");
+            dataTable.Rows.Add(2, "Ramen", "Tasty Noodles");
+            dataTable.Rows.Add(3, "Figaro Oil", "Olive Oil");
+            dataTable.Rows.Add(4, "XYZ Lentils", "Nutritious Pulses");
+
+            return dataTable;
+        }
+    }
+}
