4947a9a9680fef78f43ea5237c34d1364d4ef497
Muhammed Saleem <Muhammed.P@gds.ey.com>
2022-10-17 18:12:01 +0530

    CHanges for CM-4 - IComparable vs IComparer vs Comparison Delegate through examples

---
+++ csharp-intermediate-topics/ObjectComparison/ObjectComparison/Employee.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+
+namespace ObjectComparisons
+{
+    public class Employee : IComparable
+    {
+        public Employee(int id, string? name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            Employee? employee = obj as Employee;
+            return Id.CompareTo(employee?.Id);
+        }
+
+        private class SortByIdAscendingHelper : IComparer
+        {
+            public int Compare(object? x, object? y)
+            {
+                if ((x == null) || (y == null))
+                {
+                    return 0;
+                }
+
+                Employee? employee1 = x as Employee;
+                Employee? employee2 = y as Employee;
+
+                int? value1 = employee1?.Id;
+                int? value2 = employee2?.Id;
+
+                int v1 = value1.HasValue ? value1.Value : 0;
+                int v2 = value2.HasValue ? value2.Value : 0;
+
+                return v1.CompareTo(v2);
+            }
+        }
+
+        private class SortByIdDescendingHelper : IComparer
+        {
+            public int Compare(object? x, object? y)
+            {
+                if ((x == null) || (y == null))
+                {
+                    return 0;
+                }
+
+                Employee? employee1 = x as Employee;
+                Employee? employee2 = y as Employee;
+
+                int? value1 = employee1?.Id;
+                int? value2 = employee2?.Id;
+
+                int v1 = value1.HasValue ? value1.Value : 0;
+                int v2 = value2.HasValue ? value2.Value : 0;
+
+                return v2.CompareTo(v1);
+            }
+        }
+
+        public static IComparer SortByIdAscending()
+        {
+            return (IComparer)new SortByIdAscendingHelper();
+        }
+
+        public static IComparer SortByIdDescending()
+        {
+            return (IComparer)new SortByIdDescendingHelper();
+        }
+
+        public static int CompareEmployeesByIdAscending(Employee employee1, Employee employee2)
+        {
+            if ((employee1 == null) || (employee2 == null))
+            {
+                return 0;
+            }
+
+            int? value1 = employee1?.Id;
+            int? value2 = employee2?.Id;
+
+            int v1 = value1 ?? 0;
+            int v2 = value2 ?? 0;
+
+            return v1.CompareTo(v2);
+        }
+
+        public static int CompareEmployeesByIdDescending(Employee employee1, Employee employee2)
+        {
+            if ((employee1 == null) || (employee2 == null))
+            {
+                return 0;
+            }
+
+            int? value1 = employee1?.Id;
+            int? value2 = employee2?.Id;
+
+            int v1 = value1 ?? 0;
+            int v2 = value2 ?? 0;
+
+            return v2.CompareTo(v1);
+        }
+    }
+}
---
+++ csharp-intermediate-topics/ObjectComparison/ObjectComparison/Program.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Running;
+using ObjectComparisons;
+
+Employee[] employees = new Employee[5]
+    {
+        new Employee(4, "John"),
+        new Employee(2, "Tom"),
+        new Employee(1, "Eric"),
+        new Employee(5, "Dan"),
+        new Employee(3, "Alen")
+    };
+
+Console.WriteLine("The Employee Array:");
+
+PrintEmployees(employees);
+
+Array.Sort(employees);
+
+Console.WriteLine("\nDefault Sorting:");
+
+PrintEmployees(employees);
+
+Array.Sort(employees, Employee.SortByIdAscending());
+
+Console.WriteLine("\nSorting by Id Ascending");
+
+PrintEmployees(employees);
+
+Array.Sort(employees, Employee.SortByIdDescending());
+
+Console.WriteLine("\nSorting by Id Descending");
+
+PrintEmployees(employees);
+
+List<Employee> employeeList = new List<Employee>
+    {
+        new Employee(4, "John"),
+        new Employee(2, "Tom"),
+        new Employee(1, "Eric"),
+        new Employee(5, "Dan"),
+        new Employee(3, "Alen")
+    };
+
+employeeList.Sort(Employee.CompareEmployeesByIdAscending);
+
+Console.WriteLine("\nSorting by Id Ascending using Comparison Delegate");
+
+PrintEmployeeList(employeeList);
+
+employeeList.Sort(Employee.CompareEmployeesByIdDescending);
+
+Console.WriteLine("\nSorting by Id Descending using Comparison Delegate");
+
+PrintEmployeeList(employeeList);
+
+BenchmarkRunner.Run<ObjectComparisonBenchmark>();
+
+static void PrintEmployees(Employee[] employees)
+{
+    foreach (var employee in employees)
+    {
+        Console.WriteLine(employee.Id + "\t\t" + employee.Name);
+    }
+}
+
+static void PrintEmployeeList(List<Employee> employees)
+{
+    foreach (var employee in employees)
+    {
+        Console.WriteLine(employee.Id + "\t\t" + employee.Name);
+    }
+}
