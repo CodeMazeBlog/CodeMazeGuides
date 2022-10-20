28962c3252e6cbb9d3be1958d3bde7a7fdad1a29
Praveen Prathipati <pprathipati@areteir.com>
2022-10-20 19:44:57 +0530

    Action and Func Delegates in C#

---
+++ csharp-advanced-topics/ActionAndFuncInCSharp/ActionAndFuncInCSharp.Tests/FuncTests.cs
@@ -0,0 +1,42 @@
+using ActionAndFuncInCSharp;
+
+namespace ActionAndFunc.Tests;
+
+public class FuncTests
+{
+    [Fact]
+    public void GivenPositiveNumber_ShouldReturnNotNegativeAndNotZeroErrors()
+    {
+        var number = 10;
+        var sut = new RuleEngine();
+
+        var result = sut.ExecuteRules(number);
+
+        Assert.Contains(result, x => x == "Given number is not Negative");
+        Assert.Contains(result, x => x == "Given number is not Zero");
+    }
+
+    [Fact]
+    public void GivenNegativeNumber_ShouldReturnNotPositiveAndNotZeroErrors()
+    {
+        var number = -10;
+        var sut = new RuleEngine();
+
+        var result = sut.ExecuteRules(number);
+
+        Assert.Contains(result, x => x == "Given number is not Positive");
+        Assert.Contains(result, x => x == "Given number is not Zero");
+    }
+
+    [Fact]
+    public void GivenZeroNumber_ShouldReturnNotPositiveAndNotNegativeErrors()
+    {
+        var number = 0;
+        var sut = new RuleEngine();
+
+        var result = sut.ExecuteRules(number);
+
+        Assert.Contains(result, x => x == "Given number is not Negative");
+        Assert.Contains(result, x => x == "Given number is not Positive");
+    }
+}
--- csharp-advanced-topics/ActionAndFuncInCSharp/ActionAndFuncInCSharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncInCSharp/ActionAndFuncInCSharp/Program.cs
@@ -1,18 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-//override the defaults
 builder.Services.AddResponseCaching(options => { options.SizeLimit = 50 * 1024 * 1024; });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,8 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
