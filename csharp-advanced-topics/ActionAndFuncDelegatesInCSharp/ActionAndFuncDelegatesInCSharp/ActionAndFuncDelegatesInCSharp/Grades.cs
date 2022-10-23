c52a1f749e47b3010f89607222c293e8fe8dd9af
Ainea Wabwoba <aineah28@gmail.com>
2022-10-23 23:23:32 +0300

    added delegate implementation

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Grades.cs
@@ -0,0 +1,18 @@
+﻿namespace ActionAndFuncDelegatesInCSharp
+{
+    public class Grades
+    {
+        public string CalculateGradeFromTotalMarks(int totalMarks)
+        {
+            if (totalMarks >= 200)
+                return "A";
+            else if (totalMarks >= 150)
+                return "B";
+            else if (totalMarks >= 100)
+                return "C";
+            else
+                return "D";
+
+        }
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using ActionAndFuncDelegatesInCSharp;
+
+var marks = new List<int> { 50, 50, 45 };
+
+string CalculateGrade(Func<int, string> calculateGrade)
+{
+    var total = marks.Sum();
+    var grade = calculateGrade(total);
+    Console.WriteLine($"Your grade is: {grade}");
+    return grade;
+}
+
+
+var grades = new Grades();
+CalculateGrade(grades.CalculateGradeFromTotalMarks);
