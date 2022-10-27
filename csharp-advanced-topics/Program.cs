1d4b750fc04a639cc77cdf7c7987d16cd724cbf0
shilpsgithub <85281151+shilpsgithub@users.noreply.github.com>
2022-10-27 20:12:15 +0530

    Project file and Code file for evaluation article.
    Evaluation Article - Func and Action Delegates in C#

---
+++ csharp-advanced-topics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EvaluationDemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, string> func = AppendText; //Func delegate declaration
+            string AppendedName = func("Code", "Maze"); //invoking the delegate
+            Console.WriteLine(AppendedName);
+            Console.ReadLine();
+
+            Action<string, string> writeDelegate = AppendPrint; //Action delegate declaration
+            writeDelegate("Evaluation", "Article"); //invoking the delegate
+
+        }
+        static string AppendText(string firstText, string lastText)
+        {
+          return firstText + " " + lastText;
+        }
+
+        static void AppendPrint(string first, string last)
+        {
+            string fullText = first + " " + last;
+            Console.WriteLine($"{fullText}");
+        }
+    }
+}
