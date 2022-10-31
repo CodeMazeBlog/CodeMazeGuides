89daa82225182addf9002e6f43c70cf3e1274b18
shilpsgithub <85281151+shilpsgithub@users.noreply.github.com>
2022-10-31 20:40:55 +0530

    Test Project Files

--- csharp-advanced-topics/DelegatesInCsharp/CSharpFuncActionDelegates/TestProject1/UnitTest1.cs
+++ csharp-advanced-topics/DelegatesInCsharp/CSharpFuncActionDelegates/TestProject1/UnitTest1.cs
@@ -1,1 +1,29 @@
-testFile
+namespace TestProject1
+{
+
+
+    public class Tests
+    {
+        public static void AppendPrint(string first, string last) => Console.WriteLine($"{first + " " + last}");
+        public static string AppendText(string firstText, string lastText)
+        {
+
+            return firstText + " " + lastText;
+        }
+        [Test]
+        public void whenStringsAreSent_DelegateExecutesAppendTextMethod()
+        {
+            Func<string, string, string> func = AppendText; 
+            var result = func("South", "Africa");
+            Assert.Pass(result);
+        }
+
+        [Test]
+        public void whenStringsAreSent_DelegateExecutesAppendPrint()
+        {
+            Action<string,string> writeDelegate = AppendPrint;
+            writeDelegate("United", "Kingdom");
+            Assert.Pass();
+        }
+    }
+}
