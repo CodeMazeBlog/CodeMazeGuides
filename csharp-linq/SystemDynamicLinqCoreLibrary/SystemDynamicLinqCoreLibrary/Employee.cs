1d3e448ef4490b6497ad93f3232aa890981f2aea
Ainea Wabwoba <aineah28@gmail.com>
2022-11-02 22:15:55 +0300

    added dynamic queries

---
+++ csharp-linq/SystemDynamicLinqCoreLibrary/SystemDynamicLinqCoreLibrary/Employee.cs
@@ -0,0 +1,67 @@
+﻿namespace SystemDynamicLinqCoreLibrary;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; }
+    public int Age { get; set; }
+    public string Residence { get; set; }
+
+    public IQueryable<Employee> GetEmployees()
+    {
+        return new[]
+        {
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "John Doe",
+                Department = "IT",
+                Age = 25,
+                Residence = "Malibu"
+            },new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Jane Doe",
+                Department = "Finance",
+                Age = 30,
+                Residence = "Prague"
+            },new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Quincy Nigellus",
+                Department = "Finance",
+                Age = 28,
+                Residence = "Birmingham"
+            },new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Sharron Zachary",
+                Department = "IT",
+                Age = 23,
+                Residence = "London"
+            },new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Kristia Cletis",
+                Department = "Human Resource",
+                Age = 26,
+                Residence = "Melbourne"
+            },new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Chrissie Elihu",
+                Department = "Human Resource",
+                Age = 27,
+                Residence = "Dakar"
+            },new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Businge Missy",
+                Department = "Hospitality",
+                Age = 29,
+                Residence = "Kampala"
+            }
+        }.AsQueryable();
+    }
+}
--- csharp-linq/SystemDynamicLinqCoreLibrary/SystemDynamicLinqCoreLibrary/Program.cs
+++ csharp-linq/SystemDynamicLinqCoreLibrary/SystemDynamicLinqCoreLibrary/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq.Dynamic.Core;
+using SystemDynamicLinqCoreLibrary;
+
+var employee = new Employee();
+var employees = employee.GetEmployees();
+
+var itDepartmentEmployees = employees.Where("Department == @0", "IT").ToList();
+
+var itEmployees = employees.Where("Department == \"IT\"").ToList();
+
+var itEmployeesList = employees.Where("c => c.Department = \"IT\"").ToList();
+
+var relevantData = employees.Select("new {Name, Department}").ToDynamicList();
+
+var sortedData = employees.OrderBy("Name").ToList();
+
+var multipleSortedData = employees.OrderBy("Department desc, Name").ToList();
+
+foreach (var itDepartmentEmployee in multipleSortedData)
+{
+    Console.WriteLine($"Name: {itDepartmentEmployee.Name}, Dept: {itDepartmentEmployee.Department}");
+}
