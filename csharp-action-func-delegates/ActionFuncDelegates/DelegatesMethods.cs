f26c77e246603636bf4ef219c5af3e72e74b52be
ajaym <ajaym@evisionits.com>
2022-11-03 23:14:38 +0530

    Action Func Delegates example...

---
+++ csharp-action-func-delegates/ActionFuncDelegates/DelegatesMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionFuncDelegates
+{
+    public class DelegatesMethods
+    {
+        //Methods that takes parameters but returns nothing:
+        public static void PrintText()
+        {
+            Console.WriteLine("Text Printed with the help of Action delegates!");
+        }
+        public static void PrintNumbers(int startNumber, int EndNumber)
+        {
+            for (int i = startNumber; i < EndNumber; i++)
+        {
+                Console.Write("{0}", i);
+            }
+            Console.WriteLine();
+        }
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //Methods that takes parameters and returns a value:
+
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static string DisplayAddition(int a, int b)
+        {
+            return string.Format("Addition of {0} and {1} is {2}", a, b, a + b);
+        }
+
+        public static string FullName(string firstName, string lastName)
+        {
+            return string.Format("Your Name is {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ csharp-action-func-delegates/ActionFuncDelegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using ActionFuncDelegates;
+
+Action printText = new Action(DelegatesMethods.PrintText);
+Action<string> print = new Action<string>(DelegatesMethods.Print);
+Action<int, int> printNumber = new Action<int, int>(DelegatesMethods.PrintNumbers);
+
+Func<int, int, int> add1 = new Func<int, int, int>(DelegatesMethods.Addition);
+Func<int, int, string> add2 = new Func<int, int, string>(DelegatesMethods.DisplayAddition);
+Func<string, string, string> completeName = new Func<string, string, string>(DelegatesMethods.FullName);
+
+Console.WriteLine("\n***************** Action<> Delegate Methods ***************\n");
+
+printText();            //Parameter: 0 , Returns: nothing
+print("Mathasuriya");   //Parameter: 1 , Returns: nothing
+printNumber(50, 40);    //Parameter: 2 , Returns: nothing
+Console.WriteLine();
+
+Console.WriteLine("**************** Func<> Delegate Methods *****************\n");
+
+int addition = add1(50, 10);                            //Parameter: 2 , Returns: int
+string addition2 = add2(10, 20);                        //Parameter: 2 , Returns: string
+string name = completeName("Ajay", "Mathasuriya");      //Parameter:2 , Returns: string
+
+Console.WriteLine("Addition: {0}", addition);
+Console.WriteLine(addition2);
+Console.WriteLine(name);
+
+Console.ReadLine();
