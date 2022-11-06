f176b5547cd2e7fb746436f6dc8dba0862821c64
rushangp1 <36847519+rushangp1@users.noreply.github.com>
2022-11-06 19:51:49 +0530

    Action and Func Article Data

---
+++ csharp-advanced-topics/ActionAndFuncDelegate/ActionAndFuncDelegate/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegate
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Employee(int employeeId, string firstName, string lastName)
+        {
+            this.EmployeeId = employeeId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public void Print(Action<string> printerMethod)
+        {
+            printerMethod.Invoke($"{this.EmployeeId},{this.FirstName},{this.LastName}");
+        }
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegate/ActionAndFuncDelegate/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ActionAndFuncDelegate
+{
+    public class Program
+    {
+        static void PrintToFile(string content)
+        {
+            File.AppendAllLines("EmployeeData.txt", new List<string>() { content });
+        }
+
+        static int Sum(int x,int y)
+        {
+            return x + y;
+        }
+
+        public static void Main(string[] args)
+        {
+            #region ActionDemo
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee(1, "James", "Scott"));
+            employees.Add(new Employee(2, "Mihir", "Patel"));
+            employees.Add(new Employee(3, "Vickey", "Sharma"));
+
+            Action<string> printer = new Action<string>(PrintToFile);
+
+            printer += (x) => Console.WriteLine(x);
+
+            foreach (var employee in employees)
+            {
+                employee.Print(printer);
+            }
+
+            #endregion
+
+            #region FuncDemo
+
+            Func<int, int, int> sumCalculator = new Func<int, int, int>(Sum);
+
+            Console.WriteLine(sumCalculator(10, 20));
+
+            #endregion
+        }
+    }
+}
