4fa2dd54bf3d13585a99b4d76cc2468aeee4ea24
Praveen <55543572+prathipatipraveen@users.noreply.github.com>
2022-11-08 19:03:03 +0530

    CM-618: How to Return null from a Generic Method in C# (#693)

---
+++ csharp-intermediate-topics/ReturnNullFromAGenericMethod/ReturnNullFromAGenericMethod/ValueTypeHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace ReturnNullFromAGenericMethod
+{
+    public static class ValueTypeHelpers
+    {
+        public static T? FindItem<T>(List<T> items, T id) where T : struct
+        {
+            return null;
+        }
+    }
+}
---
+++ csharp-intermediate-topics/ReturnNullFromAGenericMethod/Tests/ValueTypeHelpersUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using ReturnNullFromAGenericMethod;
+
+namespace Tests
+{
+    public class ValueTypeHelpersUnitTest
+    {
+        [Fact]
+        public void GivenNullableValueType_WhenNullReturned_ThenThrowInvalidOperationException()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+            var number = ValueTypeHelpers.FindItem(numbers, 6);
+
+            Assert.Null(number);
+            Assert.Throws<InvalidOperationException>(() => number.Value);
+        }
+    }
+}
