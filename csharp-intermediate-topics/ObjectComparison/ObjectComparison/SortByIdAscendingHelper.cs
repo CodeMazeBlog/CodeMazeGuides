957bdc4c76314aeb9b6aab3aedb2a84496a6e524
Muhammed Saleem <Muhammed.P@gds.ey.com>
2022-11-09 18:58:19 +0530

    replaced interfaces with generic ones

--- csharp-intermediate-topics/ObjectComparison/ObjectComparison/SortByIdAscendingHelper.cs
+++ csharp-intermediate-topics/ObjectComparison/ObjectComparison/SortByIdAscendingHelper.cs
@@ -1,24 +1,12 @@
-﻿using System.Collections;
-
-namespace ObjectComparisons
+﻿namespace ObjectComparisons
 {
-    public class SortByIdAscendingHelper : IComparer
+    public class SortByIdAscendingHelper : IComparer<Employee>
     {
-        public int Compare(object? x, object? y)
+        public int Compare(Employee? firstEmployee, Employee? secondEmployee)
         {
-            if ((x is null) || (y is null))
-            {
-                return 0;
-            }
-
-            Employee? employee1 = x as Employee;
-            Employee? employee2 = y as Employee;
-
-            int value1 = employee1?.Id ?? 0;
-            int value2 = employee2?.Id ?? 0;
-
-            return value1.CompareTo(value2);
+            return ((firstEmployee is null) || (secondEmployee is null)) ?
+                0 :
+                firstEmployee.Id.CompareTo(secondEmployee.Id);
         }
     }
-
 }
