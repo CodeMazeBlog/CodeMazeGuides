1855ad7345c5917082d8e0b955cb53e696b34d76
Phil <philbroderick12@gmail.com>
2022-11-09 17:18:00 +0000

    Initial commit

---
+++ dotnet-base-libraries/Channels/Channels/Consumer.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Channels;
+
+public class Consumer
+{
+    private readonly ChannelReader<string> _channelReader;
+
+    public Consumer(ChannelReader<string> channelReader)
+    {
+        _channelReader = channelReader;
+    }
+
+    public async Task ConsumeWorkAsync()
+    {
+        //try
+        //{
+        //    while (true)
+        //    {
+        //        var todo = await _channelReader.ReadAsync(); 
+        //        Console.WriteLine($"Completing todo: {todo}"); 
+        //        await Task.Delay(1500);
+        //    }
+        //}
+        //catch (ChannelClosedException) 
+        //{ 
+        //    Console.WriteLine("Channel was closed"); 
+        //}
+        //while (_channelReader.TryRead(out var todo))
+        //{
+        //    Console.WriteLine($"Completing todo: {todo}");
+        //    await Task.Delay(1500);
+        //}
+        await foreach (var todo in _channelReader.ReadAllAsync())
+        {
+            Console.WriteLine($"Completing todo: {todo}");
+            await Task.Delay(1500);
+        }
+
+        Console.WriteLine("All items read");
+    }
+}
---
+++ dotnet-base-libraries/Channels/Channels/Producer.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Channels;
+
+public class Producer
+{
+    private readonly ChannelWriter<string> _channelWriter;
+
+    private static readonly List<string> _todoItems = new()
+    {
+        "Make a coffee",
+        "Read CodeMaze articles",
+        "Go for a run",
+        "Eat lunch"
+    };
+
+    public Producer(ChannelWriter<string> channelWriter)
+    {
+        _channelWriter = channelWriter;
+    }
+
+    public async Task ProduceWorkAsync()
+    {
+        //foreach (var todo in _todoItems)
+        //{
+        //    while (await _channelWriter.WaitToWriteAsync())
+        //    {
+        //        var todoAdded = _channelWriter.TryWrite(todo);
+        //        if (todoAdded)
+        //        {
+        //            Console.WriteLine($"Added todo: '{todo}' to channel");
+        //            break;
+        //        }
+        //    }
+        //    await Task.Delay(500);
+        //}
+        //_channelWriter.Complete();
+
+        //foreach (var todo in _todoItems)
+        //{
+        //    if (_channelWriter.TryWrite(todo))
+        //    {   
+        //        Console.WriteLine($"Added todo: '{todo}' to channel");
+        //    }
+        //    await Task.Delay(500);
+        //}
+        //_channelWriter.Complete();
+        foreach (var todo in _todoItems)
+        {
+            await _channelWriter.WriteAsync(todo);
+            Console.WriteLine($"Added todo: '{todo}' to channel");
+            await Task.Delay(500);
+        }
+        _channelWriter.TryComplete();
+    }
+}
