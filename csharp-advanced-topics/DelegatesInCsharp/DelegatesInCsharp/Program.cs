87e36a0c76df083114fd940e48e60fef956c4f79
OldPen16 <oldpen16@gmail.com>
2022-11-10 21:26:33 +0530

    Evaluation article on Action and Func delegates in c#

--- csharp-advanced-topics/DelegatesInCsharp/DelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/DelegatesInCsharp/DelegatesInCsharp/Program.cs
@@ -2,44 +2,27 @@
 
 namespace DelegatesInCsharp
 {
-	delegate void PrintMessage(string text);
-	delegate T Print<T>(T param1);
+    public class Program
+    {
+        public static double CalculateSimpleInterest(double principalAmount, float rate, int time)
+        {
+            return (principalAmount * rate * time) / 100;
+        }
 
-	class Program
-	{
-		public static void WriteText(string text) => Console.WriteLine($"Text: {text}");
-		public static void ReverseWriteText(string text) => Console.WriteLine($"Text in reverse: {Reverse(text)}");
-		public static string ReverseText(string text) => Reverse(text);
+        public static void Print(double simpleInterest)
+        {
+            Console.WriteLine(simpleInterest);
+        }
 
-		private static string Reverse(string s)
-		{
-			char[] charArray = s.ToCharArray();
-			Array.Reverse(charArray);
-			return new string(charArray);
-		}
+        static void Main(string[] args)
+        {
+            Func<double, float, int, double> funcDelegate = CalculateSimpleInterest;
+            double simpleInterest = funcDelegate.Invoke(50000, 3.5f, 3);
 
-		static void Main(string[] args)
-		{
-			var delegate1 = new PrintMessage(WriteText);
-			var delegate2 = new PrintMessage(ReverseWriteText);
-			// with + sign
-			var multicastDelegate = delegate1 + delegate2;
+            Action<double> actionDelegate = Print;
+            actionDelegate.Invoke(simpleInterest);
 
-			// with =, +=, and -=
-			multicastDelegate = delegate1;
-			multicastDelegate += delegate2;
-
-			multicastDelegate.Invoke("Go ahead, make my day.");
-			multicastDelegate("You're gonna need a bigger boat.");
-
-			var delegate3 = new Print<string>(ReverseText);
-			Console.WriteLine(delegate3("I'll be back. iou"));
-
-			// comment out other stuff
-			Action<string> executeReverseWriteAction = ReverseWriteText;
-			executeReverseWriteAction("Are you not entertained?");
-			Func<string, string> executeReverseFunc = ReverseText;
-			Console.WriteLine(executeReverseFunc("Are you not entertained?"));
-		}
-	}
+            Console.ReadLine();
+        }
+    }
 }
