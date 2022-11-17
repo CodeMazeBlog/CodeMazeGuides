1ed140f97642dc7133633e39b16a12e0447f9123
Chayan Adhikari <adhikari.chayan@gmail.com>
2022-11-17 18:37:23 +0530

    CM-553: Introduction to Microservices Architecture in ASP.NET Core (#627)

---
+++ dotnet-microservices/Introduction to Microservices Architecture in ASP.NET Core/CodeMazeShop/CodeMazeShop.Web/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeMazeShop.Web.Entities;
+
+public class Product
+{
+    public Guid ProductId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+    
+    public int Price { get; set; }
+    
+    public string ImageUrl { get; set; }
+    
+    public Guid CategoryId { get; set; }
+    
+    public string CategoryName { get; set; }
+}
---
+++ dotnet-microservices/Introduction to Microservices Architecture in ASP.NET Core/CodeMazeShop/CodeMazeShop.WebClient/Services/IShoppingCartService.cs
@@ -0,0 +1,29 @@
+﻿using CodeMazeShop.DataContracts.ShoppingCart;
+using CodeMazeShop.DataContracts.Discount;
+using CodeMazeShop.WebClient.Models;
+
+namespace CodeMazeShop.WebClient.Services;
+
+public interface IShoppingCartService
+{
+    Task<IEnumerable<CartLineItem>> GetCartLineItemsForCart(Guid cartId);
+
+    Task<Cart> GetShoppingCart(Guid cartId);    
+
+    Task<CartLineItem> AddToCart(Guid cartId, CartLineItemForCreation cartLineItem);
+
+    Task UpdateCartLineItem(Guid cartId, CartLineItemForUpdate cartLineItemUpdate);
+
+    Task RemoveLineItem(Guid cartId, Guid cartLineItemId);
+
+    Task ApplyPromotionCouponToCart(Guid cartId, Guid couponId);
+
+    Task<PromotionCoupon> GetPromotionCoupon(Guid couponId);
+
+    Task<PromotionCoupon> GetPromotionCouponByCode(string code);
+    
+    Task UsePromotionCoupon(Guid couponId);
+
+    Task Checkout(CartCheckoutViewModel checkoutModel);
+}
+
