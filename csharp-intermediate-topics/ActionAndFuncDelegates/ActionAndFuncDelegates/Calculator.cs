fd7123713b758d3fb2a5a9bbff8da229fad6b1ab
cjaynjoku <njokujennaro@gmail.com>
2022-11-18 04:25:09 +0100

    Added spacing to return statements, and removed 'System'

--- csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Calculator.cs
+++ csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Calculator.cs
@@ -5,27 +5,32 @@ public class Calculator
     public int Sum(int num1, int num2)
     {
         var result = num1 + num2;
+
         return result;
     }
 
     public void Print()
     {
-        System.Console.WriteLine("The answer is 200");
+        Console.WriteLine("The answer is 200");
     }
+    
     public void PrintResult(int result)
     {
-        System.Console.WriteLine($"The result is {result}");
+       Console.WriteLine($"The result is {result}");
     }
 
     public int Result()
     {
         var result = Sum(5, 5);
+
         return result;
     }
 
     public int Exponent(int number)
     {
-        return number * number;
+        var result = number * number;
+
+        return result;
     }
 
 }
--- csharp-intermediate-topics/ActionAndFuncDelegates/Tests/ActionAndFuncTest.cs
+++ csharp-intermediate-topics/ActionAndFuncDelegates/Tests/ActionAndFuncTest.cs
@@ -6,24 +6,19 @@ namespace Tests
     public class ActionAndFuncTest
     {
         static readonly Calculator calc = new();
+        
         [Fact]
-        public void GivenResultDelegate_WhenExecuted_ReturnsTheSameValueAsResult()
+        public void GivenResult_WhenExecuted_ReturnsAnInteger()
         {
-            Func<int> resultDelegate = calc.Result;
             var result = calc.Result();
-            var delegateResult = resultDelegate();
-            Assert.Equal(result, delegateResult);
-            
+            Assert.IsType<int>(result);
         }
+
         [Fact]
-        public void GivenExponentDelegate_WhenExecuted_ReturnsTheSameValueAsExponent()
+        public void GivenExponent_WhenExecuted_ReturnAnInteger()
         {
-            Func<int, int> exponentDelegate = calc.Exponent;
             var result = calc.Exponent(7);
-            var delegateResult = exponentDelegate(7);
-            Assert.Equal(result, delegateResult);
+            Assert.IsType<int>(result);
         }
-
-
     }
 }
