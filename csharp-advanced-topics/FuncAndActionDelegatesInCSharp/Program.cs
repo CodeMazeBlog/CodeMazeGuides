c6873ba89684a67745e817b13e40558a7d07c90b
Nikolaos Bellias <nikolaos.bellias@hotmail.com>
2022-11-20 09:36:57 +0200

    Initial simple delegate example

---
+++ csharp-advanced-topics/FuncAndActionDelegatesInCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FuncAndActionDelegatesInCSharp
+{
+    internal class Program
+    {
+        //Declare the delegate
+        public delegate float DoCalculation(float x, float y);
+
+        //Write a concrete method for the delegate
+        public static float CalculateFormula(float x, float y)
+        {
+            return (x * y) / (x + y);
+        }
+
+        static void Main(string[] args)
+        {
+            //Instantiate the delegate
+            DoCalculation calculate = CalculateFormula;
+
+            //Call the delegate
+            Console.WriteLine($"Formula equals {calculate(1.0f, 1.0f)}");
+        }
+    }
+}
