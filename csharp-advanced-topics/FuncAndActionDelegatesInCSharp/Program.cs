4a1699d3f6fc43e84e4cf6273a5f2dfd655fd1d0
Nikolaos Bellias <nikolaos.bellias@hotmail.com>
2022-11-20 09:43:16 +0200

    A complete example of Func and Action

--- csharp-advanced-topics/FuncAndActionDelegatesInCSharp/Program.cs
+++ csharp-advanced-topics/FuncAndActionDelegatesInCSharp/Program.cs
@@ -1,20 +1,48 @@
-﻿namespace FuncAndActionDelegatesInCSharp
+﻿using System;
+
+namespace FuncAndActionDelegatesInCSharp
 {
     internal class Program
     {
-        //Write a concrete method for the Func system delegate
-        public static float CalculateFormula(float x, float y)
+        public delegate float CalculateFormula(float x, float y);
+
+        public static float CalculateFirstFormula(float x, float y)
         {
             return (x * y) / (x + y);
         }
 
+        public static void CalculateSecondFormula(float x)
+        {
+            float PI = 3.14f;
+            Console.WriteLine($"Second Calculation is {x * x * PI}");
+        }
+
+        public static bool PrintTrue()
+        {
+            return true;
+        }
+
+        public static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to our site!");
+        }
+
         static void Main(string[] args)
         {
-            //Instantiate the Func delegate
-            Func<float, float, float> calculate = CalculateFormula;
+            CalculateFormula calc = CalculateFirstFormula;
+            Console.WriteLine(calc(1f, 1f));
 
-            //Call the delegate
-            Console.WriteLine($"Formula equals {calculate(1f, 1f)}");
+            Func<float, float, float> calculationHandler = CalculateFirstFormula;
+            Console.WriteLine(calculationHandler(1f, 1f));
+
+            Func<bool> printTrueHandler = PrintTrue;
+            Console.WriteLine($"Printing true: {printTrueHandler()}");
+
+            Action<float> calcHandler = CalculateSecondFormula;
+            calcHandler(2.0f);
+
+            Action messageHandler = PrintWelcomeMessage;
+            messageHandler();
         }
     }
 }
