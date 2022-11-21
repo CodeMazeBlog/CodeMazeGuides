59c707b6625c2e07b8a14b68f141e26dee5a20f2
Zaneta Borowska <zaneta.borowska@katoennatie.com>
2022-11-21 18:56:05 +0100

    Equality vs is operator example

---
+++ csharp-basic-topics/EqualityVsIsInCSharp/EqualityVsIsInCSharp/File.cs
@@ -0,0 +1,22 @@
+﻿namespace EqualityVsIsInCSharp
+{
+    public class File
+    {
+        public File(string path)
+        {
+            this.Path = path;
+        }
+
+        public string? Path { get; }
+
+        public static bool operator == (File file1, File file2)
+        {
+            return file1.Path == file2.Path;
+        }
+
+        public static bool operator != (File file1, File file2)
+        {
+            return file1.Path != file2.Path;
+        }
+    }
+}
