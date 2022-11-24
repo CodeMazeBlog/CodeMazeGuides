6d7de8be7cd3139c7fdf1739de3919dc9c221ef5
Chayan Adhikari <adhikari.chayan@gmail.com>
2022-11-24 15:46:01 +0530

    CM-553: Code cleanup (#729)

--- dotnet-microservices/Introduction to Microservices Architecture in ASP.NET Core/CodeMazeShop/CodeMazeShop.DataContracts/Discount/PromotionCoupon.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodeMazeShop.DataContracts.Discount;
-
-public class PromotionCoupon
-{
-    public Guid PromotionCouponId { get; set; }
-    public string Code { get; set; }
-    public int Amount { get; set; }
-    public bool IsAlreadyUsed { get; set; }
-}
