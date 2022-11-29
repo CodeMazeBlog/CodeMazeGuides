ed28a317a6964ce6ea6de5bd14949ce51a9e0882
Muhammed Saleem <muhammedsalimp@gmail.com>
2022-11-29 12:25:42 +0530

    fixed indentation

--- aspnetcore-webapi/APIAnalyzers/WebAPIAnalyzers/CustomConventions.cs
+++ aspnetcore-webapi/APIAnalyzers/WebAPIAnalyzers/CustomConventions.cs
@@ -3,17 +3,17 @@ using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace WebAPIAnalyzers
 {
-public static class CustomConventions
-{
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesDefaultResponseType]
-    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
-    public static void Get(
+    public static class CustomConventions
+    {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        public static void Get(
         [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
         [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.AssignableFrom)]
         int id)
-    { }
-}
+        { }
+    }
 }
