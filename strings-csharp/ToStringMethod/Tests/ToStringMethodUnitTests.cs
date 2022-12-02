e570fbdccbd8ca34fccc8f089becde88e84a5008
Ainea Wabwoba <aineah28@gmail.com>
2022-12-02 21:03:10 +0300

    run file format

--- strings-csharp/ToStringMethod/Tests/ToStringMethodUnitTests.cs
+++ strings-csharp/ToStringMethod/Tests/ToStringMethodUnitTests.cs
@@ -6,10 +6,11 @@ public class ToStringMethodUnitTests
 {
     private readonly Person _person;
     private readonly Car _car;
+
     public ToStringMethodUnitTests()
     {
         _person = new Person("Jane Doe", 26, "Secretary");
-        _car = new Car("Range Rover", "Vogue", 200000M, new DateTime(2022,12,02,20,50,10));
+        _car = new Car("Range Rover", "Vogue", 200000M, new DateTime(2022, 12, 02, 20, 50, 10));
     }
 
     [Fact]
@@ -17,7 +18,7 @@ public class ToStringMethodUnitTests
     {
         const string expected = "Jane Doe is 26 years old, and is a Secretary";
         var result = _person.ToString();
-        
+
         Assert.NotNull(result);
         Assert.Equal(expected, result);
     }
@@ -34,12 +35,13 @@ public class ToStringMethodUnitTests
         Assert.NotNull(result);
         Assert.Equal(expected, result);
     }
-                      
+
     [Theory]
     [InlineData("en-GB", "dd/MM/yyyy", "Range Rover Vogue costs £200,000.00 and was sold on 02/12/2022")]
     [InlineData("en-US", "MM/dd/yyyy", "Range Rover Vogue costs $200,000.00 and was sold on 12/02/2022")]
     [InlineData("ja-JP", "yyyy-MMM-dd", "Range Rover Vogue costs ￥200,000 and was sold on 2022-Dec-02")]
-    public void WhenCultureAndDateFormatIsSpecified_ThenReturnFormattedString(string culture, string dateFormat, string expected) 
+    public void WhenCultureAndDateFormatIsSpecified_ThenReturnFormattedString(string culture, string dateFormat,
+        string expected)
     {
         var result = _car.ToString(culture, dateFormat);
 
