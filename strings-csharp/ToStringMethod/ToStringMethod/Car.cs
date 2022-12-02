75e291344a1e65c966465c01a759e0984c246eca
Ainea Wabwoba <aineah28@gmail.com>
2022-12-02 18:58:18 +0300

    initial commit

---
+++ strings-csharp/ToStringMethod/ToStringMethod/Car.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ToStringMethod;
+
+public class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public decimal Price { get; set; }
+    public DateTime SoldAt { get; set; }
+
+    public Car(string make, string model, decimal price, DateTime soldAt)
+    {
+        Model = model;
+        Make = make;
+        Price = price;
+        SoldAt = soldAt;
+    }
+
+    public string ToString(string culture)
+    {
+        return $"{Make} {Model} costs {string.Format(Price.ToString("C", new CultureInfo(culture)))} and was sold on {SoldAt}";
+    }
+
+    public string ToString(string culture, string dateFormat)
+    {
+        return $"{Make} {Model} costs {string.Format(Price.ToString("C", new CultureInfo(culture)))} and was sold on {string.Format(SoldAt.ToString(dateFormat))}";
+    }
+}
