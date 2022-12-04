595ee024916ae44611d844fe6508937a3e610080
cjaynjoku <njokujennaro@gmail.com>
2022-12-04 18:18:05 +0100

    CollectionsInCSharp initial commit

---
+++ collections-csharp/CollectionsInCSharp/CollectionsInCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CollectionsInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myColl = ConcurrentCollections.CreateConcurrentNumbersQueue();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(ConcurrentCollections.ReadFromConcurrentNumbersQueue(myColl));
+            }
+        }
+    }
+}
