86ea866a3a573bb942d99d23b44a4abc44fc0cd6
Alvaro Montoya <alvaro.montoya@knights.ucf.edu>
2022-12-04 16:25:35 -0500

    CM-595 added test project for .NET cryptography

---
+++ csharp-advanced-topics/SpanInCsharp/CryptographyDotnet/CryptographyDotnet/RSATests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyDotnet
+{
+    [TestClass]
+    public class RSATests
+    {
+        [TestMethod]
+        public void WhenUsingRSA_ThenDataIsEncrypted()
+        {
+            var dataStr = "This is corporate research! Dont read me!";
+            var data = Encoding.ASCII.GetBytes(dataStr);
+
+            GenerateKeys(2048, out var publicKey, out var privateKey);
+
+            var encryptedData = Encrypt(data, publicKey);
+
+            Assert.AreNotEqual(data, encryptedData);
+        }
+
+        [TestMethod]
+        public void GivenSameInput_WhenUsingRSA_ThenDataIsEncryptedDifferentlyEachTime()
+        {
+            var dataStr = "This is corporate research! Dont read me!";
+            var data = Encoding.ASCII.GetBytes(dataStr);
+
+            GenerateKeys(2048, out var publicKey, out var privateKey);
+
+            var encryptedData = Encrypt(data, publicKey);
+
+            GenerateKeys(2048, out var publicKey2, out var privateKey2);
+
+            var encryptedData2 = Encrypt(data, publicKey2);
+
+            Assert.AreNotEqual(encryptedData, encryptedData2);
+        }
+
+        [TestMethod]
+        public void GivenSameInputAndSameKey_WhenUsingRSA_ThenDataIsEncryptedDifferentlyEachTime()
+        {
+            var dataStr = "This is corporate research! Dont read me!";
+            var data = Encoding.ASCII.GetBytes(dataStr);
+
+            GenerateKeys(2048, out var publicKey, out var privateKey);
+
+            var encryptedData = Encrypt(data, publicKey);
+
+            var encryptedData2 = Encrypt(data, publicKey);
+
+            Assert.AreNotEqual(encryptedData, encryptedData2);
+        }
+
+        [TestMethod]
+        public void WhenEncryptingWithRSA_ThenDataIsRecoverable()
+        {
+            var dataStr = "This is corporate research! Dont read me!";
+            var data = Encoding.ASCII.GetBytes(dataStr);
+
+            GenerateKeys(2048, out var publicKey, out var privateKey);
+
+            var encryptedData = Encrypt(data, publicKey);
+            var decryptedData = Decrypt(encryptedData, privateKey);
+
+            CollectionAssert.AreEqual(data, decryptedData);
+            Assert.AreEqual(dataStr, Encoding.ASCII.GetString(decryptedData));
+        }
+
+        [TestMethod]
+        public void GivenKeySizeTooSmall_WhenCreatingRSAKeys_ThenExceptionIsThrown()
+        {
+            var dataStr = "This is corporate research! Dont read me!";
+            var data = Encoding.ASCII.GetBytes(dataStr);
+
+            Assert.ThrowsException<CryptographicException>(() => {
+                GenerateKeys(128, out var publicKey, out var privateKey);
+            });
+        }
+
+        public void GenerateKeys(int keyLength, out RSAParameters publicKey, out RSAParameters privateKey)
+        {
+            using (var rsa = RSA.Create())
+            {
+                rsa.KeySize = keyLength;
+                publicKey = rsa.ExportParameters(includePrivateParameters: false);
+                privateKey = rsa.ExportParameters(includePrivateParameters: true);
+            }
+        }
+
+        public byte[] Encrypt(byte[] data, RSAParameters publicKey)
+        {
+            using (var rsa = RSA.Create())
+            {
+                rsa.ImportParameters(publicKey);
+
+                var result = rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
+                return result;
+            }
+        }
+
+        public byte[] Decrypt(byte[] data, RSAParameters privateKey)
+        {
+            using (var rsa = RSA.Create())
+            {
+                rsa.ImportParameters(privateKey);
+                return rsa.Decrypt(data, RSAEncryptionPadding.OaepSHA256);
+            }
+        }
+    } 
+}
---
+++ csharp-advanced-topics/SpanInCsharp/CryptographyDotnet/CryptographyDotnet/SHATests.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyDotnet
+{
+    [TestClass]
+    public class SHATests
+    {
+        //TODO review test naming rules
+        [TestMethod]
+        public async Task WhenHashingSameValue_ThenHashesAreEqual()
+        {
+            var strStreamOne = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+
+            byte[] hashOne;
+            using (var sha256 = SHA256.Create())
+            {
+                hashOne = await sha256.ComputeHashAsync(strStreamOne);
+            }
+
+            var strStreamTwo = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+            byte[] hashTwo;
+            using (var sha256 = SHA256.Create())
+            {
+                hashTwo = await sha256.ComputeHashAsync(strStreamTwo);
+            }
+
+            CollectionAssert.AreEqual(hashOne, hashTwo);
+        }
+
+        [TestMethod]
+        public async Task WhenUsingSHA256_ThenDataIsHashed()
+        {
+            var strStreamOne = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+
+            byte[] hashOne;
+            using (var sha256 = SHA256.Create())
+            {
+                hashOne = await sha256.ComputeHashAsync(strStreamOne);
+            }
+
+            Assert.AreEqual(32, hashOne.Length);
+            Assert.AreNotEqual("This is my pasword! Dont read me!", Encoding.ASCII.GetString(hashOne));
+        }
+
+        [TestMethod]
+        public async Task WhenUsingSHA384_ThenDataIsHashed()
+        {
+            var strStreamOne = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+
+            byte[] hashOne;
+            using (var sha256 = SHA384.Create())
+            {
+                hashOne = await sha256.ComputeHashAsync(strStreamOne);
+            }
+
+            Assert.AreEqual(48, hashOne.Length);
+            Assert.AreNotEqual("This is my pasword! Dont read me!", Encoding.ASCII.GetString(hashOne));
+        }
+
+        [TestMethod]
+        public async Task WhenUsingSHA512_ThenDataIsHashed()
+        {
+            var strStreamOne = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+
+            byte[] hashOne;
+            using (var sha256 = SHA512.Create())
+            {
+                hashOne = await sha256.ComputeHashAsync(strStreamOne);
+            }
+
+            Assert.AreEqual(64, hashOne.Length);
+            Assert.AreNotEqual("This is my pasword! Dont read me!", Encoding.ASCII.GetString(hashOne));
+        }
+
+        [TestMethod]
+        public async Task WhenUsingHMACSHA256_ThenDataIsHashed()
+        {
+            var strStreamOne = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+
+            byte[] hashOne;
+            byte[] key = Encoding.ASCII.GetBytes("superSecretH4shKey1!");
+            using (var hmac = new HMACSHA256(key))
+            {
+                hashOne = await hmac.ComputeHashAsync(strStreamOne);
+            }
+
+            Assert.AreEqual(32, hashOne.Length);
+            Assert.AreNotEqual("This is my pasword! Dont read me!", Encoding.ASCII.GetString(hashOne));
+        }
+
+        [TestMethod]
+        public async Task WhenDifferentKeysWithHMACSHA256_ThenDataIsHashedDifferently()
+        {
+            var strStreamOne = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+
+            byte[] hashOne;
+            byte[] key = Encoding.ASCII.GetBytes("superSecretH4shKey1!");
+            using (var hmac = new HMACSHA256(key))
+            {
+                hashOne = await hmac.ComputeHashAsync(strStreamOne);
+            }
+
+            var strStreamTwo = new MemoryStream(Encoding.ASCII.GetBytes("This is my pasword! Dont read me!"));
+
+            byte[] hashTwo;
+            byte[] keyTwo = Encoding.ASCII.GetBytes("superDup3rSecretH4shKey1!");
+            using (var hmac = new HMACSHA256(keyTwo))
+            {
+                hashTwo = await hmac.ComputeHashAsync(strStreamTwo);
+            }
+
+            Assert.AreEqual(32, hashOne.Length);
+            Assert.AreEqual(32, hashTwo.Length);
+            Assert.AreNotEqual("This is my pasword! Dont read me!", Encoding.ASCII.GetString(hashOne));
+            Assert.AreNotEqual("This is my pasword! Dont read me!", Encoding.ASCII.GetString(hashTwo));
+            Assert.AreNotEqual(Encoding.ASCII.GetString(hashOne), Encoding.ASCII.GetString(hashTwo));
+        }
+
+        public static Stream GenerateStreamFromString(byte[] data)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(data);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
