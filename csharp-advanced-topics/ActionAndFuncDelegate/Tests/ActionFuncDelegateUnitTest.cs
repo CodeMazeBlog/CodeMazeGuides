86d698fb206cb9edfe750d2039c2e5f673a43598
Ronak <ronakpatel3033@gmail.com>
2022-12-06 01:35:09 +0530

    unit test changes

--- csharp-advanced-topics/ActionAndFuncDelegate/Tests/ActionFuncDelegateUnitTest.cs
+++ csharp-advanced-topics/ActionAndFuncDelegate/Tests/ActionFuncDelegateUnitTest.cs
@@ -28,34 +28,6 @@ namespace Tests
             int result = SubFuncDelegate(50, 5);
             Assert.AreEqual(result, 45);
         }
-
-        //Action delegate Test Methods
-        [TestMethod]
-        public void WhenEvenOddActionCalled_ThenPrintNumberMethodPrintEvenOrOdd()
-        {
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
-
-                ActionDelegateEvenOrOdd(50);
-
-                string expected = "50 is even number.";
-                Assert.AreEqual<string>(expected.ToString(), sw.ToString());
-            }
-        }
-
-        [TestMethod]
-        public void WhenActionDelegateMaximumActionCalled_ThenPrintMaximumMethodPrintMaximumOfTwoNumber()
-        {
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
-
-                ActionDelegateMaximum(50, 80);
-
-                string expected = "Maximum of 50 and 80 is 80.";
-                Assert.AreEqual<string>(expected.ToString(), sw.ToString());
-            }
-        }
+       
     }
 }
