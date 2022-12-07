596e4685b63009fc233a8495059c773ecc296069
Ronak <ronakpatel3033@gmail.com>
2022-12-07 22:33:45 +0530

    Changes in Action and Func delegates and change target framework to .net7.0

---
+++ csharp-advanced-topics/ActionAndFuncDelegate/ActionAndFuncDelegate/BasicMaths.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegate
+{
+    public class BasicMaths
+    {
+        //Methods that takes parameters and returns a value:
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Methods that takes parameters but returns nothing:
+        public static void EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("{0} is even number.", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is odd number.", number);
+            }
+        }
+
+        public static void Maximum(double number1, double number2)
+        {
+            Console.WriteLine("Maximum of {0} and {1} is {2}.", number1, number2, Math.Max(number1, number2));
+        }
+    }
+}
--- csharp-advanced-topics/ActionAndFuncDelegate/Tests/ActionFuncDelegateUnitTest.cs
+++ csharp-advanced-topics/ActionAndFuncDelegate/Tests/ActionFuncDelegateUnitTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ActionAndFuncDelegate;
+using System.Text;
 
 namespace Tests
 {
@@ -19,6 +20,7 @@ namespace Tests
         public void WhenAddFuncDelegateCalled_ThenAddMethodAddTwoNumbers()
         {
             int result = AddFuncDelegate(50, 5);
+
             Assert.AreEqual(result, 55);
         }
 
@@ -26,8 +28,39 @@ namespace Tests
         public void WhenSubFuncDelegateCalled_ThenSubMethodSubstractTwoNumbers()
         {
             int result = SubFuncDelegate(50, 5);
+
             Assert.AreEqual(result, 45);
         }
-       
+
+        //Action delegate Test Methods
+        [TestMethod]
+        public void WhenEvenOddActionCalled_ThenPrintNumberIsEvenOrOdd()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ActionDelegateEvenOrOdd(50);
+                var result = sw.ToString().Trim();
+                string expected = "50 is even number.";
+
+                Assert.AreEqual(expected, result);
+
+            }
+        }
+
+        [TestMethod]
+        public void WhenMaximumNumberActionCalled_ThenPrintMaximumOfTwoNumber()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ActionDelegateMaximum(45, 55);
+                var result = sw.ToString().Trim();
+                string expected = "Maximum of 45 and 55 is 55.";
+
+                Assert.AreEqual<string>(expected, result);
+            }
+        }
+
     }
 }
