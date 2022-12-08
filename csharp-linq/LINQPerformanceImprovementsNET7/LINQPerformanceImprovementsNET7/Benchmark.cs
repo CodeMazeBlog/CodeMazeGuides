b2be6257d756771dcc235bb1eef856e72f418d29
Ivan Matec <im@bullwall.com>
2022-12-08 09:06:48 +0100

    - removed tests project - added and renamed methods

--- csharp-linq/LINQPerformanceImprovementsNET7/LINQPerformanceImprovementsNET7/Benchmark.cs
+++ csharp-linq/LINQPerformanceImprovementsNET7/LINQPerformanceImprovementsNET7/Benchmark.cs
@@ -3,12 +3,14 @@ using Bogus;
 using System.Data;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Configs;
+using System.Linq;
 
 namespace LINQPerformanceImprovementsNET7
 {
-    [MemoryDiagnoser]
-    [RankColumn, MeanColumn, MedianColumn]
-    [HideColumns(new string[] { "Gen0", "Gen1", "Gen2", "Job" })]
+    
+    [RankColumn]
+    [HideColumns(new string[] { "Job" })]
+    [MemoryDiagnoser(false)]
     [SimpleJob(RuntimeMoniker.Net60)]
     [SimpleJob(RuntimeMoniker.Net70)]
     [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByMethod)]
@@ -54,16 +56,25 @@ namespace LINQPerformanceImprovementsNET7
         public void Sum() => studentBirthYears.Sum();
 
         [Benchmark]
+        public void Count() => studentBirthYears.Sum();
+
+        [Benchmark]
         public void First() => studentBirthYears.First();
 
         [Benchmark]
-        public void Filter() => studentBirthYears.Where(m => m > 1980);
+        public void Last() => studentBirthYears.Last();
+
+        [Benchmark]
+        public void Distinct() => studentBirthYears.Distinct();
+
+        [Benchmark]
+        public void Where() => studentBirthYears.Where(m => m > 1980);
 
         [Benchmark]
         public void Group() => studentBirthYears.GroupBy(m => m);
 
         [Benchmark]
-        public void Join() => studentBirthYears.Join(studentBirthYears2, m => m, k => k, (m,k) => m);
+        public void Join() => studentBirthYears.Join(studentBirthYears2, m => m, k => k, (m, k) => m);
 
         [Benchmark]
         public void List_Min() => students.Min(m => m.BirthYear);
@@ -72,22 +83,31 @@ namespace LINQPerformanceImprovementsNET7
         public void List_Max() => students.Max(m => m.BirthYear);
 
         [Benchmark]
+        public void List_Average() => students.Average(m => m.BirthYear);
+
+        [Benchmark]
         public void List_Sum() => students.Sum(m => m.BirthYear);
 
         [Benchmark]
-        public void List_Average() => students.Average(m => m.BirthYear);
+        public void List_Count() => students.Count();
 
         [Benchmark]
         public void List_First() => students.First();
 
         [Benchmark]
-        public void List_Filter() => students.Where(m => m.BirthYear > 1980);
+        public void List_Last() => students.Last();
+
+        [Benchmark]
+        public void List_Distinct() => students.Distinct();
+
+        [Benchmark]
+        public void List_Where() => students.Where(m => m.BirthYear > 1980);
 
         [Benchmark]
         public void List_Group() => students.GroupBy(m => m.BirthYear);
 
         [Benchmark]
-        public void List_Join() => students.Join(students2, m => m.BirthYear, k => k.BirthYear, (m,k) => m);
+        public void List_Join() => students.Join(students2, m => m.BirthYear, k => k.BirthYear, (m, k) => m);
 
 
     }
