2105b06c88415fd8caf0f19946442d824483bb9e
JoshuaKirk <Joshua.kirk@hotmail.co.uk>
2022-12-15 19:07:37 +0000

    Added project and tests for updating dictionary values

---
+++ collections-dictionary/UpdateDictionaryValue/UpdateDictionaryValue/Program.cs
@@ -0,0 +1,22 @@
+﻿using UpdateDictionaryValue;
+
+var toppingsDictionary = new ToppingsDictionary();
+toppingsDictionary.AddToppings("pepperoni", 4);
+
+try
+{
+    // Throws when trying to get toppingsDictionary.Toppings["jalepeno"]
+    toppingsDictionary.Toppings["jalepeno"] = toppingsDictionary.Toppings["jalepeno"] + 1;
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+// This implementation will work
+toppingsDictionary.AddToppings("jalepeno", 1);
+
+foreach (var topping in toppingsDictionary.Toppings)
+{
+    Console.WriteLine($"{topping.Key} = {topping.Value}");
+}
---
+++ collections-dictionary/UpdateDictionaryValue/UpdateDictionaryValue/ToppingsDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateDictionaryValue
+{
+    public class ToppingsDictionary
+    {
+        public Dictionary<string, int> Toppings { get; private set; }
+
+        public ToppingsDictionary()
+        {
+            Toppings = new Dictionary<string, int>()
+            {
+                { "pepperoni", 4 },
+                { "meatball", 8 },
+                { "olive", 0 }
+            };
+        }
+
+        public void AddToppings(string toppingType, int amount)
+        {
+            if (Toppings.TryGetValue(toppingType, out int currentAmount))
+            {
+                Toppings[toppingType] = currentAmount + amount;
+            }
+            else
+            {
+                Toppings.Add(toppingType, amount);
+            }
+        }
+    }
+}
