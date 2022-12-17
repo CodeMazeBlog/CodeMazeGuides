e09eb8c37a440f2c82247f709309e454063657b0
Phil <philbroderick12@gmail.com>
2022-12-17 11:08:05 +0000

    Add tests

--- dotnet-client-libraries/OpenTelemetry/OpenTelemetry/Program.cs
+++ dotnet-client-libraries/OpenTelemetry/OpenTelemetry/Program.cs
@@ -1,4 +1,5 @@
-﻿using Serilog;
+﻿using OpenTelemetry;
+using Serilog;
 using Serilog.Formatting.Json;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
@@ -7,25 +8,24 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.Console(new JsonFormatter())
     .CreateLogger();
 
-var meter = new Meter("OpenTelemetry");
-
-var histogram = meter.CreateHistogram<float>("RequestDuration", unit: "ms");
-
-var httpClient = new HttpClient();
-
-Console.WriteLine("Press any key to start measurements");
+// Pillar 1: Traces
+Console.WriteLine("Starting trace pillar");
+var tracingPillar = new Traces();
+await tracingPillar.Start();
+Console.WriteLine("Trace pillar complete. Press any key to start metrics pillar");
 Console.ReadLine();
 
-var uri = "https://code-maze.com";
+// Pillar 2: Metrics
+Console.WriteLine("Starting metrics pillar");
+var metricsPillar = new Metrics(new HttpClient());
+await metricsPillar.Start();
+Console.WriteLine("Metrics pillar complete. Press any key to start logging pillar");
+Console.ReadLine();
 
-for (int i = 0; i < 100; i++)
-{
-    Log.Logger.Information("Starting request to {Url}", uri);
-    var stopWatch = Stopwatch.StartNew();
-    await httpClient.GetStringAsync(uri);
+// Pillar 3: Logging
+Console.WriteLine("Starting logging pillar");
+var loggingPillar = new Logging(Log.Logger);
+await loggingPillar.Start();
+Console.WriteLine("Logging pillar complete. Press any key to finish");
 
-    histogram.Record(stopWatch.ElapsedMilliseconds, tag: KeyValuePair.Create<string, object?>("Url", uri));
-}
-
-Console.WriteLine("Measurements complete");
 Console.ReadLine();
