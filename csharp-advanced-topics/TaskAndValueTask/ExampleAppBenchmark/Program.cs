ab20272b077618e9be912e86f3b10337a75fe29f
Bazen <bazenteklehaymanot29@gmail.com>
2022-12-20 09:17:22 +0300

    Minor refactoring

--- csharp-advanced-topics/TaskAndValueTask/ExampleAppBenchmark/Program.cs
+++ csharp-advanced-topics/TaskAndValueTask/ExampleAppBenchmark/Program.cs
@@ -1,5 +1,4 @@
-﻿//dotnet run -p ExampleAppBenchmark/ExampleAppBenchmark.csproj -c Release
-using ExampleAppBenchmark;
+﻿using ExampleAppBenchmark;
 using BenchmarkDotNet.Running;
 
 BenchmarkRunner.Run<TaskAndValueTaskBenchmark>();
