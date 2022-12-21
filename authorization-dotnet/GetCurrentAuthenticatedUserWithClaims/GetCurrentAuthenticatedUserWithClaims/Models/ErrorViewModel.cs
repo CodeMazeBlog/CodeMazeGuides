f07c37961b89c4b5b36517a06a9ab22c614785a2
Haris Tse <haris.tsetsekas@gmail.com>
2022-12-21 11:43:50 +0200

    Renamed project

---
+++ authorization-dotnet/GetCurrentAuthenticatedUserWithClaims/GetCurrentAuthenticatedUserWithClaims/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GetCurrentAuthenticatedUserWithClaims.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
