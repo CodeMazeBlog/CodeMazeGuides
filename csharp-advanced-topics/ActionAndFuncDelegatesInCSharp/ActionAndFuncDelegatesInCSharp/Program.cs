ba9a0162475433753eabbfd7daed7628d1088d32
Kenichiro Nakamura <kenakamu@microsoft.com>
2022-12-27 23:17:33 +0900

    remove comments and coordinate the code order

--- csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -3,26 +3,14 @@
 public class Program
 {
     // Delegate definitions
-    public delegate DateTime TimeConverter(DateTime dateTime, TimeZoneInfo timeZoneInfo);
     public delegate void LocalTimeAndZoneName(DateTime dateTime);
-
-    /// <summary>
-    /// Method returns no value
-    /// Display Local Time Name and datetime
-    /// </summary>
-    /// <param name="dateTime">Current DateTime</param>
+    public delegate DateTime TimeConverter(DateTime dateTime, TimeZoneInfo timeZoneInfo);
+    
     public static void DisplayLocalTimeAndTimeZone(DateTime dateTime)
     {
         Console.WriteLine($"Time in {TimeZoneInfo.Local.DisplayName} is {dateTime}");
     }
 
-    /// <summary>
-    /// Method returns value
-    /// Converting datetime by using TimeZoneInfo
-    /// </summary>
-    /// <param name="dateTime">Current DateTime</param>
-    /// <param name="timeZoneInfo">TimeZoneInfo to convert to</param>
-    /// <returns>Converted DateTime</returns>
     public static DateTime ConvertTimeByTimeZone(DateTime dateTime, TimeZoneInfo timeZoneInfo)
     {
         return TimeZoneInfo.ConvertTime(DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified), timeZoneInfo);
@@ -39,11 +27,9 @@ public class Program
         localTimeZoneName(now);
         Console.WriteLine($"Time in {timeZoneName} is {timeConverter(now, TimeZoneInfo.FindSystemTimeZoneById(timeZoneName))}");
 
-        // Use Action to encapsulate method which doesn't return any value
+        // Use Action and Func delegates
         Action<DateTime> localTimeZoneNameAciton = DisplayLocalTimeAndTimeZone;
-        // Use Func to encapsulate method which returns a value
         Func<DateTime, TimeZoneInfo, DateTime> timeConverterFunc = ConvertTimeByTimeZone;
-        // Execute Action and Func
         localTimeZoneNameAciton(now);
         Console.WriteLine($"Time in {timeZoneName} is {timeConverterFunc(now, TimeZoneInfo.FindSystemTimeZoneById(timeZoneName))}");
     }
