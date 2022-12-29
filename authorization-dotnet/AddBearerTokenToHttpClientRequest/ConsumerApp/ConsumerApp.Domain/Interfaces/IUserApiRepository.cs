f4549c17d33d19345c5efb0446da2198e695d296
Filipi Braga <filipibc@gmail.com>
2022-12-29 07:57:37 -0300

    CM-630: How to Add a BearerToken to an HttpClient Request (#754)
    * Implementation of two applications: ConsumerApp and UserWebApi to add bearer token to HttpClient Request
    
    * Bring the solution to the root folder
    
    * - Refactoring Solution
    - Using HttpClientFactory BaseAddress
    
    * - Refactoring the AuthenticateAsync Method
    - Renaming some methods.
    - Refactoring the LoginHandler class
    - Renaming the AuthenticateModel class
    
    * - Changing from string to var
    
    * - Create a new CheckRegisteredUser method to shrink the line of code in the Login Method.
    - Add some comments explaining how to do in a real-world application
    - Adding a GenerateBody method to shrink the AuthenticateAsync method.
    
    * - Renaming GetUsersAync method to GetUserAsync

---
+++ authorization-dotnet/AddBearerTokenToHttpClientRequest/ConsumerApp/ConsumerApp.Domain/Interfaces/IUserApiRepository.cs
@@ -0,0 +1,11 @@
+﻿using ConsumerApp.Domain.Model;
+
+namespace ConsumerApp.Domain.Interfaces
+{
+    public interface IUserApiRepository
+    {
+        Task CreateUserAsync(UserModel userModel, string token);
+        Task<IEnumerable<UserModel>> GetUsersAsync();
+        Task<UserModel> GetUserAsync(int userId);
+    }
+}
