ffecefef5c4306ddec78616f3a7f427f94f13c1e
edergus <edergus@gmail.com>
2023-01-02 14:40:53 +0100

    feat: added parser.

---
+++ aspnetcore-webapi/InsertKeyValuePairIntoExistingJSON/InsertKeyValuePairIntoExistingJSON/Parser.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace InsertKeyValuePairIntoExistingJSON
+{
+    public class Parser
+    {
+        private string _json;
+
+        public Parser(string json)
+        {
+            _json = json;
+        }
+
+        public string Add(string key, string value)
+        {
+            var json = JObject.Parse(_json);
+            json.Add(key, value);
+
+            return json.ToString();
+        }
+
+        public string Insert(string existingProperty, string newProperty, string value)
+        {
+            var json = JObject.Parse(_json);
+            if (!json.ContainsKey(existingProperty))
+            {
+                return json.ToString();
+            }
+
+            json[existingProperty][newProperty] = value;
+
+            return json.ToString();
+        }
+    }
+}
--- aspnetcore-webapi/InsertKeyValuePairIntoExistingJSON/InsertKeyValuePairIntoExistingJSON/Program.cs
+++ aspnetcore-webapi/InsertKeyValuePairIntoExistingJSON/InsertKeyValuePairIntoExistingJSON/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using InsertKeyValuePairIntoExistingJSON;
+
+var json =
+    @"{
+        ""FirstName"": ""Audrey"",
+        ""LastName"": ""Spencer"",
+        ""ContactDetails"": {
+            ""Country"": ""Spain""
+        }
+    }";
+
+var parser = new Parser(json);
+
+var jasonUpdated = parser.Add("UserName", "Audrey_Spencer72");
+Console.WriteLine(jasonUpdated);
+
+jasonUpdated = parser.Insert("ContactDetails", "Address", "4455 Landing Lange");
+Console.WriteLine(jasonUpdated);
