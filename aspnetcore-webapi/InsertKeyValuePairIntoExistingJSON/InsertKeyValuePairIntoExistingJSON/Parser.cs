28569c494da8b18987f9c4a34bcffbcb23ae99b1
edergus <edergus@gmail.com>
2023-01-03 18:05:28 +0100

    feat: modified to JObject result.

--- aspnetcore-webapi/InsertKeyValuePairIntoExistingJSON/InsertKeyValuePairIntoExistingJSON/Parser.cs
+++ aspnetcore-webapi/InsertKeyValuePairIntoExistingJSON/InsertKeyValuePairIntoExistingJSON/Parser.cs
@@ -11,46 +11,46 @@ namespace InsertKeyValuePairIntoExistingJSON
             _json = json;
         }
 
-        public string AddStringValue(string key, string value)
+        public JObject AddStringValue(string key, string value)
         {
             var json = JObject.Parse(_json);
             json.Add(key, value);
 
-            return json.ToString();
+            return json;
         }
 
-        public string AddObjectValue(string key, object value)
+        public JObject AddObjectValue(string key, object value)
         {
             var json = JObject.Parse(_json);
             json.Add(key, JObject.FromObject(value));
 
-            return json.ToString();
+            return json;
         }
 
-        public string InsertStringValue(string existingProperty, string newProperty, string value)
+        public JObject InsertStringValue(string existingProperty, string newProperty, string value)
         {
             var json = JObject.Parse(_json);
             if (!json.ContainsKey(existingProperty))
             {
-                return json.ToString();
+                return json;
             }
 
             json[existingProperty][newProperty] = value;
 
-            return json.ToString();
+            return json;
         }
 
-        public string InsertObjectValue(string existingProperty, string newProperty, object value)
+        public JObject InsertObjectValue(string existingProperty, string newProperty, object value)
         {
             var json = JObject.Parse(_json);
             if (!json.ContainsKey(existingProperty))
             {
-                return json.ToString();
+                return json;
             }
 
             json[existingProperty][newProperty] = JObject.FromObject(value);
 
-            return json.ToString();
+            return json;
         }
     }
 }
