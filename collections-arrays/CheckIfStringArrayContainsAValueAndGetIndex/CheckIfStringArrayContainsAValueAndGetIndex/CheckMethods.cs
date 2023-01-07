59cf29d2d0fdd48e44b7fcd4cc45747c08d2964f
cjaynjoku <njokujennaro@gmail.com>
2023-01-07 19:12:15 +0100

    sample code for the article

--- collections-arrays/CheckIfStringArrayContainsAValueAndGetIndex/CheckIfStringArrayContainsAValueAndGetIndex/CheckMethods.cs
+++ collections-arrays/CheckIfStringArrayContainsAValueAndGetIndex/CheckIfStringArrayContainsAValueAndGetIndex/CheckMethods.cs
@@ -1,159 +1,20 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Order;
-
-namespace CheckIfStringArrayContainsAValueAndGetIndex
+﻿namespace CheckIfStringArrayContainsAValueAndGetIndex
 {
-    [MemoryDiagnoser]
-    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class CheckMethods
     {
         private readonly string[] _stringArray =
         {
-            "dog", "cat", "bird", "horse", "pig", "cow", "sheep", "goat", "chicken", "duck", "frog", "turtle",
-            "rabbit", "hamster", "mouse", "gerbil", "rat", "guinea pig", "ferret", "beaver", "otter", "skunk",
-            "kangaroo", "monkey", "chimpanzee", "gorilla", "lizard", "snake", "toad", "crocodile", "alligator",
-            "dolphin", "whale", "shark", "fish", "crab", "lobster", "shrimp", "octopus", "squid", "clam", "scallop",
-            "oyster", "mussel", "starfish", "sponge", "jellyfish", "seagull", "penguin", "pelican", "albatross",
-            "swan", "goose", "duck", "heron", "ostrich", "emu", "turkey", "peacock", "parakeet", "parrot",
-            "flamingo", "pigeon", "dove", "woodpecker", "sparrow", "robin", "blue jay", "cardinal", "finch",
-            "thrush", "wren", "goldfinch", "toucan", "buzzard", "eagle", "raven", "crow", "magpie", "dormouse",
-            "lemur", "opossum", "panda", "koala", "sloth", "ant eater", "gopher", "marmot", "chipmunk", "nutria",
-            "mink", "weasel", "badger", "wombat", "hippopotamus", "rhinoceros", "giraffe", "elephant", "zebra",
-            "antelope", "camel", "llama", "alpaca", "vicuna", "gazelle", "deer", "elk", "moose", "caribou",
-            "reindeer", "bison", "wild boar", "pig", "warthog", "hyena", "jackal", "coyote", "dingo", "wolf",
-            "fox", "kite", "vulture", "condor", "eagle", "falcon", "hawkeye", "hornet", "wasp", "beetle", "ant",
-            "bee", "termite", "mosquito", "fly", "butterfly", "moth", "caterpillar", "centipede", "millipede",
-            "spider", "scorpion", "tick", "mite", "louse", "flea", "bedbug", "aphid", "cicada", "cricket",
-            "locust", "grasshopper", "praying mantis", "termite", "mantis", "roach", "bee", "wasp", "hornet",
-            "yellow jacket", "firefly", "lightning bug", "ladybug", "dragonfly", "damselfly", "admiral", "aurora",
-            "battalion", "apple", "banana", "orange", "pear", "grape", "strawberry", "raspberry", "blueberry",
-            "blackberry", "mango", "peach", "apricot", "plum", "cherry", "fig", "kiwi", "pineapple", "papaya",
-            "guava", "cantaloupe", "honeydew", "watermelon", "melon", "lemon", "lime", "orange", "tangerine",
-            "mandarin", "clementine", "pomegranate", "persimmon", "quince", "nectarine", "arugula", "lettuce",
-            "cabbage", "spinach", "kale", "collard greens", "broccoli", "cauliflower", "brussels sprouts",
-            "bok choy", "swiss chard", "mustard greens", "turnip greens", "beet greens", "radicchio", "endive",
-            "frisée", "escarole", "dandelion greens", "arugula", "mizuna", "watercress", "herbs", "basil",
-            "parsley", "cilantro", "mint", "dill", "tarragon", "oregano", "marjoram", "thyme", "rosemary",
-            "sage", "chives", "garlic", "onion", "scallion", "leek", "shallot", "chive", "carrot", "radish","turnip",
-            "beet", "parsnip", "rutabaga", "celeriac", "potato", "sweet potato", "yam", "butternut squash",
-            "acorn squash", "pumpkin", "zucchini", "summer squash", "pattypan squash", "spaghetti squash", "cucumber",
-            "green bean", "pea", "snow pea", "snap pea", "okra", "lima bean", "fava bean", "mung bean", "chickpea",
-            "lentil", "peanut", "soybean", "edamame", "mung bean", "split pea", "pigeon pea", "coconut", "cashew",
-            "pistachio", "almond", "pecan", "walnut", "hazelnut", "chestnut", "peanut", "macadamia", "brazil nut",
-            "pine nut", "beechnut", "hickory nut", "pignoli nut", "sunflower seed", "pumpkin seed", "sesame seed",
-            "flaxseed", "chia seed", "poppy seed", "hemp seed", "wheat", "rye", "barley",
-            "dog", "cat", "bird", "horse", "pig", "cow", "sheep", "goat", "chicken", "duck", "frog", "turtle",
-            "rabbit", "hamster", "mouse", "gerbil", "rat", "guinea pig", "ferret", "beaver", "otter", "skunk",
-            "kangaroo", "monkey", "chimpanzee", "gorilla", "lizard", "snake", "toad", "crocodile", "alligator",
-            "dolphin", "whale", "shark", "fish", "crab", "lobster", "shrimp", "octopus", "squid", "clam", "scallop",
-            "oyster", "mussel", "starfish", "sponge", "jellyfish", "seagull", "penguin", "pelican", "albatross",
-            "swan", "goose", "duck", "heron", "ostrich", "emu", "turkey", "peacock", "parakeet", "parrot",
-            "flamingo", "pigeon", "dove", "woodpecker", "sparrow", "robin", "blue jay", "cardinal", "finch",
-            "thrush", "wren", "goldfinch", "toucan", "buzzard", "eagle", "raven", "crow", "magpie", "dormouse",
-            "lemur", "opossum", "panda", "koala", "sloth", "ant eater", "gopher", "marmot", "chipmunk", "nutria",
-            "mink", "weasel", "badger", "wombat", "hippopotamus", "rhinoceros", "giraffe", "elephant", "zebra",
-            "antelope", "camel", "llama", "alpaca", "vicuna", "gazelle", "deer", "elk", "moose", "caribou",
-            "reindeer", "bison", "wild boar", "pig", "warthog", "hyena", "jackal", "coyote", "dingo", "wolf",
-            "fox", "kite", "vulture", "condor", "eagle", "falcon", "hawkeye", "hornet", "wasp", "beetle", "ant",
-            "bee", "termite", "mosquito", "fly", "butterfly", "moth", "caterpillar", "centipede", "millipede",
-            "spider", "scorpion", "tick", "mite", "louse", "flea", "bedbug", "aphid", "cicada", "cricket",
-            "locust", "grasshopper", "praying mantis", "termite", "mantis", "roach", "bee", "wasp", "hornet",
-            "yellow jacket", "firefly", "lightning bug", "ladybug", "dragonfly", "damselfly", "admiral", "aurora",
-            "battalion", "apple", "banana", "orange", "pear", "grape", "strawberry", "raspberry", "blueberry",
-            "blackberry", "mango", "peach", "apricot", "plum", "cherry", "fig", "kiwi", "pineapple", "papaya",
-            "guava", "cantaloupe", "honeydew", "watermelon", "melon", "lemon", "lime", "orange", "tangerine",
-            "mandarin", "clementine", "pomegranate", "persimmon", "quince", "nectarine", "arugula", "lettuce",
-            "cabbage", "spinach", "kale", "collard greens", "broccoli", "cauliflower", "brussels sprouts",
-            "bok choy", "swiss chard", "mustard greens", "turnip greens", "beet greens", "radicchio", "endive",
-            "frisée", "escarole", "dandelion greens", "arugula", "mizuna", "watercress", "herbs", "basil",
-            "parsley", "cilantro", "mint", "dill", "tarragon", "oregano", "marjoram", "thyme", "rosemary",
-            "sage", "chives", "garlic", "onion", "scallion", "leek", "shallot", "chive", "carrot", "radish","turnip",
-            "beet", "parsnip", "rutabaga", "celeriac", "potato", "sweet potato", "yam", "butternut squash",
-            "acorn squash", "pumpkin", "zucchini", "summer squash", "pattypan squash", "spaghetti squash", "cucumber",
-            "green bean", "pea", "snow pea", "snap pea", "okra", "lima bean", "fava bean", "mung bean", "chickpea",
-            "lentil", "peanut", "soybean", "edamame", "mung bean", "split pea", "pigeon pea", "coconut", "cashew",
-            "pistachio", "almond", "pecan", "walnut", "hazelnut", "chestnut", "peanut", "macadamia", "brazil nut",
-            "pine nut", "beechnut", "hickory nut", "pignoli nut", "sunflower seed", "pumpkin seed", "sesame seed",
-            "flaxseed", "chia seed", "poppy seed", "hemp seed", "wheat", "rye", "barley",
-            "dog", "cat", "bird", "horse", "pig", "cow", "sheep", "goat", "chicken", "duck", "frog", "turtle",
-            "rabbit", "hamster", "mouse", "gerbil", "rat", "guinea pig", "ferret", "beaver", "otter", "skunk",
-            "kangaroo", "monkey", "chimpanzee", "gorilla", "lizard", "snake", "toad", "crocodile", "alligator",
-            "dolphin", "whale", "shark", "fish", "crab", "lobster", "shrimp", "octopus", "squid", "clam", "scallop",
-            "oyster", "mussel", "starfish", "sponge", "jellyfish", "seagull", "penguin", "pelican", "albatross",
-            "swan", "goose", "duck", "heron", "ostrich", "emu", "turkey", "peacock", "parakeet", "parrot",
-            "flamingo", "pigeon", "dove", "woodpecker", "sparrow", "robin", "blue jay", "cardinal", "finch",
-            "thrush", "wren", "goldfinch", "toucan", "buzzard", "eagle", "raven", "crow", "magpie", "dormouse",
-            "lemur", "opossum", "panda", "koala", "sloth", "ant eater", "gopher", "marmot", "chipmunk", "nutria",
-            "mink", "weasel", "badger", "wombat", "hippopotamus", "rhinoceros", "giraffe", "elephant", "zebra",
-            "antelope", "camel", "llama", "alpaca", "vicuna", "gazelle", "deer", "elk", "moose", "caribou",
-            "reindeer", "bison", "wild boar", "pig", "warthog", "hyena", "jackal", "coyote", "dingo", "wolf",
-            "fox", "kite", "vulture", "condor", "eagle", "falcon", "hawkeye", "hornet", "wasp", "beetle", "ant",
-            "bee", "termite", "mosquito", "fly", "butterfly", "moth", "caterpillar", "centipede", "millipede",
-            "spider", "scorpion", "tick", "mite", "louse", "flea", "bedbug", "aphid", "cicada", "cricket",
-            "locust", "grasshopper", "praying mantis", "termite", "mantis", "roach", "bee", "wasp", "hornet",
-            "yellow jacket", "firefly", "lightning bug", "ladybug", "dragonfly", "damselfly", "admiral", "aurora",
-            "battalion", "apple", "banana", "orange", "pear", "grape", "strawberry", "raspberry", "blueberry",
-            "blackberry", "mango", "peach", "apricot", "plum", "cherry", "fig", "kiwi", "pineapple", "papaya",
-            "guava", "cantaloupe", "honeydew", "watermelon", "melon", "lemon", "lime", "orange", "tangerine",
-            "mandarin", "clementine", "pomegranate", "persimmon", "quince", "nectarine", "arugula", "lettuce",
-            "cabbage", "spinach", "kale", "collard greens", "broccoli", "cauliflower", "brussels sprouts",
-            "bok choy", "swiss chard", "mustard greens", "turnip greens", "beet greens", "radicchio", "endive",
-            "frisée", "escarole", "dandelion greens", "arugula", "mizuna", "watercress", "herbs", "basil",
-            "parsley", "cilantro", "mint", "dill", "tarragon", "oregano", "marjoram", "thyme", "rosemary",
-            "sage", "chives", "garlic", "onion", "scallion", "leek", "shallot", "chive", "JOHN DOE", "radish","turnip",
-            "beet", "parsnip", "rutabaga", "celeriac", "potato", "sweet potato", "yam", "butternut squash",
-            "acorn squash", "pumpkin", "zucchini", "summer squash", "pattypan squash", "spaghetti squash", "cucumber",
-            "green bean", "pea", "snow pea", "snap pea", "okra", "lima bean", "fava bean", "mung bean", "chickpea",
-            "lentil", "peanut", "soybean", "edamame", "mung bean", "split pea", "pigeon pea", "coconut", "cashew",
-            "pistachio", "almond", "pecan", "walnut", "hazelnut", "chestnut", "peanut", "macadamia", "brazil nut",
-            "pine nut", "beechnut", "hickory nut", "pignoli nut", "sunflower seed", "pumpkin seed", "sesame seed",
-            "fox", "kite", "vulture", "condor", "eagle", "falcon", "hawkeye", "hornet", "wasp", "beetle", "ant",
-            "bee", "termite", "mosquito", "fly", "butterfly", "moth", "caterpillar", "centipede", "millipede",
-            "spider", "scorpion", "tick", "mite", "louse", "flea", "bedbug", "aphid", "cicada", "cricket",
-            "locust", "grasshopper", "praying mantis", "termite", "mantis", "roach", "bee", "wasp", "hornet",
-            "yellow jacket", "firefly", "lightning bug", "ladybug", "dragonfly", "damselfly", "admiral", "aurora",
-            "battalion", "apple", "banana", "orange", "pear", "grape", "strawberry", "raspberry", "blueberry",
-            "blackberry", "mango", "peach", "apricot", "plum", "cherry", "fig", "kiwi", "pineapple", "papaya",
-            "guava", "cantaloupe", "honeydew", "watermelon", "melon", "lemon", "lime", "orange", "tangerine",
-            "mandarin", "clementine", "pomegranate", "persimmon", "quince", "nectarine", "arugula", "lettuce",
-            "cabbage", "spinach", "kale", "collard greens", "broccoli", "cauliflower", "brussels sprouts",
-            "bok choy", "swiss chard", "mustard greens", "turnip greens", "beet greens", "radicchio", "endive",
-            "frisée", "escarole", "dandelion greens", "flaxseed", "chia seed", "poppy seed", "hemp seed",
-            "wheat", "jane doe", "barley",
+            "dog", "horse", "emu", "lizard", "duck",
+            "frog", "SNAKE", "emu", "duck", "GOOSE"
         };
-        private const string Value = "jane doe";
-        private const string CaseInsensitiveValue = "John Doe";
 
-        [Benchmark]
-        public int ArrayIndexOf()
-        {
-            int index = Array.IndexOf(_stringArray, Value);
-
-            return index;
-        }
-
-        [Benchmark]
-        public int ArrayFindIndex()
-        {
-            int index = Array.FindIndex(_stringArray, str => str ==  Value);
-
-            return index;
-        }
-
-        public int ArrayCaseInsensitiveFindIndex()
-        {
-            int index = Array.FindIndex(_stringArray, str => str.Equals(CaseInsensitiveValue, StringComparison.OrdinalIgnoreCase));
-            
-            return index;
-        }
-
-        [Benchmark]
         public int ForLoop()
         {
+            string value = "horse";
             int index = -1;
             for (int i = 0; i < _stringArray.Length; i++)
             {
-                if (_stringArray[i] == Value)
+                if (_stringArray[i] == value)
                 {
                     index = i;
                     break;
@@ -163,14 +24,14 @@ namespace CheckIfStringArrayContainsAValueAndGetIndex
             return index;
         }
 
-        [Benchmark]
         public int ForEachLoop()
         {
+            string value = "dog";
             int index = -1;
             int step = 0;
             foreach (var item in _stringArray)
             {
-                if (item == Value)
+                if (item == value)
                 {
                     index = step;
                     break;
@@ -181,28 +42,58 @@ namespace CheckIfStringArrayContainsAValueAndGetIndex
             return index;
         }
 
-        [Benchmark]
-        public int ListIndexOf()
+        public int ArrayIndexOf()
         {
-            var stringList = _stringArray.ToList();
-            int index = stringList.IndexOf(Value);
+            string value = "lizard";
+            int index = Array.IndexOf(_stringArray, value);
 
             return index;
         }
 
-        [Benchmark]
-        public int ListFindIndex()
+        public int ArrayFindIndex()
         {
-            var stringList = _stringArray.ToList();
-            int index = stringList.FindIndex(str => str == Value);
+            string value = "frog";
+            int index = Array.FindIndex(_stringArray, str => str == value);
 
             return index;
         }
 
-        public int ListCaseInsensitiveFindIndex()
+        public int ArrayCaseInsensitiveFindIndex()
         {
-            var stringList = _stringArray.ToList();
-            int index = stringList.FindIndex(str => str.Equals(CaseInsensitiveValue, StringComparison.OrdinalIgnoreCase));
+            string caseInsensitiveValue = "Snake";
+            int index = Array.FindIndex(_stringArray, str => str.Equals(caseInsensitiveValue, StringComparison.OrdinalIgnoreCase));
+
+            return index;
+        }
+
+        public int ArrayFindIndexWithContains()
+        {
+            string value = "du";
+            int index = Array.FindIndex(_stringArray, str => str.Contains(value));
+
+            return index;
+        }
+
+        public int ArrayLastIndexOf()
+        {
+            string value = "duck";
+            int index = Array.LastIndexOf(_stringArray, value);
+
+            return index;
+        }
+
+        public int ArrayFindLastIndex()
+        {
+            string value = "emu";
+            int index = Array.FindLastIndex(_stringArray, str => str == value);
+
+            return index;
+        }
+
+        public int ArrayCaseInsensitiveFindLastIndex()
+        {
+            string caseInsensitiveValue = "Goose";
+            int index = Array.FindLastIndex(_stringArray, str => str.Equals(caseInsensitiveValue, StringComparison.OrdinalIgnoreCase));
 
             return index;
         }
--- collections-arrays/CheckIfStringArrayContainsAValueAndGetIndex/Tests/CheckMethodsTest.cs
+++ collections-arrays/CheckIfStringArrayContainsAValueAndGetIndex/Tests/CheckMethodsTest.cs
@@ -11,35 +11,11 @@ namespace Tests
         }
 
         [Fact]
-        public void GivenAnArrayAndAValue_WhenCheckingWithArrayIndexOf_ThenReturnsAnIntegerIndex()
-        {
-            var result = _checkMethods.ArrayIndexOf();
-
-            Assert.Equal(998, result);
-        }
-
-        [Fact]
-        public void GivenAnArrayAndAValue_WhenCheckingWithArrayFindIndex_ThenReturnsAnIntegerIndex()
-        {
-            var result = _checkMethods.ArrayFindIndex();
-
-            Assert.Equal(998, result);
-        }
-
-        [Fact]
-        public void GivenAnArrayAndAValue_WhenCheckingWithArrayCaseInsensitiveFindIndex_ThenReturnsAnIntegerIndex()
-        {
-            var result = _checkMethods.ArrayCaseInsensitiveFindIndex();
-
-            Assert.Equal(841, result);
-        }
-
-        [Fact]
         public void GivenAnArrayAndAValue_WhenCheckingWithForLoop_ThenReturnsAnIntegerIndex()
         {
             var result = _checkMethods.ForLoop();
 
-            Assert.Equal(998, result);
+            Assert.Equal(1, result);
         }
 
         [Fact]
@@ -47,31 +23,63 @@ namespace Tests
         {
             var result = _checkMethods.ForEachLoop();
 
-            Assert.Equal(998, result);
+            Assert.Equal(0, result);
         }
 
         [Fact]
-        public void GivenAnArrayAndAValue_WhenCheckingWithListIndexOf_ThenReturnsAnIntegerIndex()
+        public void GivenAnArrayAndAValue_WhenCheckingWithArrayIndexOf_ThenReturnsAnIntegerIndex()
         {
-            var result = _checkMethods.ListIndexOf();
+            var result = _checkMethods.ArrayIndexOf();
 
-            Assert.Equal(998, result);
+            Assert.Equal(3, result);
         }
 
         [Fact]
-        public void GivenAnArrayAndAValue_WhenCheckingWithListFindIndex_ThenReturnsAnIntegerIndex()
+        public void GivenAnArrayAndAValue_WhenCheckingWithArrayFindIndex_ThenReturnsAnIntegerIndex()
         {
-            var result = _checkMethods.ListFindIndex();
+            var result = _checkMethods.ArrayFindIndex();
 
-            Assert.Equal(998, result);
+            Assert.Equal(5, result);
         }
 
         [Fact]
-        public void GivenAnArrayAndAValue_WhenCheckingWithListCaseInsensitiveFindIndex_ThenReturnsAnIntegerIndex()
+        public void GivenAnArrayAndAValue_WhenCheckingWithArrayCaseInsensitiveFindIndex_ThenReturnsAnIntegerIndex()
         {
-            var result = _checkMethods.ListCaseInsensitiveFindIndex();
+            var result = _checkMethods.ArrayCaseInsensitiveFindIndex();
 
-            Assert.Equal(841, result);
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void GivenAnArrayAndAValue_WhenCheckingWithArrayFindIndexWithContains_ThenReturnsAnIntegerIndex()
+        {
+            var result = _checkMethods.ArrayFindIndexWithContains();
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void GivenAnArrayAndAValue_WhenCheckingWithArrayLastIndexOf_ThenReturnsAnIntegerIndex()
+        {
+            var result = _checkMethods.ArrayLastIndexOf();
+
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void GivenAnArrayAndAValue_WhenCheckingWithArrayFindLastIndex_ThenReturnsAnIntegerIndex()
+        {
+            var result = _checkMethods.ArrayFindLastIndex();
+
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void GivenAnArrayAndAValue_WhenCheckingWithArrayCaseInsensitiveFindLastIndex_ThenReturnsAnIntegerIndex()
+        {
+            var result = _checkMethods.ArrayCaseInsensitiveFindLastIndex();
+
+            Assert.Equal(9, result);
         }
     }
 }
