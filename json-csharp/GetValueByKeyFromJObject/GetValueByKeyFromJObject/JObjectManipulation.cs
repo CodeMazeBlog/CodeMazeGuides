1ecb04b29dcb4c39ab6bbfbe8458e0bb4050e989
Ainea Wabwoba <aineah28@gmail.com>
2023-01-07 17:53:13 +0300

    initial commit

---
+++ json-csharp/GetValueByKeyFromJObject/GetValueByKeyFromJObject/JObjectManipulation.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace GetValueByKeyFromJObject
+{
+    public class JObjectManipulation
+    {
+        public string JsonArray { get; set; }
+        public string SingleJsonObject { get; set; }
+
+        public JObjectManipulation()
+        {
+            InitializeData();
+        }
+
+        public void InitializeData()
+        {
+            var testData = new TestData();
+            JsonArray = testData.GenerateJsonArray();
+            SingleJsonObject = testData.GenerateSingleJsonObject();
+        }
+
+        public int GetValuesUsingIndex()
+        {
+            var jsonObject = JObject.Parse(SingleJsonObject);
+
+            var name = (string)jsonObject["name"];
+            var make = (string)jsonObject["make"];
+            var model = (string)jsonObject["model"];
+            var year = (int)jsonObject["year"];
+
+            var price = (JObject)jsonObject["price"];
+            var amount = (int)price["amount"];
+            var currency = (string)price["currency"];
+
+            Console.WriteLine($"A {make} {name} {model} {year} costs {amount} {currency} \n");
+
+            return jsonObject.Count;
+        }
+
+        public int GetValuesUsingValueMethod()
+        {
+            var jsonObject = JObject.Parse(SingleJsonObject);
+
+            var name = jsonObject.Value<string>("name");
+            var make = jsonObject.Value<string>("make");
+            var model = jsonObject.Value<string>("model");
+            var year = jsonObject.Value<string>("year");
+
+            var amount = jsonObject.Value<int>("price.amount");
+            var currency = jsonObject.Value<string>("price.currency");
+
+            Console.WriteLine($"A {make} {name} {model} {year} costs {amount} {currency} \n");
+
+            return jsonObject.Count;
+        }
+
+        public int GetValuesUsingSelectToken()
+        {
+            var jArray = JArray.Parse(JsonArray);
+
+            foreach (JObject item in jArray.Children<JObject>())
+            {
+                var name = (string)item.SelectToken("name");
+                var make = (string)item.SelectToken("make");
+                var model = (string)item.SelectToken("model");
+                var year = (int)item.SelectToken("year");
+                var amount = (int)item.SelectToken("price.amount");
+                var currency = (string)item.SelectToken("price.currency");
+
+                Console.WriteLine($"A {make} {name} {model} {year} costs {amount} {currency} \n");
+            }
+
+            return jArray.Count;
+        }
+
+
+        public int GetValuesUsingTryGetValue()
+        {
+            JObject jsonObject = JObject.Parse(SingleJsonObject);
+
+            if (jsonObject.TryGetValue("name", out JToken nameToken))
+            {
+                string name = (string)nameToken;
+                Console.WriteLine($"Name: {name}");
+            }
+
+            if (jsonObject.TryGetValue("make", out JToken makeToken))
+            {
+                string make = (string)makeToken;
+                Console.WriteLine($"Make: {make}");
+            }
+
+            if (jsonObject.TryGetValue("price", out JToken priceToken) && priceToken is JObject priceObject)
+            {
+                if (priceObject.TryGetValue("amount", out JToken amountToken))
+                {
+                    int amount = (int)amountToken;
+                    Console.WriteLine($"Price amount: {amount}");
+                }
+
+                if (priceObject.TryGetValue("currency", out JToken currencyToken))
+                {
+                    string currency = (string)currencyToken;
+                    Console.WriteLine($"Price currency: {currency}");
+                }
+            }
+
+            return jsonObject.Count;
+        }
+    }
+}
---
+++ json-csharp/GetValueByKeyFromJObject/GetValueByKeyFromJObject/TestData.cs
@@ -0,0 +1,64 @@
+﻿namespace GetValueByKeyFromJObject
+{
+    public class TestData
+    {
+        public string GenerateJsonArray()
+        {
+            const string cars = $$"""
+                [
+                  {
+                    "name": "Corvette",
+                    "make": "Chevrolet",
+                    "model": "C8",
+                    "year": 2020,
+                    "price": {
+                      "amount": 67950,
+                      "currency": "USD"
+                    }
+                  },
+                  {
+                    "name": "Mustang",
+                    "make": "Ford",
+                    "model": "GT",
+                    "year": 2021,
+                    "price": {
+                      "amount": 52800,
+                      "currency": "USD"
+                    }
+                  },
+                  
+                  {
+                    "name": "Model 3",
+                    "make": "Tesla",
+                    "model": "Standard Range Plus",
+                    "year": 2021,
+                    "price": {
+                      "amount": 41900,
+                      "currency": "USD"
+                    }
+                  }
+                ]
+                """;
+
+            return cars;
+        }
+
+        public string GenerateSingleJsonObject()
+        {
+            const string car = $$"""
+                {
+                    "name": "Charger",
+                    "make": "Dodge",
+                    "model": "RT",
+                    "year": 2019,
+                    "price": {
+                      "amount": 36100,
+                      "currency": "USD"
+                    } 
+                }
+                """;
+
+            return car;
+        }
+    }
+}
