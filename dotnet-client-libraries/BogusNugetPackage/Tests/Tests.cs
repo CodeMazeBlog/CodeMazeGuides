a538eee5527ecdf814dade7529aa6f6a37546db1
Marko Hrnčić <mahrncic@gmail.com>
2023-01-07 23:14:41 +0100

    Removed unneccessary unit tests

--- dotnet-client-libraries/BogusNugetPackage/Tests/Tests.cs
+++ dotnet-client-libraries/BogusNugetPackage/Tests/Tests.cs
@@ -29,27 +29,6 @@ public class Tests
     }
 
     [Fact]
-    public void WhenGettingSingleDbEmployee_ThenObjectItHasCorrectNumberOfVehicles()
-    {
-        DataGenerator.InitBogusData();
-
-        var employee = DataGenerator.GetSeededEmployeesFromDb().First();
-
-        Assert.Equal(DataGenerator.NumberOfVehiclesPerEmployee, employee.Vehicles.Count);
-    }
-
-    [Fact]
-    public void WhenGettingAllEmployeesFromDb_ThenNumberOfVehiclesIsCorrect()
-    {
-        DataGenerator.InitBogusData();
-        var expectedTotalNumberOfVehicles = DataGenerator.NumberOfEmployees * DataGenerator.NumberOfVehiclesPerEmployee;
-
-        var employees = DataGenerator.GetSeededEmployeesFromDb();
-
-        Assert.Equal(expectedTotalNumberOfVehicles, employees.Sum(x => x.Vehicles.Count));
-    }
-
-    [Fact]
     public void WhenGettingSingleEmployee_ThenEmailIncludesLastName()
     {
         DataGenerator.InitBogusData();
