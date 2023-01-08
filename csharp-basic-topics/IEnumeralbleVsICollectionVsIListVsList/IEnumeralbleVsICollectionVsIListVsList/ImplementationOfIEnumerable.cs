1284a02e2dfd30c53d441ad7b00e6f103cd34c60
OldPen16 <oldpen16@gmail.com>
2023-01-08 19:48:39 +0530

    CM-619 IEnumerable, ICollection, IList and List - Which One to Use?

---
+++ csharp-basic-topics/IEnumeralbleVsICollectionVsIListVsList/IEnumeralbleVsICollectionVsIListVsList/ImplementationOfIEnumerable.cs
@@ -0,0 +1,16 @@
+﻿namespace IEnumeralbleVsICollectionVsIListVsList
+{
+    public class ImplementationOfIEnumerable
+    {
+        public int CountSpecialCharacters(IEnumerable<char> specialCharacters)
+        {
+            var count = 0;
+            foreach(char c in specialCharacters)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
