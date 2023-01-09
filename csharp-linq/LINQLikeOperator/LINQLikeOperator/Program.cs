7f39f936c6e6cb634ca91f7628fec7b974312351
Kenichiro Nakamura <kenakamu@microsoft.com>
2023-01-09 22:59:30 +0900

    add linq like operator sample

---
+++ csharp-linq/LINQLikeOperator/LINQLikeOperator/Program.cs
@@ -0,0 +1,11 @@
+﻿string sqlConnectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={AppDomain.CurrentDomain.BaseDirectory}Data\Database1.mdf;Integrated Security=True";
+DbContextOptions<BlogDbContext> contextOption = new DbContextOptionsBuilder<BlogDbContext>()
+    .UseSqlServer(sqlConnectionString)
+    .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Name }, LogLevel.Information)
+    .Options;
+BlogDbContext blogDbContext = new BlogDbContext(contextOption);
+
+blogDbContext.Database.EnsureCreated();
+List<Article> articles = blogDbContext.Articles.Where(x => x.Title.Contains("n")).ToList();
+int articleCounts = blogDbContext.Articles.Count(x => x.Title.Contains("n"));
+Article? firstArticle = blogDbContext.Articles.FirstOrDefault(x => x.Title.Contains("n"));
