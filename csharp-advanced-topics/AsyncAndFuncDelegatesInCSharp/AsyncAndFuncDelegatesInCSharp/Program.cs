de81bed3336ff791c264c177d60e064dbbf35ff0
Ramirez Vargas, José Pablo <webJose@gmail.com>
2023-01-21 12:31:59 -0600

    Add project and solution files

---
+++ csharp-advanced-topics/AsyncAndFuncDelegatesInCSharp/AsyncAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!  This is a dummy project.  Examine the other C# code files for the article code snippets.");
