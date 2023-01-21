9e23478305a58280c63a5a6185700170ed7f5ec1
OldPen16 <oldpen16@gmail.com>
2023-01-21 12:26:13 +0530

    CM-619: IEnumerable, ICollection, IList and List - Which One to Use?

---
+++ collections-csharp/IEnumeralbleVsICollectionVsIListVsList/IEnumeralbleVsICollectionVsIListVsList/ImplementationOfIEnumerable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace IEnumeralbleVsICollectionVsIListVsList
+{
+    public class ImplementationOfIEnumerable
+    {
+        public int CountSpecialCharacters(IEnumerable<char> specialCharacters)
+        {
+            var count = 0;
+            foreach(char c in specialCharacters)
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        public IEnumerable<int> FilterNumbers(List<int> numbers)
+        {
+            int total = 0;
+            foreach(int num in numbers)
+            {
+                if(num > 2)
+                {
+                    total += num;
+                    yield return total;
+                }   
+            }
+        }
+    }
+}
---
+++ collections-csharp/IEnumeralbleVsICollectionVsIListVsList/IEnumeralbleVsICollectionVsIListVsList/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace IEnumeralbleVsICollectionVsIListVsList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var specialCharacter = new List<char>() {'#','@','$' };
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            var arrayOfSpecialCharacter = new char[] { '=', '!' };
+
+            var enu = new ImplementationOfIEnumerable();
+            Console.WriteLine($"IEnumerable:{enu.CountSpecialCharacters(specialCharacter)}\nIEnumerable With Yield:{ string.Join(",", enu.FilterNumbers(numbers))}");
+                
+            var icl = new ImplementationOfICollection();
+            Console.WriteLine($"ICollection:{icl.CountSpecialCharacters(specialCharacter)}");
+
+            //replacing CountSpecialCharactersArray with CountSpecialCharacters will end up in runtime exception
+            var ils = new ImplementationOfIList();
+            Console.WriteLine($"IList with List input:{ils.CountSpecialCharacters(specialCharacter)}\nIList with Array input:{ils.CountSpecialCharactersArray(arrayOfSpecialCharacter)}");
+
+            var lst = new ImplementationOfList();
+            Console.WriteLine($"List:{lst.CountSpecialCharacters(specialCharacter)}");
+
+            Console.ReadLine();
+        }       
+    }    
+}
