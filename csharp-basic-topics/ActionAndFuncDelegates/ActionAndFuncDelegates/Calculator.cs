9816d53b0dea282c40aec1f5ade6398ecaca8ade
André Minelli <andreminelli@gmail.com>
2023-01-22 11:32:08 -0300

    Add sample code for Action and Func delegates

---
+++ csharp-basic-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Calculator.cs
@@ -0,0 +1,34 @@
+﻿namespace ActionAndFuncDelegates
+{
+    public delegate void Log(string message);
+
+    public delegate long Sum(int firstNumber, int secondNumber);
+
+    public class Calculator
+    {
+        private readonly Log _logger;
+        private readonly Sum _sumOperation;
+
+        public Calculator()
+        {
+            _logger = InternalLogger;
+            _sumOperation = InternalSumOperation;
+        }
+
+        public void Sum(int first, int second)
+        {
+            var result = _sumOperation(first, second);
+            _logger("The result is: " + result);
+        }
+
+        private void InternalLogger(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private long InternalSumOperation(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ csharp-basic-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/CalculatorWithActionFunc.cs
@@ -0,0 +1,30 @@
+﻿namespace ActionAndFuncDelegates
+{
+    public class CalculatorWithActionFunc
+    {
+        private readonly Action<string> _logger;
+        private readonly Func<int, int, long> _sumOperation;
+
+        public CalculatorWithActionFunc()
+        {
+            _logger = InternalLogger;
+            _sumOperation = InternalSumOperation;
+        }
+
+        public void Sum(int first, int second) 
+        { 
+            var result = _sumOperation(first, second); 
+            _logger("The result is: " + result); 
+        }
+
+        private void InternalLogger(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private long InternalSumOperation(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
