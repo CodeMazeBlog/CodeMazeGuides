0d10d66e7ed61a89ecb3e46c9959485e810785e0
Emre Kabali <kabali68@gmail.com>
2023-01-22 21:44:22 +0300

    Added Function and Action guides

---
+++ csharp-basic-topics/ActionAndFunctionDelegatesCsharp/FunctionAndAction/Services/ConverterService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FunctionAndAction.Services
+{
+	public class ConverterService
+	{
+        Func<string, string> _converter;
+		public Action beforeAction;
+
+        public ConverterService(Func<string, string> converter)
+		{
+			_converter = converter;
+		}
+		
+
+		public string  Convert(string convertParameter)
+		{
+			if (beforeAction != null)
+			{
+                beforeAction.Invoke();
+			}
+			return _converter.Invoke(convertParameter);
+		}
+	}
+}
+
