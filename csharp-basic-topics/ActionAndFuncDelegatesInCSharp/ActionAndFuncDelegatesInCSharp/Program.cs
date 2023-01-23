9780b20284b8d83702d970f0c05bca3661939348
tarisai-gundu <tarisai.gundu@gmail.com>
2023-01-23 20:24:10 +0000

    Refactored in line action into method

--- csharp-basic-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
+++ csharp-basic-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -1,15 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 
-
-var printWithColor = (ConsoleColor color, String msg) =>
-{
-    Console.ForegroundColor = color;
-    Console.WriteLine(msg);
-};
+var printWithColor = PrintWithColor;
 
 printWithColor(ConsoleColor.Blue, "This is an informational message");
 printWithColor(ConsoleColor.Yellow, "This is a warning message");
 printWithColor(ConsoleColor.Red, "This is an error message");
 
 Console.ReadLine();
+
+static void PrintWithColor(ConsoleColor color, String msg)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(msg);
+}
