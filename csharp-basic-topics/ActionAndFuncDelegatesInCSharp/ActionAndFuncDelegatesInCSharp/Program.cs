b053a1bbdac5f93e3a6ab88d09cd66943b2d9aae
tarisai-gundu <tarisai.gundu@gmail.com>
2023-01-23 20:09:34 +0000

    Added inline action, print with color.

--- csharp-basic-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
+++ csharp-basic-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Drawing;
+
+
+var printWithColor = (ConsoleColor color, String msg) =>
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(msg);
+};
+
+printWithColor(ConsoleColor.Blue, "This is an informational message");
+printWithColor(ConsoleColor.Yellow, "This is a warning message");
+printWithColor(ConsoleColor.Red, "This is an error message");
+
+Console.ReadLine();
