d75ff4217832ae69861b977184cd218e5338edb4
Djordje Dumbelovic <djordje.dumbelovic@kindredgroup.com>
2023-01-23 09:14:51 +0100

    [Jira-Number]: Change identataion when initialize persons

--- csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/UsingActionAndFunc/Persons.cs
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/UsingActionAndFunc/Persons.cs
@@ -5,11 +5,11 @@
         public static List<Person> GetAll()
         {
             return new List<Person>
-                          {
-                              new () { Name = "Jon", Age = 15 },
-                              new () { Name = "Bob", Age = 20 },
-                              new () { Name = "Mike", Age = 70 }
-                          };
+            {
+                new () { Name = "Jon", Age = 15 },
+                new () { Name = "Bob", Age = 20 },
+                new () { Name = "Mike", Age = 70 }
+            };
         }
     }
 }
