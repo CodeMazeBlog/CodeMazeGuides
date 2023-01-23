5006ecadb177ac75e5f6d8758c9ada33deffef39
Anurag Sinha <anurag3487@gmail.com>
2023-01-23 21:15:51 +0530

    CM 302 - How to Implement Checkbox List in ASP.NET Core (#784)
    * Full Solution Commit - .NET Core MVC and MS Tests Projects
    
    * Trim additional whitespace
    
    * Review Points Completion

---
+++ aspnetcore-features/CheckboxListSolution/CheckboxList/Data/Repository.cs
@@ -0,0 +1,38 @@
+﻿using CheckboxList.Models;
+
+namespace CheckboxList.Data
+{
+    public class Repository
+    {
+        public static IReadOnlyList<CheckboxViewModel> GetCourses()
+        {
+            return new List<CheckboxViewModel>
+            {
+                new CheckboxViewModel
+                {
+                    Id = 1,
+                    LabelName = "Physics",
+                    IsChecked = true
+                },
+                new CheckboxViewModel
+                {
+                    Id = 2,
+                    LabelName = "Chemistry",
+                    IsChecked = false
+                },
+                new CheckboxViewModel
+                {
+                    Id = 3,
+                    LabelName = "Mathematics",
+                    IsChecked = true
+                },
+                new CheckboxViewModel
+                {
+                    Id = 4,
+                    LabelName = "Biology",
+                    IsChecked = false
+                }
+            };
+        }
+    }
+}
