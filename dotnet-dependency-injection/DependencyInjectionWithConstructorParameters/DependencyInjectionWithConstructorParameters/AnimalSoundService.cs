015e6af0a9fab34bc5a9d6b637aeb2253c6262b8
JoshuaKirk <Joshua.kirk@hotmail.co.uk>
2023-01-24 20:38:54 +0000

    Added project and tests for dependency injection with constructor parameteres

---
+++ dotnet-dependency-injection/DependencyInjectionWithConstructorParameters/DependencyInjectionWithConstructorParameters/AnimalSoundService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace DependencyInjectionWithConstructorParameters
+{
+    public class AnimalSoundService : IAnimalSoundService
+    {
+        public readonly List<string> AnimalSounds;
+
+        public AnimalSoundService(IDogSoundService dogSoundService, IConfiguration configuration, IOptions<CowOptions> options, string sheepSound)
+        {
+            AnimalSounds = new List<string>()
+            {
+                dogSoundService.GetSound(),
+                configuration["CatSound"],
+                options.Value.CowSound,
+                sheepSound
+            };
+        }
+
+        public void Play()
+        {
+            foreach (var sound in AnimalSounds)
+            {
+                Console.WriteLine(sound);
+            }
+        }
+    }
+}
--- dotnet-dependency-injection/README.md
+++ dotnet-dependency-injection/README.md
@@ -4,3 +4,4 @@ This section contains the topics related to dependency injection mechanisms in .
 ### Relevant articles:
 
 - [Dependency Injection Lifetimes in ASP.NET Core](https://code-maze.com/dependency-injection-lifetimes-aspnet-core/)
+- [.NET Core Dependency Injection With Constructor Parameters](https://code-maze.com/net-core-dependency-injection-with-constructor-parameters/)
