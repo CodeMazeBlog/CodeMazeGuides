7c726d5368f36a1b6448ccb44c43e88138da5e03
zanbor <44729834+zanbor@users.noreply.github.com>
2023-01-28 23:29:14 +0100

    Refactoring Bloated Code in C# (#799)
    * Refactoring C# projects - examples.
    
    * Structure fixed, readme file added
    
    * Namespace fixed, unit tests added
    
    * readme file removed, targets changed
    
    * Added README file
    
    * Modified tests classes names and methods names
    
    * README file removed
    
    ---------
    
    Co-authored-by: Zaneta Borowska <zaneta.borowska@katoennatie.com>

---
+++ csharp-refactoring/RefactoringCodeBloaters/RefactoringCodeBloaters/PrimitiveObsession/Correct/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace RefactoringCodeBloaters.PrimitiveObsession.Correct
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public Address? Address { get; private set; }
+
+        public void AssignAddress(Address address)
+        {
+            Address = address;
+        }
+    }
+}
