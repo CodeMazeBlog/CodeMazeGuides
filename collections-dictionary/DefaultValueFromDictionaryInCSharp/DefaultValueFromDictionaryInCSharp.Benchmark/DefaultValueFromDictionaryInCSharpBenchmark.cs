705d71339ea346d8c04504a7abf8bd0e0620c6b2
KonstantinosP7 <kwnbet95@gmail.com>
2023-01-30 00:38:24 +0200

    DefaultValueFromDictionaryInCSharp

---
+++ collections-dictionary/DefaultValueFromDictionaryInCSharp/DefaultValueFromDictionaryInCSharp.Benchmark/DefaultValueFromDictionaryInCSharpBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkRunner
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class DefaultValueFromDictionaryInCSharpBenchmark
+    {
+        private Dictionary<string, int> myDictionary = fillDictionary();
+
+        public static Dictionary<string, int> fillDictionary()
+        {
+            var myDictionary = new Dictionary<string, int>();
+
+            for (int i = 1; i < 10000; i++)
+            {
+                myDictionary.Add($"number_{i}", i);
+            }
+            return myDictionary;
+        }
+
+        private readonly string key = "number_1000";
+
+        [Benchmark]
+        public int ContainsKey()
+        {
+            return myDictionary.ContainsKey(key) ? myDictionary[key] : default;
+        }
+
+
+        [Benchmark]
+        public int TryGetValue()
+        {
+            return myDictionary.TryGetValue(key, out var value) ? value : default;
+        }
+
+        [Benchmark]
+        public int GetValueOrDefault()
+        {
+            return myDictionary.GetValueOrDefault(key);
+        }
+    }
+}
