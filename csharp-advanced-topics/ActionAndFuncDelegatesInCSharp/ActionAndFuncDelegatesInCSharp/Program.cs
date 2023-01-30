505394dd3cdda72d18cf2b64420d0b379d65d320
Sesethu Mhlana <smhlana98@gmail.com>
2023-01-30 15:21:29 +0200

    Showcase func and action delegates.

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ActionAndFuncDelegatesInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var areaDelegate = new Func<int, int, double>(Area);
+            Console.WriteLine(areaDelegate.Invoke(3, 7));
+
+            var displayDelegate = new Action<string>(Display);
+            displayDelegate("Hello, World!");
+        }
+
+        public static double Area(int b, int h)
+        {
+            return (b * h) / 2.0;
+        }
+
+        public static void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
