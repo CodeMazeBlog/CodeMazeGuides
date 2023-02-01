1c9523b20ef2d0295528f3cc29a2b2164a978c1a
Kenichiro Nakamura <kenakamu@microsoft.com>
2023-02-01 19:17:11 +0900

    CM-349: How to Use SQL LIKE operator in C# LINQ (#850)

--- csharp-linq/LINQLikeOperator/LINQLikeOperator/Program.cs
+++ csharp-linq/LINQLikeOperator/LINQLikeOperator/Program.cs
@@ -8,9 +8,14 @@ var contextOption = new DbContextOptionsBuilder<BlogDbContext>()
 var blogDbContext = new BlogDbContext(contextOption);
 blogDbContext.Database.EnsureCreated();
 
+// Use Contains, StartsWith and EndsWith methods
 List<Article> articles = blogDbContext.Articles.Where(x => x.Title.Contains("n")).ToList();
 int articleCounts = blogDbContext.Articles.Count(x => x.Title.Contains("n"));
 Article? firstArticle = blogDbContext.Articles.FirstOrDefault(x => x.Title.Contains("n"));
 
 articles = blogDbContext.Articles.Where(x => x.Title.StartsWith("n")).ToList();
 articles = blogDbContext.Articles.Where(x => x.Title.EndsWith("n")).ToList();
+
+// Use _ wildcard
+articles = blogDbContext.Articles.Where(x => x.Title.StartsWith("_n")).ToList();
+articles = blogDbContext.Articles.Where(x => EF.Functions.Like(x.Title, "_n%")).ToList();
