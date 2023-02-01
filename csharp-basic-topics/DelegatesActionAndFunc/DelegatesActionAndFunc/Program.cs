bff12e4b3210a1c9e537135c64cf3d485bbfc00b
Aneta Muslić <Aneta.Muslic@blu.com>
2023-02-01 10:42:13 +0100

    added init examples for action and func

---
+++ csharp-basic-topics/DelegatesActionAndFunc/DelegatesActionAndFunc/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DelegatesActionAndFunc;
+
+public class Program
+{
+    public static string Capitalize(string input)
+    {
+        return string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1));
+    }
+
+    public static void LogCurrentUTCDateTime()
+
+    {
+        Console.WriteLine($"Current date time is {DateTime.Now}");
+    }
+
+    public static void Main()
+    {
+        #region Action type delegate
+        Action writeCurrentTimeDelegate = () => Console.WriteLine($"Current date time is {DateTime.Now}");
+        writeCurrentTimeDelegate();
+
+        #endregion
+
+        #region Func type delegate
+        string myName = "john";
+        Func<string, string> capitalizeDelegate = Capitalize;
+        Console.WriteLine(capitalizeDelegate(myName)); //prints "John"
+        #endregion
+    }
+}
