7fc3be604e05690699885b68ac4dcdc30591259e
Aneta Muslić <Aneta.Muslic@blu.com>
2023-02-01 11:09:31 +0100

    added unit tests

---
+++ csharp-basic-topics/DelegatesActionAndFunc/DelegatesActionAndFunc.Tests/ActionAndFuncUnitTest.cs
@@ -0,0 +1,35 @@
+using Xunit;
+
+namespace DelegatesActionAndFunc.Tests;
+
+public class ActionAndFuncUnitTest
+{
+
+    [Theory]
+    [InlineData("JOHN", "JOHN")]
+    [InlineData("penny", "Penny")]
+    public void Capitalize_DifferentInput_ReturnsCorrectOutput(string input, string expectedOutput)
+    {
+        //Act
+        var capitalizedWord = Program.Capitalize(input);
+
+        //Assert
+        Assert.Equal(capitalizedWord, expectedOutput);
+    }
+
+    [Fact]
+    public void LogCurrentUTCDateTime_WritesOutputToConsole()
+    {
+        //Arrange
+        StringWriter stringWriter = new ();
+        Console.SetOut(stringWriter);
+        var currentDate = DateTime.Now.ToString("dd/MM/yyyy");
+        string expectedStartOfLoggedOutput = $"Current date time is {currentDate}";
+
+        //Act
+        Program.LogCurrentUTCDateTime();
+
+        //Assert
+        Assert.StartsWith(expectedStartOfLoggedOutput, stringWriter.ToString());
+    }
+}
--- csharp-basic-topics/DelegatesActionAndFunc/DelegatesActionAndFunc/Program.cs
+++ csharp-basic-topics/DelegatesActionAndFunc/DelegatesActionAndFunc/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace DelegatesActionAndFunc;
 
-public class Program
+public static class Program
 {
     public static string Capitalize(string input)
     {
