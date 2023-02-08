c9d68b09df3908d86001632fefd410eab4ae056f
Aneta Muslić <Aneta.Muslic@blu.com>
2023-02-08 18:08:32 +0100

    adding new solution

--- aspnetcore-webapi/SwaggerBaseUri/SwaggerBaseUri/SwaggerBaseUri/Controllers/WeatherForecastController.cs
+++ aspnetcore-webapi/SwaggerBaseUri/SwaggerBaseUri/SwaggerBaseUri/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace SwaggerBaseUri.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
