fe66d569ef503dc8a40cfaaab3883f488b992dad
TechnoTron100 <technologiestron@gmail.com>
2023-02-10 03:08:20 +0100

    TechnoTron100/technologiestron@gmail.com

---
+++ csharp-basic-topics/Action_Func_Delegates/Action_Func_Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Action<int, int> multiply = (x, y) => Console.WriteLine(x * y);
+        Func<int, int, int> add = (x, y) => x + y;
+
+        void Process(Action<int, int> action) { action(10, 20); }
+
+        int ProcessFunc(Func<int, int, int> func) { return func(10, 20); }
+        int result = ProcessFunc(add); //result = 30
+
+    }
+}
