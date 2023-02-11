e9565247c0a6767efc6129d3aab0d700bc31d3a3
Alvaro Montoya <alvaro.montoya@getrhyme.com>
2023-02-11 16:35:30 -0500

    WIP

---
+++ blazor-features/BlazorWebAssemblyCustomEvents/BlazorWebAssemblyCustomEvents/CustomEvents/EventHandlers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebAssemblyCustomEvents.CustomEvents
+{
+    [EventHandler("onfilechange", typeof(FileChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    public static class EventHandlers
+    {
+        // purposefully empty
+    }
+
+    public class FileChangeEventArgs : EventArgs
+    {
+        public DateTime? EventTimestamp { get; set; }
+        //public byte[]? FileData { get; set; }
+        //public string? FileExtension { get; set; }
+    }
+}
