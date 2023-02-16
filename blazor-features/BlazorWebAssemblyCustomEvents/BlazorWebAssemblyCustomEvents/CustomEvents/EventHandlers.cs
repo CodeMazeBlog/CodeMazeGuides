af1f0d82e57a82bec8d2f3b4cc65cc1020b259f6
Alvaro Montoya <alvaro.montoya@knights.ucf.edu>
2023-02-15 21:30:47 -0500

    WIP got double click detection working

--- blazor-features/BlazorWebAssemblyCustomEvents/BlazorWebAssemblyCustomEvents/CustomEvents/EventHandlers.cs
+++ blazor-features/BlazorWebAssemblyCustomEvents/BlazorWebAssemblyCustomEvents/CustomEvents/EventHandlers.cs
@@ -3,17 +3,15 @@ using Microsoft.AspNetCore.Components.Forms;
 
 namespace BlazorWebAssemblyCustomEvents.CustomEvents
 {
-    [EventHandler("onfilechange", typeof(FileChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("ondoubleclick", typeof(DoubleClickEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
     public static class EventHandlers
     {
         // purposefully empty
     }
 
-    public class FileChangeEventArgs : EventArgs
+    public class DoubleClickEventArgs : EventArgs
     {
         public DateTime? EventTimestamp { get; set; }
-        //public byte[]? FileData { get; set; }
-        public string? FileExtension { get; set; }
-        public string? Value { get; set; }
+        public bool isDoubleClick { get; set; } = false;
     }
 }
