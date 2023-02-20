0acad95ba02792100ec32e57dd67f34d35958ede
Alvaro Montoya <alvaro.montoya@knights.ucf.edu>
2023-02-20 16:25:40 -0500

    added tests and made doubleclickbutton component

---
+++ blazor-features/BlazorWebAssemblyCustomEvents/BlazorWebAssemblyCustomEventsTests/DoubleClickEventTests.cs
@@ -0,0 +1,56 @@
+using AngleSharp.Dom;
+using BlazorWebAssemblyCustomEvents.Components;
+using BlazorWebAssemblyCustomEvents.CustomEvents;
+using Bunit;
+
+namespace BlazorWebAssemblyCustomEventsTests
+{
+    [TestClass]
+    public class DoubleClickEventTests
+    {
+        public IElement? button;
+        public IRenderedComponent<DoubleClickButton>? component;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            var context = new Bunit.TestContext();
+            component = context.RenderComponent<DoubleClickButton>();
+            button = component.Find("button");
+        }
+
+        [TestMethod]
+        public void WhenDoubleClickEventRaised_ThenDoubleClickHandlerIsInvoked()
+        {
+            button?.TriggerEvent("ondoubleclick", new DoubleClickEventArgs
+            {
+                EventTimestamp = DateTime.Now,
+                isDoubleClick = true
+            });
+
+            var headerText = component?.FindAll("h1")[1].TextContent;
+
+            Assert.AreEqual("detected double click!", headerText);
+        }
+
+        [TestMethod]
+        public void WhenClickEventRaised_GivenLastInteractionWasADoubleClick_ThenMessageIsReset()
+        {
+            button?.TriggerEvent("ondoubleclick", new DoubleClickEventArgs
+            {
+                EventTimestamp = DateTime.Now,
+                isDoubleClick = true
+            });
+
+            button?.TriggerEvent("ondoubleclick", new DoubleClickEventArgs
+            {
+                EventTimestamp = DateTime.Now,
+                isDoubleClick = false
+            });
+
+            var headerText = component?.FindAll("h1")[1].TextContent;
+
+            Assert.AreEqual("", headerText);
+        }
+    }
+}
