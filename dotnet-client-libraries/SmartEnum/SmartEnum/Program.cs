ef5a04e731ae3d685b940f2343be3c7c0ffe43d6
Ivan Gechev <ivan_banov@abv.bg>
2023-02-20 19:52:59 +0200

    Final examples

--- dotnet-client-libraries/SmartEnum/SmartEnum/Program.cs
+++ dotnet-client-libraries/SmartEnum/SmartEnum/Program.cs
@@ -9,6 +9,28 @@
             Console.WriteLine($"{dev.Name} is a {dev.Level} developer.");
             Console.WriteLine($"{dev.Name}'s productivity is {dev.Level.Productivity} lines/hour.");
             Console.WriteLine($"The developer can write 500 lines of code in {dev.WriteCode(500):F2} hours.");
+
+            var juniorFromName = DeveloperLevel.FromName("Junior");
+            var juniorFromValue = DeveloperLevel.FromValue(1);
+
+            Console.WriteLine(juniorFromName == juniorFromValue);
+
+            if (DeveloperLevel.TryFromName("Regular", out var regularFromName))
+            {
+                Console.WriteLine($"Created enumeration from name: {regularFromName.Name}");
+            }
+
+            if (DeveloperLevel.TryFromValue(2, out var regularFromValue))
+            {
+                Console.WriteLine($"Created enumeration from value: {regularFromValue.Name}");
+            }
+
+            Console.WriteLine($"The {nameof(DeveloperLevel)} has {DeveloperLevel.List.Count} different enumerations:");
+
+            foreach (var enumeration in DeveloperLevel.List)
+            {
+                Console.WriteLine(enumeration.Name);
+            }
         }
     }
 }
