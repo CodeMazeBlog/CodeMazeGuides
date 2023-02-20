26e9ad0cdd4641b5926164a9185eb4190e77fdae
atanas.nikolovski <atenikolovski@gmail.com>
2023-02-20 22:29:32 +0100

    Created new solution for delegates

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Example of Action delegate
+static void PrintMessage() 
+{ 
+    Console.WriteLine("Hello, world!"); 
+}
+static void ExecuteAction(Action action) 
+{ 
+    action(); 
+}
+ExecuteAction(PrintMessage);
+
+//Example of Func delegates
+static int AddNumbers(int a, int b) 
+{ 
+    return a + b; 
+}
+static void ExecuteFunc(Func<int, int, int> func) 
+{ 
+    int result = func(10, 20); 
+    Console.WriteLine("Result = " + result); 
+}
+
+ExecuteFunc(AddNumbers);
