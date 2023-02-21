c18cf8fe519c1b173abcabd9918b5ce77b6e5af5
cjaynjoku <njokujennaro@gmail.com>
2023-02-21 17:40:57 +0100

    CM-449: Read and Parse a JSON File in C#

---
+++ json-csharp/ReadAndParseAJSONFileInCSharp/ReadAndParseAJSONFileInCSharp/ReadAndParseJsonFileWithSystemTextJson.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace ReadAndParseAJSONFileInCSharp
+{
+    public class ReadAndParseJsonFileWithSystemTextJson
+    {
+        private const string _sampleJsonFile = @"../../../Data/teachers-json.json";
+        private static readonly JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static List<Teacher> UseStreamReaderWithSystemTextJson(string sampleJsonFile = _sampleJsonFile)
+        {
+            using StreamReader streamReader = new(sampleJsonFile);
+            var json = streamReader.ReadToEnd();
+            List<Teacher> teachers = JsonSerializer.Deserialize<List<Teacher>>(json, options);
+
+            return teachers;
+        }
+
+        public static List<Teacher> UseFileReadAllTextWithSystemTextJson(string sampleJsonFile = _sampleJsonFile)
+        {
+            var json = File.ReadAllText(sampleJsonFile);
+            List<Teacher> teachers = JsonSerializer.Deserialize<List<Teacher>>(json, options);
+
+            return teachers;
+        }
+
+        public static List<Teacher> UseFileOpenReadTextWithSystemTextJson(string sampleJsonFile = _sampleJsonFile)
+        {
+            using FileStream json = File.OpenRead(sampleJsonFile);
+            List<Teacher> teachers = JsonSerializer.Deserialize<List<Teacher>>(json, options);
+
+            return teachers;
+        }
+    }
+}
