527850fcdd25748faadb2785e047e39dd0ea581f
Rahul Kumar jha <rahul.jha@gartner.com>
2023-02-23 18:16:45 +0530

    formating code

--- csharp-linq/LINQInnerJoin/LinqInnerJoin/EnrolmentHelper.cs
+++ csharp-linq/LINQInnerJoin/LinqInnerJoin/EnrolmentHelper.cs
@@ -10,16 +10,17 @@
 
             var enrolmensRelation = GetEnrolmentsRelation(enrolments, students, courses);
 
-            var result = enrolmensRelation
-                             .Select(e => new
-                             {
-                                 e.enrolmentLevel2.enrolmentLevel1.Id,
-                                 StudentName = e.enrolmentLevel2.student.Name,
-                                 CourseName = e.course.Name,
-                             });
+            var result = enrolmensRelation.Select(
+                e =>
+                    new
+                    {
+                        e.enrolmentLevel2.enrolmentLevel1.Id,
+                        StudentName = e.enrolmentLevel2.student.Name,
+                        CourseName = e.course.Name,
+                    }
+            );
 
             return result;
-
         }
 
         public static IEnumerable<dynamic> FilterEnrolments(IEnumerable<Enrolment> enrolments)
@@ -31,20 +32,24 @@
             var enrolmensRelation = GetEnrolmentsRelation(enrolments, students, courses);
 
             var result = enrolmensRelation
-                             .Select(e => new
-                             {
-                                 e.enrolmentLevel2.enrolmentLevel1.Id,
-                                 StudentName = e.enrolmentLevel2.student.Name,
-                                 CourseId = e.course.Id,
-                                 CourseName = e.course.Name,
-                             })
-                             .Where(x => x.CourseId == 1);
+                .Select(
+                    e =>
+                        new
+                        {
+                            e.enrolmentLevel2.enrolmentLevel1.Id,
+                            StudentName = e.enrolmentLevel2.student.Name,
+                            CourseId = e.course.Id,
+                            CourseName = e.course.Name,
+                        }
+                )
+                .Where(x => x.CourseId == 1);
 
             return result;
-
         }
 
-        public static IEnumerable<dynamic> FilterAndGroupEnrolments(IEnumerable<Enrolment> enrolments)
+        public static IEnumerable<dynamic> FilterAndGroupEnrolments(
+            IEnumerable<Enrolment> enrolments
+        )
         {
             var students = Student.GetDummyStudents();
 
@@ -53,15 +58,18 @@
             var enrolmensRelation = GetEnrolmentsRelation(enrolments, students, courses);
 
             var result = enrolmensRelation
-                            .Select(e => new
-                            {
-                                e.enrolmentLevel2.enrolmentLevel1.Id,
-                                StudentName = e.enrolmentLevel2.student.Name,
-                                CourseId = e.course.Id,
-                                CourseName = e.course.Name,
-                            })
-                            .Where(x => x.CourseId < 5)
-                            .GroupBy(x => x.CourseId);
+                .Select(
+                    e =>
+                        new
+                        {
+                            e.enrolmentLevel2.enrolmentLevel1.Id,
+                            StudentName = e.enrolmentLevel2.student.Name,
+                            CourseId = e.course.Id,
+                            CourseName = e.course.Name,
+                        }
+                )
+                .Where(x => x.CourseId < 5)
+                .GroupBy(x => x.CourseId);
 
             return result;
         }
@@ -70,15 +78,10 @@
         {
             var catrgories = Category.GetDummyCourseCategories();
 
-            var courseWithCategory = from course in courses
-                                     join category in catrgories
-                                     on course.CategoryId equals category.Id
-                                     select new
-                                     {
-                                         course.Id,
-                                         course.Name,
-                                         CategoryName = category.Name
-                                     };
+            var courseWithCategory =
+                from course in courses
+                join category in catrgories on course.CategoryId equals category.Id
+                select new { course.Id, course.Name, CategoryName = category.Name };
 
             return courseWithCategory;
         }
@@ -88,15 +91,11 @@
             var categories = Category.GetDummyCourseCategories();
 
             var courseWithCategoryName = courses.Join(
-                                         categories,
-                                         course => course.CategoryId,
-                                         category => category.Id,
-                                         (course, category) => new
-                                         {
-                                             course.Id,
-                                             course.Name,
-                                             CategoryName = category.Name
-                                         });
+                categories,
+                course => course.CategoryId,
+                category => category.Id,
+                (course, category) => new { course.Id, course.Name, CategoryName = category.Name }
+            );
 
             return courseWithCategoryName;
         }
@@ -105,7 +104,9 @@
         {
             foreach (var course in courses)
             {
-                Console.WriteLine($"Id: {course.Id} Course: {course.Name}  Category: {course.CategoryName}");
+                Console.WriteLine(
+                    $"Id: {course.Id} Course: {course.Name}  Category: {course.CategoryName}"
+                );
             }
         }
 
@@ -113,7 +114,9 @@
         {
             foreach (var course in Course.GetDummyCourses())
             {
-                Console.WriteLine($"Id: {course.Id} Course: {course.Name}  CategoryId: {course.CategoryId} ");
+                Console.WriteLine(
+                    $"Id: {course.Id} Course: {course.Name}  CategoryId: {course.CategoryId} "
+                );
             }
         }
 
@@ -129,7 +132,9 @@
         {
             foreach (var enrolment in enrolments)
             {
-                Console.WriteLine($"Id: {enrolment.Id}, Student Name: {enrolment.StudentName}, Course: {enrolment.CourseName} \n");
+                Console.WriteLine(
+                    $"Id: {enrolment.Id}, Student Name: {enrolment.StudentName}, Course: {enrolment.CourseName} \n"
+                );
             }
         }
 
@@ -140,25 +145,32 @@
                 Console.WriteLine($"Enrolments for Course Id: {group.Key} \n");
                 foreach (var enrolment in group)
                 {
-                    Console.WriteLine($"Id: {enrolment.Id}, Student Name: {enrolment.StudentName}, Course: {enrolment.CourseName} \n");
+                    Console.WriteLine(
+                        $"Id: {enrolment.Id}, Student Name: {enrolment.StudentName}, Course: {enrolment.CourseName} \n"
+                    );
                 }
             }
         }
 
-        private static IEnumerable<dynamic> GetEnrolmentsRelation(IEnumerable<Enrolment> enrolments, IEnumerable<Student> students, IEnumerable<Course> courses)
+        private static IEnumerable<dynamic> GetEnrolmentsRelation(
+            IEnumerable<Enrolment> enrolments,
+            IEnumerable<Student> students,
+            IEnumerable<Course> courses
+        )
         {
             return enrolments
-                                        .Join(
-                                           students,
-                                           enrolmentLevel1 => enrolmentLevel1.StudentId,
-                                           student => student.Id,
-                                           (enrolmentLevel1, student) => new { enrolmentLevel1, student })
-                                        .Join(
-                                           courses,
-                                           enrolmentLevel2 => enrolmentLevel2.enrolmentLevel1.CourseId,
-                                           course => course.Id,
-                                           (enrolmentLevel2, course) => new { enrolmentLevel2, course });
+                .Join(
+                    students,
+                    enrolmentLevel1 => enrolmentLevel1.StudentId,
+                    student => student.Id,
+                    (enrolmentLevel1, student) => new { enrolmentLevel1, student }
+                )
+                .Join(
+                    courses,
+                    enrolmentLevel2 => enrolmentLevel2.enrolmentLevel1.CourseId,
+                    course => course.Id,
+                    (enrolmentLevel2, course) => new { enrolmentLevel2, course }
+                );
         }
     }
-
 }
