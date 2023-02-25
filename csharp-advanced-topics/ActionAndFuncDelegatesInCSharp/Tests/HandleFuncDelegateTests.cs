eaae8cd090202a9e7d8a4a5e046f32bf8a11a9c6
john <chukwuchidieberejohn@gmail.com>
2023-02-25 03:44:08 +0100

    Action and Func Delegate in C#

--- csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/Tests/HandleFuncDelegateTests.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/Tests/HandleFuncDelegateTests.cs
@@ -5,13 +5,11 @@ public class HandleFuncDelegateTests
     [Fact]
     public void PrintFuncDelegate_PrintsResultToConsole()
     {
-        // Arrange
         StringWriter consoleOutput = new StringWriter();
         Console.SetOut(consoleOutput);
 
         HandleFuncDelegate.PrintFuncDelegate();
 
-        // Assert
         Assert.Equal($"\nResult = 40{Environment.NewLine}", consoleOutput.ToString());
     }
 }
