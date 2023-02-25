0a51eb4185d682dd1f8d9f4fc6a69b5872871bda
Matjaž Prtenjak <matjaz@matjazev.net>
2023-02-25 14:20:47 +0100

    Start working

---
+++ csharp-basic-topics/Zip-files-in-NET/Zip-files-in-NET/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Compression;
+using Zip_files_in_NET;
+
+var readZipStructure = new ReadZipStructure();
+
+readZipStructure.WriteName("single-file.zip");
+readZipStructure.WriteName("multi-file.zip");
+readZipStructure.WriteName("multi-folder.zip");
+
+readZipStructure.WriteFullName("single-file.zip");
+readZipStructure.WriteFullName("multi-file.zip");
+readZipStructure.WriteFullName("multi-folder.zip");
+
+Console.ReadLine();
---
+++ csharp-basic-topics/Zip-files-in-NET/Zip-files-in-NET/ReadZipStructure.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.IO.Compression;
+
+namespace Zip_files_in_NET
+{
+  public class ReadZipStructure
+  {
+    public void WriteName(string zipFileName) =>
+      ExecuteWrite(zipFileName, (entry) => entry.Name);
+
+    public void WriteFullName(string zipFileName) =>
+      ExecuteWrite(zipFileName, (entry) => entry.FullName);
+
+    private void ExecuteWrite(string zipFileName, Func<ZipArchiveEntry, string> element)
+    {
+      int counter = 0;
+
+      var zipFile = ZipFile.OpenRead(zipFileName);
+      foreach (var entry in zipFile.Entries)
+        Console.WriteLine($"{++counter,3}: {element(entry)}");
+
+      Console.WriteLine("\n----------\n");
+    }
+  }
+}
