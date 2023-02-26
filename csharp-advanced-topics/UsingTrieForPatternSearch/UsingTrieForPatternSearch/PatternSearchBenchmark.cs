fcae014a6f9d1a87aad0eadf8fdcc08a2df14692
Satya <satyap48@gmail.com>
2023-02-26 14:24:42 +0530

    CM-486: Using Trie Class for Efficient Text Pattern Searching in C#

---
+++ csharp-advanced-topics/UsingTrieForPatternSearch/UsingTrieForPatternSearch/PatternSearchBenchmark.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace UsingTrieForPatternSearch
+{
+    public class PatternSearchBenchmark
+    {
+        private const string _text = "abcdefghijklmnopqrstuvwxyz";
+        private const string _pattern = "cd";
+        private const int _iterations = 1000000;
+
+        [Benchmark]
+        public void ListSearch()
+        {
+            for (int i = 0; i < _iterations; i++)
+            {
+                ListPatternSearcher.Search(_text, _pattern);
+            }
+        }
+
+        [Benchmark]
+        public void HashSetSearch()
+        {
+            for (int i = 0; i < _iterations; i++)
+            {
+                ListPatternSearcher.Search(_text, _pattern);
+            }
+        }
+
+        [Benchmark]
+        public void SortedListSearch()
+        {
+            for (int i = 0; i < _iterations; i++)
+            {
+                SortedListPatternSearcher.Search(_text, _pattern);
+            }
+        }
+
+        [Benchmark]
+        public void TrieSearch()
+        {
+            var trie = new Trie();
+            trie.AddWord(_pattern);
+
+            for (int i = 0; i < _iterations; i++)
+            {
+                trie.Search(_text);
+            }
+        }
+    }
+}
