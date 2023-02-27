4cad1d12376fafd026a823901c93e644355d7eee
Satya Prakash <51241619+lasaetarubia@users.noreply.github.com>
2023-02-27 22:31:28 +0530

    CM-37: Access Modifiers In C# (#861)
    * CM-37: Access Specifiers In C#
    
    * CM-37: Updates to code based on suggestions
    
    * CM-37: Removed an unnecessary comment
    
    * CM-37 - Changing int to var
    
    * CM-37: Improvement suggestions implemented
    
    * CM-37: Renamed AccesSpecifiersInCSharp to AccessModifiersInCSharp

---
+++ csharp-basic-topics/AccessModifiersInCsharp/AccessModifiersInCsharp/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace AccessModifiersInCsharp
+{
+    public class Employee
+    {
+        private protected int EmployeeId { get; set; }
+        private protected string Name { get; set; }
+        private protected double Salary { get; set; }        
+
+        public virtual string GetEmployeeDetails()
+        {
+            return $"Employee ID: {EmployeeId}, Name: {Name}, Salary: {Salary}";
+        }
+    }
+}
