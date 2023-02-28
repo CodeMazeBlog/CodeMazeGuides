3d9123df41040189248f34d577c03216f0b26c64
DESKTOP-RUA7L5E\Aqsa Yasin <aqsayasintw@gmail.com>
2023-02-28 11:14:21 +0500

    Initial Commit
    1st Project

---
+++ csharp-advanced-topics/Example1/delegate4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Test {
+
+            public static void Main() {
+
+                        Action NonGen = new Action(Display);
+
+                        NonGen();
+
+            }
+
+            public static void Display() {
+
+                        Console.WriteLine("Non-generic Action Delegate.");
+
+    }
+
+ }
