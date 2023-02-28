92841ebc6cda75ba9b2c90581ee445f3cfdff43d
DESKTOP-RUA7L5E\Aqsa Yasin <aqsayasintw@gmail.com>
2023-02-28 11:25:30 +0500

    example3

---
+++ csharp-advanced-topics/Example1/Example3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Test
+
+{
+
+    public static void Main()
+
+    {
+
+        Func<int, int, int> Res = Multiply;
+
+        int val = Res(4, 9);
+
+        Console.WriteLine(val);
+
+    }
+
+    public static int Multiply(int a, int b)
+    {
+
+        return a * b;
+
+    }
+
+}
