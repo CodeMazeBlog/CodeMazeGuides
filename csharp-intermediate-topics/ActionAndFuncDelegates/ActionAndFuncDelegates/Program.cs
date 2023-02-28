4f9bfd60cc124e51be93f5299014eff42b5631d5
Enos Wakoko <enoswakoko@gmail.com>
2023-03-01 00:34:32 +0300

    Removed the .gitignore file and modified the user input prompts

--- csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
+++ csharp-intermediate-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -4,14 +4,14 @@ var scores = new List<int> { 67, 54, 32, 76, 99, 65 };
 
 Action<List<int>> action;
 Console.WriteLine("----- Actions --------\n");
-Console.WriteLine("Enter 1 for the top results and anything else for the bottom: ");
+Console.Write("Enter 1 for the top results and anything else for the bottom: ");
 var input = Console.ReadLine() ?? "";
 action = input.Equals("1") ? DisplayResults.Top : DisplayResults.Bottom;
 action(scores);
 
 Func<List<int>, int> func;
 Console.WriteLine("\n----- Funcs --------\n");
-Console.WriteLine("Enter 1 for the top results and anything else for the bottom: ");
+Console.Write("Enter 1 for the top results and anything else for the bottom: ");
 input = Console.ReadLine() ?? "";
 func = input.Equals("1") ? GetResults.Top : GetResults.Bottom;
 Console.WriteLine(func(scores));
