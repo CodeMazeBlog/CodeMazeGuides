12ae5f21d2cb883d196be3beb1a130e8cc2c808a
DESKTOP-RUA7L5E\Aqsa Yasin <aqsayasintw@gmail.com>
2023-02-28 11:22:59 +0500

    Example 2

---
+++ csharp-advanced-topics/Example1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Test
+
+{
+
+    public static void Main(string[] args)
+    {
+
+        Action<int> First = One;
+
+        Action<string, string> Second = Two;
+
+        First(14);
+
+        Second("Action", "Generic");
+
+    }
+
+    public static void One(int a)
+    {
+
+        Console.WriteLine(a);
+
+    }
+
+    public static void Two(string a, string b)
+    {
+
+        Console.WriteLine(a + b);
+
+    }
+
+}
