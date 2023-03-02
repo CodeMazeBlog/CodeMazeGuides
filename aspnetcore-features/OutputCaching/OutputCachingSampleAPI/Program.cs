5e5c170583138ac2d4ccc7f6b2306ae39959ab3a
RPM1984 <rpmir1@gmail.com>
2023-03-03 08:43:18 +1100

    Updated Program.cs

--- aspnetcore-features/OutputCaching/OutputCachingSampleAPI/Program.cs
+++ aspnetcore-features/OutputCaching/OutputCachingSampleAPI/Program.cs
@@ -1,16 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddOutputCache(options =>
 {
     options.AddBasePolicy(builder =>
-        builder.Expire(TimeSpan.FromSeconds(5))
+        builder.Expire(TimeSpan.FromSeconds(35))
                 .Tag("tag-all"));
 
     options.AddPolicy("CacheForTenSeconds", builder =>
@@ -25,7 +22,6 @@ builder.Services.AddOutputCache(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -33,11 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.UseOutputCache();
 
 app.MapGet("/etag", async (context) =>
