a77deeefbc3a9f503dc78d3f8b27b1dbde986c6a
cjaynjoku <njokujennaro@gmail.com>
2023-03-02 11:00:56 +0100

    CM-453: How to Serialize a list to JSON in C#

---
+++ json-csharp/HowToSerializeAListToJsonInCSharp/HowToSerializeAListToJsonInCSharp/SerializeMethodsBenchmark.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace HowToSerializeAListToJsonInCSharp
+{
+    [MemoryDiagnoser(false)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class SerializeMethodsBenchmark
+    {
+        private static readonly List<Club> _benchmarkLists
+            = GenerateBenchmarkData.Generate10000ListsForBenchmark();
+        private readonly SerializeListToJsonWithNewtonsoftJson _serializeWithNewtonsoftJson
+            = new(_benchmarkLists);
+        private readonly SerializeListToJsonWithSystemTextJson _serializeWithSystemTextJson
+            = new(_benchmarkLists);
+
+        [Benchmark]
+        public void UseTheSerializeObjectMethod()
+            => _serializeWithNewtonsoftJson.UseTheSerializeObjectMethod();
+
+        [Benchmark]
+        public void UseTheJsonSerializerClass()
+            => _serializeWithNewtonsoftJson.UseTheJsonSerializerClass();
+
+        [Benchmark]
+        public void UseTheSerializeMethod()
+            => _serializeWithSystemTextJson.UseTheSerializeMethod();
+
+        [Benchmark]
+        public void UseTheSerializeToUtf8BytesMethod()
+            => _serializeWithSystemTextJson.UseTheSerializeToUtf8BytesMethod();
+    }
+}
