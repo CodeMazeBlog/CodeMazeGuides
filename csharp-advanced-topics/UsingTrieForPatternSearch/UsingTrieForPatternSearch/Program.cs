17f5466dec1637efa57a2c86ef8b93e0d6e78f18
Satya Prakash <51241619+lasaetarubia@users.noreply.github.com>
2023-03-04 00:54:43 +0530

    CM-486: Using Trie Class for Efficient Text Pattern Searching in C# (#911)
    * CM-486: Using Trie Class for Efficient Text Pattern Searching in C#
    
    * CM-486: Implementing Review Suggestions
    
    * CM-486: Reordering Benchmark methods
    
    * CM-486: Getting Trie Initialization out of Benchmark

---
+++ csharp-advanced-topics/UsingTrieForPatternSearch/UsingTrieForPatternSearch/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace UsingTrieForPatternSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PatternSearchBenchmark>();
+            Console.WriteLine(summary);
+        }
+    }
+}
