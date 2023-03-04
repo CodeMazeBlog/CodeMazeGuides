7b4fc5825fa8d1e55346a5c8580ce1712aacc688
marktacker <markthomasacker@gmail.com>
2023-03-04 10:23:04 -0500

    formatting correction
    corrected formatting of WeatherFroecastController.cs to look nicer

--- aspnetcore-features/OutputCaching/OutputCachingSampleAPI/Controllers/WeatherForecastController.cs
+++ aspnetcore-features/OutputCaching/OutputCachingSampleAPI/Controllers/WeatherForecastController.cs
@@ -9,8 +9,8 @@ namespace OutputCachingSampleAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
