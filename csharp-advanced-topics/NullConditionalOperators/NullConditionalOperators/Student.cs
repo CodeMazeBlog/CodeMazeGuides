5bdff5368347a353baf6737b1443dcc39b52e645
Ivan Gechev <ivan_banov@abv.bg>
2023-03-06 17:49:09 +0200

    Initial commit

---
+++ csharp-advanced-topics/NullConditionalOperators/NullConditionalOperators/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace NullConditionalOperators
+{
+    public class Student
+    {
+        public string? Name { get; set; }
+        public string[]? Courses { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(string name, string[] courses)
+        {
+            Name = name;
+            Courses = courses;
+        }
+
+        public void Study(string course)
+        {
+            Console.WriteLine($"Studying for the {course} exam...");
+        }
+    }
+}
