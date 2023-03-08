c42190ba7e84b9b876a30d0148215225d6f90acc
Hassan Qayyum <hassan.qayyum@confiz.local>
2023-03-08 10:00:45 +0500

    Revert "a"
    This reverts commit 98e88c6a0422a470ab2335849a11e547b35e9117.

--- csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -1,5 +1,23 @@
-﻿using ActionAndFuncDelegatesInCSharp;
+﻿// See https://aka.ms/new-console-template for more information
+using ActionAndFuncDelegatesInCSharp;
 
+static void Print(string strValue)
+{
+    Console.WriteLine(strValue);
+}
+static int Sum(int x, int y)
+{
+    return x + y;
+}
+
+//Delegates for print and sum functions examples
+Action<string> printAction = new Action<string>(Print);
+printAction("Action and Func Delegates");
+
+Func<int, int, int> printFunc = new Func<int, int, int>(Sum);
+Console.WriteLine(printFunc(1, 2));
+
+//Delegates for weather application example
 string city = "New York";
 WeatherService weatherService = new WeatherService();
 
