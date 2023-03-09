f22cfe9c7c6ee93570742052cab1686789f84caf
HassanQayyum1993 <hassanqayyum11@gmail.com>
2023-03-09 14:11:42 +0500

    Added Program class and Main method in Program.cs

--- csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -1,30 +1,37 @@
 ﻿
-using ActionAndFuncDelegatesInCSharp;
-
-//Delegates for print and sum functions examples
-var example = new Example();
-Action<string> printAction = new Action<string>(example.Print);
-printAction("Action and Func Delegates");
-
-Func<int, int, int> printFunc = new Func<int, int, int>(example.Sum);
-Console.WriteLine(printFunc(1, 2));
-
-//Delegates for weather application example
-string city = "New York";
-var weatherService = new WeatherService();
-
-Action<string> displayWeatherAction = (weatherData) =>
+namespace ActionAndFuncDelegatesInCSharp
 {
-    Console.WriteLine("Current weather in " + city + ": " + weatherData);
-};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Delegates for print and sum functions examples
+            var example = new Example();
+            Action<string> printAction = new Action<string>(example.Print);
+            printAction("Action and Func Delegates");
 
-Func<string, string> formatWeatherFunc = (rawWeatherData) =>
-{
-    string[] weatherParts = rawWeatherData.Split(new char[] { ',', ':' }, StringSplitOptions.RemoveEmptyEntries);
-    var temperature = Convert.ToInt32(weatherParts[1].Trim());
-    var humidity = Convert.ToInt32(weatherParts[3].Trim());
+            Func<int, int, int> sumFunc = new Func<int, int, int>(example.Sum);
+            Console.WriteLine(sumFunc(1, 2));
 
-    return "Temperature: " + (temperature - 273) + "C, Humidity: " + humidity + "%";
-};
+            //Delegates for weather application example
+            string city = "New York";
+            var weatherService = new WeatherService();
 
-weatherService.GetWeather(city, displayWeatherAction, formatWeatherFunc);
+            Action<string> displayWeatherAction = (weatherData) =>
+            {
+                Console.WriteLine("Current weather in " + city + ": " + weatherData);
+            };
+
+            Func<string, string> formatWeatherFunc = (rawWeatherData) =>
+            {
+                string[] weatherParts = rawWeatherData.Split(new char[] { ',', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                var temperature = Convert.ToInt32(weatherParts[1].Trim());
+                var humidity = Convert.ToInt32(weatherParts[3].Trim());
+
+                return "Temperature: " + (temperature - 273) + "C, Humidity: " + humidity + "%";
+            };
+
+            weatherService.GetWeather(city, displayWeatherAction, formatWeatherFunc);
+        }
+    }
+}
