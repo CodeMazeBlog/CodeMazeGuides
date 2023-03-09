e765c9135625d28a215502b88f8d88ad31a6a74f
Ainea Wabwoba <aineah28@gmail.com>
2023-03-09 04:31:51 +0300

    create project

---
+++ aspnetcore-webapi/AddEndpointsApiExplorerMethod/AddEndpointsApiExplorerMethod/Program.cs
@@ -0,0 +1,58 @@
+using AddEndpointsApiExplorerMethod.Configurations;
+using Asp.Versioning;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen(options => options.OperationFilter<SwaggerDefaultValues>());
+
+builder.Services.AddApiVersioning(options =>
+    {
+        options.ReportApiVersions = true;
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ApiVersionReader = new QueryStringApiVersionReader();
+    }).AddApiExplorer(
+        options => { options.GroupNameFormat = "'v'VVV"; })
+    .EnableApiVersionBinding();
+
+
+var app = builder.Build();
+
+var versionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1,0))
+    .HasApiVersion(new ApiVersion(2,0))
+    .ReportApiVersions()
+    .Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        var descriptions = app.DescribeApiVersions();
+
+        foreach (var description in descriptions)
+        {
+            var url = $"/swagger/{description.GroupName}/swagger.json";
+            var name = description.GroupName.ToUpperInvariant();
+            options.SwaggerEndpoint(url, name);
+        }
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/car-models", () => new[] { "Chevrolet", "Tesla", "Nissan" })
+    .WithApiVersionSet(versionSet)
+    .HasApiVersion(1.0);
+
+app.MapGet("/car-models", () => new[] { "Chevrolet", "Tesla", "Nissan", "Toyota" })
+    .WithApiVersionSet(versionSet)
+    .HasApiVersion(2.0);
+
+app.Run();
