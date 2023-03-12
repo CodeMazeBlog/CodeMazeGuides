0b5a04d8eba8506b37c272dee6568a6a96a2adcb
Zaneta Borowska <zaneta.borowska@katoennatie.com>
2023-03-12 22:01:53 +0100

    Temporary field examples

---
+++ csharp-refactoring/RefactoringObjectOrientationAbusers/RefactoringObjectOrientationAbusers/TemporaryField/Correct/BookstoreCustomer.cs
@@ -0,0 +1,23 @@
+﻿namespace RefactoringObjectOrientationAbusers.TemporaryField.Correct
+{
+    public class BookstoreCustomer
+    {
+        public ICollection<string> BoughtBooks { get; set; }
+
+        public double GetDiscountRate()
+        {
+            var boughtBooksCount = BoughtBooks.Count;
+            var discountTreshold = GetDiscountThreshold(boughtBooksCount);
+
+            var discountBase = 0.05;
+            return discountTreshold * discountBase;
+        }
+
+        private int GetDiscountThreshold(double boughtBooksCount)
+        {
+            var firstThreshold = 1;
+            var secondThreshold = 2;
+            return boughtBooksCount < 10 ? firstThreshold : secondThreshold;
+        }
+    }
+}
---
+++ csharp-refactoring/RefactoringObjectOrientationAbusers/RefactoringObjectOrientationAbusers/TemporaryField/Incorrect/BookstoreCustomer.cs
@@ -0,0 +1,25 @@
+﻿namespace RefactoringObjectOrientationAbusers.TemporaryField.Incorrect
+{
+    public class BookstoreCustomer
+    {
+        public int DiscountTreshold { get; set; }
+        public int BoughtBooksCount { get; set; }
+        public ICollection<string> BoughtBooks { get; set; }
+
+        public double GetDiscountRate()
+        {
+            BoughtBooksCount = BoughtBooks.Count;
+            DiscountTreshold = GetDiscountThreshold();
+
+            var discountBase = 0.05;
+            return DiscountTreshold * discountBase;
+        }
+
+        private int GetDiscountThreshold()
+        {
+            var firstThreshold = 1;
+            var secondThreshold = 2;
+            return BoughtBooksCount < 10 ? firstThreshold : secondThreshold;
+        }
+    }
+}
