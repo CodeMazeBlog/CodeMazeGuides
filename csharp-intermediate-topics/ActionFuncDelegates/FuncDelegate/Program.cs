2fb4339394478751e57db894b8916ce730756bdf
Adil Ansari <mailbox.adil@gmail.com>
2023-03-14 21:12:44 +0530

    Upload code examples of Action and Func delegates

---
+++ csharp-intermediate-topics/ActionFuncDelegates/FuncDelegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncDelegate
+{
+    public class Program
+    {
+        public static int square(int n)
+        {
+            return n * n;
+        }
+        public static void Main(string[] args)
+        {
+            // create an Func delegate and assign it a method
+            Func<int, int> funcSquare = square;
+
+            // calling the Action
+            Console.WriteLine(funcSquare(2));
+
+            // alternate-1: using the anonymous method
+            Func<int, int> funcSquare2 = delegate (int n) { return n * n; };
+
+            Console.WriteLine(funcSquare2(4));
+
+            // alternate-2: using the lambda expression
+            Func<int, int> funcSquare3 = n => n * n;
+
+            Console.WriteLine(funcSquare3(8));
+        }
+    }
+}
---
+++ csharp-intermediate-topics/ActionFuncDelegates/Tests/FuncDelegateUnitTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests
+{
+    public class FuncDelegateUnitTest
+    {
+        [Fact]
+        public void SquareHappyPath()
+        {
+            // arrange
+            var input = 5;
+            var expected = 25;
+
+            // act
+            var actual = FuncDelegate.Program.square(input);
+
+            // assert
+
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void MakeSureMainMethodWorks()
+        {
+            FuncDelegate.Program.Main(new string[0]);
+        }
+    }
+}
