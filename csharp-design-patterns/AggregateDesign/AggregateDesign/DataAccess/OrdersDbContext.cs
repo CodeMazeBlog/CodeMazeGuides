651fa1e31a8318872bdc9b98a2bff2764ffac837
Diego García <wft2000@gmail.com>
2023-03-19 21:36:46 +0100

    CM-657: Aggregate Design in .NET

---
+++ csharp-design-patterns/AggregateDesign/AggregateDesign/DataAccess/OrdersDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AggregateDesign.DataAccess;
+
+#nullable disable
+
+public class OrdersDbContext : DbContext
+{
+    public DbSet<OrderModel> orders { get; set; }
+    public DbSet<OrderItemModel> orderItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=orders;Username=postgres;Password=a");
+    }
+}
