3b7b676c10be0c6be0508a781a7f583e55ee7d50
sdayube <scarige@gmail.com>
2023-03-21 13:38:27 -0300

    CM-359: Datetime Calendars in C#

---
+++ dotnet-datetime/DateTimeCalendar/DateTimeCalendar/CalendarTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace DateTimeCalendar
+{
+    public class CalendarTests
+    {
+        [Fact]
+        public void WhenUsingCalendar_ThenGetsDaysInMonth()
+        {
+            var calendar = new GregorianCalendar();
+            var daysInJanuary = calendar.GetDaysInMonth(2023, 1);
+
+            Assert.Equal(31, daysInJanuary);
+        }
+
+        [Fact]
+        public void WhenDateTimeHasCalendar_ThenGetsLocalizedMonthString()
+        {
+            var calendar = new HijriCalendar();
+            var dateTime = new DateTime(1444, 4, 2, calendar);
+
+            var defaultMonth = dateTime.ToString("MMMM");
+            var culturalMonth = dateTime.ToString("MMMM", new CultureInfo("ar-SA"));
+
+            Assert.Equal("October", defaultMonth);
+            Assert.Equal("ربيع الآخر", culturalMonth);
+        }
+
+        [Fact]
+        public void WhenUsingCultureInfo_ThenGetsCalendar()
+        {
+            var usCulture = new CultureInfo("en-US");
+            var calendar = usCulture.Calendar;
+
+            Assert.Equal(typeof(GregorianCalendar), calendar.GetType());
+        }
+
+        [Fact]
+        public void WhenUsingCultureInfo_CanCompareDates()
+        {
+            var usCulture = new CultureInfo("en-US");
+            var saudiCulture = new CultureInfo("ar-SA");
+
+            var usDate = new DateTime(2023, 3, 21, usCulture.Calendar);
+            var saudiDate = new DateTime(1444, 8, 10, saudiCulture.Calendar);
+
+            Assert.True(usDate > saudiDate);
+        }
+    }
+}
