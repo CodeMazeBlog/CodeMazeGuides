757fce8b2323cf5d403af4082cec4df3ab0067ba
Phil Broderick <33251762+PhilBroderick@users.noreply.github.com>
2023-03-23 12:17:25 +0000

    CM-640: Vendor-Agnostic Telemetry Using OpenTelemetry Collector in .NET (#956)

---
+++ dotnet-client-libraries/OpenTelemetryCollector/OpenTelemetryCollector/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetryCollector;
+using System.Diagnostics;
+
+var activitySource = new ActivitySource("OpenTelemetryCollector");
+
+using var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource("OpenTelemetryCollector")
+    .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName: "OpenTelemetryCollector"))
+    .AddConsoleExporter()
+    .AddOtlpExporter(opt =>
+    {
+        opt.Protocol = OtlpExportProtocol.Grpc;
+        opt.Endpoint = new Uri("http://localhost:4317");
+    })
+    .Build();
+
+using var activity = activitySource.StartActivity("Root trace");
+
+var traces = new Traces(activitySource);
+
+await traces.LongRunningTask();
+
