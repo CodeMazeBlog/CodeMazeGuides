2a23a300e2144d034402d51e72a63a3d78bfea54
Muhammed Saleem <muhammedsalimp@gmail.com>
2023-03-24 15:47:58 +0530

    CM-167 : How do I write logs from within Startup.cs/Program.cs?

---
+++ dotnet-logging/LoggingFromStartup/Tests/LoggingFromStartupTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Tests
+{
+    public class LoggingFromStartupTest : IClassFixture<WebApplicationFactory<LoggingFromStartup.Program>>
+    {
+
+        private readonly WebApplicationFactory<LoggingFromStartup.Program> _factory;
+
+        public LoggingFromStartupTest(WebApplicationFactory<LoggingFromStartup.Program> factory)
+        {
+            _factory = factory;
+        }
+
+
+        [Fact]
+        public async Task WhenGettingWeatherForecast_ThenReturnSuccess()
+        {
+            // arrange
+            var client = _factory.CreateClient();
+
+            // act
+            var response = await client.GetAsync("WeatherForecast");
+
+            //assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
