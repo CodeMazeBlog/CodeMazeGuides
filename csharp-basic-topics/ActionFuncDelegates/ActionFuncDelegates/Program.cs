c10797583ad4b1f63ec4d40f83cbac6fd27bf64e
ikponwosae <ikponwosae@gmail.com>
2023-03-25 01:00:41 +0100

    feat:action-func-delegates && unit-test

---
+++ csharp-basic-topics/ActionFuncDelegates/ActionFuncDelegates/Program.cs
@@ -0,0 +1,55 @@
+﻿
+namespace ActionFuncDelegates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialise the delegate
+            var myDelegate = new FirstDelegate(CountOne);
+            
+            Action<string, int> ActionDelegate = TestMethod;
+            Func<int, string, string> FuncDelegate = TestFunc;
+            
+            ActionDelegate("James", 20);
+            FuncDelegate(40, "Tomie");
+
+            //It can also be initialised this way
+            //FirstDelegate myDelegate = CountOne;
+
+            myDelegate += AddTwo;
+            myDelegate += Product;
+            myDelegate += (x) => Console.WriteLine(x / 2);
+
+            myDelegate(2);
+            myDelegate.Invoke(4);
+
+        }
+        public delegate void FirstDelegate(int n);
+
+        public static void CountOne(int number)
+        {
+            number += 1;
+            Console.WriteLine(number);
+        }
+        public static void AddTwo(int number)
+        {
+            number = number + 2;
+            Console.WriteLine(number);
+        }
+        public static void Product(int number)
+        {
+            number = number * 3;
+            Console.WriteLine(number);
+        }
+        public static void TestMethod(string name, int age)
+        {
+            Console.WriteLine($"{name} is {age} years old");
+        }
+        public static string TestFunc(int age, string name)
+        {
+            Console.WriteLine($"{name} is {age} years old");
+            return name;
+        }
+    }
+}
---
+++ csharp-basic-topics/ActionFuncDelegates/Tests/ActionFuncDelegateUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using ActionFuncDelegates;
+using Xunit.Sdk;
+
+namespace Tests;
+
+public class ActionFuncDelegateUnitTest
+{
+    public delegate int FirstDelegate(int n);
+    Action<string, int> ActionDelegate = TestMethod;
+    Func<int, string, string> FuncDelegate = TestFunc;
+
+    [Fact]
+    public void WhenGivenAnInteger_ThenReturnAnInteger()
+    {
+        var firstDelegate = new FirstDelegate(CountTwo);
+
+        Assert.Equal(4, firstDelegate(2));
+    }
+
+    [Fact]
+    public void WhenPassedAnIntegerAndString_ThenReturnAString()
+    {
+        Assert.Equal("Tomie is 40 years old", FuncDelegate(40, "Tomie"));
+    }
+
+
+    public static int CountTwo(int number)
+    {
+        return number += 2;
+    }
+    public static void TestMethod(string name, int age)
+    {
+        Console.WriteLine($"{name} is {age} years old");
+    }
+    public static string TestFunc(int age, string name)
+    {
+        Console.WriteLine($"{name} is {age} years old");
+        return name;
+    }
+}
