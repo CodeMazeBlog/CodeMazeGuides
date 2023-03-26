4b5977cc28ac1710cba1c2de3e9e9d06e5cdc2d9
zanbor <44729834+zanbor@users.noreply.github.com>
2023-03-26 13:17:05 +0200

    CM-660: Refactoring Object-Orientation Abusers in C# (#939)
    * Code for article about refactoring Object-Oriented abusers
    
    * tests change
    
    * code review
    
    * Temporary field examples
    
    * typo
    
    * CR
    
    * CR
    
    * Private fields notation changed
    
    ---------
    
    Co-authored-by: Zaneta Borowska <zaneta.borowska@katoennatie.com>

---
+++ csharp-refactoring/RefactoringObjectOrientationAbusers/RefactoringObjectOrientationAbusers/SwitchStatements/Correct/MilitaryPlane.cs
@@ -0,0 +1,12 @@
+﻿namespace RefactoringObjectOrientationAbusers.SwitchStatements.Correct
+{
+    public class MilitaryPlane : Plane
+    {
+        public override int Type { get { return Military; } }
+
+        public override double GetCapacity()
+        {
+            return 50000;
+        }
+    }
+}
