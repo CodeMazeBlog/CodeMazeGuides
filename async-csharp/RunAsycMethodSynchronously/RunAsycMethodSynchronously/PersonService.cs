d8bcc93a758867cc3e4b89a6861a283c1a820077
Ainea Wabwoba <aineah28@gmail.com>
2023-03-28 05:58:39 +0300

    set up project, added tests

---
+++ async-csharp/RunAsycMethodSynchronously/RunAsycMethodSynchronously/PersonService.cs
@@ -0,0 +1,53 @@
+﻿namespace RunAsyncMethodSynchronously;
+
+public class PersonService
+{
+    public List<Person> GetPeople()
+    {
+        var task = new Task<List<Person>>(() => new List<Person>
+        {
+            new() { Name = "Alice", Age = 25 },
+            new() { Name = "Bob", Age = 30 },
+            new() { Name = "Charlie", Age = 35 }
+        });
+
+        Thread.Sleep(5000);
+        task.RunSynchronously();
+        
+        return task.Result;
+    }
+    
+    public List<Person> GetPeopleUsingWaitMethod()
+    {
+        var task = GetPeopleAsync();
+        task.Wait();
+
+        return task.Result;
+    }
+
+    public List<Person> GetPeopleUsingResultMethod()
+    {
+        var task = GetPeopleAsync();
+
+        return task.Result;
+    }
+
+    public List<Person> GetPeopleUsingGetAwaiter()
+    {
+        var response = GetPeopleAsync().GetAwaiter().GetResult();
+
+        return response;
+    }
+
+    private async Task<List<Person>> GetPeopleAsync()
+    {
+        await Task.Delay(2000);
+
+        return new List<Person>
+        {
+            new() { Name = "Alice", Age = 25 },
+            new() { Name = "Bob", Age = 30 },
+            new() { Name = "Charlie", Age = 35 }
+        };
+    }
+}
---
+++ async-csharp/RunAsycMethodSynchronously/Tests/RunAsyncMethodSynchronouslyUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using RunAsyncMethodSynchronously;
+
+namespace Tests;
+
+public class RunAsyncMethodSynchronouslyUnitTests
+{
+    private readonly PersonService _personService;
+
+    public RunAsyncMethodSynchronouslyUnitTests()
+    {
+        _personService = new PersonService();
+    }
+
+    [Theory]
+    [InlineData(3)]
+    public void WhenRunSynchronouslyMethodIsCalled_ThenReturnListOfPeople(int count)
+    {
+        var actual = _personService.GetPeople();
+
+        Assert.Equal(count, actual.Count);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    public void WhenWaitMethodIsCalled_ThenReturnListOfPeople(int count)
+    {
+        var actual = _personService.GetPeopleUsingWaitMethod();
+
+        Assert.Equal(count, actual.Count);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    public void WhenResultPropertyIsUsed_ThenReturnListOfPeople(int count)
+    {
+        var actual = _personService.GetPeopleUsingResultMethod();
+
+        Assert.Equal(count, actual.Count);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    public void WhenGetAwaiterMethodIsCalled_ThenReturnListOfPeople(int count)
+    {
+        var actual = _personService.GetPeopleUsingGetAwaiter();
+
+        Assert.Equal(count, actual.Count);
+    }
+}
