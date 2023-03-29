f9566864abf384ac673036779fcae73c6a57e65f
Hussain Waheed <hussain.waheed@sto.mv>
2023-03-29 22:51:58 +0500

    Evaluation article - sample code for Action and Func in CSharp

---
+++ csharp-advanced-topics/ActionAndFuncInCSharp/ActionAndFuncInCSharp/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ActionAndFuncInCSharp;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var cart = GetShoppingCart();
+
+        Func<double, double, decimal> taxCalculator = CalculateTax;
+        var tax = taxCalculator(cart.Sum(x => x.Total), 0.10);
+
+
+        Action<List<CartItem>, decimal> receiptPrinter = PrintReceipt;
+        receiptPrinter.Invoke(cart, tax);
+
+        Console.ReadLine();
+    }
+
+    public static decimal CalculateTax(double total, double rate)
+    {
+
+        return Convert.ToDecimal(total * rate);
+    }
+
+    public static void PrintReceipt(List<CartItem> cart, decimal tax)
+    {
+        Console.WriteLine($"Product - Qty - Price - Gross");
+
+        foreach (var item in cart)
+        {
+
+            Console.WriteLine($"{item.Name} - {item.Quantity} - {item.Price:C2} - {item.Total:C2}");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"You tax total is {tax:C2}");
+    }
+
+    public static List<CartItem> GetShoppingCart()
+    {
+        return new List<CartItem>
+        {
+            new CartItem { Name = "Milk", Price = 12, Quantity = 2 },
+            new CartItem { Name = "Eggs", Price = 1.25, Quantity = 10 },
+            new CartItem { Name = "Flour", Price = 8, Quantity = 1 },
+            new CartItem { Name = "Cocoa", Price = 24.99, Quantity = 3 },
+            new CartItem { Name = "Butter", Price = 15, Quantity = 2 }
+        };
+    }
+
+}
+
+public record CartItem
+{
+    public required string? Name { get; set; }
+    public required double Quantity { get; set; }
+
+    public required double Price { get; set; }
+
+    public double Total => Quantity * Price;
+}
---
+++ csharp-advanced-topics/ActionAndFuncInCSharp/Tests/Tests.cs
@@ -0,0 +1,59 @@
+﻿using ActionAndFuncInCSharp;
+
+namespace Tests;
+
+public class Tests
+{
+
+
+    [Fact]
+    public void whenTotalIsSent_ReferenceFuncDelegateMethodCalculatesAndReturnsTaxValue()
+    {
+        Func<double, double, decimal> CalculateTaxFunc = Program.CalculateTax;
+        var total = 149.47;
+        decimal expected = Convert.ToDecimal(149.47 * 0.10);
+
+        var tax = CalculateTaxFunc(total, 0.10);
+
+        Assert.Equal(expected, tax);
+    }
+
+    [Fact]
+    public void whenShoppingCartAndTaxIsSent_ReferenceActionDelegateMethodPrintsReceiptToConsole()
+    {
+        Action<List<CartItem>, decimal> ReceiptPrintAction = Program.PrintReceipt;
+
+        var cart = new List<CartItem>
+        {
+            new CartItem { Name = "Milk", Price = 12, Quantity = 2 },
+            new CartItem { Name = "Eggs", Price = 1.25, Quantity = 10 },
+            new CartItem { Name = "Flour", Price = 8, Quantity = 1 },
+            new CartItem { Name = "Cocoa", Price = 24.99, Quantity = 3 },
+            new CartItem { Name = "Butter", Price = 15, Quantity = 2 }
+        };
+
+        var tax = Convert.ToDecimal(cart.Sum(x => x.Total) * 0.10);
+
+        ReceiptPrintAction(cart, tax);
+
+        // Assert
+        // Assert that the output contains the expected strings
+    }
+
+    [Fact]
+    public void whenRequestedForShoppingCart_ListOfCartItemsAreReturned()
+    {
+        var expected = new List<CartItem>
+        {
+            new CartItem { Name = "Milk", Price = 12, Quantity = 2 },
+            new CartItem { Name = "Eggs", Price = 1.25, Quantity = 10 },
+            new CartItem { Name = "Flour", Price = 8, Quantity = 1 },
+            new CartItem { Name = "Cocoa", Price = 24.99, Quantity = 3 },
+            new CartItem { Name = "Butter", Price = 15, Quantity = 2 }
+        };
+
+        var cart = Program.GetShoppingCart();
+
+        Assert.Equal(expected, cart);
+    }
+}
