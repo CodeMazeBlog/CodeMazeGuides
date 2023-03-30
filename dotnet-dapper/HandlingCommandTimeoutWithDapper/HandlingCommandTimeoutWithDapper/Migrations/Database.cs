87ea685c1a2cec236aee24447d506d48deaf2df7
ederGus <edergus@gmail.com>
2023-03-30 22:15:12 +0200

    CM-385: Handling CommandTimeout with Dapper in .NET (#971)
    * add project, models, repo, and controller.
    
    * some modifications afer code review.
    
    * test name modified.
    
    * move folder and some improvements.
    
    * removed code not needed.
    
    ---------
    
    Co-authored-by: Eder Llamuca <eder.llamuca@appspace.com>

---
+++ dotnet-dapper/HandlingCommandTimeoutWithDapper/HandlingCommandTimeoutWithDapper/Migrations/Database.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using HandlingCommandTimeoutWithDapper.Context;
+
+namespace HandlingCommandTimeoutWithDapper.Migrations
+{
+    public class Database
+    {
+        private readonly DapperContext _context;
+
+        public Database(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateDatabase(string dbName)
+        {
+            const string query = "SELECT * FROM sys.databases WHERE name = @name";
+            var parameters = new DynamicParameters();
+            parameters.Add("name", dbName);
+
+            using var connection = _context.CreateMasterConnection();
+            var records = connection.Query(query, parameters);
+            if (!records.Any())
+                connection.Execute($"CREATE DATABASE {dbName}");
+        }
+    }
+}
