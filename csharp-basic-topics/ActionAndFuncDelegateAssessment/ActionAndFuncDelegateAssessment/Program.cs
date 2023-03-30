699867ecfc88fe1cbd2e9f9803fadbc0589fb29a
Courtland <Courtland9777@gmail.com>
2023-03-30 06:54:56 -0400

    Updating Program.cs

--- csharp-basic-topics/ActionAndFuncDelegateAssessment/ActionAndFuncDelegateAssessment/Program.cs
+++ csharp-basic-topics/ActionAndFuncDelegateAssessment/ActionAndFuncDelegateAssessment/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using ActionAndFuncDelegateAssessment;
+
+var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+Examples.ActionDelegateExample(numbers);
+Console.WriteLine(Examples.FuncDelegateExample(numbers));
+Console.WriteLine(Examples.MultithreadExample());
