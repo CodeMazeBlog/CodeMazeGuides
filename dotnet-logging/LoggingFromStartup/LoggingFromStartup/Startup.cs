de620cbd09c2170355abee96e9241168f325234a
Muhammed Saleem <muhammedsalimp@gmail.com>
2023-03-31 21:04:40 +0530

    removing unused directives, formatting correction

--- dotnet-logging/LoggingFromStartup/LoggingFromStartup/Startup.cs
+++ dotnet-logging/LoggingFromStartup/LoggingFromStartup/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LoggingFromStartup
 {
@@ -26,7 +20,6 @@ namespace LoggingFromStartup
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
