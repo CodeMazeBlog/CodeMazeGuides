fd585926c253a0ee847f79bfe45e94640aad3d69
karthikns9 <karthikns999@gmail.com>
2023-03-31 10:24:21 +0530

    inital commit

---
+++ strings-csharp/SplitString/SplitString/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace SplitString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n***************** Split the String Using Character Array ***************\n");
+            SplitStringUsingCharacterArray("apple,banana,cherry;date", new[] { ',', ';'});
+
+            Console.WriteLine("\n***************** Split the String Using Character Array with String Split Options ***************\n");
+            SplitStringUsingCharacterArrayWithoptions("John & Jane & James", new[] { ' ', '&' });
+
+            Console.WriteLine("\n***************** Split the String Using Character Array Using Count ***************\n");
+            SplitStringUsingCharacterArrayUsingCount("apple,banana,cherry,orange", new[] { ',' }, 3);
+
+            Console.WriteLine("\n***************** Split the String Using Character Array with Options Using Count ***************\n");
+            SplitStringUsingCharacterArrayWithoptionsUsingCount("   apple , banana , cherry  ,,orange,   ", new[] { ',' }, 2);
+
+            Console.WriteLine("\n***************** Split the String Using String Array with String Split Options ***************\n");
+            SplitStringUsingStringArrayWithoptions("apple,banana,kiwi;grape,mango,orange", new[] { ",", ";" });
+
+            Console.WriteLine("\n***************** Split the String Using String Array with String Split Options Using Count ***************\n");
+            SplitStringUsingStringArrayWithoptionsUsingCount("apple,banana,cherry,orange,pear", new[] { "," }, 3);
+        }
+
+        public static string[] SplitStringUsingCharacterArray(string input, char[] separators)
+        {
+            string[] result = input.Split(separators);
+
+            foreach (string item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            return result;
+        }
+
+        public static string[] SplitStringUsingCharacterArrayWithoptions(string input, char[] separators)
+        {
+            string[] words = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            return words;
+        }
+
+        public static string[] SplitStringUsingCharacterArrayUsingCount(string input, char[] separators, int limit)
+        {
+            string[] result = input.Split(separators, limit);
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+            return result;
+        }
+
+        public static string[] SplitStringUsingCharacterArrayWithoptionsUsingCount(string input, char[] separators, int count)
+        {
+            string[] result = input.Split(separators, count, StringSplitOptions.TrimEntries);
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+            return result;
+        }
+
+        public static string[] SplitStringUsingStringArrayWithoptions(string input, string[] delimiters)
+        {
+            string[] result = input.Split(delimiters, StringSplitOptions.None);
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+            return result;
+        }
+
+        public static string[] SplitStringUsingStringArrayWithoptionsUsingCount(string input, string[] separators, int count)
+        {
+            string[] result = input.Split(separators, count, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s.Trim());
+            }
+
+            return result;
+        }
+    }
+}
