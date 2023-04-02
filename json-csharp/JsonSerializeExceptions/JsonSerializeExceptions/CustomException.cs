84ade4d196a9bd0995546df4aa1ca9505c1f5852
ullahahsan1983 <ullahahsan1983@gmail.com>
2023-04-01 17:50:32 -0700

    How to serialize exceptions as JSON

---
+++ json-csharp/JsonSerializeExceptions/JsonSerializeExceptions/CustomException.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace JsonSerializeExceptions;
+
+public class CustomException : Exception
+{
+    public CustomException(string message, string mitigation) 
+        : this(message, mitigation, null)
+    { }
+
+    public CustomException(string message, string mitigation, Exception? innerException) 
+        : base(message, innerException)
+    {
+        Mitigation = mitigation;
+    }
+
+    public string Mitigation { get; }
+}
