fba1ab3a48d4ca07458e4565c11b6a5ed8866ce2
PhilBroderick <philbroderick12@gmail.com>
2023-04-02 07:50:25 +0100

    Initial solution

---
+++ dotnet-client-libraries/Sieve/SievePackage/Controllers/ShoesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sieve.Models;
+using Sieve.Services;
+
+namespace SievePackage.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShoesController : ControllerBase
+{
+    private static readonly List<Shoe> _shoes = new()
+    {
+        new()
+        {
+            Id = 1,
+            Name = "Pegasus 39",
+            Brand = "Nike",
+            Price = 119.99M,
+            Category = "Running",
+            Rating = 4.5M
+        },
+        new()
+        {
+            Id = 2,
+            Name = "Pegasus Trail 3",
+            Brand = "Nike",
+            Price = 119.99M,
+            Category = "Trail",
+            Rating = 3.8M
+        },
+        new()
+        {
+            Id = 3,
+            Name = "Ride 15",
+            Brand = "Saucony",
+            Price = 59.99M,
+            Category = "Neutral",
+            Rating = 4.9M
+        }
+    };
+
+    private readonly SieveProcessor _sieveProcessor;
+
+    public ShoesController(SieveProcessor sieveProcessor)
+    {
+        _sieveProcessor = sieveProcessor;
+    }
+
+    [HttpGet]
+    public IActionResult GetShoes([FromQuery]SieveModel sieveModel)
+    {
+        var result = _sieveProcessor.Apply(sieveModel, _shoes.AsQueryable());
+
+        return Ok(result);
+    }
+}
---
+++ dotnet-client-libraries/Sieve/SievePackage/Shoe.cs
@@ -0,0 +1,21 @@
+﻿using Sieve.Attributes;
+
+namespace SievePackage;
+public class Shoe
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    [Sieve(CanFilter = true)]
+    public required string Category { get; set; }
+
+    [Sieve(CanFilter = true)]
+    public required string Brand { get; set; }
+
+    [Sieve(CanSort = true)]
+    public decimal Price { get; set; }
+
+    [Sieve(CanSort = true)]
+    public decimal Rating { get; set; }
+}
