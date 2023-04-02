a4eeb57beaa2a52eb577e60c6c1d766704d39456
Ivan Gechev <ivan_banov@abv.bg>
2023-04-02 15:49:08 +0300

    initial commit

---
+++ dotnet-client-libraries/MoreLINQ/MoreLINQ.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MoreLINQ.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ dotnet-client-libraries/MoreLINQ/MoreLINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoreLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
