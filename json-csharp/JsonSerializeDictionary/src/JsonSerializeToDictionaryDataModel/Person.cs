d9ba634c9896e5590a7e16e7a63b66fbbfb2815e
jshergal <jeff.shergalis@gmail.com>
2023-04-05 22:57:38 +0300

    Added missing unit tests for Person object ToString override

--- json-csharp/JsonSerializeDictionary/src/JsonSerializeToDictionaryDataModel/Person.cs
+++ json-csharp/JsonSerializeDictionary/src/JsonSerializeToDictionaryDataModel/Person.cs
@@ -4,6 +4,8 @@ public sealed record class Person(string FirstName, string LastName)
 {
     public override string ToString()
     {
+        if (string.IsNullOrWhiteSpace(LastName)) return FirstName;
+        if (string.IsNullOrWhiteSpace(FirstName)) return LastName;
         return FirstName + " " + LastName;
     }
 }
---
+++ json-csharp/JsonSerializeDictionary/test/JsonSerializeToDictionaryDataModel.Tests/PersonRecordTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerializeToDictionaryDataModel.Tests;
+public class PersonRecordTests
+{
+    [Fact]
+    public void GivenPerson_WhenToString_ThenProducesFirstSpaceLast()
+    {
+        const string expected = "Han Solo";
+        var names = expected.Split(" ");
+        var person = new Person(names[0], names[1]);
+        
+        Assert.Equal(expected, person.ToString());
+    }
+
+    [Fact]
+    public void GivenPersonWithEmptyFirstName_WhenToString_ThenProducesLast()
+    {
+        const string last = "Solo";
+        var person = new Person(string.Empty, last);
+        
+        Assert.Equal(last, person.ToString());
+
+    }
+
+    [Fact]
+    public void GivenPersonWithEmptyLastName_WhenToString_ThenProducesFirst()
+    {
+        const string first = "Han`";
+        var person = new Person(first, string.Empty);
+        
+        Assert.Equal(first, person.ToString());
+
+    }
+
+    [Fact]
+    public void GivenPersonWithEmptyFirstAndLast_WhenToString_ThenProducesEmpty()
+    {
+        var person = new Person(string.Empty, string.Empty);
+
+        Assert.Equal(string.Empty, person.ToString());
+    }
+}
