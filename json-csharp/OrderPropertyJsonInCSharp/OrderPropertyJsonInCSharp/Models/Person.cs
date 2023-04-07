b530735d2c063827159bb8e2d6ac219f67a44e3b
Code Maze <contact@code-maze.com>
2023-04-07 08:36:41 +0200

    Removed PR

--- json-csharp/OrderPropertyJsonInCSharp/OrderPropertyJsonInCSharp/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrderPropertyJsonInCSharp.Models
-{
-    public class Person
-    {
-        public int Id { get; set; }
-
-        public string? Name { get; set; }
-    }
-}
--- json-csharp/OrderPropertyJsonInCSharp/OrderPropertyJsonInCSharp/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrderPropertyJsonInCSharp.Models
-{
-    public class Student: Person
-    {
-        public int RegistratioNumber { get; set; }
-
-        public double Grade { get; set; }
-    }
-}
