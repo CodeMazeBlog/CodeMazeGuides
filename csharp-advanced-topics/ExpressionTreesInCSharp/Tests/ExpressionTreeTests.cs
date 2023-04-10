55122affbe75894b519de50eadf0b67577bea966
Soundar Anbalagan <SOUNDARANBU@GMAIL.COM>
2023-04-10 19:49:53 +0100

    Removed duplicate code

--- csharp-advanced-topics/ExpressionTreesInCSharp/Tests/ExpressionTreeTests.cs
+++ csharp-advanced-topics/ExpressionTreesInCSharp/Tests/ExpressionTreeTests.cs
@@ -9,22 +9,31 @@ public class ExpressionTreeTests
     public void GivenALambdaExpression_WhenConvertedToExpressionTree_ShouldContainTheExpectedValues()
     {
         var sumExpressionTree = ExpressionTrees.CreateExpressionTreeFromLambdaExpression();
+        AssertExpressionTreeProperties(sumExpressionTree);
+    }
+
+    private static void AssertExpressionTreeProperties(Expression<Func<int, int, int>> sumExpressionTree)
+    {
+        // root node
         Assert.Equal(ExpressionType.Lambda, sumExpressionTree.NodeType);
         Assert.Equal("number1", sumExpressionTree.Parameters[0].Name);
         Assert.Equal("number2", sumExpressionTree.Parameters[1].Name);
         Assert.Equal("(number1 + number2)", sumExpressionTree.Body.ToString());
         Assert.Equal(typeof(Int32), sumExpressionTree.ReturnType);
 
+        // root -> body node
         Assert.Equal(ExpressionType.Add, sumExpressionTree.Body.NodeType);
 
         BinaryExpression body = (BinaryExpression)sumExpressionTree.Body;
 
+        // root -> body -> left node
         Assert.Equal(ExpressionType.Parameter, body.Left.NodeType);
         ParameterExpression leftParam = (ParameterExpression)body.Left;
         Assert.IsAssignableFrom<Expression>(leftParam);
         Assert.Equal("number1", leftParam.Name);
         Assert.Same(leftParam, sumExpressionTree.Parameters[0]);
 
+        // root -> body -> right node
         Assert.Equal(ExpressionType.Parameter, body.Right.NodeType);
         ParameterExpression rightParam = (ParameterExpression)body.Right;
         Assert.IsAssignableFrom<Expression>(rightParam);
@@ -37,23 +46,7 @@ public class ExpressionTreeTests
     {
         var expressionTree = ExpressionTrees.CreateExpressionTreeUsingExpressionTreeClass();
 
-        BinaryExpression body = (BinaryExpression)expressionTree.Body;
-
-        Assert.Equal(ExpressionType.Lambda, expressionTree.NodeType);
-        Assert.Equal(typeof(Int32), expressionTree.ReturnType);
-        Assert.Equal(ExpressionType.Add, body.NodeType);
-
-        ParameterExpression leftParam = (ParameterExpression)body.Left;
-        Assert.IsAssignableFrom<Expression>(leftParam);
-        Assert.Same(leftParam, expressionTree.Parameters[0]);
-        Assert.Equal(ExpressionType.Parameter, leftParam.NodeType);
-        Assert.Equal("number1", leftParam.Name);
-
-        ParameterExpression rightParam = (ParameterExpression)body.Right;
-        Assert.IsAssignableFrom<Expression>(rightParam);
-        Assert.Same(rightParam, expressionTree.Parameters[1]);
-        Assert.Equal(ExpressionType.Parameter, rightParam.NodeType);
-        Assert.Equal("number2", rightParam.Name);
+        AssertExpressionTreeProperties(expressionTree);
     }
 
     [Fact]
