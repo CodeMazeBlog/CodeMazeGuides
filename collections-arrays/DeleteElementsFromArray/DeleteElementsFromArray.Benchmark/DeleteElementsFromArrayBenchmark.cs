f12d11082e3415096d743b614aa529842282741c
Konstantinos Polychronopoulos <28976830+KonstantinosP7@users.noreply.github.com>
2023-04-12 16:57:46 +0300

    CM-397: How to Delete Element(s) from an Array in C# - Upgrade (#1023)
    * CM-397: Delete Elements from an Array.
    
    * CM-397: Formatting fixes
    
    * CM-397: Fix name in RemoveAll method.
    
    * CM-397: Add Methods ArrayCopy() & ArraySegment()
    
    * CM-397: Add two new proposed methods and refactor Benchmark

--- collections-arrays/DeleteElementsFromArray/DeleteElementsFromArray.Benchmark/DeleteElementsFromArrayBenchmark.cs
+++ collections-arrays/DeleteElementsFromArray/DeleteElementsFromArray.Benchmark/DeleteElementsFromArrayBenchmark.cs
@@ -1,26 +1,26 @@
-﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
 
-namespace BenchmarkRunner 
+namespace BenchmarkRunner
 {
 
     [MemoryDiagnoser]
     [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     [RankColumn]
-    public class DeleteElementsFromAnArrayBenchmark 
+    public class DeleteElementsFromAnArrayBenchmark
     {
-        private int[] _myArray = FillArray();
+        private readonly int[] _myArray = FillArray();
 
         private readonly int _key = 7;
 
         [Benchmark]
-        public int[] DeleteWithFindAll() 
+        public int[] DeleteWithFindAll()
         {
             return Array.FindAll(_myArray, i => i != _key);
         }
 
         [Benchmark]
-        public int[] DeleteWithRemoveAll() 
+        public int[] DeleteWithRemoveAll()
         {
             var list = new List<int>(_myArray);
             list.RemoveAll(e => e == _key);
@@ -29,75 +29,124 @@ namespace BenchmarkRunner
         }
 
         [Benchmark]
-        public int[] DeleteWithWhere() 
+        public int[] DeleteWithList()
+        {
+            var indexToRemove = Array.IndexOf(_myArray, _key);
+
+            if (indexToRemove < 0)
+            {
+                return _myArray;
+            }
+
+            var list = new List<int>(_myArray.Length);
+
+            for (int i = 0; i < _myArray.Length; i++)
+            {
+                if (_myArray[i] != _key)
+                {
+                    list.Add(_myArray[i]);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        [Benchmark]
+        public int[] DeleteWithWhere()
         {
             return _myArray.Where(e => e != _key).ToArray();
         }
 
         [Benchmark]
-        public int[] DeleteWithArrayCopy() 
+        public int[] DeleteWithArrayCopy()
         {
             var indexToRemove = Array.IndexOf(_myArray, _key);
 
-            if (indexToRemove < 0) 
+            if (indexToRemove < 0)
             {
                 return _myArray;
             }
 
-            var newArray = new int[_myArray.Length - 1];
+            var tempArray = new int[_myArray.Length - 1];
 
-            Array.Copy(_myArray, 0, newArray, 0, indexToRemove);
+            Array.Copy(_myArray, 0, tempArray, 0, indexToRemove);
 
-            Array.Copy(_myArray, indexToRemove + 1, newArray, indexToRemove, _myArray.Length - indexToRemove - 1);
+            Array.Copy(_myArray, indexToRemove + 1, tempArray, indexToRemove, _myArray.Length - indexToRemove - 1);
 
-            return newArray;
+            return tempArray;
         }
 
         [Benchmark]
-        public int[] DeleteWithArraySegment() 
+        public int[] DeleteWithBufferCopy()
         {
             var indexToRemove = Array.IndexOf(_myArray, _key);
 
-            if (indexToRemove >= 0) 
+            if (indexToRemove < 0)
             {
-                var segment1 = new ArraySegment<int>(_myArray, 0, indexToRemove);
-                var segment2 = new ArraySegment<int>(_myArray, indexToRemove + 1, _myArray.Length - indexToRemove - 1);
-                _myArray = segment1.Concat(segment2).ToArray();
+                return _myArray;
             }
 
-            return _myArray;
+            var tempArray = new int[_myArray.Length - 1];
+
+            Buffer.BlockCopy(_myArray, 0, tempArray, 0, indexToRemove * 4);
+
+            Buffer.BlockCopy(_myArray, (indexToRemove + 1) * 4, tempArray, indexToRemove * 4, (_myArray.Length - indexToRemove - 1) * 4);
+
+            return tempArray;
         }
 
         [Benchmark]
-        public int[] DeleteWithLoop() 
+        public int[] DeleteWithArraySegment()
         {
             var indexToRemove = Array.IndexOf(_myArray, _key);
 
-            if (indexToRemove >= 0) 
+            if (indexToRemove < 0)
             {
-                var tempArray = new int[_myArray.Length - 1];
-                for (int i = 0, j = 0; i < _myArray.Length; i++) 
+                return _myArray;
+            }
+
+            int[] tempArray;
+
+            var segment1 = new ArraySegment<int>(_myArray, 0, indexToRemove);
+
+            var segment2 = new ArraySegment<int>(_myArray, indexToRemove + 1, _myArray.Length - indexToRemove - 1);
+
+            tempArray = segment1.Concat(segment2).ToArray();
+
+            return tempArray;
+        }
+
+        [Benchmark]
+        public int[] DeleteWithLoop()
+        {
+            var indexToRemove = Array.IndexOf(_myArray, _key);
+
+            if (indexToRemove < 0)
+            {
+                return _myArray;
+            }
+
+            var tempArray = new int[_myArray.Length - 1];
+
+            for (int i = 0, j = 0; i < _myArray.Length; i++)
+            {
+                if (i == indexToRemove)
                 {
-                    if (i == indexToRemove) 
-                    {
-                        continue;
-                    }
-                    tempArray[j] = _myArray[i];
-                    j++;
+                    continue;
                 }
-
-                return tempArray;
+                tempArray[j] = _myArray[i];
+                j++;
             }
 
-            return _myArray;
+            return tempArray;
         }
-        
-        private static int[] FillArray() 
+
+        private static int[] FillArray()
         {
             var rnd = new Random();
             var myArray = new int[1000];
 
-            for (int i = 0; i < 1000; i++) 
+            for (int i = 0; i < 1000; i++)
             {
                 myArray[i] = rnd.Next(1, 10);
             }
