c56f68a277a49f787d81237c9b000c583d3e6889
Shahrzad Abedi <shahrzad.abedi@gmail.com>
2023-04-14 17:32:13 +0430

    CM-336-How to Store JSON in an Entity Field with EF Core

---
+++ dotnet-efcore/EFCoreJsonInEntityField/EFCoreJsonInEntityField/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreJsonInEntityField.Mapping;
+using EFCoreJsonInEntityField.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+
+namespace EFCoreJsonInEntityField
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderMap());        
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
