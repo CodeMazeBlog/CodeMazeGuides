1d2965dc6a41002f344248f9e2f8331856788d7e
asfd1234 <118693767+asfd1234@users.noreply.github.com>
2023-04-17 15:17:12 +0100

    Add initial example and tests

---
+++ csharp-basic-topics/UrlEncodingAndDecoding/UrlEncodingAndDecoding/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Web;
+
+Console.WriteLine("Hello, World!");
+
+var url = @"http://example.com/resource?foo=bar#fragment";
+
+var httpUtilityEncoded = HttpUtility.UrlEncode(url);
+var httpUtilityDecoded = HttpUtility.UrlDecode(httpUtilityEncoded);
+
+var webUtilityEncoded = WebUtility.UrlEncode(url);
+var webUtilityDecoded = WebUtility.UrlDecode(webUtilityEncoded);
+
+var uriEncoded = Uri.EscapeDataString(url);
+var uriDecoded = Uri.UnescapeDataString(uriEncoded);
+
+Console.WriteLine(httpUtilityEncoded);
+Console.WriteLine(webUtilityEncoded);
+Console.WriteLine(uriEncoded);
+
+Console.WriteLine(httpUtilityDecoded);
+Console.WriteLine(webUtilityDecoded);
+Console.WriteLine(uriDecoded);
