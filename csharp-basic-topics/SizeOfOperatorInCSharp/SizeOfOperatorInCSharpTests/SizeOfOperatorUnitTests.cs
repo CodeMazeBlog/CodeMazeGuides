417fedc08154d0cf03a2856dad9bb457f9deac0b
Martin Chege <martinchege324@gmail.com>
2023-04-18 13:16:45 +0300

    Test method naming changes and added some inline comments.

--- csharp-basic-topics/SizeOfOperatorInCSharp/SizeOfOperatorInCSharpTests/SizeOfOperatorUnitTests.cs
+++ csharp-basic-topics/SizeOfOperatorInCSharp/SizeOfOperatorInCSharpTests/SizeOfOperatorUnitTests.cs
@@ -4,36 +4,36 @@ namespace SizeOfOperatorInCSharpTests
 {
     public struct MyStruct 
     {
-        public byte byteVar;
-        public int intVar;
-        public long longVar;
-        public double doubleVar;
-        public float floatVar;
-        public short shortVar;
-        public char charVar;
-        public decimal decimalVar;
-        public bool boolVar;
+        //public byte byteVar;//1
+        public int intVar;//4
+        public long longVar;//8
+        //public double doubleVar;//8
+        //public float floatVar;//4
+        //public short shortVar;//2
+        //public char charVar;//2
+        //public decimal decimalVar;//16
+        //public bool boolVar;//1
     }
 
     [TestClass]
     public class SizeOfOperatorUnitTests
     {
         [TestMethod]
-        public void GivenAPrimitiveTypeVerifyAccurateSize()
+        public void GivenAPrimitiveType_WhenSizeofInvoked_VerifyAccurateSize()
         {
-            Assert.AreEqual(1, sizeof(byte));
-            Assert.AreEqual(2, sizeof(short));
+            //Assert.AreEqual(1, sizeof(byte));
+            //Assert.AreEqual(2, sizeof(short));
             Assert.AreEqual(4, sizeof(int));
             Assert.AreEqual(8, sizeof(long));
-            Assert.AreEqual(2, sizeof(char));
-            Assert.AreEqual(4, sizeof(float));
-            Assert.AreEqual(8, sizeof(double));
-            Assert.AreEqual(16, sizeof(decimal));
-            Assert.AreEqual(1, sizeof(bool));
+            //Assert.AreEqual(2, sizeof(char));
+            //Assert.AreEqual(4, sizeof(float));
+            //Assert.AreEqual(8, sizeof(double));
+            //Assert.AreEqual(16, sizeof(decimal));
+            //Assert.AreEqual(1, sizeof(bool));
         }
 
         [TestMethod]
-        public void GivenTwoPrimitiveTypesCompareSizes()
+        public void GivenTwoPrimitiveTypes_WhenSizeofInvoked_CompareSizes()
         {
             Assert.IsTrue(sizeof(byte) < sizeof(short));
             Assert.IsTrue(sizeof(int) == sizeof(float));
@@ -42,9 +42,9 @@ namespace SizeOfOperatorInCSharpTests
         }
 
         [TestMethod]
-        public void GivenAStructTypeVerifyAccurateSize()
+        public void GivenAStructType_WhenSizeofInvoked_VerifyAccurateSize()
         {
-            var expected = 56;
+            var expected = 12;
             var actual = Marshal.SizeOf(typeof(MyStruct));
 
             Assert.AreEqual(expected, actual);
