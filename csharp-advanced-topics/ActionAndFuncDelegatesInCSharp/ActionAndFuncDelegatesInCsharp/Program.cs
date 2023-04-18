c450a0829ac1c37be9e159c8f00976baae73dc98
Ricardo Mauro <ricardo.demauro@outlook.com>
2023-04-17 20:53:30 -0400

    adding ActionAndFuncDelegateInCSharp project

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Action helloWorldDelegate = () => Console.Write("Hello, World!");
+helloWorldDelegate(); // Output: "Hello, World!"
+
+//👇 First and second arguments are integers
+Action<int, int> WriteSumOfDelegate = (operator1, operator2) =>
+{
+    Console.Write("Sum of {0} and {1} is {2}", operator1, operator2, operator1 + operator2);
+};
+
+WriteSumOfDelegate(10, 20); //Output: "Sum of 10 and 20 is 30"
+
+Func<int, int> DoubleItDelegate = (operator1) =>
+{
+    return operator1 * 2;
+};
+var result = DoubleItDelegate(2); //returns 4
+
+Console.WriteLine("Func Double result {0}", result); //Output: "Func Double result 4"
+
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/Tests/DelegateTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System.Text;
+using Xunit;
+
+namespace Tests;
+
+public class DelegateTest
+{
+    static readonly StringWriter bufferWriter = new StringWriter();
+
+    public DelegateTest()
+    {
+        StringBuilder sb = bufferWriter.GetStringBuilder();
+        sb.Remove(0, sb.Length);
+
+        Console.SetOut(bufferWriter);
+    }
+
+    [Theory]
+    [InlineData(10, 20, "Sum of 10 and 20 is 30")]
+    public void WhenSumActionIsDefined_ThemWriteTheSum(int operator1, int operator2, string expectedOutput)
+    {
+        Action<int, int> WriteSumOfDelegate = (operator1, operator2) =>
+        {
+            Console.Write("Sum of {0} and {1} is {2}", operator1, operator2, operator1 + operator2);
+        };
+        WriteSumOfDelegate(operator1, operator2);
+
+
+        string bufferOutput = bufferWriter?.ToString() ?? string.Empty;
+        Assert.Equal(expectedOutput, bufferOutput);
+    }
+
+    [Fact]
+    public void WhenActionDelegateIsDefined_ThemWriteHelloWorld()
+    {
+        Action helloWorldDelegate = () => Console.Write("Hello, World!");
+        helloWorldDelegate();
+
+        string bufferOutput = bufferWriter?.ToString() ?? string.Empty;
+        Assert.Equal("Hello, World!", bufferOutput);
+    }
+
+    [Theory]
+    [InlineData(2, 4)]
+    public void WhenFuncDoubleIsDefined_ThemReturnDoubleOfInput(int operator1, int expectedOutput)
+    {
+        Func<int, int> DoubleItDelegate = (operator1) =>
+        {
+            return operator1 * 2;
+        };
+        var result = DoubleItDelegate(operator1);
+
+        Console.WriteLine("Func Double result {0}", result);
+        Assert.Equal(expectedOutput, result);
+    }
+}
