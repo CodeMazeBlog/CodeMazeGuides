ec6bc48ddc2ef07096455bf47af059ee10fdf0db
Shahrzad Abedi <shahrzad.abedi@gmail.com>
2023-04-19 19:49:35 +0430

    CM-336-How to Store JSON in an Entity Field with EF Core

--- dotnet-efcore/EFCoreJsonInEntityField/Tests/EFCoreJsonInEntityFieldUnitTest.cs
+++ dotnet-efcore/EFCoreJsonInEntityField/Tests/EFCoreJsonInEntityFieldUnitTest.cs
@@ -1,14 +1,6 @@
 ﻿using EFCoreJsonInEntityField.Models;
 using EFCoreJsonInEntityField;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using EFCoreJsonInEntityField.Mapping;
 
 namespace Tests
 {
