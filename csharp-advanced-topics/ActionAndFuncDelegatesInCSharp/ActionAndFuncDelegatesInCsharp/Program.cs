0822b469e0e400c989abba758981e24640be6bd0
Ricardo Mauro <ricardo.demauro@outlook.com>
2023-04-22 12:03:42 -0400

    :contruction: refactoring code to address blog post 88221

--- csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -1,24 +1,65 @@
-﻿using System;
+﻿var delegates = new Delegates();
 
-Action helloWorldDelegate = () => Console.Write("Hello, World!");
-helloWorldDelegate(); // Output: "Hello, World!"
+Console.WriteLine("Running Action Delegates");
+delegates.SampleAction_1();
+delegates.SampleAction_2();
 
-//👇 First and second arguments are integers
-Action<int, int> WriteSumOfDelegate = (operator1, operator2) =>
-{
-    Console.Write("Sum of {0} and {1} is {2}", operator1, operator2, operator1 + operator2);
-};
+Console.WriteLine();
+Console.WriteLine("Running Func Deleagates");
+delegates.SampleFunc_1();
+delegates.SampleFunc_2();
 
-WriteSumOfDelegate(10, 20); //Output: "Sum of 10 and 20 is 30"
-
-Func<int, int> DoubleItDelegate = (operator1) =>
-{
-    return operator1 * 2;
-};
-var result = DoubleItDelegate(2); //returns 4
-
-Console.WriteLine("Func Double result {0}", result); //Output: "Func Double result 4"
-
-
+Console.WriteLine();
 Console.WriteLine("Press any key to exit");
 Console.ReadKey();
+
+public class Delegates
+{
+    public void SampleAction_1()
+    {
+        //👇 First and second arguments are integers
+        Action<int, int> WriteSumOfDelegate = (operator1, operator2) =>
+        {
+            Console.WriteLine("Sum of {0} and {1} is {2}", operator1, operator2, operator1 + operator2);
+        };
+
+        WriteSumOfDelegate(10, 20); //Output: "Sum of 10 and 20 is 30"
+    }
+
+    static void WriteDoubleOfAction(int operator1)
+    {
+        int doubleOf = operator1 * 2;
+        Console.WriteLine($"The Double of {operator1} is {doubleOf}");
+    }
+
+    public void SampleAction_2()
+    {
+        //Using class defined Method
+        Action<int> WriteDoubleOfDelegate = WriteDoubleOfAction;
+        WriteDoubleOfDelegate(10); //The Double of 10 is 20
+    }
+
+
+
+    public void SampleFunc_1()
+    {
+        Func<int, int> DoubleItDelegate = (operator1) =>
+        {
+            return operator1 * 2;
+        };
+        var result = DoubleItDelegate(2); //returns 4
+        Console.WriteLine("Func Double result {0}", result); //Output: "Func Double result 4"
+    }
+
+    public void SampleFunc_2()
+    {
+        //Using class defined Method
+        Func<int, int> TripleDelegate = Delegates.TripleItFunc;
+        var tripleResult = TripleDelegate(2); //returns 6
+
+        //Output: "Func TripleDelegate with Argument 2 is 6"
+        Console.WriteLine($"Func TripleDelegate with Argument {2} is {tripleResult}");
+    }
+
+    static int TripleItFunc(int operator1) => operator1 * 3;
+}
--- csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/Tests/DelegateTest.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/Tests/DelegateTest.cs
@@ -16,42 +16,47 @@ public class DelegateTest
         Console.SetOut(bufferWriter);
     }
 
-    [Theory]
-    [InlineData(10, 20, "Sum of 10 and 20 is 30")]
-    public void WhenSumActionIsDefined_ThenWriteTheSum(int operator1, int operator2, string expectedOutput)
+    [Fact]
+    public void WhenSampleAction1_ThenWriteConsole()
     {
-        Action<int, int> WriteSumOfDelegate = (operator1, operator2) =>
-        {
-            Console.Write("Sum of {0} and {1} is {2}", operator1, operator2, operator1 + operator2);
-        };
-        WriteSumOfDelegate(operator1, operator2);
+        var delegatesSample = new Delegates();
 
+        delegatesSample.SampleAction_1();
 
         string bufferOutput = bufferWriter?.ToString() ?? string.Empty;
-        Assert.Equal(expectedOutput, bufferOutput);
+        Assert.Equal($"Sum of 10 and 20 is 30{Environment.NewLine}", bufferOutput);
     }
 
     [Fact]
-    public void WhenActionDelegateIsDefined_ThenWriteHelloWorld()
+    public void WhenSampleAction2_ThenWriteConsole()
     {
-        Action helloWorldDelegate = () => Console.Write("Hello, World!");
-        helloWorldDelegate();
+        var delegatesSample = new Delegates();
+
+        delegatesSample.SampleAction_2();
 
         string bufferOutput = bufferWriter?.ToString() ?? string.Empty;
-        Assert.Equal("Hello, World!", bufferOutput);
+        Assert.Equal($"The Double of 10 is 20{Environment.NewLine}", bufferOutput);
     }
 
-    [Theory]
-    [InlineData(2, 4)]
-    public void WhenFuncDoubleIsDefined_ThenReturnDoubleOfInput(int operator1, int expectedOutput)
+    [Fact]
+    public void WhenSampleFunc1_ThenWriteConsole()
     {
-        Func<int, int> DoubleItDelegate = (operator1) =>
-        {
-            return operator1 * 2;
-        };
-        var result = DoubleItDelegate(operator1);
+        var delegatesSample = new Delegates();
 
-        Console.WriteLine("Func Double result {0}", result);
-        Assert.Equal(expectedOutput, result);
+        delegatesSample.SampleFunc_1();
+
+        string bufferOutput = bufferWriter?.ToString() ?? string.Empty;
+        Assert.Equal($"Func Double result 4{Environment.NewLine}", bufferOutput);
+    }
+
+    [Fact]
+    public void WhenSampleFunc2_ThenWriteConsole()
+    {
+        var delegatesSample = new Delegates();
+
+        delegatesSample.SampleFunc_2();
+
+        string bufferOutput = bufferWriter?.ToString() ?? string.Empty;
+        Assert.Equal($"Func TripleDelegate with Argument 2 is 6{Environment.NewLine}", bufferOutput);
     }
 }
