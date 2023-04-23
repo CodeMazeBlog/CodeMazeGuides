d7dc37b43a287b9f6be75594d54d1cdf4b1e6db3
Satya <satyap48@gmail.com>
2023-04-23 15:28:19 +0530

    CM-104: Object Initializers in C#

---
+++ csharp-basic-topics/ObjectInitializersInCSharp/ObjectInitializersInCSharp/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ObjectInitializersInCSharp
+{
+    public class Product
+    {
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ csharp-basic-topics/ObjectInitializersInCSharp/ObjectInitializersInCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace ObjectInitializersInCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args) { }
+    }
+}
