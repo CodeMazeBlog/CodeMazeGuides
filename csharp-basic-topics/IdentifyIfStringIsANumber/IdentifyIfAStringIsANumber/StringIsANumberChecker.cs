a1178e26246b1bdf21bd290023de99290ad97372
filipibc <filipibc@gmail.com>
2023-04-24 17:05:00 -0300

    Replacing parameters name

--- csharp-basic-topics/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber/StringIsANumberChecker.cs
+++ csharp-basic-topics/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber/StringIsANumberChecker.cs
@@ -4,26 +4,26 @@ namespace IdentifyIfAStringIsANumber;
 
 public class StringIsANumberChecker
 {
-    public static bool IntTryParse(string value)
+    public static bool IntTryParse(string stringValue)
     {
-        return int.TryParse(value, out _);
+        return int.TryParse(stringValue, out _);
     }
 
-    public static bool DoubleTryParse(string value)
+    public static bool DoubleTryParse(string stringValue)
     {
-        return double.TryParse(value, out _);
+        return double.TryParse(stringValue, out _);
     }
 
-    public static bool UsingRegex(string value)
+    public static bool UsingRegex(string stringValue)
     {
         var pattern = @"^-?\d+(?:\.\d+)?$";
 
         var regex = new Regex(pattern);
-        return regex.IsMatch(value);
+        return regex.IsMatch(stringValue);
     }
 
-    public static bool UsingCharIsDigit(string value)
+    public static bool UsingCharIsDigit(string stringValue)
     {
-        return value.All(char.IsDigit);
+        return stringValue.All(char.IsDigit);
     }
 }
