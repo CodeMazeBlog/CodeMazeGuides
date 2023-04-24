946e2070c125ea790368bcaaa0c3214a32f1626d
Ainea Wabwoba <aineah28@gmail.com>
2023-04-24 05:05:06 +0300

    made minor code updates, moved solution to new folder

--- dotnet-efcore/RecordsAsModelClasses/RecordsAsModelClasses/Core/Context/CarDbContext.cs
+++ dotnet-efcore/RecordsAsModelClasses/RecordsAsModelClasses/Core/Context/CarDbContext.cs
@@ -5,14 +5,13 @@ namespace RecordsAsModelClasses.Core.Context;
 
 public class CarDbContext : DbContext
 {
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<VintageCar> VintageCars { get; set; }
+
     public CarDbContext(DbContextOptions<CarDbContext> options)
         : base(options)
     {
     }
-
-    public DbSet<Car> Cars { get; set; }
-
-    public DbSet<VintageCar> VintageCars { get; set; }
 
     protected override void OnConfiguring
         (DbContextOptionsBuilder optionsBuilder)
--- dotnet-efcore/RecordsAsModelClasses/Tests/RecordsAsModelClassesUnitTests/RecordsAsModelClassesUnitTest.cs
+++ dotnet-efcore/RecordsAsModelClasses/Tests/RecordsAsModelClassesUnitTests/RecordsAsModelClassesUnitTest.cs
@@ -1,24 +1,21 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
-using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using RecordsAsModelClasses.Core.DTOs;
 using RecordsAsModelClasses.Core.Entities;
 using Xunit;
 
-namespace RecordsAsModelClassesUnitTests;
+namespace Tests;
 
-public class RecordsAsModelClassesUnitTests
+public class RecordsAsModelClassesUnitTest
 {
-    private HttpClient _client;
-    private WebApplicationFactory<Program> _application;
+    private readonly HttpClient _client;
 
-    public RecordsAsModelClassesUnitTests()
+    public RecordsAsModelClassesUnitTest()
     {
-        _application = new WebApplicationFactory<Program>();
-        _client = _application.CreateClient();
+        var application = new WebApplicationFactory<Program>();
+        _client = application.CreateClient();
     }
 
     [Fact]
@@ -31,7 +28,7 @@ public class RecordsAsModelClassesUnitTests
         var expectedResponse = new Car(1, "Toyota", "Crown", 1952);
 
         Assert.Equal(HttpStatusCode.OK, httpResponseMessage.StatusCode);
-        Assert.Equal(expectedResponse,deserializedResponse);
+        Assert.Equal(expectedResponse, deserializedResponse);
     }
 
     [Fact]
@@ -42,10 +39,20 @@ public class RecordsAsModelClassesUnitTests
         var httpResponseMessage = await _client.PutAsJsonAsync("api/cars/1", carDto);
 
         Assert.Equal(HttpStatusCode.NoContent, httpResponseMessage.StatusCode);
-    }   
+    }
 
     [Fact]
-    public async Task WhenCreateVintageCarEndpointIsCalled_ThenCreateVintageCar()
+    public async Task WhenUpdateCarUsingRecordsEndpointIsCalled_ThenUpdateCar()
+    {
+        var car = new Car(1, "Honda", "Civic", 2023);
+
+        var httpResponseMessage = await _client.PutAsJsonAsync($"api/cars/car/{car.Id}", car);
+
+        Assert.Equal(HttpStatusCode.NoContent, httpResponseMessage.StatusCode);
+    }
+
+    [Fact]
+    public async Task WhenVintageCarCreateEndpointIsCalled_ThenCreateVintageCar()
     {
         var vintageCarDto = new VintageCarDto("Chevrolet", "Corvette", 1972);
 
@@ -58,14 +65,14 @@ public class RecordsAsModelClassesUnitTests
         Assert.Equal(HttpStatusCode.OK, httpResponseMessage.StatusCode);
         Assert.Equal(expectedResponse, deserializedResponse);
     }
-        
+
     [Fact]
     public async Task WhenVintageCarUpdateEndpointIsCalled_ThenUpdateVintageCar()
     {
         var vintageCarDto = new VintageCarDto("Chevrolet", "Corvette", 1972);
 
         var httpResponseMessage = await _client.PutAsJsonAsync("api/cars/vintage-car/1", vintageCarDto);
-        
+
         Assert.Equal(HttpStatusCode.NoContent, httpResponseMessage.StatusCode);
     }
 }
