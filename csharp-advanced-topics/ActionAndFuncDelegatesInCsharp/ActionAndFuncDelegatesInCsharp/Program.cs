071cfe8eb2712d484e2dfe4c25cf3331148ed42f
kevinLug <56061354+Otimkev@users.noreply.github.com>
2023-04-25 14:14:59 +0300

    Clean up

--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -9,18 +9,9 @@ namespace ActionAndFuncDelegatesInCsharp
             Action<string> writeMessage = Display.DisplayText;
             writeMessage("WARAP");
 
-            Func<int, int, int> multiply = new Calculator().Multiply;
+            Func<int, int, int> multiply = Calculator.Multiply;
             int myResult = multiply(1, 2);
             Console.WriteLine(myResult);
-
-            //int result = addNumbers(1, 2);
-            //Console.WriteLine(result);
-            
-            //var calculator = new Calculator();
-            //int a = 2;
-            //int b = 3;
-            //int result = calculator.Multiply(a, b);
-            //Console.WriteLine(result);
         }
     }
 }
