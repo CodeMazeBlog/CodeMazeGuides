d2f6794e22b388371f778a028d3cc4ddfd3398f1
Ivan Gechev <ivan_banov@abv.bg>
2023-04-26 16:27:54 +0300

    Tests

---
+++ csharp-basic-topics/InitSetters/InitSetters.Tests/CityTests.cs
@@ -0,0 +1,39 @@
+using FluentAssertions;
+
+namespace InitSetters.Tests
+{
+    public class CityTests
+    {
+        private readonly string _name = "Tokyo";
+        private readonly double _lat = 139.839478;
+        private readonly double _lon = 35.652832;
+
+        [Fact]
+        public void WhenConstructorIsInvoked_ThenValidObjectIsReturned()
+        {
+            // Act
+            var city = new City(_name, _lat, _lon);
+
+            // Assert
+            city.Should().NotBeNull();
+            city.Should().BeOfType<City>();
+            city.Name.Should().Be(_name);
+            city.Latitude.Should().Be(_lat);
+            city.Longitude.Should().Be(_lon);
+        }
+
+        [Fact]
+        public void WhenChangeNameIsInvoked_ThenNameIsChanged()
+        {
+            // Arrange
+            var newName = "Edo";
+            var city = new City(_name, _lat, _lon);
+
+            // Act
+            city.ChangeName(newName);
+
+            // Assert
+            city.Name.Should().Be(newName);
+        }
+    }
+}
