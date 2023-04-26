560cc99633d639693eec09aaf3e3801a388c63a6
filipibc <filipibc@gmail.com>
2023-04-26 11:31:37 -0300

    Test refectoring

--- csharp-basic-topics/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber.Tests/Tests.cs
+++ csharp-basic-topics/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber.Tests/Tests.cs
@@ -3,118 +3,62 @@ namespace IdentifyIfAStringIsANumber.Tests;
 public class Tests
 {
     [Theory]
-    [InlineData("123")]
-    [InlineData("444")]
-    public void GivenAnIntegerNumbersAsString_WhenUsingIntTryParse_ThenReturnTrue(string value)
+    [InlineData("123", true)]
+    [InlineData("444", true)]
+    [InlineData("1.2345", false)]
+    [InlineData("2.4412", false)]
+    [InlineData("9999999999", false)]
+    [InlineData("abc", false)]
+    [InlineData("414aa", false)]
+    public void GivenAString_WhenUsingIntTryParseMethod_ThenTryToParseIt(string value, bool expectedResult)
     {
         var result = StringIsANumberChecker.IntTryParse(value);
 
-        Assert.True(result);
+        Assert.Equal(expectedResult, result);
     }
 
     [Theory]
-    [InlineData("1.2345")]
-    [InlineData("2.4412")]
-    public void GivenAnDecimalNumberAsString_WhenUsingIntTryParse_ThenReturnFalse(string value)
-    {
-        var result = StringIsANumberChecker.IntTryParse(value);
-
-        Assert.False(result);
-    }
-
-    [Theory]
-    [InlineData("9999999999")]
-    public void GivenABigIntegerNumberAsString_WhenUsingIntTryParse_ThenReturnFalse(string value)
-    {
-        var result = StringIsANumberChecker.IntTryParse(value);
-
-        Assert.False(result);
-    }
-
-    [Theory]
-    [InlineData("123")]
-    [InlineData("444")]
-    public void GivenAnIntegerNumbersAsString_WhenUsingCharIsDigit_ThenReturnTrue(string value)
+    [InlineData("123", true)]
+    [InlineData("444", true)]
+    [InlineData("1.2345", false)]
+    [InlineData("2.4412", false)]
+    [InlineData("9999999999", true)]
+    [InlineData("abc", false)]
+    [InlineData("414aa", false)]
+    public void GivenAString_WhenUsingCharIsDigit_ThenTryToParseIt(string value, bool expectedResult)
     {
         var result = StringIsANumberChecker.UsingCharIsDigit(value);
 
-        Assert.True(result);
+        Assert.Equal(expectedResult, result);
     }
 
     [Theory]
-    [InlineData("1.2345")]
-    [InlineData("2.4412")]
-    public void GivenAnDecimalNumberAsString_WhenUsingCharIsDigit_ThenReturnFalse(string value)
-    {
-        var result = StringIsANumberChecker.UsingCharIsDigit(value);
-
-        Assert.False(result);
-    }
-
-    [Theory]
-    [InlineData("9999999999")]
-    public void GivenABigIntegerNumberAsString_WhenUsingCharIsDigit_ThenReturnTrue(string value)
-    {
-        var result = StringIsANumberChecker.UsingCharIsDigit(value);
-
-        Assert.True(result);
-    }
-
-    [Theory]
-    [InlineData("123")]
-    [InlineData("444")]
-    public void GivenAnIntegerNumbersAsString_WhenUsingDoubleTryParse_ThenReturnTrue(string value)
+    [InlineData("123", true)]
+    [InlineData("444", true)]
+    [InlineData("1.2345", true)]
+    [InlineData("2.4412", true)]
+    [InlineData("9999999999", true)]
+    [InlineData("abc", false)]
+    [InlineData("414aa", false)]
+    public void GivenAString_WhenUsingDoubleTryParse_ThenTryToParseIt(string value, bool expectedResult)
     {
         var result = StringIsANumberChecker.DoubleTryParse(value);
 
-        Assert.True(result);
+        Assert.Equal(expectedResult, result);
     }
 
     [Theory]
-    [InlineData("1.2345")]
-    [InlineData("2.4412")]
-    public void GivenAnDecimalNumberAsString_WhenUsingDoubleTryParse_ThenReturnTrue(string value)
-    {
-        var result = StringIsANumberChecker.DoubleTryParse(value);
-
-        Assert.True(result);
-    }
-
-    [Theory]
-    [InlineData("9999999999")]
-    public void GivenABigIntegerNumberAsString_WhenUsingDoubleTryParse_ThenReturnTrue(string value)
-    {
-        var result = StringIsANumberChecker.DoubleTryParse(value);
-
-        Assert.True(result);
-    }
-
-    [Theory]
-    [InlineData("123")]
-    [InlineData("444")]
-    public void GivenAnIntegerNumbersAsString_WhenUsingRegex_ThenReturnTrue(string value)
+    [InlineData("123", true)]
+    [InlineData("444", true)]
+    [InlineData("1.2345", true)]
+    [InlineData("2.4412", true)]
+    [InlineData("9999999999", true)]
+    [InlineData("abc", false)]
+    [InlineData("414aa", false)]
+    public void GivenAString_WhenUsingRegex_ThenTryToParseIt(string value, bool expectedResult)
     {
         var result = StringIsANumberChecker.UsingRegex(value);
 
-        Assert.True(result);
-    }
-
-    [Theory]
-    [InlineData("1.2345")]
-    [InlineData("2.4412")]
-    public void GivenAnDecimalNumberAsString_WhenUsingRegex_ThenReturnTrue(string value)
-    {
-        var result = StringIsANumberChecker.UsingRegex(value);
-
-        Assert.True(result);
-    }
-
-    [Theory]
-    [InlineData("9999999999")]
-    public void GivenABigIntegerNumberAsString_WhenUsingRegex_ThenReturnTrue(string value)
-    {
-        var result = StringIsANumberChecker.UsingRegex(value);
-
-        Assert.True(result);
+        Assert.Equal(expectedResult, result);
     }
 }
