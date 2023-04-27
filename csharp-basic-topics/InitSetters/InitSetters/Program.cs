498883faa11a2202ddf9b91576f181d323bdf25f
Ivan Gechev <ivan_banov@abv.bg>
2023-04-27 20:52:41 +0300

    final changes

--- csharp-basic-topics/InitSetters/InitSetters/Program.cs
+++ csharp-basic-topics/InitSetters/InitSetters/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var city = new City("Tokyo", 139.839478, 35.652832);
+
+            Console.WriteLine($"{city.Name}: {city.Latitude}, {city.Longitude}");
+
+            city.ChangeName("Edo");
+
+            Console.WriteLine($"{city.Name}: {city.Latitude}, {city.Longitude}");
         }
     }
 }
