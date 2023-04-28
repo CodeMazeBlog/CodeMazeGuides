6481ebeff5ca6bffc8c958e28ef63277d888253e
Ivan Gechev <ivan_banov@abv.bg>
2023-04-28 20:37:57 +0300

    InitialCommit

---
+++ dotnet-client-libraries/BenchmarkDotNet-MemoryDiagnoser-Attribute/BenchmarkDotNet-MemoryDiagnoser-Attribute/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BenchmarkDotNet_MemoryDiagnoser_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
