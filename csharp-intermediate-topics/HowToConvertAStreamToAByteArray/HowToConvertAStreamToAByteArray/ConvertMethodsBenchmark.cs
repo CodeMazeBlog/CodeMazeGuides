fb01e71eb7f6acac2b8df32f82713d6134df4b56
cjaynjoku <njokujennaro@gmail.com>
2023-05-01 21:35:24 +0100

    CM-377: Convert Stream into Byte Array in C#

---
+++ csharp-intermediate-topics/HowToConvertAStreamToAByteArray/HowToConvertAStreamToAByteArray/ConvertMethodsBenchmark.cs
@@ -0,0 +1,58 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace HowToConvertAStreamToAByteArray
+{
+    [MemoryDiagnoser(false)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class ConvertMethodsBenchmark
+    {
+        private static readonly string _sampleFilePath
+            = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.Parent.Parent.FullName, "benchmarkfile.txt");
+
+        [Benchmark]
+        public void UseTheStreamDotReadMethod()
+        {
+            using var benchmarkStream = new FileStream(_sampleFilePath, FileMode.Open, FileAccess.Read);
+            var convertStreamToByteArray = new ConvertStreamToByteArray(benchmarkStream);
+
+            convertStreamToByteArray.UseTheStreamDotReadMethod();
+        }
+
+        [Benchmark]
+        public void UseAMemoryStream()
+        {
+            using var benchmarkStream = new FileStream(_sampleFilePath, FileMode.Open, FileAccess.Read);
+            var convertStreamToByteArray = new ConvertStreamToByteArray(benchmarkStream);
+
+            convertStreamToByteArray.UseAMemoryStream();
+        }
+
+        [Benchmark]
+        public void UseABufferedStream()
+        {
+            using var benchmarkStream = new FileStream(_sampleFilePath, FileMode.Open, FileAccess.Read);
+            var convertStreamToByteArray = new ConvertStreamToByteArray(benchmarkStream);
+
+            convertStreamToByteArray.UseABufferedStream();
+        }
+
+        [Benchmark]
+        public void UseAStreamReader()
+        {
+            using var benchmarkStream = new FileStream(_sampleFilePath, FileMode.Open, FileAccess.Read);
+            var convertStreamToByteArray = new ConvertStreamToByteArray(benchmarkStream);
+
+            convertStreamToByteArray.UseAStreamReader();
+        }
+
+        [Benchmark]
+        public void UseABinaryReader()
+        {
+            using var benchmarkStream = new FileStream(_sampleFilePath, FileMode.Open, FileAccess.Read);
+            var convertStreamToByteArray = new ConvertStreamToByteArray(benchmarkStream);
+
+            convertStreamToByteArray.UseABinaryReader();
+        }
+    }
+}
---
+++ csharp-intermediate-topics/HowToConvertAStreamToAByteArray/HowToConvertAStreamToAByteArray/ConvertStreamToByteArray.cs
@@ -0,0 +1,78 @@
+﻿namespace HowToConvertAStreamToAByteArray
+{
+    public class ConvertStreamToByteArray
+    {
+        private readonly Stream _stream;
+
+        public ConvertStreamToByteArray(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public byte[] UseTheStreamDotReadMethod()
+        {
+            byte[] bytes;
+            List<byte> totalStream = new();
+            byte[] buffer = new byte[32];
+            int read;
+            while ((read = _stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                totalStream.AddRange(buffer.Take(read));
+            }
+            bytes = totalStream.ToArray();
+
+            return bytes;
+        }
+
+        public byte[] UseABinaryReader()
+        {
+            byte[] bytes;
+            using (var binaryReader = new BinaryReader(_stream))
+            {
+                bytes = binaryReader.ReadBytes((int)_stream.Length);
+            }
+
+            return bytes;
+        }
+
+        public byte[] UseAStreamReader()
+        {
+            byte[] bytes;
+            using (var reader = new StreamReader(_stream))
+            {
+                bytes = System.Text.Encoding.UTF8.GetBytes(reader.ReadToEnd());
+            }
+
+            return bytes;
+        }
+
+        public byte[] UseAMemoryStream()
+        {
+            byte[] bytes;
+            if (_stream is MemoryStream stream)
+            {
+                return stream.ToArray();
+            }
+            using (var memoryStream = new MemoryStream())
+            {
+                _stream.CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            return bytes;
+        }
+
+        public byte[] UseABufferedStream()
+        {
+            byte[] bytes;
+            using (var bufferedStream = new BufferedStream(_stream))
+            {
+                using var memoryStream = new MemoryStream();
+                bufferedStream.CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+                
+            return bytes;
+        }
+    }
+}
