7465f0b529c26e02758bb947a971c5265157bf38
Satya <satyap48@gmail.com>
2023-05-01 20:10:15 +0530

    CM:104 - Added code to show the difference between constructors and object initializers

--- csharp-basic-topics/ObjectInitializersInCSharp/ObjectInitializersInCSharp/Program.cs
+++ csharp-basic-topics/ObjectInitializersInCSharp/ObjectInitializersInCSharp/Program.cs
@@ -2,6 +2,21 @@
 {
     public class Program
     {
-        static void Main(string[] args) { }
+        static void Main(string[] args)
+        {
+            //Using Object Initializer
+            var product = new Product
+            {
+                Name = "Book",
+                Quantity = 15,
+                UnitPrice = 17.99m
+            };
+
+            //Using a constructor
+            product = new Product();
+            product.Name = "Book";
+            product.Quantity = 15;
+            product.UnitPrice = 17.99m;
+        }
     }
 }
