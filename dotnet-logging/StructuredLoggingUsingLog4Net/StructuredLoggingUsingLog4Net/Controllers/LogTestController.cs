ca67d069350f529b01359f795b8c78c232c69e84
Muhammed Saleem <muhammedsalimp@gmail.com>
2023-05-03 13:04:19 +0530

    fixed formatting issues

--- dotnet-logging/StructuredLoggingUsingLog4Net/StructuredLoggingUsingLog4Net/Controllers/LogTestController.cs
+++ dotnet-logging/StructuredLoggingUsingLog4Net/StructuredLoggingUsingLog4Net/Controllers/LogTestController.cs
@@ -8,6 +8,7 @@ namespace StructuredLoggingUsingLog4Net.Controllers
     public class LogTestController : ControllerBase
     {
         private readonly ILog _logger;
+
         public LogTestController(ILog logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
@@ -19,6 +20,7 @@ namespace StructuredLoggingUsingLog4Net.Controllers
             _logger.Info("This is an informational log.");
             _logger.Error("This is an error log.");
             _logger.Info(new { id = 1, name = "John" });
+
             return Ok("Test success!");
         }
     }
