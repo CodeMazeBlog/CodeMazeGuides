db00db216799a1f5351614fab88bb2cc908b492c
NarmadhaDodda <narmadhadodda98@gmail.com>
2023-05-03 20:44:30 +0100

    ActionandFuncDelegates Example added.

---
+++ csharp-basic-topics/ActionandFuncDelegatesInCSharp/ActionandFunc/Delegates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionandFunc
+{
+    public class Delegates
+    {
+        public static void DoAction(Action<int, string> action)
+        {
+            // Do some action with the given input parameters
+            action(10, "test");
+        }
+
+        public static TResult DoFunc<TResult>(Func<int, int, TResult> func)
+        {
+            // Call the function with some input parameters
+            return func(10, 20);
+        }
+    }
+}
---
+++ csharp-basic-topics/ActionandFuncDelegatesInCSharp/ActionandFuncTests/DelegatesUnitTest.cs
@@ -0,0 +1,31 @@
+using ActionandFunc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TernaryOperatorTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void GivenAction_WhenInvoked_ThenActionExecuted()
+        {
+            bool actionExecuted = false;
+            Action<int, string> action = (i, s) => actionExecuted = true;
+
+            Delegates.DoAction(action);
+
+            Assert.IsTrue(actionExecuted);
+        }
+
+        [TestMethod]
+        public void GivenFunc_WhenInvoked_ThenFuncResultReturned()
+        {
+            Func<int, int, int> func = (x, y) => x + y;
+
+            int result = Delegates.DoFunc(func);
+
+            Assert.AreEqual(30, result);
+        }
+    }
+}
