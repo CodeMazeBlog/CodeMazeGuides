e4d9f679a66d2df45226f7387448671e41a2d2d7
ralptashinganyoni <ralphtashinga@gmail.com>
2023-05-04 10:01:19 +0200

    Removed comments to Console.WriteLine on Func delegates

--- csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/FuncDelegates.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/FuncDelegates.cs
@@ -5,10 +5,10 @@ public class FuncDelegates
     {
 
         Func<string, string, string> greetWithNameFirst = new Func<string, string, string>(GreetWithName);
-        //Console.WriteLine(greetWithNameFirst("Ralph", "Nyoni"));
+        Console.WriteLine(greetWithNameFirst("Ralph", "Nyoni"));
 
         Func<string, string, string> greetWithNameSecond = GreetWithName;
-        //Console.WriteLine(greetWithNameSecond("Nyoni", "Ralph"));
+        Console.WriteLine(greetWithNameSecond("Nyoni", "Ralph"));
     }
     public static string GreetWithName(string firstName, string lastName)
     {
