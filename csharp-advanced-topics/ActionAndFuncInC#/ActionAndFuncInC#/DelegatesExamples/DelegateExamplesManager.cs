ca09969c43375a6362a0c2e0ad3116b381c39f54
Simuzeche Kakoko Kaluwa <simuzechek@gmail.com>
2023-05-07 10:45:38 +0200

    Feat : Added Delegates and Their Tests methods

---
+++ csharp-advanced-topics/ActionAndFuncInC#/ActionAndFuncInC#/DelegatesExamples/DelegateExamplesManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExamples
+{
+    public class DelegateExamplesManager
+    {
+        public static void ActionExample()
+        {
+            // Declare an Action delegate that accepts a string parameter
+            Action<string> displayMessage = (message) => Console.WriteLine(message);
+
+            // Call the delegate with a string parameter
+            displayMessage("Hello, World!");
+        }
+
+        public static int FuncExample()
+        {
+            // Declare a Func delegate that accepts two int parameters and returns an int
+            Func<int, int, int> addNumbers = (a, b) => a + b;
+
+            // Call the delegate with two int parameters
+            int result = addNumbers(1, 3);
+
+            // Return the result
+            return result;
+        }
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncInC#/ActionAndFuncInC#/DelegatesExamplesUnitTests/DelegateExamplesTests.cs
@@ -0,0 +1,34 @@
+using DelegatesExamples;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+
+namespace DelegatesExamplesUnitTests
+{
+    public class DelegateExamplesTests
+    {
+        [Fact]
+        public void GivenActionDelegate_WhenDisplayMessageCalled_ThenDisplayHelloWorld()
+        {
+            // Arrange
+            using StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Act
+            DelegateExamplesManager.ActionExample();
+
+            // Assert
+            string expected = "Hello, World!" + Environment.NewLine;
+            Assert.Equal(expected, sw.ToString());
+        }
+
+        [Fact]
+        public void GivenFuncDelegate_WhenMultiplyNumbersCalled_ThenReturnProduct()
+        {
+            // Act
+            int result = DelegateExamplesManager.FuncExample();
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
