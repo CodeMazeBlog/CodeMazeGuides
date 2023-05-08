d1c98977fa47fae5fd8345788aed5f4ef125728d
Muhammed Saleem <muhammedsalimp@gmail.com>
2023-05-08 20:18:33 +0530

    Modified log filepath, Added unit tests for logger methods.

---
+++ dotnet-logging/StructuredLoggingUsingLog4Net/Tests/LogTestControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using Moq;
+using StructuredLoggingUsingLog4Net.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class LogTestControllerTests
+    {
+        [Fact]
+        public void Get_WhenCalled_InvokesLog4netMethods()
+        {
+            //arrange
+            Mock<ILog> mock = new();
+            LogTestController controller = new(mock.Object);
+
+            //act
+            controller.Get();
+
+            //assert
+            mock.Verify(m => m.Info(It.IsAny<object>()), Times.AtLeastOnce);
+            mock.Verify(m => m.Error(It.IsAny<object>()), Times.AtLeastOnce);            
+        }
+    }
+}
