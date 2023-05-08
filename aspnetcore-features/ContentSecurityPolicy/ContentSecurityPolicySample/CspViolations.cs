e38a599c9c18282122416cfa294ed885f59d54fc
RPM1984 <rpmir1@gmail.com>
2023-05-09 08:35:51 +1000

    [CM-501] Content Security Policy in ASP.NET Core

---
+++ aspnetcore-features/ContentSecurityPolicy/ContentSecurityPolicySample/CspViolations.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ContentSecurityPolicySample;
+
+public class CspViolation
+{
+    [JsonPropertyName("csp-report")]
+    public CspReport CspReport { get; set; }
+}
+
+public class CspReport
+{
+    [JsonPropertyName("document-uri")]
+    public string DocumentUri { get; set; }
+
+    [JsonPropertyName("referrer")]
+    public string Referrer { get; set; }
+
+    [JsonPropertyName("violated-directive")]
+    public string ViolatedDirective { get; set; }
+
+    [JsonPropertyName("effective-directive")]
+    public string EffectiveDirective { get; set; }
+
+    [JsonPropertyName("original-policy")]
+    public string OriginalPolicy { get; set; }
+
+    [JsonPropertyName("blocked-uri")]
+    public string BlockedUri { get; set; }
+
+    [JsonPropertyName("status-code")]
+    public int StatusCode { get; set; }
+}
