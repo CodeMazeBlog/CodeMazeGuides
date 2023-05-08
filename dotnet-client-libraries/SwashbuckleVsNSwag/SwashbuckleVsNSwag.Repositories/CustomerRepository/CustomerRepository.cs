77595527dc1398635f3a3013ea8dd1ab57cbb21a
Michal Kaminski <kaminski.mi@wp.pl>
2023-05-08 23:15:54 +0200

    Added basic project structure with models and controllers

---
+++ dotnet-client-libraries/SwashbuckleVsNSwag/SwashbuckleVsNSwag.Repositories/CustomerRepository/CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using SwashbuckleVsNSwag.Models.Customers;
+
+namespace SwashbuckleVsNSwag.Repositories.CustomerRepository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly Dictionary<Guid, Customer> _repository = new Dictionary<Guid, Customer>();
+
+        public Customer GetById(Guid id)
+        {
+            return _repository[id];
+        }
+
+        public void Add(Customer customer)
+        {
+            customer.CustomerId = Guid.NewGuid();
+
+            _repository[customer.CustomerId] = customer;
+        }
+
+        public void Remove(Guid id)
+        {
+            _repository.Remove(id);
+        }
+    }
+}
