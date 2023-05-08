830bfc08b5c474b72b4c01d9cd9123d12a75a8b0
ralptashinganyoni <ralphtashinga@gmail.com>
2023-05-08 20:25:02 +0200

    Refactored FuncDelegatesUnitTest following PR review

--- csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp.Tests/FuncDelegatesUnitTest.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp.Tests/FuncDelegatesUnitTest.cs
@@ -1,32 +1,19 @@
-using ActionAndFuncDelegatesInCSharp;
 namespace ActionAndFuncDelegatesInCSharp.Tests;
-public class FuncDelegatesUnitTest : IDisposable
-{
-    private readonly StringWriter _stringWriter;
-    public FuncDelegatesUnitTest()
-    {
-        _stringWriter = new StringWriter();
-        Console.SetOut(_stringWriter);
-    }
-    public void Dispose()
-    {
-        _stringWriter.Dispose();
-    }
 
+public class FuncDelegatesUnitTest
+{
     [Fact]
-    public void When_GreetWithName_Then_ReturnGreetingWithName()
+    public void WhenGreetWithName_ThenReturnGreetingWithName()
     {
         // Arrange
         Func<string, string, string> greetWithNameTest = FuncDelegates.GreetWithName;
         string firstName = "Ralph";
         string lastName = "Nyoni";
-
         // Act
         string result = greetWithNameTest(firstName, lastName);
-
         // Assert
         string expectedResult = $"Hello: {firstName + ' ' + lastName}";
         Assert.Equal(expectedResult, result);
-    } 
+    }
 }
 
