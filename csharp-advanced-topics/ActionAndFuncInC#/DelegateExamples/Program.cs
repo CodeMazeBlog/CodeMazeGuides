5f5aee272a1e711b6e65aac8705b857ba6259d6d
Simuzeche Kakoko Kaluwa <simuzechek@gmail.com>
2023-05-08 09:18:20 +0200

    Feat : Implemented tests

--- csharp-advanced-topics/ActionAndFuncInC#/DelegateExamples/Program.cs
+++ csharp-advanced-topics/ActionAndFuncInC#/DelegateExamples/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DelegateExamples;
+using System;
+
+Console.WriteLine("Action Delegate Example:");
+DelegateExamplesManager.ActionExample();
+
+// Add a separator between the examples
+Console.WriteLine("\n---------------------------------------\n");
+
+Console.WriteLine("Func Delegate Example:");
+DelegateExamplesManager.FuncExample();
+
+// Wait for the user to press a key to exit
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
