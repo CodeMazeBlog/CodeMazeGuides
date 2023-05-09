9c1f2c4a7b3e2483901f0bc373022a4443f2f9d9
ullahahsan1983 <ullahahsan1983@gmail.com>
2023-05-08 21:36:16 -0700

    Map DateOnly and TimeOnly types to SQL

---
+++ dotnet-efcore/DateOnlyAndTimeOnlyTypes/DateOnlyAndTimeOnlyTypes.Tests/DateOnlyAndTimeOnlyTypesTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+
+namespace DateOnlyAndTimeOnlyTypes.Tests;
+
+public class DateOnlyAndTimeOnlyTypesTest
+{
+    private readonly AppDbContext _context;
+
+    public DateOnlyAndTimeOnlyTypesTest()
+    {
+        _context = new AppDbContext();
+        _context.Database.EnsureDeleted();
+        _context.Database.EnsureCreated();
+    }
+
+    [Fact]
+    public void GivenDbContext_WhenHavingProperConverters_ThenRightlyStoresDateOnlyTimeOnlyValues()
+    {
+        var employee = new Employee
+        {
+            Name = "John Doe",
+            BirthDate = new DateOnly(2023, 5, 9),
+            WorkStartTime = new TimeOnly(8, 30),
+            WorkEndTime = new TimeOnly(4, 30)
+        };
+
+        _context.Employees.Add(employee);
+        _context.SaveChanges();
+
+        var storedEmployee = _context.Employees.Find(employee.Id);
+
+        Assert.NotNull(storedEmployee);
+        Assert.Equal(employee.BirthDate, storedEmployee!.BirthDate);
+        Assert.Equal(employee.WorkStartTime, storedEmployee.WorkStartTime);
+        Assert.Equal(employee.WorkEndTime, storedEmployee.WorkEndTime);
+    }
+}
