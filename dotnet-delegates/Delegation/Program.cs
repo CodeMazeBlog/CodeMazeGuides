cb24bf2d94893bd983d5a61cdbfdf8149a4e10b2
ATPL-LAP-139\falgun.modi <falgunmodi002@gmail.com>
2023-05-09 07:23:21 -0700

    Initial Commit

---
+++ dotnet-delegates/Delegation/Program.cs
@@ -0,0 +1,37 @@
+﻿delegate double MathCalculation(float value1, float value2);
+namespace Delegation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //delegate(multicast)
+            MathCalculation mathCalculation1 = new MathCalculation(AddNums);
+            MathCalculation mathCalculation2 = new MathCalculation(DivideNums);
+            Console.WriteLine("Addition value is : " + mathCalculation1.Invoke(100, 3));//with invoke keyword/method
+            Console.WriteLine("Division value is : " + mathCalculation2(100, 3));//without invoke keyword
+            Console.ReadKey();
+
+
+            //comment above code to test Func and Action delegates
+            //Action<string> executeActionDelegate = PrintActionDelegate;
+            //executeActionDelegate("Hey ! I'm Action Delegate");
+            //Func<float, float,double> executeFuncDelegate = AddNums;
+            //Console.WriteLine("Output from Func Delegate : "+executeFuncDelegate(100, 3));
+            //Console.ReadKey();
+        }
+        public static void PrintActionDelegate(string str)
+        {
+            Console.WriteLine(str);
+        }
+        public static double AddNums(float value1, float value2)
+        {
+            return value1 + value2;
+        }
+        public static double DivideNums(float value1, float value2)
+        {
+            return (value1 / value2);
+        }
+
+    }
+}
