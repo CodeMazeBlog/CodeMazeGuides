5679ff8f7c62e1828c439ae1bc04c99d3673050c
Satya <satyap48@gmail.com>
2023-05-09 23:51:22 +0530

    CM-104: Code changes based on Editor feedback

--- csharp-basic-topics/ObjectInitializersInCSharp/ObjectInitializersInCSharp/Product.cs
+++ csharp-basic-topics/ObjectInitializersInCSharp/ObjectInitializersInCSharp/Product.cs
@@ -5,5 +5,22 @@
         public string? Name { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public void GetTotalPrice()
+        {
+            TotalPrice = Quantity * UnitPrice;
+        }
+
+        public Product() { }
+
+        public Product(string name, int quantity, decimal unitPrice)
+        {
+            Name = name;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+
+            GetTotalPrice();
+        }
     }
 }
