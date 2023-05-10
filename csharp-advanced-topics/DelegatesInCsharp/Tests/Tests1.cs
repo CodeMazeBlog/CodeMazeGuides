a6f0861cd996d4ea8c68f6c7f7cb37d1efb45fb6
ATPL-LAP-139\falgun.modi <falgunmodi002@gmail.com>
2023-05-10 06:12:45 -0700

    New Project added

---
+++ csharp-advanced-topics/DelegatesInCsharp/Tests/Tests1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests1
+    {
+        delegate double MathCalculation(float value1, float value2);
+        public static double AddNums(float value1, float value2) => value1 + value2;
+        public static double DivideNums(float value1, float value2) => value1 / value2;
+        public static void DemonstrateActionDelegate(string str) => Console.WriteLine(str);
+
+        [TestMethod]
+        public void Demonstrate_DelegateWithInvoke()
+        {
+            var delegate1 = new MathCalculation(AddNums);
+            var result = delegate1.Invoke(100, 3);
+            Assert.AreEqual(AddNums(100, 3), result);
+        }
+        [TestMethod]
+        public void Demonstrate_DelegateWithoutInvoke()
+        {
+            var delegate1 = new MathCalculation(DivideNums);
+            var result = delegate1(100, 3);
+            Assert.AreEqual(DivideNums(100, 3), result);
+        }
+        [TestMethod]
+        public void Demonstrate_ActionDelegate()
+        {
+            Action<string> executeActionDelegate = DemonstrateActionDelegate;
+            var list = executeActionDelegate.GetInvocationList();
+            Assert.AreEqual(list.Length, 1);
+        }
+        [TestMethod]
+        public void Demonstrate_FuncDelegate()
+        {
+            Func<float, float, double> executeFuncDelegate = AddNums;
+            var list = executeFuncDelegate.GetInvocationList();
+            Assert.AreEqual(list.Length, 1);
+        }
+    }
+}
