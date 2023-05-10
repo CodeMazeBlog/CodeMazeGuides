a03614c5bd6cab5b9f520118172c336990311ae4
Stefan Jovanchevski <jovanchevskis.dev@gmail.com>
2023-05-10 22:05:29 +0200

    PR feedback updates
    - Return IEnumerable<User> on the first endpoint for users
    - Rename methods for returning users

--- aspnetcore-features/ReadArraysFromAppSettings/ReadArraysFromAppSettings/Program.cs
+++ aspnetcore-features/ReadArraysFromAppSettings/ReadArraysFromAppSettings/Program.cs
@@ -4,8 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet(Endpoints.GetUsersEndpointName, Endpoints.GetArrayFromAppSettings);
-app.MapGet(Endpoints.GetUsersV2EndpointName, Endpoints.GetArrayFromAppSettingsV2);
+app.MapGet(Endpoints.GetUsersEndpointName, Endpoints.GetUsersArrayFromAppSettings);
+app.MapGet(Endpoints.GetUsersV2EndpointName, Endpoints.GetUsersArrayFromAppSettingsV2);
 app.MapGet(Endpoints.GetGroupMembersEndpointName, Endpoints.GetGroupMembers);
 
 app.Run();
