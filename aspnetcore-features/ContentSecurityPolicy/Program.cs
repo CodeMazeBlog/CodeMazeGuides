5c029ec4cbc6deffd2ea9cf49bde06ca7c449938
RPM1984 <rpmir1@gmail.com>
2023-05-11 10:55:10 +1000

    Code review updates

--- aspnetcore-features/ContentSecurityPolicy/Program.cs
+++ aspnetcore-features/ContentSecurityPolicy/Program.cs
@@ -16,7 +16,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
