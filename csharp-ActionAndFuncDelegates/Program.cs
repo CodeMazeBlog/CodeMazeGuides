70a74a9c93a7a73396d5ad2cb121158afd185f09
Alejandro Aguilera <47993726+aaguilera88@users.noreply.github.com>
2023-05-13 12:22:36 -0500

    csharp-ActionAndFuncDelegates

---
+++ csharp-ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,6 @@
+﻿Action<string> printToConsole = (string s) => Console.WriteLine(s);
+printToConsole("Hello, world");
+Func<int, int, int> addNumbers = (int a, int b) => a + b; //Func Delegate definition
+int sum = addNumbers(5, 10); //Using the delegate and saving its return value in a variable
+Console.WriteLine($"The sum is {sum}");
+
