e6ee26f4ec1461961897b72b0c0483bb78432571
NarmadhaDodda <narmadha.dodda@integra.uk.net>
2023-05-13 12:23:58 +0100

    BDD convention added for tests

--- csharp-basic-topics/CorrectlyInitializeStringInCSharp/CorrectlyInitializeStringInCsharpTest/StringUnitTest.cs
+++ csharp-basic-topics/CorrectlyInitializeStringInCSharp/CorrectlyInitializeStringInCsharpTest/StringUnitTest.cs
@@ -6,97 +6,153 @@ namespace CorrectlyInitializeStringInCsharpTest
     [TestClass]
     public class StringUnitTest
     {
-
         [TestMethod]
-        public void TestMyString1()
+        public void GivenMyString1_WhenExecuted_ThenReturnsHelloWorld()
         {
+            // Arrange
             string expected = "Hello, World!";
+
+            // Act
             string actual = StringExamples.MyString1();
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestMyString2()
+        public void GivenMyString2_WhenExecuted_ThenReturnsHelloNewlineWorld()
         {
+            // Arrange
             string expected = "Hello,\nWorld!";
+
+            // Act
             string actual = StringExamples.MyString2();
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestFullName()
+        public void GivenFirstNameAndLastName_WhenFullNameCalled_ThenReturnsFullName()
         {
+            // Arrange
+            string firstName = "John";
+            string lastName = "Doe";
             string expected = "John Doe";
-            string actual = StringExamples.FullName("John", "Doe");
+
+            // Act
+            string actual = StringExamples.FullName(firstName, lastName);
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestPath()
+        public void GivenEmptyPath_WhenPathCalled_ThenReturnsPath()
         {
+            // Arrange
             string expected = @"C:\Users\Codemaze\Documents";
+
+            // Act
             string actual = StringExamples.Path();
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestMultiLineString()
+        public void GivenMultiLineString_WhenExecuted_ThenReturnsStringWithNewlines()
         {
+            // Arrange            
             string expected = @"This is
             a multi-line
             string.";
+            // Act
             string actual = StringExamples.MultiLineString();
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestEmptyString()
+        public void GivenEmptyString_WhenExecuted_ThenReturnsEmptyString()
         {
+            // Arrange
             string expected = string.Empty;
+
+            // Act
             string actual = StringExamples.EmptyString();
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestNullString()
+        public void GivenNullString_WhenExecuted_ThenReturnsNull()
         {
-            string? expected = null;
+            // Arrange
+
+            // Act
             string? actual = StringExamples.NullString();
-            Assert.AreEqual(expected, actual);
+
+            // Assert
+            Assert.IsNull(actual);
         }
 
         [TestMethod]
-        public void TestDefaultString()
+        public void GivenDefaultString_WhenExecuted_ThenReturnsNull()
         {
-            string? expected = default(string);
+            // Arrange
+
+            // Act
             string? actual = StringExamples.DefaultString();
-            Assert.AreEqual(expected, actual);
+
+            // Assert
+            Assert.IsNull(actual);
         }
 
         [TestMethod]
-        public void TestStringBuilderString()
+        public void GivenStringBuilderString_WhenExecuted_ThenReturnsHelloWorld()
         {
+            // Arrange
             string expected = "Hello World";
+
+            // Act
             string actual = StringExamples.StringBuilderString();
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestSumString()
+        public void GivenTwoNumbers_WhenSumStringCalled_ThenReturnsStringWithSum()
         {
+            // Arrange
+            int a = 5;
+            int b = 10;
             string expected = "The sum of 5 and 10 is 15";
-            string actual = StringExamples.SumString(5, 10);
+
+            // Act
+            string actual = StringExamples.SumString(a, b);
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestAsteriskString()
+        public void GivenAsteriskCount_WhenAsteriskStringCalled_ThenReturnsStringWithAsterisks()
         {
+            // Arrange
+            int count = 10;
             string expected = "**********";
-            string actual = StringExamples.AsteriskString(10);
-            Assert.AreEqual(expected, actual);
 
+            // Act
+            string actual = StringExamples.AsteriskString(count);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
+
 
     }
 }
