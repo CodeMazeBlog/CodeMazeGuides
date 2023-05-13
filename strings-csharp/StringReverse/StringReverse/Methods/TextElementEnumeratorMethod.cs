8a0df7a9788d3ef8f80572c1b15cc43236c06853
Konstantinos Polychronopoulos <28976830+KonstantinosP7@users.noreply.github.com>
2023-05-13 19:26:19 +0300

    CM-402: How to Reverse a String in C# (#1058)
    * CM-402: How to Reverse a String in C#
    
    * CM-402: ReverseStrinInCSharp reviews fix
    
    * CM-402: Code formatting changes as per last review.
    
    * CM-402: Code formatting minor fix in benchmark

---
+++ strings-csharp/StringReverse/StringReverse/Methods/TextElementEnumeratorMethod.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace StringReverse
+{
+    public partial class Methods
+    {
+        public static string? TextElementEnumeratorMethod(string stringToReverse)
+        {
+            return string.Create(stringToReverse.Length, stringToReverse, (chars, val) =>
+            {
+                var en = StringInfo.GetTextElementEnumerator(val);
+                en.MoveNext();
+                var start = en.ElementIndex;
+                var pos = chars.Length;
+                while (en.MoveNext())
+                {
+                    var next = en.ElementIndex;
+                    var len = next - start;
+                    val[start..next].CopyTo(chars[(pos - len)..pos]);
+                    pos -= len;
+                    start = next;
+                }
+
+                if (start != 0)
+                    val[start..].CopyTo(chars[0..pos]);
+            });
+        }
+    }
+}
