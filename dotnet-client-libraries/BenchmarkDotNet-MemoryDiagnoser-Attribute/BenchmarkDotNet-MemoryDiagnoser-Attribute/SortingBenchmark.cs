7747c819b7a280ac18f3b10867c32be0c5d5a60d
Ivan Gechev <ivan_banov@abv.bg>
2023-05-23 18:08:32 +0300

    CM-654: BenchmarkDotNet-MemoryDiagnoser (#1115)
    * Core of the article code
    
    * Delete csharp-advanced-topics/ActionAndFunc/ActionAndFunc directory
    
    * InitialCommit
    
    * Sorting class and benchmark
    
    * Removed Bogo sort
    
    * commented bogo sort
    
    * SortTests
    
    * adding _

---
+++ dotnet-client-libraries/BenchmarkDotNet-MemoryDiagnoser-Attribute/BenchmarkDotNet-MemoryDiagnoser-Attribute/SortingBenchmark.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNet_MemoryDiagnoser_Attribute
+{
+    [MemoryDiagnoser(true)]
+    public class SortingBenchmark
+    {
+        private const int ARRAY_SIZE = 10_000;
+        private int[] _quickSortArray;
+        private int[] _mergeSortArray;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Random random = new Random();
+            _quickSortArray = new int[ARRAY_SIZE];
+            _mergeSortArray = new int[ARRAY_SIZE];
+
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                int randomNumber = random.Next();
+                _quickSortArray[i] = randomNumber;
+                _mergeSortArray[i] = randomNumber;
+            }
+        }
+
+        [Benchmark]
+        public void QuickSort()
+        {
+            Sort.QuickSort(_quickSortArray, 0, _quickSortArray.Length - 1);
+        }
+
+        [Benchmark]
+        public void MergeSort()
+        {
+            Sort.MergeSort(_mergeSortArray, 0, _mergeSortArray.Length - 1);
+        }
+    }
+}
