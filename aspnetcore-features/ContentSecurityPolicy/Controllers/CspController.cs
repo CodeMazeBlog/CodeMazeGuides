80e8958e4ef2386b223a7c394cbec7e2f06b983e
Ryan Miranda <rpmir1@gmail.com>
2023-05-23 19:36:13 +1000

    [CM-501] Content Security Policy in ASP.NET Core (#1089)
    * [CM-501] Content Security Policy in ASP.NET Core
    
    * Fix structure
    
    * Code review updates
    
    * Code review updates
    
    * CM-501 Content Security Policy in ASP.NET Core
    
    ---------
    
    Co-authored-by: Code Maze <contact@code-maze.com>

---
+++ aspnetcore-features/ContentSecurityPolicy/Controllers/CspController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContentSecurityPolicySample.Controllers;
+
+public class CspController : Controller
+{
+    private readonly ILogger<CspController> _logger;
+
+    public CspController(ILogger<CspController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("csp-violations")]
+    public IActionResult CSPReport([FromBody] CspViolation cspViolation)
+    {
+        _logger.LogWarning($"URI: {cspViolation.CspReport.DocumentUri}, Blocked: {cspViolation.CspReport.BlockedUri}");
+
+        return Ok();
+    }
+}
