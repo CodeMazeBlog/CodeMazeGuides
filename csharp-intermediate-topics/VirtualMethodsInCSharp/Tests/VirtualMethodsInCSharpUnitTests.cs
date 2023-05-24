6bf64192506dc60ace1dab12f5bfbf412593ee10
bspoljaric <bozo.spoljaric@gmail.com>
2023-05-24 14:26:11 +0200

    changing example classes

--- csharp-intermediate-topics/VirtualMethodsInCSharp/Tests/VirtualMethodsInCSharpUnitTests.cs
+++ csharp-intermediate-topics/VirtualMethodsInCSharp/Tests/VirtualMethodsInCSharpUnitTests.cs
@@ -1,4 +1,5 @@
 using VirtualMethodsInCSharp;
+using Moq;
 
 namespace Tests
 {
@@ -6,57 +7,94 @@ namespace Tests
     public class VirtualMethodsInCSharpUnitTests
     {
         [TestMethod]
-        public void WhenBaseClassVirtualMethodA_ThenBaseClassVirtualMethodAResult()
+        public void WhenShapeClassCalculateArea_ThenResultZero()
         {
-            var baseClass = new BaseClass();
-            var baseClassVirtualMethodA = baseClass.VirtualMethodA();
+            var shape = new Shape();
+            var shapeArea = shape.CalculateArea();
 
-            Assert.AreEqual("This is a virtual method", baseClassVirtualMethodA);
+            Assert.AreEqual(0, shapeArea);
         }
 
         [TestMethod]
-        public void WhenBaseClassVirtualMethodB_ThenBaseClassVirtualMethodBResult()
+        public void WhenRectangleClassCalculateArea_ThenRectangleClassCalculateAreaResult()
         {
-            var baseClass = new BaseClass();
-            var baseClassVirtualMethodB = baseClass.VirtualMethodB();
+            var rectangle = new Rectangle() { Height = 2, Width = 3 };
+            var rectangleArea = rectangle.CalculateArea();
 
-            Assert.AreEqual("This is another virtual method", baseClassVirtualMethodB);
+            Assert.AreEqual(6, rectangleArea);
         }
 
         [TestMethod]
-        public void WhenBaseClassNonVirtualMethod_ThenBaseClassNonVirtualMethodResult()
+        public void WhenCircleClassCalculateArea_ThenCircleClassCalculateAreaResult()
         {
-            var baseClass = new BaseClass();
-            var baseClassNonVirtualMethod = baseClass.NonVirtualMethod();
+            var circle = new Circle() { Radius = 2 };
+            var circleArea = circle.CalculateArea();
 
-            Assert.AreEqual("This is non-virtual method", baseClassNonVirtualMethod);
+            Assert.AreEqual(12.566370614359172, circleArea);
         }
 
         [TestMethod]
-        public void WhenDerivedClassVirtualMethodA_ThenDerivedClassVirtualMethodAResult()
+        public void WhenShapeClassGetShapeType_ThenShapeClassGetShapeTypeResult()
         {
-            var derivedClass = new DerivedClass();
-            var derivedClassVirtualMethodA = derivedClass.VirtualMethodA();
+            var shape = new Shape();
+            var shapeType = shape.GetShapeType();
 
-            Assert.AreEqual("This method overrides a virtual method", derivedClassVirtualMethodA);
+            Assert.AreEqual("This is a generic shape", shapeType);
         }
 
         [TestMethod]
-        public void WhenDerivedClassVirtualMethodB_ThenDerivedClassVirtualMethodBResult()
+        public void WhenRectangleClassGetShapeType_ThenRectangleClassGetShapeTypeResult()
         {
-            var derivedClass = new DerivedClass();
-            var derivedClassVirtualMethodB = derivedClass.VirtualMethodB();
+            var rectangle = new Rectangle() { Height = 2, Width = 3 };
+            var rectangleType = rectangle.GetShapeType();
 
-            Assert.AreEqual("This is another virtual method", derivedClassVirtualMethodB);
+            Assert.AreEqual("This is a rectangle", rectangleType);
         }
 
         [TestMethod]
-        public void WhenDerivedClassNonVirtualMethod_ThenDerivedClassNonVirtualMethodResult()
+        public void WhenCircleClassGetShapeType_ThenShapeClassGetShapeTypeResult()
         {
-            var derivedClass = new DerivedClass();
-            var derivedClassNonVirtualMethod = derivedClass.NonVirtualMethod();
+            var circle = new Circle() { Radius = 2 };
+            var circleType = circle.GetShapeType();
 
-            Assert.AreEqual("This method hides the inherited non-virtual method", derivedClassNonVirtualMethod);
+            Assert.AreEqual("This is a generic shape", circleType);
+        }
+
+        [TestMethod]
+        public void WhenShapeClassDraw_ThenDrawingGenericShape()
+        {
+            var shape = new Shape();
+            var shapeArea = shape.Draw();
+
+            Assert.AreEqual("Drawing a generic shape", shapeArea);
+        }
+
+        [TestMethod]
+        public void WhenRectangleClassDraw_ThenDrawingRectangle()
+        {
+            var rectangle = new Rectangle() { Height = 2, Width = 3 };
+            var rectangleArea = rectangle.Draw();
+
+            Assert.AreEqual("Drawing a rectangle", rectangleArea);
+        }
+
+        [TestMethod]
+        public void WhenCircleClassDraw_ThenDrawingCircle()
+        {
+            var circle = new Circle() { Radius = 2 };
+            var circleArea = circle.Draw();
+
+            Assert.AreEqual("Drawing a circle", circleArea);
+        }
+
+        [TestMethod]
+        public void WhenCircleClassGetShapeTypeMock_ThenCircleClassGetShapeTypeResult()
+        {
+            var circleMock = new Mock<Circle>();
+            circleMock.Setup(p => p.GetShapeType()).Returns("This is a circle");
+            var circleType = circleMock.Object.GetShapeType();
+
+            Assert.AreEqual("This is a circle", circleType);
         }
     }
 }
--- csharp-intermediate-topics/VirtualMethodsInCSharp/VirtualMethodsInCSharp/Program.cs
+++ csharp-intermediate-topics/VirtualMethodsInCSharp/VirtualMethodsInCSharp/Program.cs
@@ -1,23 +1,18 @@
 ﻿using VirtualMethodsInCSharp;
 
-BaseClass baseClass = new();
-BaseClass derivedClassAsBaseClass = new DerivedClass();
+var shape = new Shape();
+var circle = new Circle() { Radius = 2 };
+var rectangle = new Rectangle() { Height = 2, Width = 3 };
 
-var baseClassVirtualMethodA = baseClass.VirtualMethodA();
-Console.WriteLine(baseClassVirtualMethodA);
-var derivedClassAsBaseClassVirtual = derivedClassAsBaseClass.VirtualMethodA();
-Console.WriteLine(derivedClassAsBaseClassVirtual);
+Console.WriteLine($"Shape area: {shape.CalculateArea()}"); // Shape area: 0
+Console.WriteLine($"Circle area: {circle.CalculateArea()}"); // Circle area: 12,566370614359172
+Console.WriteLine($"Rectangle area: {rectangle.CalculateArea()}"); // Rectangle area: 6
 
-var baseClassVirtualMethodB = baseClass.VirtualMethodB();
-Console.WriteLine(baseClassVirtualMethodB);
-var derivedClassAsBaseClassNotOverridingVirtualMethod = derivedClassAsBaseClass.VirtualMethodB();
-Console.WriteLine(derivedClassAsBaseClassNotOverridingVirtualMethod);
+Console.WriteLine($"Shape type: {shape.GetShapeType()}"); // Shape type: This is a generic shape
+Console.WriteLine($"Circle type: {circle.GetShapeType()}"); // Circle type: This is a generic shape
+Console.WriteLine($"Rectangle type: {rectangle.GetShapeType()}"); // Rectangle type: This is a rectangle
 
-var baseClassNonVirtualMethod = baseClass.NonVirtualMethod();
-Console.WriteLine(baseClassNonVirtualMethod);
-var derivedClassAsBaseClassNonVirtualMethodWithNewKeyword = derivedClassAsBaseClass.NonVirtualMethod();
-Console.WriteLine(derivedClassAsBaseClassNonVirtualMethodWithNewKeyword);
-
-DerivedClass derivedClass = new();
-var derivedClassNonVirtualMethod = derivedClass.NonVirtualMethod();
-Console.WriteLine(derivedClassNonVirtualMethod);
+Shape circleAsShape = new Circle() { Radius = 2 };
+Console.WriteLine($"Shape draw: {shape.Draw()}"); // Shape draw: Drawing a generic shape
+Console.WriteLine($"Circle draw: {circleAsShape.Draw()}"); // Circle draw: Drawing a generic shape
+Console.WriteLine($"Rectangle draw: {rectangle.Draw()}"); // Rectangle draw: Drawing a rectangle
