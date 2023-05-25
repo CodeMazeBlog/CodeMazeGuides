bcf5776e30fa721902b778be27a026bd69a21035
Ivan Gechev <ivan_banov@abv.bg>
2023-05-25 17:34:13 +0300

    Tests and packages

---
+++ dotnet-client-libraries/WireMock.NET/WireMock.NET.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WireMock.NET.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ dotnet-client-libraries/WireMock.NET/WireMock.NET/Planet.cs
@@ -0,0 +1,10 @@
+﻿namespace WireMock.NET
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Moons { get; set; }
+        public double Radius { get; set; }
+    }
+}
