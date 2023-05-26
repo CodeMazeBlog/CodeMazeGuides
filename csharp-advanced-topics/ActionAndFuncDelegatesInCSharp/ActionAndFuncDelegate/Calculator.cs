c7731cc34ea14810082657f5201a704fede97179
Tobby Umoh <umohjunior96@gmail.com>
2023-05-26 09:01:08 +0100

    updated indentation
    added indentation with 4 spaces max

--- csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegate/Calculator.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegate/Calculator.cs
@@ -1,10 +1,10 @@
-﻿namespace ActionAndFuncDelegate
+namespace ActionAndFuncDelegate 
 {
-	public class Calculator
-	{
-		public int Add(int a, int b) => a + b;
-		public int Subtract(int a, int b) => a - b;
-		public int Multiply(int a, int b) => a * b;
-		public int Divide(int a, int b) => a / b;
-	}
+    public class Calculator 
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public int Divide(int a, int b) => a / b;
+    }
 }
