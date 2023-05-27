f5d33fb4aeb78e71645ba8ca0c5b0086512a1060
Ivan Gechev <ivan_banov@abv.bg>
2023-05-27 21:51:51 +0300

    Final version of the code for now

--- dotnet-client-libraries/WireMock.NET/WireMock.NET.Tests/WireMockNETTests.cs
+++ dotnet-client-libraries/WireMock.NET/WireMock.NET.Tests/WireMockNETTests.cs
@@ -1,66 +1,73 @@
 using FluentAssertions;
 using System.Net;
-using System.Text.Json;
-using WireMock.NET.Tests.Contracts;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using WireMock.Server;
+using WireMockNet;
 
 namespace WireMock.NET.Tests
 {
-    public class WireMockNETTests :IDisposable
+    public class WireMockNETTests : IDisposable
     {
         private readonly HttpClient _httpClient;
-        private readonly WireMockServer _server;
+        private readonly WireMockServer _mockServer;
+        private readonly PlanetsService _planetService;
 
         public WireMockNETTests()
         {
-            _server = WireMockServer.Start(new Settings.WireMockServerSettings
+            _mockServer = WireMockServer.Start(new Settings.WireMockServerSettings
             {
                 Urls = new[] { "https://localhost:8888" }
             });
 
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("https://localhost:8888");
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:8888")
+            };
+
+            _planetService = new PlanetsService(_httpClient);
         }
 
         [Fact]
-        public async Task GivenThatPlanetExists_WhenGetPlanetIsInvoked_ThenOKAndValidPlanetIsReturned()
+        public async Task GivenThatPlanetExists_WhenGetPlanetByIdIsInvoked_ThenOKAndValidPlanetIsReturned()
         {
             // Arrange
-            var planet = new Planet("Mars", 6779, 2, true);
-            _server.Given(Request.Create().WithPath("/planets/3").UsingGet())
-                .RespondWith(Response.Create().WithBodyAsJson(planet).WithStatusCode(HttpStatusCode.OK));
+            var planet = new Planet(4, "Mars", 6779, 2, true);
+
+            _mockServer
+                .Given(Request.Create()
+                    .WithPath("/planets/4").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithBodyAsJson(planet)
+                    .WithStatusCode(HttpStatusCode.OK));
 
             // Act
-            var response = await _httpClient.GetAsync(_httpClient.BaseAddress + "planets/3");
-            var result = JsonSerializer.Deserialize<Planet>(await response.Content.ReadAsStringAsync());
+            var result = await _planetService.GetPlanetByIdAsync(planet.Id);
 
             // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
             result.Should().NotBeNull();
             result.Name.Should().Be(planet.Name);
             result.Diameter.Should().Be(planet.Diameter);
             result.NumberOfMoons.Should().Be(planet.NumberOfMoons);
-            result.HasAtmosphere.Should().Be(planet.HasAtmosphere);            
+            result.HasAtmosphere.Should().Be(planet.HasAtmosphere);
         }
 
         [Fact]
         public async Task GivenThatPlanetDoesntExists_WhenGetPlanetIsInvoked_ThenNotFoundIsReturned()
         {
-            _server.Given(Request.Create().WithPath("/planets/9").UsingGet())
+            _mockServer.Given(Request.Create().WithPath("/planets/9").UsingGet())
                 .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.NotFound));
 
             // Act
-            var response = await _httpClient.GetAsync(_httpClient.BaseAddress + "planets/9");
+            var result = await _planetService.GetPlanetByIdAsync(9);
 
             // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            result.Should().BeNull();
         }
 
         public void Dispose()
         {
-            _server.Stop();
+            _mockServer.Stop();
             _httpClient.Dispose();
         }
     }
---
+++ dotnet-client-libraries/WireMock.NET/WireMock.NET/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WireMock.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
