aadab8d486f153967c68a3b497ba6944a0b406e9
Eder Llamuca <eder.llamuca@appspace.com>
2023-05-28 21:08:37 +0200

    add main and test projects.

---
+++ aspnetcore-webapi/PublishVsSendInMediatR/PublishVsSendInMediatR/CommandHandlers/CreateUserCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using PublishVsSendInMediatR.Commands;
+using PublishVsSendInMediatR.Models;
+using PublishVsSendInMediatR.Repositories;
+
+namespace PublishVsSendInMediatR.CommandHandlers
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public CreateUserCommandHandler(IUserRepository userRepository) =>
+            _userRepository = userRepository;
+
+        public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User { UserName = request.UserName, Email = request.Email };
+
+            return await _userRepository.CreateUserAsync(user);
+        }
+    }
+}
---
+++ aspnetcore-webapi/PublishVsSendInMediatR/PublishVsSendInMediatR/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PublishVsSendInMediatR.Commands;
+using PublishVsSendInMediatR.Events;
+
+namespace PublishVsSendInMediatR.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult<int>> CreateUser(CreateUserCommand command)
+        {
+            int userId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(CreateUser), userId);
+        }
+
+        [HttpPost("notify")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> NotifyUser(UserCreatedEvent notification)
+        {
+            await _mediator.Publish(notification);
+
+            return Ok();
+        }
+    }
+}
