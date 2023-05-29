c6f5dc54a451d9c697664ec431fb564066017f1a
Yogichandar <boppanachandu1@gmail.com>
2023-05-29 11:27:54 -0400

    Added Action and Func Delegate Example

---
+++ csharp-basic-topics/ActionAndFunc/ActionAndFunc/Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegatesInCSharp
+{
+    public static class Example
+    {
+        public static void PrintNumbersWithoutAction(List<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public static void PrintNumbersWithAction(List<int> numbers, Action<int> action)
+        {
+            numbers.ForEach(action);
+        }
+    }
+}
---
+++ csharp-basic-topics/ActionAndFunc/ActionAndFuncTests/UnitTest.cs
@@ -0,0 +1,49 @@
+﻿
+using ActionAndFuncDelegatesInCSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ActionAndFuncDelegateTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestClass]
+        public class ExampleTests
+        {
+            [TestMethod]
+            public void PrintNumbersWithoutAction_PrintsNumbersCorrectly()
+            {
+                // Arrange
+                List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+                StringWriter stringWriter = new StringWriter();
+                Console.SetOut(stringWriter);
+                string expectedOutput = "1\r\n2\r\n3\r\n4\r\n5\r\n";
+
+                // Act
+                Example.PrintNumbersWithoutAction(numbers);
+                string actualOutput = stringWriter.ToString();
+
+                // Assert
+                Assert.AreEqual(expectedOutput, actualOutput);
+            }
+            [TestMethod]
+            public void PrintNumbersWithAction_PrintsNumbersCorrectly()
+            {
+                // Arrange
+                List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+                StringWriter stringWriter = new StringWriter();
+                Console.SetOut(stringWriter);
+                string expectedOutput = "1\r\n2\r\n3\r\n4\r\n5\r\n";
+                Action<int> printNumber = Console.WriteLine;
+
+                // Act
+                Example.PrintNumbersWithAction(numbers, printNumber);
+                string actualOutput = stringWriter.ToString();
+
+                // Assert
+                Assert.AreEqual(expectedOutput, actualOutput);
+            }
+
+        }
+    }
+}
