c5dd7d34a6a96f09618de74c1824a6bf97532bf1
ullahahsan1983 <ullahahsan1983@gmail.com>
2023-05-29 14:16:01 -0700

    Use of Factory Pattern with DI

---
+++ dotnet-dependency-injection/FactoryPatternInDependencyInjection/FactoryPatternInDependencyInjection/LabelGen/LabelGenOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace FactoryPatternInDependencyInjection.LabelGen;
+
+public class LabelGenOptions
+{
+    public string Prefix { get; set; } = null!;
+
+    public string Suffix { get; set; } = null!;
+}
