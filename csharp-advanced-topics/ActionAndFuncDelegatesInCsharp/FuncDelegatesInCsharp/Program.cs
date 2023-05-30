01d403d675311b4a3ffd452facdc51a31433cfbe
Koddek <codedek@gmail.com>
2023-05-30 17:09:51 -0400

    initial commit for Func delegate

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/FuncDelegatesInCsharp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace FuncDelegatesInCsharp;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Func delegate with lambda expression
+        Func<int, int, int> add = (a, b) => a + b;
+
+        // Invoke the delegate
+        int result = add(2, 3); // result = 5
+
+        //=== Passing Methods as arguments ===//
+
+        // Pass a lambda expression to PrintSum
+        PrintSum((_, _) => Add(2, 3));
+
+        // Pass a lambda expression that calls Add to PrintSum
+        PrintSum((a, b) => Add(a, b));
+
+        // Pass the Add method to PrintSum
+        PrintSum(2, 3, Add);
+
+        Console.Read();
+    }
+
+    // Print the return value of a func delegate
+    public static void PrintSum(Func<int, int, int> add)
+    {
+        Console.WriteLine(add(2, 3));
+    }
+
+    // Print the return value of a func delegate with two ints
+    public static void PrintSum(int x, int y, Func<int, int, int> add)
+    {
+        Console.WriteLine(add(x, y));
+    }
+
+    // Return the sum of two ints
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+
---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/FuncDelegatesInCsharpTests/ProgramUnitTests.cs
@@ -0,0 +1,65 @@
+﻿namespace FuncDelegatesInCsharpTests;
+
+public class ProgramUnitTests
+{
+    private string CaptureConsoleOutput(Action action)
+    {
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        action();
+        return writer.ToString().Trim();
+    }
+
+    [Fact]
+    public void WhenInvokingAdd_ThenReturnSum()
+    {
+        // Arrange
+        Func<int, int, int> add = (a, b) => a + b;
+
+        // Act
+        int result = add(2, 3);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void WhenPrintingSum_ThenPrintReturnValueOfGivenFunc()
+    {
+        // Arrange
+        var consoleOutput = "5";
+        Func<int, int, int> add = (a, b) => a + b;
+
+        // Act
+        var output = CaptureConsoleOutput(() => FuncDelegatesInCsharp.Program.PrintSum(add));
+
+        // Assert
+        Assert.Equal(consoleOutput, output);
+    }
+
+    [Fact]
+    public void WhenPrintingSumWithTwoInts_ThenPrintReturnValueOfGivenFuncWithTwoInts()
+    {
+        // Arrange
+        var consoleOutput = "5";
+        Func<int, int, int> add = (a, b) => a + b;
+
+        // Act
+        var output = CaptureConsoleOutput(() => FuncDelegatesInCsharp.Program.PrintSum(2, 3, add));
+
+        // Assert
+        Assert.Equal(consoleOutput, output);
+    }
+
+    [Fact]
+    public void WhenAddingTwoInts_ThenReturnSum()
+    {
+        // Arrange
+
+        // Act
+        int result = FuncDelegatesInCsharp.Program.Add(2, 3);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
