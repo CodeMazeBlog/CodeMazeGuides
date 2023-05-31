9ae2f2d2af2eae904ac220244403dcd59eedfcef
Otimkev <kevinotim4@gmail.com>
2023-05-31 11:28:09 +0300

    Update Program.cs
    clean-file

--- dotnet-logging/RollingFileLoggingWithSerilog/RollingFileLoggingWithSerilog/Program.cs
+++ dotnet-logging/RollingFileLoggingWithSerilog/RollingFileLoggingWithSerilog/Program.cs
@@ -11,16 +11,13 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
     builder.Services.AddRazorPages();
 
     var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
     if (!app.Environment.IsDevelopment())
     {
         app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
     }
 
