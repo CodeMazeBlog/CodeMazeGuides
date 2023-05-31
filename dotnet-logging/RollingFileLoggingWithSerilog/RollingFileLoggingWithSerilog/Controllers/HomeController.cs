4522b0f8dcd3c7f25a00b8e4137a702d143fa787
Otimkev <kevinotim4@gmail.com>
2023-05-31 15:37:05 +0300

    Initial setup

---
+++ dotnet-logging/RollingFileLoggingWithSerilog/RollingFileLoggingWithSerilog/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RollingFileLoggingWithSerilog.Models;
+using System.Diagnostics;
+
+namespace RollingFileLoggingWithSerilog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string message)
+        {
+            _logger.LogInformation("Recevied message: {message}",message);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
