c84975e3b6bec6b569a92091386d637c2c40da44
Ivan Gechev <ivan_banov@abv.bg>
2023-06-02 18:18:27 +0300

    Initial commit

---
+++ dotnet-client-libraries/TestDataGenerationWithAutoFixture/TestDataGenerationWithAutoFixture/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestDataGenerationWithAutoFixture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
