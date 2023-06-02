b959c3412241d79548ac41cc5547b8a2efec3a8e
Ivan Gechev <ivan_banov@abv.bg>
2023-06-02 18:31:15 +0300

    Employee and Departmen

---
+++ dotnet-client-libraries/TestDataGenerationWithAutoFixture/TestDataGenerationWithAutoFixture/Department.cs
@@ -0,0 +1,37 @@
+﻿namespace TestDataGenerationWithAutoFixture
+{
+    public class Department
+    {
+        public string Name { get; }
+        public List<Employee> Employees { get; }
+
+        public Department(string name)
+        {
+            Name = name;
+            Employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+
+        public Employee? GetEmployee(string firstName)
+        {
+            return Employees
+                .FirstOrDefault(e => e.FirstName == firstName);
+        }
+
+        public decimal CalculateAverageSalary()
+        {
+            if (!Employees.Any())
+            {
+                return 0;
+            }
+
+            decimal sumOfSalaries = Employees.Sum(e => e.Salary);
+
+            return sumOfSalaries / Employees.Count;
+        }
+    }
+}
---
+++ dotnet-client-libraries/TestDataGenerationWithAutoFixture/TestDataGenerationWithAutoFixture/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace TestDataGenerationWithAutoFixture
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(
+            string firstName,
+            string lastName,
+            int age,
+            decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string GetFullName() => $"{FirstName} {LastName}";
+    }
+}
