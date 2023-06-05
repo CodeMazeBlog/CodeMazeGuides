0b543bd4cec24e6eac5424455587eeaa0ee4f7a2
Otimkev <kevinotim4@gmail.com>
2023-06-05 12:38:05 +0300

    Update HomeController.cs
    Remove Privacy view.

--- dotnet-logging/RollingFileLoggingWithSerilog/RollingFileLoggingWithSerilog/Controllers/HomeController.cs
+++ dotnet-logging/RollingFileLoggingWithSerilog/RollingFileLoggingWithSerilog/Controllers/HomeController.cs
@@ -18,16 +18,5 @@ namespace RollingFileLoggingWithSerilog.Controllers
             _logger.LogInformation("TESTING MESSAGE 123..");
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
