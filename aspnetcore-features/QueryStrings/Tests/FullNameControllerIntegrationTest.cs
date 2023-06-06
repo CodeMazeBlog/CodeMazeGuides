ebf85dba601a701b3faf54b39f723aaf61bd6a8c
Code Maze <contact@code-maze.com>
2023-06-06 09:31:14 +0200

    CM-287: How to Read Values From the Query String With ASP.NET Core

--- aspnetcore-features/QueryStrings/Tests/FullNameControllerIntegrationTest.cs
+++ aspnetcore-features/QueryStrings/Tests/FullNameControllerIntegrationTest.cs
@@ -10,6 +10,7 @@ public class FullNameControllerIntegrationTest
         await using var app = new Api();
         using var client = app.CreateClient();
         using var response = await client.GetAsync("/v1?firstName=John&lastName=Doe");
+        
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.Equal("{\"fullName\":\"John Doe\"}", await response.Content.ReadAsStringAsync());
     }
@@ -20,6 +21,7 @@ public class FullNameControllerIntegrationTest
         await using var app = new Api();
         using var client = app.CreateClient();
         using var response = await client.GetAsync("/v2?ids=1&ids=2&ids=3");
+        
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.Equal("{\"productIds\":[1,2,3]}", await response.Content.ReadAsStringAsync());
     }
@@ -30,6 +32,7 @@ public class FullNameControllerIntegrationTest
         await using var app = new Api();
         using var client = app.CreateClient();
         using var response = await client.GetAsync("/v3?FirstName=John&LastName=Doe");
+        
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.Equal("{\"fullName\":\"John Doe\"}", await response.Content.ReadAsStringAsync());
     }
