9e98bf218c7f30fcea76c8de733190230d3c8184
Aneta Muslić <Aneta.Muslic@blu.com>
2023-06-12 12:29:19 +0200

    pr changes

--- dotnet-projects/StyleCop/StyleCop/Controllers/WeatherForecastController.cs
+++ dotnet-projects/StyleCop/StyleCop/Controllers/WeatherForecastController.cs
@@ -8,9 +8,9 @@ namespace StyleCop.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+         "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
 
         private readonly ILogger<WeatherForecastController> _logger;
 
