d0d18548699249881a373411060928d33a15bb59
gergo <gergo.vandor.dev@outlook.com>
2023-06-14 10:25:52 +0200

    added ITicketPriceProvider.cs impl

--- domain-driven design/ValueObjects/ValueObjects/TypeSafety/ITicketPrice.cs
+++
@@ -1,8 +0,0 @@
-using ValueObjects.ValueObjects;
-
-namespace ValueObjects.TypeSafety;
-
-public interface ITicketPrice
-{
-    Money GetTicketPrice(Country originCountry, Country destinationCountry, Station originStation, Station destinationStation);
-}
