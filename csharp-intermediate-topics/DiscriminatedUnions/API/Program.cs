bf1597a83e5ad7c30f3f8f893c80f64487c96b11
RPM1984 <rpmir1@gmail.com>
2023-06-14 15:59:42 +1000

    Remove boilerplate code

--- csharp-intermediate-topics/DiscriminatedUnions/API/Program.cs
+++ csharp-intermediate-topics/DiscriminatedUnions/API/Program.cs
@@ -1,19 +1,10 @@
 using API;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-
 builder.Services.AddSingleton<IOrdersService, OrdersService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
