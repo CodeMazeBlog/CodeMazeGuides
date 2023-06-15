7f79f6a7cdad2c9615eb353eb2f6e0c49fd86289
s_cule <86418620+StipeCule@users.noreply.github.com>
2023-06-15 14:08:50 +0200

    CM-419 Executing PowerShell in C# (#1035)
    * CM-419 Executing PowerShell in C#
    
    * CM-419 Executing PowerShell in C# code cleanup
    
    * CM419-First Revision
    
    * CM419-PowerShellInC#-Second Revision
    
    * CM419-PowerShell in C# Third Revision
    
    * CM-419: Execute a PowerShell Script in C#
    
    * CM-419: Executing a PowerShell Script in C#
    
    * CM-419: Executing a PowerShell Script in C#
    
    ---------
    
    Co-authored-by: scule <scule@SCULE-470G5>
    Co-authored-by: Code Maze <contact@code-maze.com>

---
+++ dotnet-cli/ExecutingPowerShellScript/Tests/PSCustomRunspaceLiveTest.cs
@@ -0,0 +1,27 @@
+﻿using ExecutingPowerShellScript;
+
+namespace Tests
+{
+    public  class PSCustomRunspaceLiveTest
+    {
+        [Fact]
+        public void GivenCommand_WhenInvoked_ThenExecutesCommandGiven()
+        {
+            var customRunspace = new PSCustomRunspace(); 
+            var result = customRunspace.ExecuteCommand("Get-Date");
+            var expectedResult = DateTime.Now.ToShortDateString();
+
+            Assert.Equal(expectedResult, DateTime.Parse(result).ToShortDateString());
+        }
+
+        [Fact]
+        public void GivenName_WhenInvoked_ThenDoesntStartAProcess()
+        {
+            var customRunspace = new PSCustomRunspace(); 
+            var result = customRunspace.StartProcess("notepad");
+            var expectedResult = false;
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
