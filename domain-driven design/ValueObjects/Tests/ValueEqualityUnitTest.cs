9a29f83619bba63d9cd74b32bb71c103536eeba9
gergo <gergo.vandor.dev@outlook.com>
2023-06-15 08:15:53 +0200

    test rename

--- domain-driven design/ValueObjects/Tests/ValueEqualityUnitTest.cs
+++ domain-driven design/ValueObjects/Tests/ValueEqualityUnitTest.cs
@@ -6,7 +6,7 @@ namespace Tests;
 public class ValueEqualityUnitTest
 {
     [Test]
-    public void WhenTwoRecordsWithTheSameData_ThenTheyShouldBeEqual()
+    public void GivenTwoRecords_WhenTheyHaveTheSameData_ThenTheyShouldBeEqual()
     {
         var hundredUsd = Money.Create(100, "USD");
         var another100Usd = Money.Create(100, "USD");
@@ -17,7 +17,7 @@ public class ValueEqualityUnitTest
     }
     
     [Test]
-    public void WhenTwoRecordsWithTheDifferentData_ThenTheyShouldNotBeEqual()
+    public void GivenTwoRecords_WhenTheyHaveTheDifferentData_ThenTheyShouldNotBeEqual()
     {
         var hundredUsd = Money.Create(100, "USD");
         var hundredEur = Money.Create(100, "EUR");
@@ -28,7 +28,7 @@ public class ValueEqualityUnitTest
     }
     
     [Test]
-    public void WhenTwoClassesWithTheSameData_ThenTheyShouldNotBeEqual()
+    public void GivenTwoClasses_WhenTheyHaveTheSameData_ThenTheyShouldNotBeEqual()
     {
         var hundredUsd = Money.Create(100, "USD");
         var payment1 = new Payment(hundredUsd.Value!);
