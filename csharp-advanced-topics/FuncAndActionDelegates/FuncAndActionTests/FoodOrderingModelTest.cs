5a50308cd4af4e5835c490ce68d44b93b9dcf345
Narges Firozi <narges.firozi@gmail.com>
2023-06-17 10:47:31 +0330

    Add New Project for New Article Named FuncAndActionDelegates

---
+++ csharp-advanced-topics/FuncAndActionDelegates/FuncAndActionTests/FoodOrderingModelTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using LibraryDemo;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace FuncAndActionTests
+{
+    public class FoodOrderingModelTest
+    {
+        List<OrderItemModel> OrderItemList = new();
+        FoodOrderingModel Order = new();
+
+        public FoodOrderingModelTest()
+        {
+            OrderItemList.Add(new OrderItemModel { Name = "Mushroom Soap", Price = 15.5M });
+            OrderItemList.Add(new OrderItemModel { Name = "Chiken Salad", Price = 20.3M });
+        }
+
+        [Fact]
+        public void WhenGetSumPrice_ThenReturnSumPrice()
+        {
+            decimal expected = 35.8M;
+
+            decimal actual=Order.GetSumPrice(OrderItemList);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void WhenGetSumPrice_ThenReturnNullException()
+        {
+            Assert.Throws<ArgumentNullException>("OrderItemList", ()=>Order.GetSumPrice(null)) ;
+        }
+    }
+}
---
+++ csharp-advanced-topics/FuncAndActionDelegates/LibraryDemo/OrderItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemo
+{
+    public class OrderItemModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
