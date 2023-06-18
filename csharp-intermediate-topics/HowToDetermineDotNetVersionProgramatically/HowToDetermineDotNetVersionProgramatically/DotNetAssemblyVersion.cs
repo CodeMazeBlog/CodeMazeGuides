fd8cd170beef2df7a01b20ecf08fae9cfc93f61d
Koddek <codedek@gmail.com>
2023-06-18 12:54:46 -0400

    fixed a bug that prevented the same result on all systems

--- csharp-intermediate-topics/HowToDetermineDotNetVersionProgramatically/HowToDetermineDotNetVersionProgramatically/DotNetAssemblyVersion.cs
+++ csharp-intermediate-topics/HowToDetermineDotNetVersionProgramatically/HowToDetermineDotNetVersionProgramatically/DotNetAssemblyVersion.cs
@@ -1,10 +1,15 @@
+using System.Reflection;
+
 namespace HowToDetermineDotNetVersionProgramatically;
 
 public static class DotNetAssemblyVersion
 {
     public static string GetAssemblyDotNetVersion(string assemblyFile)
     {
-        var fvi = FileVersionInfo.GetVersionInfo(assemblyFile);
-        return fvi.FileVersion ?? "";
+        var assembly = Assembly.LoadFrom(assemblyFile);
+        var references = assembly.GetReferencedAssemblies();
+        var version = references.FirstOrDefault(x => x.Name == "System.Runtime");
+
+        return version?.Version?.ToString() ?? "";
     }
 }
--- csharp-intermediate-topics/HowToDetermineDotNetVersionProgramatically/HowToDetermineDotNetVersionProgramaticallyTests/DotNetAssemblyVersionUnitTests.cs
+++ csharp-intermediate-topics/HowToDetermineDotNetVersionProgramatically/HowToDetermineDotNetVersionProgramaticallyTests/DotNetAssemblyVersionUnitTests.cs
@@ -6,9 +6,9 @@ public class DotNetAssemblyVersionUnitTests
     public void GivenPathToDllBuiltWithDotNet_WhenCallingGetAssemblyDotNetVersion_ThenReturnExpectedVersion()
     {
         // Given
-        var path = "/usr/local/share/dotnet/shared/Microsoft.NETCore.App/6.0.8/System.dll";
-        var expectedVersion = "6.0.8";
-
+        var path = typeof(DotNetAssemblyVersionUnitTests).Assembly.Location;
+        var expectedVersion = "7.0";
+        
         // When
         var actualVersion = DotNetAssemblyVersion.GetAssemblyDotNetVersion(path);
 
