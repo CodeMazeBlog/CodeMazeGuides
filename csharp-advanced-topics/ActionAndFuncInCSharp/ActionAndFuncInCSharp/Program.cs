6a7a053243d697501197778ccc504777f4fc1d13
Troncea Alin <alintroncea@gmail.com>
2023-06-26 10:28:25 +0300

    Added main files.

---
+++ csharp-advanced-topics/ActionAndFuncInCSharp/ActionAndFuncInCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Action example
+        void Print(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        Action<string> printAction = Print;
+
+        printAction("John");
+
+        printAction.Invoke("John");
+
+        void PrintSum(int x, int y) { Console.WriteLine(x + y); }
+
+        Action<int, int> printSumAction = PrintSum;
+
+        printSumAction.Invoke(2, 3);
+
+        // Func example
+        bool IsNumberEven(int number) { return number % 2 == 0; }
+
+        Func<int, bool> isEvenFunc = IsNumberEven;
+
+        Console.WriteLine(isEvenFunc(10));
+
+        // Func lambda
+        Func<int, int> squareFunc = x => x * x;
+
+        int squareResult = squareFunc(5);
+
+        Console.WriteLine(squareResult);
+
+        // Action lambda
+        Action<string> printMessageAction = message => Console.WriteLine(message);
+
+        printMessageAction("Hello, world!");
+    }
+}
