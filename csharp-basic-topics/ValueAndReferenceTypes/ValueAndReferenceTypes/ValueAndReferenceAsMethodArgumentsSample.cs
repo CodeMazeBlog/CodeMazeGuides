31b0513c1cf612c9edabab0b75260eb39e272478
OldPen16 <oldpen16@gmail.com>
2023-06-26 22:52:27 +0530

    CM-661: Value vs Reference Types in C# - In Depth Guide

---
+++ csharp-basic-topics/ValueAndReferenceTypes/ValueAndReferenceTypes/ValueAndReferenceAsMethodArgumentsSample.cs
@@ -0,0 +1,18 @@
+﻿namespace ValueAndReferenceTypes
+{
+    public class ValueAndReferenceAsMethodArgumentsSample
+    {
+        public void ValueTypeArguments(ValueTypeRectangle rect)
+        {
+            rect.Length = rect.Length + 1;
+            Console.WriteLine($"Length inside function = {rect.Length}");
+        }
+
+        public void ReferenceTypeArguments(ReferenceTypeRectangle rect)
+        {
+            rect.Length = rect.Length + 1;
+            Console.WriteLine($"Length inside function = {rect.Length}");
+        }
+
+    }
+}
