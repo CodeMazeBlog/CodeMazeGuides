01c29cda9e7e32f00200f2863be944b37afb8016
Stelios Pantelopoulos <pantelopoulosstelios@gmail.com>
2023-06-27 08:51:11 +0300

    Changes as pointed out from senior editor: - Seperate return statements from the rest of the code - Removed casting - Removed the two different repository classes and created a common one - Benchmark order by time and group by method - Benchmark added setup method for preparations

--- dotnet-efcore/EntityFrameworkCoreVsDapper/EntityFrameworkCoreVsDapper/Program.cs
+++ dotnet-efcore/EntityFrameworkCoreVsDapper/EntityFrameworkCoreVsDapper/Program.cs
@@ -1,15 +1,17 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
 using EntityFrameworkCoreVsDapper.Repositories;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        var efCoreRepository = new PersonsRepositoryEFCore();
-        efCoreRepository.GetXPersons(1000);
+        var PersonsRepository = new PersonsRepository();
+        PersonsRepository.GetXPersonsEFCore(1000);
 
-        var dapperRepository = new PersonsRepositoryDapper();
-        dapperRepository.GetXPersons(1000);
+        PersonsRepository.GetXPersonsDapper(1000);
 
         BenchmarkRunner.Run(typeof(Program).Assembly);
     } 
---
+++ dotnet-efcore/EntityFrameworkCoreVsDapper/EntityFrameworkCoreVsDapper/Repositories/PersonsRepository.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using Dapper;
+using EntityFrameworkCoreVsDapper.EFCore;
+using EntityFrameworkCoreVsDapper.Models;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace EntityFrameworkCoreVsDapper.Repositories
+{
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByMethod)]
+    public class PersonsRepository
+    {
+        private SqlConnection _dbConnection;
+        private PersonsContext _context;
+
+        public PersonsRepository() 
+        {
+            Setup();
+        }
+
+        [IterationSetup]
+        public void Setup()
+        {
+            _dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["PersonsDB"].ConnectionString);
+            _context = new PersonsContext();
+        }
+
+        [Benchmark]
+        [Arguments(10000)]
+        [Arguments(100000)]
+        [Arguments(1000000)]
+        public List<Person> GetXPersonsDapper(int personsNumber)
+        {
+            using (_dbConnection)
+            {
+                _dbConnection.Open();
+
+                return _dbConnection.Query<Person>($"SELECT TOP({personsNumber}) * FROM Persons").ToList();
+            }
+        }
+
+        [Benchmark]
+        [Arguments(10000)]
+        [Arguments(100000)]
+        [Arguments(1000000)]
+        public List<Person> GetXPersonsEFCore(int personsNumber)
+        {
+            using (_context)
+            {
+                return _context.Persons.Take(personsNumber).ToList();
+            }  
+        }
+    }
+}
