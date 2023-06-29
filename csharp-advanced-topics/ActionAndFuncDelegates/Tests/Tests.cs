c37070f6e0bfb01279ceb27dc9e2291558e49223
Troncea Alin <alintroncea@gmail.com>
2023-06-29 13:47:58 +0300

    Removed whitespaces from the Tests class.

--- csharp-advanced-topics/ActionAndFuncDelegates/Tests/Tests.cs
+++ csharp-advanced-topics/ActionAndFuncDelegates/Tests/Tests.cs
@@ -1,36 +1,27 @@
 using ActionAndFuncDelegates;
-
 namespace Tests
 {
     public class Tests
     {
-
-        private readonly PrintRepository printRepository;
-        private readonly MathOperationsRepository mathOperations;
-        private StringWriter stringWriter;
+        private readonly PrintRepository _printRepository;
+        private readonly MathOperationsRepository _mathOperations;
+        private readonly StringWriter _stringWriter;
 
         public Tests()
         {
-            printRepository = new PrintRepository();
-            mathOperations = new MathOperationsRepository();
-            stringWriter = new StringWriter();
+            _printRepository = new PrintRepository();
+            _mathOperations = new MathOperationsRepository();
+            _stringWriter = new StringWriter();
         }
 
         [Fact]
         public void GivenStringInput_WhenActionIsCalled_ThenPrintsTheInput()
         {
-
-            Action<string> printAction = printRepository.DisplayInput;
-
+            Action<string> printAction = _printRepository.DisplayInput;
             var input = "Michael";
-
             SetUpStringWriter();
-
             printAction(input);
-
-            var output = stringWriter.ToString();
-
-            // assert
+            var output = _stringWriter.ToString();
 
             Assert.Equal($"{input}", output.Trim());
         }
@@ -38,39 +29,25 @@ namespace Tests
         [Fact]
         public void GivenTwoNumbers_WhenActionIsInvoked_ThenPrintsCorrectAddition()
         {
-            Action<int, int> printSumAction = printRepository.DisplaySum;
-
+            Action<int, int> printSumAction = _printRepository.DisplaySum;
             SetUpStringWriter();
-
             var firstNumber = 2;
             var secondNumber = 3;
-
             printSumAction.Invoke(firstNumber, secondNumber);
-
             var expectedResult = firstNumber + secondNumber;
-
-            var output = stringWriter.ToString();
-
-            //assert
+            var output = _stringWriter.ToString();
 
             Assert.Equal($"{expectedResult}", output.Trim());
         }
-
 
         [Fact]
         public void GivenStringInput_WhenActionIsCalled_ThenPrintsItTroughLambda()
         {
             Action<string> printMessageAction = message => Console.WriteLine(message);
-
             SetUpStringWriter();
-
             var input = "Hello, world!";
-
             printMessageAction(input);
-
-            var output = stringWriter.ToString();
-
-            // assert
+            var output = _stringWriter.ToString();
 
             Assert.Equal($"{input}", output.Trim());
         }
@@ -78,8 +55,7 @@ namespace Tests
         [Fact]
         public void GivenAnEvenNumber_WhenFuncIsInvoked_ThenReturnsTheCorrectBooleanValue()
         {
-            Func<int, bool> isEvenFunc = mathOperations.IsNumberEven;
-
+            Func<int, bool> isEvenFunc = _mathOperations.IsNumberEven;
             var result = isEvenFunc(10);
 
             Assert.True(result);
@@ -89,18 +65,15 @@ namespace Tests
         public void GivenOneNumber_WhenFuncIsInvoked_ThenCorrectlyReturnsTheSquareTroughLambda()
         {
             Func<int, int> squareFunc = x => x * x;
-
             int squareResult = squareFunc(5);
 
             Assert.Equal(25, squareResult);
         }
 
         [Fact]
-
         private void SetUpStringWriter()
         {
-            Console.SetOut(stringWriter);
+            Console.SetOut(_stringWriter);
         }
-
     }
 }
