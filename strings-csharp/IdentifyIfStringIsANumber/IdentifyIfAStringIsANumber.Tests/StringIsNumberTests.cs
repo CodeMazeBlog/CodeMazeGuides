a4481a3311822a7d7837910dcd44a9d665de44ae
gergo <gergo.vandor.dev@outlook.com>
2023-07-01 12:19:54 +0200

    CM-775 updated string is a number

--- strings-csharp/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber.Tests/StringIsNumberTests.cs
+++ strings-csharp/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber.Tests/StringIsNumberTests.cs
@@ -61,4 +61,49 @@ public class StringIsNumberTests
 
         Assert.Equal(expectedResult, result);
     }
+
+    [Theory]
+    [InlineData("123", true)]
+    [InlineData("444", true)]
+    [InlineData("1.2345", true)]
+    [InlineData("2.4412", true)]
+    [InlineData("9999999999", true)]
+    [InlineData("abc", false)]
+    [InlineData("414aa", false)]
+    public void GivenAString_WhenUsingCompiledRegex_ThenTryToParseIt(string value, bool expectedResult)
+    {
+        var result = StringIsANumberChecker.UsingCompiledRegex(value);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("123", true)]
+    [InlineData("444", true)]
+    [InlineData("1.2345", false)]
+    [InlineData("2.4412", false)]
+    [InlineData("9999999999", true)]
+    [InlineData("abc", false)]
+    [InlineData("414aa", false)]
+    public void GivenAString_WhenUsingCharIsDigitWithForeach_ThenTryToParseIt(string value, bool expectedResult)
+    {
+        var result = StringIsANumberChecker.UsingCharIsDigitWithForeach(value);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("123", true)]
+    [InlineData("444", true)]
+    [InlineData("1.2345", false)]
+    [InlineData("2.4412", false)]
+    [InlineData("9999999999", true)]
+    [InlineData("abc", false)]
+    [InlineData("414aa", false)]
+    public void GivenAString_WhenUsingCharIsBetween09_ThenTryToParseIt(string value, bool expectedResult)
+    {
+        var result = StringIsANumberChecker.UsingCharIsBetween09(value);
+
+        Assert.Equal(expectedResult, result);
+    }
 }
--- strings-csharp/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber/StringIsANumberChecker.cs
+++ strings-csharp/IdentifyIfStringIsANumber/IdentifyIfAStringIsANumber/StringIsANumberChecker.cs
@@ -1,8 +1,9 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace IdentifyIfAStringIsANumber;
 
-public class StringIsANumberChecker
+public partial class StringIsANumberChecker
 {
     public static bool IntTryParse(string stringValue)
     {
@@ -11,7 +12,7 @@ public class StringIsANumberChecker
 
     public static bool DoubleTryParse(string stringValue)
     {
-        return double.TryParse(stringValue, out _);
+        return double.TryParse(stringValue, NumberStyles.Any, CultureInfo.InvariantCulture, out _);
     }
 
     public static bool UsingRegex(string stringValue)
@@ -21,9 +22,37 @@ public class StringIsANumberChecker
 
         return regex.IsMatch(stringValue);
     }
+    
+    [GeneratedRegex(@"^-?\d+(?:\.\d+)?$")] 
+    private static partial Regex IsDigitRegex();
+    
+    public static bool UsingCompiledRegex(string stringValue)
+    {
+        return IsDigitRegex().IsMatch(stringValue);
+    }
 
     public static bool UsingCharIsDigit(string stringValue)
     {
         return stringValue.All(char.IsDigit);
+    }
+
+    public static bool UsingCharIsDigitWithForeach(string stringValue)
+    {
+        foreach (var c in stringValue)
+        {
+            if (!char.IsDigit(c))
+                return false;
+        }
+        return true;
+    }
+
+    public static bool UsingCharIsBetween09(string stringValue)
+    {
+        foreach (var c in stringValue)
+        {
+            if (c is < '0' or > '9')
+                return false;
+        }
+        return true;
     }
 }
