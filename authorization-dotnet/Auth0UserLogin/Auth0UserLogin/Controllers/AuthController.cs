f15909e9eadbf6f9df7631469439a1fc39837eed
Ainea Wabwoba <aineah28@gmail.com>
2023-07-04 00:49:10 +0300

    added project files

---
+++ authorization-dotnet/Auth0UserLogin/Auth0UserLogin/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth0UserLogin.Controllers;
+
+public class AuthController : Controller
+{
+    public async Task Login(string returnUrl = "/")
+    {
+        var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(returnUrl)
+            .Build();
+
+        await HttpContext.ChallengeAsync(
+            Auth0Constants.AuthenticationScheme,
+            authenticationProperties
+        );
+    }
+
+    [Authorize]
+    public async Task Logout()
+    {
+        await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
+        {
+            RedirectUri = Url.Action("Index", "Home")
+        });
+
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
