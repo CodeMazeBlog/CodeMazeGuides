2f0fe2e809c35259617e6d1eaaf039536f1755cf
Samuel <lawalsamuel2310@gmail.com>
2023-07-06 13:07:55 +0100

    Add class TaskCompletedClass with method UseTaskCompletedMethodAsync and class ReturnClass with method UseReturnMethodAsync.

---
+++ csharp-basic-topics/TaskCompletedVsReturnInCSharp/TaskCompletedVsReturnInCSharp/ReturnClass.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskCompletedVsReturnInCSharp
+{
+    internal class ReturnClass
+    {
+        public async Task<int> UseReturnMethodAsync()
+        {
+            Console.WriteLine($"I am in {nameof(UseReturnMethodAsync)} method. About to perform some asynchronous work");
+            
+            await Task.Delay(1000);
+
+            // Return a result
+            return 42;
+        }
+    }
+}
