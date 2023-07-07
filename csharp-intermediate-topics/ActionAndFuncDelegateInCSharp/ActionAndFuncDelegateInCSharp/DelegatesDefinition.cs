766a262fdb0699ded8e8edb0b174788a8280e9fa
Rashik Tuladhar <rashiktuladhar2016@outlook.com>
2023-07-07 18:01:01 +0545

    - Project initialization - Action and Func delegate usage

---
+++ csharp-intermediate-topics/ActionAndFuncDelegateInCSharp/ActionAndFuncDelegateInCSharp/DelegatesDefinition.cs
@@ -0,0 +1,24 @@
+﻿namespace ActionAndFuncDelegateInCSharp
+{
+    public class DelegatesDefinition
+    {
+        /// <summary>
+        /// Get full name when first name and last name is supplied
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns></returns>
+        public string GetFullName(string firstName, string lastName) => $"{firstName} {lastName}";
+
+        /// <summary>
+        /// Gets the sum if two number
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        public void GetSumOfNumbers(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(firstNumber + secondNumber);
+        }
+
+    }
+}
---
+++ csharp-intermediate-topics/ActionAndFuncDelegateInCSharp/ActionAndFuncDelegateInCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using ActionAndFuncDelegateInCSharp;
+
+DelegatesDefinition delegatesDefinition = new();
+
+Action<int, int> getSumofNumbers = delegatesDefinition.GetSumOfNumbers;
+
+getSumofNumbers.Invoke(1, 2);
+
+Func<string, string, string> getFullNameDelegate = delegatesDefinition.GetFullName;
+
+Console.WriteLine(getFullNameDelegate.Invoke("Code", "Maze"));
