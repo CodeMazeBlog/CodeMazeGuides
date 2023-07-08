2eda874cfead9f283448391c7800507f584c984a
Rashik Tuladhar <rashiktuladhar2016@outlook.com>
2023-07-08 23:00:26 +0545

    - Test Cases Added

---
+++ csharp-intermediate-topics/ActionAndFuncDelegateInCSharp/Test.ActionAndFuncDelegateInCSharp/ActionAndFuncTests.cs
@@ -0,0 +1,45 @@
+﻿using ActionAndFuncDelegateInCSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.ActionAndFuncDelegateInCSharp
+{
+    [TestClass]
+    public class ActionAndFuncTests
+    {
+
+        [TestMethod]
+        public void WhenFirstNameAndLastNameProvided_DelegateReturnsFullName()
+        {
+            //Arrange
+            DelegatesDefinition delegatesDefinition = new();
+            Func<string, string, string> getFullNameDelegate = delegatesDefinition.GetFullName;
+            var firstName = "Code";
+            var lastName = "Maze";
+
+            //Act
+            var result = getFullNameDelegate.Invoke(firstName, lastName);
+
+            //Assert
+            Assert.AreEqual($"{firstName} {lastName}", result);
+        }
+
+        [TestMethod]
+        public void WhenTwoNumbersProvided_DelegateReturnsSum()
+        {
+            //Arrange
+            DelegatesDefinition delegatesDefinition = new();
+            Action<int, int> getSumofNumbers = delegatesDefinition.GetSumOfNumbers;
+            var firstNumber = 5;
+            var secondNumber = 10;
+
+            //Act
+            using StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            getSumofNumbers.Invoke(firstNumber, secondNumber);
+            var expectedResult = firstNumber + secondNumber;
+
+            //Assert
+            Assert.AreEqual(expectedResult.ToString().Trim(), stringWriter.ToString().Trim());
+        }
+    }
+}
