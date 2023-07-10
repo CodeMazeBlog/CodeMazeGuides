6ff978ddd1dd000988ef095a53ece4dcbd56c03b
Code Maze <contact@code-maze.com>
2023-07-10 09:38:43 +0200

    CM-668: Should We Use Record or Class or Struct in C#

--- csharp-intermediate-topics/ClassStructOrRecordInCSharp/GeoLocation.cs
+++ csharp-intermediate-topics/ClassStructOrRecordInCSharp/GeoLocation.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassStructOrRecordInCSharp
+﻿namespace ClassStructOrRecordInCSharp
 {
     public struct GeoLocation
     {
