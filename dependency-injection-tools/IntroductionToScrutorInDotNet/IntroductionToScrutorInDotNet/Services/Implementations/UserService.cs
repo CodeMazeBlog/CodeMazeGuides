6f05279959e27b5defea0cbbddaab19b9739d706
Bournane Abdelkrim <abdelkrim.bournane@gmail.com>
2023-07-11 16:20:27 +0200

    Add Introduction to scrutor in dotnet code

---
+++ dependency-injection-tools/IntroductionToScrutorInDotNet/IntroductionToScrutorInDotNet/Services/Implementations/UserService.cs
@@ -0,0 +1,16 @@
+﻿using IntroductionToScrutorInDotNet.Entities;
+
+namespace IntroductionToScrutorInDotNet.Services.Implementations;
+
+public class UserService : IUserService
+{
+    public User GetUser(int id)
+    {
+        return new User
+        {
+            Id = id,
+            FirstName = "John",
+            LastName = "Doe",
+        };
+    }
+}
