ea0f51ffc9f9504837b1ac77e5d3f0b0dd0bcc7e
GeoFelix <felixodhiambo538@gmail.com>
2023-07-12 17:50:04 +0300

    Action and Func Delegates in CSharp

---
+++ csharp-advanced-topics/ActionFuncDelegatesInCSharp/ActionFuncDelegates/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ActionFuncDelegates;
+
+internal class Program
+{    
+    public static void Main(string[] args)
+    {
+        //We instantiate the Site class
+        SiteInfo siteInfo = new SiteInfo();
+
+        // Our Action Delegate that takes an input and invokes it in the BestSite method in our SiteInfo Class
+        Action<string> learnCSharp = siteInfo.BestSite;
+        learnCSharp("CodeMaze");
+
+        // Func delegate that takes 6 and 9 as our integers and sums them using the TotalArticles method in the SiteInfo Class
+        Func<int, int, int> addArticles = siteInfo.TotalArticles;
+        var result = addArticles(6, 9);
+        Console.WriteLine("Total articles are: " + result);
+    }
+}
