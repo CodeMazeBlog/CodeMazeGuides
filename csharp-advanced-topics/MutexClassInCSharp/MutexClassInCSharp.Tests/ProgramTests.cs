6e95515a8ae1f62a2ec9c0b3dd8ac9a9025ac0cf
Sara Elsayeda <swe@saraelsa.net>
2023-07-13 08:05:45 -0400

    Add implementation code

---
+++ csharp-advanced-topics/MutexClassInCSharp/MutexClassInCSharp.Tests/ProgramTests.cs
@@ -0,0 +1,63 @@
+namespace MutexClassInCSharp.Tests;
+
+public sealed class ProgramTests
+{
+    private const string fileName = "numbers.txt";
+
+    private Action<string[]> _mainMethod
+        = typeof(AssemblyAccessor).Assembly.EntryPoint!.CreateDelegate<Action<string[]>>();
+
+    public ProgramTests()
+    {
+        File.Delete(fileName);
+    }
+
+    [Fact]
+    public async Task WhenProgramRunOnce_ThenNumbersWrittenConsistently()
+    {
+        await RunProgram();
+
+        await EnsureFileConsistency();
+    }
+
+    [Fact]
+    public async Task WhenProgramRunTwiceSimultaneously_ThenNumbersWrittenConsistently()
+    {
+        Task main1 = RunProgram();
+        Task main2 = RunProgram();
+
+        await main1;
+        await main2;
+
+        await EnsureFileConsistency();
+    }
+
+    private async Task RunProgram()
+        => await Task.Run(() => _mainMethod(new string[] { }));
+
+    private async Task EnsureFileConsistency()
+    {
+        string fileContents = await File.ReadAllTextAsync(fileName);
+        File.Delete(fileName);
+
+        List<int> numbers = fileContents
+            .TrimEnd()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+
+        int nextExpectedNumber = 1;
+
+        foreach (int number in numbers)
+        {
+            Assert.Equal(nextExpectedNumber, number);
+
+            nextExpectedNumber = nextExpectedNumber + 1;
+
+            if (nextExpectedNumber > 50)
+                nextExpectedNumber = 1;
+        }
+
+        Assert.Equal(1, nextExpectedNumber);
+    }
+}
---
+++ csharp-advanced-topics/MutexClassInCSharp/MutexClassInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿const string fileName = "numbers.txt";
+
+using Mutex mutex = new Mutex(initiallyOwned: false, "Global\\numbers_output", out bool mutexWasCreated);
+
+mutex.WaitOne();
+
+for (int number = 1; number <= 50; number++)
+{
+    File.AppendAllText(fileName, $"{number} ");
+    Thread.Sleep(100);
+}
+
+mutex.ReleaseMutex();
+
+// This class is for allowing the test project to access this assembly.
+public class AssemblyAccessor { }
