60f24d1a16ae46068b62745ae9c5bb936b02efd4
ederGus <edergus@gmail.com>
2023-07-14 19:13:03 +0200

    CM-700: Publish vs Send in MediatR. (#1134)
    * add main and test projects.
    
    * Add some tests.
    
    * Remove line not needed.
    
    * some indentation.
    
    * Moved to different folder
    
    ---------
    
    Co-authored-by: Eder Llamuca <eder.llamuca@appspace.com>
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ dotnet-client-libraries/PublishVsSendInMediatR/Tests/PublishVsSendInMediatRTests/EventHandlers/UserCreatedEventHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using PublishVsSendInMediatR.EventHandlers;
+using PublishVsSendInMediatR.Events;
+using PublishVsSendInMediatR.Models;
+using PublishVsSendInMediatR.Repositories;
+using PublishVsSendInMediatR.Services;
+
+namespace PublishVsSendInMediatRTests.EventHandlers
+{
+    public class UserCreatedEventHandlerTests
+    {
+        [Fact]
+        public async Task WhenHandleUserCreatedEvent_ThenVerifyUserIdAndSendNotification()
+        {
+            //Arrange
+            var userId = new Random().Next();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            userRepositoryMock
+                .Setup(x => x.GetAdminUserAsync())
+                .ReturnsAsync(new User { UserId = 1 });
+
+            var notificationServiceMock = new Mock<INotificationService>();
+            var userCreatedEventHandler = new UserCreatedEventHandler(userRepositoryMock.Object, notificationServiceMock.Object);
+            var request = new UserCreatedEvent { UserId = 1, UserName = "sampleUser" };
+
+            // Act
+            await userCreatedEventHandler.Handle(request, It.IsAny<CancellationToken>());
+
+            // Assert
+            userRepositoryMock.Verify(x => x.GetAdminUserAsync(), Times.Once);
+            notificationServiceMock.Verify(x => x.SendNotificationAsync(It.IsAny<int>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
