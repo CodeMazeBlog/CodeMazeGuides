94eaf3f73339533484e8ab11a8023bf44ca060fa
Haris Tse <haris.tsetsekas@gmail.com>
2023-07-14 13:19:15 +0300

    CM-680:Entity Splitting

---
+++ dotnet-efcore/SplitEntityIntoDifferentTableMappings/SplitEntityIntoDifferentTableMappings/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Hello, World!");
+
---
+++ dotnet-efcore/SplitEntityIntoDifferentTableMappings/SplitEntityIntoDifferentTableMappings/User.cs
@@ -0,0 +1,20 @@
+﻿namespace SplitEntityIntoDifferentTableMappings
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool RememberMe { get; set; }
+        public string Theme { get; set; }
+
+        public User(int id, string name, string email, bool rememberMe, string theme)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            RememberMe = rememberMe;
+            Theme = theme;
+        }
+    }
+}
