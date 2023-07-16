eedc9c4dccff7dbe8090f59ec24e9b8af7de2b28
Ainea Wabwoba <aineah28@gmail.com>
2023-07-16 10:23:39 +0300

    Updated signout call to use Auth0 constants, removed http and iis profiles

--- authorization-dotnet/Auth0UserLogin/Auth0UserLogin/Controllers/HomeController.cs
+++ authorization-dotnet/Auth0UserLogin/Auth0UserLogin/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace Auth0UserLogin.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
