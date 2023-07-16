23873fd7b65354c8f271c27f36bd6ee1e4f38cc9
Anoop Tripathy <anooptripathy@outlook.com>
2023-07-16 21:39:24 +0530

    console app to demonstrate Action and Fun Delegates

---
+++ Action&FuncDelegates/ActionandFuncDelegates/ActionandFuncDelegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ActionandFuncDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Action delegate example
+            Action<string> printAction = PrintMessage;
+            printAction("Hello, World!");
+
+            // Func delegate example
+            Func<int, int, int> addFunc = AddNumbers;
+            int result = addFunc(5, 10);
+
+            Console.WriteLine("Result of addition: " + result);
+            
+        }
+
+        // Method for printing a message
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        // Method for adding two numbers
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
