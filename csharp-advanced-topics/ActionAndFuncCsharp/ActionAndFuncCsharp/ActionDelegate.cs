0106b2a5c59a43bcf599fcb41adcf6a1bf4bae26
Hassan Iftikhar <hassan.iftikhar@constellationdealer.com>
2023-07-17 15:40:24 +0500

    Action and Func delegates implementation by Hassan Iftikhar

---
+++ csharp-advanced-topics/ActionAndFuncCsharp/ActionAndFuncCsharp/ActionDelegate.cs
@@ -0,0 +1,21 @@
+﻿namespace ActionAndFuncCsharp;
+public class ActionDelegate
+{
+    //Methods that takes input parameters but returns nothing;
+
+    public void DisplayText()
+    {
+        Console.WriteLine($"Action<> - Learning Delegates is Fun - Awesome!");
+    }
+    public void DisplayNumbers(int num1, int num2)
+    {
+        var sum = num1 + num2;
+        Console.WriteLine($"Action<int,int> - Sum is: {sum}");
+    }
+    public void DisplayMessage(string msg)
+    {
+        Console.WriteLine($"Action<T> : {msg}");
+    }
+
+}
+
