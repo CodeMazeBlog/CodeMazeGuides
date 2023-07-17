42c40e07cc6801988ffeed6fba69be2e69bf7ab0
Alvaro Montoya <alvaro.montoya@getrhyme.com>
2023-07-17 17:01:21 -0400

    updated geolocation to include a constructor

--- csharp-intermediate-topics/ClassStructOrRecordInCSharp/GeoLocation.cs
+++ csharp-intermediate-topics/ClassStructOrRecordInCSharp/GeoLocation.cs
@@ -5,6 +5,12 @@
         private string _latitude;
         private string _longitude;
 
+        public GeoLocation(string latitude, string longitude)
+        {
+            _latitude = latitude;
+            _longitude = longitude;
+        }
+
         public override string ToString() => $"(lat: {_latitude}, lon:{_longitude})";
     }
 }
