7f720b91296b8c8c9679aa628b03656fbe5e0067
GeoFelix <felixodhiambo538@gmail.com>
2023-07-18 23:34:18 +0300

    Committed Action and Func Delegates

---
+++ csharp-advanced-topics/Action-Func-Delegates/ActionFuncDelegates/SiteInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_Func_Delegates.ActionFuncDelegates;
+//This is a method that takes a string and prints it in the console
+public class SiteInfo
+{
+    //This is a method that takes a string and prints it in the console
+    public void BestSite(string name)
+    {
+        Console.WriteLine("Howdy, welcome to " + name + " where learning is simplified.");
+    }
+    //This is a method that takes two integer and sums them
+    public int TotalArticles(int angularArticles, int CSharpArticles)
+    {
+        return angularArticles + CSharpArticles;
+    }
+}
---
+++ csharp-advanced-topics/Action-Func-Delegates/ActionFuncDelegatesUnitTest/ActionFuncDelegateTest.cs
@@ -0,0 +1,47 @@
+using Action_Func_Delegates.ActionFuncDelegates;
+
+namespace ActionFuncDelegatesUnitTest;
+
+public class ActionFuncDelegateTest
+{
+    private SiteInfo _siteInfo;
+    [SetUp]
+    public void Setup()
+    {
+        _siteInfo = new SiteInfo();
+
+    }
+
+    [Test]
+    public void WhenBestSiteIsCalled_ThenReturnTheSiteNameToLearnCSharp()
+    {
+        //We assign the expected Action delegate output and what we get
+        var expected = "Howdy, welcome to CodeMaze where learning is simplified.";
+        StringWriter stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        //Act on the Action delegate
+        Action<string> learnCSharp = _siteInfo.BestSite;
+        learnCSharp("CodeMaze");
+
+        var result = stringWriter.ToString().Trim();
+
+        //Assert that the result on the console is the same as what our delegate has
+        Assert.AreEqual(expected, result);
+
+
+    }
+    [Test]
+    public void WhenTotalArticlesIsCalled_ThenReturnTheTotalArticles()
+    {
+        //We assign an expected number of articles
+        var expected = 15;
+
+        //We act on the Func Delegate to add the number of articles
+        Func<int, int, int> addArticles = _siteInfo.TotalArticles;
+        var result = addArticles(6, 9);
+
+        //We assert that the addition equates what we expected
+        Assert.AreEqual(expected, result);
+    }
+}
