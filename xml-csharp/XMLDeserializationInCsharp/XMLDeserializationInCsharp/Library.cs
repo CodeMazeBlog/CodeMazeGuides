8b7f9db6c318b211cda95622ad018383c9d625b7
falgunmodispade <104058740+falgunmodispade@users.noreply.github.com>
2023-07-20 13:43:22 +0530

    CM-166: XML Deserialization in C# (#1245)

---
+++ xml-csharp/XMLDeserializationInCsharp/XMLDeserializationInCsharp/Library.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace XMLDeserializationInCsharp
+{
+    [XmlRoot("Library")]
+    public class Library
+    {
+        [XmlArray("Books")]
+        [XmlArrayItem("Book")]
+        public List<Book> Books { get; set; } = new();
+    }
+}
