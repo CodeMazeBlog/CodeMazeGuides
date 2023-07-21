ed5ba85e54d21e3ea14fab063ba1dc41430ed24f
allanOAlex <allan.alex0803@gmail.com>
2023-07-22 02:48:20 +0300

    Initialize Project
    - Add .gitignore
    
    Application Stucture
    
    - Add Services Project(Class Library)
    - Console App Project
    
    Services
    
    - ActionDelegateService
    - FuncDelegateService

---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionAndFuncDelegates/ActionAndFuncDelegates.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Greeting
+
+#endregion
+
+#region Event Handling
+
+#endregion
+
+#region Mathematical Calculation
+
+#endregion
+
+#region EventFiltering
+
+#endregion
+
+
+
+
+
+public static class Methods
+{
+    public static void Greeting()
+    {
+
+    }
+}
---
+++ csharp-advanced-topics/DelegatesInCsharp/ActionAndFuncDelegates/ActionAndFuncDelegates.Services/ActionDelegateService.cs
@@ -0,0 +1,14 @@
+﻿namespace ActionAndFuncDelegates.Services
+{
+    public class ActionDelegateService
+    {
+        // Action delegate for greeting
+        public Action<string>? GreetAction;
+
+        // Method to trigger the GreetAction delegate
+        public void Greet(string name)
+        {
+            GreetAction?.Invoke(name);
+        }
+    }
+}
