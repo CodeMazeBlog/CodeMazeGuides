764fff328d8a64dd9134370bb7d8f69f67279695
sudhir parmar <sudhir16mcl19@gmail.com>
2023-07-22 22:59:55 +0530

    Refactored program file

--- csharp-advanced-topics/DelegatesInCsharp/DelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/DelegatesInCsharp/DelegatesInCsharp/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DelegatesInCsharp
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Action<int> printSquare = (x) =>
             {
                 int square = x * x;
@@ -16,18 +17,17 @@ namespace DelegatesInCsharp
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
             numbers.ForEach(printSquare);
 
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> numbers1 = new List<int> { 1, 2, 3, 4, 5 };
 
             Func<int, bool> isEven = (x) => x % 2 == 0;
 
-            List<int> evenNumbers = numbers.Where(isEven).ToList();
+            List<int> evenNumbers = numbers1.Where(isEven).ToList();
 
             foreach (int number in evenNumbers)
             {
                 Console.WriteLine(number);
             }
             Console.ReadLine();
-
         }
     }
 }
--- csharp-advanced-topics/DelegatesInCsharp/Tests/Tests.cs
+++ csharp-advanced-topics/DelegatesInCsharp/Tests/Tests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests
 {
