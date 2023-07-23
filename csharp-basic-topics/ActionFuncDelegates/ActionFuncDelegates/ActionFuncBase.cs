09b6fa9ca19f415df10eeb44a98b6ab596a32e3a
nmenkiti <nmenkiti@healthstation.ng>
2023-07-23 01:12:04 +0100

    Add my evaluation topic on Action and Func Delegates

---
+++ csharp-basic-topics/ActionFuncDelegates/ActionFuncDelegates/ActionFuncBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+internal class ActionFuncBase
+{
+
+    public static void PrintList(List<int> olist)
+    {
+        foreach (int item in olist)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+    public static List<int> TripleList(List<int> tlist)
+    {
+        List<int> result = new List<int>();
+        Triple(tlist, result);
+        return result;
+
+    }
+
+    private static void Triple(List<int> tlist, List<int> result)
+    {
+        foreach (int item in tlist)
+        {
+            result.Add(3 * item);
+        }
+    }
+}
---
+++ csharp-basic-topics/ActionFuncDelegates/ActionFuncDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program : ActionFuncBase
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+
+        Action<List<int>> printListAction = PrintList;
+        Console.WriteLine("Original List:");
+        printListAction(numbers);
+
+        Func<List<int>, List<int>> tripleListFunc = TripleList;
+        List<int> tripledResult = tripleListFunc(numbers);
+
+        Console.WriteLine("Tripled List:");
+        foreach (int element in tripledResult)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
