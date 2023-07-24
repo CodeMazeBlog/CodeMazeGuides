deae419da6655ce9a151adeceaab04cab2ddf375
JustyProgrammmer <justy.orlandy1@gmail.com>
2023-07-24 19:49:48 +0100

    #guid_action_and_func_delegates - Demo And Tests

--- csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -1,2 +1,138 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+
+namespace ActionAndFuncDelegates
+{
+    public delegate void ShowPersonData();
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void DisplayName()
+        {
+            Console.WriteLine(this.name);
+        }
+
+        public void DisplayAge()
+        {
+            Console.WriteLine(this.age + "\n");
+        }
+    }
+
+    public class Examples
+    {
+
+        public void ActionExamples()
+        {
+            Person personTwo = new Person("Orlando", 27);
+
+            Action showPersonDataWithAction = personTwo.DisplayName;
+            showPersonDataWithAction();
+
+            showPersonDataWithAction = personTwo.DisplayAge;
+            showPersonDataWithAction();
+
+            Action showDataWithAnonMethod = delegate () { personTwo.DisplayName(); };
+            showDataWithAnonMethod();
+
+            showDataWithAnonMethod = () => personTwo.DisplayAge();
+            showDataWithAnonMethod();
+
+            List<double> salaries = new List<double>();
+            salaries.Add(800);
+            salaries.Add(900);
+            salaries.Add(1500);
+            salaries.Add(750);
+            salaries.Add(3000);
+            salaries.Add(5000);
+
+            salaries.ForEach(Print);
+
+            Console.WriteLine();
+
+            salaries.ForEach(delegate (double salary)
+            {
+                Console.WriteLine(salary + (salary * 0.2));
+            });
+
+            Console.WriteLine();
+
+            salaries.ForEach((salary) =>
+            {
+                if (salary > 1000) Console.WriteLine(salary);
+            });
+
+            Console.WriteLine();
+
+            Action<string, string> showFullName = showFullNameProc;
+
+            void showFullNameProc(string fName, string lName) => Console.WriteLine(fName + " " + lName);
+            showFullName("Justino", "Sachilombo");
+
+            Console.WriteLine();
+
+            void Print(double salary)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+
+        public void FuncExamples()
+        {
+            Func<string, string> mySelector = str => str.ToUpper();
+
+            string[] listWords = { "MTV", "Apple" };
+
+            IEnumerable<String> resWords = listWords.Select(mySelector);
+
+            foreach (String word in resWords) Console.WriteLine(word);
+
+            Console.WriteLine();
+
+            Func<string, int, string> upperMethod = UppercaseString;
+            string name = "Jay";
+            int age = 48;
+
+            Console.WriteLine(upperMethod(name, age));
+
+            string UppercaseString(string strName, int age)
+            {
+                return (strName + "-" + age).ToUpper();
+            }
+        }
+
+        public void DelegateExamples()
+        {
+            Person personOne = new Person("Justino", 27);
+
+            ShowPersonData showPersonData = personOne.DisplayName;
+            showPersonData();
+
+            showPersonData = personOne.DisplayAge;
+            showPersonData();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Examples examples = new Examples();
+
+            examples.DelegateExamples();
+
+            examples.ActionExamples();
+
+            examples.FuncExamples();
+        }
+
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegatesTestes/Tests.cs
@@ -0,0 +1,72 @@
+namespace ActionAndFuncDelegatesTestes
+{
+    public delegate void ShowPersonData(double salary);
+
+    public delegate string PerformUpper(string name, int age);
+
+    [TestClass]
+    public class Tests
+    {
+        void Print(double salary) => Console.WriteLine(salary);
+
+        string UppercaseString(string strName, int age) { return (strName + "-" + age).ToUpper(); }
+
+        [TestMethod]
+        public void WhenInvokeADelegateWitchReceiveANamedMethod_DelegateContainsOneMethod()
+        {
+            var delegateTest = new ShowPersonData(Print);
+
+            var delegateMethod = delegateTest.Method;
+
+            Assert.AreEqual(delegateMethod.Name, "Print");
+        }
+
+        [TestMethod]
+        public void WhenInvokeADelegateWitchReceiveANamedMethodThatReturnsAValue_DelegateMustReturnAeValue()
+        {
+            var delegateTest = new PerformUpper(UppercaseString);
+
+            var res = delegateTest("Justin", 27);
+
+            Assert.AreEqual(res, "JUSTIN-27");
+        }
+
+        [TestMethod]
+        public void WhenInvokeAActionWitchReceiveALambdaExpression_ActionHaveToPerformTheLambdaOperation()
+        {
+            string fName = "";
+            string lName = "";
+
+            Action<string, string> showFullName = (string fuName, string laName) =>
+            {
+                fName = fuName;
+                lName = laName;
+            };
+
+            showFullName("Justino", "Sachilombo");
+
+            Assert.AreEqual(fName, "Justino");
+            Assert.AreEqual(lName, "Sachilombo");
+        }
+
+        [TestMethod]
+        public void WhenInvokeAFuncWitchReceiveALambdaExpression_FuncHaveToReturnAValue()
+        {
+            Func<string, string> mySelector = str => str.ToUpper();
+
+            var res = mySelector("Justino");
+
+            Assert.AreEqual(res, "JUSTINO");
+        }
+
+        [TestMethod]
+        public void WhenInvokeAFuncWitchReceiveAMethodWithTwoInputParams_FuncHaveToReturnAValue()
+        {
+            Func<string, int, string> upperFunc = UppercaseString;
+
+            var res = upperFunc("mArIa", 27);
+
+            Assert.AreEqual(res, "MARIA-27");
+        }
+    }
+}
