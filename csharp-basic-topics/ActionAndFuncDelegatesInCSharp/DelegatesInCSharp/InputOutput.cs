5f122a0298e37b546b48efe37fb4f08c46d2a9a9
usamakhan9106 <usamakhan9106@hotmail.com>
2023-07-25 00:23:17 +0500

    usamazahidkhan9106@gmail.com

---
+++ csharp-basic-topics/ActionAndFuncDelegatesInCSharp/DelegatesInCSharp/InputOutput.cs
@@ -0,0 +1,14 @@
+﻿public class InputOutput
+{
+    private string lastInput;
+    
+    public void Read(Func<string> input)
+    {
+        lastInput = input();
+    }
+    
+    public void Write(Action<string> printCallback)
+    {
+        printCallback($"'{lastInput}' has {lastInput.Length} characters.");
+    }
+}
---
+++ csharp-basic-topics/ActionAndFuncDelegatesInCSharp/DelegatesInCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿var input = new InputOutput();
+
+input.Read(ReadFromConsole);
+
+input.Write(WriteToConsole);
+
+Console.ReadLine();
+
+string ReadFromConsole()
+{
+    Console.WriteLine("Please enter some text to count the characters :");
+    return Console.ReadLine();
+}
+
+void WriteToConsole(string input)
+{
+    Console.WriteLine(Environment.NewLine);
+    Console.WriteLine(input);
+}
