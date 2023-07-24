54bd8f7ccb05204d88f0ad68f99db5d5919bfada
Ivan Gechev <ivan_banov@abv.bg>
2023-07-24 09:02:27 +0300

    Proper custom Exception

--- dotnet-testing/MockingWithNSubstitute/MockingWithNSubstitute/InvalidEmailException.cs
+++ dotnet-testing/MockingWithNSubstitute/MockingWithNSubstitute/InvalidEmailException.cs
@@ -2,4 +2,17 @@
 
 public class InvalidEmailException : Exception
 {
+    public InvalidEmailException()
+    {
+    }
+
+    public InvalidEmailException(string message)
+        : base(message)
+    {
+    }
+
+    public InvalidEmailException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
 }
