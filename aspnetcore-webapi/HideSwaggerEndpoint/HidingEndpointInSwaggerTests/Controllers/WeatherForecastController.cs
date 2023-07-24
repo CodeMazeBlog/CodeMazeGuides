b48b651a84878ed22ab133fdcca6533742c4efe0
Aneta Muslić <Aneta.Muslic@blu.com>
2023-07-24 09:11:10 +0200

    folder fix

---
+++ aspnetcore-webapi/HideSwaggerEndpoint/HidingEndpointInSwaggerTests/Controllers/WeatherForecastController.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HideSwaggerEndpoint.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetMethodOne")]
+        [NonAction]
+        public IEnumerable<WeatherForecast> GetMethodOne()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetMethodTwo")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IEnumerable<WeatherForecast> GetMethodTwo()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetMethodThree")]
+        public IEnumerable<WeatherForecast> GetMethodThree()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetMethodFour")]
+        public IEnumerable<WeatherForecast> GetMethodFour()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ aspnetcore-webapi/HideSwaggerEndpoint/HidingEndpointInSwaggerTests/Conventions/HideControllerConvention.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace HideSwaggerEndpoint.Conventions
+{
+    public class HideControllerConvention : IActionModelConvention
+    {
+        public void Apply(ActionModel action)
+        {
+            if (action.ActionName == "GetMethodThree")
+            {
+                action.ApiExplorer.IsVisible = false;
+            }
+        }
+    }
+}
