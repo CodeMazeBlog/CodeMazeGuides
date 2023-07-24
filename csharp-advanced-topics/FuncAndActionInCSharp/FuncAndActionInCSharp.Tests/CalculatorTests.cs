9bf474af2248ab2df09de99d1d51fa1f29ecb908
LiteObject <Lite.Object@gmail.com>
2023-07-23 22:37:03 -0500

    removed the second Func example to keep it simple

--- csharp-advanced-topics/FuncAndActionInCSharp/FuncAndActionInCSharp.Tests/CalculatorTests.cs
+++ csharp-advanced-topics/FuncAndActionInCSharp/FuncAndActionInCSharp.Tests/CalculatorTests.cs
@@ -14,18 +14,5 @@
             // ASSERT
             Assert.Equal(5, result);
         }
-
-        [Fact]
-        public void Subtract_TwoNumbers_ReturnsDifference()
-        {
-            // ARRANGE
-            Calculator calculator = new();
-
-            // ACT
-            int result = calculator.Subtract(5, 2);
-
-            // ASSERT
-            Assert.Equal(3, result);
-        }
     }
 }
--- csharp-advanced-topics/FuncAndActionInCSharp/FuncAndActionInCSharp/Calculator.cs
+++ csharp-advanced-topics/FuncAndActionInCSharp/FuncAndActionInCSharp/Calculator.cs
@@ -3,6 +3,5 @@
     public class Calculator
     {
         public Func<int, int, int> Add = (a, b) => a + b;
-        public Func<int, int, int> Subtract = (a, b) => a - b;
     }
 }
