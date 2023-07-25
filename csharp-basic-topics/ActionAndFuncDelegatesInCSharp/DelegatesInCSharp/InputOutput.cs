258b91259cd30ee37f94d440341796c834e2018b
usamakhan9106 <usamakhan9106@hotmail.com>
2023-07-26 01:58:21 +0500

    usamakhan9106@gmail.com

--- csharp-basic-topics/ActionAndFuncDelegatesInCSharp/DelegatesInCSharp/InputOutput.cs
+++ csharp-basic-topics/ActionAndFuncDelegatesInCSharp/DelegatesInCSharp/InputOutput.cs
@@ -1,14 +1,15 @@
 ﻿public class InputOutput
 {
     private string lastInput;
-    
+
     public void Read(Func<string> input)
     {
         lastInput = input();
     }
-    
+
     public void Write(Action<string> printCallback)
     {
-        printCallback($"'{lastInput}' has {lastInput.Length} characters.");
+        var length = lastInput == null ? 0 : lastInput.Length;
+        printCallback($"'{lastInput}' has {length} characters.");
     }
 }
---
+++ csharp-basic-topics/ActionAndFuncDelegatesInCSharp/Test/InputOutputTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class InputOutputTests
+{
+    [TestMethod]
+    public void Read_Write_ValidInput()
+    {
+        // Arrange
+        var inputOutput = new InputOutput();
+        var expectedInput = "Hello, world!";
+
+        // Act
+        inputOutput.Read(() => expectedInput);
+
+        string output = null;
+        inputOutput.Write((result) => output = result);
+
+        // Assert
+        Assert.AreEqual($"'{expectedInput}' has {expectedInput.Length} characters.", output);
+    }
+
+    [TestMethod]
+    public void Read_Write_EmptyInput()
+    {
+        // Arrange
+        var inputOutput = new InputOutput();
+        var expectedInput = "";
+
+        // Act
+        inputOutput.Read(() => expectedInput);
+
+        string output = null;
+        inputOutput.Write((result) => output = result);
+
+        // Assert
+        Assert.AreEqual($"'' has 0 characters.", output);
+    }
+
+    [TestMethod]
+    public void Read_Write_NullInput()
+    {
+        // Arrange
+        var inputOutput = new InputOutput();
+
+        // Act
+        inputOutput.Read(() => null);
+
+        string output = null;
+        inputOutput.Write((result) => output = result);
+
+        // Assert
+        Assert.AreEqual($"'' has 0 characters.", output);
+    }
+}
