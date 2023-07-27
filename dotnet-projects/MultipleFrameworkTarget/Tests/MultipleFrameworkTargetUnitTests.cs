ab80be150255b5091fb0cf7af1c05cda8d3676bc
Dharav12 <dvaghasiya82@gmail.com>
2023-07-27 00:45:37 -0500

    CM-763 Added Project and test project for Example

---
+++ dotnet-projects/MultipleFrameworkTarget/Tests/MultipleFrameworkTargetUnitTests.cs
@@ -0,0 +1,38 @@
+namespace Tests
+{
+    [TestClass]
+    public class MultipleFrameworkTargetUnitTests
+    {
+        [TestMethod]
+        public void TestMethodForAllFrameworks()
+        {
+            Console.WriteLine("Hello, World!");
+            Assert.IsTrue(true);
+        }
+
+#if NET6_0
+        [TestMethod]
+        public void TestMethodForNet6()
+        {
+            Console.WriteLine(".NET 6 - Hello from the latest .NET framework!");
+            Assert.IsTrue(true);
+        }
+
+#elif NETCOREAPP3_1
+        [TestMethod]
+        public void TestMethodForNetCore3_1()
+        {
+            Console.WriteLine(".NET Core 3.1 - Hello from a previous LTS version of .NET Core!");
+            Assert.IsTrue(true);
+        }
+
+#elif NET48
+        [TestMethod]
+        public void TestMethodForNet48()
+        {
+            Console.WriteLine(".NET Framework 4.8 - Hello from the traditional .NET Framework!");
+            Assert.IsTrue(true);
+        }
+#endif
+    }
+}
