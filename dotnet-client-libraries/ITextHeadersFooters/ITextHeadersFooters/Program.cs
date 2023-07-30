71273d86cefb90e3c1c7c8bcd396d4eb88fd0350
Matjaž Prtenjak <matjaz@matjazev.net>
2023-07-30 12:55:29 +0200

    CM-721: Initial project

---
+++ dotnet-client-libraries/ITextHeadersFooters/ITextHeadersFooters/Program.cs
@@ -0,0 +1,48 @@
+﻿using ITextHeadersFooters;
+using ITextHeadersFooters.ConsoleManager;
+using ITextHeadersFooters.PdfCreatorManager;
+
+var console = new RealConsole();
+var userMenu = new UserMenu(console);
+var pdfCreator = new RealPdfCreator(console);
+
+while (true)
+{
+    userMenu.DisplayOptions();
+    var userAction = userMenu.GetSelection();
+
+    switch (userAction)
+    {
+        case UserMenu.UserAction.CreateBasicDocument:
+            pdfCreator.CreatePDFFile("BasicDocument", BigDocument.CreateBasicDocument);
+            break;
+        case UserMenu.UserAction.CreateBasicDocumentWithBigMargins:
+            pdfCreator.CreatePDFFile("BasicDocumentWithBigMargins", BigDocument.CreateBasicDocumentWithBigMargins);
+            break;
+        case UserMenu.UserAction.AddTextAfterPageWasGenerated:
+            pdfCreator.CreatePDFFile("AddTextAfterPageWasGenerated", BigDocument.AddTextAfterPageWasGenerated);
+            break;
+        case UserMenu.UserAction.UsageOfShowTextAlignedMethod:
+            pdfCreator.CreatePDFFile("UsageOfShowTextAlignedMethod", BigDocument.UsageOfShowTextAlignedMethod);
+            break;
+        case UserMenu.UserAction.Classic_XofY_Footer:
+            pdfCreator.CreatePDFFile("Classic_XofY_Footer", BigDocument.Classic_XofY_Footer);
+            break;
+        case UserMenu.UserAction.PageNumberInHeaderDateInFooter:
+            pdfCreator.CreatePDFFile("PageNumberInHeaderDateInFooter", BigDocument.PageNumberInHeaderDateInFooter);
+            break;
+        case UserMenu.UserAction.DrawLine:
+            pdfCreator.CreatePDFFile("DrawLine", BigDocument.DrawLine);
+            break;
+        case UserMenu.UserAction.DifferentLeftAndRightPage:
+            pdfCreator.CreatePDFFile("DifferentLeftAndRightPage", BigDocument.DifferentLeftAndRightPage);
+            break;
+        case UserMenu.UserAction.Exit:
+            return;
+        case UserMenu.UserAction.Unknown:
+            break;
+    }
+
+    Console.WriteLine("\n\nPress any key to continue ...");
+    Console.ReadKey();
+}
---
+++ dotnet-client-libraries/ITextHeadersFooters/Tests/BigDocumentTest.cs
@@ -0,0 +1,64 @@
+﻿using ITextHeadersFooters;
+
+namespace Tests
+{
+    [TestClass]
+    public class BigDocumentTest
+    {
+        [TestMethod]
+        public void GivenCreateBasicDocumentCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.CreateBasicDocument);
+        }
+
+        [TestMethod]
+        public void GivenCreateBasicDocumentWithBigMarginsCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.CreateBasicDocumentWithBigMargins);
+        }
+
+        [TestMethod]
+        public void GivenAddTextAfterPageWasGeneratedCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.AddTextAfterPageWasGenerated);
+        }
+
+        [TestMethod]
+        public void GivenUsageOfShowTextAlignedMethodCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.UsageOfShowTextAlignedMethod);
+        }
+
+        [TestMethod]
+        public void GivenClassic_XofY_FooterCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.Classic_XofY_Footer);
+        }
+
+        [TestMethod]
+        public void GivenPageNumberInHeaderDateInFooterCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.PageNumberInHeaderDateInFooter);
+        }
+
+        [TestMethod]
+        public void GivenDrawLineCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.DrawLine);
+        }
+
+        [TestMethod]
+        public void GivenDifferentLeftAndRightPageCommand_WhenRunning_ThenExpectCreatedFile()
+        {
+            var runner = new PdfRunnerTest();
+            runner.DoTest(BigDocument.DifferentLeftAndRightPage);
+        }
+    }
+}
