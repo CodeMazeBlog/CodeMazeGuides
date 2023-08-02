eace69a45866dd7420f05c41a8e0273aac92f00f
Michal Kaminski <michal.kaminski@thetrainline.com>
2023-08-02 12:29:47 +0100

    Created new solution and project for mocking IOptions<T>

---
+++ dotnet-testing/MockingIOptions/MockingIOptions/Program.cs
@@ -0,0 +1,42 @@
+
+using MockingIOptions.Configuration;
+using MockingIOptions.Services;
+
+namespace MockingIOptions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.Configure<DatabaseConfiguration>(options => 
+                builder.Configuration.GetSection("DatabaseConfiguration").Bind(options));
+
+            builder.Services.AddScoped<ICustomerService, CustomerService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ dotnet-testing/MockingIOptions/MockingIOptionsTests/CustomerServiceTests.cs
@@ -0,0 +1,49 @@
+using Microsoft.Extensions.Options;
+using MockingIOptions.Configuration;
+using MockingIOptions.Services;
+using Moq;
+
+namespace MockingIOptionsTests
+{
+    public class CustomerServiceTests
+    {
+        private DatabaseConfiguration _configData;
+
+        [SetUp]
+        public void Setup()
+        {
+            _configData = new DatabaseConfiguration { ConnectionString = "TestConnectionString" };
+        }
+
+        [Test]
+        public void WhenMockingIOptionsWithMoq_ThenCorrectConnectionStringIsReturned()
+        {
+            var configMock = new Mock<IOptions<DatabaseConfiguration>>();
+            configMock.Setup(x => x.Value).Returns(_configData);
+            var sut = new CustomerService(configMock.Object);
+            var result = sut.GetConnectionString();
+
+            Assert.That(result, Is.EqualTo("TestConnectionString"));
+        }
+
+        [Test]
+        public void WhenMockingIOptionsWithOptionsWrapper_ThenCorrectConnectionStringIsReturned()
+        {
+            var configWrapper = new OptionsWrapper<DatabaseConfiguration>(_configData);
+            var sut = new CustomerService(configWrapper);
+            var result = sut.GetConnectionString();
+
+            Assert.That(result, Is.EqualTo("TestConnectionString"));
+        }
+
+        [Test]
+        public void WhenMockingIOptionsWithOptionsHelper_ThenCorrectConnectionStringIsReturned()
+        {
+            var configWrapperUsingHelper = Options.Create(_configData);
+            var sut = new CustomerService(configWrapperUsingHelper);
+            var result = sut.GetConnectionString();
+
+            Assert.That(result, Is.EqualTo("TestConnectionString"));
+        }
+    }
+}
