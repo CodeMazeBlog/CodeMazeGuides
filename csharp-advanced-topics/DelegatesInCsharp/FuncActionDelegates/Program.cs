d3145a0092150af3edd2bbead2a4429ead3f500c
gmosornoza <guillermo.sornoza@gmail.com>
2023-08-01 21:10:26 -0500

    Better structured code

--- csharp-advanced-topics/DelegatesInCsharp/FuncActionDelegates/Program.cs
+++ csharp-advanced-topics/DelegatesInCsharp/FuncActionDelegates/Program.cs
@@ -1,59 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Func and Action delegates");
+﻿
+namespace FuncActionDelegates
+{
+    class Program
+    {
+        static int powerActionResult = 0;
 
-Func<int, int, int> Power = PoweredTo;
+        static void Main(string[] args)
+        {
 
-int power = Power(10,5);
+            int powerActionAnonymousResult = 0;
+            int powerActionLambdaResult = 0;
+
+            Func<int, int, int> Power = PoweredTo;
+
+            Func<int, int, int> PowerAnonymous = delegate (int baseNumber, int exponent) {
+                return (int)Math.Pow(baseNumber, exponent);
+            };
+
+            Func<int, int, int> PowerLambda = (int baseNumber, int exponent) =>
+                (int)Math.Pow(baseNumber, exponent);
+
+            Action<int, int> PowerAction = PoweredToAction;
+
+            Action<int, int> PoweredActionAnonymous = delegate (int baseNumber, int exponent) {
+                powerActionAnonymousResult = (int)Math.Pow(baseNumber, exponent);
+            };
+
+            Action<int, int> PoweredActionLambda = (int baseNumber, int exponent) =>
+                powerActionLambdaResult = (int)Math.Pow(baseNumber, exponent);
 
 
+            int power = Power(10, 5);
+            int powerAnonymous = PowerAnonymous(10, 5);
+            int powerLambda = PowerLambda(10, 5);
+            Console.WriteLine(power);
+            Console.WriteLine(powerAnonymous);
+            Console.WriteLine(powerLambda);
 
-Console.WriteLine(power);
+            PowerAction(10, 5);
+            PoweredActionAnonymous(10, 5);
+            PoweredActionLambda(10, 5);
 
-Func <int, int, int> PowerAnonymous = delegate (int baseNumber, int exponent) {
-    return (int)Math.Pow(baseNumber, exponent);
-};
+            Console.WriteLine(powerActionResult);
+            Console.WriteLine(powerActionAnonymousResult);
+            Console.WriteLine(powerActionLambdaResult);
 
-int powerAnonymous = PowerAnonymous(10,5); 
+        }
 
-Console.WriteLine(powerAnonymous);
+        static int PoweredTo(int baseNumber, int exponent)
+        {
+            return (int)Math.Pow(baseNumber, exponent);
+        }
 
-Func <int, int, int> PowerLambda = (int baseNumber, int exponent) =>
-    (int)Math.Pow(baseNumber, exponent);
-
-int powerLambda = PowerLambda(10,5); 
-
-Console.WriteLine(powerLambda);
-
-int PoweredTo(int baseNumber, int exponent) {
-    return (int)Math.Pow(baseNumber, exponent);
+        static void PoweredToAction(int baseNumber, int exponent)
+        {
+            powerActionResult = (int)Math.Pow(baseNumber, exponent);
+        }
+    }
 }
 
-int powerActionAnonymousResult = 0;
-int powerActionLambdaResult = 0;
 
 
-Action<int, int> PoweredActionAnonymous = delegate (int baseNumber, int exponent) {
-    powerActionAnonymousResult = (int)Math.Pow(baseNumber, exponent);
-};
 
-PoweredActionAnonymous(10,5);
-Console.WriteLine(powerActionAnonymousResult);
 
-Action<int, int> PoweredActionLambda = (int baseNumber, int exponent) => 
-    powerActionLambdaResult = (int)Math.Pow(baseNumber, exponent);
 
-PoweredActionLambda(10,5);
-Console.WriteLine(powerActionLambdaResult);
 
-int powerActionResult = 0;
-
-Action<int, int> PowerAction = PoweredToAction;
-
-PowerAction(10,5);
-
-Console.WriteLine(powerActionResult);
-
-void PoweredToAction(int baseNumber, int exponent) {
-    powerActionResult = (int)Math.Pow(baseNumber, exponent);
-}
 
