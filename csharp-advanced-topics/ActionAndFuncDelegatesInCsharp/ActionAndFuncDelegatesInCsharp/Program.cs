d3f6b6e747a2d5f507989b212381fa7b2875f164
pradeeponsuccess <pradeepvaishya@gmail.com>
2023-08-03 11:23:58 +0530

    Action and Func Delegates in C#

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ActionAndFuncDelegatesInCsharp
+{
+   public class Program
+    {
+        public static void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"Addition Result: {result}");
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            int result = a * b;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Action addDelegate = () => Add(5, 3);
+            addDelegate(); // Output: Addition Result: 8
+
+            Func<int, int, int> multiplyDelegate = Multiply;
+            int multiplicationResult = multiplyDelegate(4, 6);
+            Console.WriteLine($"Multiplication Result: {multiplicationResult}"); // Output: Multiplication Result: 24
+        }
+    }
+}
