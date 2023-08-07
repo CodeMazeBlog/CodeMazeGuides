76d0922e50653ca62c0669cfdb995608cce32ed1
Ivan Gechev <ivan_banov@abv.bg>
2023-08-07 19:01:33 +0300

    CM-715: Primary Constructors for Classes and Structs (#1301)
    * Initial Commit
    
    * Doctor Class
    
    * Main body of the article done

---
+++ csharp-basic-topics/PrimaryConstructorsForClassesAndStructs/PrimaryConstructorsForClassesAndStructs.Tests/PointTests.cs
@@ -0,0 +1,19 @@
+﻿namespace PrimaryConstructorsForClassesAndStructs.Tests;
+
+public class PointTests
+{
+    [Fact]
+    public void WhenConstructorIsInvoked_ThenAllPropertiesAreAssigned()
+    {
+        // Arrange
+        var x = 5.0;
+        var y = 6.5;
+
+        // Act
+        var point = new Point(x, y);
+
+        // Assert
+        point.X.Should().Be(x);
+        point.Y.Should().Be(y);
+    }
+}
