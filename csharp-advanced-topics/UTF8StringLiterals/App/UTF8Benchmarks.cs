6f8ecbc18e625235d5bcb6e0a2b7d92f54cda23d
goldytech <muhammadafzal.qureshi@gmail.com>
2023-08-07 16:01:33 +0530

    First Commit

---
+++ csharp-advanced-topics/UTF8StringLiterals/App/UTF8Benchmarks.cs
@@ -0,0 +1,22 @@
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace App;
+
+[MemoryDiagnoser()]
+public class UTF8Benchmarks
+{
+    [Benchmark()]
+    public byte[] OldSyntax()
+    {
+        var returnValue = Encoding.UTF8.GetBytes("Hello World!");
+        return returnValue;
+    }
+    
+    [Benchmark()]
+    public ReadOnlySpan<byte> NewSyntax()
+    {
+        var returnValue = "Hello World!"u8;
+        return returnValue;
+    }
+}
