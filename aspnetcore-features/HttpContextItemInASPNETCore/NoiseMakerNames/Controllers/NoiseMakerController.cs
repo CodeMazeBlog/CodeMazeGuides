06ade99765350da47195c7bb18ff603093846505
Rachel Okorie <jayrachel96@gmail.com>
2023-08-08 12:30:23 +0100

    add noisemaker API

---
+++ aspnetcore-features/HttpContextItemInASPNETCore/NoiseMakerNames/Controllers/NoiseMakerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NoiseMakerNames.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoiseMakerController : ControllerBase
+    {
+        [HttpPost("add-noisemaker")]
+        public IActionResult AddNoiseMaker([FromBody] NoiseMaker noiseMaker)
+        {
+            NamesOfNoiseMakers list = new NamesOfNoiseMakers();
+
+            HttpContext.Items[list] = list.ViewNoiseMakers();
+
+            if (noiseMaker.Name == "Penny")
+            {
+                return Ok(HttpContext.Items[list]);
+            }
+
+            list.AddNoiseMaker(noiseMaker);
+            return Ok(noiseMaker);
+        }
+    }
+}
