2cfabd4c3ba70b0568fde78b36c1dc906d701c2e
aditisaxena0289 <aditisaxena0289@gmail.com>
2023-08-08 15:25:59 -0500

    CM-789: Tuple Aliases in C#

---
+++ csharp-intermediate-topics/TupleAliasInCSharp/TupleAliasInCSharp/GlobalAlias/GlobalAlias.cs
@@ -0,0 +1,12 @@
+﻿namespace TupleAliasInCSharp.GlobalAlias 
+{
+    internal class GlobalAlias
+    {
+        //ACCESS TUPLE WITH GLOBAL USING DIRECTIVE
+        public static void PrintTupleWithGlobalUsing()
+        {
+            tupleAliasGlobalUsing aliasGlobalUsing = (1, "Tuple alias declared with global using directive", 2);
+            Console.WriteLine($"num1: {aliasGlobalUsing.num1}, str: {aliasGlobalUsing.str}, num2: {aliasGlobalUsing.num2}");
+        }
+    }
+}
