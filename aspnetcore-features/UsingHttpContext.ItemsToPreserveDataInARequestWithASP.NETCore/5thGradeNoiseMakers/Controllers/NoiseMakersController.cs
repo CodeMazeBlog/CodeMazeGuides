331785fbc8165104b0b52f548efcfaaad7438ece
Rachel Okorie <jayrachel96@gmail.com>
2023-08-08 11:47:26 +0100

    noise makers api

---
+++ aspnetcore-features/UsingHttpContext.ItemsToPreserveDataInARequestWithASP.NETCore/5thGradeNoiseMakers/Controllers/NoiseMakersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FifthGradeNoiseMakers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoiseMakersController : ControllerBase
+    {
+
+        [HttpPost("add-noisemaker")]
+        public IActionResult AddNoiseMaker([FromBody] NoiseMaker noiseMaker)
+        {
+            NamesOfNoiseMakers list = new NamesOfNoiseMakers();
+
+            HttpContext.Items[list] = list.ViewNoiseMakers();
+
+            if (noiseMaker.Name == "Penny")
+            {
+                return Ok(HttpContext.Items[list]);
+            }
+
+            list.AddNoiseMaker(noiseMaker);
+            return Ok(noiseMaker);
+
+        }
+    }
+}
