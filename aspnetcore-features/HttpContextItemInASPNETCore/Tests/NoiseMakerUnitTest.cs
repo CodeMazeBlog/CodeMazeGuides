4eed42ba68a06469da0c88abfcbc27e2eb5812ab
Rachel Okorie <jayrachel96@gmail.com>
2023-08-08 12:31:20 +0100

    add unit test

---
+++ aspnetcore-features/HttpContextItemInASPNETCore/Tests/NoiseMakerUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using NoiseMakerNames;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class NoiseMakerUnitTest
+    {
+        [Fact]
+        public void WhenAddNoiseMakerIsCalled_ThenNewNoiseMakerIsAddedToTheList()
+        {
+            //arrange
+            NamesOfNoiseMakers namesOfNoiseMakers = new();
+            NoiseMaker noiseMaker = new()
+            {
+                Name = "Mira",
+                Severity = 2,
+                FirstAppearance = true
+            };
+
+            //act
+            namesOfNoiseMakers.AddNoiseMaker(noiseMaker);
+            List<NoiseMaker> noiseMakerList = namesOfNoiseMakers.ViewNoiseMakers();
+
+
+            //assert
+            Assert.NotNull(noiseMakerList);
+            Assert.Contains(noiseMaker, noiseMakerList);
+
+        }
+    }
+}
