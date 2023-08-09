cd7ecb60d10858ba359963ca1b041f17a8cf46e2
jado831123 <jado8311.23@gmail.com>
2023-08-09 12:16:37 -0700

    Example code for evaluation

---
+++ jado8311.23/Action and Func Delegates in C#/Invoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Invoice
+{
+    // Delegates for logging and email sending
+    public Action<string>? LogMessage { get; set; }
+    public Func<string, string, bool>? SendEmail { get; set; }
+
+    // Invoice data (just an example; in a real application, there would be more properties)
+    public string InvoiceNumber { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public Invoice(string invoiceNumber, decimal totalAmount)
+    {
+        InvoiceNumber = invoiceNumber;
+        TotalAmount = totalAmount;
+    }
+
+    public void SaveInvoice()
+    {
+        // Logic to save the invoice (implementation omitted for simplicity)
+        // ...
+
+        // Register a message in the log using the LogMessage delegate
+        LogMessage?.Invoke($"The invoice {InvoiceNumber} with a total amount of {TotalAmount} has been saved.");
+
+        // Send an email using the SendEmail delegate
+        string message = $"The invoice {InvoiceNumber} with a total amount of {TotalAmount} has been saved.";
+        bool emailSent = SendEmail?.Invoke("Invoice Saved", message) ?? false;
+
+        // If the email was sent successfully, we can perform some additional actions if necessary
+        if (emailSent)
+        {
+            // Perform some additional actions if needed
+            // ...
+        }
+    }
+}
---
+++ jado8311.23/Action and Func Delegates in C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, this is the starting point of our application!");
+
+        // Create an instance of the Invoice class
+        Invoice invoice = new Invoice("F12345", 100.50m);
+
+        // Subscribe the Log method from the LogManager class to the LogMessage delegate
+        invoice.LogMessage += LogManager.Log;
+
+        // Subscribe the SendEmail method from the EmailSender class to the SendEmail delegate
+        invoice.SendEmail += EmailSender.SendEmail;
+
+        // Save the invoice, which will trigger the subscribed delegates
+        invoice.SaveInvoice();
+    }
+}
