b12698e466098c6000b63640154005b6b8333cce
Richard Nwonah <53543378+richardnwonah@users.noreply.github.com>
2023-08-12 10:40:49 +0100

    Added FuncAndAction delegate console project

---
+++ csharp-basic-topics/FuncAndActionDelegates/FuncAndActionDelegateSolution/FuncAndActionDelegateExample/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+
+    static int Square(int num)
+    {
+        return num * num;
+    }
+    Func<int, int> squareFunc = Square;
+
+    static void ProcessList(List<int> numbers, Action<int> action)
+    {
+        foreach (var number in numbers)
+        {
+            action(number);
+        }
+    }
+    private static void Main(string[] args)
+    {
+       
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            // Utilizing Action to print each number
+            ProcessList(numbers, n => Console.WriteLine(n));
+            // Utilizing Func to compute squares
+            ProcessList(numbers, n => Console.WriteLine(Square(n)));
+        
+    }
+}
