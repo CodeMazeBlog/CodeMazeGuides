bd808a2acc53d4e90d9e8f680073d61b16219b3e
Ivan Gechev <ivan_banov@abv.bg>
2023-08-12 18:27:24 +0300

    DatabaseInitializer

--- dotnet-client-libraries/TestcontainersForDotNetAndDocker/TestcontainersForDotNetAndDocker/Program.cs
+++ dotnet-client-libraries/TestcontainersForDotNetAndDocker/TestcontainersForDotNetAndDocker/Program.cs
@@ -7,16 +7,17 @@ namespace TestcontainersForDotNetAndDocker;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var config = builder.Configuration;
 
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
             options.UseSqlServer(config.GetConnectionString("DefaultConnectionString")));
 
-        builder.Services.AddTransient<ICatRepository, CatRepository>();
-        builder.Services.AddTransient<ICatService, CatService>();
+        builder.Services.AddScoped<ICatRepository, CatRepository>();
+        builder.Services.AddScoped<ICatService, CatService>();
+        builder.Services.AddScoped<DatabaseInitializer, DatabaseInitializer>();
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
@@ -29,11 +30,12 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
 
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetService<DatabaseInitializer>();
+        await context.InitializeAsync();
 
         app.MapControllers();
 
