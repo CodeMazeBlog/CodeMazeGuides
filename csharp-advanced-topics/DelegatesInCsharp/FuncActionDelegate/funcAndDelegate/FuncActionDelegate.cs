bb366dcfb4aac69140ae5d26a9b6511fdeee42a3
abdulateef lawal <abdulateef>
2023-08-13 21:35:40 +0100

    func and action delegate in csharp

---
+++ csharp-advanced-topics/DelegatesInCsharp/FuncActionDelegate/funcAndDelegate/FuncActionDelegate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FuncActionDelegateSample
+{
+    public class FuncActionDelegate
+    {
+        public FuncActionDelegate()
+        {
+        }
+
+        public void PrintFullname(string firstName, string lastName)
+        {
+            Console.Write(firstName + " "+ lastName);
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
+
