634fe8ae38770735c1c6ded6f32e0c62efdaddb8
Richard Nwonah <53543378+richardnwonah@users.noreply.github.com>
2023-08-15 01:03:46 +0100

    Added uint test

---
+++ csharp-basic-topics/FuncActionDelegateSample/FuncActionDelegateSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using FuncActionDelegateSample;
+
+//declare func delegate
+Func<int, int, int> Operation;
+
+//declare Action delegate
+Action<string, string> Print;
+
+//instantiate FuncActionDelegate class
+FuncActionDelegate funcDelegate = new FuncActionDelegate();
+
+//assign func delgate to Subtract method
+Operation = funcDelegate.Subtract;
+Console.WriteLine(Operation(10, 5));
+
+//assign Action delgate to PrintFullname method
+Print = funcDelegate.PrintFullname;
+Print("Code", "Maze");
