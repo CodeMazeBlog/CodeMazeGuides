01eb52d01567d28bcd0d9d84ac9e559ec4598421
RPM1984 <rpmir1@gmail.com>
2023-08-20 21:48:11 +1000

    CM-317: Basic Authentication with HttpClient

---
+++ authorization-dotnet/BasicAuthenticationWithHttpClient/Client/WeatherForecastClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace Client;
+
+public class WeatherForecastClient
+{
+    private readonly HttpClient _client;
+
+    public WeatherForecastClient(HttpClient client, IConfiguration configuration)
+    {
+        _client = client;
+        _client.BaseAddress = new Uri("https://localhost:7003");                
+        var basicAuthenticationUsername = configuration["BasicAuthenticationUsername"];
+        var basicAuthenticationPassword = configuration["BasicAuthenticationPassword"];
+        var basicAuthenticationValue = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{basicAuthenticationUsername}:{basicAuthenticationPassword}"));
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicAuthenticationValue);
+    }
+    public async Task<string> GetAsync() => await _client.GetStringAsync("weatherforecast");
+}
