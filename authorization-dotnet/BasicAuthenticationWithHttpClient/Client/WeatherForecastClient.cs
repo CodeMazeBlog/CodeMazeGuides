0ba5311437f11ce6ea15b43043f2063ef027bf71
RPM1984 <rpmir1@gmail.com>
2023-08-24 08:27:57 +1000

    Code review updates

--- authorization-dotnet/BasicAuthenticationWithHttpClient/Client/WeatherForecastClient.cs
+++ authorization-dotnet/BasicAuthenticationWithHttpClient/Client/WeatherForecastClient.cs
@@ -5,17 +5,18 @@ namespace Client;
 
 public class WeatherForecastClient
 {
-    private readonly HttpClient _client;
+    private readonly HttpClient _httpClient;
 
-    public WeatherForecastClient(HttpClient client, IConfiguration configuration)
+    public WeatherForecastClient(HttpClient httpClient, IConfiguration configuration)
     {
-        _client = client;
-        _client.BaseAddress = new Uri("https://localhost:7003");                
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://localhost:7003");                
         var basicAuthenticationUsername = configuration["BasicAuthenticationUsername"];
         var basicAuthenticationPassword = configuration["BasicAuthenticationPassword"];
         var basicAuthenticationValue = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{basicAuthenticationUsername}:{basicAuthenticationPassword}"));
-        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicAuthenticationValue);
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicAuthenticationValue);
 
     }
-    public async Task<string> GetAsync() => await _client.GetStringAsync("weatherforecast");
+
+    public async Task<string> GetAsync() => await _httpClient.GetStringAsync("weatherforecast");
 }
