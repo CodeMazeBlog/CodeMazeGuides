3e30fdac6c5866898e6c6f85bc936b239ce9e7c7
Emmanuel Adom <79058040+Emmanuel-Adom@users.noreply.github.com>
2023-08-23 06:21:59 +0000

    Add files via upload

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/FuncDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Calculation
+{
+    public int Calculate(Func<int, int, int> operation)
+    {
+        return operation(20, 10);
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Calculation calculation = new();
+        int result = calculation.Calculate(calculation.Multiply);
+        Console.WriteLine($"Result is: {result}"); 
+    }
+}
