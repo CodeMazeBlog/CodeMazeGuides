f4fb2411c75980efa9fa90a65284f7f7e5fd1a7a
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-08-24 07:58:24 +0100

    Feat: Add project files

---
+++ aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace GettingStartedASPNETMongoDB.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Course name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Course code is required")]
+        public string Code { get; set; }
+    }
+}
---
+++ aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Models/SchoolDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace GettingStartedASPNETMongoDB.Models
+{
+    public class SchoolDatabaseSettings
+    {
+        public string StudentsCollectionName { get; set; }
+
+        public string CoursesCollectionName { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+}
