324981596859dc184a4daeb78aaa2fbb949946b1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-08-25 08:58:17 +0200

    Action and Func Delegates in C#

---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp.Tests/ActionAndFuncDelegatesInCSharpUnitTest.cs
@@ -0,0 +1,30 @@
+﻿namespace ActionAndFuncDelegatesInCSharp.Tests;
+
+public class ActionAndFuncDelegatesInCSharpUnitTest
+{
+    [Fact]
+    public void WhenActionDelegateIsUsed_ThenItShouldBeExecuted()
+    {
+        // Arrange
+        Action displayMessage = () => Console.WriteLine("Hello, C#!");
+
+        // Act
+        displayMessage();
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void WhenFuncDelegateIsUsed_ThenItShouldBeExecutedAndReturnProperResult()
+    {
+        // Arrange
+        Func<int> getRandomNumber = () => new Random().Next(minValue: 1, maxValue: 100);
+
+        // Act
+        var randomNumber = getRandomNumber();
+
+        // Assert
+        Assert.True(condition: randomNumber is >= 1 and < 100);
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿// Using Action with no parameters
+Action displayMessage = () => Console.WriteLine("Hello, C#!");
+
+// Using Action with two parameters
+Action<int, int> displaySum = (a, b) => Console.WriteLine($"The sum is: {a + b}");
+
+displayMessage();  // Outputs: Hello, C#!
+displaySum(3, 4);  // Outputs: The sum is: 7
+
+// Using Func with no input but a return type
+Func<int> getRandomNumber = () => new Random().Next(1, 100);
+
+// Using Func with two input parameters and a return type
+Func<int, int, int> findMax = (a, b) => a > b ? a : b;
+
+Console.WriteLine(getRandomNumber());  // Outputs a random number between 1 and 99
+Console.WriteLine(findMax(5, 8));      // Outputs: 8
+
+// LINQ example
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+Func<int, bool> isEven = n => n % 2 == 0;
+
+var evenNumbers = numbers.Where(isEven).ToList();
+Console.WriteLine(string.Join(", ", evenNumbers));  // Outputs: 2, 4
