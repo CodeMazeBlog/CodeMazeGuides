b679e6e947deb8d4a1fda7b3b18d90e8b95c2bbc
Joaquim Nunes <mail@jpetronilho.pt>
2023-08-25 10:20:22 +0100

    Project first commit. Added Main project and Tests.

---
+++ strings-csharp/ReadStringFromResourceFile/ReadStringFromResourceFile/Classes/ResourceItem.cs
@@ -0,0 +1,21 @@
+﻿namespace ReadStringFromResourceFile;
+
+public class ResourceItem<T>
+{
+    private T? item;
+
+    public ResourceItem(T? item = default(T))
+    {
+        this.item = item;
+    }
+
+    public T? Item { get => item; }
+
+    public bool IsValid => Item != null;
+
+    public override string ToString()
+    {
+        return Item?.ToString() ?? string.Empty;
+    }
+}
+
---
+++ strings-csharp/ReadStringFromResourceFile/ReadStringFromResourceFile/Classes/ResourcesManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace ReadStringFromResourceFile;
+
+public class ResourcesManager
+{
+    private Assembly? assembly;
+    private ResourceManager resources;
+    private CultureInfo? culture;
+
+    public ResourcesManager(string resourceFile, Assembly? assembly = null, CultureInfo? cultureInfo = null)
+    {
+        assembly = assembly ?? Assembly.GetExecutingAssembly();
+
+        culture = cultureInfo ?? CultureInfo.CurrentCulture;
+        resources = new ResourceManager(resourceFile, assembly);
+
+        if (resources is null)
+            throw new Exception($"Resource file {resourceFile} not found.");
+    }
+
+    public string? GetString(string key)
+    {
+        try
+        {
+            var resource = GetResource<string>(key);
+            
+            if (!resource.IsValid)
+                return string.Empty;
+
+            return resource.Item;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public ResourceItem<T> GetResource<T>(string key)
+    {
+        try
+        {
+            var obj = resources.GetObject(key, culture);
+
+            if (obj is T)
+                return new ResourceItem<T>((T)obj);
+            else
+                return new ResourceItem<T>();
+
+        }
+        catch
+        {
+            return new ResourceItem<T>();
+        }
+    }
+}
