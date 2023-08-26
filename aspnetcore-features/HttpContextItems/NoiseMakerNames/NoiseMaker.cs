723219fb515926991ccdd5a426604dea57c81760
acha-r <jayrachel96@gmail.com>
2023-08-26 10:13:11 +0100

    add middleware and filter examples

--- aspnetcore-features/HttpContextItems/NoiseMakerNames/NoiseMaker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NoiseMakerNames
-{
-    public class NoiseMaker
-    {
-        public string? Name { get; set; }
-        public int Severity { get; set; }
-        public bool FirstAppearance { get; set; }
-    }
-}
