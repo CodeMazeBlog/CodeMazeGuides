a1d281b0ce9acc739698789488d742cc0ed2a3b9
Joaquim Nunes <mail@jpetronilho.pt>
2023-08-27 21:06:11 +0100

    Update demo to show error when resource doesn´t exist

--- strings-csharp/ReadStringFromResourceFile/ReadStringFromResourceFile/Program.cs
+++ strings-csharp/ReadStringFromResourceFile/ReadStringFromResourceFile/Program.cs
@@ -4,6 +4,8 @@ ResourcesManager rmEnglish = new ResourcesManager(@"ReadStringFromResourceFile.R
 Console.WriteLine(rmEnglish.GetString("GREETINGS_TEXT"));
 
 var logoff = rmEnglish.GetString("LOGOFF_2");
+if (logoff == string.Empty)
+    Console.WriteLine("Resource LOGOFF_2 doesn´t exist!");
 
 ResourcesManager rmPortuguese = new ResourcesManager(@"ReadStringFromResourceFile.Resources.Texts.Portuguese");
 Console.WriteLine(rmPortuguese.GetResource<string>("GREETINGS_TEXT"));
