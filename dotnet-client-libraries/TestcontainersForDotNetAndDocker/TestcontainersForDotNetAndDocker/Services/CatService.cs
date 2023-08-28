07042e5117c214afdf9a3b89b848e0c1dc8cf8a9
Ivan Gechev <ivan_banov@abv.bg>
2023-08-28 12:18:37 +0300

    CM-725: Testing Using Testcontainers for .NET and Docker (#1349)
    * Initial Commit
    
    * Initial structure
    
    * wip
    
    * CatController
    
    * EF core stuff
    
    * DatabaseInitializer
    
    * Api is working
    
    * Testcontainers finally works
    
    * Draft done
    
    * Renaming tests classes
    
    * Renaming the classes themselves
    
    * Implementing changes suggested by Florian
    
    * Removed custom container
    
    * unused usings
    
    * more naming stuff
    
    * Removed unnecessary launch profiles
    
    ---------
    
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ dotnet-client-libraries/TestcontainersForDotNetAndDocker/TestcontainersForDotNetAndDocker/Services/CatService.cs
@@ -0,0 +1,29 @@
+﻿using TestcontainersForDotNetAndDocker.Domain;
+using TestcontainersForDotNetAndDocker.Repositories;
+
+namespace TestcontainersForDotNetAndDocker.Services;
+
+public class CatService : ICatService
+{
+    private readonly ICatRepository _catRepository;
+
+    public CatService(ICatRepository catRepository)
+    {
+        _catRepository = catRepository;
+    }
+
+    public async Task<bool> CreateCatAsync(Cat Cat)
+        => await _catRepository.CreateCatAsync(Cat);
+
+    public async Task<bool> DeleteCatAsync(Guid id)
+        => await _catRepository.DeleteCatAsync(id);
+
+    public async Task<IEnumerable<Cat>> GetAllCatsAsync()
+        => await _catRepository.GetAllCatsAsync();
+
+    public async Task<Cat?> GetCatAsync(Guid id)
+        => await _catRepository.GetCatAsync(id);
+
+    public async Task<bool> UpdateCatAsync(Cat cat)
+        => await _catRepository.UpdateCatAsync(cat);
+}
