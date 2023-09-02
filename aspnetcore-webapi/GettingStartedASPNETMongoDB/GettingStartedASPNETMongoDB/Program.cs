0bb74cb82885d6e92eb0b085c763a2cc8769990f
king-Alex-d-great <ogubuikealex@gmail.com>
2023-09-02 13:58:12 +0100

    Chore: Convert to Top Level Statement

--- aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Program.cs
+++ aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Program.cs
@@ -3,42 +3,36 @@ using GettingStartedASPNETMongoDB.Interfaces;
 using GettingStartedASPNETMongoDB.Models;
 using GettingStartedASPNETMongoDB.Services;
 
-namespace GettingStartedASPNETMongoDB;
-public class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<SchoolDatabaseSettings>(builder.Configuration.GetSection("SchoolDatabaseSettings"));
+
+builder.Services.AddSingleton<IStudentService, StudentService>();
+
+builder.Services.AddSingleton<ICourseService, CourseService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    app.UseSwagger();
 
-        builder.Services.AddControllers();
-
-        builder.Services.AddEndpointsApiExplorer();
-
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.Configure<SchoolDatabaseSettings>(builder.Configuration.GetSection("SchoolDatabaseSettings"));
-
-        builder.Services.AddSingleton<IStudentService, StudentService>();
-
-        builder.Services.AddSingleton<ICourseService, CourseService>();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
-    }
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
