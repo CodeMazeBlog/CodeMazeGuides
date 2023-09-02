4b81dd6cba102ce877f677f2ea7c8d3521a11d8e
Iacob Soloduh <iacob.soloduh@amdaris.com>
2023-09-02 17:14:41 +0300

    Iacob - Action and Func Delegates

---
+++ action-func-delegates/action-func-delegates/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace action_func_delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<User>
+            {
+                new User("Marin", "Tanner", "marin.tanner@mail.com"),
+                new User("Jakob", "Estes", "jakob.estes@mail.com"),
+                new User("Tess", "Hansen", "tess.hansen@mail.com"),
+                new User("Luka", "Lester", "luka.lester@mail.com")
+            };
+
+            Console.WriteLine("User names");
+            DisplayUsers(users, user => Console.WriteLine($"FirstName: {user.FirstName}, LastName: {user.LastName}"));
+            Console.WriteLine("User emails");
+            DisplayUsers(users, user => Console.WriteLine($"Email: {user.Email}"));
+
+            var jakob = Find(users, user => user.Email == "jakob.estes@mail.com");
+            var underage = Find(users, user => user.Age < 18);
+
+        }
+
+        static void DisplayUsers(List<User> users, Action<User> display)
+        {
+            foreach (var user in users)
+            {
+                display(user);
+            }
+        }
+
+        static User Find(List<User> users, Func<User, bool> compare)
+        {
+            foreach (var user in users)
+            {
+                if (compare(user))
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ action-func-delegates/action-func-delegates/User.cs
@@ -0,0 +1,18 @@
+﻿namespace action_func_delegates
+{
+    public class User
+    {
+        public User(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+    }
+}
