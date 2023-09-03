2a854dfae2c2366ad1a6b8866bc9f57d66763c78
Satya <satyap48@gmail.com>
2023-09-03 16:05:24 +0530

    CM-794: Code changes based on Editor review

--- csharp-advanced-topics/VolatileInterlockedLockInCsharp/Tests/Tests.cs
+++ csharp-advanced-topics/VolatileInterlockedLockInCsharp/Tests/Tests.cs
@@ -1,79 +1,58 @@
 using VolatileInterlockedLockInCsharp;
 
-namespace Tests
+namespace Tests;
+
+public class Tests
 {
-    public class Tests
+    [Fact]
+    public void GivenBalanceWithoutSync_WhenMultipleWithdrawals_ThenFinalBalanceIsIncorrect()
     {
-        private static void WithdrawBalance(Action<int> withdrawalAction)
+        var account = new Account
         {
-            using var synch = new ManualResetEventSlim(false);
+            Balance = 100000
+        };
 
-            var tasks = new Task[1000];
+        AccountService.WithdrawBalance(account.Withdraw);
 
-            for (var i = 0; i < tasks.Length; i++)
-            {
-                tasks[i] = Task.Run(() =>
-                {
-                    synch.Wait();
-                    Thread.Sleep(Random.Shared.Next(50, 300));
-                    withdrawalAction(100);
-                });
-            }
+        Assert.NotEqual(100000, account.Balance);
+    }
 
-            synch.Set();
-            Task.WaitAll(tasks);
-        }
-
-        [Fact]
-        public void GivenBalanceWithoutSync_WhenMultipleWithdrawals_ThenFinalBalanceIsIncorrect()
+    [Fact]
+    public void GivenBalanceVolatile_WhenMultipleWithdrawals_ThenFinalBalanceIsIncorrect()
+    {
+        var account = new Account
         {
-            var account = new Account
-            {
-                Balance = 100000
-            };
+            BalanceVolatile = 100000
+        };
 
-            WithdrawBalance(account.Withdraw);
+        AccountService.WithdrawBalance(account.WithdrawVolatile);
 
-            Assert.NotEqual(100000, account.Balance);
-        }
+        Assert.NotEqual(1000, account.BalanceVolatile);
+    }
 
-        [Fact]
-        public void GivenBalanceVolatile_WhenMultipleWithdrawals_ThenFinalBalanceIsIncorrect()
+    [Fact]
+    public void GivenBalanceWithLock_WhenMultipleWithdrawals_ThenFinalBalanceIsCorrect()
+    {
+        var account = new Account
         {
-            var account = new Account
-            {
-                BalanceVolatile = 100000
-            };
+            BalanceLock = 100000
+        };
 
-            WithdrawBalance(account.WithdrawVolatile);
+        AccountService.WithdrawBalance(account.WithdrawLock);
 
-            Assert.NotEqual(1000, account.BalanceVolatile);
-        }
+        Assert.Equal(0, account.BalanceLock);
+    }
 
-        [Fact]
-        public void GivenBalanceWithLock_WhenMultipleWithdrawals_ThenFinalBalanceIsCorrect()
+    [Fact]
+    public void GivenBalanceWithInterlocked_WhenMultipleWithdrawals_ThenFinalBalanceIsCorrect()
+    {
+        var account = new Account
         {
-            var account = new Account
-            {
-                BalanceLock = 100000
-            };
+            BalanceInterlocked = 100000
+        };
 
-            WithdrawBalance(account.WithdrawLock);
+        AccountService.WithdrawBalance(account.WithdrawInterlocked);
 
-            Assert.Equal(0, account.BalanceLock);
-        }
-
-        [Fact]
-        public void GivenBalanceWithInterlocked_WhenMultipleWithdrawals_ThenFinalBalanceIsCorrect()
-        {
-            var account = new Account
-            {
-                BalanceInterlocked = 100000
-            };
-
-            WithdrawBalance(account.WithdrawInterlocked);
-
-            Assert.Equal(0, account.BalanceInterlocked);
-        }
+        Assert.Equal(0, account.BalanceInterlocked);
     }
 }
