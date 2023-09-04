d384d01d1e0f7d8d63fd5d2597caea3815d2ccc7
semirteskeredzic <semir.teskeredzic@gmail.com>
2023-09-04 22:14:15 +0200

    Adapted code according to review suggestions for toppings; Adapted tests

--- blazor-features/OnChangeEventWithSelectDropdown/OnChangeEventWithSelectDropdownTests/BurgerConfiguratorTests.cs
+++ blazor-features/OnChangeEventWithSelectDropdown/OnChangeEventWithSelectDropdownTests/BurgerConfiguratorTests.cs
@@ -16,20 +16,26 @@ public class BurgerConfiguratorTests
         var totalCostElement = component.Find("p:nth-of-type(3)");
         var grandTotalElement = component.Find("p:nth-of-type(4)");
 
-        Assert.AreEqual("Burger without topping: 5.4", baseBurgerCostElement.TextContent.Trim());
-        Assert.AreEqual("Topping:", selectedToppingElement.TextContent.Trim());
-        Assert.AreEqual("Total Cost: 5.4", totalCostElement.TextContent.Trim());
+        Assert.AreEqual("Burger without topping: $5.4", baseBurgerCostElement.TextContent.Trim());
+        Assert.AreEqual("Free topping:", selectedToppingElement.TextContent.Trim());
+        Assert.AreEqual("Total Cost: $5.4", totalCostElement.TextContent.Trim());
 
-        var toppingSelectElement = component.Find(".select-element");
+        var toppingSelectElement = component.Find("select:nth-of-type(1)");
         toppingSelectElement.Change(2); // Select "Cheese" (Id: 2)
 
-        Assert.AreEqual("Burger without topping: 5.4", baseBurgerCostElement.TextContent.Trim());
-        Assert.AreEqual("Topping: Cheese", selectedToppingElement.TextContent.Trim());
-        Assert.AreEqual("Total Cost: 7.8", totalCostElement.TextContent.Trim());
+        Assert.AreEqual("Burger without topping: $5.4", baseBurgerCostElement.TextContent.Trim());
+        Assert.AreEqual("Free topping: 2", selectedToppingElement.TextContent.Trim());
+        Assert.AreEqual("Total Cost: $5.4", totalCostElement.TextContent.Trim());
+
+        var toppingSecondSelectElement = component.Find("select:nth-of-type(2)");
+        toppingSecondSelectElement.Change(2); // Select "Cheese (Id: 2)
+
+        Assert.AreEqual("Burger without topping: $5.4", baseBurgerCostElement.TextContent.Trim());
+        Assert.AreEqual("Total Cost: $7.8", totalCostElement.TextContent.Trim());
     }
 
     [TestMethod]
-    public async Task TestSecondToppingWithBurgerConfigurator()
+    public async Task TestThirdToppingWithBurgerConfigurator()
     {
         using var testContext = new Bunit.TestContext();
         var component = testContext.RenderComponent<OnChangeEventWithSelectDropdown.Pages.Index>();
@@ -37,14 +43,16 @@ public class BurgerConfiguratorTests
         var totalCostElement = component.Find("p:nth-of-type(3)");
         var grandTotalElement = component.Find("p:nth-of-type(4)");
 
-        Assert.AreEqual("Total Cost: 5.4", totalCostElement.TextContent.Trim());
-        Assert.AreEqual("Grand Total: 0", grandTotalElement.TextContent.Trim());
+        Assert.AreEqual("Total Cost: $5.4", totalCostElement.TextContent.Trim());
+        Assert.AreEqual("Grand Total: $0", grandTotalElement.TextContent.Trim());
 
-        var secondToppingSelectElement = component.Find("select:nth-of-type(2)");
-        secondToppingSelectElement.Change(3); // Select "Onions" (Id: 3)
+        var thirdToppingSelectElement = component.Find("select:nth-of-type(3)");
+        thirdToppingSelectElement.Change(3); // Select "Onions" (Id: 3)
         await component.InvokeAsync(() => component.Instance.selectedSecondTopping = "3");
         await component.InvokeAsync(() => component.Instance.CalculateGrandTotal());
 
-        Assert.AreEqual("Grand Total: 6.1", grandTotalElement.TextContent.Trim());
+        await Task.Delay(TimeSpan.FromSeconds(3));
+
+        Assert.AreEqual("Grand Total: $6.1", grandTotalElement.TextContent.Trim());
     }
 }
