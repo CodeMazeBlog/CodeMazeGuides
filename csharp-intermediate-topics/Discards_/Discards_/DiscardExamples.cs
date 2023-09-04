338f5f8821e2f9c767f2408a4df783dab91fb426
Rachel Okorie <jayrachel96@gmail.com>
2023-09-04 13:28:26 +0100

    feat: using discards

---
+++ csharp-intermediate-topics/Discards_/Discards_/DiscardExamples.cs
@@ -0,0 +1,31 @@
+﻿namespace Discards_
+{
+    public class DiscardExamples
+    {
+        public bool GetNumber()
+        {
+            Console.WriteLine("Enter any number on the keyboard");
+            var result = int.TryParse(Console.ReadLine(), out _);
+
+            if (!result)
+            {
+                Console.WriteLine("Not a valid number");
+            }
+
+            return result;
+        }
+
+        public void GetType(object[] objects)
+        {
+            foreach (var item in objects)
+            {
+                Console.WriteLine(item switch
+                {
+                    string => "it's a string",
+                    int => "it's an int",
+                    _ => "Neither string nor int"
+                });
+            }
+        }
+    }
+}
---
+++ csharp-intermediate-topics/Discards_/Discards_/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+namespace Discards_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var (_, _, sum) = GetSum(9, 89);
+            Console.WriteLine(sum);
+        }
+
+        static (int, int, int) GetSum(int num1, int num2)
+        {
+            return (num1, num2, num1 + num2);
+        }
+    }
+}
