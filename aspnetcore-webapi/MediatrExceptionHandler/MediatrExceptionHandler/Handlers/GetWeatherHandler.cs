aa3a714fe0fce1ad30ff1bc9cd527769a140d1cf
alintroncea <alintroncea@gmail.com>
2023-09-05 20:57:17 +0300

    Added MediatrExceptionHandler to aspnetcore-features

---
+++ aspnetcore-webapi/MediatrExceptionHandler/MediatrExceptionHandler/Handlers/GetWeatherHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using MediatrExceptionHandler.Requests;
+
+namespace MediatrExceptionHandler.Handlers
+{
+    public class GetWeatherHandler : IRequestHandler<GetWeatherRequest, IEnumerable<WeatherForecast>>
+    {
+        public async Task<IEnumerable<WeatherForecast>> Handle(GetWeatherRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ aspnetcore-webapi/MediatrExceptionHandler/MediatrExceptionHandler/Program.cs
@@ -0,0 +1,35 @@
+using MediatrExceptionHandler;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+// Middleware
+builder.Services.AddScoped<GlobalExceptionMiddleware>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseMiddleware(typeof(GlobalExceptionMiddleware));
+
+app.MapControllers();
+
+app.Run();
