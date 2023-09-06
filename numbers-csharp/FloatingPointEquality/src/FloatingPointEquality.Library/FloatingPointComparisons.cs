9a878f2bcc3c6d8721f4e55f83b40142a66b8eab
Code Maze <contact@code-maze.com>
2023-09-06 10:46:31 +0200

    CM805: Techniques for Checking Floating-Point Equality

--- numbers-csharp/FloatingPointEquality/src/FloatingPointEquality.Library/FloatingPointComparisons.cs
+++ numbers-csharp/FloatingPointEquality/src/FloatingPointEquality.Library/FloatingPointComparisons.cs
@@ -29,9 +29,11 @@ public static class FloatingPointComparisons
 
     public static bool EqualityUsingPrecision(double a, double b, int decimalPlaces)
     {
-        if (double.IsNaN(a) || double.IsNaN(b)) return false;
+        if (double.IsNaN(a) || double.IsNaN(b)) 
+            return false;
 
-        if (double.IsInfinity(a) || double.IsInfinity(b)) return a == b;
+        if (double.IsInfinity(a) || double.IsInfinity(b)) 
+            return a == b;
 
         return double.Abs(a - b) < HalfNegativePowersOf10[decimalPlaces];
     }
@@ -41,42 +43,52 @@ public static class FloatingPointComparisons
 
     public static bool EqualityUsingTolerance<T>(T a, T b, T tolerance) where T : IFloatingPointIeee754<T>
     {
-        if (T.IsNaN(a) || T.IsNaN(b)) return false;
+        if (T.IsNaN(a) || T.IsNaN(b)) 
+            return false;
 
-        if (T.IsInfinity(a) || T.IsInfinity(b)) return a == b;
+        if (T.IsInfinity(a) || T.IsInfinity(b)) 
+            return a == b;
 
         return T.Abs(a - b) < tolerance;
     }
 
     public static bool EqualityUsingMaxUnitsInLastPlace(double a, double b, long maxUnitsInLastPlace)
     {
-        if (maxUnitsInLastPlace < 1) throw new ArgumentOutOfRangeException(nameof(maxUnitsInLastPlace));
+        if (maxUnitsInLastPlace < 1) 
+            throw new ArgumentOutOfRangeException(nameof(maxUnitsInLastPlace));
 
         if (double.IsNaN(a) || double.IsNaN(b)) return false;
 
-        if (double.IsInfinity(a) || double.IsInfinity(b) || a.IsNegative() != b.IsNegative()) return a == b;
+        if (double.IsInfinity(a) || double.IsInfinity(b) || a.IsNegative() != b.IsNegative()) 
+            return a == b;
 
         return long.Abs(BitConverter.DoubleToInt64Bits(a) - BitConverter.DoubleToInt64Bits(b)) <= maxUnitsInLastPlace;
     }
 
     public static bool EqualityUsingMaxUnitsInLastPlace(float a, float b, int maxUnitsInLastPlace)
     {
-        if (maxUnitsInLastPlace < 1) throw new ArgumentOutOfRangeException(nameof(maxUnitsInLastPlace));
+        if (maxUnitsInLastPlace < 1) 
+            throw new ArgumentOutOfRangeException(nameof(maxUnitsInLastPlace));
 
-        if (float.IsNaN(a) || float.IsNaN(b)) return false;
+        if (float.IsNaN(a) || float.IsNaN(b)) 
+            return false;
 
-        if (float.IsInfinity(a) || float.IsInfinity(b) || a.IsNegative() != b.IsNegative()) return a == b;
+        if (float.IsInfinity(a) || float.IsInfinity(b) || a.IsNegative() != b.IsNegative()) 
+            return a == b;
 
         return int.Abs(BitConverter.SingleToInt32Bits(a) - BitConverter.SingleToInt32Bits(b)) <= maxUnitsInLastPlace;
     }
 
     public static bool EqualityUsingMaxUnitsInLastPlace(Half a, Half b, int maxUnitsInLastPlace)
     {
-        if (maxUnitsInLastPlace < 1) throw new ArgumentOutOfRangeException(nameof(maxUnitsInLastPlace));
+        if (maxUnitsInLastPlace < 1) 
+            throw new ArgumentOutOfRangeException(nameof(maxUnitsInLastPlace));
 
-        if (Half.IsNaN(a) || Half.IsNaN(b)) return false;
+        if (Half.IsNaN(a) || Half.IsNaN(b)) 
+            return false;
 
-        if (Half.IsInfinity(a) || Half.IsInfinity(b) || a.IsNegative() != b.IsNegative()) return a == b;
+        if (Half.IsInfinity(a) || Half.IsInfinity(b) || a.IsNegative() != b.IsNegative()) 
+            return a == b;
 
         return int.Abs(BitConverter.HalfToInt16Bits(a) - BitConverter.HalfToInt16Bits(b)) <= maxUnitsInLastPlace;
     }
