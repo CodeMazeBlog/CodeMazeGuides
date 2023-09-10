f9dca457c0ceb14bc02a6271f442607460c3079a
Koddek <codedek@gmail.com>
2023-09-10 10:06:59 -0400

    changed to top level statements

--- strings-csharp/ReplaceLineBreaksInAStringCSharp/ReplaceLineBreaksInAStringCSharp/Program.cs
+++ strings-csharp/ReplaceLineBreaksInAStringCSharp/ReplaceLineBreaksInAStringCSharp/Program.cs
@@ -1,16 +1,10 @@
-﻿namespace ReplaceLineBreaksInAStringCSharp;
+﻿using ReplaceLineBreaksInAStringCSharp;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        var stringReplaceResult = ReplaceLineBrake.ReplaceLineBreaksUsingTheStringReplaceMethod();
-        Console.WriteLine(stringReplaceResult);
+var stringReplaceResult = ReplaceLineBrake.ReplaceLineBreaksUsingTheStringReplaceMethod();
+Console.WriteLine(stringReplaceResult);
 
-        var stringReplaceLineEndingsResult = ReplaceLineBrake.ReplaceLineBreaksUsingTheStringReplaceLineEndingsMethod();
-        Console.WriteLine(stringReplaceLineEndingsResult);
+var stringReplaceLineEndingsResult = ReplaceLineBrake.ReplaceLineBreaksUsingTheStringReplaceLineEndingsMethod();
+Console.WriteLine(stringReplaceLineEndingsResult);
 
-        var regularExpressionReplaceResult = ReplaceLineBrake.ReplaceLineBreaksUsingTheRegularExpressionReplaceMethod();
-        Console.WriteLine(regularExpressionReplaceResult);
-    }
-}
+var regularExpressionReplaceResult = ReplaceLineBrake.ReplaceLineBreaksUsingTheRegularExpressionReplaceMethod();
+Console.WriteLine(regularExpressionReplaceResult);
