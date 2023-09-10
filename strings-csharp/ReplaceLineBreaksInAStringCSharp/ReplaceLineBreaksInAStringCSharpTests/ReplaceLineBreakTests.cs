0b2a414c4571111c7e3aa64d0e5dfcf93ea9eab9
Koddek <codedek@gmail.com>
2023-09-10 11:08:25 -0400

    removed the comments, which is a best practice for writing unit tests when using TDD and BDD.

--- strings-csharp/ReplaceLineBreaksInAStringCSharp/ReplaceLineBreaksInAStringCSharpTests/ReplaceLineBreakTests.cs
+++ strings-csharp/ReplaceLineBreaksInAStringCSharp/ReplaceLineBreaksInAStringCSharpTests/ReplaceLineBreakTests.cs
@@ -7,39 +7,30 @@ public class ReplaceLineBreakTests
     [Fact]
     public void WhenReplaceLineBreaksUsingTheStringReplaceMethod_ThenReturnStringOnOneLine()
     {
-        // Given
         const string expected = "This is a line. This is another line.";
 
-        // When
         var actual = ReplaceLineBrake.ReplaceLineBreaksUsingTheStringReplaceMethod();
 
-        // Then
         Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void WhenReplaceLineBreaksUsingTheStringReplaceLineEndingsMethod_ThenReturnStringWithUpdatedLineEnding()
     {
-        // Given
         const string expected = "This is a line.\nThis is another line.";
 
-        // When
         var actual = ReplaceLineBrake.ReplaceLineBreaksUsingTheStringReplaceLineEndingsMethod();
 
-        // Then
         Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void WhenReplaceLineBreaksUsingTheRegularExpressionReplaceMethod_ThenReturnStringWithUpdatedLineEnding()
     {
-        // Given
         const string expected = "This is a line.&lt;br /&gt;This is another line.";
 
-        // When
         var actual = ReplaceLineBrake.ReplaceLineBreaksUsingTheRegularExpressionReplaceMethod();
 
-        // Then
         Assert.Equal(expected, actual);
     }
 }
