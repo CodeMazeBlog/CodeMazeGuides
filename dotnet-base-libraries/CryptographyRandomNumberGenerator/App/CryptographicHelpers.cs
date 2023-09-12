d908c64b5f6580e2a14bb2eb043e5e71ab1792ec
André Carvalho <carlos.carvalho83@gmail.com>
2023-09-12 12:22:26 +0100

    Update CryptographicHelpers.cs

--- dotnet-base-libraries/CryptographyRandomNumberGenerator/App/CryptographicHelpers.cs
+++ dotnet-base-libraries/CryptographyRandomNumberGenerator/App/CryptographicHelpers.cs
@@ -5,8 +5,10 @@ namespace App;
 public static class CryptographicHelpers
 {
     private static readonly Random psuedoRandomNumberGenerator = new(10);
-    public static byte[] GenerateRandomKey(int bytesCount) => RandomNumberGenerator.GetBytes(bytesCount);
-    public static int GenerateRandomInteger(int minValue, int maxValue) => RandomNumberGenerator.GetInt32(minValue, maxValue);
-    public static int GeneratePseudoRandom(int minValue, int maxValue) => psuedoRandomNumberGenerator.Next(minValue, maxValue);
     
+    public static byte[] GenerateRandomKey(int bytesCount) => RandomNumberGenerator.GetBytes(bytesCount);
+    
+    public static int GenerateRandomInteger(int minValue, int maxValue) => RandomNumberGenerator.GetInt32(minValue, maxValue);
+    
+    public static int GeneratePseudoRandom(int minValue, int maxValue) => psuedoRandomNumberGenerator.Next(minValue, maxValue);
 }
