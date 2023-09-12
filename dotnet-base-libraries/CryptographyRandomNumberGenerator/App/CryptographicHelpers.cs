346f374cb3cc811e38c518fe49941fa2af1b4d06
goldytech <muhammadafzal.qureshi@gmail.com>
2023-09-12 16:12:51 +0530

    Code refactor to initialize Random only once.

--- dotnet-base-libraries/CryptographyRandomNumberGenerator/App/CryptographicHelpers.cs
+++ dotnet-base-libraries/CryptographyRandomNumberGenerator/App/CryptographicHelpers.cs
@@ -4,8 +4,9 @@ namespace App;
 
 public static class CryptographicHelpers
 {
+    private static readonly Random psuedoRandomNumberGenerator = new(10);
     public static byte[] GenerateRandomKey(int bytesCount) => RandomNumberGenerator.GetBytes(bytesCount);
     public static int GenerateRandomInteger(int minValue, int maxValue) => RandomNumberGenerator.GetInt32(minValue, maxValue);
-    public static int GeneratePseudoRandom(int minValue, int maxValue) => new Random().Next(minValue, maxValue);
+    public static int GeneratePseudoRandom(int minValue, int maxValue) => psuedoRandomNumberGenerator.Next(minValue, maxValue);
     
 }
