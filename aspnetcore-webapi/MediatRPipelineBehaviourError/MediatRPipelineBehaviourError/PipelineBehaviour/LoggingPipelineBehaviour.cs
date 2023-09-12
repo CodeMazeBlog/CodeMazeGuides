958f78a2122b92bfc3213614dd1f0087dc1e9a99
Aneta Muslić <Aneta.Muslic@blu.com>
2023-09-12 17:02:38 +0200

    formatting

--- aspnetcore-webapi/MediatRPipelineBehaviourError/MediatRPipelineBehaviourError/PipelineBehaviour/LoggingPipelineBehaviour.cs
+++ aspnetcore-webapi/MediatRPipelineBehaviourError/MediatRPipelineBehaviourError/PipelineBehaviour/LoggingPipelineBehaviour.cs
@@ -11,7 +11,10 @@ public class LoggingPipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<T
         _logger = logger;
     }
 
-    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
     {
         _logger.LogInformation("Handling {RequestType} - {RequestData}", typeof(TRequest).Name, request);
 
