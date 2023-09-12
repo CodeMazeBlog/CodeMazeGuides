4f5a47aa1cca8141f6e18f99ab7e657b9632cc6b
goldytech <muhammadafzal.qureshi@gmail.com>
2023-09-12 12:54:58 +0530

    Added Benchmarks

--- dotnet-base-libraries/CryptographyRandomNumberGenerator/App/Program.cs
+++ dotnet-base-libraries/CryptographyRandomNumberGenerator/App/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using App;
+using BenchmarkDotNet.Running;
 
 Console.WriteLine(Convert.ToBase64String(CryptographicHelpers.GenerateRandomKey(32)));
 Console.WriteLine(CryptographicHelpers.GenerateRandomInteger(1, 100));
+var summary = BenchmarkRunner.Run<RandomNumberBenchmarks>();
---
+++ dotnet-base-libraries/CryptographyRandomNumberGenerator/App/RandomNumberBenchmarks.cs
@@ -0,0 +1,21 @@
+using BenchmarkDotNet.Attributes;
+
+namespace App;
+
+public class RandomNumberBenchmarks
+{
+    [Benchmark]
+    public void UsingRealRandomNumberGenerator()
+    {
+        var randomNumber = CryptographicHelpers.GenerateRandomInteger(1, 100);
+    }
+    
+    [Benchmark]
+    public void UsingPseudoRandomNumberGenerator()
+    {
+        var randomNumber = CryptographicHelpers.GeneratePseudoRandom(1, 100);
+    }
+    
+    
+    
+}
