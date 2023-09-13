b8064620650184e927140b4e50171a3f0a50ae3d
Igor Nunes Gervásio <igorngervasio@gmail.com>
2023-09-13 01:06:12 -0300

    CM-793: Externalizing methods to a new class

--- xml-csharp/SelectingXmlNodesWithXpath/SelectingXmlNodesWithXpath/Program.cs
+++ xml-csharp/SelectingXmlNodesWithXpath/SelectingXmlNodesWithXpath/Program.cs
@@ -4,42 +4,6 @@ namespace SelectingXmlNodesWithXpath;
 
 internal class Program
 {
-    static void SelectSingleBook(XmlNode root)
-    {
-        var node = root.SelectSingleNode("//catalog/book[position()=2]");
-        if (node != null)
-        {
-            Console.WriteLine($"Book: {node.OuterXml}");
-        }
-    }
-
-    static void SelectBooks(XmlNode root)
-    {
-        var nodes = root.SelectNodes("//catalog/book[price<50.00]");
-        if (nodes?.Count > 0)
-        {
-            for (int i = 0; i < nodes.Count; i++)
-            {
-                Console.WriteLine($"Book: {nodes[i]?.OuterXml}");
-            }
-        }
-    }
-
-    static void SelectBooksUsingNamespaces(XmlDocument doc)
-    {
-        XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
-        nsmgr.AddNamespace("ex", "urn:example-schema");
-        
-        var nodes = doc.SelectNodes("descendant::ex:book", nsmgr);
-        if (nodes?.Count > 0)
-        {
-            for (int i = 0; i < nodes.Count; i++)
-            {
-                Console.WriteLine($"Book: {nodes[i]?.OuterXml}");
-            }
-        }
-    }
-
     static void Main(string[] args)
     {
         var doc = new XmlDocument();
@@ -47,12 +11,12 @@ internal class Program
         var root = doc.DocumentElement;
 
         Console.WriteLine("Let's select a single book:");
-        SelectSingleBook(root);
+        XmlNodesSelector.SelectSingleBook(root);
 
         Console.WriteLine("\nLet's select a few books:");
-        SelectBooks(root);
+        XmlNodesSelector.SelectBooks(root);
 
         Console.WriteLine("\nLet's select a single book using XML namespaces:");
-        SelectBooksUsingNamespaces(doc);
+        XmlNodesSelector.SelectBooksUsingNamespaces(doc);
     }
 }
---
+++ xml-csharp/SelectingXmlNodesWithXpath/SelectingXmlNodesWithXpath/XmlNodesSelector.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml;
+
+namespace SelectingXmlNodesWithXpath;
+
+internal static class XmlNodesSelector
+{
+    public static void SelectSingleBook(XmlNode root)
+    {
+        var node = root.SelectSingleNode("//catalog/book[position()=2]");
+        if (node != null)
+        {
+            Console.WriteLine($"Book: {node.OuterXml}");
+        }
+    }
+
+    public static void SelectBooks(XmlNode root)
+    {
+        var nodes = root.SelectNodes("//catalog/book[price<50.00]");
+        if (nodes?.Count > 0)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Console.WriteLine($"Book: {nodes[i]?.OuterXml}");
+            }
+        }
+    }
+
+    public static void SelectBooksUsingNamespaces(XmlDocument doc)
+    {
+        XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+        nsmgr.AddNamespace("ex", "urn:example-schema");
+
+        var nodes = doc.SelectNodes("descendant::ex:book", nsmgr);
+        if (nodes?.Count > 0)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Console.WriteLine($"Book: {nodes[i]?.OuterXml}");
+            }
+        }
+    }
+}
