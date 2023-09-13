a7c582c3f65b2aab94f888378c056bfa3cd1c3f7
goldytech <muhammadafzal.qureshi@gmail.com>
2023-09-13 18:43:07 +0530

    Applied the Orderer Attribute

--- dotnet-base-libraries/CryptographyRandomNumberGenerator/App/RandomNumberBenchmarks.cs
+++ dotnet-base-libraries/CryptographyRandomNumberGenerator/App/RandomNumberBenchmarks.cs
@@ -1,12 +1,14 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
 
 namespace App;
 
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
 public class RandomNumberBenchmarks
 {
     [Benchmark]
     public int UsingRealRandomNumberGenerator() => CryptographicHelpers.GenerateRandomInteger(1, 100);
-    
+
     [Benchmark]
     public int UsingPseudoRandomNumberGenerator() => CryptographicHelpers.GeneratePseudoRandom(1, 100);
 }
