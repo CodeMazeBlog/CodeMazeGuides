855a4cfffbd57d1a5a9fa9e49722d5e64570ce7f
Tosin <tosino@examroom.ai>
2023-09-18 22:12:18 +0100

    Sample Code and Unit Tests, Action and Func Delegates in C#

---
+++ csharp-intermediate-topics/ActionFuncDelegates/ActionFuncDelegate/ActionFuncDelegate/Program.cs
@@ -0,0 +1,52 @@
+﻿using Actions;
+
+namespace Actions_and_Funcs
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            ShippingProcessor processor = new ShippingProcessor();
+
+            Action<decimal> standardCostCalculator = (total) =>
+            {
+                decimal standardCost = total * 0.02m; // Assuming a flat rate of 2% of total cost
+                Console.WriteLine($"Standard Shipping Cost: {standardCost:C}");
+            };
+
+            Action<decimal> expressCostCalculator = (total) =>
+            {
+                decimal expressCost = total * 0.05m; // Assuming a flat rate of 5% of total cost
+                Console.WriteLine($"Express Shipping Cost: {expressCost:C}");
+            };
+
+
+            decimal weight = 10m; // in kilograms
+            decimal distance = 100m; // in kilometers
+
+            // Process shipping with standard cost calculation
+            processor.ProcessShipping(weight, distance, standardCostCalculator);
+
+            // Process shipping with express cost calculation
+            processor.ProcessShipping(weight, distance, expressCostCalculator);
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ csharp-intermediate-topics/ActionFuncDelegates/ActionFuncDelegate/Funcs/ShippingProcessor.cs
@@ -0,0 +1,12 @@
+﻿namespace Funcs
+{
+        public class ShippingProcessor
+        {
+            public void ProcessShipping(decimal weight, decimal distance, Func<decimal, decimal> calculateCost)
+            {
+                decimal totalCost = calculateCost(weight * distance);
+                Console.WriteLine($"Shipping Cost: {totalCost:C}");
+            }
+        }
+
+}
