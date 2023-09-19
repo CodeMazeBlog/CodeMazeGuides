02af21ea5cffb8adfc6fd24a4c27140b0537a96c
Tosin <tosino@examroom.ai>
2023-09-19 12:54:03 +0100

    Actions and Funcs Delegates C#

---
+++ csharp-intermediate-topics/ActionFuncDelegate/ActionFuncDelegate/ShippingProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace Actions
+{
+    public class ShippingProcessor
+    {
+        public void ProcessShipping(decimal weight, decimal distance, Action<decimal> calculateCost)
+        {
+            calculateCost(weight * distance);
+        }
+
+    }
+}
---
+++ csharp-intermediate-topics/ActionFuncDelegate/Funcs/Program.cs
@@ -0,0 +1,30 @@
+﻿using Funcs;
+
+class Program
+{
+
+    static void Main()
+    {
+        ShippingProcessor processor = new ShippingProcessor();
+
+        Func<decimal, decimal> standardCostCalculator = (total) =>
+        {
+            return total * 0.02m; // Assuming a flat rate of 2% of total cost
+        };
+
+        Func<decimal, decimal> expressCostCalculator = (total) =>
+        {
+            return total * 0.05m; // Assuming a flat rate of 5% of total cost
+        };
+
+
+        decimal distance = 10; // in kilometers
+        decimal weight = 100; // in kilograms
+
+        // Process shipping with standard cost calculation
+        processor.ProcessShipping(weight, distance, standardCostCalculator);
+
+        // Process shipping with express cost calculation
+        processor.ProcessShipping(weight, distance, expressCostCalculator);
+    }
+}
