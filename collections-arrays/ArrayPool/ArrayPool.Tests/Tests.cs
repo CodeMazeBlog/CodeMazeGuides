569059da5441742ff3f69ca79dd2f8c5c2094169
KonstantinosP7 <kwnbet95@gmail.com>
2023-09-20 17:59:10 +0300

    CM-798 New line before Assert in Test methods.

--- collections-arrays/ArrayPool/ArrayPool.Tests/Tests.cs
+++ collections-arrays/ArrayPool/ArrayPool.Tests/Tests.cs
@@ -6,6 +6,7 @@ public class Tests
     public void UsingArrayPoolShared_WhenIntType_ThenSuccessfulInitialization()
     {
         var defaultPool = Methods.GetDefaultArrayPool<int>();
+
         Assert.That(defaultPool, Is.Not.EqualTo(null));
     }
 
@@ -13,6 +14,7 @@ public class Tests
     public void UsingArrayPoolShared_WhenByteType_ThenSuccessfulInitialization()
     {
         var defaultPool = Methods.GetDefaultArrayPool<byte>();
+
         Assert.That(defaultPool, Is.Not.EqualTo(null));
     }
 
@@ -20,6 +22,7 @@ public class Tests
     public void UsingArrayPoolShared_WhenStringType_ThenSuccessfulInitialization()
     {
         var defaultPool = Methods.GetDefaultArrayPool<string>();
+
         Assert.That(defaultPool, Is.Not.EqualTo(null));
     }
 
@@ -27,6 +30,7 @@ public class Tests
     public void UsingCustomArrayPool_WhenIntType_ThenSuccessfulInitialization()
     {
         var defaultPool = Methods.CreateArrayPool<int>(10, 5);
+
         Assert.That(defaultPool, Is.Not.EqualTo(null));
     }
 
@@ -34,6 +38,7 @@ public class Tests
     public void UsingCustomArrayPool_WhenByteType_ThenSuccessfulInitialization()
     {
         var defaultPool = Methods.CreateArrayPool<byte>(10, 5);
+
         Assert.That(defaultPool, Is.Not.EqualTo(null));
     }
 
@@ -41,6 +46,7 @@ public class Tests
     public void UsingCustomArrayPool_WhenStringType_ThenSuccessfulInitialization()
     {
         var defaultPool = Methods.CreateArrayPool<string>(10, 5);
+
         Assert.That(defaultPool, Is.Not.EqualTo(null));
     }
 }
