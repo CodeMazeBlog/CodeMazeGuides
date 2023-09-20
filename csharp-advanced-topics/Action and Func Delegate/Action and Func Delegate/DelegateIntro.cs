b457cd63e9f4b6d317719668c5fc490b3d0badbd
stephen eduke <stephen.eduke@vnicomhub.com>
2023-09-20 17:25:55 +0100

    Added Action and Func Delegate

---
+++ csharp-advanced-topics/Action and Func Delegate/Action and Func Delegate/DelegateIntro.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_and_Func_Delegate
+{
+    public delegate void TestDelegate();
+    public delegate void CalculatorDelegate(int a, int b);
+}
