59e3a045255ba9c0b3cbf1a0c05089a54801f7a2
jshergal <jeff.shergalis@gmail.com>
2023-09-20 22:04:19 +0300

    Formatting change related to if blocks. Renamed source to input for clarity

--- collections-arrays/ConvertHexStringToByteArray/src/ConvertHexStringToByteArrayLibrary/ConversionHelpers.cs
+++ collections-arrays/ConvertHexStringToByteArray/src/ConvertHexStringToByteArrayLibrary/ConversionHelpers.cs
@@ -7,18 +7,21 @@ public static class ConversionHelpers
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static int PerformModularArithmeticCalculation(char value) => (value % 32 + 9) % 25;
 
-    public static byte[] FromHexWithModularArithmetic(ReadOnlySpan<char> source)
+    public static byte[] FromHexWithModularArithmetic(ReadOnlySpan<char> input)
     {
-        if (source.StartsWith("0x")) source = source[2..];
+        if (input.StartsWith("0x"))
+            input = input[2..];
 
-        if (source.IsEmpty) return Array.Empty<byte>();
+        if (input.IsEmpty)
+            return Array.Empty<byte>();
 
-        if (source.Length % 2 != 0) throw new ArgumentException("Invalid hex string", nameof(source));
+        if (input.Length % 2 != 0)
+            throw new ArgumentException("Input has invalid length", nameof(input));
 
-        var dest = new byte[source.Length >> 1];
+        var dest = new byte[input.Length >> 1];
         for (int i = 0, j = 0; j < dest.Length; i += 2, j++)
-            dest[j] = (byte) ((PerformModularArithmeticCalculation(source[i]) << 4) +
-                PerformModularArithmeticCalculation(source[i + 1]));
+            dest[j] = (byte) ((PerformModularArithmeticCalculation(input[i]) << 4) +
+                PerformModularArithmeticCalculation(input[i + 1]));
 
         return dest;
     }
@@ -33,16 +36,19 @@ public static class ConversionHelpers
             _ => throw new ArgumentException($"Invalid hex digit: '{hexChar}'", nameof(hexChar))
         };
 
-    public static unsafe byte[] FromHexWithSwitchComputation(ReadOnlySpan<char> source)
+    public static unsafe byte[] FromHexWithSwitchComputation(ReadOnlySpan<char> input)
     {
-        if (source.StartsWith("0x")) source = source[2..];
+        if (input.StartsWith("0x"))
+            input = input[2..];
 
-        if (source.IsEmpty) return Array.Empty<byte>();
+        if (input.IsEmpty)
+            return Array.Empty<byte>();
 
-        if (source.Length % 2 != 0) throw new ArgumentException("Invalid hex string", nameof(source));
+        if (input.Length % 2 != 0)
+            throw new ArgumentException("Input has invalid length", nameof(input));
 
-        var dest = new byte[source.Length >> 1];
-        fixed (char* s = source)
+        var dest = new byte[input.Length >> 1];
+        fixed (char* s = input)
         fixed (byte* d = dest)
         {
             var destPtr = d;
@@ -66,16 +72,19 @@ public static class ConversionHelpers
         return value + 10 + ((value >> 31) & 7);
     }
 
-    public static unsafe byte[] FromHexWithBitManipulation(ReadOnlySpan<char> source)
+    public static unsafe byte[] FromHexWithBitManipulation(ReadOnlySpan<char> input)
     {
-        if (source.StartsWith("0x")) source = source[2..];
+        if (input.StartsWith("0x"))
+            input = input[2..];
 
-        if (source.IsEmpty) return Array.Empty<byte>();
+        if (input.IsEmpty)
+            return Array.Empty<byte>();
 
-        if (source.Length % 2 != 0) throw new ArgumentException("Invalid hex string", nameof(source));
+        if (input.Length % 2 != 0)
+            throw new ArgumentException("Input has invalid length", nameof(input));
 
-        var dest = new byte[source.Length >> 1];
-        fixed (char* srcPtr = source)
+        var dest = new byte[input.Length >> 1];
+        fixed (char* srcPtr = input)
         fixed (byte* destPtr = dest)
         {
             var sPtr = &srcPtr[0];
@@ -91,28 +100,32 @@ public static class ConversionHelpers
         return dest;
     }
 
-    public static byte[] FromHexWithConvert(ReadOnlySpan<char> source)
+    public static byte[] FromHexWithConvert(ReadOnlySpan<char> input)
     {
-        if (source.StartsWith("0x")) source = source[2..];
+        if (input.StartsWith("0x"))
+            input = input[2..];
 
-        return Convert.FromHexString(source);
+        return Convert.FromHexString(input);
     }
 
-    public static unsafe byte[] FromHexWithLookup(ReadOnlySpan<char> source)
+    public static unsafe byte[] FromHexWithLookup(ReadOnlySpan<char> input)
     {
-        if (source.StartsWith("0x")) source = source[2..];
+        if (input.StartsWith("0x"))
+            input = input[2..];
 
-        if (source.IsEmpty) return Array.Empty<byte>();
+        if (input.IsEmpty)
+            return Array.Empty<byte>();
 
-        if (source.Length % 2 == 1) throw new ArgumentException("Source has invalid length", nameof(source));
+        if (input.Length % 2 == 1)
+            throw new ArgumentException("Input has invalid length", nameof(input));
 
-        var dest = new byte[source.Length >> 1];
-        fixed (char* sourceRef = source)
+        var dest = new byte[input.Length >> 1];
+        fixed (char* inputRef = input)
         fixed (byte* hiRef = LookupTables.FromHexHighBitsLookup)
         fixed (byte* lowRef = LookupTables.FromHexLowBitsLookup)
         fixed (byte* destRef = dest)
         {
-            var s = &sourceRef[0];
+            var s = &inputRef[0];
             var d = destRef;
 
             while (*s != 0)
@@ -122,7 +135,7 @@ public static class ConversionHelpers
                 if (*s > 102 || (*d = hiRef[*s++]) == 255 ||
                     *s > 102 || (lowValue = lowRef[*s++]) == 255
                 )
-                    throw new ArgumentException($"Invalid character found in string: '{*s}'", nameof(source));
+                    throw new ArgumentException($"Invalid character found in string: '{*s}'", nameof(input));
 
                 *d++ += lowValue;
             }
