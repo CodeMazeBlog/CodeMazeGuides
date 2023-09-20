2e4da992fe880c7aa335cd1931028398a8dffd66
king-Alex-d-great <ogubuikealex@gmail.com>
2023-09-20 11:19:10 +0100

    Chore: Make Ids nullable

--- aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Models/Course.cs
+++ aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Models/Course.cs
@@ -8,7 +8,7 @@ public class Course
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [Required(ErrorMessage = "Course name is required")]
     public string Name { get; set; }
--- aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Models/Student.cs
+++ aspnetcore-webapi/GettingStartedASPNETMongoDB/GettingStartedASPNETMongoDB/Models/Student.cs
@@ -8,7 +8,7 @@ public class Student
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [Required(ErrorMessage = "First name is required")]
     public string FirstName { get; set; }
