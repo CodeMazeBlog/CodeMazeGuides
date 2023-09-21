1f6077edd296e01ee0f288d2a4605c7260514748
TatendaBeta <54982151+TatendaBeta@users.noreply.github.com>
2023-09-21 15:23:29 +0200

    Add files via upload

---
+++ Action and Func Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_and_Func_Delegates
+{
+    class MovieDirector
+    {
+        static void Main()
+        {
+            Action<string> actionScene = (action) => Console.WriteLine($"Action: {action}");
+            // Let's direct our actors!
+            actionScene("Explosion"); // Action: Explosion
+            actionScene("Car Chase"); // Action: Car Chase
+            Console.ReadKey();
+
+
+            //implementing Func
+
+            Func<string, string> performScene = (emotion) => $"Actor performs: {emotion}";
+            // Cue the actors!
+            string performance = performScene("Romantic Monologue");
+            Console.WriteLine(performance); // Actor performs: Romantic Monologue
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Action and Func Delegates/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Action and Func Delegates")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Action and Func Delegates")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f0d037e-0e07-418f-85c9-e1b29b0cd3cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
