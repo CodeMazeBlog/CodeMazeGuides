cca9a798fbc8d30868cb3df9cde09bb3f46aaaf7
techie-Stephen <ohienstephen@gmail.com>
2023-09-21 15:18:05 +0100

    inital commit

---
+++ csharp-advanced-topics/Action and Func Delegate/Action and Func Delegate/DelegateWithReturnType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_and_Func_Delegate
+{
+    public delegate int CalculateDelegate(int a, int b);
+
+    public static class DelegateMethod
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
