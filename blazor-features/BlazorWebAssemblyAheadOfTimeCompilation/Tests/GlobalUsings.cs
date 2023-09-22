a33fd460900edff8c7a88dee9fe792a180a15629
cjaynjoku <njokujennaro@gmail.com>
2023-09-22 14:44:13 +0100

    CM-175: Blazor WebAssembly Ahead of Time Compilation (AoT)

---
+++ blazor-features/BlazorWebAssemblyAheadOfTimeCompilation/Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using BlazorWebAssemblyAppForAheadOfTimeCompilation;
+global using BlazorWebAssemblyAppForAheadOfTimeCompilation.Pages;
+global using Moq;
+global using Bunit;
