afdb11b937a1c6c02629351b1281bd6513fb629a
Tosin <tosino@examroom.ai>
2023-09-22 16:44:04 +0100

    Actions and Funcs Code and Unit Test

---
+++ csharp-advanced-topics/ActionFuncDelegate/ActionFuncDelegate/Action.cs
@@ -0,0 +1,27 @@
+﻿var processor = new ShippingProcessor();
+
+Action<decimal> standardCostCalculator = (total) =>
+{
+    decimal standardCost = total * 0.02m; // Assuming a flat rate of 2% of total cost
+    Console.WriteLine($"Standard Shipping Cost: {standardCost:C}");
+};
+
+Action<decimal> expressCostCalculator = (total) =>
+{
+    decimal expressCost = total * 0.05m; // Assuming a flat rate of 5% of total cost
+    Console.WriteLine($"Express Shipping Cost: {expressCost:C}");
+};
+    decimal weight = 10m; // in kilograms
+    decimal distance = 100m; // in kilometers
+    // Process shipping with standard cost calculation
+    processor.ProcessShipping(weight, distance, standardCostCalculator);
+    // Process shipping with express cost calculation
+    processor.ProcessShipping(weight, distance, expressCostCalculator);
+public class ShippingProcessor
+{
+    public void ProcessShipping(decimal weight, decimal distance, Action<decimal> calculateCost)
+    {
+        calculateCost(weight * distance);
+    }
+}
+
---
+++ csharp-advanced-topics/ActionFuncDelegate/ActionUnitTest/ActionUnitTest.cs
@@ -0,0 +1,34 @@
+[TestClass]
+public class ActionUnitTest
+{
+        [TestMethod]
+        public void WhenProcessingShippingWithStandardCostCalculator_ThenReturnCorrectCost()
+        {
+            // Arrange
+            ShippingProcessor processor = new ShippingProcessor();
+            decimal weight = 10; // in kilograms
+            decimal distance = 100; // in kilometers
+            decimal expectedCost = weight * distance * 0.02m; // Expected cost using standard cost calculator
+            // Act
+            decimal actualCost = 0;
+            processor.ProcessShipping(weight, distance, (total) => actualCost = total * 0.02m);
+           // Assert
+            Assert.AreEqual(expectedCost, actualCost, "Shipping cost is not calculated correctly using standard cost calculator.");
+        }
+        [TestMethod]
+        public void WhenProcessingShippingWithExpressCostCalculator_ThenReturnCorrectCost()
+        {
+            // Arrange
+            ShippingProcessor processor = new ShippingProcessor();
+            decimal weight = 10; // in kilograms
+            decimal distance = 100; // in kilometers
+            decimal expectedCost = weight * distance * 0.05m; // Expected cost using express cost calculator
+            // Act
+            decimal actualCost = 0;
+            processor.ProcessShipping(weight, distance, (total) => actualCost = total * 0.05m);
+            // Assert
+            Assert.AreEqual(expectedCost, actualCost, "Shipping cost is not calculated correctly using express cost calculator.");
+        }
+}
+
+
