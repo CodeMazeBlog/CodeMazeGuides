ad683d5960f0901f903767551f4d1951365f15f6
Martin Chege <martinchege324@gmail.com>
2023-09-25 06:19:50 +0300

    Initial setup

--- threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/PeriodicTimerMethods.cs
+++ threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/PeriodicTimerMethods.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace PeriodicTimerCSharp
 {
     public class PeriodicTimerMethods
     {
+        public static PeriodicTimer CreateRandomArray(int size)
+        {
+            var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+            
+            var array = new int[size];
+
+            for (int i = 0; i < size; i++)
+                array[i] = Random.Shared.Next();
+
+            return timer;
+        }
     }
 }
