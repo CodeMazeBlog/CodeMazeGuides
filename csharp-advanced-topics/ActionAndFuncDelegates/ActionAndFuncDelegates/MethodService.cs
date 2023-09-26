50ca8740497e172adef9adf64846c6e0da8572a9
Arpit poddar <arpitpoddar11@gmail.com>
2023-09-27 01:48:11 +0530

    ActionAncFuncDelegates [Arpit_P11]

---
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/MethodService.cs
@@ -0,0 +1,39 @@
+﻿namespace ActionAndFuncDelegates
+{
+    public class MethodService
+    {
+        //****************Action Delegate will either have input parameters but returns no output*****************
+        public static void GetConsoleText()
+        {
+            Console.WriteLine("Console text printed");
+        }
+        public static void GetUserDefinedText(string message)
+        {
+            Console.WriteLine("User Defined Text - " + message);
+        }
+
+        public static void GetUserDefinedNumbers(int start, int target)
+        {
+            Console.Write("User Defined Numbers - ");
+            for (int i = start; i <= target; i++)
+            {
+                Console.Write(" {0}", i);
+            }
+            Console.WriteLine();
+        }
+
+        //****************Func Delegate will takes parameters and returns a output***************
+        public static string GetNamesReturn(string FirstName, string SecondName, string ThirdName)
+        {
+            return string.Format("{0}-{1}-{2}", 
+                                  FirstName,
+                                  SecondName,
+                                  ThirdName);
+        }
+        public static int GetRandomNumber()
+        { 
+            Random r = new();
+            return r.Next(1, 100);
+        }
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using ActionAndFuncDelegates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // passing all method directly that needs to be called,as it reduces more code spaces and looks more readable
+       
+        Action PrintConsoleText = MethodService.GetConsoleText;
+        Action<string> PrintUserDefinedText = MethodService.GetUserDefinedText;
+        Action<int, int> PrintUserDefinedNumbers = MethodService.GetUserDefinedNumbers;
+
+        Func<int> PrintRandomNumber = MethodService.GetRandomNumber;
+        Func<string, string,string, string> PrintReturnNames = MethodService.GetNamesReturn;
+
+        Console.WriteLine("\n*** Action<T> Delegate without return value ***************\n");
+
+        PrintConsoleText(); // without any input, no output expected
+        PrintUserDefinedText("Example Text"); // input is there,it will only print the text with that input 
+        PrintUserDefinedNumbers(1, 10); // inputs are there,it will only print the numbers.
+        
+        Console.WriteLine("\n***** Func<T> Delegate with return value expected ************\n");
+
+        Console.WriteLine("Random Number is: {0}", PrintRandomNumber()); // without any input,return value integer
+        Console.WriteLine("Concatenated Names - " + PrintReturnNames("First", "Second", "Third")); // with inputs,return concatenated string
+        Console.ReadLine();
+    }
+}
