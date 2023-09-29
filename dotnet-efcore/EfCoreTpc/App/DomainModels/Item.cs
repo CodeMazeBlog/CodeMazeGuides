82831f68a67fae02520aef19b23c5d641d161298
goldytech <afzal.qureshi@outlook.com>
2023-09-29 18:23:01 +0530

    Pr feedback

--- dotnet-efcore/EfCoreTpc/App/DomainModels/Item.cs
+++ dotnet-efcore/EfCoreTpc/App/DomainModels/Item.cs
@@ -2,7 +2,7 @@ namespace App.DomainModels;
 
 public abstract class Item
 {
-    public Guid Id { get; set; }
+    public required Guid Id { get; set; }
     public required string Description { get; set; }
-    public decimal Price { get; set; }
+    public required decimal Price { get; set; }
 }
