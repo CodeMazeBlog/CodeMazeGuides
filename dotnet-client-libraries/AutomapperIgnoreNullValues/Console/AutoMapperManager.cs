c1ff3bb0d8cbaf171382f8f05f4cd334574a4930
asetil <osman.sokuoglu@gmail.com>
2023-10-01 16:09:11 +0300

    [CM-344][osman.sokuoglu@gmail.com][How to Ignore Null Values for All Source Members with Automapper]

---
+++ dotnet-client-libraries/AutomapperIgnoreNullValues/Console/AutoMapperManager.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+
+namespace AutoMapperIgnoreNullValues;
+
+public class AutoMapperManager
+{
+    private const string DepartmentIsNullMessage = "Department is null";
+
+    public static StudentEntity UpdateStudent(StudentItemDto source)
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<DefaultMappingProfile>();
+        });
+
+        IMapper mapper = config.CreateMapper();
+        var destination = GetSampleEntity();
+        destination = mapper.Map(source, destination);
+
+        if (destination.Department == null)
+            throw new Exception(DepartmentIsNullMessage);
+
+        return destination;
+    }
+
+    public static StudentEntity UpdateStudentIgnoreNullValues(StudentItemDto source)
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<IgnoreNullMappingProfile>();
+        });
+
+        IMapper mapper = config.CreateMapper();
+        var destination = GetSampleEntity();
+        destination = mapper.Map(source, destination);
+
+        if (destination.Department == null)
+            throw new Exception(DepartmentIsNullMessage);
+
+        return destination;
+    }
+
+    public static StudentEntity GetSampleEntity()
+    {
+        var entity = new StudentEntity()
+        {
+            Id = 101,
+            Name = "Sample",
+            Age = 29,
+            Department = "Computer Engineering",
+            Grades = new List<decimal>() { 2.36M, 2.72M, 3.06M },
+            University = new University() { Name = "Bosphorus University" }
+        };
+
+        return entity;
+    }
+}
---
+++ dotnet-client-libraries/AutomapperIgnoreNullValues/Console/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoMapperIgnoreNullValues;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var source = new StudentItemDto()
+        {
+            Name = "Test",
+            AgeInfo = 29,
+            University = null,
+            Grades = null
+        };
+
+        AutoMapperManager.UpdateStudent(source);
+
+        AutoMapperManager.UpdateStudentIgnoreNullValues(source);
+
+        Console.ReadLine();
+    }
+}
