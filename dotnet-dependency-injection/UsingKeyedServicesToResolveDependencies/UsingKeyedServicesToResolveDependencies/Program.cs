b63384dab7ee03c98b269cf655b42881ba5abff8
Ivan Gechev <ivan_banov@abv.bg>
2023-10-01 10:26:05 +0300

    Reviewer-suggested tweaks

--- dotnet-dependency-injection/UsingKeyedServicesToResolveDependencies/UsingKeyedServicesToResolveDependencies/Program.cs
+++ dotnet-dependency-injection/UsingKeyedServicesToResolveDependencies/UsingKeyedServicesToResolveDependencies/Program.cs
@@ -1,46 +1,38 @@
-using UsingKeyedServicesToResolveDependencies.Services;
 using UsingKeyedServicesToResolveDependencies.Services.Abstractions;
+using UsingKeyedServicesToResolveDependencies.Services;
 
-namespace UsingKeyedServicesToResolveDependencies;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddKeyedScoped<IEventService, OnlineEventService>("online");
+builder.Services.AddKeyedScoped<IEventService, InPersonEventService>("in-person");
+
+builder.Services.AddScoped<OnlineEventProducer>();
+builder.Services.AddScoped<InPersonEventProducer>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddKeyedScoped<IEventService, OnlineEventService>("online");
-        builder.Services.AddKeyedScoped<IEventService, InPersonEventService>("in-person");
-
-        builder.Services.AddScoped<OnlineEventProducer>();
-        builder.Services.AddScoped<InPersonEventProducer>();
-
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.MapGet("/attendOnlineEvent", (OnlineEventProducer eventProducer) =>
-        {
-            return eventProducer.ProduceEvent();
-        });
-
-        app.MapGet("/attendInPersonEvent", (InPersonEventProducer eventProducer) =>
-        {
-            return eventProducer.ProduceEvent();
-        });
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapGet("/attendOnlineEvent", (OnlineEventProducer eventProducer) =>
+{
+    return eventProducer.ProduceEvent();
+});
+
+app.MapGet("/attendInPersonEvent", (InPersonEventProducer eventProducer) =>
+{
+    return eventProducer.ProduceEvent();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
