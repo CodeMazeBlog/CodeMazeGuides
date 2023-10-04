7068442be4c5a2e13261794aebecf34ca3d95154
Sam <113020810+samhascat@users.noreply.github.com>
2023-10-04 16:04:41 +0100

    final

--- csharp-basic-topics/ActionAndFuncDelegates/Program.cs
+++ csharp-basic-topics/ActionAndFuncDelegates/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CodeMazeGuides.Delegates
 {
     class Program
     {
-        static void Main()
+        // class used for Func delegate example
+        public class Cake
         {
-            Console.WriteLine("Test");
+            public static double CalculateShare(int slices, int share)
+            {
+                double myCake = slices * (share * 0.01);
+                return Math.Round(myCake, 2);
+            }
+
+            public static double CalculateRemainingShare (double slices, int guests)
+            {
+                double remCake = slices / guests;
+                return Math.Round(remCake, 2);
+            }
+
+        }
+
+        public static void Main()
+        {
+            // Action delegate - Manipulate string provided
+            Action<string, string> ActionStringManipulator = (start, suffix) =>
+            {
+                Console.WriteLine($"The string reads: {start} {suffix}");
+                Console.WriteLine($"The string order inverted reads: {suffix} {start}");
+            };
+
+            ActionStringManipulator("I like to eat", "Cake");
+
+
+            // Func delegate - share cake between guests
+            Func<int, int, double> MyShare = Cake.CalculateShare;
+            Func<double, int, double> GuestShare = Cake.CalculateRemainingShare;
+
+            Console.WriteLine("My share of cake is 40% of 12 slices.");
+            Console.WriteLine($"I get { MyShare(12, 40) } slices...");
+            Console.WriteLine($"Everyone else gets {GuestShare(MyShare(12, 40), 4)} slices each...");
+           
+
         }
         
     }
