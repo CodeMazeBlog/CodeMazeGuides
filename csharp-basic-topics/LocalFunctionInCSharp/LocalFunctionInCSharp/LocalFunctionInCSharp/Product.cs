2f841943a894a6911a9e7879f68fef84b0f8ec5a
Shayar Shrestha <shayarshrestha23@gmail.com>
2023-10-06 23:26:45 +0545

    Local Functions in C#

---
+++ csharp-basic-topics/LocalFunctionInCSharp/LocalFunctionInCSharp/LocalFunctionInCSharp/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalFunctionInCSharp
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int StockQuantity { get; set; }
+        public int SelectedQuantity { get; set; }
+
+    }
+}
