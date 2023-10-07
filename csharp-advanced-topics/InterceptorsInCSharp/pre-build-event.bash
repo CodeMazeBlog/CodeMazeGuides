1ed2b3ac578375bbe232cd49c8e2b3f3a958b8ea
Houssam Ghannoum <housgh23@gmail.com>
2023-10-07 13:01:25 +0300

    removed cat command from script

--- csharp-advanced-topics/InterceptorsInCSharp/pre-build-event.bash
+++ csharp-advanced-topics/InterceptorsInCSharp/pre-build-event.bash
@@ -5,6 +5,3 @@ escaped_directory="$(echo "$(pwd)" | sed 's|/|\\\\\\\\|g')"
 
 # Replace the string in the file
 sed -i "s|absolute/path/to/program.cs|$escaped_directory\\\\\\\\InterceptorsInCSharp\\\\\\\\Program.cs|g" InterceptorsInCSharp/GeneratedCode.cs
-
-# Print the modified file content
-cat InterceptorsInCSharp/GeneratedCode.cs
