9631c127471155f787ba7a726036e25e305d277a
DgTm <email>
2023-10-07 20:50:40 +0300

    Add CustomNamingPolicy project

---
+++ json-csharp/CustomNamingPolicy/CustomNamingPolicy.NET8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace CustomNamingPolicy.NET8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+            };
+
+            var jsonObj = JsonSerializer.Serialize(new { PropertyName = "value" }, options);
+            Console.WriteLine(jsonObj);
+
+            options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.KebabCaseUpper
+            };
+
+            jsonObj = JsonSerializer.Serialize(new { PropertyName = "value" }, options);
+            Console.WriteLine(jsonObj);
+        }
+    }
+}
---
+++ json-csharp/CustomNamingPolicy/CustomNamingPolicy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+namespace CustomNamingPolicy
+{
+    public class CustomNamingPolicy
+    {
+        public static void Main()
+        {
+            var person = new Person()
+            {
+                Name = "Name1",
+                SurName = "Surname1"
+            };
+
+            var jsonString = JsonSerializer.Serialize(person);
+            Console.WriteLine(jsonString);
+
+            var jsonopts = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = new FlatCasePolicy()
+            };
+
+            jsonString = JsonSerializer.Serialize(person, jsonopts);
+            Console.WriteLine(jsonString);
+
+            jsonopts = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = new CamelCasePolicy()
+            };
+
+            jsonString = JsonSerializer.Serialize(person, jsonopts);
+            Console.WriteLine(jsonString);
+        }
+    }
+}
