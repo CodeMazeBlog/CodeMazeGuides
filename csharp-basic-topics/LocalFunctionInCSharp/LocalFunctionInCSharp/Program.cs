66b7d6110ad33618ab2bbfbd4c104cdefd58948b
Shayar Shrestha <shayarshrestha23@gmail.com>
2023-10-10 22:15:50 +0545

    Fixing Changes

--- csharp-basic-topics/LocalFunctionInCSharp/LocalFunctionInCSharp/Program.cs
+++ csharp-basic-topics/LocalFunctionInCSharp/LocalFunctionInCSharp/Program.cs
@@ -1,35 +1,29 @@
-﻿//using LocalFunctionInCSharp;
+﻿using LocalFunctionInCSharp;
 
-//var cart = new ShoppingCart
-//{
-//    Products = new List<Product>
-//            {
-//                new Product { Name = "Product A", StockQuantity = 10, SelectedQuantity = 2 },
-//                new Product { Name = "Product B", StockQuantity = 11, SelectedQuantity = 5 }
-//            }
-//};
+var cart = new ShoppingCart
+{
+    Products = new List<Product>
+            {
+                new Product { Name = "Product A", StockQuantity = 10, SelectedQuantity = 2 },
+                new Product { Name = "Product B", StockQuantity = 11, SelectedQuantity = 5 }
+            }
+};
 
-//if (cart.Checkout())
-//{
-//    Console.WriteLine("Checkout successful.");
-//}
-//else
-//{
-//    Console.WriteLine("Checkout failed due to validation errors.");
-//}
+if (cart.Checkout())
+{
+    Console.WriteLine("Checkout successful.");
+}
+else
+{
+    Console.WriteLine("Checkout failed due to validation errors.");
+}
 
 List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-List<int> evenNumbers = FilterEvenNumbers(numbers);
-Console.WriteLine(string.Join(", ", evenNumbers));
+List<int> evenNumbersUsingLocalFunction = FilterEvenNumbers(numbers);
+Console.WriteLine(string.Join(", ", evenNumbersUsingLocalFunction));
 
 static List<int> FilterEvenNumbers(List<int> numbers)
 {
-    bool IsEven(int num)
-    {
-        return num % 2 == 0;
-    }
-
     List<int> result = new List<int>();
     foreach (int num in numbers)
     {
@@ -38,5 +32,14 @@ static List<int> FilterEvenNumbers(List<int> numbers)
             result.Add(num);
         }
     }
+
+    bool IsEven(int num)
+    {
+        return num % 2 == 0;
+    }
+
     return result;
 }
+
+List<int> evenNumbersUsingLambdaFunction = numbers.Where(num => num % 2 == 0).ToList();
+Console.WriteLine(string.Join(", ", evenNumbersUsingLambdaFunction));
