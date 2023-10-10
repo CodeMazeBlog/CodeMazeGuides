86ac5f580c69a1463c039ac10a33b9de58d82887
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-10-11 01:12:14 +0330

    feat: all usecases are added

---
+++ csharp-basic-topics/FuncActionDelegatesInCSharp/FuncActionDelegatesInCSharp/UseCases/ActionDelegateUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace FuncActionDelegatesInCSharp.UseCases;
+
+public class ActionDelegateUseCase
+{
+    private readonly ILogger<ActionDelegateUseCase> _logger;
+    public ActionDelegateUseCase(ILogger<ActionDelegateUseCase> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Run()
+    {
+        Action<string> showMessage = (msg) => _logger.LogInformation(msg);
+        showMessage("Welcome to amazing Code-Maze");
+    }
+}
---
+++ csharp-basic-topics/FuncActionDelegatesInCSharp/FuncActionDelegatesInCSharp/UseCases/FuncDelegateUseCase.cs
@@ -0,0 +1,30 @@
+﻿using FuncActionDelegatesInCSharp.Loggign;
+using Microsoft.Extensions.Logging;
+
+namespace FuncActionDelegatesInCSharp.UseCases;
+
+public class FuncDelegateUseCase
+{
+    private readonly ILogger<FuncDelegateUseCase> Logger;
+
+    private readonly Func<int, int, int> Add = (x, y) => x + y;
+
+    private readonly Func<int, int, int> Multiply = (x, y) => x * y;
+
+    public FuncDelegateUseCase()
+    {
+        Logger = LoggingFactory<FuncDelegateUseCase>.Create();
+    }
+    
+    public void Run()
+    {
+        var x = 3;
+        var y = 5;
+
+        var addResult = Add(x, y);
+        var multiplyResult = Multiply(x, y);
+        
+        Logger.LogInformation($"The adding result is: {addResult}");
+        Logger.LogInformation($"The multiplying result is: {multiplyResult}");
+    }
+}
