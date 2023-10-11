0db6913b0fcf93a4cb697bf5e78dfa48ae03907b
kevinLug <kevinotim4@gmail.com>
2023-10-11 12:13:45 +0300

    Update dbo.Entities.data.sql
    Fix sql query

--- dotnet-dapper/HowtoGetaDatabaseRowasJSONUsingDapper/HowtoGetaDatabaseRowasJSONUsingDapper/dbo.Entities.data.sql
+++ dotnet-dapper/HowtoGetaDatabaseRowasJSONUsingDapper/HowtoGetaDatabaseRowasJSONUsingDapper/dbo.Entities.data.sql
@@ -13,6 +13,6 @@ CREATE TABLE [dbo].[Entities] (
 );
 
 SET IDENTITY_INSERT [dbo].[Entities] ON
-INSERT INTO [dbo].[Entities] ([Id], [Name], [Make], [Model], [Year], [Color]) VALUES (1, N'Mercedes Benz', N'GLC', N'2014', N'Black')
-INSERT INTO [dbo].[Entities] ([Id], [Name], [Make], [Model], [Year], [Color]) VALUES (2, N'Nissan', N'Altima', N'2014', N'White')
+INSERT INTO [dbo].[Entities] ([Id], [Make], [Model], [Year], [Color]) VALUES (1, N'Mercedes Benz', N'GLC', N'2014', N'Black')
+INSERT INTO [dbo].[Entities] ([Id], [Make], [Model], [Year], [Color]) VALUES (2, N'Nissan', N'Altima', N'2014', N'White')
 SET IDENTITY_INSERT [dbo].[Entities] OFF
