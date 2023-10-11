c584b58129f46cdaff052c86c0357257cb6ac30a
jshergal <1489638+jshergal@users.noreply.github.com>
2023-10-11 16:57:57 +0300

    Updated project with powershell script to fix up interceptor path

--- csharp-advanced-topics/InterceptorsInCSharp/pre-build-event.bash
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Escape the current directory for use in the sed command
-escaped_directory="$(echo "$(pwd)" | sed 's|/|\\\\\\\\|g')"
-
-# Replace the string in the file
-sed -i "s|absolute/path/to/program.cs|$escaped_directory\\\\\\\\InterceptorsInCSharp\\\\\\\\Program.cs|g" InterceptorsInCSharp/GeneratedCode.cs
