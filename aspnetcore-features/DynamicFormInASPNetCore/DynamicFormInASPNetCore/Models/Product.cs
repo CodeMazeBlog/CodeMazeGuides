c828d44191591f9e7ebfe2e85d73dc7ac9a121aa
Pooja-github19 <83271108+Pooja-github19@users.noreply.github.com>
2023-10-13 23:40:26 +0530

    CM-318 Changes as per PR

--- aspnetcore-features/DynamicFormInASPNetCore/DynamicFormInASPNetCore/Models/Product.cs
+++ aspnetcore-features/DynamicFormInASPNetCore/DynamicFormInASPNetCore/Models/Product.cs
@@ -7,10 +7,10 @@ public class Product
     public int Id { get; set; }
 
     [Required(ErrorMessage = "Name is required.")]
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     [Required(ErrorMessage = "Price is required.")]
     [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
-    public decimal? Price { get; set; }
+    public decimal Price { get; set; }
 }
 
