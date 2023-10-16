12b9736e3834efe67a55b31b05ef790f7fd0ffd3
acha-r <jayrachel96@gmail.com>
2023-10-16 14:00:22 +0100

    fix entry point

--- files-csharp/Directory_And_DirectoryInfo/Directory_And_DirectoryInfo/Program.cs
+++ files-csharp/Directory_And_DirectoryInfo/Directory_And_DirectoryInfo/Program.cs
@@ -1,1 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Directory_And_DirectoryInfo;
+
+var path = @"C:\Public\CM-Demos";
+
+DirectoryMethods.CreateADirectory(path);
+DirectoryMethods.GetFilesInDirectory(path);
+DirectoryMethods.DeleteDirectory(path);
