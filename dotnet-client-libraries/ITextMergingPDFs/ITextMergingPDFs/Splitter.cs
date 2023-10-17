d2a56b59afac44ed97d8906485dfc082ed1c1dad
Matjaž Prtenjak <matjaz@matjazev.net>
2023-10-17 17:54:50 +0200

    CM-720: BUG: Wrongly named method

--- dotnet-client-libraries/ITextMergingPDFs/ITextMergingPDFs/Splitter.cs
+++ dotnet-client-libraries/ITextMergingPDFs/ITextMergingPDFs/Splitter.cs
@@ -10,24 +10,27 @@ namespace ITextMergingPDFs
             using var reader = new PdfReader(sourcePdfFile);
             using var srcPdfDocument = new PdfDocument(reader);
 
-            var oddPagesFileName = Path.Combine(Path.GetDirectoryName(sourcePdfFile)!, "odd.pdf");
-            newMethod(srcPdfDocument, oddPagesFileName, pageNum => pageNum % 2 != 0);
+            string sourceFilePath = Path.GetDirectoryName(sourcePdfFile)!;
+            var oddPagesFileName = Path.Combine(sourceFilePath, "odd.pdf");
+            ExtractPagesThatMatchesCriteria(srcPdfDocument, oddPagesFileName, 
+                pageNum => pageNum % 2 != 0);
 
-            var evenPagesFileName = Path.Combine(Path.GetDirectoryName(sourcePdfFile)!, "even.pdf");
-            newMethod(srcPdfDocument, evenPagesFileName, pageNum => pageNum % 2 == 0);
+            var evenPagesFileName = Path.Combine(sourceFilePath, "even.pdf");
+            ExtractPagesThatMatchesCriteria(srcPdfDocument, evenPagesFileName, 
+                pageNum => pageNum % 2 == 0);
 
             return (oddPagesFileName, evenPagesFileName);
         }
 
-        private static void newMethod(PdfDocument srcPdfDocument, string resultPdfDocument,
-            Func<int, bool> pageSelection)
+        private static void ExtractPagesThatMatchesCriteria(PdfDocument srcPdfDocument, 
+            string resultPdfDocument, Func<int, bool> pageSelectionCriteria)
         {
             using var writer = new PdfWriter(resultPdfDocument);
             using var writerDocument = new PdfDocument(writer);
 
             var oddPagesArray = Enumerable
                 .Range(1, srcPdfDocument.GetNumberOfPages())
-                .Where(pageSelection)
+                .Where(pageSelectionCriteria)
                 .ToArray();
 
             var merger = new PdfMerger(writerDocument);
