3c42e3f33ceae5c1791e914f8e06c09fbf5035b6
Ivan Matec <im@bullwall.com>
2023-10-19 10:22:53 +0200

    - implemented the basic operations and AutoMLExperiment definition and usage

---
+++ csharp-machine-learning/ML.NET_AutoML_API/AutoML_App/AutoML_App/ModelOutput.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.ML.Data;
+public class ModelOutput
+{
+    [ColumnName(@"PredictedLabel")]
+    public bool PredictedLabel { get; set; }
+
+    [ColumnName(@"Score")]
+    public float Score { get; set; }
+}
