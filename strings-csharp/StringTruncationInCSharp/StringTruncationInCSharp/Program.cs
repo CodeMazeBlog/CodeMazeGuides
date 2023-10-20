657646611084065620dafad07760f7dfc33068d0
Samuel <lawalsamuel2310@gmail.com>
2023-10-20 17:28:21 +0100

    Create different methods for truncating a string and add unit tests

---
+++ strings-csharp/StringTruncationInCSharp/StringTruncationInCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using StringTruncationInCSharp;
+
+StringHelper stringHelper = new StringHelper();
+var originalString = "This is a long string.";
+var maxLength = 10;
+
+var truncatedString_substring = stringHelper.TruncateStringUsingSubstring(originalString, maxLength);
+Console.WriteLine("Using Substring method: " + truncatedString_substring);
+
+var truncatedString_stringInterpolation = stringHelper.TruncateStringUsingStringInterpolation(originalString, maxLength);
+Console.WriteLine("Using String Interpolation: " + truncatedString_stringInterpolation);
+
+var truncatedString_forLoop = stringHelper.TruncateStringUsingForLoop(originalString, maxLength);
+Console.WriteLine("Using For Loop: " + truncatedString_forLoop);
+
+var truncatedString_stringBuilder = stringHelper.TruncateStringUsingStringBuilder(originalString, maxLength);
+Console.WriteLine("Using StringBuilder: " + truncatedString_stringBuilder);
+
+var truncatedString_regularExpressions = stringHelper.TruncateStringUsingRegularExpressions(originalString, maxLength);
+Console.WriteLine("Using Regular Expressions: " + truncatedString_regularExpressions);
+
+var truncatedString_remove = stringHelper.TruncateStringUsingRemove(originalString, maxLength);
+Console.WriteLine("Using Remove method: " + truncatedString_remove);
+
+var truncatedString_LINQ = stringHelper.TruncateStringUsingLINQ(originalString, maxLength);
+Console.WriteLine("Using LINQ: " + truncatedString_LINQ);
+
+
+Console.ReadKey();
---
+++ strings-csharp/StringTruncationInCSharp/StringTruncationInCSharp/StringHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringTruncationInCSharp
+{
+    public class StringHelper
+    {
+        public string TruncateStringUsingSubstring(string originalString, int maxLength)
+        {
+            string truncatedString = originalString.Substring(0, maxLength);
+
+            return truncatedString;
+        }
+        public string TruncateStringUsingStringInterpolation(string originalString, int maxLength)
+        {
+            string truncatedString = $"{originalString[..maxLength]}";
+
+            return truncatedString;
+        }
+        public string TruncateStringUsingForLoop(string originalString, int maxLength)
+        {
+            string truncatedString = string.Empty;
+            int length = Math.Min(maxLength, originalString.Length);
+
+            for (int i = 0; i < length; i++)
+            {
+                truncatedString += originalString[i];
+            }
+
+            return truncatedString;
+        }
+        public string TruncateStringUsingStringBuilder(string originalString, int maxLength)
+        {
+            var truncatedStringBuilder = new StringBuilder(maxLength);
+
+            for (int i = 0; i < Math.Min(maxLength, originalString.Length); i++)
+            {
+                truncatedStringBuilder.Append(originalString[i]);
+            }
+
+            string truncatedString = truncatedStringBuilder.ToString();
+
+            return truncatedString;
+        }
+        public string TruncateStringUsingRegularExpressions(string originalString, int maxLength)
+        {
+            string truncatedString = Regex.Replace(originalString, $"^(.{{0,{maxLength}}}).*$", "$1");
+
+            return truncatedString;
+        }
+        public string TruncateStringUsingRemove(string originalString, int maxLength)
+        {
+            string truncatedString = string.Empty;
+
+            if (originalString.Length > maxLength)
+            {
+                truncatedString = originalString.Remove(maxLength);
+            }
+
+            return truncatedString;
+        }
+        public string TruncateStringUsingLINQ(string originalString, int maxLength)
+        {
+            string truncatedString = new string(originalString.Take(maxLength).ToArray());
+
+            return truncatedString;
+        }
+        
+    }
+}
