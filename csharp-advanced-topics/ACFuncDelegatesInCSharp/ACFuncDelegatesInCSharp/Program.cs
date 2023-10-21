e3afe7f575f7c77c5c5613887112c182af0a604c
kendrick chukwuka <kendrickchibueze@gmail.com>
2023-10-21 02:24:57 +0100

    feat:created MyUpdateData method and added the Action delegate as a reference to the callback method

---
+++ csharp-advanced-topics/ACFuncDelegatesInCSharp/ACFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ACFuncDelegatesInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ csharp-advanced-topics/ACFuncDelegatesInCSharp/Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
