b0fded52f9613154ce827323d2dea11f9f5926d3
kendrick chukwuka <kendrickchibueze@gmail.com>
2023-10-21 02:51:47 +0100

    feat:Overloaded the MyUpdateData method and passed in the Func delegate to hold the reference to the NewnotificationCallBack method

--- csharp-advanced-topics/ACFuncDelegatesInCSharp/ACFuncDelegatesInCSharp/Program.cs
+++ csharp-advanced-topics/ACFuncDelegatesInCSharp/ACFuncDelegatesInCSharp/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+                Console.WriteLine("********************************");
+                Console.WriteLine(" Action and Func Delegates in C# ");
+                Console.WriteLine("*********************************");
+
+                MyCustomerPresentation mycustomerPresentation = new MyCustomerPresentation();
+                mycustomerPresentation.CheckUpdate();
+            }
         }
     }
 }
