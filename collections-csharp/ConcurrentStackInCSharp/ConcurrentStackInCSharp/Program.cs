e7d65326fc4ac8ef97fed8710519b119a62acab4
Benjamin Mishra <benjamin.mishra@ringtailgames.com>
2023-10-23 14:33:35 +0530

    Added example for ConcurrentStack<T>

---
+++ collections-csharp/ConcurrentStackInCSharp/ConcurrentStackInCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConcurrentStackInCSharp;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Simulate usage of Drawing tool..");
+        UseDrawingTool();
+    }
+
+    public static void UseDrawingTool()
+    {
+        DrawingTool tool = new DrawingTool();
+
+        // Simulate multiple users performing and undoing actions concurrently
+        Parallel.Invoke(
+            () =>
+            {
+                tool.PerformAction("Draw Circle");
+                Task.Delay(50).Wait();  // Simulating some work
+                tool.UndoLastAction();
+            },
+            () =>
+            {
+                tool.PerfromMultipleActions("Draw Square", "Draw Triangle", "Draw Parallel Lines", "Draw Hexagon");
+                Task.Delay(30).Wait();  // Simulating some work
+                tool.UndoLastNActions(4); // Undo last 4 actions
+            },
+            () =>
+            {
+                tool.PerformAction("Color Circle Red");
+                Task.Delay(70).Wait();  // Simulating some work
+                tool.UndoLastAction();
+            }
+        );
+    }
+}
