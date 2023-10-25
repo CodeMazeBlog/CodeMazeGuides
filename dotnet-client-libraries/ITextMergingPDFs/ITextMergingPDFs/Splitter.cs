1c3121dd23c39da0b355b28d847125a9e3e2d976
Matjaž Prtenjak <matjaz@matjazev.net>
2023-10-25 09:39:50 +0200

    CM-720: Optimize a class

--- dotnet-client-libraries/ITextMergingPDFs/ITextMergingPDFs/Splitter.cs
+++ dotnet-client-libraries/ITextMergingPDFs/ITextMergingPDFs/Splitter.cs
@@ -10,31 +10,31 @@ namespace ITextMergingPDFs
             using var reader = new PdfReader(sourcePdfFile);
             using var srcPdfDocument = new PdfDocument(reader);
 
-            string sourceFilePath = Path.GetDirectoryName(sourcePdfFile)!;
+            var sourceFilePath = Path.GetDirectoryName(sourcePdfFile)!;
             var oddPagesFileName = Path.Combine(sourceFilePath, "odd.pdf");
-            ExtractPagesThatMatchesCriteria(srcPdfDocument, oddPagesFileName, 
+            ExtractPagesThatMatchCriteria(srcPdfDocument, oddPagesFileName, 
                 pageNum => pageNum % 2 != 0);
 
             var evenPagesFileName = Path.Combine(sourceFilePath, "even.pdf");
-            ExtractPagesThatMatchesCriteria(srcPdfDocument, evenPagesFileName, 
+            ExtractPagesThatMatchCriteria(srcPdfDocument, evenPagesFileName, 
                 pageNum => pageNum % 2 == 0);
 
             return (oddPagesFileName, evenPagesFileName);
         }
 
-        private static void ExtractPagesThatMatchesCriteria(PdfDocument srcPdfDocument, 
+        private static void ExtractPagesThatMatchCriteria(PdfDocument srcPdfDocument, 
             string resultPdfDocument, Func<int, bool> pageSelectionCriteria)
         {
             using var writer = new PdfWriter(resultPdfDocument);
             using var writerDocument = new PdfDocument(writer);
 
-            var oddPagesArray = Enumerable
+            var selectedPages = Enumerable
                 .Range(1, srcPdfDocument.GetNumberOfPages())
                 .Where(pageSelectionCriteria)
                 .ToArray();
 
             var merger = new PdfMerger(writerDocument);
-            merger.Merge(srcPdfDocument, oddPagesArray);
+            merger.Merge(srcPdfDocument, selectedPages);
         }
     }
 }
