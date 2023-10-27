2a4a2f28068de79cadd285b6ddcb1a3de7222d72
Achyut Tripathi <tripathiachyut2002@gmail.com>
2023-10-27 20:06:56 +0530

    updated file action and func

---
+++ csharp-basic-topics/Action and Func/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is for Action delegate.
+Action<string, int> Messageprint = (content, count) =>
+{
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(content);
+    }
+};
+
+Messageprint("Hello_word, Action!", 6);
+
+
+
+// This is for Fun delegate.
+Func<int, int, int> Sub = (a, b) => a - b;
+
+int res = Sub(15, 13);
+Console.WriteLine("15 - 13 = " + res);
