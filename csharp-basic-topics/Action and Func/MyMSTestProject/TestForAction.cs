066ad1bc2f2acc0fe1a3265b2b3534fabc18735d
Achyut Tripathi <tripathiachyut2002@gmail.com>
2023-10-27 22:07:28 +0530

    updated file action and func

---
+++ csharp-basic-topics/Action and Func/MyMSTestProject/TestForAction.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+[TestClass]
+public class MessagePrintTests
+{
+    [TestMethod]
+    public void GivenStringInput_WhenActionIsCalled_ThenPrintsTheInput()
+    {
+        // Arrange
+        var content = "Hello, Action!";
+        var count = 3;
+        var expectedOutput = "Hello, Action!" + Environment.NewLine + "Hello, Action!" + Environment.NewLine + "Hello, Action!" + Environment.NewLine;
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        Action<string, int> Message_print = (c, cnt) =>
+        {
+            for (int i = 0; i < cnt; i++)
+            {
+                Console.WriteLine(c);
+            }
+        };
+
+        // Act
+        Messageprint(content, count);
+
+        // Assert
+        Assert.AreEqual(expectedOutput, output.ToString());
+    }
+}
