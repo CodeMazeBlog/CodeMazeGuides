dcb17211b479ca88838e1d8f2ccbd0263aa98a79
Edinam Ahadzie <edin22.ey@gmail.com>
2023-10-29 23:10:30 +0000

    Action and Func Delegates

---
+++ csharp-basic-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/MealGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace ActionAndFuncDelegates;
+
+public class MealGenerator
+{
+    private Func<List<string>, string> generateMeal;
+    private Action<string> displayMeal;
+    public MealGenerator(Func<List<string>, string> mealGenerator, Action<string> mealDisplay)
+    {
+        generateMeal = mealGenerator;
+        displayMeal = mealDisplay;
+    }
+    public void GenerateAndDisplayMeal(List<string> ingredients)
+    {
+        var meal = generateMeal(ingredients);
+        displayMeal(meal);
+    }
+}
---
+++ csharp-basic-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ActionAndFuncDelegates;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        var mealGenerator = new MealGenerator(CookDelicousMeal, ServeDeliciousMeal);
+
+        List<string> selectedIngredients = new List<string>
+        {
+            "Chicken breast",
+            "Broccoli",
+            "Rice",
+            "Soy sauce",
+            "Garlic",
+            "Ginger",
+            "Olive oil"
+        };
+
+        mealGenerator.GenerateAndDisplayMeal(selectedIngredients);
+
+    }
+    static string CookDelicousMeal(List<string> ingredients)
+        {
+             string ingredientList = string.Join("\n", ingredients);
+            string meal = $"Your meal includes:\n{ingredientList}\n\nEnjoy your delicious meal!";
+            return meal;
+        }
+    static void ServeDeliciousMeal(string meal)
+    {
+         Console.WriteLine("Meal Recipe:\n");
+         Console.WriteLine(meal);
+    }
+        
+}
