632e21f416a6ad05640261527b338f99d34f9f88
Oluleke OKEWALE <olulekeokewale@xentenltd.com>
2023-10-29 20:52:30 +0100

    first commit

---
+++ csharp-advanced-topics/ActionAndFuncDelegates/CodeMazeGuides.Sample.Delegates/ActionDelegates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMazeGuides.Sample.Delegates
+{
+    public static class ActionDelegates
+    {
+        // Define a function that compute the cube of an integer .
+        public static Action welcomeToActionDelegate = () => Console.WriteLine("You are welcome to Action Delegate sample that takes no parameter");   // Declaration 
+																																					   // Declaration welcomeToActionDelegate(); // Invocation, which prints the message.
+
+		// Define a function that squares an integer.
+		public static Action<string> welcomeToActionDelegateWithParam = name => Console.WriteLine($"Dear {name}!, you are welcome to Action Delegate sample that takes one parameter"); // Declaration 
+																																														//  welcomeToActionDelegateWithParam("Oluleke");   //Invokes the action, which prints "Dear Oluleke!, you are welcome to Action Delegate sample that takes one parameter".
+
+		// Function composition using Func delegates.
+		public static Action<int, int> productOfTwoNos = (a, b) => Console.WriteLine($"Product: {a * b}");   // Declaration 
+																											 // Invocation productOfTwoNos(10, 4);   Invokes the action, which prints "Product: 40".
+		public static Action<string, int, string, int> displayStdInfo = (stdName1, stdAge1, stdName2, stdAge2) => 
+        { 
+            Console.WriteLine($"Name: {stdName1}, Age: {stdAge1}"); 
+            Console.WriteLine($"Name: {stdName2}, Age: {stdAge2}"); 
+        }; // Declaration
+           // displayStdInfo("Alice", 10, "Charice",11 ); // Invokes the action, which prints "Name: Alice, Age: 10 // Name: Charice, Age: 11"
+		 
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegates/CodeMazeGuides.Sample.Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using CodeMazeGuides.Sample.Delegates;
+
+//Action<> Delegates
+//No Parameter Action<> Delegate
+ActionDelegates.welcomeToActionDelegate();
+
+//One Parameter Action<T> Delegate 
+ActionDelegates.welcomeToActionDelegateWithParam("Oluleke");
+
+//Two Parameter Action<T1,T2> Delegate 
+ActionDelegates.productOfTwoNos(10, 4);
+
+//Complex Parameter Action<T1,T2,T3,T4> Delegate 
+ActionDelegates.displayStdInfo("Alice", 10, "Charice", 11);
+
+
+//Func<> Delegates
+//No Parameter Func<> Delegate
+string result = FuncDelegates.computeRandom();
+Console.WriteLine(result);
+
+//One Parameter Func<T> Delegate
+int cubeResult = FuncDelegates.Cube(3);
+Console.WriteLine(cubeResult);
+
+//One Parameter Func<T> Delegate
+  string stdInfo= FuncDelegates.displayStdInfo("Charice", 10);
+Console.WriteLine(stdInfo);
