e59ac3643b83bdd0ed3a2e2da4a73fe713f11957
Oluleke OKEWALE <olulekeokewale@xentenltd.com>
2023-10-29 20:43:42 +0100

    CodeMazeGuides.Sample.Delegates

---
+++ csharp-action-func-delegates/CodeMazeGuides.Sample.Delegates/CodeMazeGuides.Sample.Delegates/FuncDelegates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMazeGuides.Sample.Delegates
+{
+
+
+    public static class FuncDelegates
+    {
+
+		public static Func<string> computeRandom = () => { 
+            var random = new Random(); var str = string.Empty;
+            for (var i = 0; i < 10; i++) 
+                str = string.Concat(str, random.Next(10).ToString());
+                return str; 
+        };
+		//string result = computeRandom(); // Invokes the Func and gets a random number.
+
+		public static int Cube(int x) => x * x *x ;
+		//int cubeResult = cube(3);
+
+		public static Func<string, int, string> displayStdInfo = (stdName, stdAge) => $"Name: {stdName}, Age: {stdAge}";
+		//string stdInfo= displayStdInfo("Charice", 10); // Formats a string with count.
+
+		
+         
+    }
+}
