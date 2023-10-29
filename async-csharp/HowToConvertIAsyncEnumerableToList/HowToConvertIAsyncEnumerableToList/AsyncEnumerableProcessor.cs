88cc8e9c27b9c4bebfb4a31a486b458de04758e4
Koddek <codedek@outlook.com>
2023-10-29 11:49:20 -0400

    removed unneeded code

--- async-csharp/HowToConvertIAsyncEnumerableToList/HowToConvertIAsyncEnumerableToList/AsyncEnumerableProcessor.cs
+++ async-csharp/HowToConvertIAsyncEnumerableToList/HowToConvertIAsyncEnumerableToList/AsyncEnumerableProcessor.cs
@@ -2,43 +2,12 @@ namespace HowToConvertIAsyncEnumerableToList;
 
 public class AsyncEnumerableProcessor
 {
-    public async Task<List<string>> ProcessDataUsingToListAsync()
-    {
-        IAsyncEnumerable<string> dataStream = GetDataAsync();
-        List<string> dataList = await dataStream.ToListAsync();
-
-        // Work with dataList synchronously
-        foreach (var item in dataList)
-        {
-            Console.WriteLine(item);
-        }
-
-        return dataList;
-    }
-
-    public async Task<List<string>> ProcessDataUsingAsyncForeachAsync()
-    {
-        List<string> dataList = new List<string>();
-        IAsyncEnumerable<string> dataStream = GetDataAsync();
-        await foreach (string item in dataStream)
-        {
-            dataList.Add(item);
-        }
-
-        // Work with dataList synchronously
-        foreach (var item in dataList)
-        {
-            Console.WriteLine(item);
-        }
-
-        return dataList;
-    }
-
     public async IAsyncEnumerable<string> GetDataAsync()
     {
-        await Task.Delay(1000);
-        yield return "a";
-        yield return "b";
-        yield return "c";
+        for (int i = 0; i < 3; i++)
+        {
+            await Task.Delay(1000);
+            yield return $"item{i}";
+        }
     }
 }
