46381f453c3cc70ce8c41e4c82557b2244c8b7de
Houssam Ghannoum <housgh23@gmail.com>
2023-10-29 17:09:18 +0200

    removed comments

--- csharp-advanced-topics/InterceptorsInCSharp/InterceptorsInCSharp/GeneratedCode.cs
+++ csharp-advanced-topics/InterceptorsInCSharp/InterceptorsInCSharp/GeneratedCode.cs
@@ -4,8 +4,8 @@ namespace InterceptorsInCSharp;
 
 public static class GeneratedCode
 {
-    [InterceptsLocation("absolute/path/to/program.cs", 9, 28)] //Make sure to change the first argument to the absolute path to your Program.cs file
-    [InterceptsLocation("absolute/path/to/program.cs", 10, 35)] //Make sure to change the first argument to the absolute path to your Program.cs file
+    [InterceptsLocation("absolute/path/to/program.cs", 9, 28)] 
+    [InterceptsLocation("absolute/path/to/program.cs", 10, 35)]
     public static string InterceptorMethod(this Example example, string text)
     {
         return $"{text}, Code Maze!";
