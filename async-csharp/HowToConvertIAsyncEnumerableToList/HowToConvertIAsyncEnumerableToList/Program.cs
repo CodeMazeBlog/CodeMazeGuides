6242b6592e49535c81bf2cbcd0a941ba01bec133
Koddek <codedek@outlook.com>
2023-10-29 11:48:48 -0400

    update the console app to show the difference between using ToListAsync and await foreach

--- async-csharp/HowToConvertIAsyncEnumerableToList/HowToConvertIAsyncEnumerableToList/Program.cs
+++ async-csharp/HowToConvertIAsyncEnumerableToList/HowToConvertIAsyncEnumerableToList/Program.cs
@@ -1,5 +1,18 @@
 ﻿using HowToConvertIAsyncEnumerableToList;
 
 var processor = new AsyncEnumerableProcessor();
-_ = await processor.ProcessDataUsingToListAsync();
-_ = await processor.ProcessDataUsingAsyncForeachAsync();
+IAsyncEnumerable<string> dataStream = processor.GetDataAsync();
+
+List<string> dataList = await dataStream.ToListAsync();
+
+// display each item after processing the stream
+foreach (var item in dataList)
+{
+    Console.WriteLine(item);
+}
+
+// display each item while processing the stream
+await foreach (string item in dataStream)
+{
+    Console.WriteLine(item);
+}
