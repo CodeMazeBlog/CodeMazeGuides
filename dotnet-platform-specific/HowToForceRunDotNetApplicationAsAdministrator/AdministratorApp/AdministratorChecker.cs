6964058f1324515fb7d2f924c4e1a4c39924bcf2
jshergal <1489638+jshergal@users.noreply.github.com>
2023-10-31 17:40:02 +0200

    Moved project to dotnet-platform-specific

--- dotnet-platform-specific/HowToForceRunDotNetApplicationAsAdministrator/AdministratorApp/AdministratorChecker.cs
+++ dotnet-platform-specific/HowToForceRunDotNetApplicationAsAdministrator/AdministratorApp/AdministratorChecker.cs
@@ -1,10 +1,12 @@
 ﻿using System.Security.Principal;
 
+namespace AdministratorApp;
+
 public class AdministratorChecker : IAdministratorChecker
 {
     public bool IsCurrentUserAdmin()
     {
-        WindowsIdentity identity = WindowsIdentity.GetCurrent();
+        var identity = WindowsIdentity.GetCurrent();
         var principal = new WindowsPrincipal(identity);
         return principal.IsInRole(WindowsBuiltInRole.Administrator);
     }
