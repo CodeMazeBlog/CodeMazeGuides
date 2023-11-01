6ea215700bdf40fa10958bc5e0d08b4f3382155b
Igor Nunes Gervásio <igorngervasio@gmail.com>
2023-10-31 23:05:26 -0300

    CM-717: Removing old project

--- csharp-advanced-topics/DefaultValuesForLambdaExpressions/DefaultValuesForLambdaExpressions/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using DefaultValuesForLambdaExpressions;
-
-Console.WriteLine($"10 incremented by 5 is equal to: {Calculator.IncrementByValue(10, 5)}");
-
-Console.WriteLine($"10 incremented by the default value is equal to: {Calculator.IncrementByDefaultValue(10)}");
-
-int[] array = { 10, 20, 30, 40, 50 };
-Console.WriteLine($"The array total is equal to: {Calculator.Total(array)}");
