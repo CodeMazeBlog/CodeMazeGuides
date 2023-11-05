ba99318e50fd095d058f33e2830444efd92c09f6
Zaneta Borowska <zaneta.borowska@katoennatie.com>
2023-11-05 21:10:22 +0100

    Determine operating system

---
+++ aspnetcore-features/DetermineOperatingSystem/DetermineOperatingSystem/IOsPlatformResolver.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DetermineOperatingSystem
+{
+    public interface IOsPlatformResolver
+    {
+        bool IsWindows();
+        bool IsLinux();
+        bool IsOsx();
+        bool IsFreeBsd();
+        bool IsPlatform(OSPlatform osPlatform);
+    }
+}
---
+++ aspnetcore-features/DetermineOperatingSystem/DetermineOperatingSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using DetermineOperatingSystem;
+
+// For versions of .NET Framework and .NET Core prior to .NET 5:
+OSPlatform myOwnOperatingSystem = OSPlatform.Create("MyOwnOperatingSystem");
+
+var basePathResolver = new BasePathResolver(new OsPlatformResolver());
+string basePath = basePathResolver.GetBasePath(myOwnOperatingSystem);
+
+// From .NET 5 and later:
+bool isWindows = OperatingSystem.IsWindows();
+bool isMacOs = OperatingSystem.IsMacOS();
+bool isLinux = OperatingSystem.IsLinux();
