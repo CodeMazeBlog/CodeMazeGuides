4e675de78ac4008b0f2e4b256f50dc4aa93ea07d
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-11-06 14:54:30 +0100

    CM-851: Use file-scoped namespaces.

--- csharp-intermediate-topics/UsingStaticAnonymousFunctionsInCSharp/UsingStaticAnonymousFunctionsInCSharp/AnonymousFunctionsBenchmark.cs
+++ csharp-intermediate-topics/UsingStaticAnonymousFunctionsInCSharp/UsingStaticAnonymousFunctionsInCSharp/AnonymousFunctionsBenchmark.cs
@@ -1,28 +1,27 @@
 ﻿using BenchmarkDotNet.Attributes;
 
-namespace UsingStaticAnonymousFunctionsInCSharp
+namespace UsingStaticAnonymousFunctionsInCSharp;
+
+[MemoryDiagnoser]
+public class AnonymousFunctionsBenchmark
 {
-    [MemoryDiagnoser]
-    public class AnonymousFunctionsBenchmark
+    private int numNonConst = 10;
+    private const int numConst = 10;
+
+    public int Calculate(Func<int, int> func)
     {
-        private int numNonConst = 10;
-        private const int numConst = 10;
+        return func(6);
+    }        
 
-        public int Calculate(Func<int, int> func)
-        {
-            return func(6);
-        }        
+    [Benchmark]
+    public int MultiplyNonStatic()
+    {
+        return Calculate(num => numNonConst * num);
+    }
 
-        [Benchmark]
-        public int MultiplyNonStatic()
-        {
-            return Calculate(num => numNonConst * num);
-        }
-
-        [Benchmark]
-        public int MultiplyStatic()
-        {
-            return Calculate(static num => numConst * num);
-        }
+    [Benchmark]
+    public int MultiplyStatic()
+    {
+        return Calculate(static num => numConst * num);
     }
 }
