e1e8c96884d1c4cb8ff349290974acb4e56ed4f7
Zachary <zachdesira@hotmail.com>
2023-11-06 13:45:05 +0100

    Created Action and Func delegates

---
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using ActionAndFuncDelegatesInCSharp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Delegates.CreateDelegates();
+        Console.WriteLine("");
+
+        ActionDelegates.CreateActionDelegates();
+        Console.WriteLine("");
+
+        FuncDelegates.CreateFuncDelegates();
+    }
+}
