d02c56b9774bd0310b31e2ccc32a71d1373a59b7
acha-r <jayrachel96@gmail.com>
2023-11-07 15:21:09 +0100

    add client for websocket

---
+++ aspnetcore-webapi/RestVsWebSocketsInCSharp/WebSocketClient/WebSocketClient/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.WebSockets;
+using System.Text;
+
+var ws = new ClientWebSocket();
+
+await ws.ConnectAsync(new Uri("ws://localhost:5289/ws"),
+    CancellationToken.None);
+
+Console.WriteLine("Connected!");
+
+var recieveTask = Task.Run(async () =>
+{
+    var buffer = new byte[1024];
+    while (true)
+    {
+        var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer),
+            CancellationToken.None);
+
+        if (result.MessageType == WebSocketMessageType.Close)
+        {
+            break;
+        }
+
+        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+        Console.WriteLine("Recieved: " + message);
+    }
+});
+
+await recieveTask;
