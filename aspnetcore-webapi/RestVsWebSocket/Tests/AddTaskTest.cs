b7d61aadde237d9ee16b6300dc1b5a4019c357c2
acha-r <jayrachel96@gmail.com>
2023-11-10 06:18:10 +0100

    change namespace style to file-scoped

--- aspnetcore-webapi/RestVsWebSocket/Tests/AddTaskTest.cs
+++ aspnetcore-webapi/RestVsWebSocket/Tests/AddTaskTest.cs
@@ -1,25 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using RestVsWebSocket.Controllers;
 
-namespace Tests
+namespace Tests;
+
+public class AddTaskTest
 {
-    public class AddTaskTest
+    [Fact]
+    public void WhenTaskIsAdded_AddToListReturnOk()
     {
-        [Fact]
-        public void WhenTaskIsAdded_AddToListReturnOk()
-        {
-            // Arrange
-            var taskController = new TaskController(); 
-            var task = "Sample Task"; // Valid task data
+        // Arrange
+        var taskController = new TaskController(); 
+        var task = "Sample Task"; // Valid task data
 
-            // Act
-            var result = taskController.AddTask(task) as ObjectResult;
+        // Act
+        var result = taskController.AddTask(task) as ObjectResult;
 
-            // Assert
-            Assert.NotNull(result); 
-            Assert.Equal(200, result.StatusCode); 
-            Assert.Equal("Task added successfully.", result.Value);
-            Assert.Contains(task, taskController._tasks);
-        }
+        // Assert
+        Assert.NotNull(result); 
+        Assert.Equal(200, result.StatusCode); 
+        Assert.Equal("Task added successfully.", result.Value);
+        Assert.Contains(task, taskController.tasks);
     }
 }
