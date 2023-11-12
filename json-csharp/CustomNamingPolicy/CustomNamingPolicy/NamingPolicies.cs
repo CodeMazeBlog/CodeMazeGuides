6c86457735df54c06c341ca5bebc7946aa70d61c
DgTm <email>
2023-11-12 19:13:15 +0200

    CamelCasePolicy is moved to file

--- json-csharp/CustomNamingPolicy/CustomNamingPolicy/NamingPolicies.cs
+++ json-csharp/CustomNamingPolicy/CustomNamingPolicy/NamingPolicies.cs
@@ -6,18 +6,4 @@ namespace CustomNamingPolicy
     {
         public override string ConvertName(string name) => name.ToLower();
     }
-    public class CamelCasePolicy : JsonNamingPolicy
-    {
-        public override string ConvertName(string name)
-        {
-            var stringArray = name.ToCharArray();
-
-            if (char.IsUpper(stringArray[0]))
-            {
-                stringArray[0] = char.ToLower(stringArray[0]);
-            }
-
-            return new string(stringArray);
-        }
-    }
 }
