0e95397af861da587a872be08a331548259a372c
Samuel <lawalsamuel2310@gmail.com>
2023-11-14 22:15:21 +0100

    Add a method that uses ReadOnlySpan to truncate a string. Increase the length of the test string used in the Benchmark setup. Add rank order and params to Benchmark. Refactor TruncateStringUsingRemove() method.

--- strings-csharp/StringTruncationInCSharp/StringTruncationInCSharp/StringHelperBenchmark.cs
+++ strings-csharp/StringTruncationInCSharp/StringTruncationInCSharp/StringHelperBenchmark.cs
@@ -3,46 +3,82 @@
 namespace StringTruncationInCSharp
 {
     [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
     public class StringHelperBenchmark
     {
-        private readonly int _maxLength = 10;
-        private readonly string _originalString = "This is a long string.";
+        private string _originalString = "This is a long string.";
         private static readonly StringHelper _stringHelper = new StringHelper();
+
+        [Params(0.01, 0.10, 0.50, 0.95)]
+        public double MaxLengthPercentage;
+    
+        [GlobalSetup]
+        public void Setup()
+        {
+            _originalString = GetDummyStringToTestBenchmark();
+        }
+
 
         [Benchmark]
         public void TruncateStringUsingSubstring()
         {
-            _stringHelper.TruncateStringUsingSubstring(_originalString, _maxLength);
+            int maxLength = (int)(_originalString.Length * MaxLengthPercentage);
+
+            _stringHelper.TruncateStringUsingSubstring(_originalString, maxLength);
         }
 
         [Benchmark]
         public void TruncateStringUsingForLoop()
         {
-            _stringHelper.TruncateStringUsingForLoop(_originalString, _maxLength);
+            int maxLength = (int)(_originalString.Length * MaxLengthPercentage);
+
+            _stringHelper.TruncateStringUsingForLoop(_originalString, maxLength);
         }
 
         [Benchmark]
         public void TruncateStringUsingForLoopWithStringBuilder()
         {
-            _stringHelper.TruncateStringUsingForLoopWithStringBuilder(_originalString, _maxLength);
+            int maxLength = (int)(_originalString.Length * MaxLengthPercentage);
+            _stringHelper.TruncateStringUsingForLoopWithStringBuilder(_originalString, maxLength);
         }
 
         [Benchmark]
         public void TruncateStringUsingRegularExpressions()
         {
-            _stringHelper.TruncateStringUsingRegularExpressions(_originalString, _maxLength);
+            int maxLength = (int)(_originalString.Length * MaxLengthPercentage);
+            _stringHelper.TruncateStringUsingRegularExpressions(_originalString, maxLength);
         }
 
         [Benchmark]
         public void TruncateStringUsingRemove()
         {
-            _stringHelper.TruncateStringUsingRemove(_originalString, _maxLength);
+            int maxLength = (int)(_originalString.Length * MaxLengthPercentage);
+
+            _stringHelper.TruncateStringUsingRemove(_originalString, maxLength);
         }
 
         [Benchmark]
         public void TruncateStringUsingLINQ()
         {
-            _stringHelper.TruncateStringUsingLINQ(_originalString, _maxLength);
+            int maxLength = (int)(_originalString.Length * MaxLengthPercentage);
+
+            _stringHelper.TruncateStringUsingLINQ(_originalString, maxLength);
+        }
+
+        [Benchmark]
+        public void TruncateStringUsingSpan()
+        {
+            int maxLength = (int)(_originalString.Length * MaxLengthPercentage);
+
+            _stringHelper.TruncateStringUsingSpan(_originalString, maxLength);
+        }
+
+        private string GetDummyStringToTestBenchmark()
+        {
+            var dummyText = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. N";
+
+            return dummyText;
         }
     }
 }
