fb4fc15ceca5af9602c16539db69094796c06147
Ivan Matec <im@bullwall.com>
2023-11-17 18:05:45 +0100

    - added the projects

---
+++ csharp-machine-learning/ML.NET_AutoML_API/AutoML_App/Classes/ModelOutput.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+public class ModelOutput
+{
+    [ColumnName(@"PredictedLabel")]
+    public bool PredictedLabel { get; set; }
+
+    [ColumnName(@"Score")]
+    public float Score { get; set; }
+}
