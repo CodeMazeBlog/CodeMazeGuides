00c6fbc965a0505a5a1fcfa2787cb321315d9101
Ivan Gechev <ivan_banov@abv.bg>
2023-11-19 17:51:35 +0200

    DefaultPolicy

--- aspnetcore-webapi/HowToUseRequestTimeoutsMiddleware/HowToUseRequestTimeoutsMiddleware/Program.cs
+++ aspnetcore-webapi/HowToUseRequestTimeoutsMiddleware/HowToUseRequestTimeoutsMiddleware/Program.cs
@@ -1,5 +1,6 @@
 using HowToUseRequestTimeoutsMiddleware.Services;
 using Microsoft.AspNetCore.Http.Timeouts;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRequestTimeouts(options =>
 {
+    options.DefaultPolicy = new RequestTimeoutPolicy
+    {
+        Timeout = TimeSpan.FromMilliseconds(1500),
+        TimeoutStatusCode = (int)HttpStatusCode.InternalServerError
+    };
     options.AddPolicy("OneSecondTimeout", TimeSpan.FromMilliseconds(1000));
 });
 
