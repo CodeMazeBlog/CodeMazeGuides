3d073ec7764307e13c000a36f7951c3d70a16651
Deep18-03 <deepmshah113@gmail.com>
2023-11-20 21:55:53 +0530

    CM-288 : Added ActionAndFuncDelegate

---
+++ csharp-advanced-topics/ActionAndFuncDelegateinCSharp/ActionAndFuncDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        FuncDelegateSample();
+
+        Console.WriteLine($"\n");
+
+        ActionDelegateSample();
+
+
+    }
+
+    public static void ActionDelegateSample()
+    {
+        //Declaration 
+        Action<string> PrintMessage = message =>
+        {
+            Console.WriteLine(message);
+        };
+        //Delegate Invocation: 
+        PrintMessage("Hello, invoking the delegate!");
+    }
+    public static void FuncDelegateSample()
+    {
+        //Declaration 
+        Func<double, double> calArea = r =>
+        {
+            return 3.12 * r * r;
+        };
+
+        //Delegate Invocation 
+        double Area = calArea(20);
+
+        Console.WriteLine(Area);
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegateinCSharp/Tests/ProgramTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ActionDelegateSample_Test()
+        {
+            // Arrange
+            string expectedMessage = "Hello, invoking the delegate!";
+            string? actualMessage = null;
+
+            // Act
+            Action<string> testPrintMessage = message =>
+            {
+                actualMessage = message;
+            };
+
+            // Invoke the method being tested
+            Program.ActionDelegateSample();
+
+            // Assert
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod]
+        public void FuncDelegateSample_Test()
+        {
+            // Arrange
+            double radius = 20;
+            double expectedArea = 3.12 * radius * radius;
+            double actualArea = 0;
+
+            // Act
+            Func<double, double> testCalArea = r =>
+            {
+                return 3.12 * r * r;
+            };
+
+            // Invoke the method being tested
+            Program.FuncDelegateSample();
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+    }
+}
