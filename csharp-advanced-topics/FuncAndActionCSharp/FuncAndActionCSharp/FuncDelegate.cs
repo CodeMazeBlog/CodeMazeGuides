0285407fd4532b33f3e75819e52c389c439c5bc9
pavsavov <pav.savov@gmail.com>
2023-11-20 09:53:11 +0200

    Removed redudndant parameter

--- csharp-advanced-topics/FuncAndActionCSharp/FuncAndActionCSharp/FuncDelegate.cs
+++ csharp-advanced-topics/FuncAndActionCSharp/FuncAndActionCSharp/FuncDelegate.cs
@@ -3,7 +3,7 @@
     public IEnumerable<int> PerformFiltration(IEnumerable<int> numbers)
     {
         //Func delegate for filtering even numbers
-        Func<int, int, bool> isEven = (x, y) => x % 2 == 0;
+        Func<int, bool> isEven = (x) => x % 2 == 0;
 
         //Use the Func delegate in a LINQ query
         var evenNumbers = numbers.Where(isEven);
