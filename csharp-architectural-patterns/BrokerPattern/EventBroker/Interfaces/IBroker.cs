fb2a67d6d1320c54dcdee90e6fb3dcd751e160e9
Muhammed Saleem <muhammedsalimp@gmail.com>
2023-11-22 20:34:33 +0530

    file scoped namespace

--- csharp-architectural-patterns/BrokerPattern/EventBroker/Interfaces/IBroker.cs
+++ csharp-architectural-patterns/BrokerPattern/EventBroker/Interfaces/IBroker.cs
@@ -1,8 +1,7 @@
-﻿namespace EventBroker.Interfaces
-{
+﻿namespace EventBroker.Interfaces;
+
 public interface IBroker
 {
     void Publish(Message message);
     void Subscribe(string topic, Action<Message> callback);
-}
 }
