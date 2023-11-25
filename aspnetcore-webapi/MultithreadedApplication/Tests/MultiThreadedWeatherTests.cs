2a4ce3e1198bc72fbef0289b88fe3c3823a3ba69
thisisalin <alintroncea@gmail.com>
2023-11-25 12:26:55 +0200

    Added unit test. Made the required changes from the word document.

--- aspnetcore-webapi/MultithreadedApplication/Tests/MultiThreadedWeatherTests.cs
+++ aspnetcore-webapi/MultithreadedApplication/Tests/MultiThreadedWeatherTests.cs
@@ -1,11 +1,28 @@
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MultithreadedApplication.Controllers;
+
 namespace Tests
 {
     public class MultiThreadedWeatherTests
     {
         [Fact]
-        public void Test1()
+        public async Task WhenCallingWeatherController_ThenItReturnsData()
         {
+            // Arrange
+            var serviceProvider = new ServiceCollection()
+                                    .AddLogging()
+                                    .BuildServiceProvider();
 
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+
+            var logger = factory.CreateLogger<WeatherForecastController>();
+
+            var controller = new WeatherForecastController(logger);
+
+            var weather = await controller.GetWeather();
+            Assert.NotNull(weather);
         }
     }
 }
