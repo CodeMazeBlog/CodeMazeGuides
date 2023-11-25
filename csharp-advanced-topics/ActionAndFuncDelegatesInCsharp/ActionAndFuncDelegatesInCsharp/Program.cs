be7b4b477068a932e784bd74db47af486ff35544
YafeshanUnal <yafeshan01@gmail.com>
2023-11-25 14:32:21 +0300

    fix: test issues and .net version

--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 
-class Program
+public class Program
 {
     static void Main()
     {
-        Action<int, int> addActionDelegate = (a, b) => Console.WriteLine($"\nSum Action Delegate: {a + b}\n");
-        Func<int, int, int> addFuncDelegate = (a, b) => a + b;
-
         addActionDelegate(3, 5);
-        int resultFuncDelegate = addFuncDelegate(3, 5);
 
-        Console.WriteLine($"\nSum Func Delegate: {resultFuncDelegate}\n");
+        Console.WriteLine($"\nSum Func Delegate: {addFuncDelegate(3, 5)}\n");
+    }
+
+    public static void addActionDelegate(int a, int b)
+    {
+        Action<int, int> sum = (a, b) => Console.WriteLine($"\nSum Action Delegate: {a + b}\n");
+        sum(a, b);
+    }
+
+    public static int addFuncDelegate(int a, int b)
+    {
+        Func<int, int, int> sum = (a, b) => a + b;
+        return sum(a, b);
     }
 }
--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/Tests/Tests.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/Tests/Tests.cs
@@ -1,35 +1,22 @@
-namespace Tests
+
+public class Tests
 {
-    public class Tests
+    [Fact]
+    public void GivenActionDelegate_WhenAddActionDelegateIsCalled_ThenSumIsPrinted()
     {
-        [Fact]
-        public void TestActionDelegate()
-        {
-            // Arrange
-            Action<int, int> addActionDelegate = (a, b) => Console.WriteLine($"\nSum Action Delegate: {a + b}\n");
-            int expected = 8;
+        // This function is not testable because it writes to the console and does not return anything.
+        Program.addActionDelegate(3, 5);
+    }
 
-            // Act
-            addActionDelegate(3, 5);
+    [Fact]
+    public void GivenFuncDelegate_WhenAddFuncDelegateIsCalled_ThenSumIsReturned()
+    {
+        Func<int, int, int> addFuncDelegate = (a, b) => a + b;
+        int expected = 8;
+        int resultFuncDelegate = 0;
 
-            // Assert
-            Assert.Equal(expected, 8);
+        resultFuncDelegate = Program.addFuncDelegate(3, 5);
 
-
-        }
-
-        [Fact]
-        public void TestFuncDelegate()
-        {
-            // Arrange
-            Func<int, int, int> addFuncDelegate = (a, b) => a + b;
-            int expected = 8;
-
-            // Act
-            int resultFuncDelegate = addFuncDelegate(3, 5);
-
-            // Assert
-            Assert.Equal(expected, resultFuncDelegate);
-        }
+        Assert.Equal(expected, resultFuncDelegate);
     }
 }
