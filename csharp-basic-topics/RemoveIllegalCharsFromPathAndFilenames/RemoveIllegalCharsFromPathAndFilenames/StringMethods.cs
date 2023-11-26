5c57608c07f694f8acde6ec1f7b09133f538119c
Sam <113020810+samhascat@users.noreply.github.com>
2023-11-26 20:49:19 +0000

    PR Fix for updated scope

---
+++ csharp-basic-topics/RemoveIllegalCharsFromPathAndFilenames/RemoveIllegalCharsFromPathAndFilenames/StringMethods.cs
@@ -0,0 +1,42 @@
+﻿public static class StringMethods
+{
+    public static IEnumerable<string> CheckForInvalid(IEnumerable<string> strings, HashSet<char> criteria)
+    {
+        IEnumerable<string> result = new List<string>();
+
+        return strings.Where(str => criteria.Any(ch => str.Contains(ch)));
+    }
+
+    public static IEnumerable<string> CheckForInvalidLINQ(IEnumerable<string> strings, HashSet<char> criteria)
+    {
+        return strings.Where(str => str.Any(criteria.Contains)).ToList();
+    }
+
+    public static IEnumerable<string> CheckForInvalidLINQHeader(IEnumerable<string> strings, HashSet<char> criteria)
+    {
+        var result = from str in strings
+                     where str.Any(criteria.Contains)
+                     select str;
+
+        return result.ToList();
+    }
+
+    public static IEnumerable<string> CheckForInvalidRegEx(IEnumerable<string> strings, HashSet<char> criteria) 
+    {
+        Regex invalidChars = new Regex("[" + Regex.Escape(string.Join("", criteria)) + "]");
+
+        return strings.Where(str => invalidChars.IsMatch(str));
+    }
+
+    public static IEnumerable<string> GetFileNames(IEnumerable<string> paths)
+    {
+        List<string> result = new List<string>();
+
+        return paths.Select(path => Path.GetFileName(path)).Where(path => !string.IsNullOrEmpty(path));
+    }
+
+    public static string RemoveInvalidChars(string path, HashSet<char> criteria)
+    {
+        return criteria.Aggregate(path, (a, b) => a.Replace(b.ToString(), String.Empty));
+    }
+}
---
+++ csharp-basic-topics/RemoveIllegalCharsFromPathAndFilenames/Tests/InvalidCharsTests.cs
@@ -0,0 +1,73 @@
+public class InvalidCharactersTests
+{
+    // known invalid paths
+    private static readonly List<string> TestCaseSearch = new List<string> 
+    {
+        "C://User/Test/Stuff/Invalid>>>>\r<<<<--Chars",
+        "D://User/iLi\0keToIncludeINVALIDChars/*/     z"
+    };
+    private static readonly List<string> TestCaseFilenames = new List<string>
+    {
+        "A://ValidPath/SomeFolder/Invalid??File??Broken.nah",
+        "I://ProgramFiles/SomeApplication/:Invalid:Filename.txt"
+    };
+    private static readonly HashSet<char> invalidPathChars = new HashSet<char>(Path.GetInvalidPathChars());
+    private static readonly HashSet<char> invalidFilenameChars = new HashSet<char>(Path.GetInvalidFileNameChars());
+
+    [Test]
+    public void WhenGivenListOfStrings_ThenFindStringsWithInvalidChars()
+    {
+        var result = StringMethods.CheckForInvalid(TestCaseSearch, invalidPathChars);
+        Assert.That(TestCaseSearch, Is.EqualTo(result));
+    }
+        
+    [Test]
+    public void WhenGivenlListOfStrings_ThenFindStringsWithInvalidCharsUsingLINQ()
+    {
+        var result = StringMethods.CheckForInvalidLINQ(TestCaseSearch, invalidPathChars);
+        Assert.That(TestCaseSearch, Is.EqualTo(result));
+    }
+        
+    [Test]
+    public void WhenGivenListOfStrings_ThenFindStringsWithInvalidCharsUsingLINQHeaderFormat()
+    {
+        var result = StringMethods.CheckForInvalidLINQHeader(TestCaseSearch, invalidPathChars);
+        Assert.That(TestCaseSearch, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void WhenGivenListOfStrings_ThenFindStringsWithInvalidCharsUsingRegEx()
+    {
+        var result = StringMethods.CheckForInvalidRegEx(TestCaseSearch, invalidPathChars);
+        Assert.That(TestCaseSearch, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void WhenGivenListOfPaths_ThenGetFilenames()
+    {
+        var result = StringMethods.GetFileNames(TestCaseFilenames);
+        List<string> expected = new List<string>() 
+        {
+            "Invalid??File??Broken.nah",
+            ":Invalid:Filename.txt"
+        };
+
+        Assert.That(expected, Is.EqualTo(result));
+    }
+        
+    [Test]
+    public void WhenGivenInvalidFilenameList_ThenRemoveInvalidChars()
+    {
+        foreach (string path in TestCaseFilenames)
+        {
+            var result = StringMethods.RemoveInvalidChars(path, invalidFilenameChars);
+
+            if (result.Any(invalidFilenameChars.Contains))
+            {
+                Assert.Fail();                    
+            }
+        }
+
+        Assert.Pass();
+    }
+}
