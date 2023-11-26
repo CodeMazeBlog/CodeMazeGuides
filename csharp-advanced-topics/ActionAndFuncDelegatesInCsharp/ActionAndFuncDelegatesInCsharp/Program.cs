87f3f03d1614d27c1eb450d0d55bb5e987c6741b
YafeshanUnal <yafeshan01@gmail.com>
2023-11-26 13:34:10 +0300

    fix: update tests and convert name

--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -4,18 +4,18 @@ public class Program
 {
     static void Main()
     {
-        addActionDelegate(3, 5);
+        AddActionDelegate(3, 5);
 
-        Console.WriteLine($"\nSum Func Delegate: {addFuncDelegate(3, 5)}\n");
+        Console.WriteLine($"Sum Func Delegate: {AddFuncDelegate(3, 5)}");
     }
 
-    public static void addActionDelegate(int a, int b)
+    public static void AddActionDelegate(int a, int b)
     {
-        Action<int, int> sum = (a, b) => Console.WriteLine($"\nSum Action Delegate: {a + b}\n");
+        Action<int, int> sum = (a, b) => Console.WriteLine($"Sum Action Delegate: {a + b}");
         sum(a, b);
     }
 
-    public static int addFuncDelegate(int a, int b)
+    public static int AddFuncDelegate(int a, int b)
     {
         Func<int, int, int> sum = (a, b) => a + b;
         return sum(a, b);
--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/Tests/Tests.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/Tests/Tests.cs
@@ -4,18 +4,20 @@ public class Tests
     [Fact]
     public void GivenActionDelegate_WhenAddActionDelegateIsCalled_ThenSumIsPrinted()
     {
-        // This function is not testable because it writes to the console and does not return anything.
-        Program.addActionDelegate(3, 5);
+        var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        Program.AddActionDelegate(3, 5);
+
+        var expectedOutput = "Sum Action Delegate: 8" + Environment.NewLine;
+        Assert.Equal(expectedOutput, sw.ToString());
     }
 
     [Fact]
     public void GivenFuncDelegate_WhenAddFuncDelegateIsCalled_ThenSumIsReturned()
     {
-        Func<int, int, int> addFuncDelegate = (a, b) => a + b;
-        int expected = 8;
-        int resultFuncDelegate = 0;
-
-        resultFuncDelegate = Program.addFuncDelegate(3, 5);
+        var expected = 8;
+        var resultFuncDelegate = Program.AddFuncDelegate(3, 5);
 
         Assert.Equal(expected, resultFuncDelegate);
     }
