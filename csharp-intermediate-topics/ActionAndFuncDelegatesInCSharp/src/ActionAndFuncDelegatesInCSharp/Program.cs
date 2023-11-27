19e8f20e655b78699b20cee140d5df6c9969db91
Kai Westerschwiensterdt <K.Westerschwiensterdt@beckhoff.com>
2023-11-27 08:50:32 +0100

    Create action and func delegates

---
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/src/ActionAndFuncDelegatesInCSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿// Action delegate
+Action<string> helloAction = SayHello;
+Action<string> goodbyeAction = SayGoodbye;
+InteractAction(helloAction, "John");
+InteractAction(goodbyeAction, "John");
+
+static void InteractAction(Action<string> action, string message)
+{
+    action(message);
+}
+
+static void SayHello(string name)
+{
+    string message = $"Hello {name}";
+    Console.WriteLine($"Message: {message}");
+}
+
+static void SayGoodbye(string name)
+{
+    string message = $"Goodbye {name}";
+    Console.WriteLine($"Message: {message}");
+}
+
+// Func delegate
+Func<int, int, int> addFunc = AddNumbers;
+Func<int, int, int> subtractFunc = SubtractNumbers;
+int addResult = CalculateFunc(addFunc, 7, 5);
+int subtractResult = CalculateFunc(subtractFunc, 7, 5);
+
+static TResult CalculateFunc<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 arg1, T2 arg2)
+{
+    return func(arg1, arg2);
+}
+
+static int AddNumbers(int a, int b)
+{
+    return a + b;
+}
+
+static int SubtractNumbers(int a, int b)
+{
+    return a - b;
+}
