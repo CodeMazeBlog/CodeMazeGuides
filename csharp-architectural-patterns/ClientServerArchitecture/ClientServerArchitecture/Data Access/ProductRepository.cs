1ce22b47bfe13ece91a27f7a8798d3ce15235cbe
Satya <satyap48@gmail.com>
2023-11-27 15:26:57 +0530

    CM-830: Client-Server Architectural Pattern in C#

---
+++ csharp-architectural-patterns/ClientServerArchitecture/ClientServerArchitecture/Data Access/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ClientServerArchitecture.Data;
+using ClientServerArchitecture.Models;
+
+namespace ClientServerArchitecture.Data_Access
+{
+    public class ProductRepository
+    {
+        private readonly ProductDataContext _context;
+
+        public ProductRepository(ProductDataContext context)
+        {
+            _context = context;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ csharp-architectural-patterns/ClientServerArchitecture/ClientServerArchitecture/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using ClientServerArchitecture.Data_Access;
+using ClientServerArchitecture.Models;
+
+namespace ClientServerArchitecture.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductService(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _productRepository.GetProducts();
+        }
+
+        public void AddProduct(Product product)
+        {
+            _productRepository.AddProduct(product);
+        }
+    }
+}
