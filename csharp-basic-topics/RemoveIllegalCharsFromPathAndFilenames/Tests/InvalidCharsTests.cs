804072e3bd534ad5139c5b228ebc5bcf57663f89
Sam <113020810+samhascat@users.noreply.github.com>
2023-11-28 20:48:48 +0000

    Jenkins Build Fix 2

--- csharp-basic-topics/RemoveIllegalCharsFromPathAndFilenames/Tests/InvalidCharsTests.cs
+++ csharp-basic-topics/RemoveIllegalCharsFromPathAndFilenames/Tests/InvalidCharsTests.cs
@@ -1,12 +1,12 @@
 public class InvalidCharactersTests
 {
     // known invalid paths
-    private static readonly List<string> TestCaseSearch = new List<string> 
+    private static readonly IEnumerable<string> TestCaseSearch = new List<string> 
     {
         "C://User/Test/Stuff/Invalid>>>>\r<<<<--Chars",
         "D://User/iLi\0keToIncludeINVALIDChars/*/     z"
     };
-    private static readonly List<string> TestCaseFilenames = new List<string>
+    private static readonly IEnumerable<string> TestCaseFilenames = new List<string>
     {
         "A://ValidPath/SomeFolder/Invalid??File??Broken.nah",
         "I://ProgramFiles/SomeApplication/:Invalid:Filename.txt"
@@ -18,28 +18,28 @@ public class InvalidCharactersTests
     public void WhenGivenListOfStrings_ThenFindStringsWithInvalidChars()
     {
         var result = StringMethods.CheckForInvalid(TestCaseSearch, invalidPathChars);
-        Assert.That(TestCaseSearch, Is.EqualTo(result));
+        CollectionAssert.AreEquivalent(TestCaseSearch, result.ToList());
     }
         
     [Test]
     public void WhenGivenlListOfStrings_ThenFindStringsWithInvalidCharsUsingLINQ()
     {
         var result = StringMethods.CheckForInvalidLINQ(TestCaseSearch, invalidPathChars);
-        Assert.That(TestCaseSearch, Is.EqualTo(result));
+        CollectionAssert.AreEquivalent(TestCaseSearch, result.ToList());
     }
         
     [Test]
     public void WhenGivenListOfStrings_ThenFindStringsWithInvalidCharsUsingLINQHeaderFormat()
     {
         var result = StringMethods.CheckForInvalidLINQHeader(TestCaseSearch, invalidPathChars);
-        Assert.That(TestCaseSearch, Is.EqualTo(result));
+        CollectionAssert.AreEquivalent(TestCaseSearch, result.ToList());
     }
 
     [Test]
     public void WhenGivenListOfStrings_ThenFindStringsWithInvalidCharsUsingRegEx()
     {
         var result = StringMethods.CheckForInvalidRegEx(TestCaseSearch, invalidPathChars);
-        Assert.That(TestCaseSearch, Is.EqualTo(result));
+        CollectionAssert.AreEquivalent(TestCaseSearch, result.ToList());
     }
 
     [Test]
@@ -52,7 +52,7 @@ public class InvalidCharactersTests
             ":Invalid:Filename.txt"
         };
 
-        Assert.That(expected, Is.EqualTo(result));
+        CollectionAssert.AreEquivalent(expected, result);
     }
         
     [TestCaseSource(nameof(TestCaseFilenames))]
