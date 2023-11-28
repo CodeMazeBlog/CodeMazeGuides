6519a510637aff07157db687e1d06f0a9a3215d2
Shiva Kumar K <shivakumarkotakondaskk@gmail.com>
2023-11-28 23:01:32 +0530

    Added Test

--- csharp-intermediate-topics/Action and Func Delegate/Action and Func Delegate/Program.cs
+++ csharp-intermediate-topics/Action and Func Delegate/Action and Func Delegate/Program.cs
@@ -1,20 +1,34 @@
-﻿namespace Action_and_Func_Delegate
+﻿using System.Collections.Generic;
+
+namespace Action_and_Func_Delegate
 {
-    class Program
+   public class Program
     {
         public static int Increment(int originalValue, int incrementValue)
         { 
             return originalValue + incrementValue; 
+        }
+        public static IEnumerable<int> GetEvenNumbers(List<int> numbers, Func<int, bool> isEven) 
+        {
+            IEnumerable<int> evenList = numbers.Where(isEven);
+            return evenList;
+        }
+
+        public static void PrintNumbers(List<int> numbers, Action<int> print) 
+        {
+            numbers.ForEach(print);
         }
         static void Main(string[] args)
         {
             List<int> list = new List<int>(){1,2,3,4,5,6,7,8,9,10,11};
 
             Func<int,bool> isEven=num=>num%2==0;
-            IEnumerable<int> evenList = list.Where(isEven);
+            IEnumerable<int> evenList = GetEvenNumbers(list, isEven);
+                //list.Where(isEven);
 
             Action<int> print = num => Console.WriteLine(num);
-            evenList.ToList().ForEach(print);
+            PrintNumbers(evenList.ToList(),print);
+            //evenList.ToList().ForEach(print);
 
            
             Func<int, int, int> incrementDelegate = Increment; 
---
+++ csharp-intermediate-topics/Action and Func Delegate/Test/DelegatesTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Action_and_Func_Delegate;
+
+namespace Test
+{
+    [TestFixture]
+    public class DelegatesTest
+    {
+        [Test]
+        public void TestGetEvenNumbers()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Func<int, bool> isEven = num => num % 2 == 0;
+
+            IEnumerable<int> evenNumbers = Program.GetEvenNumbers(numbers, isEven); 
+            Assert.AreEqual(new List<int> { 2, 4, 6, 8, 10 }, evenNumbers);
+        }
+
+        [Test]
+        public void TestPrintNumbers()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            IEnumerable<int> numbers = new List<int> { 2, 4, 6, 8, 10 };
+            Action<IEnumerable<int>> printNumbers = list => list.ToList().ForEach(Console.WriteLine);
+
+            printNumbers(numbers);
+
+            string output = writer.GetStringBuilder().ToString().Trim();
+            Assert.AreEqual("2\r\n4\r\n6\r\n8\r\n10", output);
+        }
+    }
+}
