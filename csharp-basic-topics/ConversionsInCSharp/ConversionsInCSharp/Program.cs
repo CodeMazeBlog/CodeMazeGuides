f7d1082cbd55f9904aa4ccaeba974743349cae71
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-11-28 19:18:24 +0100

    CM-96: Add methods to test conversions and create tests

--- csharp-basic-topics/ConversionsInCSharp/ConversionsInCSharp/Program.cs
+++ csharp-basic-topics/ConversionsInCSharp/ConversionsInCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConversionsInCSharp;
+using System.Globalization;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 // basic examples
@@ -100,4 +101,73 @@ Utilities.ConvertFromStringWithBase("51", 10);
 Utilities.ConvertFromStringWithBase("3A", 16);
 Utilities.ConvertFromStringWithBase("65", 5);
 Console.WriteLine();
- 
+
+// CONVERSIONS TO BOOLEANS 
+
+// Numeric types
+Console.WriteLine("Conversions to booleans - numeric types");
+Utilities.MakeConversion(Convert.ToBoolean, 5);
+Utilities.MakeConversion(Convert.ToBoolean, 0);
+Utilities.MakeConversion(Convert.ToBoolean, -19.875);
+Utilities.MakeConversion(Convert.ToBoolean, 4.65f);
+Utilities.MakeConversion(Convert.ToBoolean, 170.54m);
+Console.WriteLine();
+
+// strings
+Console.WriteLine("Conversions to booleans - strings");
+Utilities.MakeConversion(Convert.ToBoolean, "True");
+Utilities.MakeConversion(Convert.ToBoolean, "false");
+Console.WriteLine();
+
+// CONVERSIONS BETWEEN NUMERIC AND NON-NUMERIC TYPES
+
+// bool to numeric
+Console.WriteLine("Conversions from booleans to numeric types");
+Utilities.MakeConversion(Convert.ToInt32, true);
+Utilities.MakeConversion(Convert.ToInt32, false);
+Console.WriteLine();
+
+// string to numeric
+Console.WriteLine("Conversions from strings to numeric types");
+Utilities.MakeConversion(Convert.ToInt32, "128");
+Utilities.MakeConversion(Convert.ToDouble, "1.83e3");
+Console.WriteLine();
+
+// char to numeric and numeric to char
+Console.WriteLine("Conversions between chars and numeric types");
+Utilities.MakeConversion(Convert.ToChar, 65);
+Utilities.MakeConversion(Convert.ToChar, 41);
+Utilities.MakeConversion(Convert.ToInt32, '&');
+Console.WriteLine();
+
+// CONVERSIONS WITH DATETIME
+
+Console.WriteLine("Conversions with DateTime");
+Utilities.MakeConversion(Convert.ToString, new DateTime(2023, 11, 27, 9, 45, 28));
+Utilities.MakeConversion(Convert.ToDateTime, "8/2/2003 12:00:00 AM");
+Utilities.MakeConversion(Convert.ToDateTime, "02 June 1985 3:20:17 AM");
+Console.WriteLine();
+
+// CONVERSIONS WITH IFORMATPROVIDER
+
+Console.WriteLine("Conversions with IFormatProvider");
+CultureInfo cultureDE = new("de-AT");
+string deDate = "15/2/2021";
+
+Console.WriteLine(
+    $"Austrian culture: {deDate} --- US culture: {Convert.ToDateTime(deDate, cultureDE).ToShortDateString()}");
+
+string deNumber = "280,99";
+
+Console.WriteLine(
+    $"Austrian culture: {deNumber} --- US culture: {Convert.ToDecimal(deNumber, cultureDE)}");
+
+Console.WriteLine();
+
+// CONVERSIONS FROM CUSTOM OBJECTS
+
+Console.WriteLine("Conversions from the custom Note type");
+Note note = new('C');
+Utilities.MakeConversion(Convert.ToBoolean, note);
+Utilities.MakeConversion(Convert.ToInt64, note);
+Utilities.MakeConversion(Convert.ToChar, note);
