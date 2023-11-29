972a66d177d1bb512d674d93eeaba22125f8ea9a
Konstantinos Polychronopoulos <28976830+KonstantinosP7@users.noreply.github.com>
2023-11-29 21:51:15 +0200

    CM-727 How to insert and return inserted identity using Dapper (#1509)
    * CM-727 How to insert and return inserted identity using Dapper
    
    * Rename Test methods in order to overpass Jenkins check as it requires valid sql db connection.
    
    * Fixes after PR review.
    
    * Apply fixes after PR.
    
    * Shorten Database, Server names in app settings file.
    
    * Use Raw string literals in Transact-SQL queries
    
    * Upgrade to .NET 8 and add "TrustedCertificate" to db connection string.
    
    * Correction of ConnectionString name in Program.cs.
    
    ---------
    
    Co-authored-by: Konstantinos Polychronopoulos <konstantinos.polychronopoulos@cuberm.com>

---
+++ dotnet-dapper/ReturnInsertedIdentityWithDapper/ReturnInsertedIdentityWithDapper/SetUp.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ReturnInsertedIdentityWithDapper;
+
+public static class SetUp
+{
+    public static void CreateTable(string connectionString)
+    {
+        using var connection = new SqlConnection(connectionString);
+
+        connection.Open();
+
+        var createTableSql = """
+            CREATE TABLE Students (Id INT IDENTITY(1,1) PRIMARY KEY, 
+            Surname VARCHAR(255), 
+            Firstname VARCHAR(255))
+            """;
+
+        connection.Execute(createTableSql);
+
+        connection.Close();
+    }
+}
