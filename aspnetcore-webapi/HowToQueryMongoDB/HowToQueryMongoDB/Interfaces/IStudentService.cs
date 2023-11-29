633f75cf4a5b0f179eac2fbfe2ed36c9e1d7f925
king-Alex-d-great <ogubuikealex@gmail.com>
2023-11-29 09:36:44 +0100

    Feat: Original Setup

---
+++ aspnetcore-webapi/HowToQueryMongoDB/HowToQueryMongoDB/Interfaces/IStudentService.cs
@@ -0,0 +1,26 @@
+﻿using HowToQueryMongoDB.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace HowToQueryMongoDB.Interfaces;
+
+public interface IStudentService
+{
+    Task<Student?> Create(Student student);
+
+    Task<DeleteResult> Delete(string id);
+
+    Task<List<Student>> GetAll();
+
+    Task<Student?> GetById(string id);
+
+    Task<ReplaceOneResult> Update(string id, Student student);
+
+    Task<Student> GetByIdUsingBuilder(string id);
+
+    IEnumerable<Student> GetRangeOfStudentsUsingAnd(string minimumId, string maximumId);
+
+    IEnumerable<Student> GetRangeOfStudentsUsingOr(string minimumId, string maximumId);
+
+    IEnumerable<Student> GetAllStudentsInTheList(string ids);
+}
