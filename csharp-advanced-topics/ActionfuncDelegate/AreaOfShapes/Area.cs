a24e1412a3058bcb215a715698f41c9e7a5728ed
Lucky Otono <51697000+otono-lucky@users.noreply.github.com>
2023-12-03 19:46:16 +0100

    Area of Shapes

---
+++ csharp-advanced-topics/ActionfuncDelegate/AreaOfShapes/Area.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionfuncDelegate.AreaOfShapes
+{
+    public class Area
+    {
+        public static Action<string, double> DisplayResult = (shape, area) =>
+        {
+            Console.WriteLine($"The area of the {shape} is: {area}");
+        };
+
+
+        public static Func<double, double, double> RectangleArea = (length, width) =>
+        {
+            return length * width;
+        };
+
+        public static Func<double, double> CircleArea = radius =>
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        };
+
+        public static Func<double, double, double, double> TriangleArea = (a, b, c) =>
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        };
+    }
+}
---
+++ csharp-advanced-topics/ActionfuncDelegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using ActionfuncDelegate.AreaOfShapes;
+
+namespace ActionfuncDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rectangleResult = Area.RectangleArea(5, 10);
+            double circleResult = Area.CircleArea(7);
+            double triangleResult = Area.TriangleArea(3, 4, 5);
+
+            Area.DisplayResult("rectangle", rectangleResult);
+            Area.DisplayResult("circle", circleResult);
+            Area.DisplayResult("triangle", triangleResult);
+        }
+    }
+}
