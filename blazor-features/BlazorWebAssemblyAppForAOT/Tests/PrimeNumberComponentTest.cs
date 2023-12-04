031796b4a007971ce2cbbb43088c25919a6ce4b2
cjaynjoku <76001800+cjaynjoku@users.noreply.github.com>
2023-12-04 10:58:37 +0100

    CM-175: Blazor WebAssembly Ahead of Time Compilation (AoT) (#1542)

---
+++ blazor-features/BlazorWebAssemblyAppForAOT/Tests/PrimeNumberComponentTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    public class PrimeNumberComponentTest : TestContext
+    {
+        [Fact]
+        public void GivenPrimeNumberComponentWhenInitializedReturnsListOfPrimeNumbers()
+        {
+            var mockPrimeNumberService = new Mock<IPrimeNumberService>();
+            mockPrimeNumberService.Setup(service => service.GetPrimeNumbersAsync())
+                .ReturnsAsync([2, 3, 5, 7, 11]);
+            Services.AddSingleton(mockPrimeNumberService.Object);
+
+            var cut = RenderComponent<PrimeNumber>();
+            var timeout = TimeSpan.FromSeconds(5);
+            cut.WaitForAssertion(() => Assert.NotNull(cut.Instance.PrimeNumbers), timeout);
+
+            Assert.IsType<List<int>>(cut.Instance.PrimeNumbers);
+            Assert.Equal([2, 3, 5, 7, 11], cut.Instance.PrimeNumbers);
+        }
+    }
+}
