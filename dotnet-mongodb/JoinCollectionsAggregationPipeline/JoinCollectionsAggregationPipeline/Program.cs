557d52a8f741d08e1ea2870a6cfd0dc10d5bf4f8
Houssam Ghannoum <housgh23@gmail.com>
2023-12-04 17:46:57 +0200

    Added test container in Program.cs

--- dotnet-mongodb/JoinCollectionsAggregationPipeline/JoinCollectionsAggregationPipeline/Program.cs
+++ dotnet-mongodb/JoinCollectionsAggregationPipeline/JoinCollectionsAggregationPipeline/Program.cs
@@ -1,8 +1,50 @@
 ﻿using MongoDB.Bson;
 using JoinCollectionsAggregationPipeline;
 using MongoDB.Driver;
+using Testcontainers.MongoDb;
 
-var repository = new StudentRepository(new MongoClient(DatabaseConfiguration.ConnectionString));
+var mongoDbContainer =
+        new MongoDbBuilder().Build();
+
+await mongoDbContainer.StartAsync();
+var mongoClient = new MongoClient(mongoDbContainer.GetConnectionString());
+var repository = new StudentRepository(mongoClient);
+
+//Seed Data
+var database = mongoClient.GetDatabase(DatabaseConfiguration.DatabaseName);
+var courseCollection = database.GetCollection<BsonDocument>("Courses");
+var studentCollection = database.GetCollection<BsonDocument>("Students");
+courseCollection.InsertMany(new List<BsonDocument>
+{
+    new()
+    {
+        { "_id", new ObjectId("655e134180c300fcdd067d24") } ,
+        { "Name", "Networks" },
+        { "Code", "ECEN 474" }
+    },
+    new()
+    {
+        { "_id", new ObjectId("655e134180c300fcdd067d25") } ,
+        { "Name", "Power Systems" },
+        { "Code", "ECEN 485" }
+    }
+});
+
+studentCollection.InsertMany(new List<BsonDocument>
+{
+    new()
+    {
+        { "FirstName", "John" },
+        { "LastName", "Doe" },
+        { "Major", "Electrical Engineering" },
+        { "Courses", new BsonArray {
+            new ObjectId("655e134180c300fcdd067d24"),
+            new ObjectId("655e134180c300fcdd067d25")
+        } }
+    }
+});
+
+
 var users = await repository.GetAllUsers();
 foreach (var user in users)
 {
