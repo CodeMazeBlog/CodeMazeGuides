b8c31fa0dbf64f4b0ca398ae52e6465a01f82f0d
Alvaro Montoya <alvaro.montoya@getrhyme.com>
2023-12-03 22:14:18 -0500

    final changes

--- csharp-architectural-patterns/AppWithPlugin/PluginLoadContext.cs
+++ csharp-architectural-patterns/AppWithPlugin/PluginLoadContext.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppWithPlugin
+﻿namespace AppWithPlugin
 {
     class PluginLoadContext : AssemblyLoadContext
     {
