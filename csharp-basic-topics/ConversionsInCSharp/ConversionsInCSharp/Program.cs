462c5e1bb2698d37a11bae0a787ed4181730bd75
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-12-06 13:54:42 +0100

    CM-96: Remove unused usings

--- csharp-basic-topics/ConversionsInCSharp/ConversionsInCSharp/Program.cs
+++ csharp-basic-topics/ConversionsInCSharp/ConversionsInCSharp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using ConversionsInCSharp;
 using System.Globalization;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 // basic examples
 Console.WriteLine("Some basic examples");
