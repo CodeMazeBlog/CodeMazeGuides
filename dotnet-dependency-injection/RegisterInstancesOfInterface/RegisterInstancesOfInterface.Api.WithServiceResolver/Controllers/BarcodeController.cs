00b5566262bd9df8ab982e949e7aa48dd3ba34d1
Michal Kaminski <kaminski.mi@wp.pl>
2023-12-08 13:05:37 +0100

    Moved solution to dotnet-dependency-injection directory

--- dotnet-dependency-injection/RegisterInstancesOfInterface/RegisterInstancesOfInterface.Api.WithServiceResolver/Controllers/BarcodeController.cs
+++ dotnet-dependency-injection/RegisterInstancesOfInterface/RegisterInstancesOfInterface.Api.WithServiceResolver/Controllers/BarcodeController.cs
@@ -9,6 +9,7 @@ namespace RegisterInstancesOfInterface.Api.WithServiceResolver.Controllers;
 public class BarcodeController : ControllerBase
 {
     private readonly IFulfillTickets _processor;
+
     public BarcodeController(FulfillmentProcessorResolver serviceResolver)
     {
         _processor = serviceResolver("barcode");
