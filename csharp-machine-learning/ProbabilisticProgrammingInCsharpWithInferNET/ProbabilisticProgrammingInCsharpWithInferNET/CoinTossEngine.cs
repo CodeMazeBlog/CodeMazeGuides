4e3b7749e70ab32c4a578c7ec43664df3cd25854
Ivan Matec <im@bullwall.com>
2023-12-10 11:38:27 +0100

    - implemented CoinTossEngine class and basic tests

---
+++ csharp-machine-learning/ProbabilisticProgrammingInCsharpWithInferNET/ProbabilisticProgrammingInCsharpWithInferNET/CoinTossEngine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbabilisticProgrammingInCsharpWithInferNET
+{
+    public class CoinTossEngine
+    {
+        Random random = new Random();
+        public bool FirstHead { get; private set; }
+        public bool SecondHead { get; private set; }
+        public bool BothHeads { get; private set; }
+
+        public int FirstCoinHeadCount { get; private set; }
+        public int SecondCoinHeadCount { get; private set; }
+        public int BothCoinsHeadCount { get; private set; }
+        public int RunCount { get; private set; }
+
+        public double FirstCoinPercentage { get; private set; }
+        public double SecondCoinPercentage { get; private set; }
+        public double BothCoinsPercentage { get; private set; }
+
+        public void TossTheCoins(int numberOfRuns = 1000000, Func<CoinTossEngine, bool> condition = null)
+        {
+            for (int i = 0; i < numberOfRuns; i++)
+            {
+                FirstHead = random.NextDouble() > 0.5;
+                SecondHead = random.NextDouble() > 0.5;
+                BothHeads = FirstHead && SecondHead;
+
+                if (condition == null || condition(this))
+                {
+                    if (FirstHead) FirstCoinHeadCount++;
+                    if (SecondHead) SecondCoinHeadCount++;
+                    if (BothHeads) BothCoinsHeadCount++;
+                    RunCount++;
+                }
+            }
+            FirstCoinPercentage = Math.Round(100.0 * FirstCoinHeadCount / RunCount, 2);
+            SecondCoinPercentage = Math.Round(100.0 * SecondCoinHeadCount / RunCount, 2);
+            BothCoinsPercentage = Math.Round(100.0 * BothCoinsHeadCount / RunCount, 2);
+        }
+
+        private bool IsHead { get { return random.NextDouble() > 0.5; } }
+    }
+}
---
+++ csharp-machine-learning/ProbabilisticProgrammingInCsharpWithInferNET/ProbabilisticProgrammingInCsharpWithInferNETTests/SimpleCoinTossEngineTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilisticProgrammingInCsharpWithInferNET;
+using System;
+
+namespace ProbabilisticProgrammingInCsharpWithInferNETTests
+{
+    [TestClass]
+    public class SimpleCoinTossEngineTests
+    {
+        CoinTossEngine coinTossEngine = new CoinTossEngine();
+
+        [TestMethod]
+        public void WhenBothHeadsTrue_ThanSuccess()
+        {
+            coinTossEngine.TossTheCoins();
+
+            Console.WriteLine($"P - First Coin True: {coinTossEngine.FirstCoinPercentage}%");
+            Console.WriteLine($"P - Second Coin True: {coinTossEngine.SecondCoinPercentage}%");
+            Console.WriteLine($"P - Both Coins True: {coinTossEngine.BothCoinsPercentage}%");
+
+            Assert.IsTrue(Math.Round(coinTossEngine.BothCoinsPercentage, 0) == 25.0);
+        }
+
+        [TestMethod]
+        public void WhenBothHeadsNotTrue_ThanSuccess()
+        {
+            coinTossEngine.TossTheCoins(condition: m => !m.BothHeads);
+
+            Console.WriteLine($"P - First Coin Is Head: {coinTossEngine.FirstCoinPercentage}%");
+            Console.WriteLine($"P - Second Coin Is Head: {coinTossEngine.SecondCoinPercentage}%");
+            Console.WriteLine($"P - Both Coins Are Heads: {coinTossEngine.BothCoinsPercentage}%");
+
+            Assert.IsTrue(Math.Round(coinTossEngine.FirstCoinPercentage, 0) == 33.0);
+        }
+    }
+}
