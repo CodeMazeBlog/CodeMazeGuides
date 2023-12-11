e91665294078595d9a681f74d7ab48f8c38087ac
Manase <manasesmutembei11@gmail.com>
2023-12-11 12:29:47 +0300

    func and action delegates

---
+++ csharp-basic-topics/AccessModifiersInCsharp/Action.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        // Define an Action delegate with no parameters
+        Action printMessage = () =>
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Action delegate called!");
+            // Store message in a variable instead of printing it directly
+            string message = sb.ToString();
+            // Use the message later as needed...
+        };
+
+        // Invoke the Action delegate
+        printMessage();
+
+        // Event handler example
+        button.Click += () =>
+        {
+            string message = "Button clicked!";
+            // Store event information for later use...
+        };
+
+        // Utility method example
+        void LogMessage(string message)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(message);
+            // Store log message instead of printing it directly
+            string logMessage = sb.ToString();
+            // Use the log message later for logging purposes...
+        }
+
+        Action logAction = () => LogMessage("Log message from Action delegate");
+        logAction.Invoke(); // Invoking the delegate to store the log message
+
+
+    }
+}
---
+++ csharp-basic-topics/AccessModifiersInCsharp/Func.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        // Define a Func delegate that takes an int and returns a string
+        Func<int, string> doubleToString = x =>
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"The double is: {x * 2}");
+            return sb.ToString(); // Return the formatted string instead of printing it
+        };
+
+        // Invoke the Func delegate and store the result
+        string result = doubleToString(5);
+        // Use the result later as needed...
+
+        // Data transformation example
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        List<string> doubledNumbers = numbers.Select(x =>
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(x * 2);
+            return sb.ToString(); // Return the formatted string for each element
+        }).ToList();
+
+        // Function composition example
+        Func<int, int> square = x => x * x;
+        Func<int, string> squareAndFormat = square.Compose(x =>
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"The square is: {x}");
+            return sb.ToString(); // Return the formatted string with the square value
+        });
+
+        string formattedSquare = squareAndFormat(5);
+        // Use the formatted square value later as needed...
+
+
+
+    }
+}
