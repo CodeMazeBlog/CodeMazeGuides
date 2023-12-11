4972fe71cb99e5c6417125e7d16619e262258f53
Ivan Matec <im@bullwall.com>
2023-12-11 13:41:54 +0100

    - implemented methods TossCoin and IsCoinFairExperiments - implemented a test method WhenCoinTossCountHeadsAndCalculateAverage_ThanSuccess

--- csharp-machine-learning/ProbabilisticProgrammingInCsharpWithInferNET/ProbabilisticProgrammingInCsharpWithInferNET/CoinTossEngine.cs
+++ csharp-machine-learning/ProbabilisticProgrammingInCsharpWithInferNET/ProbabilisticProgrammingInCsharpWithInferNET/CoinTossEngine.cs
@@ -1,49 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ProbabilisticProgrammingInCsharpWithInferNET
+namespace ProbabilisticProgrammingInCsharpWithInferNET;
+public class CoinTossEngine
 {
-    public class CoinTossEngine
+    private bool IsHead { get { return new Random().NextDouble() > 0.5; } }
+    public bool FirstHead { get; private set; }
+    public bool SecondHead { get; private set; }
+    public bool BothHeads { get; private set; }
+
+    public int FirstCoinHeadCount { get; private set; }
+    public int SecondCoinHeadCount { get; private set; }
+    public int BothCoinsHeadCount { get; private set; }
+    public int RunCount { get; private set; }
+
+    public double FirstCoinPercentage { get; private set; }
+    public double SecondCoinPercentage { get; private set; }
+    public double BothCoinsPercentage { get; private set; }
+
+    public void TossTheCoins(int numberOfRuns = 1000000, Func<CoinTossEngine, bool> condition = null)
     {
-        Random random = new Random();
-        public bool FirstHead { get; private set; }
-        public bool SecondHead { get; private set; }
-        public bool BothHeads { get; private set; }
-
-        public int FirstCoinHeadCount { get; private set; }
-        public int SecondCoinHeadCount { get; private set; }
-        public int BothCoinsHeadCount { get; private set; }
-        public int RunCount { get; private set; }
-
-        public double FirstCoinPercentage { get; private set; }
-        public double SecondCoinPercentage { get; private set; }
-        public double BothCoinsPercentage { get; private set; }
-
-        public void TossTheCoins(int numberOfRuns = 1000000, Func<CoinTossEngine, bool> condition = null)
+        for (int i = 0; i < numberOfRuns; i++)
         {
-            for (int i = 0; i < numberOfRuns; i++)
+            FirstHead = IsHead;
+            SecondHead = IsHead;
+            BothHeads = FirstHead && SecondHead;
+
+            if (condition == null || condition(this))
             {
-                FirstHead = random.NextDouble() > 0.5;
-                SecondHead = random.NextDouble() > 0.5;
-                BothHeads = FirstHead && SecondHead;
-
-                if (condition == null || condition(this))
-                {
-                    if (FirstHead) FirstCoinHeadCount++;
-                    if (SecondHead) SecondCoinHeadCount++;
-                    if (BothHeads) BothCoinsHeadCount++;
-                    RunCount++;
-                }
+                if (FirstHead) FirstCoinHeadCount++;
+                if (SecondHead) SecondCoinHeadCount++;
+                if (BothHeads) BothCoinsHeadCount++;
+                RunCount++;
             }
-            FirstCoinPercentage = Math.Round(100.0 * FirstCoinHeadCount / RunCount, 2);
-            SecondCoinPercentage = Math.Round(100.0 * SecondCoinHeadCount / RunCount, 2);
-            BothCoinsPercentage = Math.Round(100.0 * BothCoinsHeadCount / RunCount, 2);
         }
+        FirstCoinPercentage = Math.Round(100.0 * FirstCoinHeadCount / RunCount, 2);
+        SecondCoinPercentage = Math.Round(100.0 * SecondCoinHeadCount / RunCount, 2);
+        BothCoinsPercentage = Math.Round(100.0 * BothCoinsHeadCount / RunCount, 2);
+    }
 
-        private bool IsHead { get { return random.NextDouble() > 0.5; } }
+    public int TossCoin(int numberOfRuns = 1000000)
+    {
+        var headsCount = 0;
+        for (int i = 0; i < numberOfRuns; i++)
+            if (IsHead)
+                headsCount++;
+
+        return headsCount;
+    }
+
+    public int[] IsCoinFairExperiments(int numberOfExperiments = 10)
+    {
+        var headCounts = new List<int>();
+        for (int i = 0; i < numberOfExperiments; i++)
+            headCounts.Add(TossCoin());
+
+        return headCounts.ToArray();
     }
 }
--- csharp-machine-learning/ProbabilisticProgrammingInCsharpWithInferNET/ProbabilisticProgrammingInCsharpWithInferNETTests/SimpleCoinTossEngineTests.cs
+++ csharp-machine-learning/ProbabilisticProgrammingInCsharpWithInferNET/ProbabilisticProgrammingInCsharpWithInferNETTests/SimpleCoinTossEngineTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProbabilisticProgrammingInCsharpWithInferNET;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace ProbabilisticProgrammingInCsharpWithInferNETTests
 {
@@ -31,6 +33,21 @@ namespace ProbabilisticProgrammingInCsharpWithInferNETTests
             Console.WriteLine($"P - Both Coins Are Heads: {coinTossEngine.BothCoinsPercentage}%");
 
             Assert.IsTrue(Math.Round(coinTossEngine.FirstCoinPercentage, 0) == 33.0);
+        }
+
+        [TestMethod]
+        public void WhenCoinTossCountHeadsAndCalculateAverage_ThanSuccess()
+        {
+            var experimentsCounts = new int[] { 1, 10, 50, 100 };
+            foreach(var count in experimentsCounts)
+            {
+                var headCounts = coinTossEngine.IsCoinFairExperiments(500);
+                var average = headCounts.Average();
+
+                Console.WriteLine($"Experiment count: {count}, head count average: {average}");
+
+                Assert.IsFalse(average == 500000);
+            }
         }
     }
 }
