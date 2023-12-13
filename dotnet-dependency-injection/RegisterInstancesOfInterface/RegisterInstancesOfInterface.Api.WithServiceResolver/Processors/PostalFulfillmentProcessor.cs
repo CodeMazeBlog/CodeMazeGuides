1dee338995ab40b073458b258c7c001196d1ced6
Michal Kaminski <91052896+MichalBrylKaminski@users.noreply.github.com>
2023-12-13 23:35:16 +0100

    CM-839 | How to Register Multiple Implementations of the Same Interface in Asp.Net Core (#1569)
    * Create new project and intial files
    
    * Added Asp.Net WebApi project
    
    * Reworked interfaces
    
    * Made the example much simplier
    
    * Added controllers to Api project
    
    * Merged two projects
    
    * Added example for redesigning the solution
    
    * Added uniot tests for both projects
    
    * Fixed failing unit tests
    
    * Update to .NET 8
    
    * Renamed solution to shorten the path
    
    * Cleaned up unnecessary profiles
    
    * Moved solution to dotnet-dependency-injection directory
    
    * Added https profile for both apis
    
    ---------
    
    Co-authored-by: Michal Kaminski <kaminski.mi@wp.pl>

---
+++ dotnet-dependency-injection/RegisterInstancesOfInterface/RegisterInstancesOfInterface.Api.WithServiceResolver/Processors/PostalFulfillmentProcessor.cs
@@ -0,0 +1,8 @@
+﻿using RegisterInstancesOfInterface.Api.WithServiceResolver.Interfaces;
+
+namespace RegisterInstancesOfInterface.Api.WithServiceResolver.Processors;
+
+public class PostalFulfillmentProcessor : IFulfillTickets
+{
+    public string Fulfill(string requestId) => $"{requestId} | Fulfilling tickets using postal delivery method";
+}
---
+++ dotnet-dependency-injection/RegisterInstancesOfInterface/RegisterInstancesOfInterface.Api.WithServiceResolver/Processors/SmartcardFulfillmentProcessor.cs
@@ -0,0 +1,8 @@
+﻿using RegisterInstancesOfInterface.Api.WithServiceResolver.Interfaces;
+
+namespace RegisterInstancesOfInterface.Api.WithServiceResolver.Processors;
+
+public class SmartcardFulfillmentProcessor : IFulfillTickets
+{
+    public string Fulfill(string requestId) => $"{requestId} | Fulfilling tickets using smartcard delivery method";
+}
