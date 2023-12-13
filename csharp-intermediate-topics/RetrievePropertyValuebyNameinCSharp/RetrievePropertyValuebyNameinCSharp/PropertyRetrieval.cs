2c3dc7ed9771fce15de4ef85080b92e9417f845a
kevinLug <kevinotim4@gmail.com>
2023-12-13 16:55:51 +0300

    Update PropertyRetrieval.cs
    Remove nested code.

--- csharp-intermediate-topics/RetrievePropertyValuebyNameinCSharp/RetrievePropertyValuebyNameinCSharp/PropertyRetrieval.cs
+++ csharp-intermediate-topics/RetrievePropertyValuebyNameinCSharp/RetrievePropertyValuebyNameinCSharp/PropertyRetrieval.cs
@@ -5,39 +5,51 @@ public class PropertyRetrieval
 {
     public static object GetPropertyValue(object obj, string propertyName)
     {
-        PropertyInfo propertyInfo = obj.GetType().GetProperty(propertyName);
+        PropertyInfo? propertyInfo = obj.GetType().GetProperty(propertyName);
 
-        if (propertyInfo != null)
-        {
-            return propertyInfo.GetValue(obj);
-        }
-        else
+        if (propertyInfo == null)
         {
             Console.WriteLine($"Property {propertyName} not found.");
-            return null;
+            return null!;
         }
+
+        return propertyInfo.GetValue(obj)!;
     }
 
     public static object GetPropertyValue(object obj, string propertyName, Type expectedType)
     {
-        PropertyInfo propertyInfo = obj.GetType().GetProperty(propertyName);
+        PropertyInfo? propertyInfo = obj.GetType().GetProperty(propertyName);
+
+        if (propertyInfo == null)
+        {
+            Console.WriteLine($"Property {propertyName} not found.");
+            return null!;
+        }
+
+        object propertyValue = propertyInfo.GetValue(obj)!;
+
+        if (propertyValue == null || propertyValue.GetType() != expectedType)
+        {
+            Console.WriteLine($"Property {propertyName} has an unexpected type.");
+            return null!;
+        }
+
+        return propertyValue;
+    }
+
+    public static bool TryGetPropertyValue(object obj, string propertyName, out object? propertyValue)
+    {
+        PropertyInfo? propertyInfo = obj.GetType().GetProperty(propertyName);
 
         if (propertyInfo != null)
         {
-            object propertyValue = propertyInfo.GetValue(obj);
-
-            if (propertyValue != null && propertyValue.GetType() == expectedType)
-            {
-                return propertyValue;
-            }
-            else
-            {
-                Console.WriteLine($"Property {propertyName} has an unexpected type.");
-                return null;
-            }
+            propertyValue = propertyInfo.GetValue(obj);
+            return true;
         }
 
         Console.WriteLine($"Property {propertyName} not found.");
-        return null;
+        propertyValue = null;
+
+        return false;
     }
 }
