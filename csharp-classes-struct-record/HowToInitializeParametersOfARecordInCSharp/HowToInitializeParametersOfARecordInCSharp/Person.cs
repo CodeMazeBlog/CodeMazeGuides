3ef492f070bcb42c8fc89cd60ad9c94973172930
asfd1234 <118693767+asfd1234@users.noreply.github.com>
2023-12-14 21:02:18 +0000

    Add HowToInitializeParametersOfARecordInCSharp

---
+++ csharp-classes-struct-record/HowToInitializeParametersOfARecordInCSharp/HowToInitializeParametersOfARecordInCSharp/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace HowToInitializeParametersOfARecordInCSharp
+{
+    public record Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public record Person2(string FirstName, string LastName);
+
+    public record Person3
+    {
+        public Person3(string firstName, string lastName, IEnumerable<string>? friends = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Friends = friends ?? new List<string>();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<string> Friends { get; set; }
+    }
+
+    public record Person4(string FirstName, string LastName, IEnumerable<string>? Friends = null)
+    {
+        public IEnumerable<string> Friends { get; init; } = Friends ?? new List<string>();
+    }
+}
---
+++ csharp-classes-struct-record/HowToInitializeParametersOfARecordInCSharp/HowToInitializeParametersOfARecordInCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using HowToInitializeParametersOfARecordInCSharp;
+
+var person = new Person("Joe", "Bloggs");
+Console.WriteLine(person);
+
+var person2 = new Person2("Joe", "Bloggs");
+Console.WriteLine(person2);
+
+var person3 = new Person3("Joe", "Bloggs", new List<string> { "Alice", "Bob" });
+Console.WriteLine(person3);
+foreach (var friend in person3.Friends)
+    Console.WriteLine(friend);
+
+var person4 = new Person4("Joe", "Bloggs", new List<string> { "Alice", "Bob" });
+Console.WriteLine(person4);
+foreach (var friend in person4.Friends)
+    Console.WriteLine(friend);
