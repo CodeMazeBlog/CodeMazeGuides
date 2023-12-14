92ad1abc0662c776e3513b43090299ba9a5500f0
acha-r <jayrachel96@gmail.com>
2023-12-14 05:38:35 +0100

    test

--- aspnetcore-features/SecretsManagement/Tests/OrderServiceTest.cs
+++ aspnetcore-features/SecretsManagement/Tests/OrderServiceTest.cs
@@ -11,13 +11,13 @@ namespace SecretMgt.Tests
         {
             // Arrange
             var configuration = new Mock<IConfiguration>();
-            configuration.Setup(x => x["Paystack:APIKey"]).Returns("your-api-key");
+            configuration.Setup(x => x["Paystack:APIKey"]).Returns("sk_test_7f9a384e2dc15181cab8fb83db4fb332769fedbb");
 
-            var paystackApi = new Mock<PayStackApi>("your-api-key");
+            var paystackApi = new Mock<PayStackApi>("sk_test_7f9a384e2dc15181cab8fb83db4fb332769fedbb");
             var orderService = new OrderService(configuration.Object);
             orderService = new OrderService(configuration.Object);
             var orderId = 0;
-            var email = "test@example.com";
+            var email = "test@yopmil.com";
 
             // Act
             var result = orderService.PayViaPaystack(orderId, email);
