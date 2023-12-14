9fd02ff2ff936e14dca524554764a5fb09373c62
Lennart Pries <lennart.pries@studmail.w-hs.de>
2023-12-14 14:05:08 +0100

    CM-192: Consistent Byte Representation of Strings in C# without Encoding

---
+++ dotnet-projects/ByteRepresentationOfStrings/ByteRepresentationOfStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using ByteRepresentationOfStrings;
+
+string sampleString = "CodeMaze";
+byte[] byteArray = StringByteConverter.GetBytes(sampleString);
+string reconstructedString = StringByteConverter.GetString(byteArray);
+
+Console.WriteLine(reconstructedString);
+
+
+//Example using System.Text.Encoding
+string cafe = "Café";
+byte[] ascii = System.Text.Encoding.ASCII.GetBytes(cafe);
+byte[] utf8 = System.Text.Encoding.UTF8.GetBytes(cafe);
+
+//Different byte length
+Console.WriteLine(ascii.Length);
+Console.WriteLine(utf8.Length);
+
+Console.WriteLine(System.Text.Encoding.ASCII.GetString(ascii));
+Console.WriteLine(System.Text.Encoding.UTF8.GetString(utf8));
---
+++ dotnet-projects/ByteRepresentationOfStrings/ByteRepresentationOfStrings/StringByteConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteRepresentationOfStrings
+{
+    public class StringByteConverter
+    {
+        public static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+    }
+
+}
