ae68b00d01e88a2e7f59f0cf37c997ba95d0fd88
dmamta2016 <117552965+dmamta2016@users.noreply.github.com>
2023-12-15 11:40:31 +0530

    added folder

---
+++ csharp-advanced-topics/ActionFuncDelegates/ActionFuncDelegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+namespace ActionFuncDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple Action Delegate without parameters
+            Action test = () => Console.WriteLine("Performing some task");
+            test(); // Invoking the action delegate
+
+            // Action Delegate with parameter
+            Action<string> greet = lang => Console.WriteLine($"Welcome to {lang} demo!");
+            greet("C#"); // Displays: Welcome to C# demo
+
+            // Simple Function Delegate 
+            Func<float, float, float> divide = (a, b) => a / b;
+            Console.WriteLine(divide(5, 3)); // Displays: 1.666666
+
+            // Another Function Delegate Example
+            // Instantiate delegate to reference Titlecase method
+            Func<string, string> convertMethod = Titlecase;
+            string name = "illinois";
+            // Use delegate instance to call Titlecase method
+            Console.WriteLine(convertMethod(name));
+            Console.ReadKey();
+        }
+
+        static string Titlecase(string inputString)
+        {
+            var textinfo = new CultureInfo("en-US", false).TextInfo;
+            return textinfo.ToTitleCase(inputString);
+        }
+    }
+}
