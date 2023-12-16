b5459ce20cb1ccfd20603305d80ecdb6df6b3098
DgTm <email>
2023-12-16 16:33:22 +0200

    add new solution, main and test projects

---
+++ files-csharp/WriteFileToTempFolder/WriteFileToTempFolder.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WriteFileToTempFolder.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ files-csharp/WriteFileToTempFolder/WriteFileToTempFolder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WriteFileToTempFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
