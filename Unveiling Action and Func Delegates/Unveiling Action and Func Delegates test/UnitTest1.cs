90ed12466ccef09a1dfb5c38ef68228214140327
oladunjoye afeez <77560645+cinfotech94@users.noreply.github.com>
2023-12-17 01:38:18 +0100

    Add files via upload

---
+++ Unveiling Action and Func Delegates/Unveiling Action and Func Delegates test/UnitTest1.cs
@@ -0,0 +1,41 @@
+namespace Unveiling_Action_and_Func_Delegates_test
+{
+    using Microsoft.VisualStudio.TestPlatform.TestHost;
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class ProgramTests
+    {
+        [Fact]
+        public void TestPrintNumbers()
+        {
+            // Arrange
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+
+                // Act
+                Program.PrintNumbers(numbers);
+
+                // Assert
+                Assert.Equal("1 2 3 4 5 ", stringWriter.ToString());
+            }
+        }
+
+        [Fact]
+        public void TestCalculateSum()
+        {
+            // Arrange
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Act
+            int sum = Program.CalculateSum(numbers);
+
+            // Assert
+            Assert.Equal(15, sum);
+        }
+    }
+
+}
---
+++ Unveiling Action and Func Delegates/Unveiling Action and Func Delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Unveiling_Action_and_Func_Delegates_test
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            PrintNumbers(numbers);
+            Console.WriteLine(); // New line for clarity
+
+            int sum = CalculateSum(numbers);
+            Console.WriteLine($"Sum of the numbers: {sum}");
+        }
+
+        public static void PrintNumbers(List<int> numbers)
+        {
+            Action<int> printNumber = (num) => Console.Write($"{num} ");
+            numbers.ForEach(printNumber);
+        }
+
+        public static int CalculateSum(List<int> numbers)
+        {
+            Func<int, int, int> add = (a, b) => a + b;
+            return numbers.Aggregate(add);
+        }
+    }
+}
