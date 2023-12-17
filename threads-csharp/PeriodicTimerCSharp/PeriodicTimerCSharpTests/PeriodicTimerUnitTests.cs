d84630087fc69d30123775ee60a074b5527773ce
Martin Chege <martinchege324@gmail.com>
2023-12-17 10:59:14 +0300

    Added file-scoped namespaces and upgraded the projects to .NET 8

--- threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharpTests/PeriodicTimerUnitTests.cs
+++ threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharpTests/PeriodicTimerUnitTests.cs
@@ -1,26 +1,25 @@
-namespace PeriodicTimerCSharpTests
+namespace PeriodicTimerCSharpTests;
+
+[TestClass]
+public class UnitTest1
 {
-    [TestClass]
-    public class UnitTest1
+    [TestMethod]
+    public void GivenAnArrayWithSpecificElements_ThenValidateItsCreation()
     {
-        [TestMethod]
-        public void GivenAnArrayWithSpecificElements_ThenValidateItsCreation()
-        {
-            var array = PeriodicTimerMethods.CreateRandomArray(20);
+        var array = PeriodicTimerMethods.CreateRandomArray(20);
 
-            Assert.IsNotNull(array);
-            Assert.IsInstanceOfType(array, typeof(int[]));
-            Assert.AreEqual(20, array.Length);
-        }
+        Assert.IsNotNull(array);
+        Assert.IsInstanceOfType(array, typeof(int[]));
+        Assert.AreEqual(20, array.Length);
+    }
 
-        [TestMethod]
-        public void GivenATimerInstance_ThenValidateItsProperties()
-        {
-            var timer = PeriodicTimerMethods.RandomArrayTimerAsync();
+    [TestMethod]
+    public void GivenATimerInstance_ThenValidateItsProperties()
+    {
+        var timer = PeriodicTimerMethods.RandomArrayTimerAsync();
 
-            Assert.IsNotNull(timer);
-            Assert.IsInstanceOfType(timer, typeof(Task<PeriodicTimer>));
-            Assert.IsFalse(timer.IsCanceled);
-        }
+        Assert.IsNotNull(timer);
+        Assert.IsInstanceOfType(timer, typeof(Task<PeriodicTimer>));
+        Assert.IsFalse(timer.IsCanceled);
     }
 }
