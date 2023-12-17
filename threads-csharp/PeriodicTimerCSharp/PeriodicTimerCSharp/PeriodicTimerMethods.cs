fc37e742e4c50e32f929c925ed2ffd232177605a
Martin Chege <martinchege324@gmail.com>
2023-12-17 11:01:02 +0300

    Added curly braces in a for-loop

--- threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/PeriodicTimerMethods.cs
+++ threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/PeriodicTimerMethods.cs
@@ -7,8 +7,10 @@ public class PeriodicTimerMethods
         var array = new int[size];
 
         for (int i = 0; i < size; i++)
+        {
             array[i] = Random.Shared.Next();
-
+        }
+        
         return array;
     }
 
