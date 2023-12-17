457d9a6f1790e29da78954ea617864e6e6c2f3f9
Martin Chege <martinchege324@gmail.com>
2023-12-17 10:59:14 +0300

    Added file-scoped namespaces and upgraded the projects to .NET 8

--- threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/PeriodicTimerMethods.cs
+++ threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/PeriodicTimerMethods.cs
@@ -1,49 +1,48 @@
-﻿using System.Diagnostics;
+﻿namespace PeriodicTimerCSharp;
 
-namespace PeriodicTimerCSharp
+public class PeriodicTimerMethods
 {
-    public class PeriodicTimerMethods
+    public static int[] CreateRandomArray(int size)
     {
-        public static int[] CreateRandomArray(int size)
+        var array = new int[size];
+
+        for (int i = 0; i < size; i++)
+            array[i] = Random.Shared.Next();
+
+        return array;
+    }
+
+    public static async Task<PeriodicTimer> RandomArrayTimerAsync() 
+    {
+        using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+        using var token = new CancellationTokenSource();
+        var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+        var taskDuration = 6;
+        var numberOfElements = 5;
+        var numberOfArrays = 1;
+
+        while (await timer.WaitForNextTickAsync(token.Token))
         {
-            var array = new int[size];
-
-            for (int i = 0; i < size; i++)
-                array[i] = Random.Shared.Next();
-
-            return array;
-        }
-
-        public static async Task<PeriodicTimer> RandomArrayTimerAsync() 
-        {
-            using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
-            using var token = new CancellationTokenSource();
-            var stopWatch = Stopwatch.StartNew();
-            var taskDuration = 6;
-            var numberOfElements = 5;
-            var numberOfArrays = 1;
-
-            while (await timer.WaitForNextTickAsync(token.Token))
+            if (!token.IsCancellationRequested)
             {
-                if (!token.IsCancellationRequested)
+                var array = CreateRandomArray(numberOfElements);
+
+                foreach (var item in array) 
                 {
-                    var array = CreateRandomArray(numberOfElements);
+                    Console.WriteLine(item);
+                }
 
-                    foreach (var item in array) 
-                    {
-                        Console.WriteLine(item);
-                    }
+                Console.WriteLine($"Elapsed time: {stopWatch.Elapsed.Seconds}s and {numberOfArrays++} arrays created");
 
-                    Console.WriteLine($"Elapsed time: {stopWatch.Elapsed.Seconds}s and {numberOfArrays++} arrays created");
+                await Task.Delay(1000);
 
-                    await Task.Delay(1000);
-
-                    if (stopWatch.Elapsed.TotalSeconds > taskDuration)
-                        return timer;
+                if (stopWatch.Elapsed.TotalSeconds > taskDuration)
+                {
+                    return timer;
                 }
             }
-            
-            return timer;
         }
+        
+        return timer;
     }
 }
