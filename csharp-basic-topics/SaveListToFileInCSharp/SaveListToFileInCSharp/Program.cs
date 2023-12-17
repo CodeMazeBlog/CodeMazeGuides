dbd00e0bf34de9bfb82b67f3f66ba3a632de79fe
Zaneta Borowska <zaneta.borowska@katoennatie.com>
2023-12-17 15:10:04 +0100

    Save list to file

---
+++ csharp-basic-topics/SaveListToFileInCSharp/SaveListToFileInCSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿var docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+var fileName = "Animals.txt";
+var path = Path.Combine(docPath, fileName);
+
+//Write all animals to file
+var animals = new List<string> { "Dog", "Cat", "Parrot" };
+
+using (var sw = new StreamWriter(path))
+{
+    foreach (var animal in animals)
+    {
+        sw.WriteLine(animal);
+    }
+}
+
+Console.WriteLine("List saved to file.");
+
+//Asynchronous version
+await using (var sw = new StreamWriter(path))
+{
+    foreach (var animal in animals)
+    {
+        await sw.WriteLineAsync(animal);
+    }
+}
+
+//Append new animal to an existing file
+var newAnimals = new List<string> { "Hamster" };
+
+using (var sw = new StreamWriter(path, true))
+{
+    foreach (var animal in newAnimals)
+    {
+        sw.WriteLine(animal);
+    }
+}
+
+Console.WriteLine("Appended new animal to an existing file.");
+
+//Using the File class in C#
+File.WriteAllLines(path, animals);
+File.AppendAllLines(path, newAnimals);
+
+//Asynchronous version
+await File.WriteAllLinesAsync(path, animals);
