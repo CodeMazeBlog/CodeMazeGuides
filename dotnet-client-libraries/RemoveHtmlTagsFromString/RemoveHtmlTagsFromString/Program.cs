3d0280aae0d066066262ac3085d0170cff48974e
Osman Sokuoğlu <osman.sokuoglu@gmail.com>
2023-12-17 22:23:38 +0300

    #CM-900 How to Remove HTML Tags From a String in C#

---
+++ dotnet-client-libraries/RemoveHtmlTagsFromString/RemoveHtmlTagsFromString/Program.cs
@@ -0,0 +1,24 @@
+﻿using RemoveHtmlTagsFromString;
+
+var htmlString = "<p>This is <b>bold</b> text with <a href='#'>HTML</a> tags. 5&nbsp;&lt;&nbsp;7</p>";
+Console.WriteLine("Input: " + htmlString);
+
+var output = HtmlTagRemover.UseRegularExpression(htmlString);
+Console.WriteLine("Output Using Reqular Expressions: " + output);
+
+output = HtmlTagRemover.UseHtmlDecode(htmlString);
+Console.WriteLine("Output Using HtmlDecode: " + output);
+
+output = HtmlTagRemover.UseHtmlAgilityPack(htmlString);
+Console.WriteLine("Output Using HtmlAgilityPack: " + output);
+
+output = HtmlTagRemover.UseAngleSharp(htmlString);
+Console.WriteLine("Output Using AngleSharp: " + output);
+
+output = HtmlTagRemover.UseXmlXElement(htmlString);
+Console.WriteLine("Output Using XmlXElement: " + output);
+
+output = HtmlTagRemover.ReadHtmlFromFile();
+Console.WriteLine("Output ReadHtmlFromFile: " + output);
+
+Console.ReadLine();
