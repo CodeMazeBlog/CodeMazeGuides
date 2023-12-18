621fa9d51f08a58ef4f5cd9e56c61fb354f22913
jshergal <1489638+jshergal@users.noreply.github.com>
2023-12-18 15:20:19 +0200

    Updated code to correspond with article updates. Switched to top-level statements as well.

--- aspnetcore-webapi/SerializeObjectToQueryString/SerializeObjectToQueryString/Program.cs
+++ aspnetcore-webapi/SerializeObjectToQueryString/SerializeObjectToQueryString/Program.cs
@@ -1,23 +1,16 @@
-﻿namespace SerializeObjectToQueryString
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var baseApiUrl = "https://test.com";
+﻿using SerializeObjectToQueryString;
 
-            Console.WriteLine("\n***************** Serialize the object to query string using reflection ***************\n");
-            Console.WriteLine(QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingReflection(baseApiUrl, new Book()));
+const string baseApiUrl = "https://test.com";
 
-            Console.WriteLine("\n***************** Serialize the object to query string using Newtonsoft Json ***************\n");
-            Console.WriteLine(QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingNewtonsoftJson(baseApiUrl, new Book()));
+Console.WriteLine("\n***************** Serialize the object to query string using reflection ***************\n");
+Console.WriteLine(QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingReflection(baseApiUrl, new Book()));
 
-            Console.WriteLine("\n***************** Serialize the nested object to query string ***************\n");
-            Console.WriteLine(QueryStringSerializer.CreateURLWithProductAsQueryParams(baseApiUrl, new Product()));
+Console.WriteLine("\n***************** Serialize the object to query string using Newtonsoft Json ***************\n");
+Console.WriteLine(QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingNewtonsoftJson(baseApiUrl, new Book()));
 
-            Console.WriteLine("\n***************** Serialize the object that contains array and nested objects property to query string ***************\n");
-            Console.WriteLine(QueryStringSerializer.CreateURLWithPersonAsQueryParams(baseApiUrl, new Person()));
+Console.WriteLine("\n***************** Serialize the nested object to query string ***************\n");
+Console.WriteLine(QueryStringSerializer.CreateURLWithProductAsQueryParams(baseApiUrl, new Product()));
 
-        }
-    }
-}
+Console.WriteLine(
+    "\n***************** Serialize the object that contains array and nested objects property to query string ***************\n");
+Console.WriteLine(QueryStringSerializer.CreateURLWithPersonAsQueryParams(baseApiUrl, new Person()));
--- aspnetcore-webapi/SerializeObjectToQueryString/SerializeObjectToQueryStringUnitTests/QueryStringSerializerUnitTests.cs
+++ aspnetcore-webapi/SerializeObjectToQueryString/SerializeObjectToQueryStringUnitTests/QueryStringSerializerUnitTests.cs
@@ -1,63 +1,64 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SerializeObjectToQueryString;
 
-namespace SerializeObjectToQueryStringUnitTests
+namespace SerializeObjectToQueryStringUnitTests;
+
+[TestClass]
+public class QueryStringSerializerUnitTests
 {
-    [TestClass]
-    public class QueryStringSerializerUnitTests
+    private const string BaseApiUrl = "https://test.com";
+
+    [TestMethod]
+    public void WhenSerializingObjectToQueryStringUsingReflection_ThenCorrectApiUrlIsBuilt()
     {
-        private const string BaseApiUrl = "https://test.com";
+        //Arrange
+        var expectedApiUrl = $"{BaseApiUrl}?Author=George+Orwell&Language=English";
 
-        [TestMethod]
-        public void WhenSerializingObjectToQueryStringUsingReflection_ThenCorrectApiUrlIsBuilt()
-        {
-            //Arrange
-            var expectedApiUrl = $"{BaseApiUrl}?Author=George+Orwell&Language=English";
+        //Act
+        var result = QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingReflection(BaseApiUrl, new Book());
 
-            //Act
-            var result = QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingReflection(BaseApiUrl, new Book());
+        //Assert
+        Assert.AreEqual(expectedApiUrl, result);
+    }
 
-            //Assert
-            Assert.AreEqual(expectedApiUrl, result);
-        }
+    [TestMethod]
+    public void WhenSerializingObjectToQueryStringUsingNewtosnsoftJson_ThenCorrectApiUrlIsBuilt()
+    {
+        //Arrange
+        var expectedApiUrl = $"{BaseApiUrl}?Author=George+Orwell&Language=English";
 
-        [TestMethod]
-        public void WhenSerializingObjectToQueryStringUsingNewtosnsoftJson_ThenCorrectApiUrlIsBuilt()
-        {
-            //Arrange
-            var expectedApiUrl = $"{BaseApiUrl}?Author=George+Orwell&Language=English";
+        //Act
+        var result = QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingNewtonsoftJson(BaseApiUrl, new Book());
 
-            //Act
-            var result = QueryStringSerializer.CreateURLWithBookAsQueryParamsUsingNewtonsoftJson(BaseApiUrl, new Book());
+        //Assert
+        Assert.AreEqual(expectedApiUrl, result);
+    }
 
-            //Assert
-            Assert.AreEqual(expectedApiUrl, result);
-        }
+    [TestMethod]
+    public void WhenSerializingNestedObjectToQueryString_ThenCorrectApiUrlIsBuilt()
+    {
+        //Arrange
+        var expectedApiUrl =
+            $"{BaseApiUrl}?Name=Laptop&Category=Electronics&Manufacturer.Location=Silicon+Valley&Manufacturer.Distributor.Name=TechDistributors";
 
-        [TestMethod]
-        public void WhenSerializingNestedObjectToQueryString_ThenCorrectApiUrlIsBuilt()
-        {
-            //Arrange
-            var expectedApiUrl = $"{BaseApiUrl}?Name=Laptop&Category=Electronics&Manufacturer.Location%3dSilicon%2bValley&Manufacturer.Distributor.Name%253dTechDistributors";
+        //Act
+        var result = QueryStringSerializer.CreateURLWithProductAsQueryParams(BaseApiUrl, new Product());
 
-            //Act
-            var result = QueryStringSerializer.CreateURLWithProductAsQueryParams(BaseApiUrl, new Product());
+        //Assert
+        Assert.AreEqual(expectedApiUrl, result);
+    }
 
-            //Assert
-            Assert.AreEqual(expectedApiUrl, result);
-        }
+    [TestMethod]
+    public void WhenSerializingObjectContainsArrayToQueryString_ThenCorrectApiUrlIsBuilt()
+    {
+        //Arrange
+        var expectedApiUrl =
+            $"{BaseApiUrl}?FirstName=Smith&Age=25&Hobbies%5b0%5d=Reading&Hobbies%5b1%5d=Traveling&Hobbies%5b2%5d=Gaming&Address.Country=Australia";
 
-        [TestMethod]
-        public void WhenSerializingObjectContainsArrayToQueryString_ThenCorrectApiUrlIsBuilt()
-        {
-            //Arrange
-            var expectedApiUrl = $"{BaseApiUrl}?FirstName=Smith&Age=25&Hobbies[0]=Reading&Hobbies[1]=Traveling&Hobbies[2]=Gaming&Address.Country%3dAustralia";
-       
-            //Act
-            var result = QueryStringSerializer.CreateURLWithPersonAsQueryParams(BaseApiUrl, new Person());
+        //Act
+        var result = QueryStringSerializer.CreateURLWithPersonAsQueryParams(BaseApiUrl, new Person());
 
-            //Assert
-            Assert.AreEqual(expectedApiUrl, result);
-        }
+        //Assert
+        Assert.AreEqual(expectedApiUrl, result);
     }
 }
