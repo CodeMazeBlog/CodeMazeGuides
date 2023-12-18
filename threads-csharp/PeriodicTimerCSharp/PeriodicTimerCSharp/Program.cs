27551d2f695cb29a2323a699a345801c3d835e34
Martin Chege <martinchege324@gmail.com>
2023-12-18 20:55:20 +0300

    Refactored the code

--- threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/Program.cs
+++ threads-csharp/PeriodicTimerCSharp/PeriodicTimerCSharp/Program.cs
@@ -1,3 +1,12 @@
 ﻿using PeriodicTimerCSharp;
 
-await PeriodicTimerMethods.RandomArrayTimerAsync();
+Console.WriteLine("Enter array size");
+var numOfElements = Convert.ToInt32(Console.ReadLine());
+
+var timerTask = new PeriodicTimerMethods(TimeSpan.FromSeconds(1), numOfElements);
+timerTask.Start();
+
+Console.WriteLine("Press any button to terminate the task");
+Console.ReadKey();
+
+await timerTask.StopTaskAsync();
