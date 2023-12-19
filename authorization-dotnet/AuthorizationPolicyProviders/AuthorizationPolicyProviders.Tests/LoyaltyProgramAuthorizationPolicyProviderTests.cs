3229522495a6f3f83ae8a60035c7d73eca8fc4c7
Sara Elsayeda <swe@saraelsa.net>
2023-12-19 15:14:12 -0500

    CM754 Implement Custom Authorization Handler in ASP.NET Core (#1586)
    * CM754 Implement Custom Authorization Handler in ASP.NET Core

---
+++ authorization-dotnet/AuthorizationPolicyProviders/AuthorizationPolicyProviders.Tests/LoyaltyProgramAuthorizationPolicyProviderTests.cs
@@ -0,0 +1,66 @@
+using Microsoft.Extensions.Options;
+
+namespace AuthorizationPolicyProviders.Tests;
+
+public class LoyaltyProgramAuthorizationPolicyProviderTests
+{
+    private class TestAuthorizationRequirement : IAuthorizationRequirement { }
+
+    private static readonly AuthorizationPolicy _testAuthorizationPolicy =
+        new(
+            new List<IAuthorizationRequirement>() { new TestAuthorizationRequirement() },
+            new List<string>() { string.Empty }
+        );
+
+    private const string TestAuthorizationPolicyName = "Test";
+
+    private IOptions<AuthorizationOptions> TestAuthorizationOptions { get; }
+
+    private LoyaltyProgramAuthorizationPolicyProvider TestPolicyProvider { get; }
+
+    public LoyaltyProgramAuthorizationPolicyProviderTests()
+    {
+        var authorizationOptions = new AuthorizationOptions();
+        authorizationOptions.AddPolicy(TestAuthorizationPolicyName, _testAuthorizationPolicy);
+
+        TestAuthorizationOptions = Options.Create(authorizationOptions);
+
+        TestPolicyProvider = new(new SampleLoyaltyRequirementsRepository(), TestAuthorizationOptions);
+    }
+
+    [Fact]
+    public async Task WhenPolicyNameDoesNotStartWithPrefix_ThenBackupProviderUsedForNameLookup()
+    {
+        const string policyName = "Test";
+
+        var providedPolicy = await TestPolicyProvider.GetPolicyAsync(policyName);
+
+        Assert.Equal(_testAuthorizationPolicy, providedPolicy);
+    }
+
+    [Fact]
+    public async Task WhenRequirementsNotReturnedByRepository_ThenNoPolicyReturned()
+    {
+        const string policyName = "Loyalty:Test";
+
+        var providedPolicy = await TestPolicyProvider.GetPolicyAsync(policyName);
+
+        Assert.Null(providedPolicy);
+    }
+
+    [Fact]
+    public async Task WhenRequirementsReturnedByRepository_ThenAppropriateAuthorizationPolicyReturned()
+    {
+        const string policyName = "Loyalty:PriorityCheckIn";
+        const MembershipTier baselineMembershipTier = MembershipTier.Silver;
+        const int minimumLoyaltyPoints = 100;
+
+        var providedPolicy = await TestPolicyProvider.GetPolicyAsync(policyName);
+
+        Assert.NotNull(providedPolicy);
+        Assert.Contains(providedPolicy.Requirements, r =>
+            r is BaselineMembershipTierRequirement { BaselineMembershipTier: baselineMembershipTier });
+        Assert.Contains(providedPolicy.Requirements, r =>
+            r is MinimumLoyaltyPointsRequirement { MinimumLoyaltyPoints: minimumLoyaltyPoints });
+    }
+}
