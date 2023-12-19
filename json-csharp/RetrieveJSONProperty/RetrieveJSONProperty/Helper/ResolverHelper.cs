611a40b07c160da19f658c180f5d6ffd9501b031
KulsoomRahim <31511228+KulsoomRahim@users.noreply.github.com>
2023-12-20 01:07:49 +0500

    CM-857 - How To Retrieve JSON property names in C# (#1567)
    * Retrieval Json Property
    
    * updated tests
    
    * remove test
    
    * PR changes
    
    * Review changes
    
    * Resolver Changes
    
    * Fixed Warning Messages
    
    * Fixed spacings
    
    * Removed additional DTOs and cleaned up classes
    
    ---------
    
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ json-csharp/RetrieveJSONProperty/RetrieveJSONProperty/Helper/ResolverHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace RetrieveJSONProperty.Helper;
+
+public static class ResolverHelper
+{
+    private static readonly IContractResolver _defaultResolver = JsonSerializer.CreateDefault().ContractResolver;
+
+    public static JsonProperty GetJsonProperty<T>(T obj, string jsonName, bool exact = false, IContractResolver? resolver = null)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+
+        resolver ??= _defaultResolver;
+        var objType = typeof(T);
+        
+        if (resolver.ResolveContract(objType) is not JsonObjectContract contract)
+            throw new ArgumentException($"{objType} is not serialized as a JSON object");
+
+        var property = contract.Properties.GetProperty(jsonName, exact ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase);
+
+        if (property == null)
+            throw new ArgumentException($"Property {jsonName} was not found.");
+
+        return property;
+    }
+}
