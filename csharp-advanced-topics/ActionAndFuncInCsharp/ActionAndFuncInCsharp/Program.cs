72852d63b5204accc7ab7f6a55468df20827fb32
Kumar, Mohit <Kumar.Mohit@bcg.com>
2023-12-19 15:17:19 +0530

    Updating Program for Testing

--- csharp-advanced-topics/ActionAndFuncInCsharp/ActionAndFuncInCsharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncInCsharp/ActionAndFuncInCsharp/Program.cs
@@ -1,26 +1,45 @@
-﻿//Declare Action delegate with no parameters
-Action display = PrintMessage;
-//Calling Action delegate
-display();
-static void PrintMessage()
+﻿using System.Text;
+
+namespace ActionAndFuncInCsharp
 {
-    Console.WriteLine("Hello is printed");
+    public class Program
+    {
+        //Declare Action delegate with no parameters
+        public static Action display = PrintMessage;
+
+        //Creating Action delegate with parameters
+        public static Action<string, int> printEmployeeNameAndNo = (name, no) =>
+        {
+            Console.WriteLine($"Employee Name: {name},and No: {no}");
+        };
+
+        //Creating Func delegate with parameters
+        public static Func<int, int, int> add = (x, y) => x + y;
+
+        //Creating Func delegate with no parameters
+        public static Func<string> sayHello = () => "Hello";
+
+        public static void Main(string[] args)
+        {
+            //Calling Action delegate
+            display();
+
+            //Calling Action delegate with parameter
+            printEmployeeNameAndNo("John", 146);
+
+            //Calling Func delegate with parameter
+            Console.WriteLine(add.Invoke(10, 20));
+
+            //Calling Func delegate
+            sayHello();
+        }
+        internal static void PrintMessage()
+        {
+            Console.WriteLine("Hello is printed");
+        }
+
+
+    }
+
 }
 
-//Creating Action delegate with parameters
-Action<string, int> printEmployeeNameAndNo = (name, no) =>
-{
-    Console.WriteLine($"Employee Name: {name},and No: {no}");
-};
-//Calling Action delegate
-printEmployeeNameAndNo("John", 146);
-
-//Creating Func delegate with parameters
-Func<int, int, int> add = (x, y) => x + y;
-//Calling Func delegate
-Console.WriteLine(add.Invoke(10, 20));
-
-//Creating Func delegate with no parameters
-Func<string> sayHello = () => "Hello";
-//Calling Func delegate
-sayHello();
