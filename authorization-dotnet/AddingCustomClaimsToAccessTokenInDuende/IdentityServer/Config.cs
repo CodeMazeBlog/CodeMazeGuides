fed35fdfff632a065d0a0b78e9921f10b64aee35
Muntyan A <almunt.it@gmail.com>
2023-12-19 22:39:12 +0200

    CM-354: create a solution and add an IdentityServer project

---
+++ authorization-dotnet/AddingCustomClaimsToAccessTokenInDuende/IdentityServer/Config.cs
@@ -0,0 +1,19 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            { };
+}
---
+++ authorization-dotnet/AddingCustomClaimsToAccessTokenInDuende/IdentityServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
