2ac642ae569ca2f9f0983c3c275ead0031467e33
Ainea Wabwoba <aineah28@gmail.com>
2023-12-20 22:27:19 +0300

    Initial commit

---
+++ exception-handling/IExceptionHandlerInterface/IExceptionHandlerInterface/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace IExceptionHandlerInterface.Middleware;
+
+public class ExceptionHandlingMiddleware : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
