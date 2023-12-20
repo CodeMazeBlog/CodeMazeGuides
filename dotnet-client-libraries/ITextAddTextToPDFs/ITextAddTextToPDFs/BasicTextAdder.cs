af8aa381da71ce3cd4a53ea8779f3e2733cf15f8
Matjaž Prtenjak <matjaz@matjazev.net>
2023-12-20 05:56:53 +0100

    CM-719: Insert Text to an Existing PDF Using IText (#1576)
    * CM-719: Initial Project
    
    * CM-719: Changes after first review
    
    * Changed the library from iText7 to iText
    * Repaired the WaterMark to Watermark
    * Updated .NET 7.0 to 8.0
    * Changed 'Mor' to 'More'
    
    * CM-719: Changes after final review
    
    Restore change in another folder
    All "usings" in the test project moved to global using file

---
+++ dotnet-client-libraries/ITextAddTextToPDFs/ITextAddTextToPDFs/BasicTextAdder.cs
@@ -0,0 +1,38 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace ITextAddTextToPDFs
+{
+    public static class BasicTextAdder
+    {
+        public static void Add(string inputFile, string outputFile, string textToAdd)
+        {
+            using var writer = new PdfWriter(outputFile);
+            using var outputDocument = new PdfDocument(writer);
+
+            using var reader = new PdfReader(inputFile);
+            using var inputDocument = new PdfDocument(reader);
+
+            using var workingDocument = new Document(outputDocument);
+
+            var numberOfPages = inputDocument.GetNumberOfPages();
+            for (var i = 1; i <= numberOfPages; i++)
+            {
+                var page = inputDocument.GetPage(i);
+
+                var newPage = page.CopyTo(outputDocument);
+                outputDocument.AddPage(newPage);
+
+                var copyText = new Paragraph(textToAdd);
+                workingDocument.ShowTextAligned(
+                    p: copyText,
+                    x: UnitConverter.mm2uu(10), y: UnitConverter.mm2uu(10),
+                    pageNumber: i,
+                    textAlign: TextAlignment.LEFT, vertAlign: VerticalAlignment.TOP,
+                    radAngle: 0);
+            }
+        }
+    }
+}
