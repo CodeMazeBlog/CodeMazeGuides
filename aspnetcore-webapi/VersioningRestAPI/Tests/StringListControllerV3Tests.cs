b11c0e6841674d1e73d1637c525a4d95ebbcf33b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-12-21 07:53:43 +0100

    Code formatted

--- aspnetcore-webapi/VersioningRestAPI/Tests/StringListControllerV3Tests.cs
+++ aspnetcore-webapi/VersioningRestAPI/Tests/StringListControllerV3Tests.cs
@@ -13,17 +13,17 @@ public class StringListControllerV3Tests : IClassFixture<WebApplicationFactory<P
     public StringListControllerV3Tests(WebApplicationFactory<Program> factory)
     {
         var serviceUrl = "https://localhost:7114/";
-            _httpClient = factory.CreateClient();
-            _httpClient.BaseAddress = new Uri(serviceUrl);
-        }
+        _httpClient = factory.CreateClient();
+        _httpClient.BaseAddress = new Uri(serviceUrl);
+    }
 
     [Fact]
     public async Task GivenURLChange_WhenCalledV3_ThenReturnStringStartingWithC()
     {
-            var json = await _httpClient.GetStringAsync("/api/v3/StringList");
-            var strings = JArray.Parse(json);
-            Assert.Equal(2, strings.Count);
-            Assert.StartsWith("C", (string?)strings[0]);
-            Assert.StartsWith("C", (string?)strings[1]);
-        }
+        var json = await _httpClient.GetStringAsync("/api/v3/StringList");
+        var strings = JArray.Parse(json);
+        Assert.Equal(2, strings.Count);
+        Assert.StartsWith("C", (string?)strings[0]);
+        Assert.StartsWith("C", (string?)strings[1]);
+    }
 }
