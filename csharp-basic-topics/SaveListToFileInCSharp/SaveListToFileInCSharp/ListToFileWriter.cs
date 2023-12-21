b85b97bbd683e2b69bbcf5772ba772537c40a937
Zaneta Borowska <zaneta.borowska@katoennatie.com>
2023-12-22 00:08:43 +0100

    Using as a single line statement

--- csharp-basic-topics/SaveListToFileInCSharp/SaveListToFileInCSharp/ListToFileWriter.cs
+++ csharp-basic-topics/SaveListToFileInCSharp/SaveListToFileInCSharp/ListToFileWriter.cs
@@ -4,34 +4,28 @@ public class ListToFileWriter
 {
     public void WriteToFileWithStreamWriter(string path, List<string> animals)
     {
-        using (var sw = new StreamWriter(path))
+        using var sw = new StreamWriter(path);
+        foreach (var animal in animals)
         {
-            foreach (var animal in animals)
-            {
-                sw.WriteLine(animal);
-            }
+            sw.WriteLine(animal);
         }
     }
 
     public async Task WriteToFileWithStreamWriterAsync(string path, List<string> animals)
     {
-        await using (var sw = new StreamWriter(path))
+        await using var sw = new StreamWriter(path);
+        foreach (var animal in animals)
         {
-            foreach (var animal in animals)
-            {
-                await sw.WriteLineAsync(animal);
-            }
+            await sw.WriteLineAsync(animal);
         }
     }
 
     public void AppendToFileWithStreamWriter(string path, List<string> animals)
     {
-        using (var sw = new StreamWriter(path, true))
+        using var sw = new StreamWriter(path, append: true);
+        foreach (var animal in animals)
         {
-            foreach (var animal in animals)
-            {
-                sw.WriteLine(animal);
-            }
+            sw.WriteLine(animal);
         }
     }
 }
