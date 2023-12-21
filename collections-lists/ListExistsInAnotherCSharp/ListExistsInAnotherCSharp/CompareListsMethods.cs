a79375ca10b878f9fcb7543c44ae372fc6ee10bc
Martin Chege <martinchege324@gmail.com>
2023-12-21 08:06:02 +0300

    First draft

---
+++ collections-lists/ListExistsInAnotherCSharp/ListExistsInAnotherCSharp/CompareListsMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace ListExistsInAnotherCSharp;
+
+public class CompareListsMethods
+{
+    public bool CompareListUsingIntersect(List<int> firstList, List<int> secondList) 
+    {
+        return firstList.Intersect(secondList).Any();
+    }
+
+    public bool CompareListUsingAnyContains(List<int> firstList, List<int> secondList)
+    {
+        return secondList.Any(firstList.Contains);
+    }
+
+    public bool CompareListUsingExcept(List<int> firstList, List<int> secondList)
+    {
+        return !(secondList.Except(firstList).Any());
+    }
+}
---
+++ collections-lists/ListExistsInAnotherCSharp/ListExistsInAnotherCSharpTests/ListExistsInAnotherUnitTests.cs
@@ -0,0 +1,37 @@
+namespace ListExistsInAnotherCSharpTests;
+
+[TestClass]
+public class ListExistsInAnotherUnitTests
+{
+    private readonly CompareListsMethods _compareListsMethods = new();
+    private readonly List<int> _firstList;
+    private readonly List<int> _secondList;
+    private readonly List<int> _thirdList;
+
+    public ListExistsInAnotherUnitTests()
+    {
+        _firstList = new List<int> { 0, 1, 2, 3, 4 };
+        _secondList = new List<int> { 5, 6, 7, 8, 9 };
+        _thirdList = new List<int> { 0, 1, 2, 3, 5 };
+    }
+    [TestMethod]
+    public void GivenTwoLists_WhenComparedUsingIntersect_ValidateCorrectResults()
+    {
+        Assert.IsTrue(_compareListsMethods.CompareListUsingIntersect(_firstList, _thirdList));
+        Assert.IsFalse(_compareListsMethods.CompareListUsingIntersect(_firstList, _secondList));
+    }
+
+    [TestMethod]
+    public void GivenTwoLists_WhenComparedUsingExcept_ValidateCorrectResults()
+    {
+        Assert.IsTrue(_compareListsMethods.CompareListUsingExcept(_firstList, _thirdList));
+        Assert.IsFalse(_compareListsMethods.CompareListUsingExcept(_firstList, _secondList));
+    }
+
+    [TestMethod]
+    public void GivenTwoLists_WhenComparedUsingAnyContains_ValidateCorrectResults()
+    {
+        Assert.IsTrue(_compareListsMethods.CompareListUsingAnyContains(_firstList, _thirdList));
+        Assert.IsFalse(_compareListsMethods.CompareListUsingAnyContains(_firstList, _secondList));
+    }
+}
