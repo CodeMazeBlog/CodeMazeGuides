f96a1695abc117bf5ba1636844de40298cb7c206
asfd1234 <118693767+asfd1234@users.noreply.github.com>
2023-12-21 15:06:25 +0000

    Split out records into separate files and reorder properties and constructors

--- csharp-classes-struct-record/HowToInitializeParametersOfARecordInCSharp/HowToInitializeParametersOfARecordInCSharp/Person3.cs
+++ csharp-classes-struct-record/HowToInitializeParametersOfARecordInCSharp/HowToInitializeParametersOfARecordInCSharp/Person3.cs
@@ -1,13 +1,15 @@
 ﻿namespace HowToInitializeParametersOfARecordInCSharp;
 
-public record Person
+public record Person3
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public IEnumerable<string> Friends { get; set; }
 
-    public Person(string firstName, string lastName)
+    public Person3(string firstName, string lastName, IEnumerable<string>? friends = null)
     {
         FirstName = firstName;
         LastName = lastName;
+        Friends = friends ?? new List<string>();
     }
 }
