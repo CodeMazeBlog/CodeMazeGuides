6e33975a191e52a9ec71ee3fc060e0397c2a19e1
gergo <gergo.vandor.dev@outlook.com>
2023-12-22 22:49:39 +0100

    senior editor review fixes

--- aspnetcore-webapi/IgnorePropertyInSwagger/IgnorePropertyInSwagger/Models/WeatherForecast.cs
+++ aspnetcore-webapi/IgnorePropertyInSwagger/IgnorePropertyInSwagger/Models/WeatherForecast.cs
@@ -2,18 +2,8 @@ using IgnorePropertyInSwagger.Attributes;
 
 namespace IgnorePropertyInSwagger.Models;
 
-public record WeatherForecast
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-    public WeatherForecast(DateOnly date, int temperatureC, string? summary)
-    {
-        Date = date;
-        TemperatureC = temperatureC;
-        Summary = summary;
-    }
-
     [SwaggerIgnore]
     public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
-    public DateOnly Date { get; init; }
-    public int TemperatureC { get; init; }
-    public string? Summary { get; init; }
 }
