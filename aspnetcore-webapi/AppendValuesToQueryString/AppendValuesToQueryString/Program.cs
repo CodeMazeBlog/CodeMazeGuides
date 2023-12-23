7d40bd70124e24f29f4e832641a74be4b564bc56
karthikns999 <karthikns999@gmail.com>
2023-12-23 18:35:43 +0530

    Code review comments

--- aspnetcore-webapi/AppendValuesToQueryString/AppendValuesToQueryString/Program.cs
+++ aspnetcore-webapi/AppendValuesToQueryString/AppendValuesToQueryString/Program.cs
@@ -1,21 +1,21 @@
 ﻿using AppendValuesToQueryString;
 
-var url = "https://test.com/Books?author=rowling&language=english";
+var url = "https://test.com/Books?author=rowling&language=english#section1";
 
 var queryParams = new Dictionary<string, string>
 {
-    {"genre", "fantasy" },
-    {"author", "james" }
+    {"genre", "science fiction" },
+    {"author", "james martin" }
 };
 
-Console.WriteLine("\n***************** Modify Query String ParseQueryString Method ***************\n");
+Console.WriteLine("\n***************** Modify Query String Using ParseQueryString Method ***************\n");
 Console.WriteLine(QueryStringHelper.CreateURLUsingParseQueryString(url, queryParams));
 
-Console.WriteLine("\n***************** Modify Query String ParseQuery Method ***************\n");
+Console.WriteLine("\n***************** Modify Query String Using ParseQuery Method ***************\n");
 Console.WriteLine(QueryStringHelper.CreateURLUsingParseQuery(url, queryParams));
 
-Console.WriteLine("\n***************** Modify Query String AddQueryString Method ***************\n");
-Console.WriteLine(QueryStringHelper.CreateURLUsingAddQueryString(url, new Dictionary<string, string?> { { "genre", "fantasy" } }));
+Console.WriteLine("\n***************** Append Query String Using AddQueryString Method ***************\n");
+Console.WriteLine(QueryStringHelper.CreateURLUsingAddQueryString(url, new Dictionary<string, string?> { { "genre", "science fiction" } }));
 
 Console.WriteLine("\n***************** Modify Query String Manually ***************\n");
 Console.WriteLine(QueryStringHelper.CreateURL(url, queryParams));
