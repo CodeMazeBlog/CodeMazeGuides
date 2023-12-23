a09efea1f798ceb8ae89b34aabf2fe0bd1fa586d
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2023-12-24 00:46:20 +0330

    fix: unused class is removed

--- json-csharp/EnsuringStringAsValidJson/JsonValidators/RegularExpressionsUseCase.cs
+++ json-csharp/EnsuringStringAsValidJson/JsonValidators/RegularExpressionsUseCase.cs
@@ -6,6 +6,7 @@ namespace JsonValidators;
 public class RegularExpressionsUseCase : IJsonValidator
 {
     private const string JsonPattern = "(?<json>{(?:[^{}]|(?<Nested>{)|(?<-Nested>}))*(?(Nested)(?!))})";
+    
     public bool IsValid(string jsonString)
     {
         return Regex.IsMatch(jsonString, JsonPattern);
