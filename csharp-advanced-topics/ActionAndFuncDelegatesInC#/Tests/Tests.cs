cbc6b89efeb96720aec0416704653fb7217cfa3e
Fredrick Amoako <105827771+amt-fredrick-amoako@users.noreply.github.com>
2023-12-24 21:47:28 +0000

    remove some tests

--- csharp-advanced-topics/ActionAndFuncDelegatesInC#/Tests/Tests.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInC#/Tests/Tests.cs
@@ -32,22 +32,6 @@ public class Tests
     }
 
     [Fact]
-    public void Action_Prints_List_Item_To_Console()
-    {
-        StringWriter sw = new();
-        Console.SetOut(sw);
-
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
-        numbers.ForEach(Program.processNumber);
-
-        string actualOutput = sw.ToString().Trim();
-
-        string expectedOutput = "Processing:1\r\nProcessing:2\r\nProcessing:3\r\nProcessing:4\r\nProcessing:5\r\nProcessing:6";
-
-        Assert.Equal(expectedOutput, actualOutput);
-    }
-
-    [Fact]
     public void Func_Handles_Division_Gracefully()
     {
         int divisionResult = Program.safeDivision(20, 5);
