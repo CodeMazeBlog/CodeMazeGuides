782e855ce9e9c0da75f1687d4955d9554a4066c2
Kaybangz <devcaleb96@gmail.com>
2023-12-25 17:30:09 +0100

    Renamed const and static field in MessageConversionTests using PascalCase convention.

--- collections-arrays/ConvertingStringToByteArray/ConvertStringToByteArrayTests/MessageConversionTests.cs
+++ collections-arrays/ConvertingStringToByteArray/ConvertStringToByteArrayTests/MessageConversionTests.cs
@@ -2,47 +2,47 @@ namespace ConvertStringToByteArrayTests;
 
 public class MessageConversionTests
 {
-    private const string _message = "Welcome to CodeMaze!";
-    private static readonly byte[] _expectedByteArray
+    private const string Message = "Welcome to CodeMaze!";
+    private static readonly byte[] ExpectedByteArray
         = new byte[] { 87, 101, 108, 99, 111, 109, 101, 32, 116, 111, 32, 67, 111, 100, 101, 77, 97, 122, 101, 33 };
 
     [Fact]
     public void WhenConvertingToUTF8Bytes_ThenReturnByteArray()
     {
         // Act
-        var result = MessageConversion.ConvertStringToUTF8Bytes(_message);
+        var result = MessageConversion.ConvertStringToUTF8Bytes(Message);
 
         // Assert
-        Assert.Equal(_expectedByteArray, result);
+        Assert.Equal(ExpectedByteArray, result);
     }
 
     [Fact]
     public void WhenConvertingToByteArrayUsingCasting_ThenReturnByteArray()
     {
         // Act
-        var result = MessageConversion.ConvertStringToByteArrayUsingCasting(_message);
+        var result = MessageConversion.ConvertStringToByteArrayUsingCasting(Message);
 
         // Assert
-        Assert.Equal(_expectedByteArray, result);
+        Assert.Equal(ExpectedByteArray, result);
     }
 
     [Fact]
     public void WhenConvertingToByteArrayUsingConvertToByte_ThenReturnByteArray()
     {
         // Act
-        var result = MessageConversion.ConvertStringToByteArrayUsingConvertToByte(_message);
+        var result = MessageConversion.ConvertStringToByteArrayUsingConvertToByte(Message);
 
         // Assert
-        Assert.Equal(_expectedByteArray, result);
+        Assert.Equal(ExpectedByteArray, result);
     }
 
     [Fact]
     public void WhenConvertingToByteArrayUsingEncoding_ThenReturnByteArray()
     {
         // Act
-        var result = MessageConversion.ConvertStringToByteArrayUsingEncoding(_message);
+        var result = MessageConversion.ConvertStringToByteArrayUsingEncoding(Message);
 
         // Assert
-        Assert.Equal(_expectedByteArray, result);
+        Assert.Equal(ExpectedByteArray, result);
     }
 }
