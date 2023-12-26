d363cde9b887802782fd441b4042dc93889595c7
nitinkale82 <nitinkale82@gmail.com>
2023-12-26 14:53:30 -0500

    func-action delegate evalution code.

---
+++ func-action-delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace func_action_delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = (a, b) =>
+            {
+                return a + b;
+            };
+            int result = add(5, 3);
+            Console.WriteLine($"The result is: {result}");
+
+            Action<string> greet = (name) =>
+            {
+                Console.WriteLine($"Hello, {name}!");
+            };
+
+            greet("John");
+        }
+    }
+}
