94de9231fc39d816f879e3987977b10b75059e7e
nitinkale82 <nitinkale82@gmail.com>
2023-12-28 10:38:18 -0500

    nitinkale82\func-action-delegate code initial commit

---
+++ csharp-basic-topics/FuncActionDelegate/FuncActionDelegateTests/ProgramTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void TestFuncAddNumbers()
+        {
+            // Arrange
+            Func<int, int, int> add = (x, y) => x + y;
+            int expected = 8; // 5 + 3 = 8
+
+            // Act
+            int result = Program.AddNumbers(5, 3, add);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod()]
+        public void TestPerformAction()
+        {
+            // Arrange
+            bool actionCalled = false;
+
+            // Act
+            Program.PerformAction(() => { actionCalled = true; });
+
+            // Assert
+            Assert.IsTrue(actionCalled);
+        }
+    }
+}
