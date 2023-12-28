e5a7009ea9cbe014f69405ed8c9923a2cedf1b27
Lennart <76707412+lennart02@users.noreply.github.com>
2023-12-28 12:57:51 +0100

    CM-192: Consistent Byte Representation of Strings in C# without Encoding (#1612)
    * CM-192: Consistent Byte Representation of Strings in C# without Encoding
    
    * Fix pipeline: using a unicode file.
    
    * Revise from feedback. Adding a more modern Span variation.
    
    * Moved Project to different location. Refactor of some code.
    
    * Fixed formatting issues and renamed variables
    
    * Fixed formatting of code files
    Use const string
    
    * Make StringByteConverter class static
    
    * Changed Unicode to UTF-8
    
    ---------
    
    Co-authored-by: fl0tschi <fl0tschi>

---
+++ strings-csharp/ByteRepresentationOfStrings/ByteRepresentationOfStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteRepresentationOfStrings;
+using System.Text;
+
+//Enable Unicode characters for the console
+Console.OutputEncoding = Encoding.Unicode;
+
+const string sampleString = "𝓒𝓸𝓭𝓮𝓜𝓪𝔃𝓮";
+byte[] byteArray = StringByteConverter.GetBytes(sampleString);
+string reconstructedString = StringByteConverter.GetString(byteArray);
+
+Console.WriteLine($"Original String: {sampleString}");
+Console.WriteLine($"Converted String: {reconstructedString}");
+
+// Convert string to bytes using Span
+byte[] bytes = StringByteConverter.GetBytesWithSpan(sampleString);
+
+// Convert bytes back to string using Span
+string convertedString = StringByteConverter.GetString(bytes);
+
+Console.WriteLine($"Converted String using span: {convertedString}");
+
+//Example using System.Text.Encoding
+const string cafe = "Café";
+byte[] asciiBytes = Encoding.ASCII.GetBytes(cafe);
+byte[] utf8Bytes = Encoding.UTF8.GetBytes(cafe);
+
+//Different byte length
+Console.WriteLine(asciiBytes.Length);
+Console.WriteLine(utf8Bytes.Length);
+
+Console.WriteLine("ASCII Output: " + Encoding.ASCII.GetString(asciiBytes));
+Console.WriteLine("UTF-8 Output: " + Encoding.UTF8.GetString(utf8Bytes));
