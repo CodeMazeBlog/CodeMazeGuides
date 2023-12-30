9583c18fc3b743f7d19edd023137631435f6fb4d
KaiWesterschwiensterdt <kai.westerschwiensterdt@gmail.com>
2023-12-30 10:43:08 +0100

    Update program class

--- threads-csharp/ThreadSleepVsTaskDelay/src/ThreadSleepVsTaskDelay/Program.cs
+++ threads-csharp/ThreadSleepVsTaskDelay/src/ThreadSleepVsTaskDelay/Program.cs
@@ -1,27 +1,30 @@
-﻿public class Program
+﻿namespace ThreadSleepVsTaskDelay
 {
-    public static async Task UseTaskDelay()
+    public class Program
     {
-        Console.WriteLine($"Before delay: Thread id = {Environment.CurrentManagedThreadId}");
-        await Task.Delay(2000);
-        Console.WriteLine($"After delay: Thread id = {Environment.CurrentManagedThreadId}");
-    }
+        public static async Task UseTaskDelay()
+        {
+            Console.WriteLine($"Before delay: Thread id = {Environment.CurrentManagedThreadId}");
+            await Task.Delay(2000);
+            Console.WriteLine($"After delay: Thread id = {Environment.CurrentManagedThreadId}");
+        }
 
-    public static void UseThreadSleep()
-    {
-        Console.WriteLine($"Before sleep: Thread id = {Environment.CurrentManagedThreadId}");
-        Thread.Sleep(2000);
-        Console.WriteLine($"After sleep: Thread id = {Environment.CurrentManagedThreadId}");
-    }
+        public static void UseThreadSleep()
+        {
+            Console.WriteLine($"Before sleep: Thread id = {Environment.CurrentManagedThreadId}");
+            Thread.Sleep(2000);
+            Console.WriteLine($"After sleep: Thread id = {Environment.CurrentManagedThreadId}");
+        }
 
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Starting Thread.Sleep test...");
-        UseThreadSleep();
-        Console.WriteLine("Thread.Sleep test completed.\n");
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting Thread.Sleep test...");
+            UseThreadSleep();
+            Console.WriteLine("Thread.Sleep test completed.\n");
 
-        Console.WriteLine("Starting Task.Delay test...");
-        UseTaskDelay().Wait();
-        Console.WriteLine("Task.Delay test completed.");
+            Console.WriteLine("Starting Task.Delay test...");
+            await UseTaskDelay();
+            Console.WriteLine("Task.Delay test completed.");
+        }
     }
 }
