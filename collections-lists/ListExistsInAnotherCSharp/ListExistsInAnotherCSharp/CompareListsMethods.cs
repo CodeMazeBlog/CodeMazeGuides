310cfa7fb7cfe2d818169c272a462664aba95529
Martin Chege <martinchege324@gmail.com>
2023-12-30 14:33:49 +0300

    First draft for the article

--- collections-lists/ListExistsInAnotherCSharp/ListExistsInAnotherCSharp/CompareListsMethods.cs
+++ collections-lists/ListExistsInAnotherCSharp/ListExistsInAnotherCSharp/CompareListsMethods.cs
@@ -1,18 +1,66 @@
-﻿namespace ListExistsInAnotherCSharp;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Order;
+using System.Collections;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
+namespace ListExistsInAnotherCSharp;
+
+[MemoryDiagnoser]
+[RankColumn]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByParams)]
 public class CompareListsMethods
 {
-    public bool CompareListUsingIntersect(List<int> firstList, List<int> secondList) 
+    private readonly Random _rand;
+
+    public CompareListsMethods()
+    {
+        _rand = new Random();
+    }
+
+    public IEnumerable<object[]> SampleLists()
+    {
+        yield return new object[] 
+        { 
+            GenerateIntegerList(false, false, true, 10000000), 
+            GenerateIntegerList(false, false, true, 10000000), 
+            "Random" 
+        };
+
+        yield return new object[] 
+        { 
+            GenerateIntegerList(true, false, false, 10000000), 
+            GenerateIntegerList(true, false, false, 10000000), 
+            "Common" 
+        };
+
+        yield return new object[] 
+        { 
+            GenerateIntegerList(false, true, false, 10000000), 
+            GenerateIntegerList(false, false, false, 10000000), 
+            "Equal" 
+        };
+    }
+
+    [ArgumentsSource(nameof(SampleLists))]
+    [Benchmark]
+    public bool CompareListUsingIntersect(List<int> firstList, List<int> secondList, string listName) 
     {
         return firstList.Intersect(secondList).Any();
     }
 
-    public bool CompareListUsingAnyContains(List<int> firstList, List<int> secondList)
+    [ArgumentsSource(nameof(SampleLists))]
+    [Benchmark]
+    public bool CompareListUsingAnyContains(List<int> firstList, List<int> secondList, string listName)
     {
         return secondList.Any(firstList.Contains);
     }
 
-    public bool CompareListUsingIteration(List<int> firstList, List<int> secondList) 
+    [ArgumentsSource(nameof(SampleLists))]
+    [Benchmark]
+    public bool CompareListUsingIteration(List<int> firstList, List<int> secondList, string listName) 
     {
         var elementExists = false;
 
@@ -27,8 +75,59 @@ public class CompareListsMethods
 
         return elementExists;
     }
-    public bool CompareListUsingExcept(List<int> firstList, List<int> secondList)
+
+    [ArgumentsSource(nameof(SampleLists))]
+    [Benchmark]
+    public bool CompareListUsingExcept(List<int> firstList, List<int> secondList, string listName)
     {
-        return !(secondList.Except(firstList).Any());
+        var elementExists = false;
+
+        var difference = secondList.Except(firstList).ToList();
+
+        if (difference.Count == secondList.Count)
+        {
+            return elementExists;
+        }
+        else
+        {
+            elementExists = true;
+            return elementExists;
+        }
+    }
+
+    private List<int> GenerateIntegerList(bool commonElements, bool allElements, bool randomElements, int size)
+    {
+        var integerList = new List<int>();
+
+        if (randomElements == true) 
+        {
+            for(int i = 0; i < size; i++) 
+            {
+                integerList.Add(_rand.Next());
+            }
+        }
+
+        if (allElements == true) 
+        {
+            for (int i = 0; i < size; i++)
+            {
+                integerList.Add(i);
+            }
+        }
+
+        if (commonElements == true) 
+        {
+            for (int i = 0; i < size/2; i++)
+            {
+                integerList.Add(i);
+            }
+
+            for (int i = size/2; i < size; i++)
+            {
+                integerList.Add(_rand.Next());
+            }
+        }
+
+        return integerList;
     }
 }
--- collections-lists/ListExistsInAnotherCSharp/ListExistsInAnotherCSharpTests/ListExistsInAnotherUnitTests.cs
+++ collections-lists/ListExistsInAnotherCSharp/ListExistsInAnotherCSharpTests/ListExistsInAnotherUnitTests.cs
@@ -7,32 +7,41 @@ public class ListExistsInAnotherUnitTests
     private readonly List<int> _firstList;
     private readonly List<int> _secondList;
     private readonly List<int> _thirdList;
+    private readonly string _listName;
 
     public ListExistsInAnotherUnitTests()
     {
         _firstList = new List<int> { 0, 1, 2, 3, 4 };
         _secondList = new List<int> { 5, 6, 7, 8, 9 };
         _thirdList = new List<int> { 0, 1, 2, 3, 5 };
+        _listName = string.Empty;
     }
 
     [TestMethod]
     public void GivenTwoLists_WhenComparedUsingIntersect_ValidateCorrectResults()
     {
-        Assert.IsTrue(_compareListsMethods.CompareListUsingIntersect(_firstList, _thirdList));
-        Assert.IsFalse(_compareListsMethods.CompareListUsingIntersect(_firstList, _secondList));
+        Assert.IsTrue(_compareListsMethods.CompareListUsingIntersect(_firstList, _thirdList, _listName));
+        Assert.IsFalse(_compareListsMethods.CompareListUsingIntersect(_firstList, _secondList, _listName));
     }
 
     [TestMethod]
     public void GivenTwoLists_WhenComparedUsingIteration_ValidateCorrectResults()
     {
-        Assert.IsTrue(_compareListsMethods.CompareListUsingIteration(_firstList, _thirdList));
-        Assert.IsFalse(_compareListsMethods.CompareListUsingIteration(_firstList, _secondList));
+        Assert.IsTrue(_compareListsMethods.CompareListUsingIteration(_firstList, _thirdList, _listName));
+        Assert.IsFalse(_compareListsMethods.CompareListUsingIteration(_firstList, _secondList, _listName));
     }
 
     [TestMethod]
     public void GivenTwoLists_WhenComparedUsingAnyContains_ValidateCorrectResults()
     {
-        Assert.IsTrue(_compareListsMethods.CompareListUsingAnyContains(_firstList, _thirdList));
-        Assert.IsFalse(_compareListsMethods.CompareListUsingAnyContains(_firstList, _secondList));
+        Assert.IsTrue(_compareListsMethods.CompareListUsingAnyContains(_firstList, _thirdList, _listName));
+        Assert.IsFalse(_compareListsMethods.CompareListUsingAnyContains(_firstList, _secondList, _listName));
+    }
+
+    [TestMethod]
+    public void GivenTwoLists_WhenComparedUsingExcept_ValidateCorrectResults()
+    {
+        Assert.IsTrue(_compareListsMethods.CompareListUsingExcept(_firstList, _thirdList, _listName));
+        Assert.IsFalse(_compareListsMethods.CompareListUsingExcept(_firstList, _secondList, _listName));
     }
 }
