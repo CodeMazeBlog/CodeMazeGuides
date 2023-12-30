275a1c6651d8788483beeaaccbc566cda7b97c6d
notarandomcoder <=>
2023-12-31 01:53:25 +0600

    Upgrade to .Net8, Separate out the backslash class

--- csharp-basic-topics/EscapingBackSlashInCSharp/EscapingBackSlash/Program.cs
+++ csharp-basic-topics/EscapingBackSlashInCSharp/EscapingBackSlash/Program.cs
@@ -1,32 +1,11 @@
-﻿public static class BackslashMethods
+﻿public class Program
 {
-    public static string UsingDoubleBackslash()
-    {
-        return "C:\\Users\\User\\Documents";
-    }
-
-    public static string UsingVerbatimStringLiteral()
-    {
-        return @"C:\Users\User\Documents";
-    }
-
-    public static string UsingUnicodeEscapeSequence()
-    {
-        return "C:\u005CUsers\u005CUser\u005CDocuments";
-    }
-
-    public static string UsingStringFormat()
-    {
-        return string.Format("C:{0}Users{0}User{0}Documents", Path.DirectorySeparatorChar);
-    }
-
-    public static string UsingStringInterpolation()
-    {
-        return $"C:{Path.DirectorySeparatorChar}Users{Path.DirectorySeparatorChar}User{Path.DirectorySeparatorChar}Documents";
-    }
-
     public static void Main(string[] args)
     {
-        
+        Console.WriteLine(BackslashMethods.UsingDoubleBackslash());
+        Console.WriteLine(BackslashMethods.UsingVerbatimStringLiteral());
+        Console.WriteLine(BackslashMethods.UsingStringFormat());
+        Console.WriteLine(BackslashMethods.UsingStringInterpolation());
     }
 }
+
