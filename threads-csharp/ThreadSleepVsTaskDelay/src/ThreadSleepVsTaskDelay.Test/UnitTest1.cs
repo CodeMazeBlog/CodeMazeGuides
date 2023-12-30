d24868ce03e03f3420920789deddd2ff98161324
KaiWesterschwiensterdt <kai.westerschwiensterdt@gmail.com>
2023-12-30 10:25:14 +0100

    Add console project and create test project

---
+++ threads-csharp/ThreadSleepVsTaskDelay/src/ThreadSleepVsTaskDelay.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ThreadSleepVsTaskDelay.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ threads-csharp/ThreadSleepVsTaskDelay/src/ThreadSleepVsTaskDelay/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Starting Thread.Sleep test...");
+UseThreadSleep();
+Console.WriteLine("Thread.Sleep test completed.\n");
+
+Console.WriteLine("Starting Task.Delay test...");
+UseTaskDelay().Wait();
+Console.WriteLine("Task.Delay test completed.");
+
+static void UseThreadSleep()
+{
+    Console.WriteLine($"Before sleep: Thread id = {Environment.CurrentManagedThreadId}");
+    Thread.Sleep(2000);
+    Console.WriteLine($"After sleep: Thread id = {Environment.CurrentManagedThreadId}");
+}
+
+static async Task UseTaskDelay()
+{
+    Console.WriteLine($"Before delay: Thread id = {Environment.CurrentManagedThreadId}");
+    await Task.Delay(2000);
+    Console.WriteLine($"After delay: Thread id = {Environment.CurrentManagedThreadId}");
+}
