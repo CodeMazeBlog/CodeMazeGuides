01001019956f4f5b350dc46acdece3a37505a817
notarandomcoder <=>
2023-12-31 18:18:59 +0600

    Remove the program class & the main method

--- csharp-basic-topics/EscapingBackSlashInCSharp/EscapingBackSlash/Program.cs
+++ csharp-basic-topics/EscapingBackSlashInCSharp/EscapingBackSlash/Program.cs
@@ -1,11 +1,5 @@
-﻿public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine(BackslashMethods.UsingDoubleBackslash());
-        Console.WriteLine(BackslashMethods.UsingVerbatimStringLiteral());
-        Console.WriteLine(BackslashMethods.UsingStringFormat());
-        Console.WriteLine(BackslashMethods.UsingStringInterpolation());
-    }
-}
+﻿Console.WriteLine(BackslashMethods.UsingDoubleBackslash());
+Console.WriteLine(BackslashMethods.UsingVerbatimStringLiteral());
+Console.WriteLine(BackslashMethods.UsingStringFormat());
+Console.WriteLine(BackslashMethods.UsingStringInterpolation());
 
