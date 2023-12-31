98648f222dc247ea99c4b23b49fe33819bf907c0
Ainea Wabwoba <aineah28@gmail.com>
2023-12-31 16:20:48 +0300

    Made books property readonly.

--- exception-handling/IExceptionHandlerInterface/IExceptionHandlerInterface/Services/LibraryService.cs
+++ exception-handling/IExceptionHandlerInterface/IExceptionHandlerInterface/Services/LibraryService.cs
@@ -4,12 +4,12 @@ namespace IExceptionHandlerInterface.Services
 {
     public class LibraryService : ILibraryService
     {
-        private readonly List<Book> _books = new()
-        {
+        private readonly List<Book> _books;
+        public LibraryService() => _books = [
             new Book { Id = 1, Title = "The Catcher in the Rye", Author = "J.D. Salinger" },
             new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee" },
             new Book { Id = 3, Title = "1984", Author = "George Orwell" }
-        };
+        ];
 
         public Book GetById(int id)
         {
