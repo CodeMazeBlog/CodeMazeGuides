b80917dd9f7d542f9a728e1c10fc26b0147795ab
Lennart Pries <lennartichbin@gmail.com>
2024-01-01 17:25:40 +0100

    CM-197: How To Escape Special Characters in C# Strings

---
+++ strings-csharp/EscapingCurlyBracketsInFormatStrings/EscapingCurlyBracketsInFormatStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿//Example of Format Strings
+var name = "John";
+var formattedString = string.Format("Hello, {0}!", name);
+Console.WriteLine(formattedString);
+
+//Curly Brackets Escaping
+var value = "World";
+formattedString = string.Format("{{Hello, {0}!}}", value);
+Console.WriteLine(formattedString);
+
+//Dobble Quotes Escaping
+var message = "Important message";
+formattedString = string.Format("\"{0}\" is the message.", message);
+Console.WriteLine(formattedString);
+
+//Backslash Escaping
+var path = @"C:\Program Files\";
+formattedString = string.Format("The installation path is: {0}", path);
+Console.WriteLine(formattedString);
