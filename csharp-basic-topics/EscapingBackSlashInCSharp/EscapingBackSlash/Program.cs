da729ee6e85641f387aa1c083be24b7f5979f77f
notarandomcoder <=>
2024-01-02 13:33:32 +0600

    Remove un-necessary code statements

--- csharp-basic-topics/EscapingBackSlashInCSharp/EscapingBackSlash/Program.cs
+++ csharp-basic-topics/EscapingBackSlashInCSharp/EscapingBackSlash/Program.cs
@@ -4,6 +4,3 @@ Console.WriteLine(BackslashMethods.UsingUnicodeEscapeSequence());
 Console.WriteLine(BackslashMethods.UsingStringFormat());
 Console.WriteLine(BackslashMethods.UsingStringInterpolation());
 Console.WriteLine(BackslashMethods.UsingRawStringLiterals());
-
-string verbatimWithSingleQuote = @"It's a verbatim string with a single quote.";
-Console.WriteLine(verbatimWithSingleQuote);
