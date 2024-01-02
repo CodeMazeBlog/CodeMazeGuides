d6640dee381241a2b4107fad510269c34e9ad737
aditisaxena0289 <aditisaxena0289@gmail.com>
2024-01-01 21:35:24 -0600

    Feedback Revisions - removed unnecessary usings from Train.cs

--- csharp-methods/VirtualAndAbstractMethodInCSharp/VirtualAndAbstractMethodInCSharp/Train.cs
+++ csharp-methods/VirtualAndAbstractMethodInCSharp/VirtualAndAbstractMethodInCSharp/Train.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VirtualAndAbstractMethodInCSharp
+﻿namespace VirtualAndAbstractMethodInCSharp
 {
     // Class representing a train
     internal class Train : TransportMode
