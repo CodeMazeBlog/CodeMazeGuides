b6d1080a040d1ae56480fa922891ee7f8c28bd0e
notarandomcoder <=>
2024-01-02 10:56:59 +0600

    Add method for raw literals, remove un-necessary spaces

--- csharp-basic-topics/EscapingBackSlashInCSharp/Tests/Tests.cs
+++ csharp-basic-topics/EscapingBackSlashInCSharp/Tests/Tests.cs
@@ -4,6 +4,7 @@ namespace Tests;
 public class Tests
 {
     private const string path = "C:\\Users\\User\\Documents";
+    private string pathUsingDirSeparator = $"C:{Path.DirectorySeparatorChar}Users{Path.DirectorySeparatorChar}User{Path.DirectorySeparatorChar}Documents";
 
     [TestMethod]
     public void GivenDoubleBackslash_WhenCallingMethod_ThenReturnEscapedFilePath()
@@ -26,12 +27,12 @@ public class Tests
     [TestMethod]
     public void GivenStringFormatMethod_WhenCallingMethod_ThenReturnFormattedFilePath()
     {   
-        Assert.AreEqual($"C:{Path.DirectorySeparatorChar}Users{Path.DirectorySeparatorChar}User{Path.DirectorySeparatorChar}Documents", BackslashMethods.UsingStringFormat());
+        Assert.AreEqual(pathUsingDirSeparator, BackslashMethods.UsingStringFormat());
     }
 
     [TestMethod]
     public void GivenStringInterpolation_WhenCallingMethod_ThenReturnInterpolatedFilePath()
     {
-        Assert.AreEqual($"C:{Path.DirectorySeparatorChar}Users{Path.DirectorySeparatorChar}User{Path.DirectorySeparatorChar}Documents", BackslashMethods.UsingStringFormat());
+        Assert.AreEqual(pathUsingDirSeparator, BackslashMethods.UsingStringFormat());
     }
 }
