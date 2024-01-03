9ceb39dd7ce81f5766273bb64eba488064572738
Vadzim <01vadim10@gmail.com>
2024-01-03 22:24:35 +0100

    Logging. Simple scenario

---
+++ dotnet-logging/MessageTemplateForLoggingCA2254/MessageTemplateForLoggingCA2254/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Environment.CurrentDirectory)
+    .AddEnvironmentVariables()
+    .Build();
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddConfiguration(configuration);
+    })
+    .Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+const string userName = "John Doe";
+var loginTime = DateTime.Now;
+
+logger.LogInformation($"User {userName} logged in at {loginTime}");
