52c5c3daf0c83046db72000a915142b560c6ed55
Fredrick Amoako <105827771+amt-fredrick-amoako@users.noreply.github.com>
2024-01-04 12:04:33 +0000

    use BDD naming for test methods

--- csharp-advanced-topics/ActionAndFuncDelegatesInC#/Tests/Tests.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInC#/Tests/Tests.cs
@@ -5,44 +5,45 @@ namespace Tests;
 public class Tests
 {
     [Fact]
-    public void Action_Logs_Parameters_To_Console()
+    public void WhenGreetedWithName_ThenOutputsHelloWithName()
     {
         StringWriter sw = new();
         Console.SetOut(sw);
 
         Program.greet("Fred");
 
-        string expectedOutput = "Hello, Fred!";
-        string actualOutput = sw.ToString().Trim();
+        var expectedOutput = "Hello, Fred!";
+        var actualOutput = sw.ToString().Trim();
+
         Assert.Equal(expectedOutput, actualOutput);
     }
 
     [Fact]
-    public void Func_Returns_A_Value()
+    public void WhenMultiplyingNumbersAndMeasuringStringLength_ThenValidResultsReturned()
     {
-        int product = Program.multiply(8, 5);
+        var product = Program.multiply(8, 5);
+        var length = Program.stringLength("Love Func!");
 
-        Func<string, int> stringLength = (str) => str.Length;
-        int length = Program.stringLength("Love Func!");
+        var expectedProduct = 8 * 5;
+        var expectLength = 10;
 
-        int expectedProduct = 8 * 5;
-        int expectLength = 10;
         Assert.Equal(expectedProduct, product);
+
         Assert.Equal(expectLength, length);
     }
 
     [Fact]
-    public void Func_Handles_Division_Gracefully()
+    public void WhenPerformingSafeDivision_ThenHandlesDivisionGracefully()
     {
-        int divisionResult = Program.safeDivision(20, 5);
+        var divisionResult = Program.safeDivision(20, 5);
 
         Assert.Equal(4, divisionResult);
     }
 
     [Fact]
-    public void Func_Returns_Zero_When_Its_Divided_By_Zero()
+    public void WhenDividingByZero_ThenReturnsZero()
     {
-        int divisionResult = Program.safeDivision(20, 0);
+        var divisionResult = Program.safeDivision(20, 0);
 
         Assert.Equal(0, divisionResult);
     }
