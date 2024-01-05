2eb89526062217d903c782deea34a53ad9fabdd0
oladunjoye afeez <77560645+cinfotech94@users.noreply.github.com>
2024-01-05 15:41:52 +0100

    Add files via upload

--- ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
+++ ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -1,26 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 namespace ActionAndFuncDelegates
 {
     public class ActionAndFuncDelegates
     {
-        static void Main()
+        public static void Main()
         {
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
             // Print the numbers using Action delegate
             PrintNumbers(numbers);
             Console.WriteLine(); // New line for clarity
+
             // Calculate the sum of the numbers using Func delegate
             int sum = CalculateSum(numbers);
             Console.WriteLine($"Sum of the numbers: {sum}");
         }
+
         // Action delegate to print numbers
         public static void PrintNumbers(List<int> numbers)
         {
             Action<int> printNumber = (num) => Console.Write($"{num} ");
             numbers.ForEach(printNumber);
         }
+
         // Func delegate to calculate the sum of numbers
         public static int CalculateSum(List<int> numbers)
         {
