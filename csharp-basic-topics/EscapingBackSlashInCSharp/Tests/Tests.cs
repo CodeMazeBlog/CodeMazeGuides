9623661444576017c77413f46b986568e7e1cdf6
jshergal <1489638+jshergal@users.noreply.github.com>
2024-01-05 23:21:00 +0200

    Minor formatting in code

--- csharp-basic-topics/EscapingBackSlashInCSharp/Tests/Tests.cs
+++ csharp-basic-topics/EscapingBackSlashInCSharp/Tests/Tests.cs
@@ -3,36 +3,39 @@ namespace Tests;
 [TestClass]
 public class Tests
 {
-    private const string path = "C:\\Users\\User\\Documents";
-    private string pathUsingDirSeparator = $"C:{Path.DirectorySeparatorChar}Users{Path.DirectorySeparatorChar}User{Path.DirectorySeparatorChar}Documents";
+    private const string ExpectedPath = @"C:\Users\User\Documents";
+    
+    private static readonly char PathSeparator = Path.DirectorySeparatorChar;
+    private static readonly string PathUsingDirSeparator =
+        $"C:{PathSeparator}Users{PathSeparator}User{PathSeparator}Documents";
 
     [TestMethod]
     public void GivenDoubleBackslash_WhenCallingMethod_ThenReturnEscapedFilePath()
     {
-        Assert.AreEqual(path, BackslashMethods.UsingDoubleBackslash());
+        Assert.AreEqual(ExpectedPath, BackslashMethods.UsingDoubleBackslash());
     }
 
     [TestMethod]
     public void GivenVerbatimStringLiteral_WhenCallingMethod_ThenReturnFilePathWithVerbatimString()
     {
-        Assert.AreEqual(path, BackslashMethods.UsingVerbatimStringLiteral());
+        Assert.AreEqual(ExpectedPath, BackslashMethods.UsingVerbatimStringLiteral());
     }
 
     [TestMethod]
     public void GivenUnicodeEscapeSequence_WhenCallingMethod_ThenReturnFilePathWithUnicodeEscape()
     {
-        Assert.AreEqual(path, BackslashMethods.UsingUnicodeEscapeSequence());
+        Assert.AreEqual(ExpectedPath, BackslashMethods.UsingUnicodeEscapeSequence());
     }
 
     [TestMethod]
     public void GivenStringFormatMethod_WhenCallingMethod_ThenReturnFormattedFilePath()
     {   
-        Assert.AreEqual(pathUsingDirSeparator, BackslashMethods.UsingStringFormat());
+        Assert.AreEqual(PathUsingDirSeparator, BackslashMethods.UsingStringFormat());
     }
 
     [TestMethod]
     public void GivenStringInterpolation_WhenCallingMethod_ThenReturnInterpolatedFilePath()
     {
-        Assert.AreEqual(pathUsingDirSeparator, BackslashMethods.UsingStringFormat());
+        Assert.AreEqual(PathUsingDirSeparator, BackslashMethods.UsingStringFormat());
     }
 }
