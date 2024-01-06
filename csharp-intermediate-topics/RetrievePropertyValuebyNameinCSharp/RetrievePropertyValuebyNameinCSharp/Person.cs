1d7acf504f73f7127606474c3c489cea28e9d144
kevinLug <kevinotim4@gmail.com>
2024-01-06 19:10:33 +0300

    Separate private and public fields with a whitespace

--- csharp-intermediate-topics/RetrievePropertyValuebyNameinCSharp/RetrievePropertyValuebyNameinCSharp/Person.cs
+++ csharp-intermediate-topics/RetrievePropertyValuebyNameinCSharp/RetrievePropertyValuebyNameinCSharp/Person.cs
@@ -2,6 +2,7 @@
 public class Person
 {
     private Guid _id = Guid.NewGuid();
+
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public int Age { get; set; } = default;
