e69a53ba2a860d359e17eca867731e2ea1941c76
Ivan Matec <im@bullwall.com>
2024-01-07 19:10:52 +0100

    - minor change

--- csharp-linq/GetFirstRecordInEachGroup/GetFirstRecordInEachGroup/PerformanceBenchmark.cs
+++ csharp-linq/GetFirstRecordInEachGroup/GetFirstRecordInEachGroup/PerformanceBenchmark.cs
@@ -1,35 +1,34 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
 
-namespace GetFirstRecordInEachGroup
+namespace GetFirstRecordInEachGroup;
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[HideColumns(new string[] { "Job", "Error", "StdDev", "Median" })]
+[MemoryDiagnoser(false)]
+[RankColumn]
+public class PerformanceBenchmark 
 {
-    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
-    [HideColumns(new string[] { "Job", "Error", "StdDev", "Median" })]
-    [MemoryDiagnoser(false)]
-    [RankColumn]
-    public class PerformanceBenchmark 
-    {
-        private static readonly List<Student> students = 
-            Methods.GenerateStudents();
+    private static readonly List<Student> students = 
+        Methods.GenerateStudents();
 
-        [Benchmark]
-        public List<Student> LinqGroupBy1() => 
-            students.GetYoungestStudentInClassLinqGroupBy1();
+    [Benchmark]
+    public List<Student> LinqGroupBy1() => 
+        students.GetYoungestStudentInClassLinqGroupBy1();
 
-        [Benchmark]
-        public List<Student> LinqGroupBy2() => 
-            students.GetYoungestStudentInClassLinqGroupBy2();
+    [Benchmark]
+    public List<Student> LinqGroupBy2() => 
+        students.GetYoungestStudentInClassLinqGroupBy2();
 
-        [Benchmark]
-        public List<Student> LinqLookup() =>
-           students.GetYoungestStudentInClassLinqLookup();
+    [Benchmark]
+    public List<Student> LinqLookup() =>
+       students.GetYoungestStudentInClassLinqLookup();
 
-        [Benchmark]
-        public List<Student> LinqDictionary() =>
-           students.GetYoungestStudentInClassLinqDictionary();
+    [Benchmark]
+    public List<Student> LinqDictionary() =>
+       students.GetYoungestStudentInClassLinqDictionary();
 
-        [Benchmark]
-        public List<Student> IterativeDictionary() => 
-            students.GetYoungestStudentInClassIterativeDictionary();
-    }
+    [Benchmark]
+    public List<Student> IterativeDictionary() => 
+        students.GetYoungestStudentInClassIterativeDictionary();
 }
