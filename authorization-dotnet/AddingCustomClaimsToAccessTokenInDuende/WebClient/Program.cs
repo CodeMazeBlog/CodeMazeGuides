8937f364b234474f96f4c6206948e80a78d51ee3
Alexander Muntyan <almunt.it@gmail.com>
2024-01-08 22:48:39 +0200

    CM-354: Adding custom claims to access token in Duende (#1632)
    * CM-354: create a solution and add an IdentityServer project
    
    * CM-354: add a web client
    
    * CM-354: create a custom profile service
    
    * CM-354: improve the custom profile service
    
    * CM-354: add a tests project
    
    * CM-354: resolve PR comments
    
    * CM-354: resolve PR comments

---
+++ authorization-dotnet/AddingCustomClaimsToAccessTokenInDuende/WebClient/Program.cs
@@ -0,0 +1,52 @@
+using System.IdentityModel.Tokens.Jwt;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+
+        options.ClientId = "web";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("payments");
+
+        options.SaveTokens = true;
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages().RequireAuthorization();
+
+app.Run();
