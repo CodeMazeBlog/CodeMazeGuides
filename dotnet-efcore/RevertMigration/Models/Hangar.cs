7603bcee952259d66c852581281fd963e456c73e
Matthew Nordberg <babygrandmn@gmail.com>
2024-01-14 13:45:33 -0600

    CM-898 (clean, latest updates only) (#1679)
    * #cm-898 remove extra spacing in program.cs
    
    * #cm-898 nullable entity properties

--- dotnet-efcore/RevertMigration/Models/Hangar.cs
+++ dotnet-efcore/RevertMigration/Models/Hangar.cs
@@ -1,7 +1,7 @@
 public class Hangar
 {
     public int Id { get; set; }
-    public string HangarNumber { get; set; }
+    public string? HangarNumber { get; set; }
     public bool HasDoors { get; set; }
 
     public List<Airplane> Airplanes { get; } = new();
