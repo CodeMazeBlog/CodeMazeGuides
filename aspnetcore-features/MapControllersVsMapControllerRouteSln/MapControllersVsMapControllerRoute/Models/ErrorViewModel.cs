03d9a8c54de1409fba841e29bb04c16567dda698
Emeka Emego <emego.emeka@gmail.com>
2024-01-15 20:22:25 +0100

    add MapControllers vs MapControllerRoute project

---
+++ aspnetcore-features/MapControllersVsMapControllerRouteSln/MapControllersVsMapControllerRoute/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MapControllersVsMapControllerRoute.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
