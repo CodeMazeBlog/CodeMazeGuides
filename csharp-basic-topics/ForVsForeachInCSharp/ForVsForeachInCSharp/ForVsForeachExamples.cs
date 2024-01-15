e5ea14a537b5c6645b8e28cae87fdcaf1757296c
Martin Chege <martinchege324@gmail.com>
2024-01-15 11:57:11 +0300

    Benchmark strategy

--- csharp-basic-topics/ForVsForeachInCSharp/ForVsForeachInCSharp/ForVsForeachExamples.cs
+++ csharp-basic-topics/ForVsForeachInCSharp/ForVsForeachInCSharp/ForVsForeachExamples.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+﻿using BenchmarkDotNet.Attributes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ForVsForeachInCSharp;
 
 public class ForVsForeachExamples
 {
     private readonly GenerateData _generateData = new();
-    private readonly int[] _intArray;
-    private readonly List<int> _intList;
-    private readonly Stack<int> _intStack;
-    private readonly Queue<int> _intQueue;
-    private readonly Dictionary<int, int> _intDictionary;
+    private int[] _intArray;
+    private List<int> _intList;
+    private Stack<int> _intStack;
+    private Queue<int> _intQueue;
+    private Dictionary<int, int> _intDictionary;
+    private readonly int _intValue;
 
     public ForVsForeachExamples() 
     {
@@ -18,47 +20,65 @@ public class ForVsForeachExamples
         _intStack = _generateData.GenerateRandomStack(10000000);
         _intQueue = _generateData.GenerateRandomQueue(10000000);
         _intDictionary = _generateData.GenerateRandomDictionary(10000000);
+        _intValue = 12345;
     }
-    public void PrintArrayUsingForeach() 
+
+    [Benchmark]
+    public int[] PrintArrayUsingForeach() 
     {
         foreach (var element in _intArray) 
         {
-            Console.WriteLine(element);
+            _intArray[element - 1] = _intValue;
         }
+
+        return _intArray;
     }
 
-    public void PrintArrayUsingForLoop()
+    [Benchmark]
+    public int[] PrintArrayUsingForLoop()
     {
         for (int i = 0; i < _intArray.Length; i++)
         {
-            Console.WriteLine(_intArray[i]);
+            _intArray[i] = _intValue;
         }
+
+        return _intArray;
     }
 
-    public void PrintListUsingForeach()
+    [Benchmark]
+    public List<int> PrintListUsingForeach()
     {
         foreach (var element in _intList)
         {
-            Console.WriteLine(element);
+            _intList[_intList.IndexOf(element)] = _intValue;
         }
+
+        return _intList;
     }
 
-    public void PrintListUsingForLoop()
+    [Benchmark]
+    public List<int> PrintListUsingForLoop()
     {
         for (int i = 0; i < _intList.Count; i++)
         {
-            Console.WriteLine(_intList[i]);
+            _intList[i] = _intValue;
         }
+
+        return _intList;
     }
 
-    public void PrintStackUsingForeach()
+    [Benchmark]
+    public Stack<int> PrintStackUsingForeach()
     {
         foreach (var element in _intStack)
         {
             Console.WriteLine(element);
         }
+
+        return _intStack;
     }
 
+    [Benchmark]
     public void PrintStackUsingForLoop()
     {
         var array = _intStack.ToArray();
@@ -69,6 +89,7 @@ public class ForVsForeachExamples
         }
     }
 
+    [Benchmark]
     public void PrintQueueUsingForeach()
     {
         foreach (var element in _intQueue)
@@ -77,6 +98,7 @@ public class ForVsForeachExamples
         }
     }
 
+    [Benchmark]
     public void PrintQueueUsingForLoop()
     {
         var array = _intQueue.ToArray();
@@ -87,21 +109,26 @@ public class ForVsForeachExamples
         }
     }
 
+    [Benchmark]
     public void PrintDictionaryUsingForeach()
     {
-        foreach (var element in _intDictionary)
+        foreach (KeyValuePair<int, int> kvp in _intDictionary)
         {
-            Console.WriteLine(element);
+            Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
         }
     }
 
-    //public void PrintDictionaryUsingForLoop()
-    //{
-    //    var array = _intQueue.ToArray();
+    [Benchmark]
+    public void PrintDictionaryUsingForLoop()
+    {
+        var keysArray = new int[_intDictionary.Count];
+        _intDictionary.Keys.CopyTo(keysArray, 0);
 
-    //    for (int i = 0; i < array.Length; i++)
-    //    {
-    //        Console.WriteLine(array[i]);
-    //    }
-    //}
+        for (int i = 0; i < keysArray.Length; i++)
+        {
+            var key = keysArray[i];
+            var value = _intDictionary[key];
+            Console.WriteLine($"Key: {key}, Value: {value}");
+        }
+    }
 }
