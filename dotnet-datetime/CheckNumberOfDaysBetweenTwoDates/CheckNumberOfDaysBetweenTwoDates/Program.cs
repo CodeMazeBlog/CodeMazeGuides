0f0c012e66947890f3305d94bc5679bfcc09496e
fl0tschi <fl0tschi>
2024-01-16 21:21:59 +0100

    Fixed var usage in Program.cs

--- dotnet-datetime/CheckNumberOfDaysBetweenTwoDates/CheckNumberOfDaysBetweenTwoDates/Program.cs
+++ dotnet-datetime/CheckNumberOfDaysBetweenTwoDates/CheckNumberOfDaysBetweenTwoDates/Program.cs
@@ -1,13 +1,13 @@
 ﻿using CheckNumberOfDaysBetweenTwoDates;
 
-DateTime summerVacationStart = new DateTime(2024, 6, 1);
-DateTime currentDate = DateTime.Today;
+var summerVacationStart = new DateTime(2024, 6, 1);
+var currentDate = DateTime.Today;
 
 int daysUntilVacation = NumberOfDaysBetweenTwoDates.CalculateDaysUntilVacation(summerVacationStart, currentDate);
 Console.WriteLine($"Days until summer vacation: {daysUntilVacation}");
 
-DateTimeOffset eventDateTime = new DateTimeOffset(2024, 9, 1, 10, 0, 0, new TimeSpan(-5, 0, 0));
-DateTimeOffset currentDateTime = DateTimeOffset.Now;
+var eventDateTime = new DateTimeOffset(2024, 9, 1, 10, 0, 0, new TimeSpan(-5, 0, 0));
+var currentDateTime = DateTimeOffset.Now;
 
 int daysUntilEvent = NumberOfDaysBetweenTwoDates.CalculateDaysUntilEvent(eventDateTime, currentDateTime);
 Console.WriteLine($"Days until the event: {daysUntilEvent}");
