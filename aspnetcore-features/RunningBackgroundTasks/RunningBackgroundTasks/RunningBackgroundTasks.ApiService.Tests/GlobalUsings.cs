8a1889b8a6fa74f58497aee2306dd2e9c8a59b0e
Ivan Gechev <ivan_banov@abv.bg>
2024-01-17 17:49:54 +0200

    One_off tests

---
+++ aspnetcore-features/RunningBackgroundTasks/RunningBackgroundTasks/RunningBackgroundTasks.ApiService.Tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+global using NSubstitute;
+global using RunningBackgroundTasks.ApiService.Services.Abstractions;
+global using RunningBackgroundTasks.ApiService.Services.One_off;
+global using RunningBackgroundTasks.Domain;
+global using Xunit;
---
+++ aspnetcore-features/RunningBackgroundTasks/RunningBackgroundTasks/RunningBackgroundTasks.ApiService.Tests/One-off/InitializationHostedLifecycleServiceTests.cs
@@ -0,0 +1,26 @@
+﻿namespace RunningBackgroundTasks.ApiService.Tests.One_off;
+
+public class InitializationHostedLifecycleServiceTests
+{
+    [Fact]
+    public async Task WhenStartAsyncIsInvoked_ThenSeedDatabaseAsyncMethodIsCalledOnce()
+    {
+        // Arrange
+        // Arrange
+        var worker = Substitute.For<IWorker>();
+        var serviceProvider = new ServiceCollection()
+            .AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("someDb"))
+            .BuildServiceProvider();
+        var sut = new InitializationHostedService(worker, serviceProvider);
+
+        // Act
+        await sut.StartAsync(CancellationToken.None);
+
+        // Assert
+        await worker.ReceivedWithAnyArgs(1)
+            .SeedDatabaseAsync(
+                Arg.Any<ApplicationDbContext>(),
+                Arg.Any<CancellationToken>());
+    }
+}
