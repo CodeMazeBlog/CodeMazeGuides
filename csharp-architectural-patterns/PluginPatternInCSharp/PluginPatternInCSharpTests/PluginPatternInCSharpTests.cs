01343ccc56a830bad098845754be0dc21461458c
Code Maze <contact@code-maze.com>
2024-01-18 11:50:48 +0100

    CM-826: Plugin Architectural Pattern in C#

--- csharp-architectural-patterns/PluginPatternInCSharp/PluginPatternInCSharpTests/PluginPatternInCSharpTests.cs
+++ csharp-architectural-patterns/PluginPatternInCSharp/PluginPatternInCSharpTests/PluginPatternInCSharpTests.cs
@@ -1,18 +1,16 @@
-﻿using PluginPatternInCSharp;
-
-namespace PluginPatternInCSharpTests
+﻿namespace PluginPatternInCSharpTests
 {
     [TestClass]
     public class PluginPatternInCSharpTests
     {
         [TestMethod]
-        public void GivenCommandString_WhenMainIsInvoked_ThenProgramRunsWithoutIssues()
+        public void WhenMainIsInvoked_ThenProgramRunsWithoutIssues()
         {
             Program.Main(new [] {"temperature"});
         }
 
         [TestMethod]
-        public void GivenNoCommandString_WhenMainIsInvoked_ThenProgramRunsWithoutIssues()
+        public void WhenMainIsInvoked_ThenProgramRuns()
         {
             Program.Main([]);
         }
