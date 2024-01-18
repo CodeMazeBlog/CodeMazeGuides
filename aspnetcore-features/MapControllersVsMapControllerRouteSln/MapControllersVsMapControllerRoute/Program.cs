5ca9f0e9e5ea754910fa122b0311c9fb76cf988d
Emeka Emego <emego.emeka@gmail.com>
2024-01-18 14:40:59 +0100

    minor fixes, deletion of redundant files

--- aspnetcore-features/MapControllersVsMapControllerRouteSln/MapControllersVsMapControllerRoute/Program.cs
+++ aspnetcore-features/MapControllersVsMapControllerRouteSln/MapControllersVsMapControllerRoute/Program.cs
@@ -1,15 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,8 +14,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-// app.MapControllers();
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
