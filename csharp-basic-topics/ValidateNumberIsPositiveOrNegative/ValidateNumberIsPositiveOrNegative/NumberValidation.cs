1d075e1ec2b1a819629300f4d15b128b4508cbeb
nitinkale82 <nitinkale82@gmail.com>
2024-01-20 21:17:56 -0500

    Refactoring code.

---
+++ csharp-basic-topics/ValidateNumberIsPositiveOrNegative/ValidateNumberIsPositiveOrNegative/NumberValidation.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateNumberIsPositiveOrNegative
+{
+    public static class NumberValidation
+    {
+        public static int IsPositiveOrNegativeUsingConditionalMethod(int number)
+        {
+            var result = 0;
+
+            if (number > 0)
+            {
+                result = 1;
+            }
+            else if (number < 0)
+            {
+                result = - 1;
+            }
+            else
+            {
+                result = 0;
+            }
+            return result;
+        }
+        public static int IsPositiveOrNegativeUsingLeftShiftMethod(int number)
+        {
+            var result = 0;
+            // Using left shift operator to check the sign bit
+            if ((number & (1 << 31)) == 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = -1;
+            }
+            return result;
+        }
+        public static int IsPositiveOrNegativeUsingRightShiftMethod(int number)
+        {
+            var result = 0;
+            // Using right shift operator to check the sign bit
+            if ((number >> 31) == 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = -1;
+            }
+            return result;
+        }
+        public static int IsPositiveOrNegativeUsingMathAbsMethod(int number)
+        {
+            var result = 0;
+            int absoluteValue = Math.Abs(number);
+
+            if (absoluteValue == number)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = -1;
+            }
+            return result;
+        }
+        public static int IsPositiveOrNegativeUsingMathSignMethod(int number)
+        {
+            var result = 0;
+            int sign = Math.Sign(number);
+
+            if (sign == 1)
+            {
+                result = 1;
+            }
+            else if (sign == -1)
+            {
+                result = -1;
+            }
+            else
+            {
+                result = 0;
+            }
+            return result;
+        }
+        public static int IsPositiveOrNegative(int number)
+        {
+            var result = 0;
+            bool isPositive = Int32.IsPositive(number);
+            bool isNegative = Int32.IsNegative(number);
+
+            if (isPositive)
+            {
+                result = 1;
+            }
+            if (isNegative)
+            {
+                result = -1;
+            }
+            return result;
+        }
+    }
+}
--- csharp-basic-topics/ValidateNumberIsPositiveOrNegative/ValidateNumberIsPositiveOrNegativeTests/ValidateNumberTest.cs
+++ csharp-basic-topics/ValidateNumberIsPositiveOrNegative/ValidateNumberIsPositiveOrNegativeTests/ValidateNumberTest.cs
@@ -1,129 +1,76 @@
+using ValidateNumberIsPositiveOrNegative;
+
 namespace ValidateNumberIsPositiveOrNegativeTests
 {
     [TestFixture]
     public class ValidateNumberTest
     {
-        [TestCase(60, "IsPositiveOrNegative_ConditionalMethod - 60 is positive number.")]
-        [TestCase(-30, "IsPositiveOrNegative_ConditionalMethod - -30 is negative number.")]
-        [TestCase(0, "IsPositiveOrNegative_ConditionalMethod - The 0 is zero.")]
-        public void GivenNumber_WhenUsingConditionalMethod_ThenPrintResult(int number, string expectedOutput)
+        [TestCase(60, 1)]
+        [TestCase(-30, -1)]
+        [TestCase(0, 0)]
+        public void GivenNumber_WhenUsingConditionalMethod_ThenPrintResult(int number, int expectedOutput)
         {
             // Arrange - Nothing to arrange in this case
 
             // Act
-            using (ConsoleOutputRedirector outputRedirector = new ConsoleOutputRedirector())
-            {
-                 Program.IsPositiveOrNegative_ConditionalMethod(number);
-                string actualOutput = outputRedirector.GetOutput();
+                var actualOutput= NumberValidation.IsPositiveOrNegativeUsingConditionalMethod(number);
 
-                // Assert
-                Assert.AreEqual(expectedOutput, actualOutput.Trim());
-            }
+            // Assert
+            Assert.That(expectedOutput, Is.EqualTo(actualOutput));
         }
-        [TestCase(60, "IsPositiveOrNegative_LeftShiftMethod - 60 is a positive number (using Left shift).")]
-        [TestCase(-30, "IsPositiveOrNegative_LeftShiftMethod - -30 is a negative number (using left shift).")]
-        public void GivenNumber_WhenUsingLeftShiftMethod_ThenPrintResult(int number, string expectedOutput)
+        [TestCase(60, 1)]
+        [TestCase(-30, -1)]
+        public void GivenNumber_WhenUsingLeftShiftMethod_ThenPrintResult(int number, int expectedOutput)
         {
             // Arrange - Nothing to arrange in this case
 
             // Act
-            using (ConsoleOutputRedirector outputRedirector = new ConsoleOutputRedirector())
-            {
-                Program.IsPositiveOrNegative_LeftShiftMethod(number);
-                string actualOutput = outputRedirector.GetOutput();
+            var actualOutput = NumberValidation.IsPositiveOrNegativeUsingLeftShiftMethod(number);
 
-                // Assert
-                Assert.AreEqual(expectedOutput, actualOutput.Trim());
-            }
+            // Assert
+            Assert.That(expectedOutput, Is.EqualTo(actualOutput));
         }
-        [TestCase(60, "IsPositiveOrNegative_RightShiftMethod - 60 is a positive number (using right shift).")]
-        [TestCase(-30, "IsPositiveOrNegative_RightShiftMethod - -30 is a negative number (using right shift).")]
-        public void GivenNumber_WhenUsingRightShiftMethod_ThenPrintResult(int number, string expectedOutput)
+        [TestCase(60, 1)]
+        [TestCase(-30, -1)]
+        public void GivenNumber_WhenUsingRightShiftMethod_ThenPrintResult(int number, int expectedOutput)
         {
-            // Arrange - Nothing to arrange in this case
-
             // Act
-            using (ConsoleOutputRedirector outputRedirector = new ConsoleOutputRedirector())
-            {
-                Program.IsPositiveOrNegative_RightShiftMethod(number);
-                string actualOutput = outputRedirector.GetOutput();
+            var actualOutput = NumberValidation.IsPositiveOrNegativeUsingRightShiftMethod(number);
 
-                // Assert
-                Assert.AreEqual(expectedOutput, actualOutput.Trim());
-            }
+            // Assert
+            Assert.That(expectedOutput, Is.EqualTo(actualOutput));
         }
-        [TestCase(60, "IsPositiveOrNegative_MathAbsMethod - 60 is a positive number.")]
-        [TestCase(-30, "IsPositiveOrNegative_MathAbsMethod - -30 is a negative number.")]
-        public void GivenNumber_WhenUsingMathAbsMethod_ThenPrintResult(int number, string expectedOutput)
+        [TestCase(60, 1)]
+        [TestCase(-30, -1)]
+        public void GivenNumber_WhenUsingMathAbsMethod_ThenPrintResult(int number, int expectedOutput)
         {
-            // Arrange - Nothing to arrange in this case
-
             // Act
-            using (ConsoleOutputRedirector outputRedirector = new ConsoleOutputRedirector())
-            {
-                Program.IsPositiveOrNegative_MathAbsMethod(number);
-                string actualOutput = outputRedirector.GetOutput();
+            var actualOutput = NumberValidation.IsPositiveOrNegativeUsingMathAbsMethod(number);
 
-                // Assert
-                Assert.AreEqual(expectedOutput, actualOutput.Trim());
-            }
+            // Assert
+            Assert.That(expectedOutput, Is.EqualTo(actualOutput));
         }
 
-        [TestCase(60, "IsPositiveOrNegative_MathSignMethod - 60 is a positive number.")]
-        [TestCase(-30, "IsPositiveOrNegative_MathSignMethod - -30 is a negative number.")]
-        [TestCase(0, "IsPositiveOrNegative_MathSignMethod - The 0 is zero.")]
-        public void GivenNumber_WhenUsingMathSign_ThenPrintResult(int number, string expectedOutput)
-        {
-            // Arrange - Nothing to arrange in this case
-
+        [TestCase(60, 1)]
+        [TestCase(-30, -1)]
+        [TestCase(0, 0)]
+        public void GivenNumber_WhenUsingMathSign_ThenPrintResult(int number, int expectedOutput)
+        {            
             // Act
-            using (ConsoleOutputRedirector outputRedirector = new ConsoleOutputRedirector())
-            {
-                Program.IsPositiveOrNegative_MathSignMethod(number);
-                string actualOutput = outputRedirector.GetOutput();
+            var actualOutput = NumberValidation.IsPositiveOrNegativeUsingMathSignMethod(number);
 
-                // Assert
-                Assert.AreEqual(expectedOutput, actualOutput.Trim());
-            }
+            // Assert
+            Assert.That(expectedOutput, Is.EqualTo(actualOutput));
         }
-        [TestCase(60, "IsPositiveOrNegative - The 60 is positive.")]
-        [TestCase(-30, "IsPositiveOrNegative - The -30 is negative.")]
-        public void GivenNumber_WhenUsingIsPositiveOrNegative_ThenPrintResult(int number, string expectedOutput)
+        [TestCase(60, 1)]
+        [TestCase(-30, -1)]
+        public void GivenNumber_WhenUsingIsPositiveOrNegative_ThenPrintResult(int number, int expectedOutput)
         {
-            // Arrange - Nothing to arrange in this case
-
             // Act
-            using (ConsoleOutputRedirector outputRedirector = new ConsoleOutputRedirector())
-            {
-                Program.IsPositiveOrNegative(number);
-                string actualOutput = outputRedirector.GetOutput();
+            var actualOutput = NumberValidation.IsPositiveOrNegative(number);
 
-                // Assert
-                Assert.AreEqual(expectedOutput, actualOutput.Trim());
-            }
-        }
-    }
-    public class ConsoleOutputRedirector : IDisposable
-    {
-        private readonly StringWriter stringWriter;
-        private readonly TextWriter originalOutput;
-
-        public ConsoleOutputRedirector()
-        {
-            stringWriter = new StringWriter();
-            originalOutput = Console.Out;
-            Console.SetOut(stringWriter);
-        }
-
-        public string GetOutput()
-        {
-            return stringWriter.ToString();
-        }
-
-        public void Dispose()
-        {
-            Console.SetOut(originalOutput);
-            stringWriter.Dispose();
+            // Assert
+            Assert.That(expectedOutput, Is.EqualTo(actualOutput));
         }
     }
 }
