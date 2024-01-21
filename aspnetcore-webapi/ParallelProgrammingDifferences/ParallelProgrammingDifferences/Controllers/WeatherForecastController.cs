288c1febea7d01a0fe8319032ccec4e8e3eacbe3
Aneta Muslić <Aneta.Muslic@blu.com>
2024-01-21 12:50:13 +0100

    added solution code

---
+++ aspnetcore-webapi/ParallelProgrammingDifferences/ParallelProgrammingDifferences/Controllers/WeatherForecastController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+namespace ParallelProgrammingDifferences.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController() : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
+    ];
+
+    [HttpGet(Name = "GetWeatherForecastParallelForEachAsync")]
+    public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastParallelForEachAsync()
+    {
+        Console.WriteLine($"GetWeatherForecast started on thread: {Environment.CurrentManagedThreadId}");
+        ParallelOptions parallelOptions = new()
+        {
+            MaxDegreeOfParallelism = 3
+        };
+
+        ConcurrentBag<IEnumerable<WeatherForecast>> resultBag = [];
+
+        await Parallel.ForEachAsync(Enumerable.Range(0, 3), parallelOptions, async (index, _) =>
+        {
+            var result = await AsyncMethod(); resultBag.Add(result);
+        });
+
+        Console.WriteLine($"GetWeatherForecast completed on thread: {Environment.CurrentManagedThreadId}");
+        return resultBag.SelectMany(cr => cr);
+    }
+
+    [HttpGet(Name = "GetWeatherForecastWhenAll")]
+    public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastWhenAll()
+    {
+        var tasks = new List<Task<IEnumerable<WeatherForecast>>>();
+
+        var result1 = AsyncMethod();
+        var result2 = AsyncMethod();
+        var result3 = AsyncMethod();
+
+        tasks.Add(result1);
+        tasks.Add(result2);
+        tasks.Add(result3);
+
+        var combinedResults = await Task.WhenAll(tasks);
+        var result = combinedResults.SelectMany(cr => cr);
+        return result;
+    }
+
+
+    private static async Task<IEnumerable<WeatherForecast>> AsyncMethod()
+    {
+        Console.WriteLine($"AsyncMethod started on thread: {Environment.CurrentManagedThreadId}");
+        await Task.Delay(1000);
+
+        Console.WriteLine($"AsyncMethod completed on thread: {Environment.CurrentManagedThreadId}");
+        return Enumerable.Range(6, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = new Random().Next(-20, 55),
+            Summary = Summaries[new Random().Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
