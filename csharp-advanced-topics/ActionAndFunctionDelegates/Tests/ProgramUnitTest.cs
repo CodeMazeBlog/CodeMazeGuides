a0c38ff80e82ad5f8ab6e3de515f3fdb4133591d
Ehab Shaker <ehab-shaker319@outlook.com>
2024-01-22 00:28:12 +0300

    Fix Test name And add main project reference to Test project

--- csharp-advanced-topics/ActionAndFunctionDelegates/Tests/ProgramUnitTest.cs
+++ csharp-advanced-topics/ActionAndFunctionDelegates/Tests/ProgramUnitTest.cs
@@ -1,53 +1,48 @@
+using ActionAndFunctionDelegates;
+
 namespace Tests
 {
-    delegate int MathDelegate(int num1, int num2);
-    delegate void TextDelegate(string str);
-
     public class ProgramUnitTest
     {
-        private static int Max(int var1, int var2) { return var1 > var2 ? var1 : var2; }
-        private static void Print(string str) { Console.WriteLine(str); }
-        private static int DelegateCaller(int var1, int var2, MathDelegate delegate1) { return delegate1(var1, var2); }
-
         [Fact]
-        public void WhenIntegersIsSentToDelegate_DelegateReturnMax()
+        public void WhenMaxMethodAssignedToMathDelegateInstance_ThenInstanceReturnTheMaxOfItsParameters()
         {
-            var delegate1 = new MathDelegate(Max);
+            var delegate1 = new MathDelegate(Program.Max);
             var result = delegate1(2, 6);
 
             Assert.Equal(6, result);
         }
 
         [Fact]
-        public void WhenAssignPrintMethodToDelegate_JustPrintMethodWillInvoked()
+        public void WhenPrintMethodAssignedToTextDelegateInstance_ThenInstanceWillInvokJustPrintMethod()
         {
-            var delegate1 = new TextDelegate(Print);
+            var delegate1 = new TextDelegate(Program.Print);
             var invocationList = delegate1.GetInvocationList();
 
             Assert.Single(invocationList);
             Assert.Equal("Print", invocationList[0].Method.Name);
         }
         [Fact]
-        public void WhenMathDelegateIsSentToDelegateCaller_MathDelegateIsInvoked()
+        public void WhenWrappedMaxFunctionSentToDelegateCaller_ThenDelegateCallerReturnMaxOfItsIntegerParameters()
         {
-            var delegate1 = new MathDelegate(Max);
-            var result = DelegateCaller(0, 1, delegate1);
+            var delegate1 = new MathDelegate(Program.Max);
+            var result = Program.DelegateCaller(0, 1, delegate1);
 
             Assert.Equal(1, result);
         }
         [Fact]
-        public void WhenMaxMethodAssignedToFunc_FuncReturnTheMax()
+        public void WhenMaxMethodAssignedToFunc_ThenFuncReturnTheMaxOfItsParameters()
         {
-            Func<int, int, int> MaxFunc = Max;
+            Func<int, int, int> MaxFunc = Program.Max;
             var result = MaxFunc(-20, 10);
 
             Assert.Equal(10, result);
         }
 
         [Fact]
-        public void WhenAssignPrintMethodToActione_JustPrintMethodWillInvoke()
+        public void WhenPrintMethodAssignedToAction_ThenJustPrintMethodInvoked()
         {
-            Action<string> PrintAction = Print;
+            Action<string> PrintAction = Program.Print;
             var invocationList = PrintAction.GetInvocationList();
 
             Assert.Single(invocationList);
