455530153402bf4506b41bf44bd117fa154e73ea
Daniel LAURENS <dlaur94@gmail.com>
2024-01-22 16:51:50 +0100

    Program and tests

---
+++ csharp-basic-topics/CastObjectToNumber/ObjectToNumber/ConsoleApp1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static bool CheckIfIntegerWithEqualityOperator(object value)
+        {
+            var isInteger = value.GetType() == typeof(int);
+            return isInteger;
+        }
+
+        public static float CheckIfFloatWithExplicitCasting(object value)
+        {
+            float _float = (float)value;
+            return _float;
+        }
+
+        public static short CheckIfShortUsingConvert(object value)
+        {
+            short _short = Convert.ToInt16(value);
+            return _short;
+        }
+
+        public static bool CheckIfFloatWithIsOperator(object value)
+        {
+            if(value is float)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static int ConvertToIntWithAsOperator(object value)
+        {
+            var amount =  value as int?;
+            if (amount == null) return 0;
+            return amount.Value;
+        }
+
+        public static double CalculateAllTaxesIncludedPrice(object tax)
+        {
+            double price = 28;
+            if (tax is double vat)
+            {
+                price = price + (price * vat)/100;
+            }
+            return price;
+        }
+
+        static void Main(string[] args)
+        {
+            var boolResult = CheckIfIntegerWithEqualityOperator(123);    // Returns True
+            Console.WriteLine(boolResult);
+            boolResult = CheckIfIntegerWithEqualityOperator(456d);       // Returns False
+            Console.WriteLine(boolResult);
+            boolResult = CheckIfIntegerWithEqualityOperator(0);          // Returns True
+            Console.WriteLine(boolResult);
+
+            var intResult = CheckIfFloatWithExplicitCasting((long)5);  // System.InvalidCastException
+            intResult = CheckIfFloatWithExplicitCasting(5.0d);         // System.InvalidCastException
+            intResult = CheckIfFloatWithExplicitCasting(-2.5f);
+            Console.WriteLine(intResult);                // Returns -2.5
+
+            var convertResult = CheckIfShortUsingConvert((long)5);
+            Console.WriteLine(convertResult);       // Returns 5 
+            convertResult = CheckIfShortUsingConvert(19);
+            Console.WriteLine(convertResult);       // Returns 19
+            convertResult = CheckIfShortUsingConvert(12.54f);
+            Console.WriteLine(convertResult);       // Returns 13
+            convertResult = CheckIfShortUsingConvert(Decimal.MaxValue);
+            Console.WriteLine(convertResult);       // System.OverflowException  
+
+            var isFloat = CheckIfFloatWithIsOperator(6.99f);
+            Console.WriteLine(isFloat);     // Returns True
+            isFloat = CheckIfFloatWithIsOperator(Math.PI);
+            Console.WriteLine(isFloat);     // Returns False
+
+            var intValue = ConvertToIntWithAsOperator(19);
+            Console.WriteLine(intValue);     // Returns 19
+            intValue = ConvertToIntWithAsOperator(.25);
+            Console.WriteLine(intValue);     // Returns 0
+
+            object value = "123";
+            var isNumber = value.IsNumber();
+            Console.WriteLine(isNumber);    // Returns False
+            value = 123;
+            isNumber = value.IsNumber();
+            Console.WriteLine(isNumber);    // Returns True
+
+            var price = CalculateAllTaxesIncludedPrice(10.0);
+            Console.WriteLine(price);       // Returns 30.8
+            price = CalculateAllTaxesIncludedPrice(20);
+            Console.WriteLine(price);       // Returns 28
+        }
+    }
+}
+
+public static class Extensions
+{
+    public static bool IsNumber(this object value)
+    {
+        return value is sbyte
+                || value is byte
+                || value is short
+                || value is ushort
+                || value is int
+                || value is uint
+                || value is long
+                || value is ulong
+                || value is nint
+                || value is nuint
+                || value is float
+                || value is double
+                || value is decimal;
+    }
+}
---
+++ csharp-basic-topics/CastObjectToNumber/ObjectToNumber/Tests/UnitTest1.cs
@@ -0,0 +1,120 @@
+using ConsoleApp1;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GivenMultipleTypes_WhenCheckedWithEqualityOperator_ThenReturnsBoolean()
+        {
+            // Act
+            var checkIntIsTrue = Program.CheckIfIntegerWithEqualityOperator(-15);
+            var checkFloatIsFalse = Program.CheckIfIntegerWithEqualityOperator(2.54f);
+            var checkStringIsFalse = Program.CheckIfIntegerWithEqualityOperator("123");
+
+            // Assert
+            Assert.True(checkIntIsTrue);
+            Assert.False(checkFloatIsFalse);
+            Assert.False(checkStringIsFalse);
+        }
+
+        [Fact]
+        public void GivenMultipleTypes_WhenCheckedWithExplicitCasting_ThenReturnsFloat()
+        {
+            // Arrange
+            var integerValue = -15;
+            var floatValue = 2.54f;
+            var doubleValue = -4.7513247659008d;
+            var stringValue = "123";
+
+            // Act
+            var resultFromFloat = Program.CheckIfFloatWithExplicitCasting(floatValue);
+
+            // Assert
+            Assert.Equal(floatValue, resultFromFloat);
+            Assert.Throws<InvalidCastException>(() => Program.CheckIfFloatWithExplicitCasting(integerValue));
+            Assert.Throws<InvalidCastException>(() => Program.CheckIfFloatWithExplicitCasting(doubleValue));
+            Assert.Throws<InvalidCastException>(() => Program.CheckIfFloatWithExplicitCasting(stringValue));
+        }
+
+        [Fact]
+        public void GivenMultipleTypes_WhenCheckedWithConvert_ThenReturnsShort()
+        {
+            // Arrange
+            var negativeIntegerValue = -5;
+            var integerValue = 0;
+            var floatValue = -4.75f;
+            var stringValue = "123";
+            var doubleValue = double.MaxValue;
+            var decimalValue = decimal.MaxValue;
+
+            // Act
+            var resultFromNegativeInteger = Program.CheckIfShortUsingConvert(negativeIntegerValue);
+            var resultFromInteger = Program.CheckIfShortUsingConvert(integerValue);
+            var resultFromFloat = Program.CheckIfShortUsingConvert(floatValue);
+            var resultFromString = Program.CheckIfShortUsingConvert(stringValue);
+
+            // Assert
+            Assert.Equal(negativeIntegerValue, resultFromNegativeInteger);
+            Assert.Equal(integerValue, resultFromInteger);
+            Assert.Equal(-5, resultFromFloat);
+            Assert.Equal(123, resultFromString);
+            Assert.Throws<OverflowException>(() => Program.CheckIfShortUsingConvert(doubleValue));
+            Assert.Throws<OverflowException>(() => Program.CheckIfShortUsingConvert(decimalValue));
+        }
+
+        [Fact]
+        public void GivenMultipleTypes_WhenComparedWithIsOperator_ThenReturnsBoolean()
+        {
+            // Arrange
+            var integerValue = -15;
+            var floatValue = 2.54f;
+            var doubleValue = -4.24708d;
+            var stringValue = "123";
+
+            // Act & Assert
+            Assert.True(Program.CheckIfFloatWithIsOperator(floatValue));
+            Assert.False(Program.CheckIfFloatWithIsOperator(integerValue));
+            Assert.False(Program.CheckIfFloatWithIsOperator(doubleValue));
+            Assert.False(Program.CheckIfFloatWithIsOperator(stringValue));
+        }
+
+        [Fact]
+        public void GivenMultipleTypes_WhenConvertToIntegerWithAsOperator_ThenReturnsInteger()
+        {
+            // Arrange
+            var integerValue = -15;
+            var floatValue = 2.54f;
+            var doubleValue = -4.24708d;
+            var stringValue = "123";
+            var charactersValue = "test";
+
+            // Act & Assert
+            Assert.Equal(integerValue, Program.ConvertToIntWithAsOperator(integerValue));
+            Assert.Equal(0, Program.ConvertToIntWithAsOperator(floatValue));
+            Assert.Equal(0, Program.ConvertToIntWithAsOperator(doubleValue));
+            Assert.Equal(0, Program.ConvertToIntWithAsOperator(stringValue));
+            Assert.Equal(0, Program.ConvertToIntWithAsOperator(charactersValue));
+        }
+
+        [Fact]
+        public void GivenMultipleTypes_WhenCheckIfNumberWithExtensionMethod_ThenReturnsBoolean()
+        {
+            // Arrange
+            var shortValue = -15;
+            var floatValue = 2.54f;
+            var doubleValue = -4.24708d;
+            var stringValue = "123";
+            var charactersValue = "test";
+            var listIntegers = new List<int> { 0, 9, 8 };
+
+            // Act & Assert
+            Assert.True(shortValue.IsNumber());
+            Assert.True(floatValue.IsNumber());
+            Assert.True(doubleValue.IsNumber());
+            Assert.False(stringValue.IsNumber());
+            Assert.False(charactersValue.IsNumber());
+            Assert.False(listIntegers.IsNumber());
+        }
+    }
+}
