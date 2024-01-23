212bdbe98965c6fafc066c98245fdb39892dfae4
bozo-spoljaric <40840193+bozo-spoljaric@users.noreply.github.com>
2024-01-23 01:49:26 +0100

    CM-913: How to Select Multiple Records Based on a List of IDs Using LINQ (#1667)
    * first version
    
    * test names, spaces
    
    * folder change

---
+++ csharp-linq/SelectMultipleRecordsBasedOnListUsingLinqCSharp/SelectMultipleRecordsUsingLinqCSharp/CompanyDbContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace SelectMultipleRecordsUsingLinqCSharp;
+
+public static class CompanyDbContextExtensions
+{
+    public static void AddSeedData(this CompanyDbContext dbContext, int recordCount)
+    {
+        for (var cnt = 1; cnt <= recordCount; cnt++)
+        {
+            var employee = new Employee
+            {
+                Id = cnt,
+                Name = $"Name {cnt}",
+            };
+
+            dbContext.Employees.Add(employee);
+        }
+
+        dbContext.SaveChanges();
+    }
+}
