bc496e35e2972f807bc9cc2565b048593e5fe638
Ivan Gechev <ivan_banov@abv.bg>
2024-01-24 21:44:52 +0200

    CM-960: Different Ways to Run Background Tasks in .NET Core (#1685)
    * Initial commit
    
    * Core of the article is done
    
    * removed obsolete class
    
    * RunningBackgroundTasks.Tests
    
    * Shifted to .NET Aspire
    
    * One_off tests
    
    * More tests
    
    * https
    
    * post-review changes
    
    * fixed GlobalUsings
    
    * Implementing changes requested by Jeff
    
    * Aspire >InMemory
    
    * fixed failing tests

---
+++ aspnetcore-features/RunningBackgroundTasks/RunningBackgroundTasks/RunningBackgroundTasks/Services/Abstractions/IPeriodicTimer.cs
@@ -0,0 +1,6 @@
+﻿namespace RunningBackgroundTasks.Services.Abstractions;
+
+public interface IPeriodicTimer : IDisposable
+{
+    ValueTask<bool> WaitForNextTickAsync(CancellationToken cancellationToken = default);
+}
---
+++ aspnetcore-features/RunningBackgroundTasks/RunningBackgroundTasks/RunningBackgroundTasks/Services/Periodic/PeriodicHostedLifecycleService.cs
@@ -0,0 +1,39 @@
+﻿using RunningBackgroundTasks.Data;
+using RunningBackgroundTasks.Services.Abstractions;
+
+namespace RunningBackgroundTasks.Services.Periodic;
+
+public class PeriodicHostedLifecycleService(
+    IWorker worker,
+    IPeriodicTimer timer,
+    IServiceProvider serviceProvider) : IHostedLifecycleService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+
+    public async Task StartedAsync(CancellationToken cancellationToken)
+    {
+        using IServiceScope scope = serviceProvider.CreateScope();
+
+        await using var context = scope.ServiceProvider
+            .GetRequiredService<ApplicationDbContext>();
+
+        while (!cancellationToken.IsCancellationRequested &&
+            await timer.WaitForNextTickAsync(cancellationToken))
+        {
+            await worker.ArchiveOldClientsAsync(context, cancellationToken);
+        }
+    }
+
+    public Task StartingAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+
+    public Task StopAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+
+    public Task StoppedAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+
+    public Task StoppingAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+}
