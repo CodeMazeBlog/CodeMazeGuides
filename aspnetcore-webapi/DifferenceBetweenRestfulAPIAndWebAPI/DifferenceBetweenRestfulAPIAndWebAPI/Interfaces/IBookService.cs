39c9eb4f07a4d271350cf1a83eef3e93a4dc29c4
cjaynjoku <njokujennaro@gmail.com>
2024-01-25 13:57:14 +0100

    CM-968:Difference Between WebAPI and REST API in .NET

---
+++ aspnetcore-webapi/DifferenceBetweenRestfulAPIAndWebAPI/DifferenceBetweenRestfulAPIAndWebAPI/Interfaces/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using DifferenceBetweenRestfulAPIAndWebAPI.Models;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace DifferenceBetweenRestfulAPIAndWebAPI.Interfaces
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooks();
+    }
+}
