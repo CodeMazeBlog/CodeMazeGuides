24c1571516e0b58316f0b1af3578a32ad785fa47
Ioannis Leousis <Ioannis.Leousis@relationalfs.com>
2024-01-26 12:48:08 +0200

    Coding Style

--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -1,29 +1,29 @@
 ﻿namespace ActionAndFuncDelegatesInCsharp
 {
-	public class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Here is an example with Action delegates");
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here is an example with Action delegates");
 
-			Action<int, int> additionAction = AddActionNumbers;
-			additionAction(4, 8);
+            Action<int, int> additionAction = AddActionNumbers;
+            additionAction(4, 8);
 
-			Console.WriteLine("\nHere is an example with Func delegates");
+            Console.WriteLine("\nHere is an example with Func delegates");
 
-			Func<int, int, int> additionFunc = AddFuncNumbers;
-			var sum = additionFunc(4, 8);
-			Console.WriteLine("The addition's sum: " + sum);
-		}
+            Func<int, int, int> additionFunc = AddFuncNumbers;
+            var sum = additionFunc(4, 8);
+            Console.WriteLine("The addition's sum: " + sum);
+        }
 
-		public static void AddActionNumbers(int a, int b)
-		{
-			Console.WriteLine($"The addition's sum: {a + b}");
-		}
+        public static void AddActionNumbers(int a, int b)
+        {
+            Console.WriteLine($"The addition's sum: {a + b}");
+        }
 
-		public static int AddFuncNumbers(int i, int j)
-		{
-			return i + j;
-		}
-	}
+        public static int AddFuncNumbers(int i, int j)
+        {
+            return i + j;
+        }
+    }
 }
--- csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/Tests/ActionAndFuncUnitTest.cs
+++ csharp-advanced-topics/ActionAndFuncDelegatesInCsharp/Tests/ActionAndFuncUnitTest.cs
@@ -2,32 +2,32 @@ using ActionAndFuncDelegatesInCsharp;
 
 namespace Tests
 {
-	public class ActionAndFuncUnitTest
-	{
-		[Fact]
-		public void WhenActionDelegateIsCalled_ThenAddActionNumbersMethodIsInvoked()
-		{
-			var i = 4;
-			var j = 8;
-			var stringWriter = new StringWriter();
-			var expectedOutput = $"The addition's sum: {i + j}";
+    public class ActionAndFuncUnitTest
+    {
+        [Fact]
+        public void WhenActionDelegateIsCalled_ThenAddActionNumbersMethodIsInvoked()
+        {
+            var i = 4;
+            var j = 8;
+            var stringWriter = new StringWriter();
+            var expectedOutput = $"The addition's sum: {i + j}";
 
-			Console.SetOut(stringWriter);
-			Program.AddActionNumbers(i, j);
-			var actualOutput = stringWriter.ToString().Trim();
+            Console.SetOut(stringWriter);
+            Program.AddActionNumbers(i, j);
+            var actualOutput = stringWriter.ToString().Trim();
 
-			Assert.Equal(expectedOutput, actualOutput);
-		}
+            Assert.Equal(expectedOutput, actualOutput);
+        }
 
-		[Fact]
-		public void WhenFuncDelegateIsCalled_ThenAddFuncNumbersMethodIsInvoked()
-		{
-			var i = 4;
-			var j = 8;
-			var expectedSum = i + j;
-			var actualSum = Program.AddFuncNumbers(i, j);
+        [Fact]
+        public void WhenFuncDelegateIsCalled_ThenAddFuncNumbersMethodIsInvoked()
+        {
+            var i = 4;
+            var j = 8;
+            var expectedSum = i + j;
+            var actualSum = Program.AddFuncNumbers(i, j);
 
-			Assert.Equal(expectedSum, actualSum);
-		}
-	}
+            Assert.Equal(expectedSum, actualSum);
+        }
+    }
 }
