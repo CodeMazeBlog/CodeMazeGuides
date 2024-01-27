543af7a334e8953d00610ffc240e5e7d78a96e1a
Martin Chege <martinchege324@gmail.com>
2024-01-27 11:53:00 +0300

    First article and draft

---
+++ csharp-basic-topics/ConvertHexadecimalToDecimal/ConvertHexaDecimalToDecimalTests/ConversionExamplesUnitTests.cs
@@ -0,0 +1,61 @@
+using ConvertHexadecimalToDecimal;
+
+namespace ConvertHexaDecimalToDecimalTests;
+
+[TestClass]
+public class ConversionExamplesUnitTests
+{
+    private const string hexVal = "7FFFFFFF";
+    private const int decimalVal = int.MaxValue;
+    private readonly ConversionExamples _conversionExamples = new();
+    
+    [TestMethod]
+    public void GivenADecimalValue_WhenDecimalToHexUsingToStringInvoked_VerifyAccurateConversion()
+    {
+        var result = _conversionExamples.DecimalToHexUsingToString(decimalVal);
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(string));
+        Assert.AreEqual(hexVal, result);
+    }
+
+    [TestMethod]
+    public void GivenADecimalValue_WhenDecimalToHexUsingStringFormatInvoked_VerifyAccurateConversion()
+    {
+        var result = _conversionExamples.DecimalToHexUsingStringFormat(decimalVal);
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(string));
+        Assert.AreEqual(hexVal, result);
+    }
+
+    [TestMethod]
+    public void GivenADecimalValue_WhenDecimalToHexUsingBitwiseMethodInvoked_VerifyAccurateConversion()
+    {
+        var result = _conversionExamples.DecimalToHexUsingBitwiseMethod(decimalVal);
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(string));
+        Assert.AreEqual(hexVal, result);
+    }
+
+    [TestMethod]
+    public void GivenAHexadecimalValue_WhenHexToDecimalUsingParseInvoked_VerifyAccurateConversion()
+    {
+        var result = _conversionExamples.HexToDecimalUsingParse(hexVal);
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(int));
+        Assert.AreEqual(decimalVal, result);
+    }
+
+    [TestMethod]
+    public void GivenAHexadecimalValue_WhenHexToDecimalUsingConvertInvoked_VerifyAccurateConversion()
+    {
+        var result = _conversionExamples.HexToDecimalUsingConvert(hexVal);
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(int));
+        Assert.AreEqual(decimalVal, result);
+    }
+}
---
+++ csharp-basic-topics/ConvertHexadecimalToDecimal/ConvertHexadecimalToDecimal/ConversionExamples.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Order;
+
+namespace ConvertHexadecimalToDecimal;
+
+[MemoryDiagnoser]
+[RankColumn]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByParams)]
+public class ConversionExamples
+{
+    private const string hexVal = "7FFFFFFF";
+    private const int decimalVal = int.MaxValue;
+
+    [Benchmark]
+    [Arguments(decimalVal)]
+    public string DecimalToHexUsingToString(int decimalVal)
+    {
+        return decimalVal.ToString("X");
+    }
+
+    [Benchmark]
+    [Arguments(decimalVal)]
+    public string DecimalToHexUsingStringFormat(int decimalVal)
+    {
+        return string.Format("{0:X}", decimalVal);
+    }
+
+    [Benchmark]
+    [Arguments(decimalVal)]
+    public string DecimalToHexUsingBitwiseMethod(int decimalVal)
+    {
+        var hexVal = string.Empty;
+
+        while (decimalVal > 0)
+        {
+            var remainder = decimalVal % 16;
+            var hexDigit = remainder < 10 ? (char)(remainder + '0') : (char)(remainder - 10 + 'A');
+            hexVal = hexDigit + hexVal;
+            decimalVal /= 16;
+        }
+
+        return hexVal;
+    }
+
+    [Benchmark]
+    [Arguments(hexVal)]
+    public int HexToDecimalUsingParse(string hexVal)
+    {
+        return int.Parse(hexVal, System.Globalization.NumberStyles.HexNumber);
+    }
+
+    [Benchmark]
+    [Arguments(hexVal)]
+    public int HexToDecimalUsingConvert(string hexVal)
+    {
+        return Convert.ToInt32(hexVal, 16);
+    }
+}
