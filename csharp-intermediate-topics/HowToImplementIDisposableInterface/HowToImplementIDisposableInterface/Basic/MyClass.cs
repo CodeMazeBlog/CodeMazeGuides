720cef668b0f1baa80a8ed3505a290fc1e161ad8
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2024-01-28 13:13:05 +0100

    CM-140: How to (Correctly) Implement IDisposable Interface (#1706)
    * CM-140: How to (Correctly) Implement IDisposable Interface
    
    * Code refactored

---
+++ csharp-intermediate-topics/HowToImplementIDisposableInterface/HowToImplementIDisposableInterface/Basic/MyClass.cs
@@ -0,0 +1,37 @@
+﻿namespace HowToImplementIDisposableInterface.Basic;
+
+public sealed class MyClass : IDisposable
+{
+    private bool _disposed = false;
+
+    private readonly IManagedResource _managedResource;
+
+    public MyClass(IManagedResource managedResource)
+    {
+        _managedResource = managedResource;
+    }
+
+    public void DoSomething()
+    {
+        if (_disposed)
+        {
+            throw new ObjectDisposedException(nameof(MyClass));
+        }
+
+        Console.WriteLine($"Called {nameof(MyClass)}.{nameof(DoSomething)}");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Called {nameof(MyClass)}.{nameof(Dispose)}");
+
+        if (_disposed)
+        {
+            return;
+        }
+
+        _managedResource.Dispose();
+
+        _disposed = true;
+    }
+}
---
+++ csharp-intermediate-topics/HowToImplementIDisposableInterface/Tests/DisposePattern/MyParentClassUnitTest.cs
@@ -0,0 +1,86 @@
+﻿using HowToImplementIDisposableInterface.DisposePattern;
+using NSubstitute;
+
+namespace HowToImplementIDisposableInterface.Tests.DisposePattern;
+
+public class MyParentClassUnitTest
+{
+    [Fact]
+    public void GivenManagedResource_WhenDisposeNotCalled_ThenResourceNotDisposed()
+    {
+        // Arrange
+        var managedResource = Substitute.For<IManagedResource>();
+
+        // Act
+        {
+            var myClass = new MyParentClass(managedResource);
+            myClass.DoSomething();
+        }
+
+        // Assert
+        managedResource.DidNotReceive().Dispose();
+    }
+
+    [Fact]
+    public void GivenManagedResource_WhenDisposeCalledExplicitly_ThenResourceDisposed()
+    {
+        // Arrange
+        var managedResource = Substitute.For<IManagedResource>();
+
+        // Act
+        {
+            var myClass = new MyParentClass(managedResource);
+            myClass.DoSomething();
+            myClass.Dispose();
+        }
+
+        // Assert
+        managedResource.Received().Dispose();
+    }
+
+    [Fact]
+    public void GivenManagedResource_WhenDisposeCalledImplicitly_ThenResourceDisposed()
+    {
+        // Arrange
+        var managedResource = Substitute.For<IManagedResource>();
+
+        // Act
+        {
+            using var myClass = new MyParentClass(managedResource);
+            myClass.DoSomething();
+        }
+
+        // Assert
+        managedResource.Received().Dispose();
+    }
+
+    [Fact]
+    public void GivenManagedResource_WhenDisposeCalled_ThenDisposeCalledExactlyOnce()
+    {
+        // Arrange
+        var managedResource = Substitute.For<IManagedResource>();
+
+        // Act
+        {
+            using var myClass = new MyParentClass(managedResource);
+            myClass.DoSomething();
+        }
+
+        // Assert
+        managedResource.Received(1).Dispose();
+    }
+
+    [Fact]
+    public void GivenDisposedClass_WhenDoSomethingCalled_ThenThrowsObjectDisposedException()
+    {
+        // Arrange
+        var managedResource = Substitute.For<IManagedResource>();
+        var myClass = new MyParentClass(managedResource);
+
+        // Act
+        myClass.Dispose();
+
+        // Assert
+        Assert.Throws<ObjectDisposedException>(() => myClass.DoSomething());
+    }
+}
