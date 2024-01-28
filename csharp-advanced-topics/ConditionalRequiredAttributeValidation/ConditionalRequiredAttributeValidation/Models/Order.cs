4ef3bd77d518b0b2a752eab63e005aeff8abeb97
Ainea Wabwoba <aineah28@gmail.com>
2024-01-28 16:17:38 +0300

    Added ExpressiveAnnotations library

--- csharp-advanced-topics/ConditionalRequiredAttributeValidation/ConditionalRequiredAttributeValidation/Models/Order.cs
+++ csharp-advanced-topics/ConditionalRequiredAttributeValidation/ConditionalRequiredAttributeValidation/Models/Order.cs
@@ -1,4 +1,4 @@
-﻿using ConditionalRequiredAttributeValidation.Validators;
+﻿using ExpressiveAnnotations.Attributes;
 using System.ComponentModel.DataAnnotations;
 
 namespace ConditionalRequiredAttributeValidation.Models;
@@ -7,17 +7,18 @@ public class Order
 {
     public bool IsExpressShipping { get; set; }
 
-    [RequiredIf(nameof(IsExpressShipping), true, ErrorMessage = "Shipping address is required for express shipping.")]
+    [RequiredIf(nameof(IsExpressShipping), ErrorMessage = "Shipping address is required for express shipping.")]
     public string ShippingAddress { get; set; }
 
     [Required(ErrorMessage = "Customer name is required.")]
     public string CustomerName { get; set; }
 
+    [AssertThat("IsExpressShipping == true", ErrorMessage = "Delivery date is required for express shipping.")]
     public DateTime? DeliveryDate { get; set; }
 
-    [RequiredIf(nameof(IsExpressShipping), true, ErrorMessage = "Product ID is required for express shipping.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Please select a valid product.")]
     public int ProductId { get; set; }
 
-    [RequiredIf(nameof(ProductId), 0, ErrorMessage = "Product description is required when product ID is provided.")]
+    [RequiredIf("ProductId > 1", ErrorMessage = "Product description is required when product ID is provided.")]
     public string ProductDescription { get; set; }
 }
