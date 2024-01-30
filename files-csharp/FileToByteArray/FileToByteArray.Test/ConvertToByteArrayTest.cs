7a2c344c5828c9ef32dabcbaf2f807c19ea8ea1b
jshergal <1489638+jshergal@users.noreply.github.com>
2024-01-30 21:29:16 +0200

    Updated code samples for article update

--- files-csharp/FileToByteArray/FileToByteArray.Test/ConvertToByteArrayTest.cs
+++ files-csharp/FileToByteArray/FileToByteArray.Test/ConvertToByteArrayTest.cs
@@ -1,35 +1,248 @@
+using System.Buffers;
+using System.Security.Cryptography;
+using FluentAssertions;
+using FluentAssertions.Collections;
 using Xunit;
 
-namespace FileToByteArray.Test
+namespace FileToByteArray.Test;
+
+public sealed class ConvertToByteArrayTest(TestFilesFixture fixture) : IClassFixture<TestFilesFixture>
 {
-    public class ConvertToByteArrayTest
+    [Fact]
+    public void GivenFile_WhenConvertingUsingReadAllBytes_ThenReturnsCorrectContent()
     {
-        const string TestFilePath = "TestFiles/CodeMazeTest.pdf";
+        var bytes = File.ReadAllBytes(fixture.SmallTestFile);
 
-        [Fact]
-        public void WhenConvertToByteArray_ThenReturnsCorrectByteArray()
+        bytes.Should().BeEquivalentTo(fixture.SmallTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public async void GivenFile_WhenConvertingUsingReadAllBytesAsync_ThenReturnsCorrectContent()
+    {
+        var bytes = await File.ReadAllBytesAsync(fixture.SmallTestFile);
+
+        bytes.Should().BeEquivalentTo(fixture.SmallTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public void GivenGiantFileWithLengthExceedingMaxArray_WhenConvertingUsingReadAllBytes_ThenReturnsCorrectContent()
+    {
+        var action = () => _ = File.ReadAllBytes(fixture.GiantTestFile);
+
+        action.Should().Throw<IOException>();
+    }
+
+    [Fact]
+    public async void
+        GivenGiantFileWithLengthExceedingMaxArray_WhenConvertingUsingReadAllBytesAsync_ThenReturnsCorrectContent()
+    {
+        var func = async () => _ = await File.ReadAllBytesAsync(fixture.GiantTestFile);
+
+        await func.Should().ThrowAsync<IOException>();
+    }
+
+    [Fact]
+    public void GivenFile_WhenConvertUsingMemoryStream_ThenReturnsCorrectContent()
+    {
+        var bytes = ToByteArrayMethods.ConvertUsingMemoryStream(fixture.SmallTestFile);
+
+        bytes.Should().BeEquivalentTo(fixture.SmallTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public async void GivenFile_WhenConvertUsingMemoryStreamAsync_ThenReturnsCorrectContent()
+    {
+        var bytes = await ToByteArrayMethods.ConvertUsingMemoryStreamAsync(fixture.SmallTestFile);
+
+        bytes.Should().BeEquivalentTo(fixture.SmallTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public void GivenLargeFileExceedingBufferSize_WhenConvertUsingMemoryStream_ThenReturnsCorrectContent()
+    {
+        var bytes = ToByteArrayMethods.ConvertUsingMemoryStream(fixture.LargeTestFile);
+
+        bytes.Should().BeEquivalentTo(fixture.LargeTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public async void GivenLargeFileExceedingBufferSize_WhenConvertUsingMemoryStreamAsync_ThenReturnsCorrectContent()
+    {
+        var bytes = await ToByteArrayMethods.ConvertUsingMemoryStreamAsync(fixture.LargeTestFile);
+
+        bytes.Should().BeEquivalentTo(fixture.LargeTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public void
+        GivenGiantFileWithLengthExceedingMaxArray_WhenConvertUsingPooledArray_ThenThrowsArgumentOutOfRangeException()
+    {
+        var action = () =>
         {
-            // Arrange
-            var expected = File.ReadAllBytes(TestFilePath);
+            var (array, _) = ToByteArrayMethods.ConvertToPooledArray(fixture.GiantTestFile);
+            ArrayPool<byte>.Shared.Return(array);
+        };
 
-            // Act
-            var result = Program.ConvertToByteArray(TestFilePath);
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
 
-            // Assert
-            Assert.Equal(expected, result);
+    [Fact]
+    public async void
+        GivenGiantFileWithLengthExceedingMaxArray_WhenConvertUsingMemoryStreamAsync_ThenThrowsArgumentOutOfRangeException()
+    {
+        var func = async () => { _ = await ToByteArrayMethods.ConvertUsingMemoryStreamAsync(fixture.GiantTestFile); };
+
+        await func.Should().ThrowAsync<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void
+        GivenGiantFileWithLengthExceedingMaxArray_WhenConvertUsingPooledWriter_ThenThrowsArgumentOutOfRangeException()
+    {
+        var action = () => { _ = ToByteArrayMethods.ConvertUsingMemoryStream(fixture.GiantTestFile); };
+
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public async void
+        GivenGiantFileWithLengthExceedingMaxArray_WhenConvertUsingPooledWriterAsync_ThenThrowsArgumentOutOfRangeException()
+    {
+        var func = async () => { _ = await ToByteArrayMethods.ConvertUsingPooledWriterAsync(fixture.GiantTestFile); };
+
+        await func.Should().ThrowAsync<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void
+        GivenGiantFileWithLengthExceedingMaxArray_WhenConvertUsingMemoryStream_ThenThrowsArgumentOutOfRangeException()
+    {
+        var action = () => { _ = ToByteArrayMethods.ConvertUsingPooledWriter(fixture.GiantTestFile); };
+
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public async void
+        GivenGiantFileWithLengthExceedingMaxArray_WhenConvertUsingPooledArrayAsync_ThenThrowsArgumentOutOfRangeException()
+    {
+        var func = async () =>
+        {
+            var (array, _) = await ToByteArrayMethods.ConvertToPooledArrayAsync(fixture.GiantTestFile);
+            ArrayPool<byte>.Shared.Return(array);
+        };
+
+        await func.Should().ThrowAsync<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void
+        GivenLargeFileExceedingBufferSize_WhenConvertingUsingPooledArray_ThenReturnsCorrectContent()
+    {
+        byte[]? rentedArray = null;
+        try
+        {
+            (rentedArray, var length) = ToByteArrayMethods.ConvertToPooledArray(fixture.LargeTestFile);
+
+            rentedArray.AsSpan(0, length).Should().Equal(fixture.LargeTestFileExpectedBytes);
         }
-
-        [Fact]
-        public void WhenConvertToByteArrayChunked_ThenReturnsCorrectByteArray()
+        finally
         {
-            // Arrange
-            var expected = File.ReadAllBytes(TestFilePath);
-
-            // Act
-            var result = Program.ConvertToByteArrayChunked(TestFilePath);
-
-            // Assert
-            Assert.Equal(expected, result);
+            if (rentedArray is not null)
+            {
+                ArrayPool<byte>.Shared.Return(rentedArray);
+            }
         }
     }
+
+    [Fact]
+    public async void
+        GivenLargeFileExceedingBufferSize_WhenConvertingUsingPooledArrayAsync_ThenReturnsCorrectContent()
+    {
+        byte[]? rentedArray = null;
+        try
+        {
+            (rentedArray, var length) = await ToByteArrayMethods.ConvertToPooledArrayAsync(fixture.LargeTestFile);
+
+            rentedArray.AsSpan(0, length).Should().Equal(fixture.LargeTestFileExpectedBytes);
+        }
+        finally
+        {
+            if (rentedArray is not null)
+            {
+                ArrayPool<byte>.Shared.Return(rentedArray);
+            }
+        }
+    }
+
+    [Fact]
+    public void
+        GivenLargeFileExceedingBufferSize_WhenConvertingUsingPooledWriter_ThenReturnsCorrectContent()
+    {
+        var bytes = ToByteArrayMethods.ConvertUsingPooledWriter(fixture.LargeTestFile);
+
+        bytes.Should().Equal(fixture.LargeTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public async void
+        GivenLargeFileExceedingBufferSize_WhenConvertingUsingPooledWriterAsync_ThenReturnsCorrectContent()
+    {
+        var bytes = await ToByteArrayMethods.ConvertUsingPooledWriterAsync(fixture.LargeTestFile);
+
+        bytes.Should().Equal(fixture.LargeTestFileExpectedBytes);
+    }
+
+    [Fact]
+    public async void GivenFile_WhenCallingConvertInChunks_ThenReturnsCorrectContent()
+    {
+        using var md5Hasher = MD5.Create();
+        await foreach (var chunk in ToByteArrayMethods.ConvertInChunks(fixture.SmallTestFile,
+            ToByteArrayMethods.DefaultBufferSize))
+        {
+            md5Hasher.TransformBlock(chunk, 0, chunk.Length, null, 0);
+        }
+
+        md5Hasher.TransformFinalBlock([], 0, 0);
+        var hash = md5Hasher.Hash;
+
+        hash.Should().BeEquivalentTo(fixture.SmallTestFileHash);
+    }
+
+    [Fact]
+    public async void GivenLargeFile_WhenCallingConvertInChunks_ThenReturnsCorrectContent()
+    {
+        using var md5Hasher = MD5.Create();
+        await foreach (var chunk in ToByteArrayMethods.ConvertInChunks(fixture.LargeTestFile,
+            ToByteArrayMethods.DefaultBufferSize))
+        {
+            md5Hasher.TransformBlock(chunk, 0, chunk.Length, null, 0);
+        }
+
+        md5Hasher.TransformFinalBlock([], 0, 0);
+        var hash = md5Hasher.Hash;
+
+        hash.Should().BeEquivalentTo(fixture.LargeTestFileHash);
+    }
+
+    [Fact]
+    public async void GivenGiantFile_WhenCallingConvertInChunks_ThenReturnsCorrectContent()
+    {
+        using var md5Hasher = MD5.Create();
+        await foreach (var chunk in ToByteArrayMethods.ConvertInChunks(fixture.GiantTestFile,
+            ToByteArrayMethods.DefaultBufferSize))
+        {
+            md5Hasher.TransformBlock(chunk, 0, chunk.Length, null, 0);
+        }
+
+        md5Hasher.TransformFinalBlock([], 0, 0);
+        var hash = md5Hasher.Hash;
+
+        hash.Should().BeEquivalentTo(fixture.GiantTestFileHash);
+    }
+}
+
+public static class SpanAssertionHelpers
+{
+    public static GenericCollectionAssertions<T> Should<T>(this Span<T> span) => span.ToArray().Should();
 }
---
+++ files-csharp/FileToByteArray/FileToByteArray/ToByteArrayMethods.cs
@@ -0,0 +1,121 @@
+﻿using System.Buffers;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.HighPerformance;
+using CommunityToolkit.HighPerformance.Buffers;
+
+namespace FileToByteArray;
+
+public static class ToByteArrayMethods
+{
+    public const int DefaultBufferSize = 1024 * 4;
+
+    public static byte[] ConvertUsingMemoryStream(string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(fileInfo.Length, Array.MaxLength, "File length");
+
+        using var ms = new MemoryStream(DefaultBufferSize);
+        using var fs = File.OpenRead(filePath);
+
+        fs.CopyTo(ms);
+
+        return ms.ToArray();
+    }
+
+    public static async Task<byte[]> ConvertUsingMemoryStreamAsync(string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(fileInfo.Length, Array.MaxLength, "File length");
+
+        await using var ms = new MemoryStream(DefaultBufferSize);
+        await using var fs = File.OpenRead(filePath);
+
+        await fs.CopyToAsync(ms);
+
+        return ms.ToArray();
+    }
+
+    public static (byte[] rentedBuffer, int length) ConvertToPooledArray(string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(fileInfo.Length, Array.MaxLength, "File length");
+
+        var length = (int) fileInfo.Length;
+        var array = ArrayPool<byte>.Shared.Rent(length);
+        var span = array.AsSpan(0, length);
+
+        using var fs = fileInfo.OpenRead();
+        fs.ReadExactly(span);
+
+        return (array, length);
+    }
+
+    public static async Task<(byte[] rentedArray, int length)> ConvertToPooledArrayAsync(string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(fileInfo.Length, Array.MaxLength, "File length");
+
+        var length = (int) fileInfo.Length;
+        var array = ArrayPool<byte>.Shared.Rent(length);
+        var memory = array.AsMemory(0, length);
+        await using var fs = fileInfo.OpenRead();
+        await fs.ReadExactlyAsync(memory);
+
+        return (array, length);
+    }
+
+    public static byte[] ConvertUsingPooledWriter(string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(fileInfo.Length, Array.MaxLength, "File length");
+
+        using var writer = new ArrayPoolBufferWriter<byte>(DefaultBufferSize);
+        using var stream = writer.AsStream();
+        using var fs = File.OpenRead(filePath);
+
+        fs.CopyTo(stream);
+
+        return writer.WrittenSpan.ToArray();
+    }
+
+    public static async Task<byte[]> ConvertUsingPooledWriterAsync(string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(fileInfo.Length, Array.MaxLength, "File length");
+
+        using var writer = new ArrayPoolBufferWriter<byte>(DefaultBufferSize);
+        await using var stream = writer.AsStream();
+        await using var fs = File.OpenRead(filePath);
+
+        await fs.CopyToAsync(stream);
+
+        return writer.WrittenSpan.ToArray();
+    }
+
+    public static async IAsyncEnumerable<byte[]> ConvertInChunks(string filePath, int chunkSize,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(chunkSize, Array.MaxLength);
+
+        var rentedBuffer = ArrayPool<byte>.Shared.Rent(chunkSize);
+        try
+        {
+            var memory = rentedBuffer.AsMemory(0, chunkSize);
+
+            var fileLength = new FileInfo(filePath).Length;
+            using var mm = MemoryMappedFile.CreateFromFile(filePath);
+            await using var accessor = mm.CreateViewStream(0, fileLength);
+
+            int bytesRead;
+            while ((bytesRead = await accessor.ReadAsync(memory, cancellationToken)) != 0)
+            {
+                yield return memory[..bytesRead].ToArray();
+            }
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(rentedBuffer);
+        }
+    }
+}
