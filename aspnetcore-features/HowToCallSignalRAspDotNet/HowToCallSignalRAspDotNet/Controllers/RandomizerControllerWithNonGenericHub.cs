dc8ca9764c9f03566cbceafb0f796d2bf481474e
king-Alex-d-great <ogubuikealex@gmail.com>
2024-01-31 11:01:07 +0100

    Feat: Add Randomizer Controller with Non generic IHubContext Code Sample

---
+++ aspnetcore-features/HowToCallSignalRAspDotNet/HowToCallSignalRAspDotNet/Controllers/RandomizerControllerWithNonGenericHub.cs
@@ -0,0 +1,34 @@
+﻿using HowToCallSignalRAspDotNet.HubConfig;
+using HowToCallSignalRAspDotNet.TimerFeatures;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HowToCallSignalRAspDotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomizerControllerWithNonGenericHub: ControllerBase
+    {
+        private readonly IHubContext<RandomizerHub> _hub;
+        private readonly TimerManager _timer;
+
+        public RandomizerControllerWithNonGenericHub(IHubContext<RandomizerHub> hub, TimerManager timer)
+        {
+            _hub = hub;
+            _timer = timer;
+        }
+
+        [HttpGet]
+        public ActionResult<int> SendRandomNumber()
+        {
+            var randomValue = new Random().Next(1, 51) * 2;
+
+            if (!_timer.IsTimerStarted)
+                _timer.PrepareTimer(() =>
+                _hub.Clients.All
+                .SendAsync("SendClientRandomEvenNumber", randomValue));
+
+            return Ok(randomValue);
+        }
+    }
+}
