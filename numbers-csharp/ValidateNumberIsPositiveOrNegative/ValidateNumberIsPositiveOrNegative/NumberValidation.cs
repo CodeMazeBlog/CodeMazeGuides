9744775002b0d27d90a5b79325c90ec6169ffa06
jshergal <1489638+jshergal@users.noreply.github.com>
2024-01-31 09:43:15 +0200

    Fixed assertions in tests, cleaned up library code

--- numbers-csharp/ValidateNumberIsPositiveOrNegative/ValidateNumberIsPositiveOrNegative/NumberValidation.cs
+++ numbers-csharp/ValidateNumberIsPositiveOrNegative/ValidateNumberIsPositiveOrNegative/NumberValidation.cs
@@ -1,111 +1,59 @@
 ﻿using System.Numerics;
-using System.Runtime.InteropServices;
 
-namespace ValidateNumberIsPositiveOrNegative
+namespace ValidateNumberIsPositiveOrNegative;
+
+public static class NumberValidation
 {
-    public static class NumberValidation
+    public static int IsPositiveOrNegativeUsingConditionalMethod<T>(T number) where T : ISignedNumber<T>, IComparisonOperators<T, T, bool>
     {
-        public static int IsPositiveOrNegativeUsingConditionalMethod<T>(T number) where T : ISignedNumber<T>, IComparisonOperators<T, T, bool>
-        {
-            var result = 0;
+        if (number == T.Zero)
+            return 0;
 
-            if (number > T.Zero)
-            {
-                result = 1;
-            }
-            else if (number < T.Zero)
-            {
-                result = -1;
-            }
-            
-            return result;
-        }
-        
-        public static unsafe int IsPositiveOrNegativeUsingLeftShiftMethod<T>(T number) where T : unmanaged, IBinaryInteger<T>,ISignedNumber<T>
-        {
-            var result = 0;
+        if (number > T.Zero)
+            return 1;
 
-            if (!number.Equals(default(T)))
-            {
-                int bits = (sizeof(T) * 8) - 1;
+        return -1;
 
-                var checkNegative = ((T.CreateChecked(1) << bits) & number) != T.Zero;
+    }
 
-                if (checkNegative == true)
-                {
-                    result = -1;
-                }
-                else
-                {
-                    result = 1;
-                }
-            }
-            return result;
-        }
+    public static unsafe int IsPositiveOrNegativeUsingLeftShiftMethod<T>(T number) where T : unmanaged, IBinaryInteger<T>,ISignedNumber<T>
+    {
+        if (number == T.Zero)
+            return 0;
 
-        public static unsafe int IsPositiveOrNegativeUsingRightShiftMethod<T>(T number) where T : unmanaged, IBinaryInteger<T>, ISignedNumber<T>
-        {
-            var result = 0;
+        int bits = sizeof(T) * 8 - 1;
 
-            if (!number.Equals(default(T)))
-            {
-                int bits = (sizeof(T) * 8) - 1;
+        return ((T.One << bits) & number) == T.Zero ? 1 : -1;
+    }
 
-                var checkNegative = (number >> bits) != T.Zero;
+    public static unsafe int IsPositiveOrNegativeUsingRightShiftMethod<T>(T number) where T : unmanaged, IBinaryInteger<T>, ISignedNumber<T>
+    {
+        if (number == T.Zero)
+            return 0;
 
-                if (checkNegative == true)
-                {
-                    result = -1;
-                }
-                else
-                {
-                    result = 1;
-                }
-            }
+        int bits = (sizeof(T) * 8) - 1;
 
-            return result;
-        }
+        return number >> bits == T.Zero ? 1 : -1;
+    }
 
-        public static int IsPositiveOrNegativeUsingMathAbsMethod<T>(T number) where T : ISignedNumber<T>
-        {
-            var result = 0;
+    public static int IsPositiveOrNegativeUsingMathAbsMethod<T>(T number) where T : ISignedNumber<T>
+    {
+        if (number == T.Zero)
+            return 0;
 
-            if (number != T.Zero)
-            {
-                if (T.Abs(number) == number)
-                {
-                    result = 1;
-                }
-                else
-                {
-                    result = -1;
-                }
-            }
-            return result;
-        }
+        return T.Abs(number) == number ? 1 : -1;
+    }
 
-        public static int IsPositiveOrNegativeUsingMathSignMethod<T>(T number) where T : INumber<T>
-        {
-            return T.Sign(number);
-        }
+    public static int IsPositiveOrNegativeUsingMathSignMethod<T>(T number) where T : INumber<T>
+    {
+        return T.Sign(number);
+    }
 
-        public static int IsPositiveOrNegativeUsingBuiltInMethod<T>(T number) where T : ISignedNumber<T>
-        {
-            var result = 0;
+    public static int IsPositiveOrNegativeUsingBuiltInMethod<T>(T number) where T : ISignedNumber<T>
+    {
+        if (number == T.Zero)
+            return 0;
 
-            if (!number.Equals(default(T)))
-            {
-                if (T.IsPositive(number))
-                {
-                    result = 1;
-                }
-                else if (T.IsNegative(number))
-                {
-                    result = -1;
-                }
-            }
-
-            return result;
-        }
+        return T.IsPositive(number) ? 1 : -1;
     }
 }
