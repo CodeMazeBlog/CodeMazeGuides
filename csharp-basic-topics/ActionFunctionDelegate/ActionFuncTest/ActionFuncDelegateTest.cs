b9cc1a6d1466852c5dca1a051d708b50af6b2891
rechidesigns <alerechiordu@gmail.com>
2024-02-01 23:34:39 +0100

    action and func delegates

---
+++ csharp-basic-topics/ActionFunctionDelegate/ActionFuncTest/ActionFuncDelegateTest.cs
@@ -0,0 +1,21 @@
+﻿using ActionFunctionDelegate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ActionFuncTest
+{
+    [TestClass]
+    public class ActionFuncDelegateTest
+    {
+        [TestMethod]
+        public void GivenTwoNumbers_WhenRunDelegateInvoked_ReturnCorrectResults()
+        {
+            var delegates = new ActionFuncDelegate();
+
+            var result = delegates.RunDelegate(5, 10);
+
+            Assert.AreEqual(15, result.IncrementedNumber, "IncrementedNumber should be 15.");
+            Assert.AreEqual(15, result.AddResult, "AddResult should be 15.");
+
+        }
+    }
+}
---
+++ csharp-basic-topics/ActionFunctionDelegate/ActionFunctionDelegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using ActionFunctionDelegate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var num1 = 5;
+        var num2 = 10;
+
+        var delegates = new ActionFuncDelegate();
+        var results = delegates.RunDelegate(num1, num2);
+
+        Console.WriteLine($"Result of add: {results.AddResult}");
+        Console.WriteLine($"Incremented number: {results.IncrementedNumber}");
+    }
+}
