04248a1c1848f962d8e4ae18298f5c295c6104f4
Amr El Ramsisy <amrelramsisy@gmail.com>
2024-02-02 18:55:40 +0200

    add solution

---
+++ action_func/action_func/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Action_Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int, int> sumFunc = (a, b) => a + b; int result = sumFunc(3, 4); // result = 7
+
+            Action<int, int> ActionCalculator = (a, b) =>
+            {
+                Console.WriteLine($"Addition result: {a + b}");
+                Console.WriteLine($"Subtraction result: {a - b}");
+                Console.WriteLine($"Multiplication result: {a * b}");
+                Console.WriteLine($"Division result: {a / b}");
+            };
+
+            ActionCalculator(9, 3);
+
+            Console.ReadLine();
+        }
+    }
+}
