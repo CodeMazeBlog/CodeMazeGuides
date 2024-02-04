5cfae51e77968f945e848fee964b982b0e27fa79
Eduardo Lagunas <eduardolagunas@outlook.com>
2024-02-04 19:35:46 +0100

    initial solution template

---
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCSharp/ActionAndFuncDelegatesInCSharpTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ActionAndFuncDelegatesInCSharpTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
