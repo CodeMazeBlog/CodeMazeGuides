edc71e9f6aef1150f5acb5f0b01d3ccfd04b7d3b
Igor Nunes Gervásio <igorngervasio@gmail.com>
2024-02-05 01:52:04 -0300

    CM-996: Project commit

---
+++ strings-csharp/CheckIfAStringContainsOnlyLetters/CheckIfAStringContainsOnlyLetters/NativeStringChecker.cs
@@ -0,0 +1,32 @@
+﻿namespace CheckIfAStringContainsOnlyLetters;
+
+public static class NativeStringChecker
+{
+    public static bool IsOnlyAsciiLettersByPatternMatching(string text)
+    {
+        foreach (var item in text)
+        {
+            if (item is >= 'A' and <= 'Z' or >= 'a' and <= 'z')
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+
+    public static bool IsOnlyAsciiLettersBySwitchCase(string text)
+    {
+        foreach (var item in text)
+        {
+            switch (item)
+            {
+                case >= 'A' and <= 'Z':
+                case >= 'a' and <= 'z':
+                    continue;
+                default:
+                    return false;
+            }
+        }
+        return true;
+    }
+}
