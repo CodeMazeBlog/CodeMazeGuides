0d62879aca742c4d0b677f22115d47049528ca82
cjaynjoku <njokujennaro@gmail.com>
2024-02-05 19:31:49 +0100

    CM-881: Fastest Method to Check if a Date is Less Than or Equal to Today's Date in C#

---
+++ dotnet-datetime/CheckIfDateIsLessThanOrEqualToToday/CheckIfDateIsLessThanOrEqualToToday/CheckDateMethods.cs
@@ -0,0 +1,5 @@
+﻿namespace CheckIfDateIsLessThanOrEqualToToday;
+
+public class CheckDateMethods
+{
+}
---
+++ dotnet-datetime/CheckIfDateIsLessThanOrEqualToToday/Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
