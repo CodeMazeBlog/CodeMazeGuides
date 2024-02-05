de089a5fe22f991f57248ffc8fdfcc678c996721
Ivan Gechev <ivan_banov@abv.bg>
2024-02-05 09:21:22 +0200

    if to switch

--- dotnet-testing/OverwriteDateTimeDuringTesting/OverwriteDateTimeDuringTesting/FlightNotificationService.cs
+++ dotnet-testing/OverwriteDateTimeDuringTesting/OverwriteDateTimeDuringTesting/FlightNotificationService.cs
@@ -11,25 +11,17 @@ public class FlightNotificationService(TimeProvider timeProvider) : IFlightNotif
         var now = timeProvider.GetUtcNow().ToLocalTime();
         var timeToDeparture = (flight.Time - now).TotalHours;
 
-        if (timeToDeparture >= 2)
+        return timeToDeparture switch
         {
-            return string.Format(Messages.FlightDepartsInMoreThanTwoHours, flight.From, flight.To);
-        }
-        else if (timeToDeparture <= 2 && timeToDeparture > 1)
-        {
-            return string.Format(Messages.FlightDepartsInLessThanTwoHours, flight.From, flight.To);
-        }
-        else if (timeToDeparture <= 1 && timeToDeparture > 0.5)
-        {
-            return string.Format(Messages.FlightDepartsInLessThanOneHour, flight.From, flight.To);
-        }
-        else if (timeToDeparture <= 0.5 && timeToDeparture > 0)
-        {
-            return string.Format(Messages.FlightDepartsInLessThanThirtyMinutes, flight.From, flight.To);
-        }
-        else
-        {
-            return string.Format(Messages.FlightAlreadyDeparted, flight.From, flight.To);
-        }
+            var x when x >= 2 
+                => string.Format(Messages.FlightDepartsInMoreThanTwoHours, flight.From, flight.To),
+            var x when x <= 2 && x > 1 
+                => string.Format(Messages.FlightDepartsInLessThanTwoHours, flight.From, flight.To),
+            var x when x <= 1 && x > 0.5 
+                => string.Format(Messages.FlightDepartsInLessThanOneHour, flight.From, flight.To),
+            var x when x <= 0.5 && x > 0 
+                => string.Format(Messages.FlightDepartsInLessThanThirtyMinutes, flight.From, flight.To),
+            _ => string.Format(Messages.FlightAlreadyDeparted, flight.From, flight.To)
+        };
     }
 }
