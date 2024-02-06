c01961dfc562031b7bbe6844b92c1bc1629a0892
mtayel <moh.moh701@gmail.com>
2024-02-06 23:26:13 +0200

    change in func

--- csharp-advanced-topics/FuncActionDelegate/FuncActionDelegate/Program.cs
+++ csharp-advanced-topics/FuncActionDelegate/FuncActionDelegate/Program.cs
@@ -9,7 +9,7 @@ namespace FuncActionDelegate
             //Func Demo
             var funcSample = new FuncSample();
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            Console.WriteLine($"Sum of even numbers:{funcSample.FilterAndSum(numbers)}");
+            Console.WriteLine($"Sum of even numbers:{ funcSample.FilterAndSum(numbers)}");
 
 
             //Action Demo
