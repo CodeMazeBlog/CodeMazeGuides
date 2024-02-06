10a5b3cb8057b246b613722fa10451d6c7bd636d
Victorio <vikinik01@abv.bg>
2024-02-06 14:45:41 +0200

    Adding a `URL Validator` class containing different validation methods, moving examples to `Program.cs`, simplifying `Unit Tests`.

---
+++ csharp-basic-topics/HowToCheckIfURLIsValid/HowToCheckIfURLIsValid/UrlValidator.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace HowToCheckIfURLIsValid;
+
+public static class UrlValidator
+{
+    public static bool ValidateUrlWithRegex(string url)
+    {
+        var urlRegex = new Regex(
+            @"^(https?|ftp):\/\/(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}(?::\d+)?(?:\/(?:[-a-zA-Z0-9@:%_\+.~#?&=]+\/?)*)?$",
+            RegexOptions.IgnoreCase);
+        return urlRegex.IsMatch(url);
+    }
+
+    public static bool ValidateUrlWithUriCreate(string url, out Uri? uri)
+    {
+        var success = Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out uri);
+        return success;
+    }
+
+    public static bool ValidateUrlWithUriWellFormedString(string url)
+    {
+        var success = Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute);
+        return success;
+    }
+
+
+    public static async Task<bool> ValidateUrlWithHttpClient(string url)
+    {
+        using var client = new HttpClient();
+        try
+        {
+            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+            return response.IsSuccessStatusCode;
+        }
+        catch (HttpRequestException e) when (e.InnerException is SocketException { SocketErrorCode: SocketError.HostNotFound })
+        {
+            return false;
+        }
+    }
+}
--- csharp-basic-topics/HowToCheckIfURLIsValid/Tests/HowToCheckIfURLIsValidUnitTests.cs
+++ csharp-basic-topics/HowToCheckIfURLIsValid/Tests/HowToCheckIfURLIsValidUnitTests.cs
@@ -1,5 +1,4 @@
-using System.Net.Sockets;
-using System.Text.RegularExpressions;
+using HowToCheckIfURLIsValid;
 
 namespace Tests;
 
@@ -10,13 +9,9 @@ public class HowToCheckIfURLIsValidUnitTests
     {
         var url = "https://www.amazon.com";
         var urlTwo = "https://api.example.org/v1/data";
-        
-        var urlRegex = new Regex(
-            @"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
-            RegexOptions.IgnoreCase);
 
-        Assert.Matches(urlRegex, url);
-        Assert.Matches(urlRegex, urlTwo);
+        Assert.True(UrlValidator.ValidateUrlWithRegex(url));
+        Assert.True(UrlValidator.ValidateUrlWithRegex(urlTwo));
     }
 
     [Fact]
@@ -25,17 +20,12 @@ public class HowToCheckIfURLIsValidUnitTests
         List<string> urls =
         [
             "http:/www.amazon.com",
-            "https://site.company",
+            "https://site.company///",
             "xxx://org.company.com"
         ];
-
-        var urlRegex = new Regex(
-            "^(http(s)?:\\/\\/.)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$",
-            RegexOptions.IgnoreCase);
-
         foreach (var url in urls)
         {
-            Assert.DoesNotMatch(urlRegex, url);
+            Assert.False(UrlValidator.ValidateUrlWithRegex(url));
         }
     }
 
@@ -43,36 +33,14 @@ public class HowToCheckIfURLIsValidUnitTests
     public void GivenACorrectUrl_WhenValidatedWithUriTryCreate_ThenItShouldBeValid()
     {
         var url = "https://api.facebook.com:443";
-        var success = Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out var uri);
-
-        Assert.True(success);
-        Assert.NotNull(uri);
-
-        Assert.Equal("api.facebook.com", uri.Host);
-        Assert.Equal(443, uri.Port);
-        Assert.Equal("https", uri.Scheme);
-        Assert.Equal(string.Empty, uri.Query);
+        Assert.True(UrlValidator.ValidateUrlWithUriCreate(url, out var uri));
         
         var url2 = "ftps://user:password@secure.example.org/files";
-        success = Uri.TryCreate(url2, UriKind.RelativeOrAbsolute, out uri);
-
-        Assert.True(success);
-        Assert.NotNull(uri);
-
-        Assert.Equal("secure.example.org", uri.Host);
-        Assert.Equal("user:password", uri.UserInfo);
-        Assert.Equal("ftps", uri.Scheme);
-        Assert.Equal(string.Empty, uri.Query);
+        Assert.True(UrlValidator.ValidateUrlWithUriCreate(url2, out uri));
         
         var url3 = "file:///C:/Users/username/Documents/file.txt";
-        success = Uri.TryCreate(url3, UriKind.RelativeOrAbsolute, out uri);
+        Assert.True(UrlValidator.ValidateUrlWithUriCreate(url3, out uri));
         
-        Assert.True(success);
-        Assert.NotNull(uri);
-
-        Assert.True(uri.IsFile);
-        Assert.Equal("file", uri.Scheme);
-
         List<string> urls = [url, url2, url3];
         Assert.True(urls.All(x => Uri.IsWellFormedUriString(x, UriKind.RelativeOrAbsolute)));
     }
@@ -88,34 +56,23 @@ public class HowToCheckIfURLIsValidUnitTests
             "ftp://api.site.com?value=word1 word2"
         ];
         
-        Assert.False(Uri.IsWellFormedUriString(urls[0], UriKind.RelativeOrAbsolute));
-        Assert.True(Uri.IsWellFormedUriString(urls[1], UriKind.RelativeOrAbsolute));
-        Assert.True(Uri.IsWellFormedUriString(urls[2], UriKind.RelativeOrAbsolute));
-        Assert.False(Uri.IsWellFormedUriString(urls[3], UriKind.RelativeOrAbsolute));
+        Assert.False(UrlValidator.ValidateUrlWithUriWellFormedString(urls[0]));
+        Assert.True(UrlValidator.ValidateUrlWithUriWellFormedString(urls[1]));
+        Assert.True(UrlValidator.ValidateUrlWithUriWellFormedString(urls[2]));
+        Assert.False(UrlValidator.ValidateUrlWithUriWellFormedString(urls[3]));
     }
 
     [Fact]
     public async Task GivenACorrectUrl_WhenValidatedWithUriHttpRequest_ThenItShouldBeValid()
     {
-        using var client = new HttpClient();
         var url = "https://api.facebook.com";
-
-        var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
-        Assert.True(response.IsSuccessStatusCode);
+        Assert.True(await UrlValidator.ValidateUrlWithHttpClient(url));
     }
 
     [Fact]
     public async Task GivenAnIncorrectUrl_WhenValidatedWithUriHttpRequest_ThenItShouldBeInvalid()
     {
-        using var client = new HttpClient();
         var url = "https://www.example-nonexistent-url.com";
-
-        var exception = await Assert.ThrowsAsync<HttpRequestException>(async () =>
-        {
-            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
-        });
-        
-        Assert.IsType<SocketException>(exception.InnerException);
-        Assert.Equal(SocketError.HostNotFound, (exception.InnerException as SocketException)?.SocketErrorCode);
+        Assert.False(await UrlValidator.ValidateUrlWithHttpClient(url));
     }
 }
