5ecdd17e0f8a5a579de42406461ee951cb1bc2e8
André Carvalho <carlos.carvalho83@gmail.com>
2024-02-06 17:18:26 +0000

    Update Program.cs

--- dotnet-logging/LoggingClassAndMethodNamesUsingSerilog/Api/Program.cs
+++ dotnet-logging/LoggingClassAndMethodNamesUsingSerilog/Api/Program.cs
@@ -3,6 +3,7 @@ using Serilog;
 using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Host.UseSerilog((context, configuration) =>
 {
     configuration
@@ -11,16 +12,22 @@ builder.Host.UseSerilog((context, configuration) =>
         .Enrich.FromLogContext()
         .WriteTo.Console(new CustomJsonFormatter());
 });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
+
 app.MapGet("/weatherforecast", new WeatherForecastEndpoint().GetWeatherForecast)
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+
 app.Run();
