9d987191a1a715973c136a8855abaa9f5dc6b1a6
Zachary <zachdesira@hotmail.com>
2024-02-08 22:03:09 +0100

    CM-690: How to use Serilog with Microsoft's ILogger

---
+++ dotnet-logging/UsingSerilogWithMicrosoftILogger/Tests/WeatherForecastControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using UsingSerilogWithMicrosoftILogger.Controllers;
+
+namespace Tests
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void WhenLoggingInformationWithAMessage_ThenMessageShouldBeShownInConsole()
+        {
+            // Arrange
+            Mock<ILogger<WeatherForecastController>> _loggerMock = new Mock<ILogger<WeatherForecastController>>();
+
+            // Act
+            var response = new WeatherForecastController(_loggerMock.Object).Get();
+
+            // Assert
+            _loggerMock.Verify(logger => logger.Log(LogLevel.Information,
+                        It.IsAny<EventId>(),
+                        It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("Processing GetWeatherForecast...")),
+                        It.IsAny<Exception>(),
+                        It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.Once);
+        }
+    }
+}
---
+++ dotnet-logging/UsingSerilogWithMicrosoftILogger/UsingSerilogWithMicrosoftILogger/Program.cs
@@ -0,0 +1,34 @@
+using Serilog;
+
+namespace UsingSerilogWithMicrosoftILogger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog((context, loggerConfiguration) => 
+            { 
+                loggerConfiguration.WriteTo.Console(); 
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
