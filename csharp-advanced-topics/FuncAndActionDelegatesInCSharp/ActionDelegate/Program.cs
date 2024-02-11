bb8513a06cdd03a47a3ec9b5ca80dff86c58aa0e
imariom <connect@imariom.com>
2024-02-12 00:10:32 +0200

    added action delegate setup code

---
+++ csharp-advanced-topics/FuncAndActionDelegatesInCSharp/ActionDelegate/Program.cs
@@ -0,0 +1,6 @@
+﻿// Action delegate with one parameter
+Action<string> notify
+    = (string message) => Console.WriteLine($"Received message: '{message}'");
+
+// Invoke the Action delegate
+notify("build project");
