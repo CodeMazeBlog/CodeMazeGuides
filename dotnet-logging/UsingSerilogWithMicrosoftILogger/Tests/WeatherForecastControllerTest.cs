73f207471cc7e4da857a62b3b97191aa1ff066d1
Zachary <zachdesira@hotmail.com>
2024-02-12 08:46:10 +0100

    Updated classes to conform with top-level statements

--- dotnet-logging/UsingSerilogWithMicrosoftILogger/Tests/WeatherForecastControllerTest.cs
+++ dotnet-logging/UsingSerilogWithMicrosoftILogger/Tests/WeatherForecastControllerTest.cs
@@ -3,25 +3,25 @@ using Moq;
 using UsingSerilogWithMicrosoftILogger.Controllers;
 using Xunit;
 
-namespace Tests
+namespace Tests;
+
+public class WeatherForecastControllerTest
 {
-    public class WeatherForecastControllerTest
+    [Fact]
+    public void WhenLoggingInformationWithAMessage_ThenMessageShouldBeShownInConsole()
     {
-        [Fact]
-        public void WhenLoggingInformationWithAMessage_ThenMessageShouldBeShownInConsole()
-        {
-            // Arrange
-            var _loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        // Arrange
+        var _loggerMock = new Mock<ILogger<WeatherForecastController>>();
 
-            // Act
-            var response = new WeatherForecastController(_loggerMock.Object).Get();
+        // Act
+        var response = new WeatherForecastController(_loggerMock.Object).Get();
 
-            // Assert
-            _loggerMock.Verify(logger => logger.Log(LogLevel.Information,
-                        It.IsAny<EventId>(),
-                        It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("Processing GetWeatherForecast...")),
-                        It.IsAny<Exception>(),
-                        It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.Once);
-        }
+        // Assert
+        _loggerMock.Verify(logger => logger.Log(LogLevel.Information,
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("Processing GetWeatherForecast...")),
+                    It.IsAny<Exception>(),
+                    It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.Once);
     }
 }
+
