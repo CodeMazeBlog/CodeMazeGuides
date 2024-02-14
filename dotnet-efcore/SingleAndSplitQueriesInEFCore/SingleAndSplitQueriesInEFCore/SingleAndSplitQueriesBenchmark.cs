13695020b6f347f9961ba1e64addaa8f52197049
jshergal <1489638+jshergal@users.noreply.github.com>
2024-02-15 00:07:02 +0200

    Minor tweaks to benchmark to remove warnings about short iteration time

--- dotnet-efcore/SingleAndSplitQueriesInEFCore/SingleAndSplitQueriesInEFCore/SingleAndSplitQueriesBenchmark.cs
+++ dotnet-efcore/SingleAndSplitQueriesInEFCore/SingleAndSplitQueriesInEFCore/SingleAndSplitQueriesBenchmark.cs
@@ -4,47 +4,44 @@ using SingleAndSplitQueriesInEFCore.Model;
 namespace SingleAndSplitQueriesInEFCore;
 
 [MemoryDiagnoser]
+[IterationCount(100)]
 public class SingleAndSplitQueriesBenchmark
 {
-    private CompaniesContext _dbContext;
-    private CompaniesRepository _companiesRepository;
+    private CompaniesContext _dbContext = null!;
+    private CompaniesRepository _companiesRepository = null!;
 
-    [IterationSetup]
+    [GlobalSetup]
     public void Setup()
     {
-        this._dbContext = new CompaniesContext();
+        _dbContext = new CompaniesContext();
         _companiesRepository = new CompaniesRepository(_dbContext);
     }
 
-    [IterationCleanup]
+    [GlobalCleanup]
     public void Cleanup()
     {
         _dbContext.Dispose();
     }
 
     [Benchmark]
-    [IterationCount(100)]
     public IReadOnlyCollection<Company> GetCompaniesWithDepartmentsUsingSingleQuery()
     {
         return _companiesRepository.GetCompaniesWithDepartmentsUsingSingleQuery();
     }
 
     [Benchmark]
-    [IterationCount(100)]
     public IReadOnlyCollection<Company> GetCompaniesWithDepartmentsAndProductsUsingSingleQuery()
     {
         return _companiesRepository.GetCompaniesWithDepartmentsAndProductsUsingSingleQuery();
     }
 
     [Benchmark]
-    [IterationCount(100)]
     public IReadOnlyCollection<Company> GetCompaniesWithDepartmentsUsingSplitQuery()
     {
         return _companiesRepository.GetCompaniesWithDepartmentsUsingSplitQuery();
     }
 
     [Benchmark]
-    [IterationCount(100)]
     public IReadOnlyCollection<Company> GetCompaniesWithDepartmentsAndProductsUsingSplitQuery()
     {
         return _companiesRepository.GetCompaniesWithDepartmentsAndProductsUsingSplitQuery();
