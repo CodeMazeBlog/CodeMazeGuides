5c4d9593b9456250a159fe2248a155f87bcc423a
Emmanuel Adom <79058040+Emmanuel-Adom@users.noreply.github.com>
2024-02-15 06:11:41 +0000

    Initial commit

---
+++ dotnet-client-libraries/SystemDrawingCommonAndAForgeDotNet/AForgeDotNetLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using AForgeDotNetLibrary;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+string outputPath = @"..\..\..\Image\outputImage.jpeg";
+
+var bitmap = ImageService.CreateBlankImage(480, 300);
+bitmap = ImageService.DrawHexagonOnImage(bitmap, Brushes.Blue, GetHexagonPoints());
+var sepiaFilteredImage = ImageService.ApplySepiaFilter(bitmap);
+var gaussianBlurredImage = ImageService.ApplyGaussianBlurFilter(sepiaFilteredImage, 2.0);
+ImageService.SaveImage(gaussianBlurredImage, outputPath, ImageFormat.Jpeg);
+
+Console.WriteLine($"Image created, modified, and saved successfully in Image Folder.");
+
+static PointF[] GetHexagonPoints()
+{
+    return [
+        new PointF(240, 100), 
+        new PointF(290, 125), 
+        new PointF(290, 175), 
+        new PointF(240, 200), 
+        new PointF(190, 175), 
+        new PointF(190, 125) 
+    ];
+}
