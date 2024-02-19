4623af57bde4aef19ac040f81b5642862b11e58b
Georgios Panagopoulos <geopanagopoulosz@gmail.com>
2024-02-19 14:56:48 +0000

    Implementation of examples for how to use Action, Func delegate types in C#. Includes examples for: - Action delegate without parameter - Action delegate with a parameter - Func delegate with a parameter - LINQ/ Where & Select use cases - Added tests

---
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCsharp/ActionAndFuncDelegatesInCsharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Tests")]
+
+
+namespace ActionAndFuncDelegatesInCsharp;
+
+internal class Program
+{
+    internal static void Print()
+    {
+        Console.WriteLine("Happy Coding");
+    }
+
+    internal static void PrintMessage(string message1, string message2)
+    {
+        Console.WriteLine($"{message1}{message2}");
+    }
+
+    internal static string CapitaliseMessage(string message)
+    {
+        return message.ToUpper();
+    }
+
+    static void Main(string[] args)
+    {
+        //Declare Actions and Func
+        Action actionWithoutParameters = Print;
+        Action<string, string> actionWithParameters = PrintMessage;
+        Func<string, string> funcWithParameter = CapitaliseMessage;
+
+        // Execute Actions and Func
+        actionWithoutParameters();
+        actionWithParameters("Happy Coding", " with CodeMaze");
+
+        string upperHappyCoding = funcWithParameter("Happy Coding");
+        Console.WriteLine(upperHappyCoding);
+
+        var messages = new List<string>() { "Happy", "Coding", "with", "CodeMaze" };
+        messages = messages
+                    .Where(m => m.Length > 4)
+                    .Select(m => m.ToUpper())
+                    .ToList();
+        //messages is: { "HAPPY", "CODING", "CODEMAZE" }
+    }
+
+
+}
---
+++ csharp-intermediate-topics/ActionAndFuncDelegatesInCsharp/Tests/UnitTests.cs
@@ -0,0 +1,42 @@
+namespace Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UnitTests
+{
+    [TestMethod]
+    public void GivenAMessage_WhenFuncIsCalled_ThenMessageCapitalised()
+    {
+        string messsage = "Happy Coding";
+        Func<string, string> funcWithParameter = ActionAndFuncDelegatesInCsharp.Program.CapitaliseMessage;
+        
+        string capitalised = funcWithParameter(messsage);
+
+        Assert.AreEqual("HAPPY CODING", capitalised);
+    }
+
+    [TestMethod]
+    public void GivenAnAction_WhenActionIsCalled_ThenNoFailure()
+    {
+        Action actionWithoutParameters = ActionAndFuncDelegatesInCsharp.Program.Print;
+        actionWithoutParameters();
+
+        Assert.IsNotNull(actionWithoutParameters);
+    }
+
+    [TestMethod]
+    public void GivenAListWithMessages_WhenFuncsAreApplied_ThenListIsModified()
+    {
+        var messages = new List<string>() { "Happy", "Coding", "with", "CodeMaze" };
+        messages = messages
+                    .Where(m => m.Length > 4)
+                    .Select(m => m.ToUpper())
+                    .ToList();
+
+        Assert.AreEqual(3, messages.Count);
+        Assert.IsTrue(messages.Contains("HAPPY"));
+        Assert.IsTrue(messages.Contains("CODING"));
+        Assert.IsTrue(messages.Contains("CODEMAZE"));
+    }
+}
