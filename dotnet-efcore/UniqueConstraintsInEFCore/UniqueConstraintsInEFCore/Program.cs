32d701a2dbbf527a6d9bf8adfa3b8bd436cca36b
Ivan Gechev <ivan_banov@abv.bg>
2024-02-19 07:42:33 +0200

    public partial class Program { }

--- dotnet-efcore/UniqueConstraintsInEFCore/UniqueConstraintsInEFCore/Program.cs
+++ dotnet-efcore/UniqueConstraintsInEFCore/UniqueConstraintsInEFCore/Program.cs
@@ -3,49 +3,43 @@ using UniqueConstraintsInEFCore.Data;
 using UniqueConstraintsInEFCore.Data.Models;
 using UniqueConstraintsInEFCore.Services;
 
-namespace UniqueConstraintsInEFCore;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHostedService<InitializationService>();
+
+builder.Services.AddDbContext<SolarSystemDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SolarSystemDatabase")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddHostedService<InitializationService>();
-
-        builder.Services.AddDbContext<SolarSystemDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("SolarSystemDatabase")));
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.MapPost("/planets", async (Planet planet, SolarSystemDbContext context) =>
-        {
-            try
-            {
-                await context.Planets.AddAsync(planet);
-                await context.SaveChangesAsync();
-
-                return Results.Created($"/planets/{planet.Id}", planet);
-            }
-            catch
-            {
-                return Results.BadRequest();
-            }
-        })
-        .WithName("AddPlanet")
-        .WithOpenApi();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.MapPost("/planets", async (Planet planet, SolarSystemDbContext context) =>
+{
+    try
+    {
+        await context.Planets.AddAsync(planet);
+        await context.SaveChangesAsync();
+
+        return Results.Created($"/planets/{planet.Id}", planet);
+    }
+    catch
+    {
+        return Results.BadRequest();
+    }
+})
+.WithName("AddPlanet")
+.WithOpenApi();
+
+app.Run();
+
+public partial class Program { }
