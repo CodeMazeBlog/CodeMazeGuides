e762888a6d3bb09bd47dd60d7606ba502e851573
the-KALU-DEV <the.kalu.dev@gmail.com>
2024-02-20 21:28:03 +0100

    initial commit

---
+++ csharp-advanced-topics/ActionAndFuncDelegates/ActionAndFuncDelegatesLib/ActionAndFuncDelegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegatesLib;
+
+public class ActionAndFuncDelegates
+{
+    public static void GreetUser(string name) => Console.WriteLine($"Hello {name}, welcome to the magnificent world of Action Delegates.");
+    public static string SquareToString(int x) => (x * x).ToString();
+    public static IEnumerable<int> GetEvenSquaredNumbersAbove30()
+    {
+        List<int> numbers = Enumerable.Range(1, 10).ToList();
+
+        Func<int, bool> isEven = x => x % 2 == 0;
+        Func<int, int> square = x => x * x;
+        Func<int, bool> isAbove30 = x => x > 30;
+
+        var evenSquaredNumbersAbove30 = numbers
+            .Where(isEven)
+            .Select(square)
+            .Where(isAbove30);
+
+        return evenSquaredNumbersAbove30;
+    }
+}
---
+++ csharp-advanced-topics/ActionAndFuncDelegates/Tests/ActionAndFuncDelegatesUnitTests/ActionAndFuncDelegatesUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using ActionAndFuncDelegatesLib;
+
+namespace ActionAndFuncDelegatesUnitTests;
+
+public class ActionAndFuncDelegatesUnitTest
+{
+    [Fact]
+    public void WhenGreetUser_ThenPrintMessage()
+    {
+        //Arrange
+        var output = new StringWriter();
+        Console.SetOut(output);
+        string name = "Dave";
+
+        //Act
+        ActionAndFuncDelegates.GreetUser(name);
+
+        //Assert
+        string expected = $"Hello {name}, welcome to the magnificent world of Action Delegates.";
+        Assert.Equal(expected, output.ToString().Trim());
+    }
+
+    [Fact]
+    public void WhenSquareNumber_ThenReturnCorrectValue()
+    {
+        //Arrange
+        int input = 3;
+        string expected = "9";
+
+        //Act
+        string actual = ActionAndFuncDelegates.SquareToString(input);
+
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void WhenGetEvenSquaredNumbersAbove30_ThenReturnCorrectValues()
+    {
+        //Arrange
+        var expected = new List<int> { 36, 64, 100 };
+
+        //Act
+        var actual = ActionAndFuncDelegates.GetEvenSquaredNumbersAbove30().ToList();
+
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+}
