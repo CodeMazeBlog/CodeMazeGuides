34720ab4c5e6429e504f5b62f975acee74a1976b
Ivan Gechev <ivan_banov@abv.bg>
2024-02-20 19:59:58 +0200

    CM-321: How to Inject a DbContext Instance Into an IHostedService (#1799)
    * InitialCommit
    
    * Code is done
    
    * tests
    
    * namespace change
    
    * minor changes
    
    * Top-Level statement and renaming of private variables
    
    ---------
    
    Co-authored-by: fl0tschi <fl0tschi>

---
+++ dotnet-dependency-injection/InjectDbContextIntoIHostedService/InjectDbContextIntoIHostedService/Data/Cat.cs
@@ -0,0 +1,8 @@
+﻿namespace InjectDbContextIntoIHostedService.Data;
+
+public class Cat
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required int Age { get; set; }
+}
---
+++ dotnet-dependency-injection/InjectDbContextIntoIHostedService/InjectDbContextIntoIHostedService/Services/CatsSeedingService.cs
@@ -0,0 +1,32 @@
+﻿using InjectDbContextIntoIHostedService.Data;
+
+namespace InjectDbContextIntoIHostedService.Services;
+
+public class CatsSeedingService(IServiceScopeFactory scopeFactory)
+    : IHostedService
+{
+    private static readonly int _maxAge = 15;
+    private static readonly string[] _names =
+        ["Whiskers", "Luna", "Simba", "Bella", "Oliver", "Shadow", "Gizmo", "Cleo", "Jasper", "Mocha"];
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var scope = scopeFactory.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<CatsDbContext>();
+
+        await context.Database.EnsureCreatedAsync(cancellationToken);
+
+        context.Cats.AddRange(Enumerable.Range(1, 50)
+            .Select(_ => new Cat
+            {
+                Id = Guid.NewGuid(),
+                Name = _names[Random.Shared.Next(_names.Length)],
+                Age = Random.Shared.Next(1, _maxAge)
+            }));
+
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+}
