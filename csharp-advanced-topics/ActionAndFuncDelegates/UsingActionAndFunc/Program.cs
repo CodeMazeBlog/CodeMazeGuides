ddb049816bf0975836de68a75528596f36a9256d
the-KALU-DEV <the.kalu.dev@gmail.com>
2024-02-22 14:27:21 +0100

    Refactored code formatting inconsistencies.

--- csharp-advanced-topics/ActionAndFuncDelegates/UsingActionAndFunc/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegates/UsingActionAndFunc/Program.cs
@@ -10,18 +10,20 @@ greetings("Dave");
 
 Func<int, string> squaredResult = ActionAndFunc.SquareToString;
 
-string result = squaredResult(3);
+var result = squaredResult(3);
 
 Console.WriteLine(result);
 
 //With lambda and LINQ
 
-List<int> numbers = Enumerable.Range(1, 10).ToList();
+var numbers = Enumerable.Range(1, 10).ToList();
 
 //Any Func<int, bool> below is supposed to be a Predicate<int, bool> as it is best fitted for this purpose,
 //but seeing as it wasn't introduced, we maintain the Func<int, bool>
 Func<int, bool> isEven = x => x % 2 == 0;
+
 Func<int, int> square = x => x * x;
+
 Func<int, bool> isAbove30 = x => x > 30;
 
 var evenSquaredNumbersAbove30 = numbers
