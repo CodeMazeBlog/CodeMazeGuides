429721d43ee6be0993ca0709b340f8afb2d5097b
Ehab Shaker <ehab-shaker319@outlook.com>
2024-02-23 15:18:58 +0300

    Adding Result Record

---
+++ files-csharp/ValidatingFileUploadExtension/ValidatingFileUploadExtension/Record.cs
@@ -0,0 +1,8 @@
+﻿namespace ValidatingFileUploadExtension
+{
+    public record Result(bool Acceptable, Status Status, string Message)
+    {
+
+    }
+    public enum Status { GENUINE, FAKE, NOT_SUPPORTED }
+}
