f07228ca466de55a0dd9e7421d60d5ad02aa744b
acha-r <jayrachel96@gmail.com>
2024-02-26 08:38:03 +0100

    calculate directory size

---
+++ files-csharp/CalculateDirectorySize/Tests/DirectorySizeCalculatorTests.cs
@@ -0,0 +1,37 @@
+using CalculateDirectorySize;
+
+namespace Tests
+{
+    public class DirectorySizeCalculatorTests
+    {
+        //add a valid directory path
+        string testDirectory = @"";
+
+        [Fact]
+        public void WhenDirectoryInfoIsPassed_ReturnDirectorySize()
+        {
+            //Arrange
+            DirectoryInfo directoryInfo = new DirectoryInfo(testDirectory);
+            long size;
+
+            //Act
+            size = DirectorySizeCalculator.GetSizeWithRecursion(directoryInfo);
+
+            //Assert
+            Assert.True(size > 0);
+        }
+
+        [Fact]
+        public void WhenDirectoryInfoIsPassedToParallelProcess_ReturnDirectorySize()
+        {
+            // Arrange
+            DirectoryInfo directoryInfo = new DirectoryInfo(testDirectory);
+
+            // Act
+            long size = DirectorySizeCalculator.GetSizeByParallelProcessing(directoryInfo);
+
+            // Assert
+            Assert.True(size > 0);
+        }
+    }
+}
