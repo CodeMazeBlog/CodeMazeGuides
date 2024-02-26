0f23382f80c2ab506a38d69b537695c132472a26
André Carvalho <carlos.carvalho83@gmail.com>
2024-02-26 10:26:17 +0000

    Update DigitCounter.cs

--- csharp-basic-topics/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/DigitCounter.cs
+++ csharp-basic-topics/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/DigitCounter.cs
@@ -49,13 +49,13 @@
             number = Math.Abs(number);
             if (number < 10) return 1;
             if (number < 100) return 2;
-            if (number < 1000) return 3;
-            if (number < 10000) return 4;
-            if (number < 100000) return 5;
-            if (number < 1000000) return 6;
-            if (number < 10000000) return 7;
-            if (number < 100000000) return 8;
-            if (number < 1000000000) return 9;
+            if (number < 1_000) return 3;
+            if (number < 10_000) return 4;
+            if (number < 100_000) return 5;
+            if (number < 1_000_000) return 6;
+            if (number < 10_000_000) return 7;
+            if (number < 100_000_000) return 8;
+            if (number < 1_000_000_000) return 9;
 
             return 10;
         }
