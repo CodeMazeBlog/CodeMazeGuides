be1e3185d7c8f7f218c76fefe5c5aa44359033a3
fl0tschi <fl0tschi>
2024-02-26 21:48:50 +0100

    Fixed formatting of GlobalExceptionHandler

--- csharp-basic-topics/ExceptionHandlingInCSharp/ExceptionHandlingInCSharp/Exceptions/GlobalExceptionHandler.cs
+++ csharp-basic-topics/ExceptionHandlingInCSharp/ExceptionHandlingInCSharp/Exceptions/GlobalExceptionHandler.cs
@@ -4,7 +4,11 @@ public static class GlobalExceptionHandler
 {
     public static void HandleException(object sender, UnhandledExceptionEventArgs e)
     {
-        if (e.ExceptionObject is not Exception exception) return;
+        if (e.ExceptionObject is not Exception exception) 
+        {
+            return;
+        }
+        
         Console.WriteLine("Global Exception Handler caught an exception: " + exception.Message);
     }
 }
