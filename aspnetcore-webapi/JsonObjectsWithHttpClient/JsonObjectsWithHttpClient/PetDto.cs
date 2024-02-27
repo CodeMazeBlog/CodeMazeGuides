7b086a5078b8f5c8e8ba6a2713215a9faebe7105
Crownedprinz <crownedprinz@Jays-MacBook-Pro.local>
2024-02-27 19:16:28 +0100

    CM-928: How to Send a JsonObject Using HttpClient in .NET

---
+++ aspnetcore-webapi/JsonObjectsWithHttpClient/JsonObjectsWithHttpClient/PetDto.cs
@@ -0,0 +1,14 @@
+﻿namespace JsonObjectsWithHttpClient;
+
+public class PetDto
+{
+    public long id { get; set; }
+    public Category category { get; set; }
+    public string name { get; set; }
+    public string status { get; set; }
+}
+
+public class Category
+{
+    public string name { get; set; }
+}
---
+++ aspnetcore-webapi/JsonObjectsWithHttpClient/Tests/PetTest.cs
@@ -0,0 +1,18 @@
+﻿using JsonObjectsWithHttpClient.Controllers;
+
+namespace Tests;
+
+public class PetTest
+{
+    [Fact]
+    public async void GivenPetObjectHasValues_WhenPetControllerIsCalled_ThenPetResultIsReturned()
+    {
+        var petController = new PetController();
+
+        var successResult = await petController.Post();
+
+        Assert.NotEqual(0, successResult.id);
+
+        Assert.Equal("German Shepherd", successResult.name);
+    }
+}
