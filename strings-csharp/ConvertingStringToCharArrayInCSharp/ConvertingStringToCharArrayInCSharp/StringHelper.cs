286fab5d0001c85c253b5e613e88c53c65cf7ea9
Samuel <lawalsamuel2310@gmail.com>
2024-03-01 00:44:25 +0100

    Create StringHelper class with these methods: StringToCharArray, StringToChar, StringToCharArrayUsingReadOnlySpan, StringArrayToCharArrayUsingLoop, StringArrayToCharArrayUsingLinq

---
+++ strings-csharp/ConvertingStringToCharArrayInCSharp/ConvertingStringToCharArrayInCSharp/StringHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace ConvertingStringToCharArrayInCSharp
+{
+    public static class StringHelper
+    {
+        public static char[] StringToCharArray(string inputString)
+        {
+            return inputString.ToCharArray();
+        }
+
+        public static ReadOnlySpan<char> StringToCharArrayUsingReadOnlySpan(string inputString)
+        {
+            var charSpan = inputString.AsSpan();
+            return charSpan;
+        }
+
+        public static char StringToChar(string inputString)
+        {
+            if (inputString.Length == 1)
+            {
+                return inputString[0];
+            }
+
+            throw new ArgumentException("Input string must contain exactly one character.");
+        }
+
+        public static char[] StringArrayToCharArrayUsingLoop(string[] stringArray)
+        {
+            StringBuilder combinedString = new StringBuilder();
+
+            foreach (string str in stringArray)
+            {
+                combinedString.Append(str);
+            }
+
+            return combinedString.ToString().ToCharArray();
+        }
+
+        public static char[] StringArrayToCharArrayUsingLinq(string[] stringArray)
+        {
+            return stringArray.SelectMany(s => s.ToCharArray()).ToArray();
+        }
+    }
+}
