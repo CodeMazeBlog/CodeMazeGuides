5e95f1ef556198c1c477ddc0fb603f3a8f798f0d
alessandro0108 <alessandro0108@gmail.com>
2024-02-29 21:51:13 +0100

    Added GetBitManipCount() and its test

--- csharp-basic-topics/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/DigitCounter.cs
+++ csharp-basic-topics/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/DigitCounter.cs
@@ -2,6 +2,27 @@
 {
     public static class DigitCounter
     {
+        private static readonly int[] _guessDigits = [9, 9, 9, 8, 8, 8, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0];
+        private static readonly int[] _powersOf10 = [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000];
+
+        public static int GetBitManipCount(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            number = Math.Abs(number);
+            var leading = BitOperations.LeadingZeroCount((uint)number);
+            var digits = _guessDigits[leading];
+            if (number >= _powersOf10[digits])
+            {
+                ++digits;
+            }
+
+            return digits;
+        }
+
         public static int GetIterativeCount(int number)
         {
             if (number == 0)
--- csharp-basic-topics/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/GlobalUsings.cs
+++ csharp-basic-topics/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/GlobalUsings.cs
@@ -1,1 +1,2 @@
 ﻿global using CountNumberOfDigitsInANumber;
+global using System.Numerics;
