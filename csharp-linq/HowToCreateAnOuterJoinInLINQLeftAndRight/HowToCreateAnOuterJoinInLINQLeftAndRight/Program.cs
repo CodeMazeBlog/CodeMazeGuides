4d00d69de29bcdaac741fbba8ab7a08d893fb459
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2024-03-02 09:35:08 +0100

    CM-901: How to Create an Outer Join in LINQ - (Left and Right) (#1807)
    * CM-901: Initial commit
    
    * CM 901: Implement left and right join plus tests
    
    * CM 901: Implement the less common outer joins.
    
    * CM-901: Remove extra white spaces in code.

---
+++ csharp-linq/HowToCreateAnOuterJoinInLINQLeftAndRight/HowToCreateAnOuterJoinInLINQLeftAndRight/Program.cs
@@ -0,0 +1,105 @@
+﻿using HowToCreateAnOuterJoinInLINQLeftAndRight;
+
+Console.WriteLine("Left outer join - query syntax");
+Console.WriteLine();
+
+var results = Utilities.PerformLeftJoinWithQuerySyntax(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Right outer join - query syntax");
+Console.WriteLine();
+
+results = Utilities.PerformRightJoinWithQuerySyntax(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Left outer join - method syntax");
+Console.WriteLine();
+
+results = Utilities.PerformLeftJoinWithMethodSyntax(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Right outer join - method syntax");
+Console.WriteLine();
+
+results = Utilities.PerformRightJoinWithMethodSyntax(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Full outer join");
+Console.WriteLine();
+
+results = Utilities.PerformFullOuterJoin(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Left excluding join");
+Console.WriteLine();
+
+results = Utilities.PerformLeftExcludingJoin(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Right excluding join");
+Console.WriteLine();
+
+results = Utilities.PerformRightExcludingJoin(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Full outer excluding join");
+Console.WriteLine();
+
+results = Utilities.PerformFullOuterExcludingJoin(Utilities.Songs, Utilities.Authors);
+
+foreach (var item in results)
+{
+    Console.WriteLine($"Title: {item.Title}, Author: {item.AuthorName}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
---
+++ csharp-linq/HowToCreateAnOuterJoinInLINQLeftAndRight/HowToCreateAnOuterJoinInLINQLeftAndRight/SongWithAuthorComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HowToCreateAnOuterJoinInLINQLeftAndRight;
+
+internal class SongWithAuthorComparer : IEqualityComparer<SongWithAuthor>
+{
+    public bool Equals(SongWithAuthor? x, SongWithAuthor? y)
+    {
+        if (x?.AuthorName == y?.AuthorName && x?.Title == y?.Title)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int GetHashCode([DisallowNull] SongWithAuthor obj)
+    {
+        return obj.AuthorName.GetHashCode();
+    }
+}
