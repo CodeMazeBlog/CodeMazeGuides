3a4aaf96bb2b62e8e797500aef3b9e18718c9472
Daniel LAURENS <dlaur94@gmail.com>
2024-03-02 22:28:02 +0100

    Initialization CM-1019

---
+++ dotnet-efcore/VirtualKeywordInEFCore/VirtualKeywordInEFCore/Models/DataContextLazyLoading.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VirtualKeywordInEFCore.Models
+{
+    public class DataContextLazyLoading : DbContext
+    {
+        public DataContextLazyLoading() : base()
+        {
+            //this.ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database=LibraryLazy.db;Trusted_Connection=True;MultipleActiveResultSets=true")
+                   .UseLazyLoadingProxies();
+
+            base.OnConfiguring(options);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book_lazy>()
+                .HasOne<Author_lazy>(s => s.Author)
+                .WithMany(g => g.Books)
+                .HasForeignKey(s => s.AuthorId);
+        }
+
+        public DbSet<Author_lazy> Authors_lazy { get; set; }
+        public DbSet<Book_lazy> Books_lazy { get; set; }
+    }
+}
