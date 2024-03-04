69fb19579d8953919ad502b39587f7b6e0b7fa69
Ivan Gechev <ivan_banov@abv.bg>
2024-03-04 18:57:00 +0200

    CM-608: Architecture Tests with NetArchTest.Rules (#1836)
    * InitialCommit
    
    * Basic code is done
    
    * Architecture.Tests
    
    * Tests
    
    * Renamed tests

---
+++ dotnet-testing/ArchitectureTestsWithNetArchTest/Domain/Entities/Cat.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Cat
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Breed { get; set; }
+    public required DateTime DateOfBirth { get; set; }
+}
