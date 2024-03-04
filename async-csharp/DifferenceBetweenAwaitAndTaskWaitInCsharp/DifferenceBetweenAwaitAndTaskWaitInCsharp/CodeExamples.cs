4e9039e46382e7299b5ec2a8c67822536d0315d4
Florian Prinz (FLPR) <flpr@simcorp.com>
2024-03-04 19:40:42 +0100

    Changed formatting

--- async-csharp/DifferenceBetweenAwaitAndTaskWaitInCsharp/DifferenceBetweenAwaitAndTaskWaitInCsharp/CodeExamples.cs
+++ async-csharp/DifferenceBetweenAwaitAndTaskWaitInCsharp/DifferenceBetweenAwaitAndTaskWaitInCsharp/CodeExamples.cs
@@ -1,70 +1,78 @@
-﻿namespace DifferenceBetweenAwaitAndTaskWaitInCsharp
+﻿namespace DifferenceBetweenAwaitAndTaskWaitInCsharp;
+
+public static class CodeExamples
 {
-    public static class CodeExamples
+    public static void BlockingCodeExample()
     {
-        public static void BlockingCodeExample()
-        {
-            Console.WriteLine("** Blocking code Example **");
+        Console.WriteLine("** Blocking code Example **");
 
-            Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
-            Task<string> taskToBlock = Task<string>.Run(() =>
+        Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
+
+        Task<string> taskToBlock = Task<string>.Run(() =>
+        {
+            Thread.Sleep(5000);
+            return "Hello, from a blocking task";
+        });
+
+        taskToBlock.Wait();
+
+        string blockingResult = taskToBlock.Result;
+        Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
+    }
+
+    public static void BlockingExceptionHandling()
+    {
+        Console.WriteLine("** Blocking Exception Handling **");
+
+        try
+        {
+            Task taskToFail = Task.Run(() =>
             {
-                Thread.Sleep(5000);
-                return "Hello, from a blocking task";
+                throw new ApplicationException("Error in the blocking long-running operation");
             });
-            taskToBlock.Wait();
-            string blockingResult = taskToBlock.Result;
-            Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
+
+            taskToFail.Wait();
         }
-
-        public static void BlockingExceptionHandling()
+        catch (ApplicationException ex)
         {
-            Console.WriteLine("** Blocking Exception Handling **");
-
-            try
-            {
-                Task taskToFail = Task.Run(() =>
-                {
-                    throw new ApplicationException("Error in the blocking long-running operation");
-                });
-                taskToFail.Wait();
-            }
-            catch (ApplicationException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine(ex.Message);
         }
+    }
 
-        public async static Task AsynchronousCodeExampleAsync()
+    public async static Task AsynchronousCodeExampleAsync()
+    {
+        Console.WriteLine("** Asynchronous code Example **");
+
+        Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
+
+        Task<string> taskToAwait = Task<string>.Run(() =>
         {
-            Console.WriteLine("** Asynchronous code Example **");
+            Thread.Sleep(5000);
 
-            Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
-            Task<string> taskToAwait = Task<string>.Run(() =>
+            return "Hello, from an asyncrhonous task";
+        });
+
+        string awaitResult = await taskToAwait;
+        
+        Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
+    }
+
+    public async static Task AsynchronousExceptionHandlingAsync()
+    {
+        Console.WriteLine("** Asynchronous Exception Handling **");
+
+        try
+        {
+            Task taskToFail = Task.Run(() =>
             {
-                Thread.Sleep(5000);
-                return "Hello, from an asyncrhonous task";
+                throw new ApplicationException("Error in the asynchronous long-running operation");
             });
-            string awaitResult = await taskToAwait;
-            Console.WriteLine($"Current ManagedThreadId: {Environment.CurrentManagedThreadId}");
+
+            await taskToFail;
         }
-
-        public async static Task AsynchronousExceptionHandlingAsync()
+        catch (ApplicationException ex)
         {
-            Console.WriteLine("** Asynchronous Exception Handling **");
-
-            try
-            {
-                Task taskToFail = Task.Run(() =>
-                {
-                    throw new ApplicationException("Error in the asynchronous long-running operation");
-                });
-                await taskToFail;
-            }
-            catch (ApplicationException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine(ex.Message);
         }
     }
 }
