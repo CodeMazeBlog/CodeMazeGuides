15a61e81aa0ebd9eb48bf25343402c52138ff611
Ivan Gechev <ivan_banov@abv.bg>
2024-03-04 10:22:47 +0200

    InitialCommit

---
+++ aspnetcore-webapi/AutomaticRegistrationOfMinimalAPIs/AutomaticRegistrationOfMinimalAPIs/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/something", () =>
+{
+    return Results.Ok("anything");
+})
+.WithName("Something")
+.WithOpenApi();
+
+app.Run();
