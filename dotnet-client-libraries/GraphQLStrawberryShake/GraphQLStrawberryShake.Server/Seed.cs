3f176ffb85cb7cd1db27b7375db24dd499eaa374
Vadzim <01vadim10@gmail.com>
2024-03-05 23:07:57 +0100

    CM-815: Introduction to Strawberry Shake and GraphQL in .NET

---
+++ dotnet-client-libraries/GraphQLStrawberryShake/GraphQLStrawberryShake.Server/Seed.cs
@@ -0,0 +1,65 @@
+﻿namespace GraphQLStrawberryShake.Server;
+
+public static class Seed
+{
+    public static void Initialize(ApplicationDbContext context)
+    {
+        context.Database.EnsureCreated();
+
+        if (context.ShippingContainers.Any())
+        {
+            return;
+        }
+
+        var containers = new ShippingContainer[]
+        {
+            new()
+            {
+                Id = "1",
+                Name = "Container 1",
+                Space = new ()
+                {
+                    Length = 12,
+                    Width = 8,
+                    Height = 8
+                }
+            },
+            new()
+            {
+                Id = "2",
+                Name = "Container 2",
+                Space = new ()
+                {
+                    Length = 12,
+                    Width = 8,
+                    Height = 8
+                }
+            },
+            new()
+            {
+                Id = "3",
+                Name = "Container 3",
+                Space = new ()
+                {
+                    Length = 12,
+                    Width = 8,
+                    Height = 8
+                }
+            },
+            new()
+            {
+                Id = "4",
+                Name = "Container 4",
+                Space = new ()
+                {
+                    Length = 12,
+                    Width = 8,
+                    Height = 8
+                }
+            }
+        };
+
+        context.ShippingContainers.AddRange(containers);
+        context.SaveChanges();
+    }
+}
