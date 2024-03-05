940f8f32858f476651cd964ca067d3c85d44eb86
DESKTOP-4M7NQN3\User <Pooya.Alamirpour@gmail.com>
2024-03-05 03:40:53 +0330

    Adding usage in program.cs

--- dotnet-cli/ReadingAppsettingsInConsoleApplication/ReadingAppsettingsInConsoleApplication/Program.cs
+++ dotnet-cli/ReadingAppsettingsInConsoleApplication/ReadingAppsettingsInConsoleApplication/Program.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using ReadingAppsettingsInConsoleApplication.Configurations.ConfigurationProviders;
+﻿using ReadingAppsettingsInConsoleApplication.Configurations.ConfigurationProviders;
 
 Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "development");
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
@@ -13,12 +11,19 @@ var configuration = new ConfigurationBuilder()
             $"appsettings.{environment}.json")
     .Build();
 
-
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IConfiguration>(configuration)
     .BuildServiceProvider();
 
-var tmp1 = new SimpleKeyValueRetrievalConfiguration();
-var tmp2 = new RetrievalConfigurationWithGenericMethod();
-var tmp3 = new StrongTypeConfigurationRetrieval();
+var simpleKeyValueRetrievalConfiguration = new SimpleKeyValueRetrievalConfiguration();
+var retrievalConfigurationWithGenericMethod = new RetrievalConfigurationWithGenericMethod();
+var strongTypeConfigurationRetrieval = new StrongTypeConfigurationRetrieval();
+
+var(message1, number1) = simpleKeyValueRetrievalConfiguration.GetApplicationSettings(serviceProvider);
+var(message2, number2) = retrievalConfigurationWithGenericMethod.GetApplicationSettings(serviceProvider);
+var(message3, number3) = strongTypeConfigurationRetrieval.GetApplicationSettings(serviceProvider);
+
+Console.WriteLine($"Message for {nameof(SimpleKeyValueRetrievalConfiguration)} is {message1}");
+Console.WriteLine($"Message for {nameof(RetrievalConfigurationWithGenericMethod)} is {message2}");
+Console.WriteLine($"Message for {nameof(StrongTypeConfigurationRetrieval)} is {message3}");
 
