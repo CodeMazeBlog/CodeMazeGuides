860cd1419457dba2c93423e7f5b30abc0921112b
Florian Prinz (FLPR) <flpr@simcorp.com>
2024-03-06 19:53:17 +0100

    Removed unnecessary tests - no assertions

--- async-csharp/DifferenceBetweenAwaitAndTaskWaitInCsharp/Tests/UnitTests.cs
+++ async-csharp/DifferenceBetweenAwaitAndTaskWaitInCsharp/Tests/UnitTests.cs
@@ -6,27 +6,9 @@ namespace Tests;
 public class UnitTests
 {
     [TestMethod]
-    public void GivenATask_WhenTaskWaitIsCalled_ThenCodeBlocks()
-    {
-        CodeExamples.BlockingCodeExample();
-    }
-
-    [TestMethod]
-    public async Task GivenATask_WhenAwaitIsUsed_ThenCodeIsAsync()
-    {
-        await CodeExamples.AsynchronousCodeExampleAsync();
-    }
-
-    [TestMethod]
     [ExpectedException(typeof(AggregateException))]
     public void GivenAFailingTask_WhenTaskWaitIsCalled_ThenAggregateExceptionIsRaised()
     {
         CodeExamples.BlockingExceptionHandling();
-    }
-
-    [TestMethod]
-    public async Task GivenAFailingTask_WhenAwaitIsCalled_ThenExceptionIsHandled()
-    {
-        await CodeExamples.AsynchronousExceptionHandlingAsync();
     }
 }
