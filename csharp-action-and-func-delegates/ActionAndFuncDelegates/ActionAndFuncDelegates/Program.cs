003c81d1fd060f03bcde5e22b58d2d2fae825269
samet.kayikci <samet.kayikci@turk.net>
2024-03-07 18:31:10 +0300

    This is a Test PR and after raising it, I will be closing it.

---
+++ csharp-action-and-func-delegates/ActionAndFuncDelegates/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿// We defined an Action delegate that points to the PrintMessage method.
+Action<string> printAction = PrintMessage;
+
+// Invoke the method via delegate and print the result to the console.
+printAction("Hello code maze, this is an action delegate!");
+
+static void PrintMessage(string message)
+{
+    Console.WriteLine(message);
+}
+
+
+// We defined a Func director that takes an int and returns its square.
+Func<int, int> squareFunc = Square;
+
+// Invoke the method via delegate and print the result to the console.
+int result = squareFunc(7);
+Console.WriteLine($"Square of 7 is {result}");
+
+static int Square(int number)
+{
+    return number * number;
+}
