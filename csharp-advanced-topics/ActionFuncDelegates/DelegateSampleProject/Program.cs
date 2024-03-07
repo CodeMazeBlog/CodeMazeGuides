6b62b7e26dd185e6db109f33c05085146c232bae
kundank2024 <kundan.sriv@gmail.com>
2024-03-07 22:05:58 +0530

    Comments and var keyword
    Added comments and changed the object name as var.

--- csharp-advanced-topics/ActionFuncDelegates/DelegateSampleProject/Program.cs
+++ csharp-advanced-topics/ActionFuncDelegates/DelegateSampleProject/Program.cs
@@ -5,17 +5,34 @@ namespace ActionFuncDelegate
 
     public class NumberAddition
     {
+        /// <summary>
+        /// A Delegate that takes 2 input parameters
+        /// </summary>
+        /// <param name="number1">Parameter1 is an integere and name of parameter is number1</param>
+        /// <param name="number2">Parameter1 is an integere and name of parameter is number2</param>
+        /// <returns>It return and integer value</returns>
         public delegate int AddNumDelegate(int number1, int number2);
         public int AddWithDelegate(int num1, int num2)
         {
             return num1 + num2;
         }
 
+        /// <summary>
+        /// An Action Delegate that takes 2 input parameters
+        /// </summary>
+        /// <param name="number1">Parameter1 is an integere and name of parameter is number1</param>
+        /// <param name="number2">Parameter1 is an integere and name of parameter is number2</param>
         public static void AddWithActionDelegate(int number1, int number2)
         {
             Console.WriteLine("Using Action Delegate | Result is {0}", number1 + number2);
         }
 
+        /// <summary>
+        /// Func Delegate that takes 2 input parameters
+        /// </summary>
+        /// <param name="number1">Parameter1 is an integere and name of parameter is number1</param>
+        /// <param name="number2">Parameter1 is an integere and name of parameter is number2</param>
+        /// <returns>It return and integer value</returns>
         public static int AddWithFuncDelegate(int number1, int number2)
         {
             return number1 + number2;
@@ -27,9 +44,9 @@ namespace ActionFuncDelegate
         static void Main(string[] args)
         {
             //Invoke Delegate
-            NumberAddition numberAddition = new NumberAddition();
-            AddNumDelegate addNumDelegate = new AddNumDelegate(numberAddition.AddWithDelegate);
-            int resultWithDelegate = addNumDelegate(10, 20);
+            var numberAddition = new NumberAddition();
+            var addNumDelegate = new AddNumDelegate(numberAddition.AddWithDelegate);
+            var resultWithDelegate = addNumDelegate(10, 20);
             Console.WriteLine("Using Delegate | Result is {0}", resultWithDelegate);
 
             //Invoke Action Delegate
@@ -38,7 +55,7 @@ namespace ActionFuncDelegate
 
             //Invoke Func Delegate
             Func<int, int, int> addNumberFuncDelegate = NumberAddition.AddWithFuncDelegate;
-            int resultWithFuncDelegate = addNumberFuncDelegate(30, 50);
+            var resultWithFuncDelegate = addNumberFuncDelegate(30, 50);
             Console.WriteLine("Using Func Delegate | Result is {0}", resultWithFuncDelegate);
             Console.ReadLine();
         }
--- csharp-advanced-topics/ActionFuncDelegates/Tests/ActionFuncDelegateTests.cs
+++ csharp-advanced-topics/ActionFuncDelegates/Tests/ActionFuncDelegateTests.cs
@@ -6,36 +6,32 @@ namespace Tests
     [TestClass]
     public class ActionFuncDelegateTests
     {
-
-
-
         [TestMethod]
-        public void WhenNumberSent_DelegateExecuteTheMethod()
+        public void GivenTwoNumbers_WhenDelegateInvoked_ThenExpectSumOfNumbers()  
         {
-            NumberAddition numberAddition = new NumberAddition();
-            AddNumDelegate addNumDelegate = new AddNumDelegate(numberAddition.AddWithDelegate);
-            int actualResultWithDelegate = addNumDelegate(10, 20);
+            var numberAddition = new NumberAddition();
+            var addNumDelegate = new AddNumDelegate(numberAddition.AddWithDelegate);
+            var actualResultWithDelegate = addNumDelegate(10, 20);
+
             Assert.AreEqual(30, actualResultWithDelegate);
         }
 
-
         [TestMethod]
-        public void WhenNumberSent_ActionDelegateExecuteTheMethod()
+        public void GivenTwoNumbers_WhenActionDelegateInvoked_ThenSumOfNumbers()
         {
             Action<int, int> actionDelegate = new Action<int, int>(NumberAddition.AddWithActionDelegate);
+
             Assert.IsTrue(true);
-            
         }
 
         [TestMethod]
-        public void WhenNumberSent_FuncDelegateExecuteTheMethod()
+        public void GivenTwoNumbers_WhenFuncDelegateInvoked_ThenExpectSumOfNumbers()
         {
-
             Func<int, int, int> addNumberFuncDelegate = NumberAddition.AddWithFuncDelegate;
-            int actualResultWithFuncDel = 0;
+            var actualResultWithFuncDel = 0;
             actualResultWithFuncDel = addNumberFuncDelegate(30, 50);
-            Assert.AreEqual(80, actualResultWithFuncDel);
 
+            Assert.AreEqual(80, actualResultWithFuncDel);
         }
     }
 }
