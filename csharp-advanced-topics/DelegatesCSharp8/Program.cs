329c0744077eb13eb42004fe888910ac3645a030
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2024-03-08 12:00:35 +0100

    The most important changes include the creation of a new project file `Delegates.csproj` with specific settings:
    1. A new `Program.cs` file was created in the `Delegates` namespace. This file contains a `Program` class with a `Main` method. Inside the `Main` method, two delegate instances are created: `printNumbers` of type `Action<double, double>` and `multiply` of type `Func<double, double, double>`. The `printNumbers` delegate is assigned to the `ConsolePrinter.PrintString` method and is invoked with the arguments `2` and `3`. The `multiply` delegate is assigned to the `MultiplicationUtility.MultiplyTwoNumbers` method and its result is printed to the console. The `MultiplicationUtility` and `ConsolePrinter` static classes are also defined in the `Program` class.

---
+++ csharp-advanced-topics/DelegatesCSharp8/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Delegates
+{
+    class Program
+    {
+        static class MultiplicationUtility
+        {
+            public static double MultiplyTwoNumbers(double a, double b) => a * b;
+        }
+
+        static class ConsolePrinter
+        {
+            public static void PrintString(double num1, double num2) =>
+                Console.WriteLine($"The values are: {num1} and {num2}");
+        }
+
+        static void Main(string[] args)
+        {
+            Action<double, double> printNumbers = ConsolePrinter.PrintString;
+            printNumbers(2, 3); // The values are: 2 and 3
+
+            Func<double, double, double> multiply = MultiplicationUtility.MultiplyTwoNumbers;
+            Console.WriteLine(multiply(2, 3)); // 6
+        }
+    }
+}
