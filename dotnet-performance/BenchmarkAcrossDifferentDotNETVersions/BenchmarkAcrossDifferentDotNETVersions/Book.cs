466b01cc57c6bb18582546273db1a2fdfb2cdee1
cjaynjoku <njokujennaro@gmail.com>
2024-03-09 23:10:37 +0100

    CM-835: How to Benchmark a Method's Performance Across Different .NET Versions

---
+++ dotnet-performance/BenchmarkAcrossDifferentDotNETVersions/BenchmarkAcrossDifferentDotNETVersions/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace BenchmarkAcrossDifferentDotNETVersions;
+
+public class Book
+{
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Publisher { get; set; }
+}
---
+++ dotnet-performance/BenchmarkAcrossDifferentDotNETVersions/BenchmarkAcrossDifferentDotNETVersions/BookService.cs
@@ -0,0 +1,12 @@
+﻿namespace BenchmarkAcrossDifferentDotNETVersions;
+
+public class BookService
+{
+    public static List<Book> GetBooks()
+        => new()
+        {
+            new() { Title = "Measuring Time", Author = "Helon Habila", Publisher = "W. W. Norton & Company" },
+            new() { Title = "Americanah", Author = "Chimamanda Adichie", Publisher = "Alfred Knopf" },
+            new() { Title = "Things Fall Apart", Author = "Chinua Achebe", Publisher = "William Heinemann Ltd." }
+        };
+}
