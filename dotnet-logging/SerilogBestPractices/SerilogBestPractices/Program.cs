0afa394412ca5acf13d6abf07d16f5a366245ed3
Ivan Gechev <ivan_banov@abv.bg>
2024-03-10 14:36:05 +0200

    wrap

--- dotnet-logging/SerilogBestPractices/SerilogBestPractices/Program.cs
+++ dotnet-logging/SerilogBestPractices/SerilogBestPractices/Program.cs
@@ -1,18 +1,24 @@
+using Serilog;
+using SerilogBestPractices.Enrichers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging(options
+  => options.EnrichDiagnosticContext = RequestEnricher.LogAdditionalInfo);
 
 app.UseHttpsRedirection();
 
