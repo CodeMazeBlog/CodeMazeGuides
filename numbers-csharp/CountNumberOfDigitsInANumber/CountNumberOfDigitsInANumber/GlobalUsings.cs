b391f639a56c087bc0564b18f9af12530ae0162b
alessandro0108 <alessandro0108@gmail.com>
2024-03-10 21:46:28 +0100

    Style changes

--- numbers-csharp/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/GlobalUsings.cs
+++ numbers-csharp/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using CountNumberOfDigitsInANumber;
 global using CountNumberOfDigitsInANumber.Benchmark;
 global using System.Numerics;
+global using System.Runtime.CompilerServices;
+global using System.Runtime.InteropServices;
