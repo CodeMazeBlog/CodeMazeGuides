8d351060b1f0aec0910a902069ac0ed94c2426a8
Ivan Gechev <ivan_banov@abv.bg>
2024-03-10 14:40:53 +0200

    Tests

--- dotnet-logging/SerilogBestPractices/SerilogBestPractices/Program.cs
+++ dotnet-logging/SerilogBestPractices/SerilogBestPractices/Program.cs
@@ -48,3 +48,5 @@ internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+public partial class Program { }
