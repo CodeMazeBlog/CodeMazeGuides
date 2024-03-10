33e8e0baa7695159224f1836444e9158fff3fbf0
Kai Westerschwiensterdt <K.Westerschwiensterdt@beckhoff.com>
2024-03-10 23:23:47 +0100

    Add project logic

--- collections-csharp/UpdateItemInListCSharp/src/UpdateItemInListCSharp/Program.cs
+++ collections-csharp/UpdateItemInListCSharp/src/UpdateItemInListCSharp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UpdateItemInListCSharp.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var library = new Library();
+        var book = library.AddBook("J.K. Rowling", "978-0545010221", false, "Harry Potter and the Deathly Hallows");
+
+        library.CheckoutBookFind("978-0545010221");
+        library.CheckoutBookFindIndex("978-0545010221");
+        library.CheckoutBookFirstOrDefault("978-0545010221");
+        library.CheckoutBookForeach("978-0545010221");
+        library.CheckoutBookSingleOrDefault("978-0545010221");
+        library.CheckoutBookIndexOf(book);
+    }
+}
