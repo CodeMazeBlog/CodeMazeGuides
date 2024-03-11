5f2e537c6042b2fdac66b9df5b592abec5d4b745
Osman Sokuoğlu <osman.sokuoglu@gmail.com>
2024-03-11 22:39:47 +0300

    CM-988 : How to Validate a GUID in C#

---
+++ csharp-classes-struct-record/HowToValidateGuid/HowToValidateGuid/GuidHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HowToValidateGuid;
+
+public class GuidHelper
+{
+    public static bool ValidateWithRegex(string input)
+    {
+        const string pattern = @"^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$";
+
+        return Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase);
+    }
+
+    public static bool ValidateWithGuidParse(string input)
+    {
+        try
+        {
+            Guid.Parse(input);
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static bool ValidateWithGuidParseExact(string input, string format)
+    {
+        try
+        {
+            Guid.ParseExact(input, format);
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static bool ValidateWithGuidTryParse(string input)
+    {
+        return Guid.TryParse(input, out Guid _);
+    }
+
+    public static bool ValidateWithGuidTryParseExact(string input, string format)
+    {
+        return Guid.TryParseExact(input, format, out Guid _);
+    }
+
+    public static bool ValidateWithNewGuid(string input)
+    {
+        try
+        {
+            var _ = new Guid(input);
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
