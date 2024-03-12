7f6c1fa3f707593bf088f65ab59f7da8c53f52f3
Ivan Gechev <ivan_banov@abv.bg>
2024-03-12 13:39:30 +0200

    structured logging bit update

--- dotnet-logging/SerilogBestPractices/SerilogBestPractices/Program.cs
+++ dotnet-logging/SerilogBestPractices/SerilogBestPractices/Program.cs
@@ -1,52 +1,73 @@
 using Serilog;
 using SerilogBestPractices.Enrichers;
+using SerilogBestPractices.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(
+        "logs/log.txt",
+        retainedFileCountLimit: 21,
+        rollingInterval: RollingInterval.Day)
+    .MinimumLevel.Information()
+    .CreateLogger();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Host.UseSerilog((context, config) =>
-    config.ReadFrom.Configuration(context.Configuration));
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Host.UseSerilog((context, config) =>
+        config.ReadFrom.Configuration(context.Configuration));
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseSerilogRequestLogging(options
+      => options.EnrichDiagnosticContext = RequestEnricher.LogAdditionalInfo);
+
+    app.UseHttpsRedirection();
+
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    app.MapGet("/weatherforecast", (ILoggerFactory factory) =>
+    {
+        var logger = factory.CreateLogger("GetWeatherForecast");
+
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+
+        logger.LogInformation(
+            "The weather today will be {Summary} and {Temperature} degrees.",
+            forecast[0].Summary,
+            forecast[0].TemperatureC);
+
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+    app.Run();
 }
-
-app.UseSerilogRequestLogging(options
-  => options.EnrichDiagnosticContext = RequestEnricher.LogAdditionalInfo);
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
+catch (Exception ex)
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    Log.Error(
+        "The following {Exception} was thrown during application startup",
+        ex);
 }
 
 public partial class Program { }
