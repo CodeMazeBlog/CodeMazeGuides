7c42036918f647af18b22932c4c8e7ad1bd485b8
Ehab Shaker <ehab-shaker319@outlook.com>
2024-03-12 16:56:38 +0300

    Adding main project classes and controller

---
+++ aspnetcore-features/ValidatingFileUploadExtension/ValidatingFileUploadExtension/FileFormats/FileFormatDescriptor.cs
@@ -0,0 +1,36 @@
+﻿using ValidatingFileUploadExtension;
+
+public abstract class FileFormatDescriptor
+{
+    protected FileFormatDescriptor()
+    {
+        Initialize();
+        MaxMagicNumberLength = MagicNumbers.Max(m => m.Length);
+    }
+
+    protected abstract void Initialize();
+
+    protected HashSet<string> Extensions { get; } = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+    protected List<byte[]> MagicNumbers { get; } = [];
+    protected int MaxMagicNumberLength { get; }
+    protected string TypeName { get; set; }
+
+    public bool IsIncludedExtention(string extention) => Extensions.Contains(extention);
+
+    public Result Validate(IFormFile file)
+    {
+        using var stream = file.OpenReadStream();
+        Span<byte> initialBytes = stackalloc byte[MaxMagicNumberLength];
+        int Readbytes = stream.Read(initialBytes);
+
+        foreach (var magicNumber in MagicNumbers)
+        {
+            if (initialBytes[..magicNumber.Length].SequenceCompareTo(magicNumber) == 0)
+            {
+                return new Result(true, Status.GENUINE, $"{Status.GENUINE} {TypeName}");
+            }
+        }
+
+        return new Result(false, Status.FAKE, $"{Status.FAKE} {TypeName}!");
+    }
+}
