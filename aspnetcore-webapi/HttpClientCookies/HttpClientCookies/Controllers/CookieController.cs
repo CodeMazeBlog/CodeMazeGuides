d842cefb260705ea9fef73ef906a50cfd7544ae5
thisisalin <alintroncea@gmail.com>
2024-03-14 20:49:35 +0200

    CM-476: How to add a cookie to an HTTP Client Request

---
+++ aspnetcore-webapi/HttpClientCookies/HttpClientCookies/Controllers/CookieController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientCookies.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CookieController : ControllerBase
+    {
+        [HttpGet(nameof(GetCookie))]
+        public IActionResult GetCookie()
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddHours(1);
+            options.Secure = true;
+            options.Domain = "example.com";
+
+            Response.Cookies.Append("SimpleCookie", "RHsMeXPsMK", options);
+            return Ok();
+        }
+    }
+}
