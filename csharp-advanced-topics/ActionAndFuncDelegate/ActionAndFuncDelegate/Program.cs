37d7b2a47e96b1ac2ee6bf1a26f82a70bd489b70
Joshua <79511167+Allenkeys@users.noreply.github.com>
2024-03-16 08:14:03 +0100

    Initial commit
    - Added boilerplate files and gitignore

---
+++ csharp-advanced-topics/ActionAndFuncDelegate/ActionAndFuncDelegate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ActionAndFuncDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
