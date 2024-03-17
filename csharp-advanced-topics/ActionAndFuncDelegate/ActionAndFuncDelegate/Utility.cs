ad9ee7a9e6e6c1e6dc779d7fc1f64ee670dfc86e
Joshua <79511167+Allenkeys@users.noreply.github.com>
2024-03-17 10:33:13 +0100

    Added utility class

---
+++ csharp-advanced-topics/ActionAndFuncDelegate/ActionAndFuncDelegate/Utility.cs
@@ -0,0 +1,26 @@
+﻿namespace ActionAndFuncDelegate;
+
+public class Utility
+{
+    private static readonly List<string> _data = new List<string>
+    {
+        "Keneth",
+        "Ruth",
+        "Harryson",
+        "Jason",
+        "Josh",
+        "Janelle",
+        "Davidson"
+    };
+
+    public void DisplayAll(Action<List<string>> action)
+    {
+        action(_data);
+    }
+        
+    public string GetByCondition(Func<List<string>, string> func)
+    {
+        return func(_data);
+    }
+}
+
