d0356f006b2c1e6c7d553b81f0126de62303fa64
Joshua <79511167+Allenkeys@users.noreply.github.com>
2024-03-17 10:33:58 +0100

    Implemented utility class methods in the program class

--- csharp-advanced-topics/ActionAndFuncDelegate/ActionAndFuncDelegate/Program.cs
+++ csharp-advanced-topics/ActionAndFuncDelegate/ActionAndFuncDelegate/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Utility utility = new Utility();
+
+            string name = utility.GetByCondition(Get);
+
+            string anotherName = utility.GetByCondition(x => x.FirstOrDefault(n => n == "Janelle"));
+
+            utility.DisplayAll(PrintAll);
+
+            utility.DisplayAll(x => x.ForEach(a => Console.WriteLine(a)));
+        }
+
+
+        public static void PrintAll(List<string> items) 
+        { 
+            items.ForEach(x => Console.WriteLine(x)); 
+        }
+
+        public static string Find(string name)
+        {
+            return name;
+        }
+
+        public static void Display(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        public static string Get(List<string> items)
+        {
+            string request = "Jason";
+
+            string name = "Not Found";
+
+            foreach (var item in items)
+            {
+                if (item.Equals(request))
+                    return name = item;
+            }
+            return name;
         }
     }
 }
