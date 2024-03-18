042c2bf366a85e1dc9d0dfd2f6e7302951ffb089
joydipkanjilal <joydipkanjilal@yahoo.com>
2024-03-18 18:48:09 +0530

    Naming convension for unit tests applied.

--- csharp-intermediate-topics/DelegatesInCsharp/Tests/Tests.cs
+++ csharp-intermediate-topics/DelegatesInCsharp/Tests/Tests.cs
@@ -7,14 +7,14 @@ namespace Tests
         private const string TEXT_IN_UPPER_CASE = "PROGRAMMING DELEGATES IN CSHARP";
 
         [Fact]
-        public void IsTextInLowerCaseValid_ReturnTrue()
+        public void Given_TextInLowerCase_Then_DelegateReturnsTextInUpperCase()
         {
             var textInLowerCase = DelegateExample.GetTextInLowercase(TEXT_IN_UPPER_CASE);
             Assert.True(TEXT_IN_UPPER_CASE.ToLower().Equals(textInLowerCase), "Error converting text to lower case.");
         }
 
         [Fact]
-        public void IsHraValid_ReturnTrue()
+        public void When_HraIsValid_Then_DelegateReturnTrue()
         {
             var hra = DelegateExample.GetHra(1000.00);
             Assert.True(hra == 400.00, "Error in hra calculation.");
