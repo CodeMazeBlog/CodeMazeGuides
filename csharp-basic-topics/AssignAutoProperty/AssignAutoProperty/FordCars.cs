25a73a1255831331cd2a33e85df40fe68bad380d
kundank2024 <kundan.sriv@gmail.com>
2024-03-19 21:21:16 +0530

    AssignAutoProperty-CM-869-New

---
+++ csharp-basic-topics/AssignAutoProperty/AssignAutoProperty/FordCars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignAutoProperty
+{
+    /// <summary>
+    /// Class to set initial value with DefaultValue attribute
+    /// </summary>
+    public class FordCars
+    {
+        private string? _color = "Red";
+
+        [DefaultValue("Blue")] // Set default value using DefaultValueAttribute
+        public string? Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+    }
+}
---
+++ csharp-basic-topics/AssignAutoProperty/Tests/AutoPropertyAssignTest.cs
@@ -0,0 +1,45 @@
+using AssignAutoProperty;
+using System.ComponentModel;
+
+namespace Tests
+{
+    [TestClass]
+    public class AutoPropertyAssignTest
+    {
+        [TestMethod]
+        public void GivenInitialValueWithInlineInitialization_WhenPropertyAccessed_ThenExpectAssignedValue()
+        {
+            var hondaCars = new HondaCars();
+
+            Assert.AreEqual("White", hondaCars.Color);
+            Assert.AreEqual(500000.00m, hondaCars.Cost);
+        }
+
+        [TestMethod]
+        public void GivenInitialValueWithConstructorInitialization_WhenPropertyAccessed_ThenExpectAssignedValue()
+        {
+            var toyotaCars = new ToyotaCars();
+
+            Assert.AreEqual("Black", toyotaCars.Color);
+            Assert.AreEqual(400000.00m, toyotaCars.Cost);
+        }
+
+        [TestMethod]
+        public void GivenInitialValueWithPropertySetter_WhenPropertyAccessed_ThenExpectAssignedValue()
+        {
+            var kiaCars = new KiaCars();
+
+            Assert.AreEqual("Silver", kiaCars.Color);
+            Assert.AreEqual(600000.00m, kiaCars.Cost);
+        }
+        [TestMethod]
+        public void GivenInitialValueWithDefaultValueAttribute_WhenPropertyAccessed_ThenExpectAssignedValue()
+        {
+            var fordCars = new FordCars();
+            DefaultValueAttribute attribute = (DefaultValueAttribute)TypeDescriptor.GetProperties(fordCars)["Color"].Attributes[typeof(DefaultValueAttribute)];
+            string? defaultValue = (string)(attribute.Value);
+
+            Assert.AreEqual("Blue", defaultValue);
+        }
+    }
+}
