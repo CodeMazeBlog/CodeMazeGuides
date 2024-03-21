529add2714e767bb88fb3e582ab158f9ef7a1530
Florian Prinz (FLPR) <flpr@simcorp.com>
2024-03-21 23:21:31 +0100

    Fixed spacing in program output

--- csharp-basic-topics/AssignInitialValueToAutoProperty/AssignInitialValueToAutoProperty/Program.cs
+++ csharp-basic-topics/AssignInitialValueToAutoProperty/AssignInitialValueToAutoProperty/Program.cs
@@ -4,11 +4,11 @@ using System.ComponentModel;
 var hondaCars = new HondaCars();
 Console.WriteLine("===========Inline initialization==========");
 Console.WriteLine($"Car Color: {hondaCars.Color}");
-Console.WriteLine($"Car Cost:  {hondaCars.Cost}");
+Console.WriteLine($"Car Cost: {hondaCars.Cost}");
 
 var toyotaCars = new ToyotaCars();
 Console.WriteLine("======Constructor initialization==========");
-Console.WriteLine($"Car Color:{toyotaCars.Color}");
+Console.WriteLine($"Car Color: {toyotaCars.Color}");
 Console.WriteLine($"Car Cost: {toyotaCars.Cost}");
 
 var kiaCars = new KiaCars();
