8df22659832edc29366ccfe96f217182e6c3edb1
cjaynjoku <njokujennaro@gmail.com>
2024-03-22 09:51:59 +0100

    CM-1042:Build a URL Shortener in .NET

---
+++ aspnetcore-webapi/DotNetURLShortener/DotNetURLShortener/Controllers/URLShortenerController.cs
@@ -0,0 +1,40 @@
+﻿using DotNetURLShortener.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortenerWebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class URLShortenerController(IUrlShortenerService urlShortenerService) : ControllerBase
+{
+    private readonly IUrlShortenerService _urlShortenerService = urlShortenerService;
+
+    [HttpGet("/{shortCode}")]
+    public IActionResult RedirectShortUrl(string shortCode)
+    {
+        var longUrl = _urlShortenerService.GetLongUrl(shortCode);
+        if (longUrl is null)
+        {
+            return NotFound();
+        }
+
+        return Redirect(longUrl);
+    }
+
+    [HttpGet("/samplepage")]
+    public IActionResult GetSamplePage()
+    {
+        return Ok("This is a sample page to test our URL shortener.");
+    }
+
+    [HttpPost("/createshorturl")]
+    public IActionResult ShortenUrl([FromBody] string longUrl)
+    {
+        if (!Uri.TryCreate(longUrl, UriKind.Absolute, out _))
+        {
+            return BadRequest("This is not a valid URL");
+        }
+
+        return Ok(_urlShortenerService.GetShortCode(longUrl));
+    }
+}
