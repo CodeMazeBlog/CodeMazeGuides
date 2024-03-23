8ede84408bcf18727fa099f58f7d4ad6a86427f8
Georgi Georgiev <georgy.georgiev93@gmail.com>
2024-03-23 11:36:14 +0200

    #CM-953 Difference between <out T> and <T> in Generics in C#

---
+++ csharp-intermediate-topics/DifferenceBetweenInAndOutInGenerics/DifferenceBetweenInAndOutInGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using DifferenceBetweenInAndOutInGenerics.Consumers;
+using DifferenceBetweenInAndOutInGenerics.Messages;
+using DifferenceBetweenInAndOutInGenerics.Producers;
+
+IProducer<BaseMessage> producer = new Producer<SubMessage>();
+
+var message = producer.Produce();
+
+IConsumer<SubMessage> consumer = new Consumer<BaseMessage>();
+
+var subMessage = new SubMessage();
+consumer.Consume(subMessage);
