35c7fab30a21164cf6e8a7789a22d26ce5c286db
Ansa Baby <81709896+AnsaBaby@users.noreply.github.com>
2024-03-23 17:10:15 +0000

    CM-466
    CM-466: How to Serialize Object with Root Name in C#

---
+++ json-csharp/SerializationWithRootName/SerializationWithRootName/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace SerializationWithRootName
+{
+	public class Car
+	{
+		public string? Make { get; set; }
+		public string? Model { get; set; }
+		public int Year { get; set; }
+	}
+}
+
+
