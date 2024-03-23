52fec28929645277fa06dda3687e02bd494e472e
Georgi Georgiev <georgy.georgiev93@gmail.com>
2024-03-23 12:26:48 +0200

    remove redundant method

--- csharp-intermediate-topics/DifferenceBetweenInAndOutInGenerics/DifferenceBetweenInAndOutInGenerics/MessageEditors/MessageEdditor.cs
+++ csharp-intermediate-topics/DifferenceBetweenInAndOutInGenerics/DifferenceBetweenInAndOutInGenerics/MessageEditors/MessageEdditor.cs
@@ -4,11 +4,6 @@ using Messages;
 
 public class MessageEdditor<TMessage> : IMessageEdditor<TMessage> where TMessage : BaseMessage, new()
 {
-    public void EditOriginalMessage(TMessage message)
-    {
-        // edit message
-    }
-
     public TMessage EditAndCopyOriginalMessage(TMessage message)
     {
         var newMessage = new TMessage();
