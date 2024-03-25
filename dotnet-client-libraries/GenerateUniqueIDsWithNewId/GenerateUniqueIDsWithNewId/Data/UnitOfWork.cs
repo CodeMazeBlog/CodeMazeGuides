e2e0b0ff1a80305fb51af2b8d71651fb00365276
Ivan Gechev <ivan_banov@abv.bg>
2024-03-25 20:11:24 +0200

    CM-1063: Generate Sortable Unique IDs With the NewId Library in .NET (#1905)
    * initialcommit
    
    * Base is done
    
    * Tests

---
+++ dotnet-client-libraries/GenerateUniqueIDsWithNewId/GenerateUniqueIDsWithNewId/Data/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using GenerateUniqueIDsWithNewId.Data.Repositories;
+using GenerateUniqueIDsWithNewId.Data.Repositories.Abstractions;
+
+namespace GenerateUniqueIDsWithNewId.Data;
+
+public class UnitOfWork(OrdersDbContext context) : IUnitOfWork
+{
+    public IOrderRepository OrderRepository { get; } = new OrderRepository(context);
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+     => await context.SaveChangesAsync(cancellationToken);
+}
