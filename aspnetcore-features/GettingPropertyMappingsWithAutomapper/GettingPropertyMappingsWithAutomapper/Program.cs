c492ff684dcff912fca3dd7ae7e176974da87301
gangzhucoll <gangzhucoll@noreply.gitcode.com>
2024-03-29 23:53:53 +0800

    Uploading code

---
+++ aspnetcore-features/GettingPropertyMappingsWithAutomapper/GettingPropertyMappingsWithAutomapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using AutoMapper.Internal;
+using GettingPropertyMappingsWithAutomapper;
+
+
+var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MyProfile()); });
+
+var mapper = config.CreateMapper();
+var source = new SourceClass { Name = "Jack", Age = 20 };
+var destination = mapper.Map<SourceClass, DestinationClass>(source);
+Console.WriteLine($"Name: {destination.FullName}, Age: {destination.YearsOld}");
+
+var typeMaps = mapper.ConfigurationProvider.Internal().GetAllTypeMaps();
+foreach (var typeMapType in typeMaps)
+{
+    Console.WriteLine($"Source Type: {typeMapType.SourceType.Name}");
+    Console.WriteLine($"Destination Type: {typeMapType.DestinationType.Name}");
+    foreach (var memberMap in typeMapType.MemberMaps)
+    {
+        var sourceProperty = memberMap.SourceMember.Name;
+        Console.WriteLine($"Source Property: {sourceProperty}");
+        Console.WriteLine($"Destination Property: {memberMap}");
+    }
+}
+
---
+++ aspnetcore-features/GettingPropertyMappingsWithAutomapper/GettingPropertyMappingsWithAutomapper/SourceClass.cs
@@ -0,0 +1,7 @@
+﻿namespace GettingPropertyMappingsWithAutomapper;
+
+public class SourceClass
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
