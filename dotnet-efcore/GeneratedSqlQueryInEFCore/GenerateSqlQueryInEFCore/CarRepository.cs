e5d54f0916e5345f09f4f032012bab806d751b2a
John <ademolajhon@gmail.com>
2024-03-29 02:15:28 +0100

    Added db contexts, migrations and unit tests

---
+++ dotnet-efcore/GeneratedSqlQueryInEFCore/GenerateSqlQueryInEFCore/CarRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GenerateSqlQueryInEFCore;
+
+public class CarRepository : ICarRepository
+{
+    readonly ApiContext _context;
+
+    public CarRepository(ApiContext context)
+    {
+        _context = context;
+    }
+
+    public List<Car> GetCars()
+    {
+        var cars = _context.Cars;
+        Console.WriteLine(cars.ToQueryString());
+
+        return cars.ToList();
+    }
+}
--- dotnet-efcore/GeneratedSqlQueryInEFCore/GeneratedSqlQueryInEFCore/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-public class Program
-{
-    public static void Main(string[] args)
-    {
-
-    }
-}
