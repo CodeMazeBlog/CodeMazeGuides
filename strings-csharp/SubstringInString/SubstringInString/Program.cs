21653c8e16d051447e31b5d68879130d50670f8b
Lynda nneka <anaedobelyn001@gmail.com>
2024-04-01 18:28:24 +0100

    Updated How to Find All the Positions of a Substring in Another String in C#

---
+++ strings-csharp/SubstringInString/SubstringInString/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using SubstringInString;
+
+SubstringMethod.Substringmethod();
+IndexOfMethod.Indexofmethod();
+RegexMethod.Regexmethod();
+LinqMethod.Linqmethod();
+SplitMethod.Splitmethod();
+
+var summary = BenchmarkRunner.Run<Benchmark>();
+
+// Output summary
+Console.WriteLine(summary);
---
+++ strings-csharp/SubstringInString/Tests/SubstringTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Tests
+{
+    [TestFixture]
+    public class SubstringMethodTests
+    {
+        [Test]
+        public void Given_UserInput_When_Substringmethod_Called_Then_ExpectedOutput()
+        {
+            
+            var input = "This is a substring method for searching for substrings in a string";
+            var substring = "sub";
+            var expectedOutput = $"Found 'sub' in 'This is a substring method for searching for substrings in a string' at position 10\r\n" +
+                     $"Found 'sub' in 'This is a substring method for searching for substrings in a string' at position 45\r\n" +
+                     $"Total occurrences found: 2\r\n";
+
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                // Act
+                SubstringMethod.Substringmethod();
+
+                // Assert
+                Assert.AreEqual(expectedOutput, sw.ToString());
+            }
+        }
+    }
+}
