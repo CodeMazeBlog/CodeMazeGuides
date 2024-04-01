4585ecd55f86e024e6782fe8b82c95c22b207957
Ivan Gechev <ivan_banov@abv.bg>
2024-04-01 20:54:26 +0300

    Base of the article is done

--- dotnet-client-libraries/IntroductionToCarter/IntroductionToCarter/Program.cs
+++ dotnet-client-libraries/IntroductionToCarter/IntroductionToCarter/Program.cs
@@ -1,13 +1,18 @@
+using Carter;
 using IntroductionToCarter.Contracts;
 using IntroductionToCarter.Data;
 using IntroductionToCarter.Services;
 using IntroductionToCarter.Services.Abstractions;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCarter();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IBookService, BookService>();
@@ -17,6 +22,8 @@ builder.Services.AddDbContext<BooksDbContext>(options =>
 
 var app = builder.Build();
 
+app.MapCarter();
+
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
@@ -24,45 +31,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.MapGet("/books", async (IBookService service) =>
-{
-	var books = await service.GetAllAsync();
-
-	return Results.Ok(books);
-})
-.WithOpenApi();
-
-app.MapGet("/books/{id:guid}", async (Guid id, IBookService service) =>
-{
-	var book = await service.GetByIdAsync(id);
-
-	return Results.Ok(book);
-})
-.WithOpenApi();
-
-app.MapPost("/books", async (CreateBookRequest request, IBookService service) =>
-{
-	var book = await service.CreateAsync(request);
-
-	Results.Created($"/books/{book.Id}", book);
-})
-.WithOpenApi();
-
-app.MapPut("/books/{id:guid}", async (Guid id, UpdateBookRequest request, IBookService service) =>
-{
-	await service.UpdateAsync(id, request);
-
-	return Results.NoContent();
-})
-.WithOpenApi();
-
-app.MapDelete("/books/{id:guid}", async (Guid id, IBookService service) =>
-{
-	await service.DeleteAsync(id);
-
-	return Results.NoContent();
-})
-.WithOpenApi();
 
 app.Run();
