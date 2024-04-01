13e728bc8227c318161c0f6f00d53f9047374f55
Lynda nneka <anaedobelyn001@gmail.com>
2024-04-01 19:28:17 +0100

    CM-930 How to Find All the Positions of a Substring in Another String in C#

---
+++ strings-csharp/SubstringInString/SubstringInString/RegexMethod.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SubstringInString
+{
+    public static class RegexMethod
+    {
+        public static void Regexmethod() 
+        {
+            var input = "In programming, understanding the logic behind the code," +
+                " writing efficient code, debugging code errors, " +
+                "and documenting the code properly are essential skills for any developer.";
+
+            var pattern = @"code";
+
+            var matches = Regex.Matches(input, pattern, RegexOptions.IgnoreCase);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine("substring found at index " + match.Index + ": " + match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+        }
+    }
+}
