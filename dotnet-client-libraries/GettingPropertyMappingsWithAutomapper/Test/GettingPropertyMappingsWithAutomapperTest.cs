c9d0a873f70e4926da6cc7849324f7a16e084c03
gangzhucoll <gangzhucoll@noreply.gitcode.com>
2024-04-04 15:49:51 +0800

    update unit test

--- dotnet-client-libraries/GettingPropertyMappingsWithAutomapper/Test/GettingPropertyMappingsWithAutomapperTest.cs
+++ dotnet-client-libraries/GettingPropertyMappingsWithAutomapper/Test/GettingPropertyMappingsWithAutomapperTest.cs
@@ -3,10 +3,30 @@ namespace Test
     [TestClass]
     public class GettingPropertyMappingsWithAutomapperTest
     {
-        [TestMethod]
-        public void WhenProgrammeExecutionThenCorrectOutputLiveTest()
+        private IMapper mapper = null;
+        public GettingPropertyMappingsWithAutomapperTest()
         {
+            var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MyProfile()); });
+            mapper = config.CreateMapper();
+        }
+
+        [TestMethod]
+        public void WhenSourceMapsToDestinationThenEqualValuesUnitTest()
+        {
+            var source = new Source { Name = "Jack", Age = 20 };
+            var destination = mapper.Map<Source, Destination>(source);
+
+            Assert.AreEqual(source.Name,destination.FullName);
+            Assert.AreEqual(source.Age,destination.YearsOld);
+        }
+
+        [TestMethod]
+        public void WhenGettingMappingsThenCorrectMappingUnitTest()
+        {
+            var typeMaps = mapper.ConfigurationProvider.Internal().GetAllTypeMaps();
             
+            Assert.AreEqual(typeMaps.First().SourceType.Name,nameof(Source));
+            Assert.AreEqual(typeMaps.First().DestinationType.Name, nameof(Destination));
         }
     }
 }
