a499ea5fab326d016bd5d01e648d4e417c14c785
kundank2024 <kundan.sriv@gmail.com>
2024-04-05 05:19:56 +0530

    How to Call a Controller Action Method From Another Controller in .NET MVC
    CM-977-How to Call a Controller Action Method From Another Controller in .NET MVC

---
+++ aspnetcore-features/CallingActionFromAnotherController/CallingActionFromAnotherController/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using CallingActionFromAnotherController.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CallingActionFromAnotherController.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("List", "Product");
+        }
+        public IActionResult OurProduct()
+        {
+            return RedirectToRoute("productlist");
+        }
+
+        public IActionResult FetchProduct()
+        {
+            return RedirectToRoute(new
+            {
+                controller = "Product",
+                action = "List"
+            });
+        }
+    }
+}
---
+++ aspnetcore-features/CallingActionFromAnotherController/CallingActionFromAnotherController/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CallingActionFromAnotherController.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
