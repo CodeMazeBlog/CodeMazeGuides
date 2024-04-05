91519a91b93810c79bcddf3a977d73d8dc202a82
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2024-04-05 10:31:29 -0400

    rename tests to given/when/then format; fix order of assert

--- dotnet-client-libraries/GettingPropertyMappingsWithAutomapper/Test/GettingPropertyMappingsWithAutomapperTest.cs
+++ dotnet-client-libraries/GettingPropertyMappingsWithAutomapper/Test/GettingPropertyMappingsWithAutomapperTest.cs
@@ -1,32 +1,32 @@
-namespace Test
+namespace Test;
+
+[TestClass]
+public class GettingPropertyMappingsWithAutomapperTest
 {
-    [TestClass]
-    public class GettingPropertyMappingsWithAutomapperTest
+    private readonly IMapper mapper;
+
+    public GettingPropertyMappingsWithAutomapperTest()
     {
-        private IMapper mapper = null;
-        public GettingPropertyMappingsWithAutomapperTest()
-        {
-            var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MyProfile()); });
-            mapper = config.CreateMapper();
-        }
+        var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MyProfile()); });
+        mapper = config.CreateMapper();
+    }
 
-        [TestMethod]
-        public void WhenSourceMapsToDestinationThenEqualValuesUnitTest()
-        {
-            var source = new Source { Name = "Jack", Age = 20 };
-            var destination = mapper.Map<Source, Destination>(source);
+    [TestMethod]
+    public void GivenMappedTypes_WhenSourceIsMappedToDestination_ThenValuesAreMappedCorrectly()
+    {
+        var source = new Source { Name = "Jack", Age = 20 };
+        var destination = mapper.Map<Source, Destination>(source);
 
-            Assert.AreEqual(source.Name,destination.FullName);
-            Assert.AreEqual(source.Age,destination.YearsOld);
-        }
+        Assert.AreEqual(source.Name, destination.FullName);
+        Assert.AreEqual(source.Age, destination.YearsOld);
+    }
 
-        [TestMethod]
-        public void WhenGettingMappingsThenCorrectMappingUnitTest()
-        {
-            var typeMaps = mapper.ConfigurationProvider.Internal().GetAllTypeMaps();
-            
-            Assert.AreEqual(typeMaps.First().SourceType.Name,nameof(Source));
-            Assert.AreEqual(typeMaps.First().DestinationType.Name, nameof(Destination));
-        }
+    [TestMethod]
+    public void GivenMappedTypes_WhenGettingAllMappedTypes_ThenMappedTypesAreCorrect()
+    {
+        var typeMaps = mapper.ConfigurationProvider.Internal().GetAllTypeMaps();
+
+        Assert.AreEqual(nameof(Source), typeMaps.First().SourceType.Name);
+        Assert.AreEqual(nameof(Destination), typeMaps.First().DestinationType.Name);
     }
 }
