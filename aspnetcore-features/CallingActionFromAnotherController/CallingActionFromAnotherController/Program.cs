71bb349222d96d32f85d9c53fed9e9d52eea14e1
kundank2024 <kundan.sriv@gmail.com>
2024-04-06 18:17:25 +0530

    removed few extra spaces

--- aspnetcore-features/CallingActionFromAnotherController/CallingActionFromAnotherController/Program.cs
+++ aspnetcore-features/CallingActionFromAnotherController/CallingActionFromAnotherController/Program.cs
@@ -1,7 +1,6 @@
 using System.Web.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
