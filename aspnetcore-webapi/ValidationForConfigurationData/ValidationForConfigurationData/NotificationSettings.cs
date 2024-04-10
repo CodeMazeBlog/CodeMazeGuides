94458efe09182d6024ecdb2184fc4615544a8016
Ivan Gechev <ivan_banov@abv.bg>
2024-04-10 18:25:45 +0300

    InitialCommit

---
+++ aspnetcore-webapi/ValidationForConfigurationData/ValidationForConfigurationData/NotificationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ValidationForConfigurationData;
+
+public class NotificationSettings
+{
+	public string Sender { get; init; }
+	public bool EnableSms { get; init; }
+	public bool EnableEmail { get; init; }
+	public int MaxNumberOfRetries { get; init; }
+}
