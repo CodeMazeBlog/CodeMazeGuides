25c43128ae53a98faed4bfa8b8ce08964c10fa6b
John <ademolajhon@gmail.com>
2024-04-10 23:58:59 +0100

    removed private field as it is not necessary

--- dotnet-efcore/GeneratedSqlQueryInEFCore/GenerateSqlQueryInEFCore/CarRepository.cs
+++ dotnet-efcore/GeneratedSqlQueryInEFCore/GenerateSqlQueryInEFCore/CarRepository.cs
@@ -4,11 +4,9 @@ namespace GenerateSqlQueryInEFCore;
 
 public class CarRepository(ApiContext context) : ICarRepository
 {
-    readonly ApiContext _context = context;
-
     public List<Car> GetCars()
     {
-        var cars = _context.Cars;
+        var cars = context.Cars;
         Console.WriteLine(cars.ToQueryString());
 
         return cars.ToList();
