9db403fd275bec94b43557d23c3576083cb1f6ca
Emmanuel Adom <79058040+Emmanuel-Adom@users.noreply.github.com>
2024-04-13 19:53:00 +0000

    Initial Commit

---
+++ csharp-images/GenerateImagesInCSharpUsingImageSharp/ImageSharpLibrary/Program.cs
@@ -0,0 +1,14 @@
+﻿using ImageSharpLibrary;
+using SixLabors.ImageSharp;
+
+string outputPath = @"outputImage.png";
+
+var imageSize = new SizeF(350, 280);
+var point1 = new PointF(50, 50);
+var point2 = new PointF(250, 50);
+var point3 = new PointF(150, 200);
+var image = ImageService.CreateBlankImage((int)imageSize.Width, (int)imageSize.Height);
+ImageService.DrawTriangleOnImage(image, point1, point2, point3);
+ImageService.SaveImage(image, outputPath);
+
+Console.WriteLine("Image generated and saved successfully.");
