9a0b386420196778bef10e27e2ece59ff228cdf1
Ademola John <AdemolaJhon@gmail.com>
2024-04-16 16:20:15 +0100

    CM-1013: How to Show the Generated SQL Query in EF Core (#1920)
    * CM-1013: How to Show the Generated SQL Query in EF Core
    
    * added program entry point
    
    * Added db contexts, migrations and unit tests
    
    * removed readable connection details
    
    * refactored constructor to primary constructor
    
    * removed private field as it is not necessary
    
    * changed http profile to https
    
    * changed URI to https
    
    ---------
    
    Co-authored-by: Crownedprinz <crownedprinz@Jays-MacBook-Pro.local>

---
+++ dotnet-efcore/GeneratedSqlQueryInEFCore/GenerateSqlQueryInEFCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using GenerateSqlQueryInEFCore;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApiContext>(opts => opts
+                .UseSqlServer(builder.Configuration["ConnectionString:CarsDB"]));
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/cars", (ICarRepository carRepository) =>
+{
+    return carRepository.GetCars();
+});
+
+app.Run();
