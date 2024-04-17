2ad4df80dc76e482cfe32eaad1e5fdc4131a6059
Ivan Gechev <ivan_banov@abv.bg>
2024-04-17 20:35:51 +0300

    final changes

--- aspnetcore-webapi/ValidationForConfigurationData/ValidationForConfigurationData/NotificationSettings.cs
+++ aspnetcore-webapi/ValidationForConfigurationData/ValidationForConfigurationData/NotificationSettings.cs
@@ -1,9 +1,16 @@
-﻿namespace ValidationForConfigurationData;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationForConfigurationData;
 
 public class NotificationSettings
 {
+	[Required]
 	public string Sender { get; init; }
+	[Required]
 	public bool EnableSms { get; init; }
+	[Required]
 	public bool EnableEmail { get; init; }
+	[Required]
+	[DeniedValues([0])]
 	public int MaxNumberOfRetries { get; init; }
 }
