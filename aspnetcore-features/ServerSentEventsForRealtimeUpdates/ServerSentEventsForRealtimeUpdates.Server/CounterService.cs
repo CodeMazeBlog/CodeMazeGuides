4f153f5940a67e8ba4728481b0f01655d960e089
fl0tschi <fl0tschi>
2024-04-19 21:12:36 +0200

    Fix counter constants and counter index page

--- aspnetcore-features/ServerSentEventsForRealtimeUpdates/ServerSentEventsForRealtimeUpdates.Server/CounterService.cs
+++ aspnetcore-features/ServerSentEventsForRealtimeUpdates/ServerSentEventsForRealtimeUpdates.Server/CounterService.cs
@@ -2,16 +2,16 @@ namespace ServerSentEventsForRealtimeUpdates.Server;
 
 public class CounterService : ICounterService
 {
-    private const int cStartValue = 30;
-    private const int cMillisecondsDelay = 1000;
+    private const int StartValueCounter = 30;
+    private const int MillisecondsDelay = 1000;
 
     public async Task CountdownDelay(CancellationToken cancellationToken)
     {
-        await Task.Delay(cMillisecondsDelay, cancellationToken);
+        await Task.Delay(MillisecondsDelay, cancellationToken);
     }
 
     public int StartValue
     {
-        get => cStartValue;
+        get => StartValueCounter;
     }
 }
