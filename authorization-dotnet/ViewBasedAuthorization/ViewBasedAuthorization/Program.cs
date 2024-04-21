dab4acc651a112fba8e77be0164a0177a6e584fb
Satya <satyap48@gmail.com>
2024-04-21 13:05:31 +0530

    CM-1040: View-Based Authorization in ASP.NET Core

---
+++ authorization-dotnet/ViewBasedAuthorization/ViewBasedAuthorization/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ViewBasedAuthorization.Data;
+
+namespace ViewBasedAuthorization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<DocumentContext>(options =>
+        {
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = true;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<DocumentContext>();
+
+        builder.Services.ConfigureApplicationCookie(options => options.LoginPath = "/Account/Login");
+
+        builder.Services.AddAuthorizationBuilder()
+            .AddPolicy("EditDocument", policy => policy.RequireClaim("Permission", "CanEdit"))
+            .AddPolicy("DeleteDocument", policy => policy.RequireRole("Admin"));
+
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetService(typeof(UserManager<IdentityUser>)) as UserManager<IdentityUser>;
+            var roleManager = scope.ServiceProvider.GetService(typeof(RoleManager<IdentityRole>)) as RoleManager<IdentityRole>;
+
+            DataInitializer.SeedData(userManager, roleManager);
+        }
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
