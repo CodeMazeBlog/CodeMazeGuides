c7806052c15d0f9e3440861493082738ebc8156c
notarandomcoder <=>
2024-04-22 00:46:16 +0600

    Adding custom properties to Serilog templates

---
+++ dotnet-logging/CustomPropertiesWithSerilogOutputTemplates/CustomPropertiesWithSerilog/CustomPropertiesWithSerilog/CustomPropertiesFromContextEnricher.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+using Serilog.Context;
+
+namespace CustomPropertiesWithSerilog;
+
+public class CustomPropertiesFromContextEnricher
+{
+    private readonly ILogger _logger;
+
+    public CustomPropertiesFromContextEnricher(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void EnrichFromContextPushProperty(string userId)
+    {
+        LogContext.PushProperty("UserId", userId);
+        _logger.Information("User Tried to Login");
+    }
+
+    public void EnrichFromContextPushPropertyScoped(string userId)
+    {
+        using (LogContext.PushProperty("UserId", userId))
+        {
+            _logger.Information("User Tried to Login");
+        }
+    }
+
+    public void EnrichFromContextForContext(string userId)
+    {
+        _logger.ForContext("UserId", userId).Information("User tried to login");
+    }
+}
---
+++ dotnet-logging/CustomPropertiesWithSerilogOutputTemplates/CustomPropertiesWithSerilog/CustomPropertiesWithSerilog/CustomPropertiesWithPropertyEnricher.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+
+namespace CustomPropertiesWithSerilog;
+
+public class CustomPropertiesWithPropertyEnricher
+{
+    private readonly ILogger _logger;
+
+    public CustomPropertiesWithPropertyEnricher(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogUsingPropertyEnrichedLogger()
+    {
+        _logger.Information("Application Started");
+    }
+}
