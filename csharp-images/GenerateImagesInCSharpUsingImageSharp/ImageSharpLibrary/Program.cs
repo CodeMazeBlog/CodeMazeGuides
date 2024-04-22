282fd78077936c559c23926515bad92c0625559d
jshergal <1489638+jshergal@users.noreply.github.com>
2024-04-22 15:09:33 +0300

    Removed unnecessary size variable

--- csharp-images/GenerateImagesInCSharpUsingImageSharp/ImageSharpLibrary/Program.cs
+++ csharp-images/GenerateImagesInCSharpUsingImageSharp/ImageSharpLibrary/Program.cs
@@ -3,12 +3,13 @@ using SixLabors.ImageSharp;
 
 string outputPath = @"outputImage.png";
 
-var imageSize = new SizeF(350, 280);
+using var image = ImageService.CreateBlankImage(width: 350, height: 280);
+
 var point1 = new PointF(50, 50);
 var point2 = new PointF(250, 50);
 var point3 = new PointF(150, 200);
-var image = ImageService.CreateBlankImage((int)imageSize.Width, (int)imageSize.Height);
 ImageService.DrawTriangleOnImage(image, point1, point2, point3);
+
 ImageService.SaveImage(image, outputPath);
 
 Console.WriteLine("Image generated and saved successfully.");
