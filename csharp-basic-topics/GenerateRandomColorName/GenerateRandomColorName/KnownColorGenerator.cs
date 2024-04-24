177e13074e570de12238cb9d2e87f13f1b1b7644
Ellie <ellieslife@icloud.com>
2024-04-24 17:31:13 -0300

    Fix - naming convention _allColors

--- csharp-basic-topics/GenerateRandomColorName/GenerateRandomColorName/KnownColorGenerator.cs
+++ csharp-basic-topics/GenerateRandomColorName/GenerateRandomColorName/KnownColorGenerator.cs
@@ -3,12 +3,12 @@ using System.Drawing;
 
 public static class KnownColorGenerator
 {
-    private static readonly KnownColor[] allColors = Enum.GetValues<KnownColor>();
+    private static readonly KnownColor[] _allColors = Enum.GetValues<KnownColor>();
 
     public static Color GetRandomKnownColor()
     {
-        int index = Random.Shared.Next(allColors.Length);
-        KnownColor randomColorName = allColors[index];
+        int index = Random.Shared.Next(_allColors.Length);
+        KnownColor randomColorName = _allColors[index];
 
         return Color.FromKnownColor(randomColorName);
     }
