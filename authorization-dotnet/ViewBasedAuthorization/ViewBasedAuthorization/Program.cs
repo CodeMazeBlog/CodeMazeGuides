3deeed2588540876cdae45384cfceb292afdd2e6
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2024-04-26 17:55:25 -0400

    remove boilerplate comments

--- authorization-dotnet/ViewBasedAuthorization/ViewBasedAuthorization/Program.cs
+++ authorization-dotnet/ViewBasedAuthorization/ViewBasedAuthorization/Program.cs
@@ -4,7 +4,6 @@ using ViewBasedAuthorization.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<DocumentContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -27,11 +26,9 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
