5f8533ac425892945d42790e5e06d9771256ba2d
Semir Teskeredzic <semir.teskeredzic@gmail.com>
2024-04-28 06:52:36 +0200

    CM-984: Count the Number of Vowels in a String in C# (#1861)
    Count the Number of Vowels in a String in C#

---
+++ strings-csharp/CountNumberOfVowelsInString/CountNumberOfVowelsInString/VowelCounters.cs
@@ -0,0 +1,100 @@
+﻿using System.Buffers;
+using System.Text.RegularExpressions;
+
+namespace CountNumberOfVowelsInString;
+
+public static partial class VowelCounters
+{
+    [GeneratedRegex(@"[AEIOUaeiou]")]
+    private static partial Regex _regexVowels();
+
+    [GeneratedRegex(@"[^AEIOUaeiou]+")]
+    private static partial Regex _regexNotVowels();
+
+    public static int CountVowelsUsingForLoop(ReadOnlySpan<char> sentence, ReadOnlySpan<char> vowels)
+    {
+        var total = 0;
+
+        for (var i = 0; i < sentence.Length; i++)
+        {
+            if (vowels.Contains(sentence[i]))
+            {
+                total++;
+            }
+        }
+
+        return total;
+    }
+
+    public static int CountVowelsUsingForEachLoop(ReadOnlySpan<char> sentence, ReadOnlySpan<char> vowels)
+    {
+        var total = 0;
+
+        foreach (var c in sentence)
+        {
+            if (vowels.Contains(c))
+            {
+                total++;
+            }
+        }
+
+        return total;
+    }
+
+    public static int CountVowelsUsingSearchValues(ReadOnlySpan<char> sentence, SearchValues<char> vowelsSearchValues)
+    {
+        var total = 0;
+
+        for (var i = 0; i < sentence.Length; ++i)
+        {
+            if (vowelsSearchValues.Contains(sentence[i]))
+                ++total;
+        }
+
+        return total;
+    }
+
+    public static int CountVowelsUsingSwitchStatement(ReadOnlySpan<char> sentence)
+    {
+        var total = 0;
+
+        foreach (var c in sentence)
+        {
+            switch (c)
+            {
+                case 'A':
+                case 'a':
+                case 'E':
+                case 'e':
+                case 'I':
+                case 'i':
+                case 'O':
+                case 'o':
+                case 'U':
+                case 'u':
+                    total++;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        return total;
+    }
+
+    public static int CountVowelsUsingRegexCount(string sentence)
+    {
+        return _regexVowels().Count(sentence);
+    }
+
+    public static int CountVowelsUsingRegexReplaceAndLength(string sentence)
+    {
+        return _regexNotVowels().Replace(sentence, "").Length;
+    }
+
+    public static int CountVowelsUsingLinq(string sentence, HashSet<char> vowels)
+    {
+        return sentence.Count(x => vowels.Contains(x));
+    }
+}
