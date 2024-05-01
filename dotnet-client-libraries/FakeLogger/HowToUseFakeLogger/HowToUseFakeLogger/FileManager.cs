174c05c0fe9e8606194a08e4945321135bb8fc0a
Osman Sokuoğlu <osman.sokuoglu@gmail.com>
2024-05-01 22:14:40 +0300

    CM-1082: How to Use FakeLogger to Test Logging Code in .NET

---
+++ dotnet-client-libraries/FakeLogger/HowToUseFakeLogger/HowToUseFakeLogger/FileManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HowToUseFakeLogger;
+
+public class FileManager(ILogger logger)
+{
+    private readonly ILogger _logger = logger;
+
+    public string ReadFile(string fileName)
+    {
+        try
+        {
+            var extension = Path.GetExtension(fileName);
+            var allowedExtensions = new string[] { ".pdf", ".txt" };
+
+            if (!allowedExtensions.Contains(extension))
+            {
+                _logger.LogWarning("Invalid extension");
+                return string.Empty;
+            }
+
+            _logger.LogInformation("Reading file: {FileName}", fileName);
+
+            var filePath = Path.Combine("./files/", fileName);
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllText(filePath);
+            }
+            else
+            {
+                throw new FileNotFoundException("File not found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred");
+            return string.Empty;
+        }
+    }
+}
