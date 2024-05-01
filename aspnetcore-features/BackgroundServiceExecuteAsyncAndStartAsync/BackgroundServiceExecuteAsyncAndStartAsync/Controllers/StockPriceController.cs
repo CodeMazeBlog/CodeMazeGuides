5b0aea5a28d0256428f30d4dde4ebaa25edeab8b
Georgios Panagopoulos <geopanagopoulosz@gmail.com>
2024-05-01 12:16:25 +0100

    CM-961 Initial implementation of the BackgroundService to show how we can use ExecuteAsync() and StartAsync()

---
+++ aspnetcore-features/BackgroundServiceExecuteAsyncAndStartAsync/BackgroundServiceExecuteAsyncAndStartAsync/Controllers/StockPriceController.cs
@@ -0,0 +1,14 @@
+﻿using BackgroundServiceExecuteAsyncAndStartAsync.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackgroundServiceExecuteAsyncAndStartAsync.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockPriceController(IStockPriceRepository stockPriceRepository) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetTodayStockPrices() => Ok(await stockPriceRepository.GetTodayStockPricesAsync());
+    }
+}
