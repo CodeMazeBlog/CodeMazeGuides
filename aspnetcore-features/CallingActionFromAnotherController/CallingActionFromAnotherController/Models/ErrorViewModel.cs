a35eb34881b2123a99e7c1cf9b0912eea0eaf017
kundank2024 <162119127+kundank2024@users.noreply.github.com>
2024-05-03 13:06:19 +0530

    CM-977: How to Call a Controller Action Method From Another Controller in .NET MVC

---
+++ aspnetcore-features/CallingActionFromAnotherController/CallingActionFromAnotherController/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CallingActionFromAnotherController.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
