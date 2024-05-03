afc1a143e98020c0d4cddde8c322487aff9ae489
cjaynjoku <njokujennaro@gmail.com>
2024-05-03 16:41:28 +0100

    Changed the AreKeysAndCountEqual Method

--- collections-dictionary/CompareTwoDictionaries/CompareTwoDictionaries/DictionaryEqualityComparer.cs
+++ collections-dictionary/CompareTwoDictionaries/CompareTwoDictionaries/DictionaryEqualityComparer.cs
@@ -2,16 +2,21 @@
 
 public class DictionaryEqualityComparer
 {
-    public static bool AreKeysAndCountNotEqual<TKey, TValue>(Dictionary<TKey, TValue> dict1, Dictionary<TKey, TValue> dict2)
-        where TKey : notnull
+    public static bool AreKeysAndCountEqual<TKey, TValue>(Dictionary<TKey, TValue> dict1, Dictionary<TKey, TValue> dict2)
+        where TKey : notnull where TValue : IEquatable<TValue>
     {
-        return dict1.Count != dict2.Count || dict1.Keys.Except(dict2.Keys).Any() || dict2.Keys.Except(dict1.Keys).Any();
+        if (dict1.Count != dict2.Count) return false;
+
+        var dict1HashSet = new HashSet<TKey>(dict1.Keys);
+        var dict2HashSet = new HashSet<TKey>(dict2.Keys);
+
+        return dict1HashSet.SetEquals(dict2HashSet);
     }
 
     public static bool UseSequenceEqual<TKey, TValue>(Dictionary<TKey, TValue> dict1, Dictionary<TKey, TValue> dict2)
         where TKey : notnull where TValue : IEquatable<TValue>
     {
-        if (AreKeysAndCountNotEqual(dict1, dict2)) return false;
+        if (!AreKeysAndCountEqual(dict1, dict2)) return false;
 
         return dict1.OrderBy(kvp => kvp.Key).SequenceEqual(dict2.OrderBy(kvp => kvp.Key));
     }
@@ -19,7 +24,7 @@ public class DictionaryEqualityComparer
     public static bool UseEnumerableAll<TKey, TValue>(Dictionary<TKey, TValue> dict1, Dictionary<TKey, TValue> dict2)
         where TKey : notnull where TValue : IEquatable<TValue>
     {
-        if (AreKeysAndCountNotEqual(dict1, dict2)) return false;
+        if (!AreKeysAndCountEqual(dict1, dict2)) return false;
 
         return dict1.Keys.All(k => dict2.ContainsKey(k) && dict2[k].Equals(dict1[k]));
     }
@@ -27,7 +32,7 @@ public class DictionaryEqualityComparer
     public static bool UseForeachLoop<TKey, TValue>(Dictionary<TKey, TValue> dict1, Dictionary<TKey, TValue> dict2)
         where TKey : notnull where TValue : IEquatable<TValue>
     {
-        if (AreKeysAndCountNotEqual(dict1, dict2)) return false;
+        if (!AreKeysAndCountEqual(dict1, dict2)) return false;
 
         foreach (var kvp in dict1)
         {
