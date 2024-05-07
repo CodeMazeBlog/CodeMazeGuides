dcd449fa2106db1818f8d5a57bd172eaa6e53bea
Muhammed Saleem <muhammedsalimp@gmail.com>
2024-05-07 23:55:54 +0530

    CM-1045- Real-Time .NET Error Reporitng With Exceptionless

---
+++ dotnet-logging/Exceptionless/ExceptionlessTestClient/Program.cs
@@ -0,0 +1,31 @@
+using Exceptionless;
+
+namespace ExceptionlessClient;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddExceptionless();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.UseExceptionless();
+
+        app.Run();
+    }
+}
