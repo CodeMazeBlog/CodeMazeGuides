017e74e93ec2c012f59f5b04bcd0a00acad0e6a8
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2024-05-14 11:38:37 +0200

    CM-908: How to Filter a List Based on Another List in C Sharp (#1967)
    * CM-908: Initial commit
    
    * CM-908: Create the classes and tests
    
    * CM-908: Add slight code refactoring
    
    * Simplified the PrintList method
    
    * CM-908: Reorder some methods
    
    * CM-908: Modify the FilterUsingHashSet method
    
    * Removed WhereSimplified/renamed it Where
    
    * Fixed missing blank line before return statement
    
    ---------
    
    Co-authored-by: jshergal <1489638+jshergal@users.noreply.github.com>

---
+++ collections-lists/HowToFilterAListBasedOnAnotherListInCSharp/HowToFilterAListBasedOnAnotherListInCSharp/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace HowToFilterAListBasedOnAnotherListInCSharp;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int SchoolId { get; set; }
+    public string City { get; set; }
+
+    override public string ToString()
+    {
+        return $"{Name} from {City}";
+    }
+}
