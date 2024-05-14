b8f5c4cf66126be72518caf97b45e4e663567b14
cjaynjoku <76001800+cjaynjoku@users.noreply.github.com>
2024-05-14 19:20:35 +0100

    CM-1085: How to Compare Two Dictionaries in C# (#1983)
    * CM-1085: How to Compare Two Dictionaries in C#
    
    * modified the comparison and test methods and fixed formatting
    
    * removed the teacherdictionary comparer and tests and made the DictionaryEqualityComparer methods generic
    
    * Changed the AreKeysAndCountEqual Method
    
    * Delete CompareTwoDictionariesBenchmark project
    
    * fixed the comparison methods and benchmark dictionaries
    
    * Applied the IEquatable<TKey> constraint to our SequenceEqual method keys

---
+++ collections-dictionary/CompareTwoDictionaries/CompareTwoDictionaries/DictionaryEqualityComparerBenchmark.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace CompareTwoDictionaries;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class DictionaryEqualityComparerBenchmark
+{
+    private static readonly Dictionary<int, string> _dict1
+        = Enumerable.Range(1, 1000).ToDictionary(k => k, v => v.ToString());
+
+    private static readonly Dictionary<int, string> _dict2
+        = Enumerable.Range(1, 1000).ToDictionary(k => k, v => v.ToString());
+
+    [Benchmark]
+    public bool UseSequenceEqual()
+        => DictionaryEqualityComparer.UseSequenceEqual(_dict1, _dict2);
+
+    [Benchmark]
+    public bool UseEnumerableAll()
+        => DictionaryEqualityComparer.UseEnumerableAll(_dict1, _dict2);
+
+    [Benchmark]
+    public bool UseForeachLoop()
+        => DictionaryEqualityComparer.UseForeachLoop(_dict1, _dict2);
+}
---
+++ collections-dictionary/CompareTwoDictionaries/Tests/DictionaryEqualityComparerTest.cs
@@ -0,0 +1,70 @@
+﻿using CompareTwoDictionaries;
+
+namespace Tests;
+
+public class DictionaryEqualityComparerTest
+{
+    private static readonly Dictionary<int, string> _dict1
+         = new()
+         {
+            {1, "Rosary Ogechi"},
+            {2, "Clare Chiamaka"}
+         };
+
+    private static readonly Dictionary<int, string> _dict2
+         = new()
+         {
+            {1, "Rosary Ogechi"},
+            {2, "Clare Chiamaka"}
+         };
+
+    private static readonly Dictionary<int, string> _dict3
+         = new()
+         {
+            {1, "Rosary Ogechi"},
+            {2, "Clare Chiamaka"}
+         };
+
+    private static readonly Dictionary<int, string> _dict4
+        = new()
+        {
+            {1, "Clare Ogechi"},
+            {2, "Rosary Chiamaka"}
+        };
+
+    [Fact]
+    public void WhenUseSequenceEqualsIsCalledWithEqualDictionaries_ThenReturnsTrue()
+    {
+        Assert.True(DictionaryEqualityComparer.UseSequenceEqual(_dict1, _dict2));
+    }
+
+    [Fact]
+    public void WhenUseEnumerableAllIsCalledWithEqualDictionaries_ThenReturnsTrue()
+    {
+        Assert.True(DictionaryEqualityComparer.UseEnumerableAll(_dict1, _dict2));
+    }
+
+    [Fact]
+    public void WhenUseForeachLoopIsCalledWithEqualDictionaries_ThenReturnsTrue()
+    {
+        Assert.True(DictionaryEqualityComparer.UseForeachLoop(_dict1, _dict2));
+    }
+
+    [Fact]
+    public void WhenUseSequenceEqualsIsCalledWithUnEqualDictionaries_ThenReturnsFalse()
+    {
+        Assert.False(DictionaryEqualityComparer.UseSequenceEqual(_dict3, _dict4));
+    }
+
+    [Fact]
+    public void WhenUseEnumerableAllIsCalledWithUnEqualDictionaries_ThenReturnsFalse()
+    {
+        Assert.False(DictionaryEqualityComparer.UseEnumerableAll(_dict3, _dict4));
+    }
+
+    [Fact]
+    public void WhenUseForeachLoopIsCalledWithUnEqualDictionaries_ThenReturnsFalse()
+    {
+        Assert.False(DictionaryEqualityComparer.UseForeachLoop(_dict3, _dict4));
+    }
+}
