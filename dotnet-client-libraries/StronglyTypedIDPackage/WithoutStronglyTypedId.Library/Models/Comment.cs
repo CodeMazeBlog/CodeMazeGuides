e6d4335b4de2651b0645e04cdbf2479e572a2d91
Caleb Okechukwu <devcaleb96@gmail.com>
2024-05-17 05:30:47 +0100

    CM-1062: Introduction to StronglyTypedId (#1994)

---
+++ dotnet-client-libraries/StronglyTypedIDPackage/WithoutStronglyTypedId.Library/Models/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace WithoutStronglyTypedId.Library.Models;
+
+public class Comment
+{
+    public Guid Id { get; set; }
+    public string? Text { get; set; }
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
+}
