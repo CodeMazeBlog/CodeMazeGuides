b7f189ae25202231c7d508a8712038d6767ab587
Ivan Gechev <ivan_banov@abv.bg>
2024-05-18 17:57:24 +0300

    Final code and tests

---
+++ dotnet-testing/HowToMockIConfiguration/HowToMockIConfiguration.Tests/Services/FinanceServiceTests.cs
@@ -0,0 +1,28 @@
+﻿namespace HowToMockIConfiguration.Tests.Services;
+
+public class FinanceServiceTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(1.5)]
+    [InlineData(1000)]
+    public void WhenCalculateTotalAmountIsInvoked_ThenValidResultIsReturned(double hours)
+    {
+        // Arrange
+        const string rate = "25.50";
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                {"FinanceSettings:HourlyRate", rate}
+            })
+            .Build();
+
+        var financeService = new FinanceService(configuration);
+
+        // Act
+        var result = financeService.CalculateTotalAmount(hours);
+
+        // Assert
+        result.Should().Be(hours * double.Parse(rate));
+    }
+}
