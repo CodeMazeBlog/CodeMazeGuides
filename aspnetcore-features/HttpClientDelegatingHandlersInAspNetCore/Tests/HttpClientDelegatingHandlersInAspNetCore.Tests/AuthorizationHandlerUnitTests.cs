150577933e54cec68117050ddf9831cb96302478
Georgi Georgiev <51854143+georgy93@users.noreply.github.com>
2024-05-20 13:53:12 +0300

    CM-1043: Using Delegating Handlers to Extend HttpClient in ASP.NET Core (#1979)
    * CM-1043 Use Delegating Handlers to extend HttpClient
    
    * add GC.SuppressFinalize(this); to silence compiler suggestion
    
    * Use the proper AddHttpClient overload
    
    * fix unit tests and make GenerateToken an async method for the sake of the example
    
    * simplyfy examples
    
    * use async versions of methods
    
    * return results from the minimal apis
    
    * add example for SetHandlerLifeTime
    
    * fix typo
    
    * fix naming
    
    * two lines for log message to fit on one screen
    
    * Fixed indent and HTTPS connection
    
    ---------
    
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ aspnetcore-features/HttpClientDelegatingHandlersInAspNetCore/Tests/HttpClientDelegatingHandlersInAspNetCore.Tests/AuthorizationHandlerUnitTests.cs
@@ -0,0 +1,59 @@
+﻿namespace HttpClientDelegatingHandlersInAspNetCore.Tests;
+
+using DelegatingHandlers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.Protected;
+using System.Net;
+
+public class AuthorizationHandlerUnitTests
+{
+    [Fact]
+    public async Task GivenSendAsyncIsInvoked_WhenAuthorizationHandlerIsUsed_ThenLogsInformationBeforeAndAfterSending()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<AuthorizationHandler>>();
+        var mockInnerHandler = new Mock<HttpMessageHandler>();
+
+        mockInnerHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK));
+
+        var authorizationHandler = new AuthorizationHandler(mockLogger.Object)
+        {
+            InnerHandler = mockInnerHandler.Object
+        };
+
+        var httpClient = new HttpClient(authorizationHandler);
+
+        // Act
+        var request = new HttpRequestMessage(HttpMethod.Get, "http://example.com/test");
+        var response = await httpClient.SendAsync(request);
+
+        // Assert
+        Assert.NotNull(request.Headers.Authorization);
+        Assert.Equal("Bearer", request.Headers.Authorization.Scheme);
+        Assert.NotEmpty(request.Headers.Authorization.Parameter);
+
+        mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Hello from AuthorizationHandler")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+
+        mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Goodbye from AuthorizationHandler")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+    }
+}
