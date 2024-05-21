ce67c5d3199c29b3d513874d7437eb8c32a533fc
Zaneta Borowska <zaneta.borowska@katoennatie.com>
2024-05-21 21:06:34 +0200

    CM-1088 How to Ignore Mapping a Property With AutoMapper

---
+++ dotnet-client-libraries/AutoMapperIgnoreProperty/AutoMapperIgnoreProperty/UserDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+
+namespace AutoMapperIgnoreProperty;
+
+[AutoMap(typeof(User))]
+public class UserDetailsDto
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    [Ignore]
+    public string Password { get; set; }
+    [Ignore]
+    public bool IsAdmin { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ dotnet-client-libraries/AutoMapperIgnoreProperty/AutoMapperIgnoreProperty/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoMapperIgnoreProperty;
+public class UserDto
+{
+    public int Id { get; set; }
+    public string Password { get; set; }
+    public bool IsAdmin { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
