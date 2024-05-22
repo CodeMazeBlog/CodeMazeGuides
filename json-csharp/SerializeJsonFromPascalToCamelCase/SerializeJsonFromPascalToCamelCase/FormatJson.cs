6ede6e734711f8c74698be66a4455ca12da928bd
John <ademolajhon@gmail.com>
2024-05-22 22:18:45 +0100

    using a file-scoped namespace in formatJson

--- json-csharp/SerializeJsonFromPascalToCamelCase/SerializeJsonFromPascalToCamelCase/FormatJson.cs
+++ json-csharp/SerializeJsonFromPascalToCamelCase/SerializeJsonFromPascalToCamelCase/FormatJson.cs
@@ -3,19 +3,18 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 
-namespace SerializeJsonFromPascalToCamelCase
-{
-    public class FormatJson
-	{
-		public static JObject ConvertToCamel(JObject jsonObject)
-		{
-            var settings = JsonSerializer.Create(
-                            new JsonSerializerSettings
-                            {
-                                ContractResolver = new CamelCasePropertyNamesContractResolver()
-                            });
+namespace SerializeJsonFromPascalToCamelCase;
 
-            return JObject.FromObject(jsonObject.ToObject<ExpandoObject>(), settings);
-        }
-	}
+public class FormatJson
+{
+    public static JObject ConvertToCamel(JObject jsonObject)
+    {
+        var settings = JsonSerializer.Create(
+                        new JsonSerializerSettings
+                        {
+                            ContractResolver = new CamelCasePropertyNamesContractResolver()
+                        });
+
+        return JObject.FromObject(jsonObject.ToObject<ExpandoObject>(), settings);
+    }
 }
