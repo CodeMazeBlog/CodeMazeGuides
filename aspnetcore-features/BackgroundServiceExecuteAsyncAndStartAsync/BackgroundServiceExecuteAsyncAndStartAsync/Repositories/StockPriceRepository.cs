42289d3ac4e9b155d2ece1ba323cc6b95ed77a95
Georgios Panagopoulos <geopanagopoulosz@gmail.com>
2024-05-25 12:39:05 +0100

    CM-961: Comparing ExecuteAsync() and StartAsync() Methods from the BackgroundService Class in .NET (#1985)
    * CM-961 Initial implementation of the BackgroundService to show how we can use ExecuteAsync() and StartAsync()
    
    * CM-961 Fixes as per PR feedback
    * Kept only https profile in launchSettings.json
    * Changed test names to folllow the BDD convention GivenX_WhenY_ThenZ
    
    * CM-961 Removed redundant iissettings section from launchSettings.json file
    
    * CM-961 Fixed PR comments:
    * Removed http endpoint
    * Utilised file-scoped namespaces everywhere
    * Reduced network delays to allow for faster CI builds

---
+++ aspnetcore-features/BackgroundServiceExecuteAsyncAndStartAsync/BackgroundServiceExecuteAsyncAndStartAsync/Repositories/StockPriceRepository.cs
@@ -0,0 +1,23 @@
+﻿using BackgroundServiceExecuteAsyncAndStartAsync.Data;
+using BackgroundServiceExecuteAsyncAndStartAsync.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackgroundServiceExecuteAsyncAndStartAsync.Repositories;
+
+public class StockPriceRepository(StockPricesDbContext dbContext) : IStockPriceRepository
+{
+    public async Task AddRangeAsync(StockPrice[] stockPrices, CancellationToken cancellationToken)
+    {
+        await dbContext.StockPrices.AddRangeAsync(stockPrices, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<IReadOnlyCollection<StockPrice>> GetTodayStockPricesAsync()
+    {
+        return await dbContext.StockPrices
+                        .Where(s => s.MeasurementTimeStamp.Date == DateTime.Today)
+                        .OrderByDescending(s => s.MeasurementTimeStamp)
+                        .AsNoTracking()
+                        .ToListAsync();
+    }
+}
