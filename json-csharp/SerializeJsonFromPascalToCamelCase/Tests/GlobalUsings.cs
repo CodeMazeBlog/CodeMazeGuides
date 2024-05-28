7ba7108416c32ad75e50a3394cf41226221eec88
Ademola John <AdemolaJhon@gmail.com>
2024-05-28 12:14:34 +0100

    CM-1056: How to Serialize a PascalCase JSON.NET JObject to camelCase (#1989)
    * CM-1056: How to Serialize a PascalCase JSON.NET JObject to camelCase
    
    * Implemented FormatJson Converter and Added Tests
    
    * Added a new line between result and write line
    
    * *modified unit test name
    *used top-level statements in the Program class.
    
    * removed extra spacing
    
    * using a file-scoped namespace in formatJson
    
    * Changed GetData() method to houseInput instance
    
    * Format changes
    
    ---------
    
    Co-authored-by: Code Maze <contact@code-maze.com>

---
+++ json-csharp/SerializeJsonFromPascalToCamelCase/Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Xunit;
+global using Newtonsoft.Json.Linq;
+global using SerializeJsonFromPascalToCamelCase;
+global using SerializeJsonFromPascalToCamelCase.Models;
+global using Newtonsoft.Json;
+global using Newtonsoft.Json.Serialization;
