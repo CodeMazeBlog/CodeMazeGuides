4d1a34e27476bc6f13e18cd86aa0d4df5eca8151
cjaynjoku <njokujennaro@gmail.com>
2024-06-04 00:21:36 +0100

    CM-1116: What Does the null! Statement Do?

---
+++ csharp-intermediate-topics/Thenull!StatementInCSharp/Tests/StudentTest.cs
@@ -0,0 +1,18 @@
+﻿using ThenullbangStatementInCSharp;
+
+namespace Tests;
+
+public class StudentTest
+{
+    [Fact]
+    public void WhenCreatingAStudent_ThenReturnsANonNullableName()
+    {
+        var student = new Student
+        {
+            Id = 1,
+            Name = "Duke"
+        };
+
+        Assert.IsType<string>(student.Name);
+    }
+}
---
+++ csharp-intermediate-topics/Thenull!StatementInCSharp/Thenull!StatementInCSharp/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace ThenullbangStatementInCSharp;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
