3635e8ef937822bbb036ba5e57eaad4afc0d24b9
Martin Chege <martinchege324@gmail.com>
2024-06-06 06:10:55 +0300

    CM-1119: Using the Except Method in LINQ (#1992)
    * First draft
    
    * First article draft
    
    * Initial feedback implemented
    
    * Minor formatting issue fixed.
    
    * Removed Sales Employees list and refactored some functions based on the feedback provided.

---
+++ csharp-linq/UsingExceptInCSharp/UsingExceptInCSharp/EmployeeComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace UsingExceptInCSharp;
+
+public class EmployeeComparer : IEqualityComparer<Employee>
+{
+    public bool Equals(Employee? x, Employee? y)
+    {
+        if (x is null && y is null) return true;
+        if (x is null || y is null) return false;
+
+        return x.ID == y.ID && x.Name == y.Name && x.Age == y.Age && x.Department == y.Department;
+    }
+
+    public int GetHashCode([DisallowNull] Employee obj)
+    {
+        var hashID = obj.ID.GetHashCode();
+        var hashName = obj.Name.GetHashCode();
+
+        return hashID ^ hashName;
+    }
+}
