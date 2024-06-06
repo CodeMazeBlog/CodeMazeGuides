52135eb2edc1f3fffbf574549f37210a29367a15
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2024-06-06 23:39:09 +0200

    CM-118: final commit

--- visual-studio/AdvancedDebuggingInCSharp/AdvancedDebuggingInCSharp/Program.cs
+++ visual-studio/AdvancedDebuggingInCSharp/AdvancedDebuggingInCSharp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string txt1 = "unbelievable";
+string txt2 = "complicated";
+string txt3 = "interesting";
+string txt4 = "temporary";
+string txt5 = "extraordinary";
+string txt6 = "proportional";
+
+List<string> txts = [txt1, txt2, txt3, txt4, txt5, txt6];
+
+Thread thread = Thread.CurrentThread;
+thread.Name = "Main";
+
+foreach (var txt in txts)
+{
+    Random rnd = new();
+    int length = rnd.Next(3, txt.Length); 
+    
+    Console.WriteLine(PrintPart(txt, length));
+}
+
+static string PrintPart(string txt, int charsCount)
+{
+    return txt[..charsCount];
+}
