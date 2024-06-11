38b49e33a0b8ba61f85ccceecceb3822cfd6366b
zanbor <44729834+zanbor@users.noreply.github.com>
2024-06-11 21:39:34 +0200

    CM-1088 How to Ignore Mapping a Property With AutoMapper (#2004)
    * CM-1088 How to Ignore Mapping a Property With AutoMapper
    
    * Fixed spacings
    
    * Add newline in Dto class
    
    * Given_When_Then
    
    * Added When into tests
    
    ---------
    
    Co-authored-by: Zaneta Borowska <zaneta.borowska@katoennatie.com>
    Co-authored-by: fl0tschi <fl0tschi>
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ dotnet-client-libraries/AutoMapperIgnoreProperty/AutoMapperIgnorePropertyTests/MappersTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapperIgnoreProperty;
+
+namespace AutoMapperIgnorePropertyTests;
+
+[TestClass]
+public class MappersTests
+{
+    [TestMethod]
+    public void GivenSensitiveProperties_WhenExecutingMap_ThenPasswordAndIsAdminShouldBeIgnored()
+    {
+        // Arrange
+        var user = new User
+        {
+            Id = 1,
+            Email = "user01@codemaze.com",
+            CreatedAt = DateTime.Now,
+            Password = "Password123!",
+            IsAdmin = true
+        };
+
+        var mapper = MapperHelper.GetMapperIgnoringSensitiveProperties();
+
+        // Act
+        var userDto = mapper.Map<UserDto>(user);
+
+        // Assert
+        Assert.AreEqual(user.Id, userDto.Id);
+        Assert.IsNull(userDto.Password);
+        Assert.IsFalse(userDto.IsAdmin);
+        Assert.AreEqual(user.CreatedAt, userDto.CreatedAt);
+    }
+
+    [TestMethod]
+    public void GivenSensitivePropertiesMarkedWithAttribute_WhenExecutingMap_ThenPasswordAndIsAdminShouldBeIgnored()
+    {
+        // Arrange
+        var user = new User
+        {
+            Id = 1,
+            Email = "user01@codemaze.com",
+            CreatedAt = DateTime.Now,
+            Password = "Password123!",
+            IsAdmin = true
+        };
+
+        var mapper = MapperHelper.GetMapperIgnoringSensitivePropertiesWithAttribute();
+
+        // Act
+        var userDetailsDto = mapper.Map<UserDetailsDto>(user);
+
+        // Assert
+        Assert.AreEqual(user.Id, userDetailsDto.Id);
+        Assert.AreEqual(user.Email, userDetailsDto.Email);
+        Assert.IsNull(userDetailsDto.Password);
+        Assert.IsFalse(userDetailsDto.IsAdmin);
+        Assert.AreEqual(user.CreatedAt, userDetailsDto.CreatedAt);
+    }
+
+    [TestMethod]
+    public void GivenMapperIgnoringSourceMembers_WhenExecutingMap_ThenEmailValidationShouldBeIgnored()
+    {
+        // Arrange
+        var user = new User
+        {
+            Id = 1,
+            Email = "user01@codemaze.com",
+            CreatedAt = DateTime.Now,
+            Password = "Password123!",
+            IsAdmin = true
+        };
+
+        var mapper = MapperHelper.GetMapperIgnoringSourceMembers();
+
+        // Act
+        var userDto = mapper.Map<UserDto>(user);
+
+        // Assert
+        Assert.AreEqual(user.Id, userDto.Id);
+        Assert.AreEqual(user.CreatedAt, userDto.CreatedAt);
+    }
+}
