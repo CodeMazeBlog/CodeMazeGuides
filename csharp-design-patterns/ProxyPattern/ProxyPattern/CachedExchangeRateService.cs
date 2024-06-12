5bb396ab0ca8bef8f78ce7342d7307f8c2143ffb
Ahsan Ullah <ullahahsan1983@gmail.com>
2024-06-11 23:59:31 -0400

    Proxy Design Pattern in C#

---
+++ csharp-design-patterns/ProxyPattern/ProxyPattern/CachedExchangeRateService.cs
@@ -0,0 +1,26 @@
+﻿namespace ProxyPattern;
+
+public class CachedExchangeRateService : IExchangeRateService
+{
+    private readonly IExchangeRateService _exchangeRateService;
+    private ExchangeRate[]? _exhangeRates;
+
+    public CachedExchangeRateService()
+    {
+        _exchangeRateService = new ExchangeRateService();
+    }
+
+    public ExchangeRate[] GetExchangeRates()
+    {
+        if (_exhangeRates is null)
+        {
+            _exhangeRates = _exchangeRateService.GetExchangeRates();
+
+            return _exhangeRates;
+        }
+
+        Console.WriteLine("Read data from cache");
+
+        return _exhangeRates;
+    }
+}
