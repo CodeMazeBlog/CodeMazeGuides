395a5f81ba12b60ddb42ece4a3165335c9a1f14d
cjaynjoku <njokujennaro@gmail.com>
2024-06-12 18:15:20 +0100

    CM-1153:Improve Entity Framework Core Performance with Complex Types

---
+++ dotnet-efcore/EFCoreComplexTypes/EFCoreComplexTypes/ComplexTypesDBService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreComplexTypes;
+
+public class ComplexTypesDBService()
+{
+    private readonly AppDbContext _context = new();
+    public async void SaveComplexType()
+    {
+        var user = new User()
+        {
+            UserName = "Luther",
+            Address = new Address("Slessor Way", "Bendel", "Rivers", "504103", "Nigeria")
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<User> GetComplexTypeOwningEntity(int id)
+        => await _context.Users.FirstAsync(user => user.Id == id);
+
+    public async Task<Address> GetComplexTypeFromOwningEntity(int id)
+        => await _context.Users
+        .Where(user => user.Id == id)
+        .Select(u => u.Address).SingleAsync();
+}
---
+++ dotnet-efcore/EFCoreComplexTypes/EFCoreComplexTypes/User.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCoreComplexTypes;
+
+public record User
+{
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+    public required Address Address { get; set; }
+}
