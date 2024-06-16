2525745023d32e4032b19add92abfb454a79b1f0
Florian Prinz (FLPR) <flpr@simcorp.com>
2024-06-16 21:08:54 +0200

    Fixed benchmark to return the result

--- csharp-algorithms/FindAllPositionsOfAString/FindAllPositionsOfAString/Benchmarks/SearcherBenchmarks.cs
+++ csharp-algorithms/FindAllPositionsOfAString/FindAllPositionsOfAString/Benchmarks/SearcherBenchmarks.cs
@@ -5,58 +5,56 @@ using FindAllPositionsOfAString.Samples;
 
 namespace FindAllPositionsOfAString.Benchmarks;
 
-#pragma warning disable IDE0059 // Unnecessary assignment of a value
 public class SearcherBenchmarks
 {
     public static IEnumerable<object[]> Samples() => SearchingSamples.SamplesForBenchmark();
 
     [Benchmark(Baseline = true)]
     [ArgumentsSource(nameof(Samples))]
-    public void BenchmarkSearchUsingIndexOf(string text, string searchText)
+    public List<int> BenchmarkSearchUsingIndexOf(string text, string searchText)
     {
         var searcher = new SearchUsingIndexOf();
-        var result = searcher.FindAll(text, searchText);
+        return searcher.FindAll(text, searchText);
     }
 
     [Benchmark]
     [ArgumentsSource(nameof(Samples))]
-    public void BenchmarkSearchUsingSearchValues(string text, string searchText)
+    public List<int> BenchmarkSearchUsingSearchValues(string text, string searchText)
     {
         var searcher = new SearchUsingSearchValues();
-        var result = searcher.FindAll(text, searchText);
+        return searcher.FindAll(text, searchText);
     }
 
     [Benchmark]
     [ArgumentsSource(nameof(Samples))]
-    public void BenchmarkSearchUsingRegexMatch(string text, string searchText)
+    public List<int> BenchmarkSearchUsingRegexMatch(string text, string searchText)
     {
         var searcher = new SearchUsingRegexWithMatch();
-        var result = searcher.FindAll(text, searchText);
+        return searcher.FindAll(text, searchText);
     }
 
     [Benchmark]
     [ArgumentsSource(nameof(Samples))]
-    public void BenchmarkSearchUsingRegexMatches(string text, string searchText)
+    public List<int> BenchmarkSearchUsingRegexMatches(string text, string searchText)
     {
         var searcher = new SearchUsingRegexWithMatches();
         searcher.SkipWholeFoundText = true;
-        var result = searcher.FindAll(text, searchText);
+        return searcher.FindAll(text, searchText);
     }
 
     [Benchmark]
     [ArgumentsSource(nameof(Samples))]
-    public void BenchmarkSearchUsingBruteForce(string text, string searchText)
+    public List<int> BenchmarkSearchUsingBruteForce(string text, string searchText)
     {
         var searcher = new SearchUsingBruteForceAlgorithm();
-        var result = searcher.FindAll(text, searchText);
+        return searcher.FindAll(text, searchText);
     }
 
     [Benchmark]
     [ArgumentsSource(nameof(Samples))]
-    public void BenchmarkSearchUsingKMP(string text, string searchText)
+    public List<int> BenchmarkSearchUsingKMP(string text, string searchText)
     {
         var searcher = new SearchUsingKMPAlgorithm();
-        var result = searcher.FindAll(text, searchText);
+        return searcher.FindAll(text, searchText);
     }
 }
-#pragma warning restore IDE0059 // Unnecessary assignment of a value
