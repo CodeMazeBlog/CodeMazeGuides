d897f668c190e8cd654105857141eb9712ffcced
Ahsan Ullah <ullahahsan1983@gmail.com>
2024-06-21 15:34:37 -0400

    Bridge Pattern in C#

---
+++ csharp-design-patterns/BridgePattern/BridgePattern/Problem/ExpressPriceCalculator.cs
@@ -0,0 +1,8 @@
+﻿namespace BridgePattern.Problem;
+
+public class ExpressPriceCalculator : PriceCalculator
+{
+    protected override decimal GetDeliveryFee() => 8m;
+
+    protected override decimal GetDiscount(decimal price) => 0;
+}
---
+++ csharp-design-patterns/BridgePattern/BridgePatternTests/PriceCalculatorWithoutBridgePatternTests.cs
@@ -0,0 +1,29 @@
+﻿using BridgePattern;
+using BridgePattern.Problem;
+
+namespace BridgePatternTests;
+
+public class PriceCalculatorWithoutBridgePatternTests
+{
+    [Fact]
+    public void WhenNotUsingBridgePattern_ThenEachCalculatorClass_ServesASpecificDiscountModel()
+    {
+        var productPrice = 100m;
+
+        var standard = new StandardPriceCalculator()
+            .Calculate(productPrice);
+        Console.WriteLine($"Standard: {standard}"); // 102.5
+
+        var standardPromo = new PromoCodeStandardPriceCalculator(PromoCode.Free10)
+            .Calculate(productPrice);
+        Console.WriteLine($"Standard Promo: {standardPromo}"); // 92.5
+
+        var standardHotSale = new HotSaleStandardPriceCalculator()
+            .Calculate(productPrice);
+        Console.WriteLine($"Standard HotSale: {standardHotSale}"); // 82.5
+
+        Assert.Equal(102.5m, standard);
+        Assert.Equal(92.5m, standardPromo);
+        Assert.Equal(82.5m, standardHotSale);
+    }
+}
