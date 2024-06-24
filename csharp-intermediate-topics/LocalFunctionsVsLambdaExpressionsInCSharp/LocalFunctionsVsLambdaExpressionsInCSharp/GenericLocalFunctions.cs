7a5a4de2a5a0615b80b32f8b8664241c609b2540
bozo-spoljaric <40840193+bozo-spoljaric@users.noreply.github.com>
2024-06-24 20:43:48 +0200

    CM-956: Local functions vs lambda expressions in C# (#2016)
    * Local functions vs lambda expressions in C#
    
    * Fix of warnings in the lambda expression for factorial calculation
    
    * Fix return statement newlines
    
    * Fixed benchmark order
    
    ---------
    
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>
    Co-authored-by: fl0tschi <fl0tschi>

---
+++ csharp-intermediate-topics/LocalFunctionsVsLambdaExpressionsInCSharp/LocalFunctionsVsLambdaExpressionsInCSharp/GenericLocalFunctions.cs
@@ -0,0 +1,16 @@
+﻿namespace LocalFunctionsVsLambdaExpressionsInCSharp;
+
+public static class GenericLocalFunctions
+{
+    public static void SwapElements(ref object left, ref object right)
+    {
+        static void Swap<T>(ref T left, ref T right)
+        {
+            T temp = left;
+            left = right;
+            right = temp;
+        }
+
+        Swap(ref left, ref right);
+    }
+}
---
+++ csharp-intermediate-topics/LocalFunctionsVsLambdaExpressionsInCSharp/LocalFunctionsVsLambdaExpressionsInCSharp/Recursivity.cs
@@ -0,0 +1,34 @@
+﻿namespace LocalFunctionsVsLambdaExpressionsInCSharp;
+
+public class Recursivity
+{
+    public static int FactorialAsLocalFunction(int input)
+    {
+        int Factorial(int n)
+        {
+            if (n <= 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+
+        int result = Factorial(input);
+
+        return result;
+    }
+
+    public static int FactorialAsLambdaExpression(int input)
+    {
+        Func<int, int> factorial = null!;
+
+        factorial = (int n) =>
+        {
+            if (n <= 1) return 1;
+
+            return n * factorial(n - 1);
+        };
+
+        int result = factorial(input);
+        
+        return result;
+    }
+}
