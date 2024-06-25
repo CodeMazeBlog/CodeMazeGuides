eb8099c3c26f7c0fee400845c52dc13091ef9a04
Ahsan Ullah <48589386+ullahahsan1983@users.noreply.github.com>
2024-06-25 08:04:54 -0400

    Bridge Pattern in C# (#2020)

---
+++ csharp-design-patterns/BridgePattern/BridgePattern/Problem/PriceCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace BridgePattern.Problem;
+
+public abstract class PriceCalculator
+{
+    public decimal Calculate(decimal basePrice)
+    {
+        var deliveryFee = GetDeliveryFee();
+
+        var discount = GetDiscount(basePrice);
+
+        return basePrice + deliveryFee - discount;
+    }
+
+    protected abstract decimal GetDeliveryFee();
+
+    protected abstract decimal GetDiscount(decimal price);
+}
