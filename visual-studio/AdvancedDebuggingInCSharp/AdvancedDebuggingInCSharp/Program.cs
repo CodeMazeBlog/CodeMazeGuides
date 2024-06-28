7bfe651cca9a2abdf24afb91306c915acb6a8997
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2024-06-28 17:42:08 +0200

    CM-118: Use file-scoped namespace in tests

--- visual-studio/AdvancedDebuggingInCSharp/AdvancedDebuggingInCSharp/Program.cs
+++ visual-studio/AdvancedDebuggingInCSharp/AdvancedDebuggingInCSharp/Program.cs
@@ -9,7 +9,6 @@ List<string> txts = [txt1, txt2, txt3, txt4, txt5, txt6];
 
 Thread thread = Thread.CurrentThread;
 thread.Name = "Main";
-
 Random rnd = new();
 
 foreach (var txt in txts)
--- visual-studio/AdvancedDebuggingInCSharp/Tests/AdvancedDebuggingInCSharpUnitTests.cs
+++ visual-studio/AdvancedDebuggingInCSharp/Tests/AdvancedDebuggingInCSharpUnitTests.cs
@@ -1,42 +1,41 @@
 using System.Reflection;
 
-namespace Tests
+namespace Tests;
+
+public class AdvancedDebuggingInCSharpUnitTests
 {
-    public class AdvancedDebuggingInCSharpUnitTests
+    [Fact]
+    public void GivenPrintPartMethod_WhenArgumentsAreCorrect_ThenCorrectSubstringIsReturned()
     {
-        [Fact]
-        public void GivenPrintPartMethod_WhenArgumentsAreCorrect_ThenCorrectSubstringIsReturned()
-        {
-            // Arrange
-            string word = "beautiful";
-            int length = 4;
+        // Arrange
+        string word = "beautiful";
+        int length = 4;
 
-            TypeInfo program = typeof(Program).GetTypeInfo();
-            var printPartMethod = program.DeclaredMethods.Single(m => m.Name.Contains("PrintPart"));
+        TypeInfo program = typeof(Program).GetTypeInfo();
+        var printPartMethod = program.DeclaredMethods.Single(m => m.Name.Contains("PrintPart"));
 
-            // Act
-            string result = printPartMethod.Invoke(null, new object[] { word, length }).ToString();
+        // Act
+        string result = printPartMethod.Invoke(null, new object[] { word, length }).ToString();
 
-            // Assert
-            Assert.Equal("beau", result); 
-        }
+        // Assert
+        Assert.Equal("beau", result); 
+    }
 
-        [Fact]
-        public void GivenMainMethod_WhenProgramIsRun_ThenPrintPartMethodIsCalledForEachString()
-        {
-            // Arrange
-            StringWriter writer = new StringWriter();
-            Console.SetOut(writer);
-                        
-            TypeInfo program = typeof(Program).GetTypeInfo();
-            var mainMethod = program.DeclaredMethods.Single(m => m.Name == "<Main>$");
+    [Fact]
+    public void GivenMainMethod_WhenProgramIsRun_ThenPrintPartMethodIsCalledForEachString()
+    {
+        // Arrange
+        StringWriter writer = new StringWriter();
+        Console.SetOut(writer);
+                    
+        TypeInfo program = typeof(Program).GetTypeInfo();
+        var mainMethod = program.DeclaredMethods.Single(m => m.Name == "<Main>$");
 
-            // Act
-            mainMethod.Invoke(null, new object[] { new string[2] { "", "" } });
-            var wordCount = writer.ToString().Count(x => x == '\n');
+        // Act
+        mainMethod.Invoke(null, new object[] { new string[2] { "", "" } });
+        var wordCount = writer.ToString().Count(x => x == '\n');
 
-            // Assert
-            Assert.Equal(6, wordCount);
-        }
+        // Assert
+        Assert.Equal(6, wordCount);
     }
 }
