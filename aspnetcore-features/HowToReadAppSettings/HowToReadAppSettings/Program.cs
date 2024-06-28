25890bb59fe3c0e0346b54a6be9d46061da88d46
Matheus Dasuke <Matheus.Dasuke@stgen.com>
2024-06-28 14:03:38 -0500

    General improvements

--- aspnetcore-features/HowToReadAppSettings/HowToReadAppSettings/Program.cs
+++ aspnetcore-features/HowToReadAppSettings/HowToReadAppSettings/Program.cs
@@ -7,27 +7,24 @@ builder.SetBasePath(Directory.GetCurrentDirectory())
 
 IConfigurationRoot configuration = builder.Build();
 
-#region Bind configuration to strongly-typed object
+// Bind configuration to strongly-typed object
 var appSettings = new AppSettings();
 configuration.GetSection("AppSettings").Bind(appSettings);
 Console.WriteLine(appSettings.Settings2);
-#endregion Bind configuration to strongly-typed object
 
-#region Nested example
 var example1 = configuration["Settings1"];
 Console.WriteLine(example1);
 
 var example1UsingSection = configuration.GetSection("Settings1");
 Console.WriteLine(example1UsingSection.Value);
 
+// Nested example
 var settings = configuration["AppSettings:Settings2"];
 Console.WriteLine(settings);
 
 var settings2 = configuration.GetSection("AppSettings");
-Console.WriteLine(settings2.GetSection("Settings2").Value);
-#endregion Nested example
+Console.WriteLine(settings2["Settings2"]);
 
-#region Connection String
+// Connection String
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 Console.WriteLine(connectionString);
-#endregion Connection String
