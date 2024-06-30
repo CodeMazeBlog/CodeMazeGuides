d1a6dd36a6db72853778a289e603c39ff0eab169
karthikns999 <karthikns999@gmail.com>
2024-06-30 20:27:18 +0530

    added benchmark

---
+++ strings-csharp/StringValues/StringValuesBenchmark/ImplementationBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+public class ImplementationBenchmark
+{
+    [Benchmark]
+    public void AddMultipleHeadersUsingNaiveMethod()
+    {
+        NaiveImplementation naiveImplementation = new();
+
+        for (int i = 0; i < 5; i++)
+        {
+            naiveImplementation.AddHeader("MyHeader", $"value{i}");
+        }
+    }
+
+    [Benchmark]
+    public void AddMultipleHeadersUsingLegacyImplementation()
+    {
+        LegacyImplementation legacyImplementation = new();
+
+        for (int i = 0; i < 5; i++)
+        {
+            legacyImplementation.AddHeader("MyHeader", $"value{i}");
+        }
+    }
+
+    [Benchmark]
+    public void AddMultipleHeadersUsingStringValues()
+    {
+        StringValuesImplementation stringValuesImplementation = new();
+
+        for (int i = 0; i < 5; i++)
+        {
+            stringValuesImplementation?.AddHeader("MyHeader", $"value{i}");
+        }
+    }
+}
