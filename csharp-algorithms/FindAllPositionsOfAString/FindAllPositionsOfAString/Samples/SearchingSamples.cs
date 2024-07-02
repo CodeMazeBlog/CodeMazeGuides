18cc963ee5008c77a8b0c8741b9a91c3cf8160f4
Matjaž Prtenjak <matjaz@matjazev.net>
2024-07-02 16:39:32 +0200

    CM-1132: How to Find All the Positions of a Substring in Another String in C# (#2008)
    * CM-1132: Initial code sample
    
    * CM-1132: Add case sensitivity and skipping whole words
    
    * CM-1132: Code sample for review
    
    * CM-1132: PR code review changes
    
    * CM-1132: Optimize the KMP algorithm
    
    * CM-1132: Using ToUpper instead of ToLower
    
    * CM-1132: Adding SearchValues<string> algorithm
    
    * CM-1132: remove the repeating text in tests into private field
    
    * Fixed benchmark to return the result
    
    * CM-1132: Change interface so each algorithm also has the initialization phase
    
    * Refactor search algorithms to use constructor injection for initialization
    
    * Fixed unnecessary null member variable initialization
    
    ---------
    
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ csharp-algorithms/FindAllPositionsOfAString/FindAllPositionsOfAString/Samples/SearchingSamples.cs
@@ -0,0 +1,24 @@
+﻿namespace FindAllPositionsOfAString.Samples;
+
+public record SearchPair(string Text, string SearchText);
+
+public static class SearchingSamples
+{
+    private static readonly string JuliusCaesar = JuliusCaesarText.Read();
+
+    public static readonly SearchPair[] SearchPairs =
+    [
+        new SearchPair("Some consider the occurrences to be the unconscious mind’s attempts to communicate to the conscious mind.", "the"),
+        new SearchPair("The quick brown fox jumps over the lazy dog?", "the"),
+        new SearchPair("ABABABABABABABABABABABABABABABABABABABABABABABABABABABABABAB", "ABABABABABAB"),
+        new SearchPair(JuliusCaesar, "Themselves"),
+    ];
+
+    public static IEnumerable<SearchPair> SampleForProgram()
+    {
+        foreach (SearchPair searchPair in SearchPairs)
+        {
+            yield return searchPair;
+        }
+    }
+}
---
+++ csharp-algorithms/FindAllPositionsOfAString/Tests/JuliusCaesarTextTest.cs
@@ -0,0 +1,48 @@
+﻿using FindAllPositionsOfAString;
+
+namespace Tests;
+
+[TestClass]
+public class JuliusCaesarTextTest
+{
+    [TestMethod]
+    public void GivenJuliusCaesarText_WhenReading_ThenTextShouldNotBeEmpty()
+    {
+        var text = JuliusCaesarText.Read();
+
+        Assert.IsFalse(string.IsNullOrEmpty(text));
+    }
+
+    [TestMethod]
+    public void GivenJuliusCaesarText_WhenReading_ThenTextShouldNotHaveDoubleSpaces()
+    {
+        var text = JuliusCaesarText.Read();
+
+        Assert.IsFalse(text.Contains("  "));
+    }
+
+    [TestMethod]
+    public void GivenJuliusCaesarText_WhenReading_ThenTextShouldShouldHaveOnlyOneLine()
+    {
+        var text = JuliusCaesarText.Read();
+
+        Assert.IsFalse(text.Contains('\n'));
+    }
+
+    [TestMethod]
+    public void GivenJuliusCaesarText_WhenReading_ThenTextShouldContainOnlyLettersAndDigits()
+    {
+        var text = JuliusCaesarText.Read();
+
+        Assert.IsTrue(text.All(c => char.IsLetterOrDigit(c) || c == ' '));
+    }
+
+    [TestMethod]
+    public void GivenJuliusCaesarText_WhenReading_ThenWordsShouldBeIncluded()
+    {
+        var text = JuliusCaesarText.Read();
+        var content = "Strato Countrymen My heart doth joy that yet in all my life I found no man";
+
+        Assert.IsTrue(text.Contains(content));
+    }
+}
