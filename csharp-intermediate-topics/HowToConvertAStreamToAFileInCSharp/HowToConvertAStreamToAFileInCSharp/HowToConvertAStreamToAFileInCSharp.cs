85206450aec88b48756feef10c3196a8a2b3a083
bozo-spoljaric <40840193+bozo-spoljaric@users.noreply.github.com>
2024-07-15 11:16:20 +0200

    CM-870: How to Convert a Stream to a File in C# (#2032)
    * how to convert a stream to a file
    
    * fix Path.Combine

---
+++ csharp-intermediate-topics/HowToConvertAStreamToAFileInCSharp/HowToConvertAStreamToAFileInCSharp/HowToConvertAStreamToAFileInCSharp.cs
@@ -0,0 +1,32 @@
+﻿namespace HowToConvertAStreamToAFileInCSharp;
+
+public static class ConvertAStreamToAFileInCSharp
+{
+    public static void CopyToFile(MemoryStream stream, string path)
+    {
+        using var fileStream = File.Create(path);
+        stream.Position = 0;
+        stream.CopyTo(fileStream);
+    }
+
+    public static void WriteToFileStream(MemoryStream stream, string path)
+    {
+        using var fileStream = File.Create(path);
+        fileStream.Write(stream.ToArray());
+    }
+
+    public static void WriteAllBytesFile(MemoryStream stream, string path)
+    {
+        File.WriteAllBytes(path, stream.ToArray());
+    }
+
+    public static void WriteByteToFileStream(MemoryStream stream, string path)
+    {
+        using var fileStream = File.Create(path);
+        stream.Position = 0;
+        while (stream.Position < stream.Length)
+        {
+            fileStream.WriteByte((byte)stream.ReadByte());
+        }
+    }
+}
---
+++ csharp-intermediate-topics/HowToConvertAStreamToAFileInCSharp/HowToConvertAStreamToAFileInCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Running;
+using HowToConvertAStreamToAFileInCSharp;
+
+var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+if (!Directory.Exists(directory))
+{ 
+    return; 
+}
+
+var sourceFile = Path.Combine([directory, "Files", "source.png"]);
+var destinationPath = Path.Combine([directory, "Files"]);
+
+using var fileStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
+using var memoryStream = new MemoryStream();
+fileStream.CopyTo(memoryStream);
+
+ConvertAStreamToAFileInCSharp.CopyToFile(memoryStream, Path.Combine([destinationPath, "CopyTo.png"]));
+ConvertAStreamToAFileInCSharp.WriteToFileStream(memoryStream, Path.Combine([destinationPath, "WriteTo.png"]));
+ConvertAStreamToAFileInCSharp.WriteByteToFileStream(memoryStream, Path.Combine([destinationPath, "WriteByte.png"]));
+ConvertAStreamToAFileInCSharp.WriteAllBytesFile(memoryStream, Path.Combine([destinationPath, "WriteAllBytes.png"]));
+
+ConvertAStreamToAFileInCSharpBenchmark.Stream = memoryStream;
+ConvertAStreamToAFileInCSharpBenchmark.DestinationPath = destinationPath;
+
+BenchmarkRunner.Run<ConvertAStreamToAFileInCSharpBenchmark>();
