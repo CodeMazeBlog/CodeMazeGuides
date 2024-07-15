1dd9f04efcfbba379fc65465529716407f06562f
Vadzim Zylevich <01vadim10@gmail.com>
2024-07-15 21:05:29 +0200

    CM-1083: Mastering Multi-Source Data Integration with Strawberry Shake Subscriptions (#1988)
    * Initial commit
    
    * Working on Mutations on Server
    
    * Added Mutations
    
    * Added subscription files
    
    * Updated graphql schema with Mutations
    
    * Added Subscriptions
    
    * Added subscriptions to GraphQL client
    
    * Added GraphQL tests
    
    * Fixed article review comments
    
    * Renamed test methods in ShippingContainerTests
    
    * Removed https profile from launchsettings.json

---
+++ dotnet-client-libraries/GraphQLStrawberryShakeSubscriptions/GraphQLStrawberryShakeSubs.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLStrawberryShakeSubs.Server.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<ShippingContainer> ShippingContainers { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShippingContainer>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            entity.OwnsOne(e => e.Space, space =>
+            {
+                space.Property(e => e.Length).IsRequired();
+                space.Property(e => e.Width).IsRequired();
+                space.Property(e => e.Height).IsRequired();
+            });
+        });
+    }
+}
