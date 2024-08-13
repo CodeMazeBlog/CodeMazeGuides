c4949f36dbe1ab826a5aac89f4f064aadc85d622
bspoljaric <bozo.spoljaric@gmail.com>
2024-08-13 15:11:42 +0200

    first version

---
+++ aspnetcore-features/TheIHostedLifecycleServiceInterface/TheIHostedLifecycleServiceInterface/Services/HostedServiceA.cs
@@ -0,0 +1,41 @@
+﻿namespace TheIHostedLifecycleServiceInterface.Services;
+
+public class HostedServiceA : IHostedLifecycleService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceA)} start.");
+        return Task.CompletedTask;
+    }
+
+    public Task StartedAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceA)} started.");
+        return Task.CompletedTask;
+    }
+
+    public Task StartingAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceA)} starting.");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceA)} stop.");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppedAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceA)} stopped.");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppingAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceA)} stopping.");
+        return Task.CompletedTask;
+    }
+}
+
---
+++ aspnetcore-features/TheIHostedLifecycleServiceInterface/TheIHostedLifecycleServiceInterface/Services/HostedServiceB.cs
@@ -0,0 +1,40 @@
+﻿namespace TheIHostedLifecycleServiceInterface.Services;
+
+public class HostedServiceB : IHostedLifecycleService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceB)} start.");
+        return Task.CompletedTask;
+    }
+
+    public Task StartedAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceB)} started.");
+        return Task.CompletedTask;
+    }
+
+    public Task StartingAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceB)} starting.");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceB)} stop.");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppedAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceB)} stopped.");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppingAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Service {nameof(HostedServiceB)} stopping.");
+        return Task.CompletedTask;
+    }
+}
