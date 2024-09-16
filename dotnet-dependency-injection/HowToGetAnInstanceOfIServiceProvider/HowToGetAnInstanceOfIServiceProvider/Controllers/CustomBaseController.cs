dc6245975b076decf7e34505525666735b774d51
Georgi Georgiev <georgy.georgiev93@gmail.com>
2024-09-16 13:04:46 +0300

    apply changes from review doc

---
+++ dotnet-dependency-injection/HowToGetAnInstanceOfIServiceProvider/HowToGetAnInstanceOfIServiceProvider/Controllers/CustomBaseController.cs
@@ -0,0 +1,10 @@
+﻿namespace HowToGetAnInstanceOfIServiceProvider.Controllers;
+
+using HowToGetAnInstanceOfIServiceProvider.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+public abstract class CustomBaseController : ControllerBase
+{
+    protected IExampleService ExampleService => HttpContext.RequestServices.GetService<IExampleService>();
+}
--- dotnet-dependency-injection/HowToGetAnInstanceOfIServiceProvider/HowToGetAnInstanceOfIServiceProvider/Controllers/TestController.cs
+++ dotnet-dependency-injection/HowToGetAnInstanceOfIServiceProvider/HowToGetAnInstanceOfIServiceProvider/Controllers/TestController.cs
@@ -1,14 +1,6 @@
 ﻿namespace HowToGetAnInstanceOfIServiceProvider.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
-using Services;
-
-[ApiController]
-public abstract class CustomBaseController : ControllerBase
-{
-    protected IExampleService ExampleService => HttpContext.RequestServices.GetService<IExampleService>();  
-}
 
 public class ExampleController : CustomBaseController
 {
