5013a6d3d3a2fb1d19df1e19ac408aded35392ed
Florian Prinz (FLPR) <flpr@simcorp.com>
2024-09-23 18:19:26 +0200

    Removed private field from ExampleController

--- dotnet-dependency-injection/HowToGetAnInstanceOfIServiceProvider/HowToGetAnInstanceOfIServiceProvider/Controllers/ExampleController.cs
+++ dotnet-dependency-injection/HowToGetAnInstanceOfIServiceProvider/HowToGetAnInstanceOfIServiceProvider/Controllers/ExampleController.cs
@@ -6,12 +6,10 @@ using Services;
 [ApiController]
 public class ExampleController : ControllerBase
 {
-    private IExampleService ExampleService => HttpContext.RequestServices.GetService<IExampleService>();
-
     [HttpGet("/api/example/get-serviceprovider-within-api-controller")]
     public IActionResult GetIServiceProviderFromHttpContext()
     {
-        var resolvedService = ExampleService;
+        var resolvedService = HttpContext.RequestServices.GetService<IExampleService>();
 
         return Ok(resolvedService is not null);
     }
