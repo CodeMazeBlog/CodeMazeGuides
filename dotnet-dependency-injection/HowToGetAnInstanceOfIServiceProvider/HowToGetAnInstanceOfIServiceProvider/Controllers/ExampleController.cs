96eeaeb8fc7748ba14d860b7783ae932bdeea42f
Georgi Georgiev <51854143+georgy93@users.noreply.github.com>
2024-10-05 18:22:20 +0300

    [CM-685] How to Get an Instance of IServiceProvider in .NET (#2054)
    * #CM-685
    
    * apply changes from review doc
    
    * Renamed file for ExampleController
    
    * trigger build
    
    * apply latest review changes
    
    * Removed unnecessary base controller
    
    * Removed private field from ExampleController
    
    ---------
    
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ dotnet-dependency-injection/HowToGetAnInstanceOfIServiceProvider/HowToGetAnInstanceOfIServiceProvider/Controllers/ExampleController.cs
@@ -0,0 +1,16 @@
+﻿namespace HowToGetAnInstanceOfIServiceProvider.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+[ApiController]
+public class ExampleController : ControllerBase
+{
+    [HttpGet("/api/example/get-serviceprovider-within-api-controller")]
+    public IActionResult GetIServiceProviderFromHttpContext()
+    {
+        var resolvedService = HttpContext.RequestServices.GetService<IExampleService>();
+
+        return Ok(resolvedService is not null);
+    }
+}
---
+++ dotnet-dependency-injection/HowToGetAnInstanceOfIServiceProvider/HowToGetAnInstanceOfIServiceProvider/Services/ExampleBackgroundService.cs
@@ -0,0 +1,12 @@
+﻿namespace HowToGetAnInstanceOfIServiceProvider.Services;
+
+public class ExampleBackgroundService(IServiceProvider ServiceProvider) : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var scope = ServiceProvider.CreateScope();
+        var service = scope.ServiceProvider.GetRequiredService<IExampleService>();
+
+        return Task.CompletedTask;
+    }
+}
