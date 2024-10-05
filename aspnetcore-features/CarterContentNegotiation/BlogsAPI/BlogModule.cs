495016f336524f1215a000da007e10b74af4a021
RPM1984 <rpmir1@gmail.com>
2024-10-05 14:07:23 +1000

    Code review updates

---
+++ aspnetcore-features/CarterContentNegotiation/BlogsAPI/BlogModule.cs
@@ -0,0 +1,40 @@
+﻿using Carter;
+using Carter.Response;
+
+public class BlogModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/blogs", (HttpResponse response) => response.Negotiate(new Blog[]
+        {
+            new()
+            {
+                Id = 1,
+                DatePublished = DateOnly.FromDateTime(DateTime.Now),
+                Title = "Building .NET Minimal API's",
+                Tags = ["c#", ".net", "api"]
+            },
+            new()
+            {
+                Id = 2,
+                DatePublished = DateOnly.FromDateTime(DateTime.Now.AddDays(-1)),
+                Title = "Content Negotiation in .NET API's",
+                Tags = [ "c#", ".net", "api", "content negotiation"]
+            },
+            new()
+            {
+                Id = 3,
+                DatePublished = DateOnly.FromDateTime(DateTime.Now.AddDays(-2)),
+                Title = "Using .http files to test APIs",
+                Tags = ["c#", ".net", "api", "testing"]
+            },
+            new()
+            {
+                Id = 4,
+                DatePublished = DateOnly.FromDateTime(DateTime.Now.AddDays(-3)),
+                Title = "Different Web Content Types",
+                Tags = ["web", "content types"]
+            }
+        }));
+    }
+}
--- aspnetcore-features/CarterContentNegotiation/BlogsAPI/Program.cs
+++ aspnetcore-features/CarterContentNegotiation/BlogsAPI/Program.cs
@@ -1,6 +1,5 @@
 using BlogsAPI;
 using Carter;
-using Carter.Response;
 
 public partial class Program
 {
@@ -17,26 +16,4 @@ public partial class Program
         app.UseHttpsRedirection();
         app.Run();
     }
-}
-
-public class BlogModule : ICarterModule
-{
-    public void AddRoutes(IEndpointRouteBuilder app)
-    {
-        app.MapGet("/blogs", (HttpResponse response) => response.Negotiate(new Blog[]
-        {
-            new() { Id = 1, DatePublished = DateOnly.FromDateTime(DateTime.Now), Title = "Building .NET Minimal API's", Tags = ["c#", ".net", "api"] },
-            new() { Id = 2, DatePublished = DateOnly.FromDateTime(DateTime.Now.AddDays(-1)), Title = "Content Negotiation in .NET API's", Tags = [ "c#", ".net", "api", "content negotiation"] },
-            new() { Id = 3, DatePublished = DateOnly.FromDateTime(DateTime.Now.AddDays(-2)), Title = "Using .http files to test APIs", Tags = ["c#", ".net", "api", "testing"] },
-            new() { Id = 4, DatePublished = DateOnly.FromDateTime(DateTime.Now.AddDays(-3)), Title = "Different Web Content Types", Tags = ["web", "content types"] }
-        }));
-    }
-}
-
-public class Blog
-{
-    public int Id { get; init; }
-    public DateOnly DatePublished { get; init; }
-    public string? Title { get; init; }
-    public string[]? Tags { get; init; }
 }
