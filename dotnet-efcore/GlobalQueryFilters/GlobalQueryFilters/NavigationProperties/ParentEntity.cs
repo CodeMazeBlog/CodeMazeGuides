5450498f10ff25fc072a172c152b6b5e998c434e
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2024-12-01 17:50:30 -0500

    trying to include all code from article in the pr

---
+++ dotnet-efcore/GlobalQueryFilters/GlobalQueryFilters/NavigationProperties/ParentEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace GlobalQueryFilters;
+
+public sealed class ParentEntity(IEnumerable<ChildEntity> children)
+{
+    public Guid Id { get; private set; }
+    public bool IsDeleted { get; set; }
+    public IEnumerable<ChildEntity> Children { get; private set; } = children;
+    
+    private ParentEntity() : this(null!) { } // ctor for EF
+}
--- dotnet-efcore/GlobalQueryFilters/GlobalQueryFilters/Program.cs
+++ dotnet-efcore/GlobalQueryFilters/GlobalQueryFilters/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 services.AddDbContext<SoftDeleteDbContext>(options => options.UseInMemoryDatabase("GlobalQueryFilters"));
+services.AddDbContext<NavPropDbContext>(options => options.UseInMemoryDatabase("GlobalQueryFilters"));
 
 var serviceProvider = services.BuildServiceProvider();
 
@@ -15,7 +16,9 @@ await dbContext.SaveChangesAsync();
 
 Console.WriteLine($"New entity added with id: {entity.Id}");
 
-var entityToDelete = await dbContext.SoftDeleteEntities.FirstOrDefaultAsync(e => e.Id == entity.Id);
+var entityToDelete = await dbContext.SoftDeleteEntities
+    .FirstOrDefaultAsync(e => e.Id == entity.Id);
+
 Console.WriteLine($"Entity queried from DB: {entityToDelete!.Id}");
 
 entityToDelete.IsDeleted = true;
@@ -26,9 +29,36 @@ Console.WriteLine("Entity soft deleted");
 var entityAfterDelete = await dbContext.SoftDeleteEntities.FirstOrDefaultAsync(e => e.Id == entity.Id);
 Console.WriteLine($"Entity queried from DB after soft delete: {entityAfterDelete?.Id.ToString() ?? "null"}");
 
+
+// Disabling Global Query Filters
+
 var entityAfterDeleteWithDisabledQueryFilter = await dbContext.SoftDeleteEntities
     .IgnoreQueryFilters()
     .FirstOrDefaultAsync(e => e.Id == entity.Id);
 
 Console.WriteLine("Entity queried from DB after soft delete with disabled query filter: " +
     entityAfterDeleteWithDisabledQueryFilter?.Id.ToString() ?? "null");
+
+
+// Filtered Required Navigation Properties
+
+var navPropDbContext = serviceProvider.GetRequiredService<NavPropDbContext>();
+
+var child = new ChildEntity();
+var parent = new ParentEntity([child]);
+navPropDbContext.ParentEntities.Add(parent);
+await navPropDbContext.SaveChangesAsync();
+
+Console.WriteLine($"New parent added with id: {parent.Id}");
+Console.WriteLine($"New child added with id: {child.Id}");
+
+parent.IsDeleted = true;
+await navPropDbContext.SaveChangesAsync();
+
+Console.WriteLine("Parent soft deleted");
+
+var childAfterParentDelete = await navPropDbContext.Children
+    .Include(c => c.Parent)
+    .FirstOrDefaultAsync(c => c.Id == child.Id);
+
+Console.WriteLine($"Child queried after parent is soft deleted: {childAfterParentDelete?.Id.ToString() ?? "null"}");
