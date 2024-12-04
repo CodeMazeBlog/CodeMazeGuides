d7ac212644a6ec9417e8eb2e3c9aeb4d129df30b
bspoljaric <bozo.spoljaric@gmail.com>
2024-12-04 19:58:30 +0100

    split long lines

--- csharp-intermediate-topics/PersistValuesInAsyncFlowUsingAsyncLocalInCSharp/PersistValuesInAsyncFlowUsingAsyncLocalInCSharp/AsyncLocalExample.cs
+++ csharp-intermediate-topics/PersistValuesInAsyncFlowUsingAsyncLocalInCSharp/PersistValuesInAsyncFlowUsingAsyncLocalInCSharp/AsyncLocalExample.cs
@@ -9,7 +9,8 @@ public static class AsyncLocalExample
         AsyncLocalInt.Value = 1;
         Console.WriteLine($"AsyncLocal value in DoMainWork method: {AsyncLocalInt.Value}");
         await DoSubTaskLevel1();
-        Console.WriteLine($"AsyncLocal value in DoMainWork method after executing DoSubTaskLevel1 method: {AsyncLocalInt.Value}");
+        Console.WriteLine($"AsyncLocal value in DoMainWork method after executing " +
+            $"DoSubTaskLevel1 method: {AsyncLocalInt.Value}");
     }
 
     private static async Task DoSubTaskLevel1()
@@ -18,7 +19,8 @@ public static class AsyncLocalExample
         AsyncLocalInt.Value++;
         Console.WriteLine($"AsyncLocal value after changing in DoSubTaskLevel1 method: {AsyncLocalInt.Value}");
         await DoSubTaskLevel2();
-        Console.WriteLine($"AsyncLocal value in DoSubTaskLevel1 method after executing DoSubTaskLevel2 method: {AsyncLocalInt.Value}");
+        Console.WriteLine($"AsyncLocal value in DoSubTaskLevel1 method after executing " +
+            $"DoSubTaskLevel2 method: {AsyncLocalInt.Value}");
     }
 
     private static async Task DoSubTaskLevel2()
