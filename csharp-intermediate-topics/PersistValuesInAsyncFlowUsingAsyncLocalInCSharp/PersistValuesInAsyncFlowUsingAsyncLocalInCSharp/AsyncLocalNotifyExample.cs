d6632e5705da92d39267d35b2e03a2e6ad7d693d
bozo-spoljaric <40840193+bozo-spoljaric@users.noreply.github.com>
2024-12-11 21:01:22 +0100

    CM-1078: Persist Values Across An Async Flow With The AsyncLocal Class in C# (#2064)
    * persist values in async flow with AsyncLocal demo
    
    * split long lines
    
    * .NET 9, spaces

---
+++ csharp-intermediate-topics/PersistValuesInAsyncFlowUsingAsyncLocalInCSharp/PersistValuesInAsyncFlowUsingAsyncLocalInCSharp/AsyncLocalNotifyExample.cs
@@ -0,0 +1,39 @@
+﻿namespace PersistValuesInAsyncFlowUsingAsyncLocalInCSharp;
+
+public static class AsyncLocalNotifyExample
+{
+    public static readonly AsyncLocal<string> AsyncLocalString = new(AsyncLocalValueChangedAction);
+
+    static Action<AsyncLocalValueChangedArgs<string>> AsyncLocalValueChangedAction =>
+        asyncLocalValueChangedArgs => Console.WriteLine($"Current: {asyncLocalValueChangedArgs.CurrentValue}, " +
+                               $"Previous: {asyncLocalValueChangedArgs.PreviousValue}, " +
+                               $"Thread: {Environment.CurrentManagedThreadId}, " +
+                               $"ThreadContextChanged: {asyncLocalValueChangedArgs.ThreadContextChanged}");
+
+    public static async Task DoWork()
+    {
+        AsyncLocalString.Value = "Enter DoWork method";
+
+        await DoSubTaskLevel1();
+
+        AsyncLocalString.Value = "Exit DoWork method";
+    }
+
+    private static async Task DoSubTaskLevel1()
+    {
+        AsyncLocalString.Value = "Enter DoSubTaskLevel1 method";
+
+        await DoSubTaskLevel2();
+
+        AsyncLocalString.Value = "Exit DoSubTaskLevel1 method";
+    }
+
+    private static async Task DoSubTaskLevel2()
+    {
+        AsyncLocalString.Value = "Enter DoSubTaskLevel2 method";
+
+        await Task.Delay(100);
+
+        AsyncLocalString.Value = "Exit DoSubTaskLevel2 method";
+    }
+}
