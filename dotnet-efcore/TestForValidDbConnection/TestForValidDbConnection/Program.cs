efa6cd04b6c4a1dfd8a2c8e1fe26ece3430000e9
Georgi Georgiev <51854143+georgy93@users.noreply.github.com>
2025-01-09 13:41:19 +0200

    CM-935: Testing Database Connection with EF Core (#2070)
    * CM-935 Check db connection with ef core
    
    * remove unused packages
    
    * Flipped using and namespace declaration
    
    * disable container reuse
    
    * Use IClassFixture and DI instead of manualy calling WebApplicationFactory constructor
    
    ---------
    
    Co-authored-by: Florian Prinz (FLPR) <flpr@simcorp.com>

---
+++ dotnet-efcore/TestForValidDbConnection/TestForValidDbConnection/Program.cs
@@ -0,0 +1,61 @@
+using DotNet.Testcontainers.Builders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using TestForValidDbConnection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var user = "sa";
+var password = "$trongPassword";
+var portNumber = 1433;
+
+// https://medium.com/codenx/integration-testing-using-testcontainers-in-net-8-520e8911d081
+var container = new ContainerBuilder()
+                .WithName("SqlServerTestContainerDb")
+                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+                .WithPortBinding(portNumber, true)
+                .WithEnvironment("ACCEPT_EULA", "Y")
+                .WithEnvironment("SQLCMDUSER", user)
+                .WithEnvironment("SQLCMDPASSWORD", password)
+                .WithEnvironment("MSSQL_SA_PASSWORD", password)
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(portNumber))
+                .Build();
+
+await container.StartAsync();
+
+var host = container.Hostname;
+var port = container.GetMappedPublicPort(portNumber);
+
+var connectionString =
+    $"Server={host},{port};Database=master;User Id={user};Password={password};TrustServerCertificate=True";
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString),
+    contextLifetime: ServiceLifetime.Scoped);
+
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<ApplicationDbContext>("Database Health", failureStatus: HealthStatus.Unhealthy);
+
+var app = builder.Build();
+
+app.Lifetime.ApplicationStopped.Register(() =>
+{
+    container.StopAsync().GetAwaiter().GetResult();
+    container.DisposeAsync().GetAwaiter().GetResult();
+});
+
+app.UseHttpsRedirection();
+app.MapHealthChecks("/health");
+
+app.MapGet("/can-connect", async (ApplicationDbContext applicationDbContext, CancellationToken cancellationToken) =>
+{
+    var canConnect = await applicationDbContext.Database.CanConnectAsync(cancellationToken);
+
+    return canConnect
+        ? Results.Ok("Connected successfully")
+        : Results.Problem("Cannot connect", statusCode: StatusCodes.Status503ServiceUnavailable);
+});
+
+await app.RunAsync();
+
+public partial class Program { }
---
+++ dotnet-efcore/TestForValidDbConnection/Tests/TestForValidDbConnection.LiveTests/ConnectionStateTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace TestForValidDbConnection.LiveTests;
+
+public class ConnectionStateLiveTests(WebApplicationFactory<Program> Factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task GivenDatabaseIsAvailable_WhenRequestingConnectivityStatus_ThenReturnOk()
+    {
+        // Arrange
+        var requestUri = "/can-connect";
+        var client = Factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(requestUri);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GivenDatabaseIsAvailable_WhenRequestingHealthCheckStatus_ThenReturnOk()
+    {
+        // Arrange
+        var requestUri = "/health";
+        var client = Factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(requestUri);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
