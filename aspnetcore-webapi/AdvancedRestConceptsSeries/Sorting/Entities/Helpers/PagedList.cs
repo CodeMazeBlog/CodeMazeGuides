4558df3531f02566f3067598c5014c0500608d4f
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2026-08-30 17:06:23 +0200

    Advanced REST Concepts series: restore the PagedList parameterless constructor so XML responses serialize

--- aspnetcore-webapi/AdvancedRestConceptsSeries/Sorting/Entities/Helpers/PagedList.cs
+++ aspnetcore-webapi/AdvancedRestConceptsSeries/Sorting/Entities/Helpers/PagedList.cs
@@ -16,6 +16,10 @@ public class PagedList<T> : List<T>
     public bool HasPrevious => CurrentPage > 1;
     public bool HasNext => CurrentPage < TotalPages;
 
+    public PagedList()
+    {
+    }
+
     public PagedList(List<T> items, int count, int pageNumber, int pageSize)
     {
         TotalCount = count;
