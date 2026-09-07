b13bba3dceed0b0a203488967da23963e5851a1e
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2026-09-07 13:42:30 +0200

    ConvertStringAndIntToEnum: retarget net10.0, MSTest 4.3.3, rename DayOfWeek to WeekDay, generic Enum overloads
    Rename the sample enum off DayOfWeek so it no longer collides with
    System.DayOfWeek, which declares the opposite member ordering.
    
    Lift Enum.Parse and Enum.IsDefined to the generic overloads, move the
    three files to file-scoped namespaces, and add two tests: a numeric
    string that is not a member parsed through Enum.TryParse, and a mask
    check over the declared UserType flags for the values 0 through 8.

--- csharp-basic-topics/ConvertStringAndIntToEnum/ConvertStringAndIntToEnumTests/ConvertStringAndIntToEnumTests.cs
+++ csharp-basic-topics/ConvertStringAndIntToEnum/ConvertStringAndIntToEnumTests/ConvertStringAndIntToEnumTests.cs
@@ -1,85 +1,123 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace ConvertStringAndIntToEnumTests
+namespace ConvertStringAndIntToEnumTests;
+
+[TestClass]
+public class ConvertStringAndIntToEnumTests
 {
-    [TestClass]
-    public class ConvertStringAndIntToEnumTests
+    [TestMethod]
+    public void GivenValidEnumAsString_WhenConvertingToEnum_ThenCorrectlyConverted()
     {
-        [TestMethod]
-        public void GivenValidEnumAsString_WhenConvertingToEnum_ThenCorrectlyConverted()
+        var inputString = "Sunday";
+        var mixedCaseInputString = "SaTurDaY";
+
+        var weekDay = Enum.Parse<WeekDay>(inputString);
+        var caseInsensitiveWeekDay = Enum.Parse<WeekDay>(mixedCaseInputString, true);
+
+        Assert.AreEqual(WeekDay.Sunday, weekDay);
+        Assert.AreEqual(WeekDay.Saturday, caseInsensitiveWeekDay);
+    }
+
+    [TestMethod]
+    public void GivenInvalidEnumAsString_WhenConvertingToEnum_ThenFails()
+    {
+        var inputString = "Today";
+
+        var isEnumParsed = Enum.TryParse(inputString, true, out WeekDay weekDay);
+
+        Assert.IsFalse(isEnumParsed);
+    }
+
+    [TestMethod]
+    public void GivenValidEnumAsStringInteger_WhenConvertingToEnum_ThenCorrectlyConverted()
+    {
+        var inputString = "0";
+
+        var isEnumParsed = Enum.TryParse(inputString, true, out WeekDay weekDay);
+
+        Assert.IsTrue(isEnumParsed);
+        Assert.AreEqual(WeekDay.Monday, weekDay);
+    }
+
+    [TestMethod]
+    public void GivenNumericStringThatIsNotAMember_WhenConvertingToEnum_ThenParsesIntoAnUndefinedValue()
+    {
+        var inputString = "42";
+
+        var isEnumParsed = Enum.TryParse<WeekDay>(inputString, out var weekDay);
+
+        Assert.IsTrue(isEnumParsed);
+        Assert.IsFalse(Enum.IsDefined(weekDay));
+        Assert.AreEqual("42", weekDay.ToString());
+    }
+
+    [TestMethod]
+    public void GivenValidEnumAsInteger_WhenConvertingToEnum_ThenCorrectlyConverted()
+    {
+        var inputInt = 2;
+
+        var isEnumParsed = Enum.IsDefined((WeekDay)inputInt);
+
+        Assert.IsTrue(isEnumParsed);
+        WeekDay weekDay = (WeekDay)inputInt;
+        Assert.AreEqual(WeekDay.Wednesday, weekDay);
+    }
+
+    [TestMethod]
+    public void GivenInvalidEnumAsInteger_WhenConvertingToEnum_ThenFails()
+    {
+        var inputInt = 9;
+
+        var isEnumParsed = Enum.IsDefined((WeekDay)inputInt);
+
+        Assert.IsFalse(isEnumParsed);
+    }
+
+    [TestMethod]
+    public void GivenValidFlagsEnumAsInteger_WhenConvertingToEnum_ThenCorrectlyConverted()
+    {
+        var inputInt = 3;
+        var parsedEnum = (UserType)inputInt;
+
+        var isEnumParsed = Enum.IsDefined(parsedEnum) || parsedEnum.ToString().Contains(",");
+
+        Assert.IsTrue(isEnumParsed);
+        Assert.AreEqual(UserType.Customer | UserType.Driver, parsedEnum);
+    }
+
+    [TestMethod]
+    public void GivenInvalidFlagsEnumAsInteger_WhenConvertingToEnum_ThenFails()
+    {
+        var inputInt = 8;
+        var parsedEnum = (UserType)inputInt;
+
+        var isEnumParsed = Enum.IsDefined(parsedEnum) || parsedEnum.ToString().Contains(",");
+
+        Assert.IsFalse(isEnumParsed);
+    }
+
+    [TestMethod]
+    public void GivenFlagsEnumValues_WhenCheckingAgainstTheDeclaredFlagsMask_ThenOnlyDeclaredCombinationsAreValid()
+    {
+        var allFlags = (UserType)0;
+
+        foreach (var flag in Enum.GetValues<UserType>())
         {
-            var inputString = "Sunday";
-            
-            DayOfWeek dayOfWeek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), inputString);
-            
-            Assert.AreEqual(DayOfWeek.Sunday, dayOfWeek);
+            allFlags |= flag;
         }
 
-        [TestMethod]
-        public void GivenInvalidEnumAsString_WhenConvertingToEnum_ThenFails()
-        {
-            var inputString = "Today";
-            
-            var isEnumParsed = Enum.TryParse(inputString, true, out DayOfWeek dayOfWeek);
-            
-            Assert.IsFalse(isEnumParsed);
-        }
+        Assert.AreEqual((UserType)7, allFlags);
 
-        [TestMethod]
-        public void GivenValidEnumAsStringInteger_WhenConvertingToEnum_ThenCorrectlyConverted()
+        for (var inputInt = 0; inputInt <= 7; inputInt++)
         {
-            var inputString = "0";
-            
-            var isEnumParsed = Enum.TryParse(inputString, true, out DayOfWeek dayOfWeek);
-            
-            Assert.IsTrue(isEnumParsed);
-            Assert.AreEqual(DayOfWeek.Monday, dayOfWeek);
-        }
-
-        [TestMethod]
-        public void GivenValidEnumAsInteger_WhenConvertingToEnum_ThenCorrectlyConverted()
-        {
-            var inputInt = 2;
-            
-            var isEnumParsed = Enum.IsDefined(typeof(DayOfWeek), inputInt);
-            
-            Assert.IsTrue(isEnumParsed);
-            DayOfWeek dayOfWeek = (DayOfWeek)inputInt;
-            Assert.AreEqual(DayOfWeek.Wednesday, dayOfWeek);
-        }
-
-        [TestMethod]
-        public void GivenInvalidEnumAsInteger_WhenConvertingToEnum_ThenFails()
-        {
-            var inputInt = 9;
-            
-            var isEnumParsed = Enum.IsDefined(typeof(DayOfWeek), inputInt);
-            
-            Assert.IsFalse(isEnumParsed);
-        }
-
-        [TestMethod]
-        public void GivenValidFlagsEnumAsInteger_WhenConvertingToEnum_ThenCorrectlyConverted()
-        {
-            var inputInt = 3;
             var parsedEnum = (UserType)inputInt;
-            
-            var isEnumParsed = Enum.IsDefined(typeof(UserType), inputInt) || parsedEnum.ToString().Contains(",");
-            
-            Assert.IsTrue(isEnumParsed);
-            Assert.AreEqual(UserType.Customer| UserType.Driver, parsedEnum);
+
+            Assert.IsTrue((parsedEnum & ~allFlags) == 0, $"{inputInt} should be a valid combination");
         }
 
-        [TestMethod]
-        public void GivenInvalidFlagsEnumAsInteger_WhenConvertingToEnum_ThenFails()
-        {
-            var inputInt = 8;
-            var parsedEnum = (UserType)inputInt;
-            
-            var isEnumParsed = Enum.IsDefined(typeof(UserType), inputInt) || parsedEnum.ToString().Contains(",");
-            
-            Assert.IsFalse(isEnumParsed);
-        }
+        var invalidEnum = (UserType)8;
+
+        Assert.IsFalse((invalidEnum & ~allFlags) == 0);
     }
 }
