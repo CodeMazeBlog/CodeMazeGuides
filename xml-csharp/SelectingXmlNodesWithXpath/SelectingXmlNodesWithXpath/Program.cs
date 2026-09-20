69842fc98306bdb4ad69624efe87fa76c2054ea4
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2026-09-20 11:11:43 +0200

    Selecting XML nodes with XPath: retarget net10.0, drop null-forgiving operators, add LINQ to XML XPath method and tests (#2199)

--- xml-csharp/SelectingXmlNodesWithXpath/SelectingXmlNodesWithXpath/Program.cs
+++ xml-csharp/SelectingXmlNodesWithXpath/SelectingXmlNodesWithXpath/Program.cs
@@ -1,13 +1,16 @@
 ﻿using SelectingXmlNodesWithXpath;
 using System.Xml;
+using System.Xml.Linq;
+
+var path = Path.Combine(AppContext.BaseDirectory, "BooksCatalog.xml");
 
 var doc = new XmlDocument();
-doc.Load("BooksCatalog.xml");
+doc.Load(path);
 var root = doc.DocumentElement!;
 
 Console.WriteLine("Selected book:");
 var singleResult = XmlNodesSelector.SelectSingleBook(root);
-Console.WriteLine(singleResult);
+Console.WriteLine(singleResult ?? "No book matched the query.");
 
 Console.WriteLine("\nSelected books:");
 var results = XmlNodesSelector.SelectBooks(root);
@@ -16,3 +19,7 @@ results.ForEach(Console.WriteLine);
 Console.WriteLine("\nSelected books:");
 var resultsFromNamespaces = XmlNodesSelector.SelectBooksUsingNamespaces(doc);
 resultsFromNamespaces.ForEach(Console.WriteLine);
+
+Console.WriteLine("\nSelected books with LINQ to XML:");
+var linqResults = XmlNodesSelector.SelectBooksWithLinqToXml(XDocument.Load(path));
+linqResults.ForEach(Console.WriteLine);
